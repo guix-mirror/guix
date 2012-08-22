24cf75b9ef6818a228c9e071dea08bf97e3d8c4f
Ludovic Courtès <ludo@gnu.org>
2012-08-22 17:24:38 +0200

    distro: Add XZ.
    * distro/base.scm (xz): New variable.

--- distro/base.scm
+++ distro/base.scm
@@ -205,6 +205,33 @@ superior compression ratio of gzip is just a bonus.")
    (license "GPLv3+")
    (home-page "http://www.gnu.org/software/gzip/")))
 
+(define-public xz
+  (package
+   (name "xz")
+   (version "5.0.4")
+   (source (origin
+            (method http-fetch)
+            (uri (string-append "http://tukaani.org/xz/xz-" version
+                                ".tar.gz"))
+            (sha256
+             (base32
+              "1dl35ca8fdss9z2d6y234gxh24ixq904xksizrjmjr5dimwhax6n"))))
+   (build-system gnu-build-system)
+   (description
+    "XZ, general-purpose data compression software, successor of LZMA")
+   (long-description
+    "XZ Utils is free general-purpose data compression software with high
+compression ratio.  XZ Utils were written for POSIX-like systems, but also
+work on some not-so-POSIX systems.  XZ Utils are the successor to LZMA Utils.
+
+The core of the XZ Utils compression code is based on LZMA SDK, but it has
+been modified quite a lot to be suitable for XZ Utils.  The primary
+compression algorithm is currently LZMA2, which is used inside the .xz
+container format.  With typical files, XZ Utils create 30 % smaller output
+than gzip and 15 % smaller output than bzip2.")
+   (license '("GPLv2+" "LGPLv2.1+"))              ; bits of both
+   (home-page "http://tukaani.org/xz/")))
+
 (define-public patch
   (package
    (name "patch")
