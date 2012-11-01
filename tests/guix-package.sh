0afdc48532ea7d8eea32b3e3b78ba3832e7f18b2
Ludovic Courtès <ludo@gnu.org>
2012-11-01 01:46:15 +0100

    Add a preliminary `guix-package' command-line tool.
    * guix-package.in, tests/guix-package.sh: New files.
    * configure.ac: Output `guix-package'.
    * Makefile.am (TESTS): Add `tests/guix-package.sh'.
      (bin_SCRIPTS): Add `guix-package'.

---
+++ tests/guix-package.sh
@@ -0,0 +1,43 @@
+# Guix --- Nix package management from Guile.         -*- coding: utf-8 -*-
+# Copyright (C) 2012 Ludovic Courtès <ludo@gnu.org>
+#
+# This file is part of Guix.
+#
+# Guix is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.
+#
+# Guix is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Test the `guix-package' command-line utility.
+#
+
+guix-package --version
+
+profile="t-profile-$$"
+rm -f "$profile"
+
+guix-package -b -p "$profile"						\
+    -i `guix-build -e '(@@ (distro packages base) %bootstrap-guile)'`
+test -L "$profile" && test -L "$profile-1-link"
+test -f "$profile/bin/guile"
+
+
+guix-package -b -p "$profile"						\
+    -i `guix-build -e '(@@ (distro packages base) gnu-make-boot0)'`
+test -L "$profile-2-link"
+test -f "$profile/bin/make" && test -f "$profile/bin/guile"
+
+guix-package -b -p "$profile" -r "guile-bootstrap-2.0"
+test -L "$profile-3-link"
+test -f "$profile/bin/make" && ! test -f "$profile/bin/guile"
+
+rm "$profile" "$profile-"[0-9]*
