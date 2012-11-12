92b8d1ea8b2dab6df9d76b2bf8d78e33fec197f1
Ludovic Courtès <ludo@gnu.org>
2012-11-13 00:40:29 +0100

    tests: Make sure to use the bootstrap Guile.
    * tests/builders.scm ("url-fetch", "gnu-build"): Pass `url-fetch'
      #:guile %BOOTSTRAP-GUILE.

--- tests/builders.scm
+++ tests/builders.scm
@@ -58,7 +58,8 @@
                      "ftp://ftp.gnu.org/gnu/hello/hello-2.8.tar.gz"))
          (hash     (nix-base32-string->bytevector
                     "0wqd8sjmxfskrflaxywc7gqw7sfawrfvdxd9skxawzfgyy0pzdz6"))
-         (drv-path (url-fetch %store url 'sha256 hash))
+         (drv-path (url-fetch %store url 'sha256 hash
+                              #:guile %bootstrap-guile))
          (out-path (derivation-path->output-path drv-path)))
     (and (build-derivations %store (list drv-path))
          (file-exists? out-path)
@@ -72,7 +73,8 @@
   (let* ((url      "http://ftp.gnu.org/gnu/hello/hello-2.8.tar.gz")
          (hash     (nix-base32-string->bytevector
                     "0wqd8sjmxfskrflaxywc7gqw7sfawrfvdxd9skxawzfgyy0pzdz6"))
-         (tarball  (url-fetch %store url 'sha256 hash))
+         (tarball  (url-fetch %store url 'sha256 hash
+                              #:guile %bootstrap-guile))
          (build    (gnu-build %store "hello-2.8" tarball
                               %bootstrap-inputs
                               #:implicit-inputs? #f
