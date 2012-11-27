5321f74f234742ad9e039de33e60288a9a830949
Ludovic Courtès <ludo@gnu.org>
2012-11-27 21:33:00 +0100

    distro: flex: Fix module name.
    * distro/packages/flex.scm: Change the module name to (distro packages
      flex).

--- distro/packages/flex.scm
+++ distro/packages/flex.scm
@@ -16,7 +16,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (distro packages bison)
+(define-module (distro packages flex)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
