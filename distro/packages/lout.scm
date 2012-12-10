836676450dbfc14e675a68889ac5f3d5ab8cebfa
Ludovic Courtès <ludo@gnu.org>
2012-12-10 14:35:03 +0100

    distro: lout: Delay use of of `nixpkgs-derivation'.
    * distro/packages/lout.scm (lout): Don't fork with `nixpkgs-derivation',
      to avoid any issues in the absence of `nix-instantiate'.

--- distro/packages/lout.scm
+++ distro/packages/lout.scm
@@ -88,7 +88,8 @@
                "12gkyqrn0kaa8xq7sc7v3wm407pz2fxg9ngc75aybhi5z825b9vq"))))
     (build-system gnu-build-system)               ; actually, just a makefile
     (outputs '("out" "doc"))
-    (inputs `(("ghostscript" ,(nixpkgs-derivation "ghostscript"))))
+    (inputs `(("ghostscript" ,(lambda _    ; FIXME: replace with our own
+                                (nixpkgs-derivation "ghostscript")))))
     (arguments `(#:modules ((guix build utils)
                             (guix build gnu-build-system)
                             (srfi srfi-1))        ; we need SRFI-1
