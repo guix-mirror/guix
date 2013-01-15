a01b6da792e40ada0ddac4256e58e35e44797142
Nikita Karetnikov <nikita@karetnikov.org>
2013-01-15 02:29:28 +0000

    distro: Add Python 2.7.3.
    * distro/packages/python.scm: New file.
    * Makefile.am (MODULES): Add it.

---
+++ distro/packages/python.scm
@@ -0,0 +1,58 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Nikita Karetnikov <nikita@karetnikov.org>
+;;; Copyright © 2013 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (distro packages python)
+  #:use-module ((guix licenses) #:select (psfl))
+  #:use-module (distro packages compression)
+  #:use-module (distro packages openssl)
+  #:use-module (distro packages base)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public python
+  (package
+    (name "python")
+    (version "2.7.3")
+    (source
+     (origin
+      (method url-fetch)
+      (uri (string-append "http://www.python.org/ftp/python/"
+                          version "/Python-" version ".tar.xz"))
+      (sha256
+       (base32
+        "11f9aw855lrmknr6c82gm1ijr3n0smc6idyp94y7774yivjnplv1"))))
+    (build-system gnu-build-system)
+    (arguments `(#:tests? #f)) ; XXX: some tests fail
+    (inputs
+     `(("zlib" ,zlib)
+       ("openssl" ,openssl)
+       ("bzip2" ,bzip2)))
+    (home-page "http://python.org")
+    (synopsis
+     "Python, a high-level dynamically-typed programming language")
+    (description
+     "Python is a remarkably powerful dynamic programming language that
+is used in a wide variety of application domains.  Some of its key
+distinguishing features include: clear, readable syntax; strong
+introspection capabilities; intuitive object orientation; natural
+expression of procedural code; full modularity, supporting hierarchical
+packages; exception-based error handling; and very high level dynamic
+data types.")
+    (license psfl)))
