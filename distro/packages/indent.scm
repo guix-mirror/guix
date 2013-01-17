c1e628f6ce95eef2ae3426f0ef1394733b0198ea
Andreas Enge <andreas@enge.fr>
2013-01-17 21:15:09 +0100

    distro: Add Indent.
    * distro/packages/indent.scm: New file.
    * Makefile.am (MODULES): Add it.

---
+++ distro/packages/indent.scm
@@ -0,0 +1,44 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Andreas Enge <andreas@enge.fr>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (distro packages indent)
+  #:use-module (distro)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public indent
+  (package
+   (name "indent")
+   (version "2.2.10")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "mirror://gnu/indent/indent-" version
+                                ".tar.gz"))
+            (sha256 (base32
+                     "0f9655vqdvfwbxvs1gpa7py8k1z71aqh8hp73f65vazwbfz436wa"))))
+   (build-system gnu-build-system)
+   (synopsis "GNU Indent, a program for code indentation and formatting")
+   (description
+    "GNU Indent can be used to make code easier to read. It can also convert
+from one style of writing C to another. Indent understands a substantial
+amount about the syntax of C, but it also attempts to cope with incomplete
+and misformed syntax. The GNU style of indenting is the default.")
+   (license gpl3+)
+   (home-page "http://www.gnu.org/software/indent/")))
