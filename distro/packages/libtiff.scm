082b3c6a6ccf5fc08b817df9d6e3c16131c11ea8
Andreas Enge <andreas@enge.fr>
2013-01-18 00:14:42 +0100

    distro: Add libtiff.
    * distro/packages/libtiff.scm: New file.
    * Makefile.am (MODULES): Add it.

---
+++ distro/packages/libtiff.scm
@@ -0,0 +1,65 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Andreas Enge <andreas@enge.fr>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (distro packages libtiff)
+  #:use-module (distro)
+  #:use-module (distro packages file)
+  #:use-module (distro packages libjpeg)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public libtiff
+  (package
+   (name "libtiff")
+   (version "4.0.3")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "ftp://ftp.remotesensing.org/pub/libtiff/tiff-"
+                   version ".tar.gz"))
+            (sha256 (base32
+                     "0wj8d1iwk9vnpax2h29xqc2hwknxg3s0ay2d5pxkg59ihbifn6pa"))))
+   (build-system gnu-build-system)
+   (inputs `(("file" ,file)
+             ("libjpeg-8" ,libjpeg-8)))
+             ;; currently does not compile with libjpeg version 9
+   (arguments
+    `(#:configure-flags
+      (list (string-append "--with-jpeg-include-dir="
+                           (assoc-ref %build-inputs "libjpeg-8")
+                           "/include"))
+      #:phases
+      (alist-replace
+       'configure
+       (lambda* (#:key #:allow-other-keys #:rest args)
+        (let ((configure (assoc-ref %standard-phases 'configure)))
+          (substitute* "configure"
+            (("`/usr/bin/file")
+            (string-append "`" (which "file"))))
+          (apply configure args)))
+      %standard-phases)))
+   (synopsis "Libtiff, a library for handling TIFF files")
+   (description
+    "Libtiff provides support for the Tag Image File Format (TIFF), a format
+used for storing image data.
+Included are a library, libtiff, for reading and writing TIFF and a small
+collection of tools for doing simple manipulations of TIFF images.")
+   (license (bsd-style "file://COPYRIGHT"
+                       "See COPYRIGHT in the distribution."))
+   (home-page "http://www.libtiff.org/")))
