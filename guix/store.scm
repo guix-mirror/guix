guix-mirror/guix

commit 82c38fe64c84fc3febcc5c5aa7fe86454ccaf456

store: Micro-optimize `write-string'.
* guix/store.scm (write-string): Optimize to write the length, contents,
  and padding all at once.  This yields a 2% improvement on the
  execution time of "guix-build gdb".

guix/store.scm
@@ -158,10 +158,13 @@
             (put-bytevector p zero 0 (- 8 m)))))))
 
 (define (write-string s p)
-  (let ((b (string->utf8 s)))
-    (write-int (bytevector-length b) p)
-    (put-bytevector p b)
-    (write-padding (bytevector-length b) p)))
+  (let* ((s (string->utf8 s))
+         (l (bytevector-length s))
+         (m (modulo l 8))
+         (b (make-bytevector (+ 8 l (if (zero? m) 0 (- 8 m))))))
+    (bytevector-u64-native-set! b 0 l)
+    (bytevector-copy! s 0 b 8 l)
+    (put-bytevector p b)))
 
 (define (read-string p)
   (let* ((len (read-int p))

commit 238e43b9faa45c88fc2581e3d6d3a87187f3ec33

store: Write string length as a 32-bit integer.
* guix/store.scm (write-string): Write L as a 32-bit integer.  Bug
  introduced in 82c38fe64c84fc3febcc5c5aa7fe86454ccaf456 ("store:
  Micro-optimize `write-string'.")

guix/store.scm
@@ -162,7 +162,7 @@
          (l (bytevector-length s))
          (m (modulo l 8))
          (b (make-bytevector (+ 8 l (if (zero? m) 0 (- 8 m))))))
-    (bytevector-u64-native-set! b 0 l)
+    (bytevector-u32-set! b 0 l (endianness little))
     (bytevector-copy! s 0 b 8 l)
     (put-bytevector p b)))
 