2d73db7a05d0f83232e44d6524450daf028ba4f6
Ludovic Courtès <ludo@gnu.org>
2013-02-08 15:44:24 +0100

    gnu: Add PatchELF.
    * gnu/packages/patchelf.scm: New file.
    * Makefile.am (MODULES): Add it.

---
+++ gnu/packages/patchelf.scm
@@ -0,0 +1,47 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages patchelf)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public patchelf
+  (package
+    (name "patchelf")
+    (version "0.6")
+    (source (origin
+             (method url-fetch)
+             (uri (string-append
+                   "http://nixos.org/releases/patchelf/patchelf-"
+                   version
+                   "/patchelf-"
+                   version
+                   ".tar.bz2"))
+             (sha256
+              (base32
+               "00bw29vdsscsili65wcb5ay0gvg1w0ljd00sb5xc6br8bylpyzpw"))))
+    (build-system gnu-build-system)
+    (home-page "http://nixos.org/patchelf.html")
+    (synopsis
+     "A small utility to modify the dynamic linker and RPATH of ELF executables")
+    (description
+     "PatchELF allows the ELF \"interpreter\" and RPATH of an ELF binary to be
+changed.")
+    (license gpl3+)))
