2b6bdf7eb3c95716ac107ea6caea2e0b7077ae77
Ludovic Courtès <ludo@gnu.org>
2013-04-25 23:21:49 +0200

    gnu: libgcrypt: Update to 1.5.2.
    * gnu/packages/gnupg.scm (libgcrypt): Update to 1.5.2.

--- gnu/packages/gnupg.scm
+++ gnu/packages/gnupg.scm
@@ -57,14 +57,14 @@ Daemon and possibly more in the future.")
 (define-public libgcrypt
   (package
     (name "libgcrypt")
-    (version "1.5.1")
+    (version "1.5.2")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnupg/libgcrypt/libgcrypt-"
                                  version ".tar.bz2"))
              (sha256
               (base32
-               "09z5zbxhvg6c7n8qcm8h9ygr28qli2n83hfq1f69jsg711cb37md"))))
+               "0gwnzqd64cpwdmk93nll54nidsr74jpimxzj4p4z7502ylwl66p4"))))
     (build-system gnu-build-system)
     (propagated-inputs
      `(("libgpg-error" ,libgpg-error)))
