3ec7ad8e0f386fd24b2ce25b7198a2a4b7ff9f82
Ludovic Courtès <ludo@gnu.org>
2013-05-24 00:03:57 +0200

    gnu: Add UnRTF.
    * gnu/packages/unrtf.scm: New file.
    * Makefile.am (MODULES): Add it.

---
+++ gnu/packages/unrtf.scm
@@ -0,0 +1,57 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages unrtf)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public unrtf
+  (package
+    (name "unrtf")
+    (version "0.19.7")
+    (source (origin
+             (method url-fetch)
+             (uri (string-append "mirror://gnu/unrtf/" version
+                                 "/unrtf-" version ".tar.gz"))
+             (sha256
+              (base32
+               "1c15miv4ni395wix6qxiqgjfspd6ssdk1ly6mm8srh0jgpidgjzf"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:phases (alist-replace
+                 'configure
+                 (lambda* (#:key outputs #:allow-other-keys)
+                   ;; No `configure' script; do it by hand.
+                   (let ((out (assoc-ref outputs "out")))
+                     (substitute* "Makefile"
+                       (("/usr/local") out))))
+                 %standard-phases)
+
+       ;; There are test files, but apparently no automated test suite.
+       #:tests? #f))
+    (home-page "http://www.gnu.org/software/unrtf")
+    (synopsis "Convert Rich Text Format documents to other formats")
+    (description
+     "UnRTF is a command-line program written in C which converts documents in
+Rich Text Format (.rtf) to HTML, LaTeX, troff macros, and RTF
+itself. Converting to HTML, it supports a number of features of Rich Text
+Format, such as changes to the text's font, underlines, text shadowing and
+outlining, and more.")
+    (license gpl2+)))
