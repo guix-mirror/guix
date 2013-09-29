29a2ccd3841e4f82957e29b0adb34f1da8bfbd23
Aljosha Papsch <misc@rpapsch.de>
2013-09-29 20:55:47 +0200

    Add httpd
    * gnu/packages/web.scm (httpd): New variable
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

---
+++ gnu/packages/web.scm
@@ -0,0 +1,64 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Aljosha Papsch <misc@rpapsch.de>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages web)
+  #:use-module ((guix licenses)
+                #:renamer (symbol-prefix-proc 'l:))
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages apr)
+  #:use-module (gnu packages openssl)
+  #:use-module (gnu packages pcre))
+
+(define-public httpd
+  (package
+    (name "httpd")
+    (version "2.4.6")
+    (source (origin
+             (method url-fetch)
+             (uri (string-append "mirror://apache/httpd/httpd-"
+                                 version ".tar.bz2"))
+             (sha256
+              (base32
+               "1sig08xxq1kqxr2a42ndwr9g4mm6zdqnxldhxraym2y0xcjkd7yw"))))
+    (build-system gnu-build-system)
+    (inputs `(("apr" ,apr)
+              ("apr-util" ,apr-util)
+              ("openssl" ,openssl)
+              ("pcre" ,pcre)))
+    (arguments
+     `(#:test-target "test"
+       #:configure-flags (list "--enable-rewrite"
+                               "--enable-userdir"
+                               "--enable-vhost-alias"
+                               "--enable-ssl"
+                               "--enable-mime-magic"
+                               (string-append "--sysconfdir="
+                                              (assoc-ref %outputs "out")
+                                              "/etc/httpd"))))
+    (synopsis "Featureful HTTP server")
+    (description
+     "The Apache HTTP Server Project is a collaborative software development
+effort aimed at creating a robust, commercial-grade, featureful, and
+freely-available source code implementation of an HTTP (Web) server. The project
+is jointly managed by a group of volunteers located around the world, using the
+Internet and the Web to communicate, plan, and develop the server and its
+related documentation.")
+    (license l:asl2.0)
+    (home-page "https://httpd.apache.org/")))
