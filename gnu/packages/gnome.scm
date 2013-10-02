d9c1a22b1272b1109e4e4582eb962b5a56cd768c
Andreas Enge <andreas@enge.fr>
2013-10-02 13:19:21 +0200

    gnu: gnome: Add libnotify.
    * gnu/packages/gnome.scm (libnotify): New variable.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -17,11 +17,13 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages gnome)
-  #:use-module ((guix licenses) #:select (gpl2+))
+  #:use-module ((guix licenses) #:select (gpl2+ lgpl2.1+))
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages glib)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages libpng)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages xml))
@@ -55,3 +57,33 @@
 Gnome project.  It includes xml2po tool which makes it easier to translate
 and keep up to date translations of documentation.")
     (license gpl2+))) ; xslt under lgpl
+
+(define-public libnotify
+  (package
+    (name "libnotify")
+    (version "0.7.6")
+    (source
+     (origin
+      (method url-fetch)
+      (uri (string-append "mirror://gnome/sources/" name "/"
+                          (string-copy version 0 (string-rindex version #\.)) "/"
+                          name "-" version ".tar.xz"))
+      (sha256
+       (base32
+        "0dyq8zgjnnzcah31axnx6afb21kl7bks1gvrg4hjh3nk02j1rxhf"))))
+    (build-system gnu-build-system)
+    (inputs
+     `(("gdk-pixbuf" ,gdk-pixbuf)
+       ("glib" ,glib)
+       ("gtk+" ,gtk+)
+       ("libpng" ,libpng)
+       ("pkg-config" ,pkg-config)))
+    (home-page "https://developer-next.gnome.org/libnotify/")
+    (synopsis
+     "GNOME desktop notification library")
+    (description
+     "Libnotify is a library that sends desktop notifications to a
+notification daemon, as defined in the Desktop Notifications spec. These
+notifications can be used to inform the user about an event or display
+some form of information without getting in the user's way.")
+    (license lgpl2.1+)))
