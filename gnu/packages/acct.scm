f349fa02def606e20e8e33e76b847b13038612c6
Nikita Karetnikov <nikita@karetnikov.org>
2013-10-10 10:32:15 +0000

    gnu: Add GNU acct.
    * gnu/packages/acct.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/acct.scm
@@ -0,0 +1,44 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Nikita Karetnikov <nikita@karetnikov.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages acct)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public acct
+  (package
+    (name "acct")
+    (version "6.6.1")
+    (source
+     (origin
+      (method url-fetch)
+      (uri (string-append "mirror://gnu/acct/acct-"
+                          version ".tar.gz"))
+      (sha256
+       (base32
+        "1jzz601cavml7894fjalw661gz28ia35002inw990agr3rhiaiam"))))
+    (build-system gnu-build-system)
+    (home-page "https://gnu.org/software/acct/")
+    (synopsis "Standard login and process accounting utilities")
+    (description
+     "GNU acct provides a means for system administrators to determine
+system usage patterns.  It provides information on, for example, connections,
+programs executed, and system resources used.")
+    (license gpl3+)))
