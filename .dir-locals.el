35ef3633001b05c25324c6e5a69d635ddc62cb6d
Ludovic Courtès <ludo@gnu.org>
2013-11-07 00:18:20 +0100

    Add indentation rule for 'origin'.

--- .dir-locals.el
+++ .dir-locals.el
@@ -14,6 +14,7 @@
    (eval . (put 'substitute* 'scheme-indent-function 1))
    (eval . (put 'with-directory-excursion 'scheme-indent-function 1))
    (eval . (put 'package 'scheme-indent-function 0))
+   (eval . (put 'origin 'scheme-indent-function 0))
    (eval . (put 'manifest-entry 'scheme-indent-function 0))
    (eval . (put 'manifest-pattern 'scheme-indent-function 0))
    (eval . (put 'substitute-keyword-arguments 'scheme-indent-function 1))
