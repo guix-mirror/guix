e3aaefe71bd26daf6fdbfd0634f68a90985e059b
Eric Bavier <bavier@member.fsf.org>
2013-11-20 11:05:57 -0600

    gnu: Add GNU Parallel.
    * gnu/packages/parallel.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

---
+++ gnu/packages/parallel.scm
@@ -0,0 +1,47 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 Eric Bavier <bavier@member.fsf.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages parallel)
+  #:use-module (guix packages)
+  #:use-module (guix licenses)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages perl))
+
+(define-public parallel
+  (package
+    (name "parallel")
+    (version "20131022")
+    (source
+     (origin
+      (method url-fetch)
+      (uri (string-append "mirror://gnu/parallel/parallel-"
+                          version ".tar.bz2"))
+      (sha256
+       (base32
+        "1ydn8aj72wfjdvldzjwah9cvqay8vzr3dbspa5l0g2y10dx0qa4k"))))
+    (build-system gnu-build-system)
+    (inputs `(("perl" ,perl))) 
+    (home-page "http://www.gnu.org/software/parallel/")
+    (synopsis "Build and execute command lines in parallel")
+    (description
+     "GNU Parallel is a tool for executing shell jobs in parallel using one
+or more computers.  Jobs can consist of single commands or of scripts
+and they are executed on lists of files, hosts, users or other items.")
+    (license gpl3+)))
