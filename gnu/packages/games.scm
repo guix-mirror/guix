490c6528edaa8db1acbcc0fa6b6bad6f436a0e95
John Darrington <jmd@gnu.org>
2013-12-28 08:19:43 +0100

    gnu: games: New module
    * gnu/packages/games.scm (gnubik): New variable
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

---
+++ gnu/packages/games.scm
@@ -0,0 +1,59 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2013 John Darrington <jmd@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages games)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (guix build-system gnu))
+
+(define-public gnubik
+  (package
+    (name "gnubik")
+    (version "2.4.1")
+    (source
+     (origin
+      (method url-fetch)
+      (uri (string-append "mirror://gnu/gnubik/gnubik-"
+                          version ".tar.gz"))
+      (sha256
+       (base32
+        "0mfpwz341i1qpzi2qgslpc5i7d4fv7i01kv392m11pczqdc7i7m5"))))
+    (build-system gnu-build-system)
+    (inputs `(("gtk+" ,gtk+-2)
+              ("mesa" ,mesa)
+              ("libx11" ,libx11)
+              ("guile" ,guile-2.0)
+              ("gtkglext" ,gtkglext)))
+    (native-inputs `(("gettext" ,gnu-gettext)
+                     ("pkg-config" ,pkg-config)))
+    (home-page "https://www.gnu.org/software/gnubik/")
+    (synopsis "3d Rubik's cube game.")
+    (description "GNUbik is a puzzle game in which you must manipulate a cube to make
+each of its faces have a uniform color.  The game is customizable, allowing
+you to set the size of the cube (the default is 3x3) or to change the colors.
+You may even apply photos to the faces instead of colors.  The game is
+scriptable with Guile.")
+    (license gpl3+)))
