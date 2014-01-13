4aeea896f8de7c1643c9462b2080ff84314afc79
Ludovic Courtès <ludo@gnu.org>
2014-01-13 21:32:39 +0100

    gnu: Rename (gnu packages system) to (gnu packages admin).
    * gnu/packages/system.scm: Rename to...
    * gnu/packages/admin.scm: ... this.  Adjust module name accordingly.
    * gnu-system.am (GNU_SYSTEM_MODULES): Update.

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -17,7 +17,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (gnu packages system)
+(define-module (gnu packages admin)
   #:use-module (guix licenses)
   #:use-module (guix packages)
   #:use-module (guix download)
