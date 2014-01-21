a869565b983a76796cb45209f508901048a0fef3
Ludovic Courtès <ludo@gnu.org>
2014-01-22 00:21:19 +0100

    gnu: Add GNU V.E.R.A.
    * gnu/packages/dictionaries.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/dictionaries.scm
@@ -0,0 +1,78 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2014 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages dictionaries)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system trivial)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages texinfo)
+  #:use-module ((gnu packages compression)
+                #:select (gzip)))
+
+(define-public vera
+  (package
+    (name "vera")
+    (version "1.21a")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/vera/vera-" version
+                                  ".tar.gz"))
+              (sha256
+               (base32
+                "09qz1g8js8qw735wmd8kraqbk1d1997v3px2lcc58frf1r66bp8f"))))
+    (build-system trivial-build-system)
+    (arguments
+     `(#:builder (begin
+                   (use-modules (guix build utils))
+
+                   (let* ((out    (assoc-ref %outputs "out"))
+                          (info   (string-append out "/share/info"))
+                          (html   (string-append out "/share/html"))
+                          (source (assoc-ref %build-inputs "source"))
+                          (tar    (assoc-ref %build-inputs "tar"))
+                          (gz     (assoc-ref %build-inputs "gzip"))
+                          (texi   (assoc-ref %build-inputs "texinfo")))
+                     (setenv "PATH" (string-append gz "/bin"))
+                     (system* (string-append tar "/bin/tar") "xvf" source)
+
+                     (chdir (string-append "vera-" ,version))
+                     (mkdir-p info)
+                     (mkdir-p html)
+
+                     ;; XXX: Use '--force' because the document is unhappy
+                     ;; with Texinfo 5 (yes, documents can be unhappy.)
+                     (and (zero?
+                           (system* (string-append texi "/bin/makeinfo")
+                                    "vera.texi" "--force" "-o"
+                                    (string-append info "/vera.info")))
+                          (zero?
+                           (system* (string-append texi "/bin/makeinfo")
+                                    "vera.texi" "--force" "--html" "-o"
+                                    (string-append html "/vera.html"))))))
+      #:modules ((guix build utils))))
+    (native-inputs `(("texinfo" ,texinfo)
+                     ("tar" ,tar)
+                     ("gzip" ,gzip)))
+    (home-page "http://savannah.gnu.org/projects/vera/")
+    (synopsis "List of acronyms")
+    (description
+     "V.E.R.A. (Virtual Entity of Relevant Acronyms) is a list of computing
+acronyms distributed as an info document.")
+    (license fdl1.3+)))
