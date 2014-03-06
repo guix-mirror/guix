guix-mirror/guix

commit 99fbddf9a623757e39d88bfb431f8f7d6f24b75b

store: Change 'export-paths' to always export in topological order.
* guix/store.scm (export-paths): Pass PATHS through
  'topologically-sorted' before iterating.
* tests/store.scm ("export/import paths, ensure topological order"): New
  test.

guix/store.scm
@@ -732,10 +732,10 @@ is raised if the set of paths read from PORT is not signed (as per
     (= 1 (read-int s))))
 
 (define* (export-paths server paths port #:key (sign? #t))
-  "Export the store paths listed in PATHS to PORT, signing them if SIGN?
-is true."
+  "Export the store paths listed in PATHS to PORT, in topological order,
+signing them if SIGN? is true."
   (let ((s (nix-server-socket server)))
-    (let loop ((paths paths))
+    (let loop ((paths (topologically-sorted server paths)))
       (match paths
         (()
          (write-int 0 port))

tests/store.scm
@@ -398,6 +398,25 @@ Deriver: ~a~%"
                                  get-string-all))
                              files)))))))
 
+(test-assert "export/import paths, ensure topological order"
+  (let* ((file1 (add-text-to-store %store "foo" (random-text)))
+         (file2 (add-text-to-store %store "bar" (random-text)
+                                   (list file1)))
+         (files (list file1 file2))
+         (dump1 (call-with-bytevector-output-port
+                 (cute export-paths %store (list file1 file2) <>)))
+         (dump2 (call-with-bytevector-output-port
+                 (cute export-paths %store (list file2 file1) <>))))
+    (delete-paths %store files)
+    (and (every (negate file-exists?) files)
+         (bytevector=? dump1 dump2)
+         (let* ((source   (open-bytevector-input-port dump1))
+                (imported (import-paths %store source)))
+           (and (equal? imported (list file1 file2))
+                (every file-exists? files)
+                (null? (references %store file1))
+                (equal? (list file1) (references %store file2)))))))
+
 (test-assert "import corrupt path"
   (let* ((text (random-text))
          (file (add-text-to-store %store "text" text))

commit cafb92d853c66b677111594727c586b87bbdd58f

store: 'export-paths' doesn't export references of the given files.
This fixes a regression introduced in
99fbddf9a623757e39d88bfb431f8f7d6f24b75b ("store: Change 'export-paths'
to always export in topological order.")

* guix/store.scm (export-paths): Define 'ordered' variable.  Iterate
  over it.
* tests/store.scm ("export/import paths, ensure topological order"): Add
  'file0'.  Adjust accordingly.

guix/store.scm
@@ -734,8 +734,13 @@ is raised if the set of paths read from PORT is not signed (as per
 (define* (export-paths server paths port #:key (sign? #t))
   "Export the store paths listed in PATHS to PORT, in topological order,
 signing them if SIGN? is true."
+  (define ordered
+    ;; Sort PATHS, but don't include their references.
+    (filter (cut member <> paths)
+            (topologically-sorted server paths)))
+
   (let ((s (nix-server-socket server)))
-    (let loop ((paths (topologically-sorted server paths)))
+    (let loop ((paths ordered))
       (match paths
         (()
          (write-int 0 port))

tests/store.scm
@@ -399,7 +399,9 @@ Deriver: ~a~%"
                              files)))))))
 
 (test-assert "export/import paths, ensure topological order"
-  (let* ((file1 (add-text-to-store %store "foo" (random-text)))
+  (let* ((file0 (add-text-to-store %store "baz" (random-text)))
+         (file1 (add-text-to-store %store "foo" (random-text)
+                                   (list file0)))
          (file2 (add-text-to-store %store "bar" (random-text)
                                    (list file1)))
          (files (list file1 file2))
@@ -412,9 +414,10 @@ Deriver: ~a~%"
          (bytevector=? dump1 dump2)
          (let* ((source   (open-bytevector-input-port dump1))
                 (imported (import-paths %store source)))
+           ;; DUMP1 should contain exactly FILE1 and FILE2, not FILE0.
            (and (equal? imported (list file1 file2))
                 (every file-exists? files)
-                (null? (references %store file1))
+                (equal? (list file0) (references %store file1))
                 (equal? (list file1) (references %store file2)))))))
 
 (test-assert "import corrupt path"