9037ea2c123eb5ad351fdc108bbd2ced7f1a81a8
Ludovic Courtès <ludo@gnu.org>
2014-03-13 00:04:07 +0100

    gnu: guile-xcb: Upgrade to 1.3.
    * gnu/packages/guile-wm.scm (guile-xcb): Upgrade to 1.3.
      [arguments] Pass --with-guile-site-dir= instead of --datadir=,
      and pass --with-guile-site-ccache-dir=.  Remove #:phases.

--- gnu/packages/guile-wm.scm
+++ gnu/packages/guile-wm.scm
@@ -29,36 +29,26 @@
 (define-public guile-xcb
   (package
     (name "guile-xcb")
-    (version "1.2")
+    (version "1.3")
     (source (origin
              (method url-fetch)
              (uri (string-append "http://www.markwitmer.com/dist/guile-xcb-"
                                  version ".tar.gz"))
              (sha256
               (base32
-               "009qrw46ay74z3mw8gz7jqvn90z9ilyhy00801w5vpyias02730y"))))
+               "04dvbqdrrs67490gn4gkq9zk8mqy3mkls2818ha4p0ckhh0pm149"))))
     (build-system gnu-build-system)
     (arguments '(;; Parallel builds fail.
                  #:parallel-build? #f
 
-                 ;; The '.scm' files go to $(datadir), so set that to the
-                 ;; standard value.
                  #:configure-flags (list (string-append
-                                          "--datadir="
+                                          "--with-guile-site-dir="
                                           (assoc-ref %outputs "out")
-                                          "/share/guile/site/2.0"))
-                 #:phases (alist-cons-before
-                           'configure 'set-go-directory
-                           (lambda* (#:key outputs #:allow-other-keys)
-                             ;; The makefile sets the .go directory to Guile's
-                             ;; own .go site directory, which is read-only.
-                             ;; Change it to point to $out/share/guile/site/2.0.
-                             (let ((out (assoc-ref outputs "out")))
-                               (substitute* "Makefile.in"
-                                 (("^godir = .*$")
-                                  (string-append "godir = " out
-                                                 "/share/guile/site/2.0\n")))))
-                           %standard-phases)))
+                                          "/share/guile/site/2.0")
+                                         (string-append
+                                          "--with-guile-site-ccache-dir="
+                                          (assoc-ref %outputs "out")
+                                          "/share/guile/site/2.0"))))
     (native-inputs `(("pkg-config" ,pkg-config)))
     (inputs `(("guile" ,guile-2.0)
               ("xcb" ,xcb-proto)))
