guix-mirror/guix

commit d4bf49b140bd5cfb3580d2a038679160dc1331b5

gnu: Remove unused lambda arguments and prefer separate phases over augmented phases.
* gnu/packages/compression.scm (sharutils) [arguments]: Remove unused
  lambda args.
* gnu/packages/gl.scm (mesa) [arguments]: Same
* gnu/packages/fltk.scm [arguments]: Same.  Substitute const check
  phase with #:tests? #f.  Add pre-configure phase.
* gnu/packages/ghostscript.scm (ghostscript) [arguments]: Put makefile
  patches in separate phase.  Put so steps oinseparate phases.
* gnu/packages/glib.scm (gobject-introspection) [arguments]: Remove
  unused lambda args.  Put patches in pre-configure phase.
* gnu/packages/gnupg.scm (gnupg) [arguments]: Put patch in
  pre-configure phase.
  (pius) [arguments]: Delete const #t configure and build phases.
  (signing-party) [arguments]: Factor patches into post-unpack phase.
  (paperkey) [arguments]: Remove unused lambda args.  Factor out
  patches into pre-check phase.
* gnu/packages/icu4c.scm [arguments]: Change source dir after standard
  unpack phase.  Factor configure patches into pre-configure phase.
* gnu/packages/lsof.scm [arguments]: Remove unused lambda args.
  Remove unnecessary apply.
* gnu/packages/lvm.scm (lvm2) [arguments]: Factor out patches into
  pre-configure phase.
* gnu/packages/libtiff.scm [arguments]: Same
* gnu/packages/maths.scm (hdf5) [arguments]: Same
* gnu/packages/gtk.scm (gtk+-2) [arguments]: Same
* gnu/packages/mp3.scm (libmad) [arguments]: Same
  (id3lib) [arguments]: Same
* gnu/packages/python.scm (python-2) [arguments]: Same
* gnu/packages/texlive.scm (texlive) [arguments]: Same
* gnu/packages/pretty-print.scm (a2ps) [arguments]: Same
  (trueprint) [arguments]: Same
  (source-highlight) [arguments]: Same.  Remove unused lambda args.
* gnu/packages/netpbm.scm [arguments]: Remove unused lambda args.
  Factor out test patches into pre-check phase.  Condense calls to
  substitute*
* gnu/packages/openldap.scm [arguments]: Factor out libtool copy into
  post-configure phases. [synopsis] Remove package name.

gnu/packages/texlive.scm
@@ -116,12 +116,10 @@
          "--with-system-zziplib")
       #:phases
        (alist-replace
-        'configure
-        (lambda* (#:key outputs #:allow-other-keys #:rest args)
-         (let ((configure (assoc-ref %standard-phases 'configure)))
-           (substitute* "utils/psutils/Makefile.in"
-             (("/usr/bin/env perl") (which "perl")))
-           (apply configure args)))
+        'configure 'patch-perl-shebang
+        (lambda _
+          (substitute* "utils/psutils/Makefile.in"
+            (("/usr/bin/env perl") (which "perl"))))
        (alist-cons-after
         'install 'postinst
          (lambda* (#:key inputs outputs #:allow-other-keys #:rest args)

gnu/packages/compression.scm
@@ -280,7 +280,7 @@ archiving.  Lzip is a clean implementation of the LZMA algorithm.")
          'patch-source-shebangs 'unpatch-source-shebang
          ;; revert the patch-shebang phase on a script which is
          ;; in fact test data
-         (lambda* (#:key #:allow-other-keys)
+         (lambda _
            (substitute* "tests/shar-1.ok"
              (((which "sh")) "/bin/sh")))
          %standard-phases)))

gnu/packages/fltk.scm
@@ -40,19 +40,14 @@
       `(("libx11" ,libx11)
         ("mesa" ,mesa)))
     (arguments
-     `(#:phases
-       (alist-replace
-        'check
-        (lambda* (#:key inputs #:allow-other-keys) #t) ;; fltk does not have a
-                                                       ;; check target
-        (alist-replace
-         'configure
-         (lambda* (#:key outputs #:allow-other-keys #:rest args)
-           (let ((configure (assoc-ref %standard-phases 'configure)))
-             (substitute* "makeinclude.in"
-               (("/bin/sh") (which "sh")))
-             (apply configure args)))
-         %standard-phases))))
+     `(#:tests? #f                      ;TODO: compile programs in "test" dir
+       #:phases
+       (alist-cons-before
+        'configure 'patch-makeinclude
+        (lambda _
+          (substitute* "makeinclude.in"
+            (("/bin/sh") (which "sh"))))
+        %standard-phases)))
     (home-page "https://www.fltk.org")
     (synopsis "3D C++ GUI library")
     (description "FLTK is a C++ GUI toolkit providing modern GUI functionality without the

gnu/packages/ghostscript.scm
@@ -142,27 +142,19 @@ printing, and psresize, for adjusting page sizes.")
         ("tcl" ,tcl)))
    (arguments
     `(#:phases
-      (alist-replace
-       'configure
-       (lambda* (#:key #:allow-other-keys #:rest args)
-        (let ((configure (assoc-ref %standard-phases 'configure)))
-          (apply configure args)
-          (substitute* "base/all-arch.mak"
-            (("/bin/sh") (which "bash")))
-          (substitute* "base/unixhead.mak"
-            (("/bin/sh") (which "bash")))))
-      (alist-replace
-       'build
-       (lambda* (#:key #:allow-other-keys #:rest args)
-        (let ((build (assoc-ref %standard-phases 'build)))
-          (apply build args)
-          (system* "make" "so")))
-      (alist-replace
-       'install
-       (lambda* (#:key #:allow-other-keys #:rest args)
-        (let ((install (assoc-ref %standard-phases 'install)))
-          (apply install args)
-          (system* "make" "install-so")))
+      (alist-cons-after
+       'configure 'patch-config-files
+       (lambda _
+         (substitute* "base/all-arch.mak"
+           (("/bin/sh") (which "bash")))
+         (substitute* "base/unixhead.mak"
+           (("/bin/sh") (which "bash"))))
+      (alist-cons-after
+       'build 'build-so
+       (lambda _ (system* "make" "so"))
+      (alist-cons-after
+       'install 'install-so
+       (lambda _ (system* "make" "install-so"))
       %standard-phases)))))
    (synopsis "PostScript and PDF interpreter")
    (description

gnu/packages/gl.scm
@@ -159,7 +159,7 @@ Polygon meshes, and Extruded polygon meshes")
         #:phases
          (alist-cons-after
           'unpack 'remove-symlink
-          (lambda* (#:key #:allow-other-keys)
+          (lambda _
             ;; remove dangling symlink to /usr/include/wine/windows
             (delete-file "src/gallium/state_trackers/d3d1x/w32api"))
          %standard-phases)))

gnu/packages/glib.scm
@@ -196,13 +196,11 @@ dynamic loading, and an object system.")
        ("libffi" ,libffi)))
     (arguments
      `(#:phases
-        (alist-replace
-         'configure
-         (lambda* (#:key #:allow-other-keys #:rest args)
-          (let ((configure (assoc-ref %standard-phases 'configure)))
+        (alist-cons-before
+         'configure 'patch-paths
+         (lambda _
            (substitute* "giscanner/sourcescanner.py"
-             (("GUIX_GCC_PATH") (which "gcc")))
-           (apply configure args)))
+             (("GUIX_GCC_PATH") (which "gcc"))))
          %standard-phases)))
     (home-page "https://wiki.gnome.org/GObjectIntrospection")
     (synopsis "Generate interface introspection data for GObject libraries")

gnu/packages/gnupg.scm
@@ -191,13 +191,11 @@ specifications are building blocks of S/MIME and TLS.")
        ("readline" ,readline)))
    (arguments
     `(#:phases
-       (alist-replace
-        'configure
-        (lambda* (#:key #:allow-other-keys #:rest args)
-         (let ((configure (assoc-ref %standard-phases 'configure)))
-           (substitute* "tests/openpgp/Makefile.in"
-             (("/bin/sh") (which "bash")))
-           (apply configure args)))
+       (alist-cons-before
+        'configure 'patch-config-files
+        (lambda _
+          (substitute* "tests/openpgp/Makefile.in"
+            (("/bin/sh") (which "bash"))))
        %standard-phases)))
     (home-page "http://gnupg.org/")
     (synopsis "GNU Privacy Guard")
@@ -286,12 +284,10 @@ and every application benefits from this.")
    (arguments
     `(#:tests? #f
       #:phases
-       (alist-replace
+       (alist-delete
         'configure
-        (lambda* (#:key #:allow-other-keys) #t)
-       (alist-replace
+       (alist-delete
         'build
-        (lambda* (#:key #:allow-other-keys) #t)
        (alist-replace
         'install
         (lambda* (#:key inputs outputs #:allow-other-keys)
@@ -334,13 +330,9 @@ PGP keysigning parties.")
    (arguments
     `(#:tests? #f
       #:phases
-      (alist-replace
-       'unpack
-       (lambda* (#:key #:allow-other-keys #:rest args)
-         (let ((unpack (assoc-ref %standard-phases 'unpack)))
-           (apply unpack args)
-           ;; remove spurious symlink
-           (delete-file "keyanalyze/pgpring/depcomp")))
+      (alist-cons-after
+       'unpack 'remove-spurious-links
+       (lambda _ (delete-file "keyanalyze/pgpring/depcomp"))
       (alist-replace
        'configure
        (lambda* (#:key outputs #:allow-other-keys)
@@ -463,14 +455,12 @@ enter a passphrase when `gpg' or `gpg2' is run and needs it.")
     (build-system gnu-build-system)
     (arguments
      `(#:phases
-       (alist-replace
-        'check
-        (lambda* (#:key #:allow-other-keys #:rest args)
-          (let ((check (assoc-ref %standard-phases 'check)))
-            (substitute* '("checks/roundtrip.sh"
-                           "checks/roundtrip-raw.sh")
-              (("/bin/echo") "echo"))
-            (apply check args)))
+       (alist-cons-before
+        'check 'patch-check-scripts
+        (lambda _
+          (substitute* '("checks/roundtrip.sh"
+                         "checks/roundtrip-raw.sh")
+            (("/bin/echo") "echo")))
         %standard-phases)))
     (home-page "http://www.jabberwocky.com/software/paperkey/")
     (synopsis "Backup OpenPGP keys to paper")

gnu/packages/gtk.scm
@@ -331,14 +331,12 @@ is part of the GNOME accessibility project.")
       ("python-wrapper" ,python-wrapper)))
    (arguments
     `(#:phases
-      (alist-replace
-       'configure
-       (lambda* (#:key #:allow-other-keys #:rest args)
-        (let ((configure (assoc-ref %standard-phases 'configure)))
-          ;; FIXME: re-enable tests requiring an X server
-          (substitute* "gtk/Makefile.in"
-           (("SUBDIRS = theme-bits . tests") "SUBDIRS = theme-bits ."))
-          (apply configure args)))
+      (alist-cons-before
+       'configure 'disable-tests
+       (lambda _
+         ;; FIXME: re-enable tests requiring an X server
+         (substitute* "gtk/Makefile.in"
+           (("SUBDIRS = theme-bits . tests") "SUBDIRS = theme-bits .")))
       %standard-phases)))
    (synopsis "Cross-platform toolkit for creating graphical user interfaces")
    (description

commit 67880c8e2bdd6d9d3a9a9dc3fe242d115f711ce9

gnu: texlive: Fix bug recently introduced in d4bf49b.
Fixes bug introduced in d4bf49b140bd5cfb3580d2a038679160dc1331b5.

* gnu/packages/texlive.scm (texlive): Change 'alist-replace' to
  'alist-cons-before'.

gnu/packages/texlive.scm
@@ -115,7 +115,7 @@
          "--with-system-zlib"
          "--with-system-zziplib")
       #:phases
-       (alist-replace
+       (alist-cons-before
         'configure 'patch-perl-shebang
         (lambda _
           (substitute* "utils/psutils/Makefile.in"