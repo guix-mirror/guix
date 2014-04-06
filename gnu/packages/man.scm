cd2e0b64f2e5a83c4f041de03aca432249c56f75
David Thompson <dthompson@member.fsf.org>
2014-04-06 15:36:40 -0400

    gnu: Add libpipeline.
    * gnu/packages/man.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/man.scm
@@ -0,0 +1,43 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2014 David Thompson <dthompson2@worcester.edu>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages man)
+  #:use-module (guix licenses)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (guix build-system gnu))
+
+(define-public libpipeline
+  (package
+    (name "libpipeline")
+    (version "1.3.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://savannah/libpipeline/libpipeline-"
+                    version ".tar.gz"))
+              (sha256
+               (base32
+                "12d6ldcj7kv2nv832b23v97g7035d0ybq0ig7h0yr7xk9czd3z7i"))))
+    (build-system gnu-build-system)
+    (home-page "http://libpipeline.nongnu.org/")
+    (synopsis "C library for manipulating pipelines of subprocesses")
+    (description
+     "libpipeline is a C library for manipulating pipelines of subprocesses in
+a flexible and convenient way.")
+    (license gpl3+)))
