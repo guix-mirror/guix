8ede638c4b84d5e26844fdc5726992ed78244cfd
Eric Bavier <bavier@member.fsf.org>
2014-05-08 14:48:59 -0500

    gnu: openmpi: Avoid "open source" verbiage
    * gnu/packages/mpi.scm (openmpi)[synopsis,description]: Remove mention
      of "open source".

--- gnu/packages/mpi.scm
+++ gnu/packages/mpi.scm
@@ -53,14 +53,13 @@
                            "--enable-mpi-ext=all"
                            "--with-devel-headers")))
     (home-page "http://www.open-mpi.org")
-    (synopsis "Open source MPI-2 implementation")
+    (synopsis "MPI-2 implementation")
     (description
-     "The Open MPI Project is an open source MPI-2 implementation that is
-developed and maintained by a consortium of academic, research, and industry
-partners.  Open MPI is therefore able to combine the expertise, technologies,
-and resources from all across the High Performance Computing community in
-order to build the best MPI library available.  Open MPI offers advantages for
-system and software vendors, application developers and computer science
-researchers.")
+     "The Open MPI Project is an MPI-2 implementation that is developed and
+maintained by a consortium of academic, research, and industry partners.  Open
+MPI is therefore able to combine the expertise, technologies, and resources
+from all across the High Performance Computing community in order to build the
+best MPI library available.  Open MPI offers advantages for system and
+software vendors, application developers and computer science researchers.")
     ;; See file://LICENSE
     (license bsd-2)))
