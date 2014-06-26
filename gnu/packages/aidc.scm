51d3dcea42f570d3d44af701e0c09ef1d1bfcea0
John Darrington <jmd@gnu.org>
2014-06-26 20:40:21 +0200

    gnu: Add qrencode.
    * gnu/packages/aidc.scm (qrencode): New variable.

--- gnu/packages/aidc.scm
+++ gnu/packages/aidc.scm
@@ -22,6 +22,8 @@
                 #:renamer (symbol-prefix-proc 'license:))
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages libpng)
   #:use-module (guix build-system gnu))
 
 
@@ -43,3 +45,25 @@ measurements.  Barcodes can be output in PostScript or Encapsulated PostScript
 formats.")
     (license license:gpl3+)
     (home-page "http://www.gnu.org/software/barcode/")))
+
+(define-public qrencode
+  (package
+    (name "qrencode")
+    (version "3.4.3")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "http://fukuchi.org/works/qrencode/qrencode-" version
+                    ".tar.bz2")) 
+              (sha256 (base32
+                       "163sb580p570p27imc6jhkfdw15kzp8vy1jq92nip1rwa63i9myz"))))
+    (build-system gnu-build-system)
+    (inputs `(("libpng" ,libpng)))
+    (native-inputs `(("pkg-config" ,pkg-config)))
+    (synopsis "Encode data into a QR Code symbol")
+    (description "Libqrencode is a C library for encoding data in a QR Code
+symbol, a kind of 2D symbology that can be scanned by handy terminals such as
+a mobile phone with CCD. The capacity of QR Code is up to 7000 digits or 4000
+characters, and is highly robust.")
+    (license license:lgpl2.1+)
+    (home-page "http://fukuchi.org/works/qrencode")))
