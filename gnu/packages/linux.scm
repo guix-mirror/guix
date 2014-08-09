0b28f1584ff5e75275215b2e300497794d16c2a1
Mark H Weaver <mhw@netris.org>
2014-08-09 02:49:03 -0400

    gnu: linux-libre: Update to 3.15.9.
    * gnu/packages/linux.scm (linux-libre): Update to 3.15.9.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -189,7 +189,7 @@ for SYSTEM, or #f if there is no configuration for SYSTEM."
      #f)))
 
 (define-public linux-libre
-  (let* ((version "3.15.8")
+  (let* ((version "3.15.9")
          (build-phase
           '(lambda* (#:key system inputs #:allow-other-keys #:rest args)
              ;; Apply the neat patch.
@@ -262,7 +262,7 @@ for SYSTEM, or #f if there is no configuration for SYSTEM."
              (uri (linux-libre-urls version))
              (sha256
               (base32
-               "1ichq7b08rrfq61i8kpan9vxw9mxcfpcl8cw0a6lbc1ycwzvm7xw"))))
+               "0q1dipb7ivv83nxrhhllpvs3g2mv7z4s2xdxxrzxaw86ryh0nn1y"))))
     (build-system gnu-build-system)
     (native-inputs `(("perl" ,perl)
                      ("bc" ,bc)
