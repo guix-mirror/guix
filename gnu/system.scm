ba544e78dc22a3fa8e5aaa9dc25bd95ab50e677c
Ludovic Courtès <ludo@gnu.org>
2014-09-13 10:52:21 +0200

    system: Add Diffutils to '%base-packages'.
    * gnu/system.scm (%base-packages): Add DIFFUTILS.

--- gnu/system.scm
+++ gnu/system.scm
@@ -261,7 +261,7 @@ explicitly appear in OS."
          ;; there to avoid duplication.
          (map canonical-package
               (list guile-2.0 bash coreutils findutils grep sed
-                    gawk tar gzip bzip2 xz lzip))))
+                    diffutils gawk tar gzip bzip2 xz lzip))))
 
 (define %default-issue
   ;; Default contents for /etc/issue.
