108e0c6101711d9940fc798963e13e022c026337
Jason Self <j@jxself.org>
2014-10-06 05:04:24 -0700

    gnu: linux-libre: Update to 3.17.
    * gnu/packages/linux.scm (linux-libre): Update to version 3.17.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -192,7 +192,7 @@ for SYSTEM, or #f if there is no configuration for SYSTEM."
      #f)))
 
 (define-public linux-libre
-  (let* ((version "3.16.4")
+  (let* ((version "3.17")
          (build-phase
           '(lambda* (#:key system inputs #:allow-other-keys #:rest args)
              ;; Apply the neat patch.
@@ -265,7 +265,7 @@ for SYSTEM, or #f if there is no configuration for SYSTEM."
              (uri (linux-libre-urls version))
              (sha256
               (base32
-               "0g5y7qnxfg1ilicpbxh155hlvvw14g4187vqra44iawbk9r41kb6"))))
+               "03hzyjsc0p7x4wz3hksg8dj2dlvxs0va9mcmiwqhlffs5a39lbiv"))))
     (build-system gnu-build-system)
     (native-inputs `(("perl" ,perl)
                      ("bc" ,bc)
