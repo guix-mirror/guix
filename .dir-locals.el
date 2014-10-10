d53e8ba6b5656d973f764d29a7dc8f8bbf8708b6
Ludovic Courtès <ludo@gnu.org>
2014-10-10 18:19:43 +0200

    Augment '.dir-locals.el' with bug-reference-mode settings.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,13 @@
 ;; Per-directory local variables for GNU Emacs 23 and later.
 
-((nil             . ((fill-column . 78)
-                     (tab-width   .  8)))
+((nil
+  . ((fill-column . 78)
+     (tab-width   .  8)
+
+     ;; For use with 'bug-reference-prog-mode'.
+     (bug-reference-url-format . "http://bugs.gnu.org/%s")
+     (bug-reference-bug-regexp
+      . "<https?://\\(debbugs\\|bugs\\)\\.gnu\\.org/\\([0-9]+\\)>")))
  (c-mode          . ((c-file-style . "gnu")))
  (scheme-mode
   .
