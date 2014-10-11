58d08c369be102927a97c9c37359504691aa703d
Mark H Weaver <mhw@netris.org>
2014-10-11 02:54:55 -0400

    Add copyright line to gnuzilla.scm.
    * gnu/packages/gnuzilla.scm: Add copyright line for Mark H Weaver, which
      should have been added in 74c7af9 (gnu: icecat: Update to 31.1.1).

--- gnu/packages/gnuzilla.scm
+++ gnu/packages/gnuzilla.scm
@@ -1,6 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2013 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014 Mark H Weaver <mhw@netris.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
