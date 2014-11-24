c4a298c52c8c8e80f0f08619ea171e6ad23e3654
Ludovic Courtès <ludo@gnu.org>
2014-11-24 22:47:41 +0100

    gnu: grep: Update to 2.21.
    * gnu/packages/base.scm (grep): Update to 2.21.

--- gnu/packages/base.scm
+++ gnu/packages/base.scm
@@ -64,14 +64,14 @@ command-line arguments, multiple languages, and so on.")
 (define-public grep
   (package
    (name "grep")
-   (version "2.20")
+   (version "2.21")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/grep/grep-"
                                 version ".tar.xz"))
             (sha256
              (base32
-              "0rcs0spsxdmh6yz8y4frkqp6f5iw19mdbdl9s2v6956hq0mlbbzh"))))
+              "1pp5n15qwxrw1pibwjhhgsibyv5cafhamf8lwzjygs6y00fa2i2j"))))
    (build-system gnu-build-system)
    (synopsis "Print lines matching a pattern")
    (description
