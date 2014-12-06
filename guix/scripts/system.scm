guix-mirror/guix

commit cc7fa5929c26fcdd53ce83ce7a46de4dfc7af1a2

guix system: Use 'mbegin' for 'install'.
* guix/scripts/system.scm (install): Use 'mbegin'.  Lift FORMAT and
  POPULATE-ROOT-FILE-SYSTEM, and use the result.

guix/scripts/system.scm
@@ -151,18 +151,22 @@ When GRUB? is true, install GRUB on DEVICE, using GRUB.CFG."
             ;; Copy items to the new store.
             (copy-closure to-copy target #:log-port log-port)))))
 
-  (mlet* %store-monad ((os-dir -> (derivation->output-path os-drv))
-                       (%         (maybe-copy os-dir)))
+  (let ((os-dir   (derivation->output-path os-drv))
+        (format   (lift %store-monad format))
+        (populate (lift2 %store-monad populate-root-file-system)))
 
-    ;; Create a bunch of additional files.
-    (format log-port "populating '~a'...~%" target)
-    (populate-root-file-system os-dir target)
+    (mbegin %store-monad
+      (maybe-copy os-dir)
 
-    (when grub?
-      (unless (false-if-exception (install-grub grub.cfg device target))
-        (leave (_ "failed to install GRUB on device '~a'~%") device)))
+      ;; Create a bunch of additional files.
+      (format log-port "populating '~a'...~%" target)
+      (populate os-dir target)
 
-    (return #t)))
+      (begin
+        (when grub?
+          (unless (false-if-exception (install-grub grub.cfg device target))
+            (leave (_ "failed to install GRUB on device '~a'~%") device)))
+        (return #t)))))
 
 
 ;;;

commit c9e46f1c3967297ff14287f6d511d252a409518f

guix system: Fix typo affecting 'guix system init'.
Fixes <http://bugs.gnu.org/19279>.
Fixes a regression introduced in cc7fa59.
Reported by Tomas Cech <tcech@suse.cz>.

* guix/scripts/system.scm (install): Fix order of arguments to 'lift'
  and 'lift2'.

guix/scripts/system.scm
@@ -164,8 +164,8 @@ When GRUB? is true, install GRUB on DEVICE, using GRUB.CFG."
             (copy-closure to-copy target #:log-port log-port)))))
 
   (let ((os-dir   (derivation->output-path os-drv))
-        (format   (lift %store-monad format))
-        (populate (lift2 %store-monad populate-root-file-system)))
+        (format   (lift format %store-monad))
+        (populate (lift2 populate-root-file-system %store-monad)))
 
     (mbegin %store-monad
       (maybe-copy os-dir)