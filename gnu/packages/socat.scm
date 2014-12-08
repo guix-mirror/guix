f6d9b3ded7642260915e2217b856c376940e2aa3
Ludovic Courtès <ludo@gnu.org>
2014-12-08 23:28:48 +0100

    gnu: Add socat.
    * gnu/packages/socat.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/socat.scm
@@ -0,0 +1,55 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2014 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages socat)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages openssl))
+
+;; XXX: Group with other networking tools like tcpdump in a module?
+(define-public socat
+  (package
+    (name "socat")
+    (version "1.7.2.4")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "http://www.dest-unreach.org/socat/download/socat-"
+                    version ".tar.bz2"))
+              (sha256
+               (base32
+                "028yjka2zr6j1i8pmfmvzqki8ajczdl1hnry1x31xbbg3j83jxsb"))))
+    (build-system gnu-build-system)
+    (arguments '(#:tests? #f))                    ;no 'check' phase
+    (inputs `(("openssl" ,openssl)))
+    (home-page "http://www.dest-unreach.org/socat/")
+    (synopsis
+     "Open bidirectional communication channels from the command line")
+    (description
+     "socat is a relay for bidirectional data transfer between two independent
+data channels---files, pipes, devices, sockets, etc.  It can create
+\"listening\" sockets, named pipes, and pseudo terminals.
+
+socat can be used, for instance, as TCP port forwarder, as a shell interface
+to UNIX sockets, IPv6 relay, for redirecting TCP oriented programs to a serial
+line, to logically connect serial lines on different computers, or to
+establish a relatively secure environment (su and chroot) for running client
+or server shell scripts with network connections. ")
+    (license license:gpl2)))
