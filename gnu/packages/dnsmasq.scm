dd2efd3d5b83d56b8d1eeafb1ccdfbcaf6f78c32
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2015-01-31 21:51:08 +0100

    gnu: Add dnsmasq.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add dnsmasq.scm.
    * gnu/packages/dnsmasq.scm: New file.

---
+++ gnu/packages/dnsmasq.scm
@@ -0,0 +1,56 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages dnsmasq)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu))
+
+(define-public dnsmasq
+  (package
+    (name "dnsmasq")
+    (version "2.72")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "http://www.thekelleys.org.uk/dnsmasq/dnsmasq-"
+                    version ".tar.xz"))
+              (sha256
+               (base32
+                "1c80hq09hfm8cp5pirfb8wdlc7dqkp7zzmbmdaradcvlblzx42vx"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (alist-delete 'configure %standard-phases)
+       #:make-flags (list (string-append "PREFIX=" (assoc-ref %outputs "out"))
+                          "CC=gcc")
+       ;; No 'check' target.
+       #:tests? #f))
+    (home-page "http://www.thekelleys.org.uk/dnsmasq/doc.html")
+    (synopsis "Small caching DNS proxy and DHCP/TFTP server")
+    (description
+     "Dnsmasq is a lightweight DNS forwarder and DHCP server.  It is designed
+to provide DNS and optionally, DHCP, to a small network.  It can serve the
+names of local machines which are not in the global DNS.  The DHCP server
+integrates with the DNS server and allows machines with DHCP-allocated
+addresses to appear in the DNS with names configured either in each host or in
+a central configuration file.  Dnsmasq supports static and dynamic DHCP leases
+and BOOTP/TFTP for network booting of diskless machines.")
+    ;; Source files only say GPL2 and GPL3 are allowed.
+    (license (list gpl2 gpl3))))
