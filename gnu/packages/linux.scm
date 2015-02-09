1f4edec4fd9b4bc6f4eda8c196647eea5215f040
Jason Self <j@jxself.org>
2015-02-09 14:46:04 -0800

    gnu: linux-libre: Update to 3.19
    * gnu/packages/linux.scm (linux-libre): Update to version 3.19.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -197,7 +197,7 @@ for SYSTEM, or #f if there is no configuration for SYSTEM."
      #f)))
 
 (define-public linux-libre
-  (let* ((version "3.18.6")
+  (let* ((version "3.19")
          (build-phase
           '(lambda* (#:key system inputs #:allow-other-keys #:rest args)
              ;; Apply the neat patch.
@@ -270,7 +270,7 @@ for SYSTEM, or #f if there is no configuration for SYSTEM."
              (uri (linux-libre-urls version))
              (sha256
               (base32
-               "1mqy91q4291pikximwj542rx2mgrinn57df3403s3rkf8ix97bdw"))))
+               "1ndrflzalkcyy61im6kcm8z681yaq2hwqgn6zbd7r3j9mscyqq1a"))))
     (build-system gnu-build-system)
     (native-inputs `(("perl" ,perl)
                      ("bc" ,bc)
