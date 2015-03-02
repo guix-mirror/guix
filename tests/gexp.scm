guix-mirror/guix

commit f9efe568c3cd46f0aecb5bdd35731e98a29dbcea

gexp: Aggregate outputs of compound gexps.
* guix/gexp.scm (gexp-outputs)[add-reference-output]: Recurse into
  lists.
* tests/gexp.scm ("output list + ungexp-splicing list, combined gexps"):
  New test.

guix/gexp.scm
@@ -314,12 +314,12 @@ references."
        (cons name result))
       ((? gexp? exp)
        (append (gexp-outputs exp) result))
+      ((lst ...)
+       (fold-right add-reference-output result lst))
       (_
        result)))
 
-  (fold-right add-reference-output
-              '()
-              (gexp-references exp)))
+  (add-reference-output (gexp-references exp) '()))
 
 (define* (gexp->sexp exp #:key
                      (system (%current-system))

tests/gexp.scm
@@ -230,6 +230,16 @@
                 (gexp-outputs exp2))
          (= 2 (length (gexp-outputs exp2))))))
 
+(test-assert "output list + ungexp-splicing list, combined gexps"
+  (let* ((exp0  (gexp (mkdir (ungexp output))))
+         (exp1  (gexp (mkdir (ungexp output "foo"))))
+         (exp2  (gexp (begin (display "hi!")
+                             (ungexp-splicing (list exp0 exp1))))))
+    (and (lset= equal?
+                (append (gexp-outputs exp0) (gexp-outputs exp1))
+                (gexp-outputs exp2))
+         (= 2 (length (gexp-outputs exp2))))))
+
 (test-assertm "gexp->file"
   (mlet* %store-monad ((exp -> (gexp (display (ungexp %bootstrap-guile))))
                        (guile  (package-file %bootstrap-guile))

commit 7e75a6739bf02c39baf8340e31e590c2c7c5fd16

gexp: Make sure 'gexp-outputs' removes duplicate outputs.
Fixes a regression introduced in f9efe56.

* guix/gexp.scm (gexp-outputs): Add call to 'delete-duplicates'.
* tests/gexp.scm ("output list, combined gexps, duplicate output"): New
  test.

guix/gexp.scm
@@ -319,7 +319,8 @@ references."
       (_
        result)))
 
-  (add-reference-output (gexp-references exp) '()))
+  (delete-duplicates
+   (add-reference-output (gexp-references exp) '())))
 
 (define* (gexp->sexp exp #:key
                      (system (%current-system))

tests/gexp.scm
@@ -230,6 +230,13 @@
                 (gexp-outputs exp2))
          (= 2 (length (gexp-outputs exp2))))))
 
+(test-equal "output list, combined gexps, duplicate output"
+  1
+  (let* ((exp0 (gexp (mkdir (ungexp output))))
+         (exp1 (gexp (begin (mkdir (ungexp output)) (ungexp exp0))))
+         (exp2 (gexp (begin (mkdir (ungexp output)) (ungexp exp1)))))
+    (length (gexp-outputs exp2))))
+
 (test-assert "output list + ungexp-splicing list, combined gexps"
   (let* ((exp0  (gexp (mkdir (ungexp output))))
          (exp1  (gexp (mkdir (ungexp output "foo"))))