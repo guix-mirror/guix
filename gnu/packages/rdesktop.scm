00e650981c07abf257e1a319647b1cfe3da8eeb6
Ludovic Courtès <ludo@gnu.org>
2015-03-03 14:17:42 +0100

    gnu: Add rdesktop.
    * gnu/packages/rdesktop.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/rdesktop.scm
@@ -0,0 +1,59 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages rdesktop)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages openssl))
+
+(define-public rdesktop
+  (package
+    (name "rdesktop")
+    (version "1.8.3")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://sourceforge/rdesktop/rdesktop-"
+                    version ".tar.gz"))
+              (sha256
+               (base32
+                "1r7c1rjmw2xzq8fw0scyb453gy9z19774z1z8ldmzzsfndb03cl8"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:configure-flags (list (string-append "--with-openssl="
+                                              (assoc-ref %build-inputs
+                                                         "openssl"))
+
+                               ;; XXX: optional dependencies missing
+                               "--disable-credssp"
+                               "--disable-smartcard")
+
+       #:tests? #f))                              ;no 'check' target
+    (inputs
+     `(("libx11" ,libx11)
+       ("openssl" ,openssl)))
+    (home-page "http://www.rdesktop.org/")
+    (synopsis "Client for Windows Terminal Services")
+    (description
+     "rdesktop is a client for Microsoft's Windows Remote Desktop Services,
+capable of natively speaking Remote Desktop Protocol (RDP.)  It allows users
+to remotely control a user's Windows desktop.")
+    (license license:gpl3+)))
