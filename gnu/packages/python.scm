guix-mirror/guix

commit a1920bc9f25210b243ebd20f0a3624d644d08efb

gnu: Add python-testlib and python2-testlib.
* gnu/packages/python.scm (python-testlib, python2-testlib): New variables.

gnu/packages/python.scm
@@ -3312,3 +3312,39 @@ interfaces in an easy and portable manner.")
 providing a clean and modern domain specific specification language (DSL) in
 Python style, together with a fast and comfortable execution environment.")
     (license license:expat)))
+
+(define-public python-testlib
+  (package
+    (name "python-testlib")
+    (version "0.6.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://pypi.python.org/packages/source/t/testlib/testlib-"
+             version ".zip"))
+       (sha256
+        (base32 "1mz26cxn4x8bbgv0rn0mvj2z05y31rkc8009nvdlb3lam5b4mj3y"))))
+    (build-system python-build-system)
+    (inputs
+      `(("python-setuptools" ,python-setuptools)))
+    (native-inputs
+     `(("unzip" ,unzip)))
+    (arguments
+     `(#:phases
+       (alist-replace
+        'unpack
+        (lambda* (#:key inputs outputs #:allow-other-keys)
+          (let ((unzip (string-append (assoc-ref inputs "unzip")
+                                      "/bin/unzip"))
+                (source (assoc-ref inputs "source")))
+            (and (zero? (system* unzip source))
+                 (chdir (string-append "testlib-" ,version)))))
+        %standard-phases)))
+    (synopsis "Python micro test suite harness")
+    (description "A micro unittest suite harness for Python.")
+    (home-page "https://github.com/trentm/testlib")
+    (license expat)))
+
+(define-public python2-testlib
+  (package-with-python2 python-testlib))

commit 1cb9c00614da7a9d56e2ee28c6da1bd8a53ef4e1

gnu: python-testlib: Fix license field.
Fixes a problem introduced in a1920bc9f25210b2.
Debugged by David Thompson.

* gnu/packages/python.scm (python-testlib)[license]: 'expat' ->
  'license:expat'.

gnu/packages/python.scm
@@ -3344,7 +3344,7 @@ Python style, together with a fast and comfortable execution environment.")
     (synopsis "Python micro test suite harness")
     (description "A micro unittest suite harness for Python.")
     (home-page "https://github.com/trentm/testlib")
-    (license expat)))
+    (license license:expat)))
 
 (define-public python2-testlib
   (package-with-python2 python-testlib))