guix-mirror/guix

commit 722ec722441a755e18fbc696d23441427dfb26af

gnu: Move autoconf-style phases after the unpack phase.
* gnu/packages/audio.scm (soundtouch)[arguments]: Move 'bootstrap'
  after 'unpack'.  Remove custom /usr/bin/file patching, which will
  now be handled by the 'patch-usr-bin-file' phase.
* gnu/packages/emacs.scm (emacs-w3m)[arguments]: Rename 'pre-configure'
  phase to 'autoconf' and move it after 'unpack'.
* gnu/packages/fontutils.scm (libuninameslist)[arguments]: Move 'bootstrap'
  phase after 'unpack'.
* gnu/packages/libevent.scm (libuv)[arguments]: Move 'autogen' phase after
  'unpack'.  Run "sh autogen.sh" instead of "./autogen.sh".
* gnu/packages/linux.scm (bridge-utils)[arguments]: Move 'bootstrap' phase
  after 'unpack'.
* gnu/packages/mail.scm (libetpan)[arguments]: Move 'autogen' phase after
  'unpack'.  Run "sh autogen.sh" instead of "./autogen.sh".  Setenv
  "NOCONFIGURE" before running autogen.sh.
* gnu/packages/package-management.scm (guix-devel)[arguments]: Move
  'bootstrap' phase after 'unpack'.  Run "sh autogen.sh" instead of
  "./autogen.sh".
* gnu/packages/rdf.scm (lrdf)[arguments]: Move 'autoreconf' phase after
  'remove-out-of-tree-references' phase.
* gnu/packages/tor.scm (privoxy)[arguments]: Move 'autoconf' phase after
  'unpack'.
* gnu/packages/web.scm (libpsl)[arguments]: Move 'bootstrap' phase after
  'unpack'.  Run "sh autogen.sh" instead of "./autogen.sh".  Remove
  'fix-autogen-shebang' phase.

gnu/packages/rdf.scm
@@ -140,9 +140,9 @@ Java Lucene text search engine API to C++.")
                    (substitute* "examples/Makefile.am"
                      (("instances_test remove_test") "instances_test")
                      (("\\$\\(TESTS\\) remove_test") "$(TESTS)")))
-                 (alist-cons-before
-                  'configure 'autoreconf
-                  (lambda* (#:key inputs #:allow-other-keys)
+                 (alist-cons-after
+                  'remove-out-of-tree-references 'autoreconf
+                  (lambda _
                     (zero? (system* "autoreconf" "-vfi")))
                   %standard-phases))))
     (inputs

gnu/packages/audio.scm
@@ -804,13 +804,10 @@ stretching and pitch scaling of audio.  This package contains the library.")
        ("file" ,file)))
     (arguments
      '(#:phases
-       (alist-cons-before
-        'configure 'bootstrap
+       (alist-cons-after
+        'unpack 'bootstrap
         (lambda _
-          (unless (zero? (system* "sh" "bootstrap"))
-            (error "bootstrap failed"))
-          (substitute* '("configure")
-            (("/usr/bin/file") "file")))
+          (zero? (system* "sh" "bootstrap")))
         %standard-phases)))
     (home-page "http://www.surina.net/soundtouch/")
     (synopsis

gnu/packages/emacs.scm
@@ -309,8 +309,8 @@ operations.")
                               out "/share/images/emacs-w3m")))
        #:tests? #f  ; no check target
        #:phases
-       (alist-cons-before
-        'configure 'pre-configure
+       (alist-cons-after
+        'unpack 'autoconf
         (lambda _
           (zero? (system* "autoconf")))
         (alist-cons-before

gnu/packages/fontutils.scm
@@ -278,8 +278,8 @@ smooth contours with constant curvature at the spline joins.")
                      ("automake" ,automake)
                      ("libtool" ,libtool)))
     (arguments
-     `(#:phases (alist-cons-before
-                 'configure 'bootstrap
+     `(#:phases (alist-cons-after
+                 'unpack 'bootstrap
                  (lambda _
                    (zero? (system* "autoreconf" "-vi")))
                  %standard-phases)))

gnu/packages/libevent.scm
@@ -73,12 +73,12 @@ loop.")
                 "1ys2wlypdbv59yywn91d5vl329z50mi7ivi3fj5rjm4mr9g3wnmr"))))
     (build-system gnu-build-system)
     (arguments
-     '(#:phases (alist-cons-before
-                 'configure 'autogen
+     '(#:phases (alist-cons-after
+                 'unpack 'autogen
                  (lambda _
                    ;; Fashionable people don't run 'make dist' these days, so
                    ;; we need to do that ourselves.
-                   (zero? (system* "./autogen.sh")))
+                   (zero? (system* "sh" "autogen.sh")))
                  %standard-phases)
 
        ;; XXX: Some tests want /dev/tty, attempt to make connections, etc.

gnu/packages/linux.scm
@@ -1005,8 +1005,8 @@ Linux-based operating systems.")
     (native-inputs `(("autoconf" ,autoconf)
                      ("automake" ,automake)))
     (arguments
-     '(#:phases (alist-cons-before
-                 'configure 'bootstrap
+     '(#:phases (alist-cons-after
+                 'unpack 'bootstrap
                  (lambda _
                    (zero? (system* "autoreconf" "-vf")))
                  %standard-phases)

gnu/packages/mail.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013, 2014, 2015 Ludovic Courtès <ludo@gnu.org>
-;;; Copyright © 2014 Mark H Weaver <mhw@netris.org>
+;;; Copyright © 2014, 2015 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2014 Ian Denhardt <ian@zenhack.net>
 ;;; Copyright © 2014 Sou Bunnbu <iyzsong@gmail.com>
 ;;; Copyright © 2014 Julien Lepiller <julien@lepiller.eu>
@@ -432,14 +432,11 @@ useful features.")
      `(("curl" ,curl)
        ("expat" ,expat)))
     (arguments
-      '(#:phases (alist-cons-before
-                  'configure 'autogen
+      '(#:phases (alist-cons-after
+                  'unpack 'autogen
                   (lambda _
-                    (system* "./autogen.sh")) ;; Note: this fails because the
-                         ;; generated configure script uses /bin/sh. It is
-                         ;; replaced in the configure phase by the correct
-                         ;; value. TODO: replace the configure phase by the
-                         ;; autogen phase and have the SHELL variable be replaced
+                    (setenv "NOCONFIGURE" "true")
+                    (zero? (system* "sh" "autogen.sh")))
                   %standard-phases)
         #:configure-flags
         '("--disable-static" "--disable-db")))

gnu/packages/package-management.scm
@@ -156,15 +156,15 @@ the Nix package manager.")
       (arguments
        (substitute-keyword-arguments (package-arguments guix-0.8.1)
          ((#:phases phases)
-          `(alist-cons-before
-            'configure 'bootstrap
+          `(alist-cons-after
+            'unpack 'bootstrap
             (lambda _
               ;; Make sure 'msgmerge' can modify the PO files.
               (for-each (lambda (po)
                           (chmod po #o666))
                         (find-files "." "\\.po$"))
 
-              (zero? (system* "./bootstrap")))
+              (zero? (system* "sh" "bootstrap")))
             ,phases))))
       (native-inputs
        `(("autoconf" ,(autoconf-wrapper))

commit 5c3f2a5a77031c52b5b9f6b912382646afbc60ff

gnu: lrdf: Fix phases.
Fixes a regression caused by 722ec722441a755e18fbc696d23441427dfb26af.

* gnu/packages/rdf.scm (lrdf)[arguments]: Swap order of 'alist-cons-after'
  calls.

gnu/packages/rdf.scm
@@ -127,24 +127,24 @@ Java Lucene text search engine API to C++.")
     (build-system gnu-build-system)
     (arguments
      '(#:phases (alist-cons-after
-                 'unpack 'remove-out-of-tree-references
+                 'remove-out-of-tree-references 'autoreconf
                  (lambda _
-                   ;; remove symlinks to files in /usr/
-                   (delete-file-recursively "m4")
-                   (for-each delete-file '("config.guess"
-                                           "config.sub"
-                                           "depcomp"
-                                           "install-sh"
-                                           "ltmain.sh"
-                                           "missing"))
-                   ;; remove_test depends on an out-of-tree RDF file
-                   (substitute* "examples/Makefile.am"
-                     (("instances_test remove_test") "instances_test")
-                     (("\\$\\(TESTS\\) remove_test") "$(TESTS)")))
+                   (zero? (system* "autoreconf" "-vfi")))
                  (alist-cons-after
-                  'remove-out-of-tree-references 'autoreconf
+                  'unpack 'remove-out-of-tree-references
                   (lambda _
-                    (zero? (system* "autoreconf" "-vfi")))
+                    ;; remove symlinks to files in /usr/
+                    (delete-file-recursively "m4")
+                    (for-each delete-file '("config.guess"
+                                            "config.sub"
+                                            "depcomp"
+                                            "install-sh"
+                                            "ltmain.sh"
+                                            "missing"))
+                    ;; remove_test depends on an out-of-tree RDF file
+                    (substitute* "examples/Makefile.am"
+                      (("instances_test remove_test") "instances_test")
+                      (("\\$\\(TESTS\\) remove_test") "$(TESTS)")))
                   %standard-phases))))
     (inputs
      `(("raptor" ,raptor2)