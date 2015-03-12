9a10acc9860d649f4b17c7d84ea685a98ba54b3f
Ludovic Courtès <ludo@gnu.org>
2015-03-12 19:00:54 +0100

    system: Adjust '.bashrc' skeleton for non-interactive SSH sessions.
    * gnu/system/shadow.scm (default-skeletons)[bashrc]: Source /etc/profile
      when in a non-interactive SSH session.

--- gnu/system/shadow.scm
+++ gnu/system/shadow.scm
@@ -120,6 +120,18 @@
 # Honor per-interactive-shell startup file
 if [ -f ~/.bashrc ]; then . ~/.bashrc; fi\n"))
                       (bashrc (text-file "bashrc" "\
+# Bash initialization for interactive non-login shells and
+# for remote shells (info \"(bash) Bash Startup Files\").
+
+if [ -n \"$SSH_CLIENT\" -a -z \"`type -P cat`\" ]
+then
+    # We are being invoked from a non-interactive SSH session
+    # (as in \"ssh host command\") but 'cat' cannot be found
+    # in $PATH.  Source /etc/profile so we get $PATH and other
+    # essential variables.
+    source /etc/profile
+fi
+
 PS1='\\u@\\h \\w\\$ '
 alias ls='ls -p --color'
 alias ll='ls -l'\n"))
