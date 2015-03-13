36fb36b55108d60c23fa7623a8b7c26f5cd97886
Tomáš Čech <sleep_walker@suse.cz>
2015-03-13 12:06:19 +0100

    gnu: Rename module dnsmasq to dns.
    * gnu/packages/dnsmasq.scm: Rename to...
    * gnu/packages/dns.scm: ... this.  Change module name accordingly.
    * gnu-system.am (GNU_SYSTEM_MODULES): Rename dnsmasq module to dns.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -16,7 +16,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (gnu packages dnsmasq)
+(define-module (gnu packages dns)
   #:use-module (guix licenses)
   #:use-module (guix packages)
   #:use-module (guix download)
