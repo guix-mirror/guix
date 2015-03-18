104785238c0ea28a273287bde834c0653c30c00f
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2015-03-18 11:21:29 +0100

    gnu: lcms: Use mirror:// URI.
    * gnu/packages/ghostscript.scm (lcms): Use a mirror:// URI for the source.

--- gnu/packages/ghostscript.scm
+++ gnu/packages/ghostscript.scm
@@ -40,8 +40,7 @@
    (source (origin
             (method url-fetch)
             (uri (string-append
-                   "http://downloads.sourceforge.net/project/lcms/lcms/"
-                   version "/lcms2-" version ".tar.gz"))
+                  "mirror://sourceforge/lcms/lcms2-" version ".tar.gz"))
             (sha256 (base32
                      "1c8lgq8gfs3nyplvbx9k8wzfj6r2bqi3f611vb1m8z3476454wji"))))
    (build-system gnu-build-system)
