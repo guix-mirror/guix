guix-mirror/guix

commit 666aa995f0486a0753a8bb68a5e32c675d0f8b5b

gnu: sdl: Don't use dlopen for shared object loading.
* gnu/packages/sdl.scm (sdl)[arguments]<#:configure-flags>:
  Use '--disable-sdl-dlopen'.
  [propagated-inputs]: Add libcap.

gnu/packages/sdl.scm
@@ -1,6 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013 David Thompson <dthompson2@worcester.edu>
 ;;; Copyright © 2014 Mark H Weaver <mhw@netris.org>
+;;; Copyright © 2015 Sou Bunnbu <iyzsong@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -58,16 +59,15 @@
              (patches (list (search-patch "sdl-libx11-1.6.patch")))))
     (build-system gnu-build-system)
     (arguments
-     '(;; Explicitly link against Xext because SDL tries to dlopen it and
-       ;; doesn't go very far otherwise (see
-       ;; <https://lists.gnu.org/archive/html/guix-devel/2013-11/msg00088.html>
-       ;; for details.)
-       #:configure-flags '("LDFLAGS=-lXext")
+     '(;; Explicitly link against shared libraries instead of dlopening them.
+       ;; For X11, ALSA, PulseAudio, etc.
+       #:configure-flags '("--disable-sdl-dlopen")
 
        #:tests? #f)) ; no check target
     (propagated-inputs
      ;; SDL headers include X11 headers.
-     `(("libx11" ,libx11)))
+     `(("libx11" ,libx11)
+       ("libcap" ,libcap))) ; 'libSDL.la' contain `-lcap'.
     (native-inputs `(("pkg-config" ,pkg-config)))
     (inputs `(("libxrandr" ,libxrandr)
               ("mesa" ,mesa)

commit 83bb3a3ee4292d75cc6cfe08947ffb12989eccba

gnu: sdl: Allow dlopen for OpenGL.
Fixes a regression introduced in 666aa99.
Reported by Felipe López and David Thompson.

* gnu/packages/sdl.scm (sdl)[inputs]: Add glu.
  [arguments]<#:configure-flags>: Replace '--disable-sdl-dlopen' with
  '--disable-alsa-shared --disable-pulseaudio-shared --disable-x11-shared'.

gnu/packages/sdl.scm
@@ -60,8 +60,11 @@
     (build-system gnu-build-system)
     (arguments
      '(;; Explicitly link against shared libraries instead of dlopening them.
-       ;; For X11, ALSA, PulseAudio, etc.
-       #:configure-flags '("--disable-sdl-dlopen")
+       ;; For X11, ALSA, and PulseAudio.
+       ;; OpenGL library is still dlopened at runtime.
+       #:configure-flags '("--disable-alsa-shared"
+                           "--disable-pulseaudio-shared"
+                           "--disable-x11-shared")
 
        #:tests? #f)) ; no check target
     (propagated-inputs
@@ -71,6 +74,7 @@
     (native-inputs `(("pkg-config" ,pkg-config)))
     (inputs `(("libxrandr" ,libxrandr)
               ("mesa" ,mesa)
+              ("glu" ,glu)
               ("alsa-lib" ,alsa-lib)
               ("pulseaudio" ,pulseaudio)))
     (synopsis "Cross platform game development library")