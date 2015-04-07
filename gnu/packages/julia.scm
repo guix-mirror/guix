9e54e957f99cfdf93c5414ee2f63ecf1faa0c804
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2015-04-07 10:39:25 +0200

    gnu: julia: Mark as unsupported on ARM and MIPS.
    * gnu/packages/julia.scm (julia): Add 'supported-systems' field.

--- gnu/packages/julia.scm
+++ gnu/packages/julia.scm
@@ -161,6 +161,9 @@
        ("pkg-config" ,pkg-config)
        ("python" ,python-2)
        ("which" ,which)))
+    ;; Julia is not officially released for ARM and MIPS.
+    ;; See https://github.com/JuliaLang/julia/issues/10639
+    (supported-systems '("i686-linux" "x86_64-linux"))
     (home-page "http://julialang.org/")
     (synopsis "High-performance dynamic language for technical computing")
     (description
