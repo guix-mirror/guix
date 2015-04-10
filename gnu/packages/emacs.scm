guix-mirror/guix

commit 8ff3df5baa061d1de4fb999921a7db33bcbeb2ff

gnu: Refer to %GNU-BUILD-SYSTEM-MODULES instead of listing modules.
* gnu/packages/cdrom.scm (cdparanoia)[arguments]: Refer to
  %GNU-BUILD-SYSTEM-MODULES instead of providing the actual list.
* gnu/packages/emacs.scm (magit, emacs-w3m, emacs-wget, emms):
  Likewise.
* gnu/packages/haskell.scm (ghc): Likewise.
* gnu/packages/samba.scm (samba): Likewise.
* gnu/packages/video.scm (ffmpeg): Likewise.

gnu/packages/emacs.scm
@@ -247,8 +247,7 @@ when typing parentheses directly or commenting out code line by line.")
      `(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build emacs-utils))
        #:tests? #f  ; no check target
        #:phases
@@ -308,8 +307,7 @@ operations.")
      '(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build emacs-utils))
        #:configure-flags
        (let ((out (assoc-ref %outputs "out")))
@@ -376,8 +374,7 @@ operations.")
      '(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,%gnu-build-system-modules
                            (guix build emacs-utils))
        #:tests? #f  ; no check target
        #:phases
@@ -444,8 +441,7 @@ operations.")
      '(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,%gnu-build-system-modules
                            (guix build emacs-utils))
 
        #:phases (alist-replace

gnu/packages/haskell.scm
@@ -110,8 +110,7 @@
                   (guix build rpath)
                   (srfi srfi-26)
                   (srfi srfi-1))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,%gnu-build-system-modules
                            (guix build rpath))
        #:configure-flags
        (list

gnu/packages/samba.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013, 2015 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2015 Mark H Weaver <mhw@netris.org>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -127,8 +127,7 @@ anywhere.")
                   (guix build utils)
                   (guix build rpath)
                   (srfi srfi-26))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,%gnu-build-system-modules
                            (guix build rpath))
 
        ;; This flag is required to allow for "make test".

gnu/packages/cdrom.scm
@@ -169,8 +169,7 @@ files.")
                   (guix build utils)
                   (guix build rpath)
                   (srfi srfi-26))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build rpath))
        #:phases
         (alist-cons-after

gnu/packages/video.scm
@@ -283,8 +283,7 @@ standards (MPEG-2, MPEG-4 ASP/H.263, MPEG-4 AVC/H.264, and VC-1/VMW3).")
                   (guix build utils)
                   (guix build rpath)
                   (srfi srfi-26))
-       #:imported-modules ((guix build gnu-build-system)
-                           (guix build utils)
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build rpath))
        #:phases
          (alist-replace

commit caaf1933a453e7f3f8d7444f2c97dac3f8a93e1c

gnu: Fix incorrect references to %GNU-BUILD-SYSTEM-MODULES.
Fixes a regression introduced in 8ff3df5.

* gnu/packages/emacs.scm (emacs-w3m)[arguments]: Use quasiquote, not
  quote.
  (emacs-wget)[arguments]: Likewise, and use unquote-splicing for
  %GNU-BUILD-SYSTEM-MODULES.
  (emms): Likewise.
* gnu/packages/haskell.scm (ghc): Likewise.
* gnu/packages/samba.scm (samba): Likewise.

gnu/packages/emacs.scm
@@ -306,7 +306,7 @@ operations.")
               ("imagemagick" ,imagemagick)
               ("emacs" ,emacs-no-x)))
     (arguments
-     '(#:modules ((guix build gnu-build-system)
+     `(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
        #:imported-modules (,@%gnu-build-system-modules
@@ -373,10 +373,10 @@ operations.")
     (inputs `(("wget" ,wget)
               ("emacs" ,emacs-no-x)))
     (arguments
-     '(#:modules ((guix build gnu-build-system)
+     `(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
-       #:imported-modules (,%gnu-build-system-modules
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build emacs-utils))
        #:tests? #f  ; no check target
        #:phases
@@ -440,10 +440,10 @@ operations.")
                    (string-append "all: " rest " emms-print-metadata\n"))))))
     (build-system gnu-build-system)
     (arguments
-     '(#:modules ((guix build gnu-build-system)
+     `(#:modules ((guix build gnu-build-system)
                   (guix build utils)
                   (guix build emacs-utils))
-       #:imported-modules (,%gnu-build-system-modules
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build emacs-utils))
 
        #:phases (alist-replace

gnu/packages/haskell.scm
@@ -116,7 +116,7 @@
                   (guix build rpath)
                   (srfi srfi-26)
                   (srfi srfi-1))
-       #:imported-modules (,%gnu-build-system-modules
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build rpath))
        #:configure-flags
        (list

gnu/packages/samba.scm
@@ -105,7 +105,7 @@ anywhere.")
                "0l9pz2m67vf398q3c2dwn8jwdxsjb20igncf4byhv6yq5dzqlb4g"))))
     (build-system gnu-build-system)
     (arguments
-     '(#:phases (alist-cons-before
+     `(#:phases (alist-cons-before
                  'configure 'chdir
                  (lambda _
                    (chdir "source3"))
@@ -127,7 +127,7 @@ anywhere.")
                   (guix build utils)
                   (guix build rpath)
                   (srfi srfi-26))
-       #:imported-modules (,%gnu-build-system-modules
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build rpath))
 
        ;; This flag is required to allow for "make test".