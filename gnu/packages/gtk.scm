guix-mirror/guix

commit 6b1f238834fd60a487e2465c28f5d333c91c72aa

gnu: gtk+-2: Move html documentation to 'doc' output.
* gnu/packages/gtk.scm (gtk+-2)[outputs]: New field.
  [arguments]<#:configure-flags>: Add '--with-html-dir'.

gnu/packages/gtk.scm
@@ -402,6 +402,7 @@ is part of the GNOME accessibility project.")
              (base32
               "1x14rnjvqslpa1q19fp1qalz5sxds72amsgjk8m7769rwk511jr0"))))
    (build-system gnu-build-system)
+   (outputs '("out" "doc"))
    (propagated-inputs
     `(("atk" ,atk)
       ("gdk-pixbuf" ,gdk-pixbuf)
@@ -424,7 +425,10 @@ is part of the GNOME accessibility project.")
    (arguments
     `(#:make-flags '("CC=gcc")
       #:configure-flags
-      (list "--with-xinput=yes")
+      (list "--with-xinput=yes"
+            (string-append "--with-html-dir="
+                           (assoc-ref %outputs "doc")
+                           "/share/gtk-doc/html"))
       #:phases
       (alist-cons-before
        'configure 'disable-tests

commit 260b07a7d0e67bd9a22259192b6e4222e468b0d9

gnu: gtk+: Pass --with-html-dir=$doc.
Fixes a regression introduced in 6b1f238.

* gnu/packages/gtk.scm (gtk+)[arguments]: Add #:configure-flags.

gnu/packages/gtk.scm
@@ -505,7 +505,12 @@ application suites.")
       ("python-wrapper" ,python-wrapper)
       ("xorg-server" ,xorg-server)))
    (arguments
-    `(#:phases
+    `(;; 47 MiB goes to "out" (24 of which is locale data!), and 26 MiB goes
+      ;; to "doc".
+      #:configure-flags (list (string-append "--with-html-dir="
+                                             (assoc-ref %outputs "doc")
+                                             "/share/gtk-doc/html"))
+      #:phases
       (alist-cons-before
        'configure 'pre-configure
        (lambda _