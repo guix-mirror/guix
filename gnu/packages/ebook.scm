c3768fa49bc50b14c4bd5964eafc9a04f058b3dd
Andreas Enge <andreas@enge.fr>
2015-04-14 20:00:55 +0200

    gnu: calibre: Update to 2.23.0.
    * gnu/packages/ebook.scm (calibre): Update to 2.23.0.

--- gnu/packages/ebook.scm
+++ gnu/packages/ebook.scm
@@ -60,7 +60,7 @@
 (define-public calibre
   (package
     (name "calibre")
-    (version "2.22.0")
+    (version "2.23.0")
     (source
       (origin
         (method url-fetch)
@@ -69,7 +69,7 @@
                             version ".tar.xz"))
         (sha256
           (base32
-           "19hpm5xzhjr0nfjm6xyqxjx2iwm3iw7y6bbs11337arfrxn16ly0"))
+           "0n99la1pacrw4ahb3wk0p68120p7j7v76mvpaf4qnkxy1vfdif7d"))
         ;; Remove non-free or doubtful code, see
         ;; https://lists.gnu.org/archive/html/guix-devel/2015-02/msg00478.html
         (modules '((guix build utils)))
