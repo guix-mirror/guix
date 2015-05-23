508891e62a5f569cc791f45e1f489f9cc74ff5fc
宋文武 <iyzsong@gmail.com>
2015-05-23 10:54:05 +0800

    gnu: gtk+: Update to 3.16.3.
    * gnu/packages/gtk.scm (gtk+): Update to 3.16.3.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -486,7 +486,7 @@ application suites.")
 (define-public gtk+
   (package (inherit gtk+-2)
    (name "gtk+")
-   (version "3.16.2")
+   (version "3.16.3")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnome/sources/" name "/"
@@ -494,7 +494,7 @@ application suites.")
                                 name "-" version ".tar.xz"))
             (sha256
              (base32
-              "1yhwg2l72l3khfkprydcjlpxjrg11ccqfc80sjl56llz3jk66fd0"))))
+              "195ykv53sl2gsc847wcnd79zilm1yzcc2cfjxnrakhh2dd5gshr9"))))
    (propagated-inputs
     `(("at-spi2-atk" ,at-spi2-atk)
       ("atk" ,atk)
