975b8942a664f9938a9128b4fa617f925078d01b
amz3 <amirouche@hypermove.net>
2015-06-13 21:10:45 +0200

    gnu: Rename (gnu packages dwm) to (gnu packages suckless).
    * gnu/packages/dwm.scm: Rename this ...
    * gnu/packages/suckless.scm: ... to this.
    * gnu-system.am (GNU_SYSTEM_MODULES): Change dwm.scm to suckless.scm.

--- gnu/packages/suckless.scm
+++ gnu/packages/suckless.scm
@@ -17,7 +17,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (gnu packages dwm)
+(define-module (gnu packages suckless)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
