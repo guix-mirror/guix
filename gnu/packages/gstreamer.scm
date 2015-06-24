96693d1d92ba0d57cd47259e846bbb33c9076f32
宋文武 <iyzsong@gmail.com>
2015-06-24 23:02:32 +0800

    gnu: gstreamer: Add search path specification for 'GST_PLUGIN_SYSTEM_PATH'.
    This reverts commit e53fc0c8a, which add it wrongly to gst-plugins-base.
    
    * gnu/packages/gstreamer.scm (gstreamer)[native-search-paths]: New field.
      (gst-plugins-base)[native-search-paths]: Remove.

--- gnu/packages/gstreamer.scm
+++ gnu/packages/gstreamer.scm
@@ -109,6 +109,10 @@ arrays of data.")
        ("perl" ,perl)
        ("pkg-config" ,pkg-config)
        ("python-wrapper" ,python-wrapper)))
+    (native-search-paths
+     (list (search-path-specification
+            (variable "GST_PLUGIN_SYSTEM_PATH")
+            (files '("lib/gstreamer-1.0")))))
     (home-page "http://gstreamer.freedesktop.org/")
     (synopsis "Multimedia library")
     (description
@@ -171,10 +175,6 @@ This package provides the core library and elements.")
           ;; for g-ir-scanner.
           (setenv "CC" "gcc"))
         %standard-phases)))
-    (native-search-paths
-     (list (search-path-specification
-            (variable "GST_PLUGIN_SYSTEM_PATH")
-            (files '("lib/gstreamer-1.0")))))
     (home-page "http://gstreamer.freedesktop.org/")
     (synopsis
      "Plugins for the GStreamer multimedia library")
