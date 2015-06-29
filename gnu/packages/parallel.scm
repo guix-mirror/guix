b1f5fe0eb7c5b1562a4968411d20fc489dea2e0b
Ludovic Courtès <ludo@gnu.org>
2015-06-29 14:39:03 +0200

    gnu: parallel: Update to 20150622.
    * gnu/packages/parallel.scm (parallel): Update to 20150622.

--- gnu/packages/parallel.scm
+++ gnu/packages/parallel.scm
@@ -28,7 +28,7 @@
 (define-public parallel
   (package
     (name "parallel")
-    (version "20150422")
+    (version "20150622")
     (source
      (origin
       (method url-fetch)
@@ -36,7 +36,7 @@
                           version ".tar.bz2"))
       (sha256
        (base32
-        "1x6lvbw6msjkibadihzr2s0mbbcx2h2wxd723q5bgz6mcnsml346"))))
+        "0j7gg88zk2r4jv3mvmrl4bhwcv9k8rxanygp6lssbx685ab9qfln"))))
     (build-system gnu-build-system)
     (inputs `(("perl" ,perl)))
     (home-page "http://www.gnu.org/software/parallel/")
