32efa254a80672bdf5199b8e200764615a3cf68b
David Thompson <davet@gnu.org>
2015-07-01 20:32:07 -0400

    scripts: Add 'container' subcommand.
    * guix/scripts/container.scm: New file.
    * guix/scripts/container/exec.scm: New file.
    * po/guix/POTFILES.in: Add them.
    * Makefile.am (MODULES): Add them.
    * doc/guix.texi (Invoking guix container): New section.

---
+++ guix/scripts/container.scm
@@ -0,0 +1,63 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 David Thompson <davet@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (guix scripts container)
+  #:use-module (ice-9 match)
+  #:use-module (guix ui)
+  #:export (guix-container))
+
+(define (show-help)
+  (display (_ "Usage: guix container ACTION ARGS...
+Build and manipulate Linux containers.\n"))
+  (newline)
+  (display (_ "The valid values for ACTION are:\n"))
+  (newline)
+  (display (_ "\
+   exec            execute a command inside of an existing container\n"))
+  (newline)
+  (display (_ "
+  -h, --help             display this help and exit"))
+  (display (_ "
+  -V, --version          display version information and exit"))
+  (newline)
+  (show-bug-report-information))
+
+(define %actions '("exec"))
+
+(define (resolve-action name)
+  (let ((module (resolve-interface
+                 `(guix scripts container ,(string->symbol name))))
+        (proc (string->symbol (string-append "guix-container-" name))))
+    (module-ref module proc)))
+
+(define (guix-container . args)
+  (with-error-handling
+    (match args
+      (()
+       (format (current-error-port)
+               (_ "guix container: missing action~%")))
+      ((or ("-h") ("--help"))
+       (show-help)
+       (exit 0))
+      (("--version")
+       (show-version-and-exit "guix container"))
+      ((action args ...)
+       (if (member action %actions)
+           (apply (resolve-action action) args)
+           (format (current-error-port)
+                   (_ "guix container: invalid action~%")))))))
---
+++ guix/scripts/container/exec.scm
@@ -0,0 +1,86 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 David Thompson <davet@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (guix scripts container exec)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-11)
+  #:use-module (srfi srfi-37)
+  #:use-module (guix scripts)
+  #:use-module (guix ui)
+  #:use-module (guix utils)
+  #:use-module (gnu build linux-container)
+  #:export (guix-container-exec))
+
+(define %options
+  (list (option '(#\h "help") #f #f
+                (lambda args
+                  (show-help)
+                  (exit 0)))
+        (option '(#\V "version") #f #f
+                (lambda args
+                  (show-version-and-exit "guix container exec")))))
+
+(define (show-help)
+  (display (_ "Usage: guix container exec PID COMMAND [ARGS...]
+Execute COMMMAND within the container process PID.\n"))
+  (newline)
+  (display (_ "
+  -h, --help             display this help and exit"))
+  (display (_ "
+  -V, --version          display version information and exit"))
+  (newline)
+  (show-bug-report-information))
+
+(define (partition-args args)
+  "Split ARGS into two lists; one containing the arguments for this program,
+and the other containing arguments for the command to be executed."
+  (break (lambda (arg)
+           ;; Split after the pid argument.
+           (not (false-if-exception (string->number arg))))
+         args))
+
+(define (guix-container-exec . args)
+  (define (handle-argument arg result)
+    (if (assoc-ref result 'pid)
+        (leave (_ "~a: extraneous argument~%") arg)
+        (alist-cons 'pid (string->number* arg) result)))
+
+  (with-error-handling
+    (let-values (((args command) (partition-args args)))
+      (let* ((opts (parse-command-line args %options '(())
+                                       #:argument-handler
+                                       handle-argument))
+             (pid  (assoc-ref opts 'pid)))
+
+        (unless pid
+          (leave (_ "no pid specified~%")))
+
+        (when (null? command)
+          (leave (_ "no command specified~%")))
+
+        (unless (file-exists? (string-append "/proc/" (number->string pid)))
+          (leave (_ "no such process ~d~%") pid))
+
+        (let ((result (container-excursion pid
+                        (lambda ()
+                          (match command
+                            ((program . program-args)
+                             (apply execlp program program program-args)))))))
+          (unless (zero? result)
+            (leave (_ "exec failed with status ~d~%") result)))))))
