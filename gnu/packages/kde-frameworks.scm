4a8276704fb43d573b90ba19cf7b1b91978cfd18
Andreas Enge <andreas@enge.fr>
2015-07-24 23:01:14 +0200

    gnu: Add kwindowsystem.
    * gnu/packages/kde-frameworks.scm (kwindowsystem): New variable.

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -21,7 +21,10 @@
   #:use-module (guix download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages xorg))
 
 (define kde-frameworks-version "5.12.0")
 
@@ -48,3 +51,40 @@
 modules provided by CMake to find common software.  In addition, it provides
 common build settings used in software produced by the KDE community.")
     (license license:bsd-3)))
+
+(define-public kwindowsystem
+  (package
+    (name "kwindowsystem")
+    (version kde-frameworks-version)
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append "http://download.kde.org/stable/frameworks/"
+                            (version-major+minor version) "/"
+                            name "-" version ".tar.xz"))
+        (sha256
+          (base32 "0fjxhf07r186cmp0mjvinrwxg4z90zlyvycqhy0n18fdp67szckl"))))
+    (build-system cmake-build-system)
+    (native-inputs
+      `(("pkg-config" ,pkg-config)
+        ("xorg-server" ,xorg-server))) ; for the tests
+    (inputs
+      `(("extra-cmake-modules" ,extra-cmake-modules)
+        ("libxrender" ,libxrender)
+        ("qt" ,qt)
+        ("xcb-utils-keysyms" ,xcb-util-keysyms)))
+    (arguments
+      `(#:tests? #f)) ; FIXME: The first seven tests fail with "Exception".
+    (home-page "https://community.kde.org/Frameworks")
+    (synopsis "KDE access to the windowing system")
+    (description "KWindowSystem provides information about and allows
+interaction with the windowing system.  It provides a high level API, which
+is windowing system independent and has platform specific
+implementations.  This API is inspired by X11 and thus not all functionality
+is available on all windowing systems.
+
+In addition to the high level API, this framework also provides several
+lower level classes for interaction with the X Windowing System.")
+    ;; Some source files mention lgpl2.0+, but the included license is
+    ;; the lgpl2.1. Some source files are under non-copyleft licenses.
+    (license license:lgpl2.1+)))
