008b525734d42276b796c8fcde2d9161b268c6c8
Mark H Weaver <mhw@netris.org>
2015-08-04 16:10:42 -0400

    gnu: libotr: Modify description to not rely on preservation of formatting.
    * gnu/packages/messaging.scm (libotr)[description]: Modify formatting.

--- gnu/packages/messaging.scm
+++ gnu/packages/messaging.scm
@@ -60,16 +60,14 @@
     (synopsis "Off-the-Record (OTR) Messaging Library and Toolkit")
     (description
      "OTR allows you to have private conversations over instant messaging by
-providing:
-* Encryption: No one else can read your instant messages.
-* Authentication: You are assured the correspondent is who you think it is.
-* Deniability: The messages you send do not have digital signatures that are
-  checkable by a third party.  Anyone can forge messages after a conversation
-  to make them look like they came from you.  However, during a conversation,
-  your correspondent is assured the messages he sees are authentic and
-  unmodified.
-* Perfect forward secrecy: If you lose control of your private keys, no
-  previous conversation is compromised.")
+providing: (1) Encryption: No one else can read your instant messages.  (2)
+Authentication: You are assured the correspondent is who you think it is.  (3)
+Deniability: The messages you send do not have digital signatures that are
+checkable by a third party.  Anyone can forge messages after a conversation to
+make them look like they came from you.  However, during a conversation, your
+correspondent is assured the messages he sees are authentic and
+unmodified.  (4) Perfect forward secrecy: If you lose control of your private
+keys, no previous conversation is compromised.")
     (home-page "https://otr.cypherpunks.ca/")
     (license (list lgpl2.1 gpl2))))
 
