cbe95af7123fe4d384d36f4a55bfba7286a8de1b
Manolis Ragkousis <manolis837@gmail.com>
2015-08-19 14:11:15 +0300

    Remove unneeded HAVE_UNSHARE.
    * src/libstore/build.cc (CHROOT_ENABLED): Remove HAVE_UNSHARE.

--- nix/libstore/build.cc
+++ nix/libstore/build.cc
@@ -51,7 +51,7 @@
 #include <linux/fs.h>
 #endif
 
-#define CHROOT_ENABLED HAVE_CHROOT && HAVE_UNSHARE && HAVE_SYS_MOUNT_H && defined(MS_BIND) && defined(MS_PRIVATE) && defined(CLONE_NEWNS) && defined(SYS_pivot_root)
+#define CHROOT_ENABLED HAVE_CHROOT && HAVE_SYS_MOUNT_H && defined(MS_BIND) && defined(MS_PRIVATE) && defined(CLONE_NEWNS) && defined(SYS_pivot_root)
 
 #if CHROOT_ENABLED
 #include <sys/socket.h>
