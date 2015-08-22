491ae037ec625e6a53ffe2a5f80719a400baaa2f
Leo Famulari <leo@famulari.name>
2015-08-21 20:47:05 -0400

    gnu: Add dvtm.
    * gnu/packages/dvtm.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/dvtm.scm
@@ -0,0 +1,54 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 Leo Famulari <leo@famulari.name>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages dvtm)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix download)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (gnu packages ncurses))
+
+(define-public dvtm
+  (package
+   (name "dvtm")
+   (version "0.14")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "http://www.brain-dump.org/projects/dvtm/dvtm-"
+                                 version ".tar.gz"))
+            (sha256
+             (base32 "0ykl8dz7ivjgdzhmhlgidnp2ffh5gxq9lbg276w7iid4z10v76wa"))))
+   (build-system gnu-build-system)
+   (arguments
+    `(#:make-flags (list "CC=gcc"
+                         (string-append "PREFIX=" (assoc-ref %outputs "out")))
+      #:phases (modify-phases %standard-phases
+                 (delete 'configure)
+                 (delete 'check))))  ; no test suite
+   (inputs `(("ncurses" ,ncurses)))
+   (synopsis "Tiling window management for the console")
+   (description "dvtm brings the concept of tiling window management,
+popularized by X11-window managers like dwm, to the console.  As a console
+window manager it tries to make it easy to work with multiple console based
+programs.")
+   (home-page "http://www.brain-dump.org/projects/dvtm/")
+
+   ;; "dvtm reuses some code of dwm and is released under the same MIT/X11
+   ;; license. The terminal emulation part is licensed under the ISC license."
+   ;; source: http://www.brain-dump.org/projects/dvtm/#license
+   (license (list expat isc))))
