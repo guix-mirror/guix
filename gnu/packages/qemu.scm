d2a633660983c12b00ef1489a73289b3871f905b
Mark H Weaver <mhw@netris.org>
2015-09-26 00:35:00 -0400

    gnu: qemu: Update to 2.4.0.1.  Include fix for CVE-2015-6855.
    * gnu/packages/patches/qemu-CVE-2015-3209.patch,
      gnu/packages/patches/qemu-CVE-2015-4037.patch,
      gnu/packages/patches/qemu-CVE-2015-4103.patch,
      gnu/packages/patches/qemu-CVE-2015-4104.patch,
      gnu/packages/patches/qemu-CVE-2015-4105.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt1.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt2.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt3.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt4.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt5.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt6.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt7.patch,
      gnu/packages/patches/qemu-CVE-2015-4106-pt8.patch,
      gnu/packages/patches/qemu-CVE-2015-5745.patch: Delete files.
    * gnu/packages/patches/qemu-CVE-2015-6855.patch: New file.
    * gnu-system.am (dist_patch_DATA): Add the new patch and delete the old ones.
    * gnu/packages/qemu.scm (qemu-headless): Update to 2.4.0.1.  Add the new patch
      and delete the old ones.

--- gnu/packages/qemu.scm
+++ gnu/packages/qemu.scm
@@ -44,28 +44,15 @@
   ;; This is QEMU without GUI support.
   (package
     (name "qemu-headless")
-    (version "2.3.1")
+    (version "2.4.0.1")
     (source (origin
              (method url-fetch)
              (uri (string-append "http://wiki.qemu-project.org/download/qemu-"
                                  version ".tar.bz2"))
              (sha256
               (base32
-               "0px1vhkglxzjdxkkqln98znv832n1sn79g5inh3aw72216c047b6"))
-             (patches (map search-patch '("qemu-CVE-2015-3209.patch"
-                                          "qemu-CVE-2015-4037.patch"
-                                          "qemu-CVE-2015-4103.patch"
-                                          "qemu-CVE-2015-4104.patch"
-                                          "qemu-CVE-2015-4105.patch"
-                                          "qemu-CVE-2015-4106-pt1.patch"
-                                          "qemu-CVE-2015-4106-pt2.patch"
-                                          "qemu-CVE-2015-4106-pt3.patch"
-                                          "qemu-CVE-2015-4106-pt4.patch"
-                                          "qemu-CVE-2015-4106-pt5.patch"
-                                          "qemu-CVE-2015-4106-pt6.patch"
-                                          "qemu-CVE-2015-4106-pt7.patch"
-                                          "qemu-CVE-2015-4106-pt8.patch"
-                                          "qemu-CVE-2015-5745.patch")))))
+               "1nqv5p94zpnhcaqkifnn83ap7dd0qrb0qiicswbyhhby0f48pzpc"))
+             (patches (map search-patch '("qemu-CVE-2015-6855.patch")))))
     (build-system gnu-build-system)
     (arguments
      '(#:phases (alist-replace
