guix-mirror/guix

commit 5d8d8f3b0a07edb3bd435830b076749f906537d8

lint: Improve 'check-texinfo-markup'.
* guix/scripts/lint.scm (check-description-style): Set 'field' parameter
  when emitting a warning in 'check-texinfo-markup'.  Catch any error
  that may occur in during the 'texi->plain-text' conversion.  This is a
  followup to commit 2748ee3.

guix/scripts/lint.scm
@@ -146,11 +146,11 @@ monad."
   (define (check-texinfo-markup description)
     "Check that DESCRIPTION can be parsed as a Texinfo fragment.  If the
 markup is valid return a plain-text version of DESCRIPTION, otherwise #f."
-    (catch 'parser-error
-      (lambda () (texi->plain-text description))
-      (lambda (keys . args)
-        (emit-warning package (_ "Texinfo markup in description is invalid"))
-        #f)))
+    (unless (false-if-exception (texi->plain-text description))
+      (emit-warning package
+                    (_ "Texinfo markup in description is invalid")
+                    'description)
+      #f))
 
   (define (check-proper-start description)
     (unless (or (properly-starts-sentence? description)

commit cd8b7cfb3f80631049bfb548838209b354222705

lint: Fix 'check-texinfo-markup'.
Fixes a regression introduced in 5d8d8f3.

* guix/scripts/lint.scm (check-description-style): When no exception is
  thrown in 'check-texinfo-markup', return the rendered description.

guix/scripts/lint.scm
@@ -146,11 +146,13 @@ monad."
   (define (check-texinfo-markup description)
     "Check that DESCRIPTION can be parsed as a Texinfo fragment.  If the
 markup is valid return a plain-text version of DESCRIPTION, otherwise #f."
-    (unless (false-if-exception (texi->plain-text description))
-      (emit-warning package
-                    (_ "Texinfo markup in description is invalid")
-                    'description)
-      #f))
+    (catch #t
+      (lambda () (texi->plain-text description))
+      (lambda (keys . args)
+        (emit-warning package
+                      (_ "Texinfo markup in description is invalid")
+                      'description)
+        #f)))
 
   (define (check-proper-start description)
     (unless (or (properly-starts-sentence? description)