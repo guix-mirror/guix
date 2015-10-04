85b81cf8c1fa79cc3929c5fadfabc1b9a47b77c3
Ludovic Courtès <ludo@gnu.org>
2015-10-04 10:19:42 +0200

    packages: Use locales from the /X.Y sub-directory of the locale package.
    This is a followup to commit f2d7bbb.
    
    * guix/packages.scm (patch-and-repack)[build]: Append "/X.Y" to
      LOCPATH.

--- guix/packages.scm
+++ guix/packages.scm
@@ -477,7 +477,10 @@ IMPORTED-MODULES specify modules to use/import for use by SNIPPET."
           (when #+locales
             ;; First of all, install a UTF-8 locale so that UTF-8 file names
             ;; are correctly interpreted.  During bootstrap, LOCALES is #f.
-            (setenv "LOCPATH" (string-append #+locales "/lib/locale"))
+            (setenv "LOCPATH"
+                    (string-append #+locales "/lib/locale/"
+                                   #+(and locales
+                                          (package-version locales))))
             (setlocale LC_ALL "en_US.utf8"))
 
           (setenv "PATH" (string-append #+xz "/bin" ":"
