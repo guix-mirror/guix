d82633d06a0b7f24e98f12a2686c1ffdf12f5cd3
Andreas Enge <andreas@enge.fr>
2015-10-16 22:56:39 +0200

    gnu: mutt: Update to 1.5.24.
    * gnu/packages/patches/mutt-CVE-2014-9116.patch: Delete file.
    * gnu/packages/mail.scm (mutt): Update to 1.5.24.
      [source]: Update and remove patch.
    * gnu-system.am (dist_patch_DATA): Unregister removed patch.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -171,21 +171,15 @@ aliasing facilities to work just as they would on normal mail.")
 (define-public mutt
   (package
     (name "mutt")
-    (version "1.5.23")
+    (version "1.5.24")
     (source (origin
              (method url-fetch)
-             (uri (list ;; Temporarily put bitbucket first, because
-                        ;; ftp.mutt.org has been down for a while.
-                        (string-append "https://bitbucket.org/mutt/mutt/downloads/mutt-"
-                                       version ".tar.gz")
-                        (string-append "ftp://ftp.mutt.org/mutt/devel/mutt-"
-                                       version ".tar.gz")))
+             (uri (string-append "ftp://ftp.mutt.org/pub/mutt/mutt-"
+                                 version ".tar.gz"))
              (sha256
               (base32
-               "0dzx4qk50pjfsb6cs5jahng96a52k12f7pm0sc78iqdrawg71w1s"))
-             (patches (map search-patch
-                           '("mutt-CVE-2014-9116.patch"
-                             "mutt-store-references.patch")))))
+               "0012njrgxf1barjksqkx7ccid2l0xyikhna9mjs9vcfpbrvcm4m2"))
+             (patches (list (search-patch "mutt-store-references.patch")))))
     (build-system gnu-build-system)
     (inputs
      `(("cyrus-sasl" ,cyrus-sasl)
