guix-mirror/guix

commit d18c3c646c9a72f843332ee5bd0abfad9cbf6998

gnu: mesa: Update to 11.0.3.
* gnu/packages/gl.scm (mesa): Update to 11.0.3.

gnu/packages/gl.scm
@@ -185,7 +185,7 @@ also known as DXTn or DXTC) for Mesa.")
 (define-public mesa
   (package
     (name "mesa")
-    (version "10.5.4")
+    (version "11.0.3")
     (source
       (origin
         (method url-fetch)
@@ -193,7 +193,7 @@ also known as DXTn or DXTC) for Mesa.")
                             version "/mesa-" version ".tar.xz"))
         (sha256
          (base32
-          "00v89jna7m6r2w1yrnx09isc97r2bd1hkn4jib445n1078zp47mm"))))
+          "1mikw0biw0wxq0fn3cp18bm6kjrkd66fy84774yc5b91rvp94adb"))))
     (build-system gnu-build-system)
     (propagated-inputs
       `(("glproto" ,glproto)
@@ -257,9 +257,6 @@ also known as DXTn or DXTC) for Mesa.")
                             "src/mesa/main/texcompress_s3tc.c")
                         (("\"libtxc_dxtn\\.so")
                          (string-append "\"" s2tc "/lib/libtxc_dxtn.so")))
-                      (substitute* "src/gallium/targets/egl-static/egl_st.c"
-                        (("\"libglapi\"")
-                         (string-append "\"" out "/lib/libglapi\"")))
                       (substitute* "src/loader/loader.c"
                         (("dlopen\\(\"libudev\\.so")
                          (string-append "dlopen(\"" udev "/lib/libudev.so")))

commit 20848f3637af2bb97db03e9cd2e01d11494c6f02

gnu: mesa: Patch the dlopen call for 'libudev.so'.
Fixes a regression introduced in d18c3c6.

* gnu/packages/gl.scm (mesa)[arguments]: Update the 'substitute*'
  call for 'libudev.so'.

gnu/packages/gl.scm
@@ -260,8 +260,9 @@ also known as DXTn or DXTC) for Mesa.")
                         (("\"libtxc_dxtn\\.so")
                          (string-append "\"" s2tc "/lib/libtxc_dxtn.so")))
                       (substitute* "src/loader/loader.c"
-                        (("dlopen\\(\"libudev\\.so")
-                         (string-append "dlopen(\"" udev "/lib/libudev.so")))
+                        (("udev_handle = dlopen\\(name")
+                         (string-append "udev_handle = dlopen(\""
+                                        udev "/lib/libudev.so\"")))
                       (substitute* "src/glx/dri_common.c"
                         (("dlopen\\(\"libGL\\.so")
                          (string-append "dlopen(\"" out "/lib/libGL.so")))