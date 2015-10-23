6747de140d6ef714a7940bfa4cd0db544c9fe945
宋文武 <iyzsong@gmail.com>
2015-10-23 21:31:49 +0800

    gnu: bluez: Update to 5.35, fix test.
    * gnu/packages/patches/bluez-tests.patch: New file.
    * gnu-system.am (dist_patch_DATA): Add it.
    * gnu/packages/linux.scm (bluez): Update to 5.35.
      [source]: Add patch.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -2315,7 +2315,7 @@ applications.")
 (define-public bluez
   (package
     (name "bluez")
-    (version "5.30")
+    (version "5.35")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -2323,7 +2323,9 @@ applications.")
                     version ".tar.xz"))
               (sha256
                (base32
-                "0b1qbnq1xzcdw5rajg9yyg31bf21jnff0n6gnf1snz89bbdllfhy"))))
+                "1qphz25hganfnd5ipfscbj7s70anv5favmwqmi9ig2saciaf1zhs"))
+              (patches
+               (list (search-patch "bluez-tests.patch")))))
     (build-system gnu-build-system)
     (arguments
      '(#:configure-flags
