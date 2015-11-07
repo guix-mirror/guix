0ca1eb705d29c20f901fc385ee4e1bb1eaa52f75
Mark H Weaver <mhw@netris.org>
2015-11-06 22:08:30 -0500

    gnu: icecat: Add several security fixes.
    * gnu/packages/patches/icecat-CVE-2015-4513-pt01.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt02.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt03.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt04.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt05.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt06.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt07.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt08.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt09.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt10.patch,
      gnu/packages/patches/icecat-CVE-2015-4513-pt11.patch,
      gnu/packages/patches/icecat-CVE-2015-7188.patch,
      gnu/packages/patches/icecat-CVE-2015-7189.patch,
      gnu/packages/patches/icecat-CVE-2015-7193.patch,
      gnu/packages/patches/icecat-CVE-2015-7194.patch,
      gnu/packages/patches/icecat-CVE-2015-7196.patch,
      gnu/packages/patches/icecat-CVE-2015-7197.patch,
      gnu/packages/patches/icecat-CVE-2015-7198.patch,
      gnu/packages/patches/icecat-CVE-2015-7199.patch: New files.
    * gnu-system.am (dist_patch_DATA): Add them.
    * gnu/packages/gnuzilla.scm (icecat)[source]: Add patches.

--- gnu/packages/gnuzilla.scm
+++ gnu/packages/gnuzilla.scm
@@ -239,7 +239,26 @@ standards.")
        (base32
         "0vm6f7f1i5vkq2713mgzjdfnm8rpz9l0q8sv4s123vsam0j9gzh8"))
       (patches (map search-patch '("icecat-avoid-bundled-includes.patch"
-                                   "icecat-freetype-2.6.patch")))
+                                   "icecat-freetype-2.6.patch"
+                                   "icecat-CVE-2015-4513-pt01.patch"
+                                   "icecat-CVE-2015-4513-pt02.patch"
+                                   "icecat-CVE-2015-4513-pt03.patch"
+                                   "icecat-CVE-2015-4513-pt04.patch"
+                                   "icecat-CVE-2015-4513-pt05.patch"
+                                   "icecat-CVE-2015-4513-pt06.patch"
+                                   "icecat-CVE-2015-4513-pt07.patch"
+                                   "icecat-CVE-2015-4513-pt08.patch"
+                                   "icecat-CVE-2015-4513-pt09.patch"
+                                   "icecat-CVE-2015-4513-pt10.patch"
+                                   "icecat-CVE-2015-4513-pt11.patch"
+                                   "icecat-CVE-2015-7188.patch"
+                                   "icecat-CVE-2015-7189.patch"
+                                   "icecat-CVE-2015-7193.patch"
+                                   "icecat-CVE-2015-7194.patch"
+                                   "icecat-CVE-2015-7196.patch"
+                                   "icecat-CVE-2015-7197.patch"
+                                   "icecat-CVE-2015-7198.patch"
+                                   "icecat-CVE-2015-7199.patch")))
       (modules '((guix build utils)))
       (snippet
        '(begin
