guix-mirror/guix

commit 279ec1df202698d94c7992162621f6f7c00a7695

ftp-client: Restrict to TCP connections.
* guix/ftp-client.scm (ftp-open): Force SOCK_STREAM as the socket type
  to avoid calling 'connect' on a datagram socket.

guix/ftp-client.scm
@@ -95,7 +95,7 @@ or a TCP port number), and return it."
 
   (let loop ((addresses addresses))
     (let* ((ai (car addresses))
-           (s  (socket (addrinfo:fam ai) (addrinfo:socktype ai)
+           (s  (socket (addrinfo:fam ai) SOCK_STREAM ;TCP only
                        (addrinfo:protocol ai))))
 
       (catch 'system-error

commit b7f4677999f9ad9f457088b69e1dc86e9664ec76

ftp-client: Restrict to TCP/IP connections.
Fixes <http://bugs.gnu.org/21925>.
Regression introduced in 279ec1d.
Reported by Chris Marusich <cmmarusich@gmail.com>.

* guix/ftp-client.scm (ftp-open): Restrict sockets to
  SOCK_STREAM/IPPROTO_IP.

guix/ftp-client.scm
@@ -140,8 +140,9 @@ TIMEOUT, an ETIMEDOUT error is raised."
 
   (let loop ((addresses addresses))
     (let* ((ai (car addresses))
-           (s  (socket (addrinfo:fam ai) SOCK_STREAM ;TCP only
-                       (addrinfo:protocol ai))))
+           (s  (socket (addrinfo:fam ai)
+                       ;; TCP/IP only
+                       SOCK_STREAM IPPROTO_IP)))
 
       (catch 'system-error
         (lambda ()