guix-mirror/guix

commit a9346faee6f61d7a2b0e066b110c9ce660827dbd

gnu: weechat: Record the absolute file name of 'python'.
Fixes <http://bugs.gnu.org/21879>.
Reported by Luis Felipe López Acevedo <felipe.lopez@openmailbox.org>.

* gnu/packages/weechat.scm (weechat)[arguments]: Add
  'set-python-file-name' phase.

gnu/packages/weechat.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2014 Kevin Lemonnier <lemonnierk@ulrar.net>
+;;; Copyright © 2015 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -82,7 +83,15 @@
        #:phases (modify-phases %standard-phases
                   (add-before 'configure 'autogen
                     (lambda _
-                      (zero? (system* "./autogen.sh")))))))
+                      (zero? (system* "./autogen.sh"))))
+                  (add-before 'build 'set-python-file-name
+                    (lambda* (#:key inputs #:allow-other-keys)
+                      (substitute* "src/plugins/python/weechat-python.c"
+                        (("python2_bin = .*;")
+                         (string-append "python2_bin = \""
+                                        (assoc-ref inputs "python")
+                                        "/bin/python\";\n")))
+                      #t)))))
     (synopsis "Extensible chat client")
     (description "WeeChat (Wee Enhanced Environment for Chat) is an
 Internet Relay Chat client, which is designed to be light and fast.

commit 7be4a4b0ae68de0974e0ac6a4e313f1001bcd73d

gnu: weechat: Fix patching of assignments to 'python2_bin'.
Fixes a regression introduced in a9346fa.

* gnu/packages/weechat.scm (weechat)[arguments]: Make sure PYTHON2_BIN
  can be freed with 'free'.

gnu/packages/weechat.scm
@@ -87,10 +87,10 @@
                   (add-before 'build 'set-python-file-name
                     (lambda* (#:key inputs #:allow-other-keys)
                       (substitute* "src/plugins/python/weechat-python.c"
-                        (("python2_bin = .*;")
-                         (string-append "python2_bin = \""
+                        (("python2_bin = weechat_python_get_python2_bin.*;")
+                         (string-append "python2_bin = strdup (\""
                                         (assoc-ref inputs "python")
-                                        "/bin/python\";\n")))
+                                        "/bin/python\");\n")))
                       #t)))))
     (synopsis "Extensible chat client")
     (description "WeeChat (Wee Enhanced Environment for Chat) is an