guix-mirror/guix

commit 8a5063f7774c225626224697b5548f2e953c6af4

http-client: 'http-fetch' and 'http-fetch/cached' support HTTPS.
* guix/http-client.scm (http-fetch): Use 'open-connection-for-uri', to
support HTTPS.

guix/http-client.scm
@@ -35,7 +35,8 @@
   #:use-module ((guix build utils)
                 #:select (mkdir-p dump-port))
   #:use-module ((guix build download)
-                #:select (open-socket-for-uri resolve-uri-reference))
+                #:select (open-socket-for-uri
+                          open-connection-for-uri resolve-uri-reference))
   #:re-export (open-socket-for-uri)
   #:export (&http-get-error
             http-get-error?
@@ -207,7 +208,7 @@ unbuffered port, suitable for use in `filtered-port'.
 
 Raise an '&http-get-error' condition if downloading fails."
   (let loop ((uri uri))
-    (let ((port (or port (open-socket-for-uri uri))))
+    (let ((port (or port (open-connection-for-uri uri))))
       (unless buffered?
         (setvbuf port _IONBF))
       (let*-values (((resp data)

commit 25d188ce12e4e192e4167d03728a8ff69fe0bb35

http-client: 'http-fetch' converts strings to URIs.
* guix/http-client.scm (http-fetch): Use 'string->uri' if URI is a
string.  Fixes a regression introduced in 8a5063f.

guix/http-client.scm
@@ -207,7 +207,9 @@ textual.  Follow any HTTP redirection.  When BUFFERED? is #f, return an
 unbuffered port, suitable for use in `filtered-port'.
 
 Raise an '&http-get-error' condition if downloading fails."
-  (let loop ((uri uri))
+  (let loop ((uri (if (string? uri)
+                      (string->uri uri)
+                      uri)))
     (let ((port (or port (open-connection-for-uri uri))))
       (unless buffered?
         (setvbuf port _IONBF))