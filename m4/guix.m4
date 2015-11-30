guix-mirror/guix

commit 968ae903189c6c4a41c0e2cddf41ffc5a8c32f93

build: Reject ARMv6 systems.
Fixes <http://bugs.gnu.org/21987>.
Reported by Martin Vahi <martin.vahi@softf1.com>.

* m4/guix.m4 (GUIX_SYSTEM_TYPE): Restrict ARM systems to arm|armv[7-9].

m4/guix.m4
@@ -50,8 +50,13 @@ AC_DEFUN([GUIX_SYSTEM_TYPE], [
        i*86)
 	  machine_name="i686";;
        amd64)
-	  machine_name="x86_64";;
-       arm*)
+          machine_name="x86_64";;
+       arm|armv[7-9]*)
+          # Here we want to exclude CPUs such as "armv6l".  On ARMv7
+          # machines, we normally get "armv7l".  However, in Guix, we
+          # configure with --build=arm-unknown-linux-gnueabihf, leading
+          # to just "arm", so we also have to allow it.
+          #
           # TODO: If not cross-compiling, add a sanity check to make
           #       sure this build machine has the needed features to
           #       support executables compiled using our armhf gcc,

commit dfdb15d9c2badaebfd6a047b303b687ff251256a

build: Fix detection of ARM systems.
Reported by Andreas Enge <andreas@enge.fr>.
This fixes a regression introduced in 968ae903.

* m4/guix.m4 (GUIX_SYSTEM_TYPE): Add extra square braquets around
armv[7-9] pattern.

m4/guix.m4
@@ -51,7 +51,7 @@ AC_DEFUN([GUIX_SYSTEM_TYPE], [
 	  machine_name="i686";;
        amd64)
           machine_name="x86_64";;
-       arm|armv[7-9]*)
+       arm|armv[[7-9]]*)
           # Here we want to exclude CPUs such as "armv6l".  On ARMv7
           # machines, we normally get "armv7l".  However, in Guix, we
           # configure with --build=arm-unknown-linux-gnueabihf, leading