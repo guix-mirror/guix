8cabd349b4f82e31a49202cb56b0a999fc926804
Efraim Flashner <efraim@flashner.co.il>
2015-12-11 12:23:33 +0200

    gnu: goffice: Update to 0.10.24.
    * gnu/packages/gnome.scm (goffice): Update to 0.10.24.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -1458,14 +1458,14 @@ Hints specification (EWMH).")
 (define-public goffice
   (package
     (name "goffice")
-    (version "0.10.14")
+    (version "0.10.24")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
                                   (version-major+minor version)  "/"
                                   name "-" version ".tar.xz"))
               (sha256
-               (base32 "0kj0iwng6w4axm7yv2zy7myn5dhw5ilrlq2pzrjlm9i852ikqy60"))))
+               (base32 "0nmghi26dpjcw7knkviq031crhm0zjy4k650pv1jj3hb1fmhx9yd"))))
     (build-system gnu-build-system)
     (inputs
      `(("gtk+" ,gtk+)
