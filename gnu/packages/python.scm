64cb064c0c55f13b53d2d9fa8fc3e8e82888e563
Ludovic Courtès <ludo@gnu.org>
2015-12-22 22:24:41 +0100

    gnu: python-wrapper: Append "-wrapper" to the underlying package name.
    Reported by Federico Beffa <beffa@ieee.org>.
    
    * gnu/packages/python.scm (wrap-python3): Change the default value of
    'name' to include the name of PYTHON.  This disambiguates between
    'python-minimal-wrapper' and 'python-wrapper'.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -307,7 +307,9 @@ data types.")
     (inputs `(("openssl" ,openssl)
               ("zlib" ,zlib)))))
 
-(define* (wrap-python3 python #:optional (name "python-wrapper"))
+(define* (wrap-python3 python
+                       #:optional
+                       (name (string-append (package-name python) "-wrapper")))
   (package (inherit python)
     (name name)
     (source #f)
