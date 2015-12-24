8d479d27ca74f86a69b48d385f3d9537adf5392b
Leo Famulari <leo@famulari.name>
2015-12-24 01:03:53 -0500

    gnu: colordiff: Update source URL.
    * gnu/packages/patchutils.scm (colordiff)[source]: Update URL.

--- gnu/packages/patchutils.scm
+++ gnu/packages/patchutils.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2014 Eric Bavier <bavier@member.fsf.org>
+;;; Copyright © 2015 Leo Famulari <leo@famulari.name>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -151,12 +152,13 @@ refreshed, and more.")
     (name "colordiff")
     (version "1.0.13")
     (source
-     (origin
-      (method url-fetch)
-      (uri (string-append "http://www.colordiff.org/colordiff-"
-                          version ".tar.gz"))
+      (origin
+        (method url-fetch)
+        (uri (list (string-append "http://www.colordiff.org/archive/colordiff-"
+                                  version ".tar.gz")))
       (sha256
-       (base32 "0akcz1p3klsjnhwcqdfq4grs6paljc5c0jzr3mqla5f862hhaa6f"))))
+       (base32
+        "0akcz1p3klsjnhwcqdfq4grs6paljc5c0jzr3mqla5f862hhaa6f"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f
