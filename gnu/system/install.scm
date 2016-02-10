3a9cfba8796665b61d68936e9a391813a71485cd
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2016-02-10 05:01:47 +0100

    install: Add btrfs-progs to the image.
    * gnu/system/install.scm (installation-os)[packages]: Add BTRFS-PROGS.
    
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/system/install.scm
+++ gnu/system/install.scm
@@ -370,6 +370,7 @@ Use Alt-F2 for documentation.
                      parted ddrescue
                      grub                  ;mostly so xrefs to its manual work
                      cryptsetup
+                     btrfs-progs
                      wireless-tools iw wpa-supplicant-minimal iproute
                      ;; XXX: We used to have GNU fdisk here, but as of version
                      ;; 2.0.0a, that pulls Guile 1.8, which takes unreasonable
