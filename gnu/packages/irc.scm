27930f853d5435e48a8b76717e448285d5b16c6c
Efraim Flashner <efraim@flashner.co.il>
2016-02-28 11:48:03 +0200

    gnu: irssi: Move to irc.scm.
    * gnu/packages/irssi.scm (irssi): Move from here ...
    * gnu/packages/irc.scm (irssi): ... to here.
    * gnu/packages/irssi.scm: Delete file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Remove irssi.scm.

--- gnu/packages/irc.scm
+++ gnu/packages/irc.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2016 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2013 Cyril Roelandt <tipecaml@gmail.com>
+;;; Copyright © 2015, 2016 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -21,11 +22,16 @@
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (guix build-system cmake)
+  #:use-module (guix build-system gnu)
   #:use-module (gnu packages compression)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages ncurses)
   #:use-module (gnu packages kde)
   #:use-module (gnu packages kde-frameworks)
+  #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages qt))
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages tls))
 
 (define-public quassel
   (package
@@ -66,3 +72,43 @@ clients can attach to and detach from the central core.  It resembles the
 popular combination of screen and a text-based IRC client such as WeeChat or
 irssi, but graphical.")
     (license (list license:gpl2 license:gpl3)))) ;; dual licensed
+
+(define-public irssi
+  (package
+    (name "irssi")
+    (version "0.8.17")
+    (source (origin
+             (method url-fetch)
+             (uri (string-append "https://github.com/irssi-import/irssi/"
+                                 "releases/download/0.8.17/irssi-"
+                                 version ".tar.bz2"))
+             (sha256
+              (base32
+               "01v82q2pfiimx6lh271kdvgp8hl4pahc3srg04fqzxgdsb5015iw"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (alist-replace
+        'configure
+        (lambda* (#:key inputs outputs #:allow-other-keys)
+          (let ((out (assoc-ref outputs "out"))
+                (ncurses (assoc-ref inputs "ncurses")))
+            (setenv "CONFIG_SHELL" (which "bash"))
+            (zero?
+             (system* "./configure"
+                      (string-append "--prefix=" out)
+                      (string-append "--with-ncurses=" ncurses)))))
+        %standard-phases)))
+    (inputs
+     `(("glib" ,glib)
+       ("ncurses" ,ncurses)
+       ("openssl" ,openssl)))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)
+       ("perl" ,perl)))
+    (home-page "http://www.irssi.org/")
+    (synopsis "Terminal-based IRC client")
+    (description
+     "Irssi is a terminal based IRC client for UNIX systems.  It also supports
+SILC and ICB protocols via plugins.")
+    (license license:gpl2+)))
