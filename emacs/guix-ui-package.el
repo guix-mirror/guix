b8fa5a2ae8b4c78ec84792e1a1d807ac6a675443
Alex Kost <alezost@gmail.com>
2016-03-03 12:55:21 +0300

    emacs: hydra: Use '-' to separate job names and version numbers.
    * emacs/guix-hydra.el (guix-hydra-job-name-specification): New procedure.
    * emacs/guix-ui-package.el (guix-package-info-insert-systems)
    (guix-package-list-latest-builds): Use it.

--- emacs/guix-ui-package.el
+++ emacs/guix-ui-package.el
@@ -34,6 +34,7 @@
 (require 'guix-guile)
 (require 'guix-entry)
 (require 'guix-utils)
+(require 'guix-hydra)
 (require 'guix-hydra-build)
 (require 'guix-read)
 (require 'guix-license)
@@ -388,7 +389,7 @@ formatted with this string, an action button is inserted.")
                           :system (button-label btn))))
                (apply #'guix-hydra-build-get-display
                       'latest args)))
-   'job-name (guix-package-name-specification
+   'job-name (guix-hydra-job-name-specification
               (guix-entry-value entry 'name)
               (guix-entry-value entry 'version))))
 
@@ -776,7 +777,7 @@ for all ARGS."
   (interactive
    (let ((entry (guix-list-current-entry)))
      (guix-hydra-build-latest-prompt-args
-      :job (guix-package-name-specification
+      :job (guix-hydra-job-name-specification
             (guix-entry-value entry 'name)
             (guix-entry-value entry 'version)))))
   (apply #'guix-hydra-latest-builds number args))
