34a6f4dcf926d94f75c51c9919df051bb6266364
Efraim Flashner <efraim@flashner.co.il>
2016-03-10 21:24:32 +0200

    gnu: cppunit: Update to 1.13.2.
    * gnu/packages/check.scm (cppunit): Update to 1.13.2.
    [source]: Change to libreoffice's hosted release tarballs.
    [home-page]: Change to freedesktop.

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -90,15 +90,14 @@ with a flexible variety of user interfaces.")
 (define-public cppunit
   (package
     (name "cppunit")
-    (version "1.12.1")
+    (version "1.13.2")
     (source (origin
              (method url-fetch)
-              (uri (string-append "mirror://sourceforge/cppunit/" name "/"
-                                  name "-"
-                                  version ".tar.gz"))
+              (uri (string-append "http://dev-www.libreoffice.org/src/"
+                                  name "-" version ".tar.gz"))
              (sha256
               (base32
-               "0jm49v5rmc5qw34vqs56gy8xja1dhci73bmh23cig4kcir6a0a5c"))))
+               "17s2kzmkw3kfjhpp72rfppyd7syr7bdq5s69syj2nvrlwd3d4irz"))))
     ;; Explicitly link with libdl. This is expected to be done by packages
     ;; relying on cppunit for their tests. However, not all of them do.
     ;; If we added the linker flag to such packages, we would pollute all
@@ -106,7 +105,7 @@ with a flexible variety of user interfaces.")
     (arguments
      `(#:make-flags '("LDFLAGS=-ldl")))
     (build-system gnu-build-system)
-    (home-page "http://sourceforge.net/projects/cppunit/")
+    (home-page "https://wiki.freedesktop.org/www/Software/cppunit/")
     (synopsis "Unit testing framework for C++")
     (description "CppUnit is the C++ port of the famous JUnit framework for
 unit testing.  Test output is in XML for automatic testing and GUI based for
