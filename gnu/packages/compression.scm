ea5d388257664d703df23cf3eb0da7b6546d6c42
Efraim Flashner <efraim@flashner.co.il>
2016-04-01 17:42:28 +0300

    gnu: gzip: Update to 1.7.
    * gnu/packages/compression.scm (gzip): Update to 1.7.

--- gnu/packages/compression.scm
+++ gnu/packages/compression.scm
@@ -129,14 +129,14 @@ adding and extracting files to/from a tar archive.")
 (define-public gzip
   (package
    (name "gzip")
-   (version "1.6")
+   (version "1.7")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/gzip/gzip-"
                                 version ".tar.gz"))
             (sha256
              (base32
-              "0zlgdm4v3dndrbiz7b67mbbj25dpwqbmbzjiycssvrfrcfvq7swp"))))
+              "1as1ddq58spflzz5kxm0ni0xfpswrkkrncjpxyb3aw77gizcacgv"))))
    (build-system gnu-build-system)
    (synopsis "General file (de)compression (using lzw)")
    (arguments
