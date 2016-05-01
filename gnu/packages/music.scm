457b5eb6d2441913303058be016fcf2bbaee310e
Ricardo Wurmus <rekado@elephly.net>
2016-05-01 18:09:30 +0200

    gnu: qtractor: Update to 0.7.7.
    * gnu/packages/music.scm (qtractor): Update to 0.7.7.

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -1209,14 +1209,14 @@ computer's keyboard.")
 (define-public qtractor
   (package
     (name "qtractor")
-    (version "0.7.5")
+    (version "0.7.7")
     (source (origin
               (method url-fetch)
               (uri (string-append "http://downloads.sourceforge.net/qtractor/"
                                   "qtractor-" version ".tar.gz"))
               (sha256
                (base32
-                "0drqzp1rbqmqiwdzc9n3307y8rm882fha3awy5qlvir5ma2mwl80"))))
+                "0q8kvy1ynlg64v1w7jxix1rpq0lp2ixgb2y8cbbwxd2b28r3r2vl"))))
     (build-system gnu-build-system)
     (arguments `(#:tests? #f)) ; no "check" target
     (inputs
