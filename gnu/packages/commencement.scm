e760ec4187244c0960f21803abef1849c97a8203
Ludovic Courtès <ludo@gnu.org>
2016-05-03 17:34:54 +0200

    gnu: gcc: Add 6.1.0.
    * gnu/packages/gcc.scm (gcc-6): New variable.
    * gnu/packages/commencement.scm (gcc-toolchain-6): New variable.

--- gnu/packages/commencement.scm
+++ gnu/packages/commencement.scm
@@ -879,4 +879,7 @@ and binaries, plus debugging symbols in the 'debug' output), and Binutils.")
 (define-public gcc-toolchain-5
   (gcc-toolchain gcc-5))
 
+(define-public gcc-toolchain-6
+  (gcc-toolchain gcc-6))
+
 ;;; commencement.scm ends here
