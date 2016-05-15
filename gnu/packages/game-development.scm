e91fd7f591cfc41412095876a3beeff286074852
宋文武 <iyzsong@gmail.com>
2016-05-15 18:24:15 +0800

    gnu: love: Update to 0.10.1.
    * gnu/packages/game-development.scm (love): Update to 0.10.1.

--- gnu/packages/game-development.scm
+++ gnu/packages/game-development.scm
@@ -272,14 +272,14 @@ archive on a per-file basis.")
 (define-public love
   (package
     (name "love")
-    (version "0.10.0")
+    (version "0.10.1")
     (source (origin
              (method url-fetch)
              (uri (string-append "https://bitbucket.org/rude/love/downloads/"
                                  "love-" version "-linux-src.tar.gz"))
              (sha256
               (base32
-               "1r2n1nrw3hcdvy14fjbwz3l9swcy65v3lqwpj2frnkkcwncdz94p"))))
+               "1ys18m7c4994k5s7avqlf17sc2icx5zgvfplz504q1ka16hwkc52"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
