d68ba5f483a42ea324ff31746fde7dcc41600463
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2016-05-17 16:42:41 +0200

    guix import: Print list of expressions.
    * guix/scripts/import.scm (guix-import): Print list of expressions.

--- guix/scripts/import.scm
+++ guix/scripts/import.scm
@@ -107,10 +107,17 @@ Run IMPORTER with ARGS.\n"))
      (show-version-and-exit "guix import"))
     ((importer args ...)
      (if (member importer importers)
-         (match (apply (resolve-importer importer) args)
-           ((and expr ('package _ ...))
-            (pretty-print expr (newline-rewriting-port
-                                (current-output-port))))
-           (x
-            (leave (_ "'~a' import failed~%") importer)))
+         (let ((print (lambda (expr)
+                        (pretty-print expr (newline-rewriting-port
+                                            (current-output-port))))))
+           (match (apply (resolve-importer importer) args)
+             ((and expr ('package _ ...))
+              (print expr))
+             ((? list? expressions)
+              (for-each (lambda (expr)
+                          (print expr)
+                          (newline))
+                        expressions))
+             (x
+              (leave (_ "'~a' import failed~%") importer))))
          (leave (_ "~a: invalid importer~%") importer)))))
