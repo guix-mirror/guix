9fcc3509a7af4348d855141e60501d9728fd6b6f
Ben Woodcroft <donttrustben@gmail.com>
2016-05-28 23:44:16 +1000

    gnu: Update diamond to 0.8.3.
    * gnu/packages/bioinformatics.scm (diamond): Update to 0.8.3.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -1482,7 +1482,7 @@ identify enrichments with functional annotations of the genome.")
 (define-public diamond
   (package
     (name "diamond")
-    (version "0.8.1")
+    (version "0.8.3")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1491,7 +1491,7 @@ identify enrichments with functional annotations of the genome.")
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "1dqancz32c2l7w1b2vkvh5zqa2jnf99j1c41djnx1l8pxn044zdc"))))
+                "0z9sm46bivz0ghwg8kajizgiigv8dnz4w7cyzsg8wcsm9h2z6zyc"))))
     (build-system cmake-build-system)
     (arguments
      '(#:tests? #f ; no "check" target
