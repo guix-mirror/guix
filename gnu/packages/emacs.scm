guix-mirror/guix

commit f99f3f24ef604f44023379da4b375c80e6844be1

gnu: emacs-constants: Update to 2.6.
* gnu/packages/emacs.scm (emacs-constants): Change to versioned repository and
  update to 2.6.

gnu/packages/emacs.scm
@@ -1409,17 +1409,17 @@ identifiers in the MIT-Scheme documentation.")
 (define-public emacs-constants
   (package
     (name "emacs-constants")
-    (version "2.2")
+    (version "2.6")
     (home-page "https://staff.fnwi.uva.nl/c.dominik/Tools/constants")
     (source
      (origin
        (file-name (string-append name "-" version ".el"))
-       (method uncompressed-file-fetch)
-       (uri (string-append home-page "/constants.el")) ;FIXME: unversioned URI
-       (patches (search-patches "emacs-constants-lisp-like.patch"))
+       (method url-fetch)
+       (uri (string-append "https://github.com/fedeinthemix/emacs-constants"
+                           "/archive/v" version ".tar.gz"))
        (sha256
         (base32
-         "14q094aphsjhq8gklv7i5a7byl0ygz63cv3n6b5p8ji2jy0mnnw3"))))
+         "0pnrpmmxq8mh5h2hbrp5vcym0j0fh6dv3s7c5ccn18wllhzg9g7n"))))
     (build-system emacs-build-system)
     (synopsis "Enter definition of constants into an Emacs buffer")
     (description

commit 37dbfc50841a8e91f9fb0dc80ff9b5f23de38689

gnu: emacs-constants: Fix source file-name.
Fixes a regression introduced in f99f3f24ef604f44023379da4b375c80e6844be1.

* gnu/packages/emacs.scm (emacs-constants)[source]: Fix file-name.

gnu/packages/emacs.scm
@@ -1428,7 +1428,7 @@ identifiers in the MIT-Scheme documentation.")
     (home-page "https://staff.fnwi.uva.nl/c.dominik/Tools/constants")
     (source
      (origin
-       (file-name (string-append name "-" version ".el"))
+       (file-name (string-append name "-" version ".tar.gz"))
        (method url-fetch)
        (uri (string-append "https://github.com/fedeinthemix/emacs-constants"
                            "/archive/v" version ".tar.gz"))