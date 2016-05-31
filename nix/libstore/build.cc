guix-mirror/guix

commit e08380fb6cefd3fd67c3c220a3ddaf385e6413cf

daemon: Handle /tmp being a symlink.
* nix/libstore/build.cc (DerivationGoal::startBuilder): Call 'canonPath'
on "/tmp".

nix/libstore/build.cc
@@ -1717,7 +1717,7 @@ void DerivationGoal::startBuilder()
 
     /* In a sandbox, for determinism, always use the same temporary
        directory. */
-    tmpDirInSandbox = useChroot ? "/tmp/guix-build-" + drvName + "-0" : tmpDir;
+    tmpDirInSandbox = useChroot ? canonPath("/tmp/guix-build-", true) + drvName + "-0" : tmpDir;
 
     /* For convenience, set an environment pointing to the top build
        directory. */

commit 4078fa8f72fd109882f8b5e918cb6f7a9c1ed535

daemon: Fix typo.
Fixes a regression/typo introduced in
e08380fb6cefd3fd67c3c220a3ddaf385e6413cf.

* nix/libstore/build.cc (DerivationGoal::startBuilder): Canonicalize
"/tmp", not "/tmp/guix-build".

nix/libstore/build.cc
@@ -1718,7 +1718,7 @@ void DerivationGoal::startBuilder()
 
     /* In a sandbox, for determinism, always use the same temporary
        directory. */
-    tmpDirInSandbox = useChroot ? canonPath("/tmp/guix-build-", true) + drvName + "-0" : tmpDir;
+    tmpDirInSandbox = useChroot ? canonPath("/tmp", true) + "/guix-build-" + drvName + "-0" : tmpDir;
 
     /* For convenience, set an environment pointing to the top build
        directory. */