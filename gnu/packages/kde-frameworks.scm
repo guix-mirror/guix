d2e4c02d36503ced72e038f57150b559c1920902
David Craven <david@craven.ch>
2016-06-12 20:47:13 +0200

    gnu: kde-frameworks: Add kitemviews.
    * gnu/packages/kde-frameworks.scm (kitemviews): New variable.
    
    Co-authored-by: Hartmut Goebel <h.goebel@crazy-compilers.com>

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -616,6 +616,46 @@ model to observers
 @end itemize")
     (license license:lgpl2.1+)))
 
+(define-public kitemviews
+  (package
+    (name "kitemviews")
+    (version "5.24.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append "mirror://kde/stable/frameworks/"
+                            (version-major+minor version) "/"
+                            name "-" version ".tar.xz"))
+        (sha256
+         (base32
+          "0y3fx9hk1x27arrmwfzq783a44cs7p8dpmhxrwzh0di4mwa8jafw"))))
+    (build-system cmake-build-system)
+    (native-inputs
+     `(("extra-cmake-modules" ,extra-cmake-modules)
+       ("qttools" ,qttools)
+       ("xorg-server" ,xorg-server)))
+    (inputs
+     `(("qtbase" ,qtbase)))
+    (arguments
+     `(#:phases
+        (modify-phases %standard-phases
+          (add-before 'check 'check-setup
+            (lambda* _
+              (setenv "DBUS_FATAL_WARNINGS" "0")))
+          (add-before 'check 'start-xorg-server
+            (lambda* (#:key inputs #:allow-other-keys)
+              ;; The test suite requires a running X server.
+              (system (string-append (assoc-ref inputs "xorg-server")
+                                     "/bin/Xvfb :1 &"))
+              (setenv "DISPLAY" ":1")
+             #t)))))
+    (home-page "https://community.kde.org/Frameworks")
+    (synopsis "Set of item views extending the Qt model-view framework")
+    (description "KItemViews includes a set of views, which can be used with
+item models.  It includes views for categorizing lists and to add search filters
+to flat and hierarchical lists.")
+    (license (list license:gpl2+ license:lgpl2.1+))))
+
 (define-public kwindowsystem
   (package
     (name "kwindowsystem")
