8b5ed303dd690a2bbd0758c3478dd4a9bd7cb435
宋文武 <iyzsong@gmail.com>
2016-07-09 13:49:02 +0800

    gnu: conky: Update to 1.10.3.
    * gnu/packages/conky.scm (conky): Update to 1.10.3.
    [arguments]: Add #:configure-flags.
    [inputs]: Add libxinerama.

--- gnu/packages/conky.scm
+++ gnu/packages/conky.scm
@@ -32,7 +32,7 @@
 (define-public conky
   (package
     (name "conky")
-    (version "1.10.0")
+    (version "1.10.3")
     (source
      (origin
        (method url-fetch)
@@ -40,10 +40,15 @@
                            version ".tar.gz"))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1szq4ckfkvyabv5llf9nkdxipn7429sralsxyr7z0dyc3zwz74pk"))))
+        (base32 "1m9byrmpc2sprzk44v447yaqjzsvw230a0mlw7y1ngz3m3y44qs5"))))
     (build-system cmake-build-system)
     (arguments
      `(#:tests? #f ; there are no tests
+       #:configure-flags
+       '("-DRELEASE=true"
+         ;; XXX: it checks ncurses with pkg-config.
+         ;; TODO: add 'ncurses.pc' to the ncurses package.
+         "-DBUILD_NCURSES=false")
        #:phases
        (alist-cons-after
         'unpack 'add-freetype-to-search-path
@@ -67,6 +72,7 @@
        ("libx11" ,libx11)
        ("libxdamage" ,libxdamage)
        ("libxft" ,libxft)
+       ("libxinerama" ,libxinerama)
        ("lua" ,lua)))
     (native-inputs
      `(("pkg-config" ,pkg-config)))
