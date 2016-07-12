guix-mirror/guix

commit afb6fdaa5de2607261015372f31b1c19fd503a61

gnu: flycheck: Update to 28.
* gnu/packages/emacs.scm (flycheck): Update to 28.

gnu/packages/emacs.scm
@@ -672,7 +672,7 @@ the body are let-bound and this search is done at compile time.")
 (define-public flycheck
   (package
     (name "emacs-flycheck")
-    (version "0.23")
+    (version "28")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -680,7 +680,7 @@ the body are let-bound and this search is done at compile time.")
                     version "/flycheck-" version ".tar"))
               (sha256
                (base32
-                "1n2cifzsl5dbv42l82bi3y1vk6q33msi8dd4bj7b9nvnl9jfjj5b"))))
+                "1yjxivk11d7w39zfhj2xr4h6xhwx1aj6yhyzd63rjrad7xpjfl86"))))
     (build-system emacs-build-system)
     (propagated-inputs
      `(("emacs-dash" ,emacs-dash)

commit bdff55eac313016b9de6605f52528909f73b0db3

gnu: emacs-flycheck: Add missing dependency on emacs-seq.
Fixes a regression introduced in
afb6fdaa5de2607261015372f31b1c19fd503a61.

* gnu/packages/emacs.scm (flycheck)[propagated-inputs]: Add EMACS-SEQ.

gnu/packages/emacs.scm
@@ -710,7 +710,8 @@ the body are let-bound and this search is done at compile time.")
     (build-system emacs-build-system)
     (propagated-inputs
      `(("emacs-dash" ,emacs-dash)
-       ("emacs-let-alist" ,let-alist)))
+       ("emacs-let-alist" ,let-alist)
+       ("emacs-seq" ,emacs-seq)))
     (home-page "https://www.flycheck.org")
     (synopsis "On-the-fly syntax checking")
     (description