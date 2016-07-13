66f8f5f9c35861aa48dd7f8564f2becddf631af3
Ludovic Courtès <ludo@gnu.org>
2016-07-13 12:40:42 +0200

    gnu: screen: Remove unneeded configure flags.
    * gnu/packages/screen.scm (screen)[arguments]: Remove --infodir and
    --mandir, which are no longer needed.

--- gnu/packages/screen.scm
+++ gnu/packages/screen.scm
@@ -49,16 +49,9 @@
        ("perl" ,perl)))
     (arguments
      `(#:configure-flags
-       (let ((out (assoc-ref %outputs "out")))
-         (list
-          ;; By default, man and info pages are put in PREFIX/{man,info},
-          ;; but we want them in PREFIX/share/{man,info}.
-          (string-append "--mandir=" out "/share/man")
-          (string-append "--infodir=" out "/share/info")
-
-          ;; By default, screen supports 16 colors, but we want 256 when
-          ;; ~/.screenrc contains 'term xterm-256color'
-          "--enable-colors256"))))
+       ;; By default, screen supports 16 colors, but we want 256 when
+       ;; ~/.screenrc contains 'term xterm-256color'.
+       '("--enable-colors256")))
     (home-page "http://www.gnu.org/software/screen/")
     (synopsis "Full-screen window manager providing multiple terminals")
     (description
