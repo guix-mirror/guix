bc85b127df622575988f8e760f72d608d0900a75
Ludovic Courtès <ludo@gnu.org>
2016-07-24 22:15:54 +0200

    gnu: gnupg@2.0: Add the 'gpg' and 'gpgv' commands.
    * gnu/packages/gnupg.scm (gnupg-2.0)[arguments]: Add
    'rename-v2-commands' phase.

--- gnu/packages/gnupg.scm
+++ gnu/packages/gnupg.scm
@@ -284,7 +284,20 @@ libskba (working with X.509 certificates and CMS data).")
           (lambda _
             (substitute* "tests/openpgp/Makefile.in"
               (("/bin/sh") (which "bash")))
-            #t)))))))
+            #t))
+        (add-after 'install 'rename-v2-commands
+          (lambda* (#:key outputs #:allow-other-keys)
+            ;; Upstream suggests removing the trailing '2' from command names:
+            ;; <http://debbugs.gnu.org/cgi/bugreport.cgi?bug=22883#58>.
+            (let ((out (assoc-ref outputs "out")))
+              (with-directory-excursion (string-append out "/bin")
+                (rename-file "gpgv2" "gpgv")
+                (rename-file "gpg2" "gpg")
+
+                ;; Keep the old name around to ease transition.
+                (symlink "gpgv" "gpgv2")
+                (symlink "gpg" "gpg2")
+                #t)))))))))
 
 (define-public gnupg-1
   (package (inherit gnupg)
