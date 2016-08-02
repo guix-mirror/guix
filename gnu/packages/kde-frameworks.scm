guix-mirror/guix

commit d26e2b9f306a1170d46f7c860c81840d9d600161

gnu: kde-frameworks: Remove kde-frameworks-version.
* gnu/packages/kde-frameworks (kde-frameworks-version): Delete variable.
  (kwindowsystem)[version]: Replace kde-frameworks-version with version string.
  (oxygen-icons)[version]: Replace kde-frameworks-version with version string.

gnu/packages/kde-frameworks.scm
@@ -27,8 +27,6 @@
   #:use-module (gnu packages qt)
   #:use-module (gnu packages xorg))
 
-(define kde-frameworks-version "5.21.0")
-
 (define-public extra-cmake-modules
   (package
     (name "extra-cmake-modules")
@@ -57,7 +55,7 @@ common build settings used in software produced by the KDE community.")
 (define-public kwindowsystem
   (package
     (name "kwindowsystem")
-    (version kde-frameworks-version)
+    (version "5.24.0")
     (source
       (origin
         (method url-fetch)
@@ -95,7 +93,7 @@ lower level classes for interaction with the X Windowing System.")
 (define-public oxygen-icons
   (package
     (name "oxygen-icons")
-    (version kde-frameworks-version)
+    (version "5.24.0")
     (source
       (origin
         (method url-fetch)

commit 7b5fc3a320eea930e6bc85a4d65c2ddbe0c9cffa

gnu: kde-frameworks: Fix version numbers.
Fixes a regression introduced in
d26e2b9f306a1170d46f7c860c81840d9d600161.

* gnu/packages/kde-frameworks.scm (kwindowsystem, oxygen-icons): Set to
"5.21.0".

gnu/packages/kde-frameworks.scm
@@ -55,7 +55,7 @@ common build settings used in software produced by the KDE community.")
 (define-public kwindowsystem
   (package
     (name "kwindowsystem")
-    (version "5.24.0")
+    (version "5.21.0")
     (source
       (origin
         (method url-fetch)
@@ -93,7 +93,7 @@ lower level classes for interaction with the X Windowing System.")
 (define-public oxygen-icons
   (package
     (name "oxygen-icons")
-    (version "5.24.0")
+    (version "5.21.0")
     (source
       (origin
         (method url-fetch)