4b2b7135b1cf4ca659182f08506837fcb21f50b4
Alex Kost <alezost@gmail.com>
2016-08-18 01:22:29 +0300

    gnu: guile-xosd: Update to 0.2.1.
    * gnu/packages/guile.scm (guile-xosd): Update to 0.2.1.

--- gnu/packages/guile.scm
+++ gnu/packages/guile.scm
@@ -1129,7 +1129,7 @@ SQL databases.  This package implements the interface for SQLite.")
 (define-public guile-xosd
   (package
     (name "guile-xosd")
-    (version "0.2")
+    (version "0.2.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/alezost/" name
@@ -1137,7 +1137,7 @@ SQL databases.  This package implements the interface for SQLite.")
                                   "/" name "-" version ".tar.gz"))
               (sha256
                (base32
-                "1j0b07kycccfslp5n6q0hz7adwc7k41fpzds2dvrly67gavjqljv"))))
+                "1ri5065c16kmgrf2pysn2ymxjqi5302lhpb07wkl1jr75ym8fn8p"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
