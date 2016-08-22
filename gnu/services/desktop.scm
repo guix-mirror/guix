17073dafc59d62fb8cbb8b94d61d3ecc488ac59f
Mark H Weaver <mhw@netris.org>
2016-08-22 08:16:37 -0400

    Revert "services: elogind: Provide '%elogind-file-systems' by extension."
    This reverts commit 3cf319a3f8e23831960a0f1320122cc514188a37.

--- gnu/services/desktop.scm
+++ gnu/services/desktop.scm
@@ -27,8 +27,6 @@
   #:use-module (gnu services avahi)
   #:use-module (gnu services xorg)
   #:use-module (gnu services networking)
-  #:use-module ((gnu system file-systems)
-                #:select (%elogind-file-systems))
   #:use-module (gnu system shadow)
   #:use-module (gnu system pam)
   #:use-module (gnu packages glib)
@@ -762,11 +760,7 @@ seats.)"
 
                        ;; Extend PAM with pam_elogind.so.
                        (service-extension pam-root-service-type
-                                          pam-extension-procedure)
-
-                       ;; We need /run/user, /run/systemd, etc.
-                       (service-extension file-system-service-type
-                                          (const %elogind-file-systems))))))
+                                          pam-extension-procedure)))))
 
 (define* (elogind-service #:key (config (elogind-configuration)))
   "Return a service that runs the @command{elogind} login and seat management
