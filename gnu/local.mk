d6a07ee6751cdd414d702194dbeb0fd71f0781c9
John Darrington <jmd@gnu.org>
2016-09-03 08:26:05 +0200

    gnu: Add rpc-daemon service
    * gnu/services/nfs.scm: New file.
    * gnu/local.mk (GNU_SYSTEM_MODULES): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -389,6 +389,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/services/mail.scm				\
   %D%/services/mcron.scm			\
   %D%/services/networking.scm			\
+  %D%/services/nfs.scm			\
   %D%/services/shepherd.scm			\
   %D%/services/herd.scm				\
   %D%/services/sddm.scm				\
