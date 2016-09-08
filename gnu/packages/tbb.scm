b6337f30cccd55b6f687f0bd80ef5b32a193f1b1
ng0 <ng0@we.make.ritual.n0.is>
2016-09-08 23:04:52 +0000

    gnu: tbb: Update to 2017.
    * gnu/packages/tbb.scm (tbb): Update to 2017.
    [license]: Change to ASL2.0.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/tbb.scm
+++ gnu/packages/tbb.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2015 Ricardo Wurmus <rekado@elephly.net>
+;;; Copyright © 2016 ng0 <ng0@we.make.ritual.n0.is>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -27,16 +28,16 @@
 (define-public tbb
   (package
     (name "tbb")
-    (version "4.3.2")
+    (version "2017")
     (source (origin
               (method url-fetch)
               (uri (string-append
                     "https://www.threadingbuildingblocks.org/sites/default"
                     "/files/software_releases/source/"
-                    "tbb43_20141204oss_src.tgz"))
+                    "tbb" version "_20160722oss_src.tgz"))
               (sha256
                (base32
-                "0jsczl99jfgj47kj7c4sd4fk7v3rbaiax1ng9ypykz1hh0lrrsws"))
+                "038rmv3s8si51bjrzwyv8ldqw742fjjdfayi8pmjaq5zw32b8pzx"))
               (modules '((guix build utils)))
               (snippet
                '(substitute* "build/common.inc"
@@ -85,6 +86,4 @@ the low-level threading details necessary for optimal multi-core performance.
 It uses common C++ templates and coding style to eliminate tedious threading
 implementation work.  It provides parallel loop constructs, asynchronous
 tasks, synchronization primitives, atomic operations, and more.")
-    ;; GPLv2 with run-time exception:
-    ;; <https://www.threadingbuildingblocks.org/licensing#runtime-exception>
-    (license gpl2)))
+    (license asl2.0)))
