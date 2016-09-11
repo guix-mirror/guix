2932ab9ca44cc6a5fac6355e228cc691ef29a1a2
David Craven <david@craven.ch>
2016-09-11 18:54:13 +0200

    install: Use login-service.
    * gnu/system/install.scm (installation-services): Use login-service.

--- gnu/system/install.scm
+++ gnu/system/install.scm
@@ -268,21 +268,21 @@ You have been warned.  Thanks for being so brave.
 ")))
     (define (normal-tty tty)
       (mingetty-service (mingetty-configuration (tty tty)
-                                                (motd motd)
                                                 (auto-login "root")
                                                 (login-pause? #t))))
 
     (list (mingetty-service (mingetty-configuration
                              (tty "tty1")
-                             (motd motd)
                              (auto-login "root")))
+
+          (login-service (login-configuration
+                          (motd motd)))
 
           ;; Documentation.  The manual is in UTF-8, but
           ;; 'console-font-service' sets up Unicode support and loads a font
           ;; with all the useful glyphs like em dash and quotation marks.
           (mingetty-service (mingetty-configuration
                              (tty "tty2")
-                             (motd motd)
                              (auto-login "guest")
                              (login-program (log-to-info))))
 
