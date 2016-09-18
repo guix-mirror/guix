c92d944c1e731ca5576c2acd90f04c2a708cf304
ng0 <ng0@we.make.ritual.n0.is>
2016-09-18 16:10:16 +0000

    gnu: Add ghc-chell-quickcheck.
    * gnu/packages/haskell.scm (ghc-chell-quickcheck): New variable.
    
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/haskell.scm
+++ gnu/packages/haskell.scm
@@ -7304,4 +7304,28 @@ testing strategies.")
     (description "More complex tests for @code{chell}.")
     (license license:expat)))
 
+(define-public ghc-chell-quickcheck
+  (package
+    (name "ghc-chell-quickcheck")
+    (version "0.2.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/chell-quickcheck/chell-quickcheck-"
+             version ".tar.gz"))
+       (sha256
+        (base32
+         "02bkcnx5k6r5csdnnkvk4wfd0l36nxb87i1463ynw17n7ym9s4cs"))))
+    (build-system haskell-build-system)
+    (inputs
+     `(("ghc-chell" ,ghc-chell)
+       ("ghc-chell-quickcheck-bootstrap" ,ghc-chell-quickcheck-bootstrap)
+       ("ghc-random" ,ghc-random)
+       ("ghc-quickcheck" ,ghc-quickcheck)))
+    (home-page "https://john-millikin.com/software/chell/")
+    (synopsis "QuickCheck support for the Chell testing library")
+    (description "More complex tests for @code{chell}.")
+    (license license:expat)))
+
 ;;; haskell.scm ends here
