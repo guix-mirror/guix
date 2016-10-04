guix-mirror/guix

commit 7f8ad82bf23b032ad6bd85bb1daa87cc83de509c

mapped-devices: Make RAID device opening message clearer.
* gnu/system/mapped-devices.scm (open-raid-device): Rename 'source' to
'sources'.  Make 'waiting' message more informative.
(close-raid-device): Rename 'source' to 'sources'.

gnu/system/mapped-devices.scm
@@ -128,20 +128,24 @@
    (open open-luks-device)
    (close close-luks-device)))
 
-(define (open-raid-device source target)
-  "Return a gexp that assembles SOURCE (a list of devices) to the RAID device
-TARGET, using 'mdadm'."
-  #~(let ((every (@ (srfi srfi-1) every)))
-      (let loop ()
-        (unless (every file-exists? '#$source)
-          (format #t "waiting a bit...~%")
-          (sleep 1)
-          (loop)))
-       (zero? (system* (string-append #$mdadm "/sbin/mdadm")
-                                      "--assemble" #$target
-                                      #$@source))))
+(define (open-raid-device sources target)
+  "Return a gexp that assembles SOURCES (a list of devices) to the RAID device
+TARGET (e.g., \"/dev/md0\"), using 'mdadm'."
+  #~(begin
+      (use-modules (srfi srfi-1) (ice-9 format))
 
-(define (close-raid-device source target)
+      (let ((sources '#$sources))
+        (let loop ()
+          (unless (every file-exists? sources)
+            (format #t "waiting for RAID source devices~{ ~a~}...~%"
+                    sources)
+            (sleep 1)
+            (loop)))
+
+        (zero? (system* (string-append #$mdadm "/sbin/mdadm")
+                        "--assemble" #$target sources)))))
+
+(define (close-raid-device sources target)
   "Return a gexp that stops the RAID device TARGET."
   #~(zero? (system* (string-append #$mdadm "/sbin/mdadm")
                     "--stop" #$target)))

commit 6e52376d2e6c9ef849bd46b2ee72b58ad30d1947

mapped-devices: Properly open RAID devices.
This fixes a type error introduced in
7f8ad82bf23b032ad6bd85bb1daa87cc83de509c, given that SOURCES is a list.

* gnu/system/mapped-devices.scm (open-raid-device): Add 'apply'
invocation.

gnu/system/mapped-devices.scm
@@ -150,8 +150,8 @@ TARGET (e.g., \"/dev/md0\"), using 'mdadm'."
           (sleep 1)
           (loop (+ 1 attempts))))
 
-      (zero? (system* (string-append #$mdadm "/sbin/mdadm")
-                      "--assemble" #$target sources))))
+      (zero? (apply system* (string-append #$mdadm "/sbin/mdadm")
+                    "--assemble" #$target sources))))
 
 (define (close-raid-device sources target)
   "Return a gexp that stops the RAID device TARGET."