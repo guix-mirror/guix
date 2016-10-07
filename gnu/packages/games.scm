b314360f30ffbc8774f4c3e01e4eb299c15d911e
Ricardo Wurmus <rekado@elephly.net>
2016-10-07 09:20:25 +0200

    gnu: abbaye: Update source URL.
    * gnu/packages/games.scm (abbaye)[source]: Update tarball URL.

--- gnu/packages/games.scm
+++ gnu/packages/games.scm
@@ -179,8 +179,10 @@ scriptable with Guile.")
     (source
      (origin
        (method url-fetch)
-       (uri (string-append "http://abbaye-for-linux.googlecode.com/files/"
-                           "abbaye-for-linux-src-" version ".tar.gz"))
+       (uri (string-append "https://storage.googleapis.com/"
+                           "google-code-archive-downloads/v2/code.google.com/"
+                           "abbaye-for-linux/abbaye-for-linux-src-"
+                           version ".tar.gz"))
        (sha256
         (base32
          "1wgvckgqa2084rbskxif58wbb83xbas8s1i8s7d57xbj08ryq8rk"))))
