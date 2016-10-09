6f83d224bbb95367f3a1f816d48da7fc4d60a058
Ricardo Wurmus <rekado@elephly.net>
2016-10-09 22:38:37 +0200

    gnu: tcsh: Do not define BSDWAIT.
    * gnu/packages/patches/tcsh-do-not-define-BSDWAIT.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/shells.scm (tcsh)[source]: Use it.

--- gnu/packages/shells.scm
+++ gnu/packages/shells.scm
@@ -185,7 +185,8 @@ has a small feature set similar to a traditional Bourne shell.")
               (sha256
                (base32
                 "1a4z9kwgx1iqqzvv64si34m60gj34p7lp6rrcrb59s7ka5wa476q"))
-              (patches (search-patches "tcsh-fix-autotest.patch"))
+              (patches (search-patches "tcsh-fix-autotest.patch"
+                                       "tcsh-do-not-define-BSDWAIT.patch"))
               (patch-flags '("-p0"))))
     (build-system gnu-build-system)
     (inputs
