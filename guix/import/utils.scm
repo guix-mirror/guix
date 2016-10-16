guix-mirror/guix

commit fbe9c1012820ab72f022a6ec958c35b431ae7a74

import: Reorder imports in (guix import utils).
* guix/import/utils.scm (define-module): Reorder imports alphabetically.

guix/import/utils.scm
@@ -17,14 +17,15 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (guix import utils)
-  #:use-module (ice-9 match)
-  #:use-module (ice-9 regex)
-  #:use-module (srfi srfi-1)
-  #:use-module (guix hash)
   #:use-module (guix base32)
+  #:use-module ((guix build download) #:prefix build:)
+  #:use-module (guix hash)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix utils)
-  #:use-module ((guix build download) #:prefix build:)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 regex)
+  #:use-module (json)
+  #:use-module (srfi srfi-1)
   #:export (factorize-uri
 
             hash-table->alist

commit b37e1e64281564c671bf3d0381bd9a6c03c6f62b

import: utils: Remove dependency on (json) module.
This fixes a regression introduced by commit
fbe9c1012820ab72f022a6ec958c35b431ae7a74.

* guix/import/utils.scm: Remove unused (json) module.

guix/import/utils.scm
@@ -26,7 +26,6 @@
   #:use-module (guix utils)
   #:use-module (ice-9 match)
   #:use-module (ice-9 regex)
-  #:use-module (json)
   #:use-module (srfi srfi-1)
   #:export (factorize-uri
 