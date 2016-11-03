5b3b2d2f025126cea405e642ca77e4d087e141fe
Ricardo Wurmus <rekado@elephly.net>
2016-11-03 17:48:46 +0100

    gnu: r-limma: Update to 3.30.2.
    * gnu/packages/bioinformatics.scm (r-limma): Update to 3.30.2.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -6235,13 +6235,13 @@ coding changes and predict coding outcomes.")
 (define-public r-limma
   (package
     (name "r-limma")
-    (version "3.30.0")
+    (version "3.30.2")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "limma" version))
               (sha256
                (base32
-                "0d8wp7b7nymawf4czwsg27k4c61i4ij2lhv7phi6cb3hdd8c76yf"))))
+                "04jris7wk2lxksrrvrjsysznsdb2k04lfgrnp18ic49sazva0hfy"))))
     (build-system r-build-system)
     (home-page "http://bioinf.wehi.edu.au/limma")
     (synopsis "Package for linear models for microarray and RNA-seq data")
