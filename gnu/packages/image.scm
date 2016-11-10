5c66314bd98e4eb27f88a750cdd2812c62671fc6
Leo Famulari <leo@famulari.name>
2016-11-10 18:23:57 -0500

    gnu: libtiff: Fix CVE-2016-9273.
    * gnu/packages/patches/libtiff-CVE-2016-9273.patch: New patch.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/image.scm (libtiff/fixed): Use it.

--- gnu/packages/image.scm
+++ gnu/packages/image.scm
@@ -292,7 +292,8 @@ collection of tools for doing simple manipulations of TIFF images.")
                          "libtiff-CVE-2016-5314.patch"
                          "libtiff-CVE-2016-5321.patch"
                          "libtiff-CVE-2016-5323.patch"
-                         "libtiff-CVE-2016-5652.patch"))))))
+                         "libtiff-CVE-2016-5652.patch"
+                         "libtiff-CVE-2016-9273.patch"))))))
 
 (define-public libwmf
   (package
