f2bd238bcd3e55ea107887bdc034b42005aabcbd
Ricardo Wurmus <rekado@elephly.net>
2016-11-18 23:45:26 +0100

    gnu: synthv1: Update to 0.8.0.
    * gnu/packages/music.scm (synthv1): Update to 0.8.0.

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -881,7 +881,7 @@ users to select LV2 plugins and run them with jalv.")
 (define-public synthv1
   (package
     (name "synthv1")
-    (version "0.7.5")
+    (version "0.8.0")
     (source (origin
               (method url-fetch)
               (uri
@@ -889,7 +889,7 @@ users to select LV2 plugins and run them with jalv.")
                               "/synthv1-" version ".tar.gz"))
               (sha256
                (base32
-                "0h5zja78phf9705i9g54zh61iczb24iv7rxhljyms30sjgajig1y"))))
+                "155pfyhr6d35ciw95pbxlqy7751cmij8j5d849rvblqbjzyzb5qx"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f ; There are no tests.
