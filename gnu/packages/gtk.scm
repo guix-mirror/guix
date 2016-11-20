9fff9e6484a1ebe5246e2811c5d74fbdab43313c
宋文武 <iyzsong@gmail.com>
2016-11-20 11:59:38 +0800

    gnu: gtk-engines: Don't propagate GTK+.
    * gnu/packages/gtk.scm (gtk-engines): Move gtk+ to 'inputs'.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -1289,7 +1289,8 @@ can also be used to document application code.")
     (native-inputs
      `(("pkg-config" ,pkg-config)
        ("intltool" ,intltool)))
-    (propagated-inputs
+    (inputs
+     ;; Don't propagate GTK+ to reduce "profile pollution".
      `(("gtk+" ,gtk+-2))) ; required by gtk-engines-2.pc
     (home-page "http://live.gnome.org/GnomeArt")
     (synopsis "Theming engines for GTK+ 2.x")
