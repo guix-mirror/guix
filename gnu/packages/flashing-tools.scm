7526edb9d55f510fbedb522f108349909a925cec
Efraim Flashner <efraim@flashner.co.il>
2016-11-21 22:13:37 +0200

    gnu: Add rkflashtool.
    * gnu/packages/flashing-tools.scm (rkflashtool): New variable.

--- gnu/packages/flashing-tools.scm
+++ gnu/packages/flashing-tools.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2014 Manolis Fragkiskos Ragkousis <manolis837@gmail.com>
 ;;; Copyright © 2016 Hartmut Goebel <h.goebel@crazy-compilers.com>
 ;;; Copyright © 2016 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2016 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -22,6 +23,7 @@
 (define-module (gnu packages flashing-tools)
   #:use-module (guix licenses)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (gnu packages)
   #:use-module (guix build-system gnu)
@@ -211,3 +213,37 @@ You need to add the udev rules to make the Teensy update available for
 non-root users.")
       (home-page "https://www.pjrc.com/teensy/loader_cli.html")
       (license gpl3))))
+
+(define-public rkflashtool
+  (let ((commit "094bd6410cb016e487e2ccb1050c59eeac2e6dd1")
+        (revision "1"))
+    (package
+      (name "rkflashtool")
+      (version (string-append "0.0.0-" revision "." (string-take commit 7)))
+      (source
+        (origin
+          (method git-fetch)
+          (uri (git-reference
+                (url "https://github.com/linux-rockchip/rkflashtool.git")
+                (commit commit)))
+          (file-name (string-append name "-" version "-checkout"))
+          (sha256
+           (base32
+            "1zkd8zxir3rfg3sy9r20bcnxclnplryn583gqpcr3iad0k3xbah7"))))
+      (build-system gnu-build-system)
+      (arguments
+       '(#:phases
+         (modify-phases %standard-phases
+           (delete 'configure)) ; no configure
+         #:make-flags (list (string-append "PREFIX=" %output))
+         #:tests? #f)) ; no tests
+      (native-inputs
+       `(("pkg-config" ,pkg-config)))
+      (inputs
+       `(("libusb" ,libusb)))
+      (home-page "https://github.com/linux-rockchip/rkflashtool")
+      (synopsis "Tools for flashing Rockchip devices")
+      (description "Allows flashing of Rockchip based embedded linux devices.
+The list of currently supported devices is: RK2818, RK2918, RK2928, RK3026,
+RK3036, RK3066, RK312X, RK3168, RK3188, RK3288, RK3368.")
+      (license bsd-2))))
