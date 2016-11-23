d4ba9a5f210e62742fbd2e9a0cbcde3c1b504fd6
Ricardo Wurmus <rekado@elephly.net>
2016-11-23 16:00:58 +0100

    gnu: Add perl-mojolicious.
    * gnu/packages/perl-web.scm: New file.
    * gnu/local.mk (GNU_SYSTEM_MODULES): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -294,6 +294,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/packages/pdf.scm				\
   %D%/packages/pem.scm				\
   %D%/packages/perl.scm				\
+  %D%/packages/perl-web.scm			\
   %D%/packages/photo.scm			\
   %D%/packages/php.scm				\
   %D%/packages/pkg-config.scm			\
