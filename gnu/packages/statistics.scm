6335bf1c805940f2c3ea9b16f477084c2cf0eb52
Ricardo Wurmus <rekado@elephly.net>
2016-11-24 15:16:53 +0100

    gnu: Add r-sfsmisc.
    * gnu/packages/statistics.scm (r-sfsmisc): New variable.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -3564,6 +3564,25 @@ memory-saving routines for clustering of vector data, which go beyond what the
 existing packages provide.")
     (license license:bsd-2)))
 
+(define-public r-sfsmisc
+  (package
+    (name "r-sfsmisc")
+    (version "1.1-0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "sfsmisc" version))
+       (sha256
+        (base32
+         "0580piv4n1nispl3pa8nfjjfnb8iwaqky2dzdy0aqnxrxgrhqhvz"))))
+    (build-system r-build-system)
+    (home-page "http://cran.r-project.org/web/packages/sfsmisc")
+    (synopsis "Utilities from \"Seminar fuer Statistik\" ETH Zurich")
+    (description
+     "This package provides useful utilities from Seminar fuer Statistik ETH
+Zurich, including many that are related to graphics.")
+    (license license:gpl2+)))
+
 (define-public r-kernsmooth
   (package
     (name "r-kernsmooth")
