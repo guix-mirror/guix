35c0ad444861362e3b48acea2609bf8951cce5c5
Tobias Geerinckx-Rice <me@tobias.gr>
2016-11-27 01:14:33 +0100

    gnu: libgphoto2: Update to 2.5.10.
    * gnu/packages/photo.scm (libgphoto2): Update to 2.5.10.

--- gnu/packages/photo.scm
+++ gnu/packages/photo.scm
@@ -89,14 +89,14 @@ data as produced by digital cameras.")
 (define-public libgphoto2
   (package
     (name "libgphoto2")
-    (version "2.5.2")
+    (version "2.5.10")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://sourceforge/gphoto/libgphoto/"
                                   version "/libgphoto2-" version ".tar.bz2"))
               (sha256
                (base32
-                "0f1818l1vs5fbmrihzyv3qasddbqi3r01jik5crrxddwalsi2bd3"))))
+                "1wjf79ipqwb5phfjjwf15rwgigakylnfqaj4crs5qnds6ba6i1ld"))))
     (build-system gnu-build-system)
     (native-inputs `(("pkg-config" ,pkg-config)))
     (inputs
