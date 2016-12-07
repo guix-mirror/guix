guix-mirror/guix

commit dea91108cf6b9bb46071c8f65c9abf834c5b064d

hydra: Add "i686-w64-mingw32" as a cross-compilation target.
* build-aux/hydra/gnu-system.scm (%cross-targets): Add "i686-w64-mingw32".
(hydra-jobs)[cross-jobs?](pointless?): New procedure.
Use it.

build-aux/hydra/gnu-system.scm
@@ -123,7 +123,8 @@ SYSTEM."
 (define %cross-targets
   '("mips64el-linux-gnu"
     "mips64el-linux-gnuabi64"
-    "arm-linux-gnueabihf"))
+    "arm-linux-gnueabihf"
+    "i686-w64-mingw32"))
 
 (define (demo-os)
   "Return the \"demo\" 'operating-system' structure."
@@ -278,6 +279,11 @@ valid."
       ;; 'mips64el-linux'.
       (string-contains target system))
 
+    (define (pointless? target)
+      ;; Return #t if it makes no sense to cross-build to TARGET from SYSTEM.
+      (and (string-contains target "mingw")
+           (not (string=? "x86_64-linux" system))))
+
     (define (either proc1 proc2)
       (lambda (x)
         (or (proc1 x) (proc2 x))))
@@ -287,7 +293,8 @@ valid."
                          (package-cross-job store (job-name package)
                                             package target system))
                        %packages-to-cross-build))
-                (remove (either from-32-to-64? same?) %cross-targets)))
+                (remove (either from-32-to-64? same? pointless?)
+                        %cross-targets)))
 
   ;; Turn off grafts.  Grafting is meant to happen on the user's machines.
   (parameterize ((%graft? #f))

commit a69bc7071ec05ed896dcc14f613c88023b6b2f80

hydra: Fix wrong-num-args error when computing the cross jobs.
Fixes a regression introduced in
dea91108cf6b9bb46071c8f65c9abf834c5b064d.

* build-aux/hydra/gnu-system.scm (hydra-jobs)[cross-jobs](either): Add
third parameter and honor it.

build-aux/hydra/gnu-system.scm
@@ -283,9 +283,9 @@ valid."
       (and (string-contains target "mingw")
            (not (string=? "x86_64-linux" system))))
 
-    (define (either proc1 proc2)
+    (define (either proc1 proc2 proc3)
       (lambda (x)
-        (or (proc1 x) (proc2 x))))
+        (or (proc1 x) (proc2 x) (proc3 x))))
 
     (append-map (lambda (target)
                   (map (lambda (package)