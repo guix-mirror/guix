a77f03608498de5e93560da8aac0cb8bf7cec7a7
Christopher Baines <mail@cbaines.net>
2016-12-10 09:55:37 +0000

    gnu: password-store: Don't run tests in parallel.
    * gnu/packages/password-utils.scm (password-store): Don't run tests in
      parallel, as it causes them to hang and the build to timeout.
    
    Signed-off-by: Marius Bakke <mbakke@fastmail.com>

--- gnu/packages/password-utils.scm
+++ gnu/packages/password-utils.scm
@@ -291,6 +291,9 @@ any X11 window.")
                (wrap-program (string-append out "/bin/pass")
                  `("PATH" ":" prefix (,(string-join path ":"))))))))
        #:make-flags (list "CC=gcc" (string-append "PREFIX=" %output))
+       ;; Parallel tests may cause a race condition leading to a
+       ;; timeout in some circumstances.
+       #:parallel-tests? #f
        #:test-target "test"))
     (inputs
      `(("getopt" ,util-linux)
