45e38a07eb45a0ecae8eace9a892c66e6f3c2b0f
Tobias Geerinckx-Rice <me@tobias.gr>
2016-12-15 23:47:09 +0100

    gnu: kmod: Update to 23.
    * gnu/packages/linux.scm (kmod): Update to 23.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -1667,7 +1667,7 @@ to use Linux' inotify mechanism, which allows file accesses to be monitored.")
 (define-public kmod
   (package
     (name "kmod")
-    (version "22")
+    (version "23")
     (source (origin
               (method url-fetch)
               (uri
@@ -1675,7 +1675,7 @@ to use Linux' inotify mechanism, which allows file accesses to be monitored.")
                               "kmod-" version ".tar.xz"))
               (sha256
                (base32
-                "10lzfkmnpq6a43a3gkx7x633njh216w0bjwz31rv8a1jlgg1sfxs"))
+                "0mc12sx06p8il1ym3hdmgxxb37apn9yv7xij26gddjdfkx8xa0yk"))
               (patches (search-patches "kmod-module-directory.patch"))))
     (build-system gnu-build-system)
     (native-inputs
