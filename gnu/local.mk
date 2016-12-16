f8faf537cae393b9d9bdb7fd476ad5f79dedaf91
Leo Famulari <leo@famulari.name>
2016-12-16 15:23:47 -0500

    gnu: libupnp: Fix CVE-2016-8863.
    * gnu/packages/patches/libupnp-CVE-2016-8863.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Use it.
    * gnu/packages/libupnp.scm (libupnp)[source]: Use it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -679,6 +679,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/libtool-skip-tests2.patch		\
   %D%/packages/patches/libunwind-CVE-2015-3239.patch		\
   %D%/packages/patches/libupnp-CVE-2016-6255.patch		\
+  %D%/packages/patches/libupnp-CVE-2016-8863.patch		\
   %D%/packages/patches/libvpx-CVE-2016-2818.patch		\
   %D%/packages/patches/libwebp-CVE-2016-9085.patch		\
   %D%/packages/patches/libwmf-CAN-2004-0941.patch		\
