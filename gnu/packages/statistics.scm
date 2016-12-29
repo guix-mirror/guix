ab607061cdb37ed7d3819a23150127bd530d3521
Ricardo Wurmus <rekado@elephly.net>
2016-12-29 21:38:21 +0100

    gnu: r-hmisc: Update to 4.0-1.
    * gnu/packages/statistics.scm (r-hmisc): Update to 4.0-1.
    [propagated-inputs]: Remove r-cluster, r-foreign, r-lattice, r-nnet, and
    r-rpart; add r-base64.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -3510,37 +3510,33 @@ package instead.")
 (define-public r-hmisc
   (package
     (name "r-hmisc")
-    (version "4.0-0")
+    (version "4.0-1")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "Hmisc" version))
        (sha256
         (base32
-         "0azxmg9h9c456biydc0wpi9xhljmd37m8bv0zaadbvc93g7x0l4b"))))
+         "1dlm2gj1b9l6nf9w9zc0cn8d2japhx6cwifzmgjv4gg4skazlldq"))))
     (properties `((upstream-name . "Hmisc")))
     (build-system r-build-system)
     (native-inputs
      `(("gfortran" ,gfortran)))
     (propagated-inputs
      `(("r-acepack" ,r-acepack)
-       ("r-cluster" ,r-cluster)
+       ("r-base64" ,r-base64)
        ("r-data-table" ,r-data-table)
-       ("r-foreign" ,r-foreign)
        ("r-formula" ,r-formula)
        ("r-ggplot2" ,r-ggplot2)
        ("r-gridextra" ,r-gridextra)
        ("r-gtable" ,r-gtable)
-       ("r-lattice" ,r-lattice)
        ;; Hmisc needs survival >= 2.40.1, so it cannot use the survival
-       ;; package that comes with R 3.3.1.
+       ;; package that comes with R 3.3.2.
        ("r-survival" ,r-survival)
        ("r-latticeextra" ,r-latticeextra)
        ("r-htmltable" ,r-htmltable)
        ("r-htmltools" ,r-htmltools)
-       ("r-viridis" ,r-viridis)
-       ("r-nnet" ,r-nnet)
-       ("r-rpart" ,r-rpart)))
+       ("r-viridis" ,r-viridis)))
     (home-page "http://biostat.mc.vanderbilt.edu/Hmisc")
     (synopsis "Miscellaneous data analysis and graphics functions")
     (description
