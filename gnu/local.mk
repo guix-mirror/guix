5f0fabec54812e9ebd9a54b7c24b29899c765548
Leo Famulari <leo@famulari.name>
2017-01-01 15:12:48 -0500

    gnu: unrtf: Fix CVE-2016-10091.
    * gnu/packages/patches/unrtf-CVE-2016-10091.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/unrtf.scm (unrtf)[source]: Use it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -881,6 +881,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/tophat-build-with-later-seqan.patch	\
   %D%/packages/patches/totem-debug-format-fix.patch		\
   %D%/packages/patches/tuxpaint-stamps-path.patch		\
+  %D%/packages/patches/unrtf-CVE-2016-10091.patch		\
   %D%/packages/patches/unzip-CVE-2014-8139.patch		\
   %D%/packages/patches/unzip-CVE-2014-8140.patch		\
   %D%/packages/patches/unzip-CVE-2014-8141.patch		\
