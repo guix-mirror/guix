4a6f099e2c6dc3115abe708ea500d9e96e2ec0dc
Carlos Sánchez de La Lama <csanchezdll@gmail.com>
2017-01-04 23:52:41 +0100

    gnu: bootstrap: Add the dynamic linker name for "powerpc-linux".
    This commit completes cross-compilation support for powerpc-linux-gnu.
    
    * gnu/packages/bootstrap.scm (glibc-dynamic-linker): Add "powerpc-linux"
    case.

--- gnu/packages/bootstrap.scm
+++ gnu/packages/bootstrap.scm
@@ -168,6 +168,7 @@ successful, or false to signal an error."
         ((string=? system "i586-gnu") "/lib/ld.so.1")
         ((string=? system "i686-gnu") "/lib/ld.so.1")
         ((string=? system "aarch64-linux") "/lib/ld-linux-aarch64.so.1")
+        ((string=? system "powerpc-linux") "/lib/ld.so.1")
 
         ;; XXX: This one is used bare-bones, without a libc, so add a case
         ;; here just so we can keep going.
