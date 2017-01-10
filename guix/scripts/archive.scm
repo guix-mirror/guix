1545a012cb7cd78e25ed99ecee26df457be590e9
Ludovic Courtès <ludo@gnu.org>
2017-01-10 15:55:57 +0100

    guix archive: Allow compilation in the absence of Guile-JSON.
    Fixes <http://bugs.gnu.org/25409>.
    Reported by Ben Woodcroft <b.woodcroft@uq.edu.au>.
    
    * guix/scripts/archive.scm: Use 'module-autoload!' instead of #:use-module
    to (guix docker).

--- guix/scripts/archive.scm
+++ guix/scripts/archive.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2014, 2015, 2016 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013, 2014, 2015, 2016, 2017 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2017 Ricardo Wurmus <rekado@elephly.net>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -31,7 +31,6 @@
   #:use-module (guix ui)
   #:use-module (guix pki)
   #:use-module (guix pk-crypto)
-  #:use-module (guix docker)
   #:use-module (guix scripts)
   #:use-module (guix scripts build)
   #:use-module (gnu packages)
@@ -45,6 +44,11 @@
   #:use-module (ice-9 binary-ports)
   #:export (guix-archive
             options->derivations+files))
+
+;; XXX: Use this hack instead of #:autoload to avoid compilation errors.
+;; See <http://bugs.gnu.org/12202>.
+(module-autoload! (current-module)
+                  '(guix docker) '(build-docker-image))
 
 
 ;;;
