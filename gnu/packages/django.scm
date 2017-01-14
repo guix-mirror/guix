117134a78f98f9da293f5f3c480d745548c7a6ed
Hartmut Goebel <h.goebel@crazy-compilers.com>
2017-01-14 23:32:08 +0100

    gnu: python-django: Update to 1.10.5.
    * gnu/packages/django.scm(python-django): Update to 1.10.5.

--- gnu/packages/django.scm
+++ gnu/packages/django.scm
@@ -29,13 +29,13 @@
 (define-public python-django
   (package
     (name "python-django")
-    (version "1.10.3")
+    (version "1.10.5")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "Django" version))
               (sha256
                (base32
-                "0c4c8zs7kzb0bdlpy4vlzv6va26dbazr32h91rldf6waxs6z14kg"))))
+                "12szjsmnfhh2yr54sfynyjr8vl0q9gb6qak3ayqcifcinrs97f0d"))))
     (build-system python-build-system)
     (arguments
      '(#:phases
