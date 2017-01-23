2903ee1ea51fffa4e62d18d9f6807c8956fc4c8d
Thomas Danckaert <post@thomasdanckaert.be>
2017-01-23 19:21:52 +0100

    gnu: duplicity: Update to 0.7.11.
    * gnu/packages/backup.scm (duplicity): Update to 0.7.11.
      [source]: Remove patches.
      [inputs]: Add lftp, update gnupg, remove python-2, make python2-lockfile a
      propagated input, make python2-mock a native-input.
      [propagated-inputs]: Add python2-lockfile, python2-urllib3.
      [native-inputs]: Add python2-pexpect, python2-mock.
      [arguments]: Add build phase to embed gnupg store name.
    * gnu/packages/patches/duplicity-piped-password.patch: Delete it.
    * gnu/packages/patches/duplicity-test_selection-tmp.patch: Delete it.
    * gnu/local.mk (dist_patch_DATA): Remove patches.
    
    Signed-off-by: Marius Bakke <mbakke@fastmail.com>

--- gnu/local.mk
+++ gnu/local.mk
@@ -528,8 +528,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/doc++-include-directives.patch		\
   %D%/packages/patches/doc++-segfault-fix.patch			\
   %D%/packages/patches/doxygen-test.patch			\
-  %D%/packages/patches/duplicity-piped-password.patch		\
-  %D%/packages/patches/duplicity-test_selection-tmp.patch	\
   %D%/packages/patches/elfutils-tests-ptrace.patch		\
   %D%/packages/patches/elixir-disable-failing-tests.patch	\
   %D%/packages/patches/einstein-build.patch			\
