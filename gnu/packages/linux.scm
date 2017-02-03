883aab6462c49d4f4846a6f22168325e70227663
Mark H Weaver <mhw@netris.org>
2017-02-03 00:50:04 -0500

    gnu: linux-libre: Update to 4.9.7.
    * gnu/packages/linux.scm (%linux-libre-version, %linux-libre-hash)
    (linux-libre): Update to 4.9.7.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -335,8 +335,8 @@ It has been modified to remove all non-free binary blobs.")
 (define %intel-compatible-systems '("x86_64-linux" "i686-linux"))
 
 (define-public linux-libre
-  (make-linux-libre "4.9.6"
-                    "0mafa628la5qj26rff014mmih2widl5k2sjxg152lmpgijmf6qhd"
+  (make-linux-libre "4.9.7"
+                    "1qhdgniwfa3f57pmrc8jzhypr2fzfilag3gwvi0c4nf2sfgp2670"
                     %intel-compatible-systems
                     #:configuration-file kernel-config))
 
@@ -353,8 +353,8 @@ It has been modified to remove all non-free binary blobs.")
                     #:configuration-file kernel-config))
 
 ;; Avoid rebuilding kernel variants when there is a minor version bump.
-(define %linux-libre-version "4.9.6")
-(define %linux-libre-hash "0mafa628la5qj26rff014mmih2widl5k2sjxg152lmpgijmf6qhd")
+(define %linux-libre-version "4.9.7")
+(define %linux-libre-hash "1qhdgniwfa3f57pmrc8jzhypr2fzfilag3gwvi0c4nf2sfgp2670")
 
 (define-public linux-libre-arm-generic
   (make-linux-libre %linux-libre-version
