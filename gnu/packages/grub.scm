guix-mirror/guix

commit d846834fc2b2f76aa2e258685bc211edd31866c5

gnu: grub: Update to 2.02rc1.
* gnu/packages/grub.scm (grub, grub-efi): Update to 2.02rc1.
[arguments]: Don't disable grub_func_test. Remove #:configure-flags.

gnu/packages/grub.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2015 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015 Leo Famulari <leo@famulari.name>
 ;;; Copyright © 2016 Jan Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2016, 2017 Marius Bakke <mbakke@fastmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -52,23 +53,20 @@
 (define-public grub
   (package
     (name "grub")
-    (version "2.02beta3")
+    (version "2.02rc1")
     (source (origin
              (method url-fetch)
              (uri (string-append
                    "ftp://alpha.gnu.org/gnu/grub/grub-"
-                   "2.02~beta3"
+                   "2.02~rc1"
                    ".tar.xz"))
              (file-name (string-append name "-" version ".tar.xz"))
              (sha256
               (base32
-               "18ddwnw0vxs7zigvah0g6a5z5vvlz0p8fjglxv1h59sjbrakvv1h"))))
+               "0y02v19x9sb5jvj740f604vvi5j1rx8pily1jk0l64bdp7lkjlj4"))))
     (build-system gnu-build-system)
     (arguments
-     '(;; Two warnings: suggest braces, signed/unsigned comparison.
-       #:configure-flags '("--disable-werror")
-
-       #:phases (modify-phases %standard-phases
+     '(#:phases (modify-phases %standard-phases
                   (add-after 'unpack 'patch-stuff
                    (lambda* (#:key inputs #:allow-other-keys)
                      (substitute* "grub-core/Makefile.in"
@@ -87,13 +85,6 @@
                      ;; Make the font visible.
                      (copy-file (assoc-ref inputs "unifont") "unifont.bdf.gz")
                      (system* "gunzip" "unifont.bdf.gz")
-
-                     ;; We hit an assertion failure in
-                     ;; grub-core/tests/video_checksum.c, as reported at
-                     ;; <https://lists.gnu.org/archive/html/grub-devel/2016-07/msg00026.html>.
-                     ;; Disable this test for now.
-                     (substitute* "tests/grub_func_test.in"
-                       (("set -e") "exit 77\nset -e"))
                      #t)))))
     (inputs
      `(("gettext" ,gettext-minimal)

commit ef753a1a8f9e7c971957abfda9b672a7728cd073

gnu: grub-efi: Really build the EFI variant.
Fixes a regression introduced in
d846834fc2b2f76aa2e258685bc211edd31866c5 where '--with-platform=efi'
would no longer be passed.

* gnu/packages/grub.scm (grub-efi)[arguments]: Provide a default value
for #:configure-flags.

gnu/packages/grub.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2014, 2015, 2016 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013, 2014, 2015, 2016, 2017 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2015 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015 Leo Famulari <leo@famulari.name>
 ;;; Copyright © 2016 Jan Nieuwenhuizen <janneke@gnu.org>
@@ -139,8 +139,8 @@ menu to select one of the installed operating systems.")
        ;; Search for 'OVMF' in "tests/util/grub-shell.in".
        #:tests? #f
        ,@(substitute-keyword-arguments (package-arguments grub)
-           ((#:configure-flags flags) `(cons* "--with-platform=efi"
-                                              ,flags))
+           ((#:configure-flags flags ''())
+            `(cons "--with-platform=efi" ,flags))
            ((#:phases phases)
             `(modify-phases ,phases
                (add-after 'patch-stuff 'use-absolute-efibootmgr-path