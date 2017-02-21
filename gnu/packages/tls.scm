3853bf2542e949c642410a3ef06651f063af726e
Efraim Flashner <efraim@flashner.co.il>
2017-02-21 20:27:47 +0200

    gnu: libtasn1: Update to 4.10.
    * gnu/packages/tls.scm (libtasn1): Update to 4.10.

--- gnu/packages/tls.scm
+++ gnu/packages/tls.scm
@@ -52,7 +52,7 @@
 (define-public libtasn1
   (package
     (name "libtasn1")
-    (version "4.9")
+    (version "4.10")
     (source
      (origin
       (method url-fetch)
@@ -60,7 +60,7 @@
                           version ".tar.gz"))
       (sha256
        (base32
-        "0869cp6jx7cajgv6cnddsh3vc7bimmdkdjn80y1jpb4iss7plvsg"))))
+        "00jsix5hny0g768zv4hk78dib7w0qmk5fbizf4jj37r51nd4s6k8"))))
     (build-system gnu-build-system)
     (native-inputs `(("perl" ,perl)))
     (home-page "http://www.gnu.org/software/libtasn1/")
