b01a89854ba1bd22b94c2373662bc07d035be2c1
Marius Bakke <mbakke@fastmail.com>
2017-02-26 21:19:39 +0100

    gnu: Remove unused patch.
    This is a followup to 6f9d5b2e8c861c3a1243937a26400f8394946346.
    
    * gnu/packages/patches/libssh-0.6.5-CVE-2016-0739.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -692,7 +692,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/libmad-frame-length.patch		\
   %D%/packages/patches/libmad-mips-newgcc.patch			\
   %D%/packages/patches/libpng-CVE-2016-10087.patch		\
-  %D%/packages/patches/libssh-0.6.5-CVE-2016-0739.patch		\
   %D%/packages/patches/libtar-CVE-2013-4420.patch \
   %D%/packages/patches/libtheora-config-guess.patch		\
   %D%/packages/patches/libtiff-CVE-2016-10092.patch		\
