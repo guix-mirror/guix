1721fb4fe79eecc8061c7bcdfe45c3503627587f
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2017-03-11 09:06:55 +1000

    gnu: Add r-modeltools.
    * gnu/packages/statistics.scm (r-modeltools): New variable.
    
    Signed-off-by: Ben Woodcroft <donttrustben@gmail.com>

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -4189,3 +4189,23 @@ forest of trees using random inputs, for classification and regression.")
 multimodality and provides a test with simulation based p-values, where the
 original public code has been corrected.")
     (license license:gpl2+)))
+
+(define-public r-modeltools
+  (package
+    (name "r-modeltools")
+    (version "0.2-21")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "modeltools" version))
+       (sha256
+        (base32
+         "0ynds453xprxv0jqqzi3blnv5w6vrdww9pvd1sq4lrr5ar3k3cq7"))))
+    (build-system r-build-system)
+    (home-page "http://cran.r-project.org/web/packages/modeltools")
+    (synopsis "Tools and classes for statistical models")
+    (description
+     "This package provides a collection of tools to deal with statistical
+models.  The functionality is experimental and the user interface is likely
+to change in the future.")
+    (license license:gpl2)))
