567b154fb0c7fd123a9f29caaa5441751e5b569e
Ricardo Wurmus <rekado@elephly.net>
2017-03-12 00:19:00 +0100

    gnu: r-rpart: Move to the set of recommended packages.
    * gnu/packages/statistics.scm (r-rpart): Move up to the set of recommended
    packages.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -472,6 +472,25 @@ distributions beyond the exponential family.")
 single hidden layer, and for multinomial log-linear models.")
     (license (list license:gpl2+ license:gpl3+))))
 
+(define-public r-rpart
+  (package
+    (name "r-rpart")
+    (version "4.1-10")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "rpart" version))
+       (sha256
+        (base32
+         "119dvh2cpab4vq9blvbkil5hgq6w018amiwlda3ii0fki39axpf5"))))
+    (build-system r-build-system)
+    (home-page "http://cran.r-project.org/web/packages/rpart")
+    (synopsis "Recursive partitioning and regression trees")
+    (description
+     "This package provides recursive partitioning functions for
+classification, regression and survival trees.")
+    (license (list license:gpl2+ license:gpl3+))))
+
 (define-public r-bit
   (package
     (name "r-bit")
@@ -3643,25 +3662,6 @@ following problems:
  overlapping groups,...).
 @end enumerate\n")
     (license license:gpl3+)))
-
-(define-public r-rpart
-  (package
-    (name "r-rpart")
-    (version "4.1-10")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (cran-uri "rpart" version))
-       (sha256
-        (base32
-         "119dvh2cpab4vq9blvbkil5hgq6w018amiwlda3ii0fki39axpf5"))))
-    (build-system r-build-system)
-    (home-page "http://cran.r-project.org/web/packages/rpart")
-    (synopsis "Recursive partitioning and regression trees")
-    (description
-     "This package provides recursive partitioning functions for
-classification, regression and survival trees.")
-    (license (list license:gpl2+ license:gpl3+))))
 
 (define-public r-survival
   (package
