4f3dcdd99ba13ab3bdbf1e014afcd076cd95fac7
Marius Bakke <mbakke@fastmail.com>
2017-03-13 16:53:27 +0100

    gnu: nss, nss-certs: Update to 3.29.3.
    * gnu/packages/gnuzilla.scm (nss): Update to 3.29.3.
    * gnu/packages/certs.scm (nss-certs): Update to 3.29.3.

--- gnu/packages/gnuzilla.scm
+++ gnu/packages/gnuzilla.scm
@@ -186,7 +186,7 @@ in the Mozilla clients.")
 (define-public nss
   (package
     (name "nss")
-    (version "3.29.2")
+    (version "3.29.3")
     (source (origin
               (method url-fetch)
               (uri (let ((version-with-underscores
@@ -197,7 +197,7 @@ in the Mozilla clients.")
                       "nss-" version ".tar.gz")))
               (sha256
                (base32
-                "149807rmzb76hnh48rw4m9jw83iw0168njzchz0hmbsgc8mk0i5w"))
+                "1sz1r2iml9bhd4iqiqz75gii855a25895vpy9scjky0y4lqwrp9m"))
               ;; Create nss.pc and nss-config.
               (patches (search-patches "nss-pkgconfig.patch"))))
     (build-system gnu-build-system)
