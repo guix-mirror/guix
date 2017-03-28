guix-mirror/guix

commit a4f542341511f33ece18d16b68118214da8143ec

download: Handle username and password properties for FTP URIs.
* guix/build/download.scm (ftp-fetch): Process username and password from a URI.

guix/build/download.scm
@@ -241,7 +241,18 @@ and 'guix publish', something like
 (define* (ftp-fetch uri file #:key timeout)
   "Fetch data from URI and write it to FILE.  Return FILE on success.  Bail
 out if the connection could not be established in less than TIMEOUT seconds."
-  (let* ((conn (ftp-open (uri-host uri) #:timeout timeout))
+  (let* ((userinfo (string-split (uri-userinfo uri) #\:))
+         (conn (match userinfo
+                 (("")
+                  (ftp-open (uri-host uri) #:timeout timeout))
+                 (((? string? user))
+                  (ftp-open (uri-host uri) #:timeout timeout
+                                           #:username user))
+                 (((? string? user) (? string? pass))
+                  (ftp-open (uri-host uri) #:timeout timeout
+                                           #:username user
+                                           #:password pass))
+                 (_ (ftp-open (uri-host uri) #:timeout timeout))))
          (size (false-if-exception (ftp-size conn (uri-path uri))))
          (in   (ftp-retr conn (basename (uri-path uri))
                          (dirname (uri-path uri)))))

commit 6dfd683dc742ebb80983137f80a457af38ca7d8d

download: Properly parse ftp:// URLs that lack user info.
Fixes a regression introduced in
a4f542341511f33ece18d16b68118214da8143ec.

Reported by Hartmut Goebel.

* guix/build/download.scm (ftp-fetch): Account for the case where
'uri-userinfo' returns #f.  Remove the case where it returns "" since
that cannot happen.

guix/build/download.scm
@@ -241,10 +241,8 @@ and 'guix publish', something like
 (define* (ftp-fetch uri file #:key timeout)
   "Fetch data from URI and write it to FILE.  Return FILE on success.  Bail
 out if the connection could not be established in less than TIMEOUT seconds."
-  (let* ((userinfo (string-split (uri-userinfo uri) #\:))
-         (conn (match userinfo
-                 (("")
-                  (ftp-open (uri-host uri) #:timeout timeout))
+  (let* ((conn (match (and=> (uri-userinfo uri)
+                             (cut string-split <> #\:))
                  (((? string? user))
                   (ftp-open (uri-host uri) #:timeout timeout
                                            #:username user))