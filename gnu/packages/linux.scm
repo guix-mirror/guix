bacc1d26c6e49f65b9c55296e3643999e7e8bc72
Mark H Weaver <mhw@netris.org>
2017-03-31 18:20:54 -0400

    gnu: linux-libre: Update to 4.10.8 [fixes CVE-2017-7184].
    * gnu/packages/linux.scm (%linux-libre-version): Update to 4.10.8.
    (%linux-libre-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -342,8 +342,8 @@ It has been modified to remove all non-free binary blobs.")
 
 (define %intel-compatible-systems '("x86_64-linux" "i686-linux"))
 
-(define %linux-libre-version "4.10.7")
-(define %linux-libre-hash "0cjhnq67h3kk2dx0kd3nw0f1lbxvc8jr8n5khb9bh07913ag2pyq")
+(define %linux-libre-version "4.10.8")
+(define %linux-libre-hash "0936i2h9l4gn67gby9kx70w6zqv1nbzbgwssry6k11wrjwa2p8n4")
 
 (define-public linux-libre
   (make-linux-libre %linux-libre-version
