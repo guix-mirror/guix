821072e22b0619087675bdc178081f0efa26e1a9
Marius Bakke <mbakke@fastmail.com>
2017-04-03 14:56:52 +0200

    gnu: gdbm: Update to 1.13.
    * gnu/packages/databases.scm (gdbm): Update to 1.13.

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -123,14 +123,14 @@ either single machines or networked clusters.")
 (define-public gdbm
   (package
     (name "gdbm")
-    (version "1.12")
+    (version "1.13")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/gdbm/gdbm-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "1smwz4x5qa4js0zf1w3asq6z7mh20zlgwbh2bk5dczw6xrk22yyr"))))
+                "0lx201q20dvc70f8a3c9s7s18z15inlxvbffph97ngvrgnyjq9cx"))))
     (arguments `(#:configure-flags '("--enable-libgdbm-compat")))
     (build-system gnu-build-system)
     (home-page "http://www.gnu.org.ua/software/gdbm")
