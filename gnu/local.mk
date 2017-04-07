fbd6fb1a9d75bd7b5d1df24cb805b7df335b0223
Leo Famulari <leo@famulari.name>
2017-04-07 00:16:18 -0400

    gnu: qemu: Update to 2.8.1 [security fixes].
    Fixes CVE-2016-{9602,9603} and CVE-2017-{2615,2620,2630,5667,5931}.
    
    * gnu/packages/qemu.scm (qemu): Update to 2.8.1.
    * gnu/packages/patches/qemu-CVE-2017-2615.patch,
    gnu/packages/patches/qemu-CVE-2017-2620.patch,
    gnu/packages/patches/qemu-CVE-2017-2630.patch,
    gnu/packages/patches/qemu-CVE-2017-5667.patch,
    gnu/packages/patches/qemu-CVE-2017-5931.patch: Delete files.
    * gnu/local.mk (dist_patch_DATA): Remove them.

--- gnu/local.mk
+++ gnu/local.mk
@@ -890,18 +890,13 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-pygpgme-fix-pinentry-tests.patch	\
   %D%/packages/patches/python2-subprocess32-disable-input-test.patch	\
   %D%/packages/patches/qemu-CVE-2016-10155.patch			\
-  %D%/packages/patches/qemu-CVE-2017-2615.patch			\
-  %D%/packages/patches/qemu-CVE-2017-2620.patch			\
-  %D%/packages/patches/qemu-CVE-2017-2630.patch			\
   %D%/packages/patches/qemu-CVE-2017-5525.patch			\
   %D%/packages/patches/qemu-CVE-2017-5526.patch			\
   %D%/packages/patches/qemu-CVE-2017-5552.patch			\
   %D%/packages/patches/qemu-CVE-2017-5578.patch			\
   %D%/packages/patches/qemu-CVE-2017-5579.patch			\
-  %D%/packages/patches/qemu-CVE-2017-5667.patch			\
   %D%/packages/patches/qemu-CVE-2017-5856.patch			\
   %D%/packages/patches/qemu-CVE-2017-5898.patch			\
-  %D%/packages/patches/qemu-CVE-2017-5931.patch			\
   %D%/packages/patches/qt4-ldflags.patch			\
   %D%/packages/patches/quickswitch-fix-dmenu-check.patch	\
   %D%/packages/patches/rapicorn-isnan.patch			\
