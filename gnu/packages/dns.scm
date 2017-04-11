d4bcbbb994c56e97e06067839d55b4eb42c5d455
Tobias Geerinckx-Rice <me@tobias.gr>
2017-04-11 18:11:34 +0200

    gnu: knot: Update to 2.4.3.
    * gnu/packages/dns.scm (knot): Update to 2.4.3.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -318,14 +318,14 @@ Extensions} (DNSSEC).")
 (define-public knot
   (package
     (name "knot")
-    (version "2.4.2")
+    (version "2.4.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://secure.nic.cz/files/knot-dns/"
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0licfnjl0mq9lzdn6frzdasrj65253xc9n33dhvxcjqr3z7pznip"))
+                "0kvhibnnk77nqi9gdw2zbnp0dydfcy6zy96qv0f3a7wwnay5h0pr"))
               (modules '((guix build utils)))
               (snippet
                '(begin
