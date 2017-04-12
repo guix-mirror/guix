0eb0fe2d302028b51185b98ac55e45b483a5ea82
Efraim Flashner <efraim@flashner.co.il>
2017-04-12 06:19:56 +0300

    gnu: jasper: Fixx CVE-2017-6850.
    * gnu/packages/image.scm (jasper)[source]: Add patch.
    * gnu/packages/patches/jasper-CVE-2017-6850.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -661,6 +661,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/id3lib-CVE-2007-4460.patch			\
   %D%/packages/patches/ilmbase-fix-tests.patch			\
   %D%/packages/patches/isl-0.11.1-aarch64-support.patch	\
+  %D%/packages/patches/jasper-CVE-2017-6850.patch		\
   %D%/packages/patches/jbig2dec-ignore-testtest.patch		\
   %D%/packages/patches/jbig2dec-CVE-2016-9601.patch		\
   %D%/packages/patches/jq-CVE-2015-8863.patch			\
