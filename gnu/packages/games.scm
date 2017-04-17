9505dafb9940686302835f03c22e4e24100e0798
宋文武 <iyzsong@member.fsf.org>
2017-04-17 22:34:36 +0800

    retroarch: Update to 1.5.0.
    * gnu/packages/games.scm (retroarch): Update to 1.5.0.

--- gnu/packages/games.scm
+++ gnu/packages/games.scm
@@ -433,7 +433,7 @@ that beneath its ruins lay buried an ancient evil.")
            (lambda _
              (substitute* "acinclude.m4"
                (("ncursesw5-config") "ncursesw6-config"))
-             (zero? (system* "sh" "autogen.sh"))))))) 
+             (zero? (system* "sh" "autogen.sh")))))))
     (native-inputs
      `(("autoconf" ,autoconf)
        ("automake" ,automake)))
@@ -1084,7 +1084,7 @@ either by Infocom or created using the Inform compiler.")
 (define-public retroarch
   (package
     (name "retroarch")
-    (version "1.3.6")
+    (version "1.5.0")
     (source
      (origin
        (method url-fetch)
@@ -1092,7 +1092,7 @@ either by Infocom or created using the Inform compiler.")
                            version ".tar.gz"))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1xar0wagcz50clwwkvjg4zq9m1sjqw47vw3xx44pisdj94g21m5y"))))
+        (base32 "1rbdax3i33myg1v938pxy28117ihff2lml1ky6g70c8099fkirjx"))))
     (build-system gnu-build-system)
     (arguments
      '(#:tests? #f                      ; no tests
