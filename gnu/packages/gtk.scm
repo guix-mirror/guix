40522a7e89bc169233ec724592c9439e8004f4ad
Kei Kebreau <kei@openmailbox.org>
2017-04-17 21:25:17 -0400

    gnu: gtk+: Update to 3.22.12.
    * gnu/packages/gtk.scm (gtk+): Update 3.22.12.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -651,7 +651,7 @@ application suites.")
    (name "gtk+")
    ;; NOTE: When updating the version of 'gtk+', the hash of 'mate-themes' in
    ;;       mate.scm will also need to be updated.
-   (version "3.22.6")
+   (version "3.22.12")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnome/sources/" name "/"
@@ -659,7 +659,7 @@ application suites.")
                                 name "-" version ".tar.xz"))
             (sha256
              (base32
-              "0bqpx8825b1fdjmz14wq20zq58gq1yi1p5xjps8l6zqid8hmm9zb"))
+              "1359w81sxs2izkan2rni985x78s6zr1arf469qmyw4bazg7f1yl4"))
             (patches (search-patches "gtk3-respect-GUIX_GTK3_PATH.patch"
                                      "gtk3-respect-GUIX_GTK3_IM_MODULE_FILE.patch"))))
    (outputs '("out" "bin" "doc"))
