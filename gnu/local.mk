f9ff014bf623071b9343265e8a5aa994a10ec9d4
Petter <petter@mykolab.ch>
2017-04-23 13:46:41 +0200

    gnu: Add cool-retro-term.
    * gnu/packages/terminals.scm (cool-retro-term): New variable.
    * gnu/packages/patches/cool-retro-term-remove-non-free-fonts.patch,
    gnu/packages/patches/cool-retro-term-fix-array-size.patch,
    gnu/packages/patches/cool-retro-term-memory-leak-1.patch,
    gnu/packages/patches/cool-retro-term-remove-non-free-fonts.patch: New patches.
    * gnu/local.mk (dist_patch_DATA): Add them.
    
    Co-authored-by: Eric Bavier <bavier@member.fsf.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -529,6 +529,10 @@ dist_patch_DATA =						\
   %D%/packages/patches/clx-remove-demo.patch			\
   %D%/packages/patches/cmake-fix-tests.patch			\
   %D%/packages/patches/coda-use-system-libs.patch		\
+  %D%/packages/patches/cool-retro-term-dont-check-uninit-member.patch	\
+  %D%/packages/patches/cool-retro-term-fix-array-size.patch	\
+  %D%/packages/patches/cool-retro-term-memory-leak-1.patch	\
+  %D%/packages/patches/cool-retro-term-remove-non-free-fonts.patch	\
   %D%/packages/patches/coreutils-cut-huge-range-test.patch	\
   %D%/packages/patches/coreutils-fix-cross-compilation.patch    \
   %D%/packages/patches/cpio-CVE-2016-2037.patch			\
