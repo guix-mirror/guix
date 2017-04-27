1115f140179b8fafb5a0f7c91a22dfdb7d6b21cc
Andy Wingo <wingo@igalia.com>
2017-04-27 14:09:16 +0200

    services: Add certbot service.
    * gnu/services/certbot.scm: New file.
    * gnu/local.mk (GNU_SYSTEM_MODULES): Add new file.
    * doc/guix.texi (Certificate Services): New section.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -444,6 +444,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/services/audio.scm                        \
   %D%/services/avahi.scm			\
   %D%/services/base.scm				\
+  %D%/services/certbot.scm			\
   %D%/services/configuration.scm		\
   %D%/services/cuirass.scm			\
   %D%/services/cups.scm				\
