f8ff364ab77a92951b54eedb0c985f5b498206e0
Marius Bakke <mbakke@fastmail.com>
2017-05-08 12:22:19 +0200

    gnu: re2: Update to 2017-05-01.
    * gnu/packages/regex.scm (re2): Update to 2017-05-01.

--- gnu/packages/regex.scm
+++ gnu/packages/regex.scm
@@ -28,7 +28,7 @@
 (define-public re2
    (package
      (name "re2")
-     (version "2017-04-01")
+     (version "2017-05-01")
      (source (origin
                (method url-fetch)
                (uri
@@ -38,7 +38,7 @@
                (file-name (string-append name "-" version ".tar.gz"))
                (sha256
                 (base32
-                 "14i9pf9ap8h48j1584ifsq7c7a884cr1s4r7zvpcb9x1xr9232wh"))))
+                 "19glvic4ipd122yhcavf7ifg7ms5k61398vq7gyb90wggf2w0z9k"))))
      (build-system gnu-build-system)
      (arguments
       `(#:modules ((guix build gnu-build-system)
