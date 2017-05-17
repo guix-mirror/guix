5ae59eb6d99b7ac6e3334418781b0559bf604a23
Arun Isaac <arunisaac@systemreboot.net>
2017-05-17 21:30:12 +0530

    guix: lint: Slightly simplify `check-source-file-name'.
    * guix/scripts/lint.scm (check-source-file-name): Implement file name matching
      with regular expression.

--- guix/scripts/lint.scm
+++ guix/scripts/lint.scm
@@ -673,11 +673,10 @@ descriptions maintained upstream."
     (let ((file-name (origin-actual-file-name origin))
           (version (package-version package)))
       (and file-name
-           (not (or (string-prefix? version file-name)
-                    ;; Common in many projects is for the filename to start
-                    ;; with a "v" followed by the version,
-                    ;; e.g. "v3.2.0.tar.gz".
-                    (string-prefix? (string-append "v" version) file-name))))))
+           ;; Common in many projects is for the filename to start
+           ;; with a "v" followed by the version,
+           ;; e.g. "v3.2.0.tar.gz".
+           (not (string-match (string-append "^v?" version) file-name)))))
 
   (let ((origin (package-source package)))
     (unless (or (not origin) (origin-file-name-valid? origin))
