e7fbd49132406bb9ec12141ac77ac401f58ee267
Ludovic Courtès <ludo@gnu.org>
2017-05-19 10:50:20 +0200

    gnu: guile-ssh: Fix potential double-free/use-after-free issue.
    Fixes <http://bugs.gnu.org/26976>.
    Reported by Mark H Weaver <mhw@netris.org>.
    
    * gnu/packages/patches/guile-ssh-double-free.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/ssh.scm (guile-ssh)[source](patches): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -654,6 +654,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/guile-relocatable.patch			\
   %D%/packages/patches/guile-rsvg-pkgconfig.patch		\
   %D%/packages/patches/guile-ssh-rexec-bug.patch		\
+  %D%/packages/patches/guile-ssh-double-free.patch		\
   %D%/packages/patches/gtk2-respect-GUIX_GTK2_PATH.patch	\
   %D%/packages/patches/gtk2-respect-GUIX_GTK2_IM_MODULE_FILE.patch \
   %D%/packages/patches/gtk2-theme-paths.patch			\
