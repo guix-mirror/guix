guix-mirror/guix

commit 2f861edf5cf5118ad560737343312c9a5efe5b2f

gnu: unionfs-fuse: Update to 2.0.
* gnu/packages/linux.scm (unionfs-fuse): Update to 2.0.
[home-page]: Update to Github page.

gnu/packages/linux.scm
@@ -1510,19 +1510,20 @@ user-space processes.")
 (define-public unionfs-fuse
   (package
     (name "unionfs-fuse")
-    (version "0.26")
+    (version "2.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
-                    "http://podgorny.cz/unionfs-fuse/releases/unionfs-fuse-"
-                    version ".tar.xz"))
+                    "https://github.com/rpodgorny/unionfs-fuse/archive/v"
+                    version ".tar.gz"))
+              (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0qpnr4czgc62vsfnmv933w62nq3xwcbnvqch72qakfgca75rsp4d"))))
+                "0hsn8l1iblvx27bpd4dvnvnbh9ri3sv2f9xzpsnfz3379kb7skgj"))))
     (build-system cmake-build-system)
     (inputs `(("fuse" ,fuse)))
     (arguments '(#:tests? #f))                    ; no tests
-    (home-page "http://podgorny.cz/moin/UnionFsFuse")
+    (home-page "https://github.com/rpodgorny/unionfs-fuse")
     (synopsis "User-space union file system")
     (description
      "UnionFS-FUSE is a flexible union file system implementation in user

commit 9f8d6eb24a5f193683076e16ffb68da18a537140

gnu: unionfs-fuse-static: Remove 'unionfsctl' executable.
This avoids pulling in glibc & co.  Fixes a regression introduced in
2f861edf5cf5118ad560737343312c9a5efe5b2f (the 'unionfsctl' executable
did not exist in unionfs-fuse 0.26).

* gnu/packages/linux.scm (unionfs-fuse/static)[arguments]
<post-install>: Remove 'unionfsctl' binary.

gnu/packages/linux.scm
@@ -1568,7 +1568,12 @@ UnionFS-FUSE additionally supports copy-on-write.")
                           (exe (string-append out "/bin/unionfs")))
                      ;; By default, 'unionfs' keeps references to
                      ;; $glibc/share/locale and similar stuff.  Remove them.
-                     (remove-store-references exe)))
+                     (remove-store-references exe)
+
+                     ;; 'unionfsctl' has references to glibc as well.  Since
+                     ;; we don't need it, remove it.
+                     (delete-file (string-append out "/bin/unionfsctl"))
+                     #t))
                  %standard-phases)))
     (inputs `(("fuse" ,fuse-static)))))
 