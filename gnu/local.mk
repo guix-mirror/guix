34cfeca84e62b92e4f2b32c5bf33fe4982253f48
Sergei Trofimovich <slyfox@inbox.ru>
2017-06-03 16:17:58 +0100

    gnu: libgit2: Fix Clar test framework.
    This commit reverts 'patch' hack introduced in
    a48a1071a686b250bf8b7982ca0ce8dda1d93c61 and fixes bug in the Clar test
    framework.
    
    Patch is proposed to Clar upstream as
    <https://github.com/vmg/clar/pull/78>.
    
    * gnu/local.mk: Add libgit2-0.25.1-mtime-0.patch.
    * gnu/packages/patches/libgit2-0.25.1-mtime-0.patch: New file.
    * gnu/packages/version-control.scm (libgit2)[arguments]:
    Remove 'apply-patch' phase.
    [inputs]: Remove "patch".
    [source]: Add 'patches' field.
    
    Signed-off-by: Sergei Trofimovich <slyfox@inbox.ru>
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -729,6 +729,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/libevent-2.1-dns-tests.patch		\
   %D%/packages/patches/libevent-2.1-skip-failing-test.patch	\
   %D%/packages/patches/libextractor-ffmpeg-3.patch		\
+  %D%/packages/patches/libgit2-0.25.1-mtime-0.patch		\
   %D%/packages/patches/libgit2-use-after-free.patch		\
   %D%/packages/patches/libjxr-fix-function-signature.patch	\
   %D%/packages/patches/libjxr-fix-typos.patch			\
