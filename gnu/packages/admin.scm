001f8cdc48eda3d272b585370f4c21980bcf2e12
Leo Famulari <leo@famulari.name>
2017-06-15 12:11:02 -0400

    gnu: isc-dhcp: Update bundled bind to 9.9.10-P1.
    * gnu/packages/admin.scm (isc-dhcp): Update bundled bind to 9.9.10-P1.

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -481,9 +481,9 @@ connection alive.")
 (define-public isc-dhcp
   (let* ((bind-major-version "9")
          (bind-minor-version "9")
-         (bind-patch-version "9")
+         (bind-patch-version "10")
          (bind-release-type "-P")         ; for patch release, use "-P"
-         (bind-release-version "8")      ; for patch release, e.g. "6"
+         (bind-release-version "1")      ; for patch release, e.g. "6"
          (bind-version (string-append bind-major-version
                                       "."
                                       bind-minor-version
@@ -599,7 +599,7 @@ connection alive.")
                                         "/bind-" bind-version ".tar.gz"))
                     (sha256
                      (base32
-                      "1f5i64f6y4rmy61y63r5if1lifw8dw8r8dh6ns3x4002hanzrpgz"))))
+                      "1ibbparr9k52rbs0qf0ar8jwvhhx6lja7ylxzpf32swklmhz629c"))))
 
                 ;; When cross-compiling, we need the cross Coreutils and sed.
                 ;; Otherwise just use those from %FINAL-INPUTS.
