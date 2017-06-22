992a9e59dd6cdd44d3b57dee4b736081be2db4a3
Ludovic Courtès <ludo@gnu.org>
2017-06-22 10:15:40 +0200

    gnu: Remove unnecessary module import.
    This is a followup to d2885c83929b66a0cf98e740372bddb7e8787631.
    
    * gnu/packages/version-control.scm: Remove needless module import.

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -37,7 +37,6 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix build utils)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system haskell)
