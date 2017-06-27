2482c02f3b23b2490a6647e0717cf8a4ccf3f6a8
Arun Isaac <arunisaac@systemreboot.net>
2017-06-27 11:34:58 +0530

    gnu: emacs-which-key: Avoid leading "(" in description line.
    * gnu/packages/emacs.scm (emacs-which-key): Reformat description to avoid a
    line beginning with "(" because such lines break indentation in Emacs.

--- gnu/packages/emacs.scm
+++ gnu/packages/emacs.scm
@@ -4990,10 +4990,11 @@ displays results pretty-printed in XML or JSON with @code{restclient-mode}")
                              "-f" "ert-run-tests-batch-and-exit")))))))
     (home-page "https://github.com/justbur/emacs-which-key")
     (synopsis "Display available key bindings in popup")
-    (description "@code{emacs-which-key} is a minor mode for Emacs that
-displays the key bindings following your currently entered incomplete command
-(a prefix) in a popup.  For example, after enabling the minor mode if you
-enter C-x and wait for the default of 1 second, the minibuffer will expand
-with all of the available key bindings that follow C-x (or as many as space
-allows given your settings).")
+    (description
+     "@code{emacs-which-key} is a minor mode for Emacs that displays the key
+bindings following your currently entered incomplete command (a prefix) in a
+popup.  For example, after enabling the minor mode if you enter C-x and wait
+for the default of 1 second, the minibuffer will expand with all of the
+available key bindings that follow C-x (or as many as space allows given your
+settings).")
     (license license:gpl3+)))
