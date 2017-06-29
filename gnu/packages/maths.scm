guix-mirror/guix

commit ef39a17717a10a7f406730258acfdb07934c7255

gnu: lapack: Update to 3.7.1.
* gnu/packages/maths.scm (lapack): Update to 3.7.1.

gnu/packages/maths.scm
@@ -428,7 +428,7 @@ large scale eigenvalue problems.")
 (define-public lapack
   (package
     (name "lapack")
-    (version "3.5.0")
+    (version "3.7.1")
     (source
      (origin
       (method url-fetch)
@@ -436,7 +436,7 @@ large scale eigenvalue problems.")
                           version ".tgz"))
       (sha256
        (base32
-        "0lk3f97i9imqascnlf6wr5mjpyxqcdj73pgj97dj2mgvyg9z1n4s"))))
+        "0yavf6m9l78pwlnk5g61cg8x28mr30j0g8gkai0jrdqfjjmf3whs"))))
     (build-system cmake-build-system)
     (home-page "http://www.netlib.org/lapack/")
     (inputs `(("fortran" ,gfortran)

commit 3ab869b63fc43c8d13864d711c2dc55fb3691456

gnu: lapack: Install libraries to PREFIX/lib, not PREFIX/lib64.
This fixes a regression introduced in
ef39a17717a10a7f406730258acfdb07934c7255, which would break
python-scipy ("undefined reference to `zungqr_'").

* gnu/packages/maths.scm (lapack)[arguments]: Add
"-DCMAKE_INSTALL_LIBDIR" to #:configure-flags.

gnu/packages/maths.scm
@@ -442,11 +442,18 @@ large scale eigenvalue problems.")
     (inputs `(("fortran" ,gfortran)
               ("python" ,python-2)))
     (arguments
-     `(#:configure-flags '("-DBUILD_SHARED_LIBS:BOOL=YES"
-                           "-DLAPACKE=ON"
+     `(#:configure-flags (list
+                          ;; Install to PREFIX/lib (the default is
+                          ;; PREFIX/lib64).
+                          (string-append "-DCMAKE_INSTALL_LIBDIR="
+                                         (assoc-ref %outputs "out")
+                                         "/lib")
 
-                           ;; Build the 'LAPACKE_clatms' functions.
-                           "-DLAPACKE_WITH_TMG=ON")
+                          "-DBUILD_SHARED_LIBS:BOOL=YES"
+                          "-DLAPACKE=ON"
+
+                          ;; Build the 'LAPACKE_clatms' functions.
+                          "-DLAPACKE_WITH_TMG=ON")
        #:phases (alist-cons-before
                  'check 'patch-python
                  (lambda* (#:key inputs #:allow-other-keys)