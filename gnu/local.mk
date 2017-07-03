4732e6ee84fea4e58b8a62cad054b10966f4f6e6
Ben Woodcroft <donttrustben@gmail.com>
2017-07-03 23:00:14 +1000

    gnu: blast+: Update to 2.6.0.
    * gnu/packages/bioinformatics.scm (blast+): Update to 2.6.0.
    [origin]: Remove bundled pcre.  Add patch.
    [arguments]: Replace paths in run_with_lock.c.  Configure with pcre.
    [inputs]: Add pcre, perl, python.
    * gnu/packages/patches/blast+-fix-makefile.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -522,6 +522,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/binutils-ld-new-dtags.patch		\
   %D%/packages/patches/binutils-loongson-workaround.patch	\
   %D%/packages/patches/binutils-mips-bash-bug.patch		\
+  %D%/packages/patches/blast+-fix-makefile.patch		\
   %D%/packages/patches/byobu-writable-status.patch		\
   %D%/packages/patches/cairo-CVE-2016-9082.patch			\
   %D%/packages/patches/calibre-drop-unrar.patch			\
