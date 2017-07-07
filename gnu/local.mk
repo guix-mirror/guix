7f0980839e4d9d3c7e06301bcbb2eb292bd7dbee
Ludovic Courtès <ludo@gnu.org>
2017-07-07 16:34:10 +0200

    gnu: wget: Fix compilation with Perl 5.26; skip faulty tests.
    * gnu/packages/wget.scm (wget)[arguments]: New field.
    [source](patches): Add "wget-perl-5.26.patch".
    * gnu/packages/patches/wget-perl-5.26.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1028,6 +1028,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/weechat-python.patch			\
   %D%/packages/patches/wget-CVE-2017-6508.patch			\
   %D%/packages/patches/wget-fix-504-test-timeout.patch			\
+  %D%/packages/patches/wget-perl-5.26.patch			\
   %D%/packages/patches/wicd-bitrate-none-fix.patch		\
   %D%/packages/patches/wicd-get-selected-profile-fix.patch	\
   %D%/packages/patches/wicd-urwid-1.3.patch			\
