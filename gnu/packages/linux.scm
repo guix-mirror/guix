a6ae4f83a4ecdc7dc19e0060462e85dc56e0d105
Mark H Weaver <mhw@netris.org>
2017-07-13 13:48:47 -0400

    gnu: linux-libre: Update to 4.12.1.
    * gnu/packages/linux.scm (%linux-libre-version): Update to 4.12.1.
    (%linux-libre-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -363,8 +363,8 @@ It has been modified to remove all non-free binary blobs.")
 
 (define %intel-compatible-systems '("x86_64-linux" "i686-linux"))
 
-(define %linux-libre-version "4.12")
-(define %linux-libre-hash "153ibjdgys7friij4hnkvmp9ycvx50zgpyl9mwbr4prr409q3ndf")
+(define %linux-libre-version "4.12.1")
+(define %linux-libre-hash "1pgp48hf4zziv3nvzm54zvalh9y7kh5a7prrsw2n1qal9h1yslsp")
 
 (define-public linux-libre
   (make-linux-libre %linux-libre-version
