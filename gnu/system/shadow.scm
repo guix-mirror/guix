138fe4119881577249be7362b3ea3b1125837902
Ludovic Courtès <ludo@gnu.org>
2017-07-27 14:44:28 +0200

    system: Add 'grep --color' alias in '.bash_profile' skeleton.
    * gnu/system/shadow.scm (default-skeletons)[profile]: Add 'grep' alias.

--- gnu/system/shadow.scm
+++ gnu/system/shadow.scm
@@ -174,7 +174,8 @@ else
     PS1='\\u@\\h \\w\\$ '
 fi
 alias ls='ls -p --color'
-alias ll='ls -l'\n"))
+alias ll='ls -l'
+alias grep='grep --color'\n"))
         (zlogin    (plain-file "zlogin" "\
 # Honor system-wide environment variables
 source /etc/profile\n"))
