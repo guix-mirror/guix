e7ea30d59a22857e7a2992ef7bd099b8f88127f7
Ben Woodcroft <donttrustben@gmail.com>
2017-08-08 09:19:22 +1000

    ruby: Replace with ruby-2.4.1.
    * gnu/packages/ruby.scm (ruby-2.4.1): New variable.
    (ruby)[replacement]: New field.

--- gnu/packages/ruby.scm
+++ gnu/packages/ruby.scm
@@ -49,6 +49,7 @@
 (define-public ruby
   (package
     (name "ruby")
+    (replacement ruby-2.4.1)
     (version "2.4.0")
     (source
      (origin
@@ -101,6 +102,26 @@
 a focus on simplicity and productivity.")
     (home-page "https://ruby-lang.org")
     (license license:ruby)))
+
+(define-public ruby-2.4.1
+  (package
+    (inherit ruby)
+    (name "ruby")
+    (version "2.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "http://cache.ruby-lang.org/pub/ruby/"
+                           (version-major+minor version)
+                           "/ruby-" version ".tar.xz"))
+       (sha256
+        (base32
+         "0m763zf2v0jhrha3cx21g4dif6vc9gm714invs8h3sg35ncskj2g"))
+       (modules '((guix build utils)))
+       (snippet `(begin
+                   ;; Remove bundled libffi
+                   (delete-file-recursively "ext/fiddle/libffi-3.2.1")
+                   #t))))))
 
 (define-public ruby-2.3
   (package
