33ce12e2936bb7dadc1bc03677f06bfda5ef06a7
Ricardo Wurmus <rekado@elephly.net>
2017-08-14 17:56:07 +0200

    gnu: Add r-auc.
    * gnu/packages/cran.scm (r-auc): New variable.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -148,3 +148,27 @@ where the bound function accepts additional arguments.")
      "This package provides an easy way to fill an environment with active
 bindings that call a C++ function.")
     (license license:expat)))
+
+(define-public r-auc
+  (package
+    (name "r-auc")
+    (version "0.3.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "AUC" version))
+       (sha256
+        (base32
+         "0ripcib2qz0m7rgr1kiz68nx8f6p408l1ww7j78ljqik7p3g41g7"))))
+    (properties `((upstream-name . "AUC")))
+    (build-system r-build-system)
+    (home-page "http://cran.r-project.org/web/packages/AUC")
+    (synopsis "Compute the area under the curve of selected measures")
+    (description
+     "This package includes functions to compute the area under the curve of
+selected measures: the area under the sensitivity curve (AUSEC), the area
+under the specificity curve (AUSPC), the area under the accuracy
+curve (AUACC), and the area under the receiver operating characteristic
+curve (AUROC).  The curves can also be visualized.  Support for partial areas
+is provided.")
+    (license license:gpl2+)))
