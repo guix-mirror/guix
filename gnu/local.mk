d9f15d7e48bc7c361b8a88849542d9f5d7442126
Leo Famulari <leo@famulari.name>
2017-08-18 16:33:04 -0400

    gnu: newsbeuter: Fix CVE-2017-12904.
    * gnu/packages/patches/newsbeuter-CVE-2017-12904.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/syndication.scm (newsbeuter)[source]: Use it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -879,6 +879,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/netsurf-system-utf8proc.patch		\
   %D%/packages/patches/netsurf-y2038-tests.patch		\
   %D%/packages/patches/netsurf-longer-test-timeout.patch	\
+  %D%/packages/patches/newsbeuter-CVE-2017-12904.patch		\
   %D%/packages/patches/ngircd-handle-zombies.patch		\
   %D%/packages/patches/ninja-zero-mtime.patch			\
   %D%/packages/patches/node-9077.patch				\
