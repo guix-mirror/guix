19ecdbedd3e39dfecbc834d2f70dbbea64f7ca84
Leo Famulari <leo@famulari.name>
2017-09-01 13:47:49 -0400

    gnu: python-click-threading: Update to 0.4.3.
    * gnu/packages/python.scm (python-click-threading): Update to 0.4.3.
    [source]: Use pypi-uri.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -9198,16 +9198,13 @@ with python-requests.")
 (define-public python-click-threading
   (package
     (name "python-click-threading")
-    (version "0.2.0")
+    (version "0.4.3")
     (source (origin
              (method url-fetch)
-             (uri (string-append
-                    "https://pypi.python.org/packages/"
-                    "fe/b7/e7f609d18a2a351cb71616adcf54df1acd82f83cb9b5936935a4d20e2c23/"
-                    "click-threading-" version ".tar.gz"))
+             (uri (pypi-uri "click-threading" version))
              (sha256
               (base32
-               "18bcqikxwb3drb8rf60cclxkxw52521b38ax3byah6j8cn8y9p4j"))))
+               "0xs4bg2ws0zgyiplk312l049hi23c2zqf1g771rjhh5vr2msk4cg"))))
     (build-system python-build-system)
     (propagated-inputs
      `(("python-click" ,python-click)))
