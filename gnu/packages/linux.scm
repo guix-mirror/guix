3e51cd8c765d6fa4b3dc2a7e4c9aec335861e209
Mark H Weaver <mhw@netris.org>
2017-09-02 17:45:37 -0400

    gnu: linux-libre@4.4: Update to 4.4.86.
    * gnu/packages/linux.scm (linux-libre-4.4): Update to 4.4.86.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -383,8 +383,8 @@ It has been modified to remove all non-free binary blobs.")
                     #:configuration-file kernel-config))
 
 (define-public linux-libre-4.4
-  (make-linux-libre "4.4.85"
-                    "1ahcgz2531c0zmyyw54w5z2fzwlqqn68hl7dyy4hxdns4yx3irmz"
+  (make-linux-libre "4.4.86"
+                    "0zm283262k63c5sa3l2lg8lqdjmgzym60qf3kvfva21xqswzcpas"
                     %intel-compatible-systems
                     #:configuration-file kernel-config))
 
