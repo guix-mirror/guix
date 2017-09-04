1dc32c2961402c946c25e5a1aabe79a30cce3cff
Marius Bakke <mbakke@fastmail.com>
2017-09-04 22:15:49 +0200

    gnu: gtk+: Update to 3.22.20.
    * gnu/packages/gtk.scm (gtk+): Update to 3.22.20.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -653,7 +653,7 @@ application suites.")
    (name "gtk+")
    ;; NOTE: When updating the version of 'gtk+', the hash of 'mate-themes' in
    ;;       mate.scm will also need to be updated.
-   (version "3.22.19")
+   (version "3.22.20")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnome/sources/" name "/"
@@ -661,7 +661,7 @@ application suites.")
                                 name "-" version ".tar.xz"))
             (sha256
              (base32
-              "0v0nwpl6zp8bcjhdwivi1wfi83lha9yyhq76ihbb7ya4ab5d70wq"))
+              "1n82ki9sbdnaz6k5f5k3vgd7g4756j6acfbl1bf617w0lyc0kjbh"))
             (patches (search-patches "gtk3-respect-GUIX_GTK3_PATH.patch"
                                      "gtk3-respect-GUIX_GTK3_IM_MODULE_FILE.patch"))))
    (outputs '("out" "bin" "doc"))
