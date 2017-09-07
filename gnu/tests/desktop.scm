guix-mirror/guix

commit fa67d5654176b4b815832eaf259188e1486c65ab

gnu: elogind: Update to 232.3.
* gnu/packages/freedesktop.scm (elogind): Update to 232.3.
[home-page]: Use new upstream home page.
[arguments]: Disable tests, add new required configure flags; add build phase
"autogen".
[native-inputs]: Add autoconf, automake, libtool, and python.

gnu/packages/freedesktop.scm
@@ -204,14 +204,14 @@ the freedesktop.org XDG Base Directory specification.")
 (define-public elogind
   (package
     (name "elogind")
-    (version "219.14")
+    (version "232.3")
     (source (origin
               (method url-fetch)
-              (uri (string-append "https://wingolog.org/pub/" name "/"
-                                  name "-" version ".tar.xz"))
+              (uri (string-append "https://github.com/elogind/elogind/"
+                                  "archive/v" version ".tar.gz"))
               (sha256
                (base32
-                "1jckc4wx199n1q4r4fv43ibjs6nlq91s39w9r78ilk1z383m1hcx"))
+                "1g3bqzw5dhd5zaivqgbi13n5zy219qmkrk6vmbhfprs8qvyn843f"))
               (modules '((guix build utils)))
               (snippet
                '(begin
@@ -222,25 +222,41 @@ the freedesktop.org XDG Base Directory specification.")
                     (("XSLTPROC_FLAGS = ") "XSLTPROC_FLAGS = --novalid"))))))
     (build-system gnu-build-system)
     (arguments
-     `(#:configure-flags
+     `(#:tests? #f ;FIXME: "make check" in the "po" directory fails.
+       #:configure-flags
        (list (string-append "--with-libcap="
                             (assoc-ref %build-inputs "libcap"))
              (string-append "--with-udevrulesdir="
                             (assoc-ref %outputs "out")
-                            "/lib/udev/rules.d"))
+                            "/lib/udev/rules.d")
+             (string-append "--with-rootprefix="
+                            (assoc-ref %outputs "out"))
+             ;; These are needed to ensure that lto linking works.
+             "RANLIB=gcc-ranlib"
+             "AR=gcc-ar"
+             "NM=gcc-nm")
        #:make-flags '("PKTTYAGENT=/run/current-system/profile/bin/pkttyagent")
-       #:phases (modify-phases %standard-phases
-                  (add-before 'build 'fix-service-file
-                    (lambda* (#:key outputs #:allow-other-keys)
-                      ;; Fix the file name of the 'elogind' binary in the D-Bus
-                      ;; '.service' file.
-                      (substitute* "src/login/org.freedesktop.login1.service"
-                        (("^Exec=.*")
-                         (string-append "Exec=" (assoc-ref %outputs "out")
-                                        "/libexec/elogind/elogind\n"))))))))
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'configure 'autogen
+           (lambda _
+             (and (zero? (system* "intltoolize" "--force" "--automake"))
+                  (zero? (system* "autoreconf" "-vif")))))
+         (add-before 'build 'fix-service-file
+           (lambda* (#:key outputs #:allow-other-keys)
+             ;; Fix the file name of the 'elogind' binary in the D-Bus
+             ;; '.service' file.
+             (substitute* "src/login/org.freedesktop.login1.service"
+               (("^Exec=.*")
+                (string-append "Exec=" (assoc-ref %outputs "out")
+                               "/libexec/elogind/elogind\n"))))))))
     (native-inputs
-     `(("intltool" ,intltool)
+     `(("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("libtool" ,libtool)
+       ("intltool" ,intltool)
        ("gettext" ,gettext-minimal)
+       ("python" ,python)
        ("docbook-xsl" ,docbook-xsl)
        ("docbook-xml" ,docbook-xml)
        ("xsltproc" ,libxslt)
@@ -260,7 +276,7 @@ the freedesktop.org XDG Base Directory specification.")
        ("dbus" ,dbus)
        ("eudev" ,eudev)
        ("acl" ,acl)))           ;to add individual users to ACLs on /dev nodes
-    (home-page "https://github.com/wingo/elogind")
+    (home-page "https://github.com/elogind/elogind")
     (synopsis "User, seat, and session management service")
     (description "Elogind is the systemd project's \"logind\" service,
 extracted out as a separate project.  Elogind integrates with PAM to provide

commit cd1ad27e6cdf90230d07efc18a8fcfe45494aad0

gnu: elogind: Use itself as the cgroup controller.
Fixes a regression introduced in
fa67d5654176b4b815832eaf259188e1486c65ab whereby elogind would fail to
start with:

  Cannot determine cgroup we are running in: No data available
  Out of memory.

thereby preventing log-in altogether.

* gnu/packages/freedesktop.scm (elogind)[arguments]: Pass
"--with-cgroup-controller=elogind".
* gnu/tests/desktop.scm: New file.
* gnu/local.mk (GNU_SYSTEM_MODULES): Add it.

gnu/local.mk
@@ -487,6 +487,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/tests/audio.scm				\
   %D%/tests/base.scm				\
   %D%/tests/databases.scm			\
+  %D%/tests/desktop.scm				\
   %D%/tests/dict.scm				\
   %D%/tests/nfs.scm				\
   %D%/tests/install.scm				\

gnu/packages/freedesktop.scm
@@ -227,6 +227,12 @@ the freedesktop.org XDG Base Directory specification.")
        (list (string-append "--with-udevrulesdir="
                             (assoc-ref %outputs "out")
                             "/lib/udev/rules.d")
+
+             ;; Let elogind be its own cgroup controller, rather than relying
+             ;; on systemd or OpenRC.  By default, 'configure' makes an
+             ;; incorrect guess.
+             "--with-cgroup-controller=elogind"
+
              (string-append "--with-rootprefix="
                             (assoc-ref %outputs "out"))
              (string-append "--with-rootlibexecdir="

gnu/tests/desktop.scm
@@ -1,0 +1,105 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2017 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu tests desktop)
+  #:use-module (gnu tests)
+  #:use-module (gnu services)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu system vm)
+  #:use-module (guix gexp)
+  #:use-module (srfi srfi-1)
+  #:export (%test-elogind))
+
+
+;;;
+;;; Elogind.
+;;;
+
+(define (run-elogind-test vm)
+  (define test
+    (with-imported-modules '((gnu build marionette)
+                             (guix build syscalls))
+      #~(begin
+          (use-modules (gnu build marionette)
+                       (guix build syscalls)
+                       (srfi srfi-64))
+
+          (define marionette
+            (make-marionette '(#$vm)))
+
+          (mkdir #$output)
+          (chdir #$output)
+
+          (test-begin "elogind")
+
+          ;; Log in as root on tty1, and check what 'loginctl' returns.
+          (test-equal "login on tty1"
+            '(("c1" "0" "root" "seat0" "/dev/tty1") ;session
+              ("seat0")                             ;seat
+              ("0" "root"))                         ;user
+
+            (begin
+              ;; Wait for tty1.
+              (marionette-eval
+               '(begin
+                  (use-modules (gnu services herd))
+                  (start-service 'term-tty1))
+               marionette)
+              (marionette-control "sendkey ctrl-alt-f1" marionette)
+
+              ;; Now we can type.
+              (marionette-type "root\n" marionette)
+              (marionette-type "loginctl list-users --no-legend > users\n"
+                               marionette)
+              (marionette-type "loginctl list-seats --no-legend > seats\n"
+                               marionette)
+              (marionette-type "loginctl list-sessions --no-legend > sessions\n"
+                               marionette)
+
+
+              ;; Read the three files.
+              (marionette-eval '(use-modules (rnrs io ports)) marionette)
+              (let ((guest-file (lambda (file)
+                                  (string-tokenize
+                                   (wait-for-file file marionette
+                                                  #:read 'get-string-all)))))
+                (list (guest-file "/root/sessions")
+                      (guest-file "/root/seats")
+                      (guest-file "/root/users")))))
+
+          (test-end)
+          (exit (= (test-runner-fail-count (test-runner-current)) 0)))))
+
+  (gexp->derivation "elogind" test))
+
+(define %test-elogind
+  (system-test
+   (name "elogind")
+   (description
+    "Test whether we can log in when elogind is enabled, and whether
+'loginctl' reports accurate user, session, and seat information.")
+   (value
+    (let ((os (marionette-operating-system
+               (simple-operating-system
+                (service elogind-service-type)
+                (service polkit-service-type)
+                (service dbus-root-service-type))
+               #:imported-modules '((gnu services herd)
+                                    (guix combinators)))))
+      (run-elogind-test (virtual-machine os))))))