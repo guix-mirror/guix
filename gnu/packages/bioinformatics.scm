guix-mirror/guix

commit d10092b849153dc27cfed0a9601fde6c7bdec918

gnu: Put autoconf-related phases immediately after the 'unpack phase.
* gnu/packages/audio.scm (audacity, rtmidi)[arguments]: Correct phases
accordingly.
* gnu/packages/bioinformatics.scm (mash, seek, vsearch, emboss,
htslib-for-sambamba)[arguments]: Likewise.
* gnu/packages/ci.scm (cuirass)[arguments]: Likewise.
* gnu/packages/compression.scm (minizip, xdelta)[arguments]: Likewise.
* gnu/packages/cpp.scm (libzen)[arguments]: Likewise.
* gnu/packages/crypto.scm (opendht)[arguments]: Likewise.
* gnu/packages/databases.scm (4store, mdbtools)[arguments]: Likewise.
* gnu/packages/debug.scm (stress-make)[arguments]: Likewise.
* gnu/packages/dns.scm (dnscrypt-proxy, dnscrypt-wrapper)[arguments]:
Likewise.
* gnu/packages/emacs.scm (emacs-pdf-tools)[arguments]: Likewise.
* gnu/packages/embedded.scm (libjaylink, openocd)[arguments]: Likewise.
* gnu/packages/engineering.scm (gerbv)[arguments]: Likewise.
* gnu/packages/erlang.scm (erlang)[arguments]: Likewise.
* gnu/packages/ftp.scm (weex)[arguments]: Likewise.
* gnu/packages/gnome.scm (dia)[arguments]: Likewise.
* gnu/packages/gnunet.scm (gnurl, guile-gnunet)[arguments]: Likewise.
* gnu/packages/gtk.scm (guile-rsvg, graphene)[arguments]: Likewise.
* gnu/packages/guile.scm (guildhall, guile-ics, guile-sqlite3)[arguments]:
Likewise.
* gnu/packages/ibus.scm (ibus-libpinyin)[arguments]: Likewise.
* gnu/packages/irc.scm (weechat)[arguments]: Likewise.
* gnu/packages/java.scm (classpath-devel)[arguments]: Likewise.
* gnu/packages/libreoffice.scm (libetonyek)[arguments]: Likewise.
* gnu/packages/libusb.scm (hidapi)[arguments]: Likewise.
* gnu/packages/linux.scm (gpm)[arguments]: Likewise.
* gnu/packages/logging.scm (glog)[arguments]: Likewise.
* gnu/packages/machine-learning.scm (ghmm)[arguments]: Likewise.
* gnu/packages/mail.scm (dovecot-trees, dovecot-libsodium-plugin, esmtp)
[arguments]: Likewise.
* gnu/packages/messaging.scm (freetalk, libmesode, libstrophe)[arguments]:
Likewise.

gnu/packages/bioinformatics.scm
@@ -3450,7 +3450,7 @@ sequences).")
                (("^#include \"kseq\\.h\"")
                 "#include \"htslib/kseq.h\""))
              #t))
-         (add-before 'configure 'autoconf
+         (add-after 'fix-includes 'autoconf
            (lambda _ (zero? (system* "autoconf")))))))
     (native-inputs
      `(("autoconf" ,autoconf)
@@ -4486,7 +4486,7 @@ distribution, coverage uniformity, strand specificity, etc.")
                        "Data2DB"
                        "PCL2Bin")))
            (modify-phases %standard-phases
-             (add-before 'configure 'bootstrap
+             (add-after 'unpack 'bootstrap
                (lambda _
                  (zero? (system* "bash" "gen_auto"))))
              (add-after 'build 'build-additional-tools
@@ -6164,8 +6164,8 @@ track.  The database is exposed as a @code{TxDb} object.")
     (arguments
      `(#:phases
        (modify-phases %standard-phases
-         (add-before 'configure 'autogen
-                     (lambda _ (zero? (system* "autoreconf" "-vif")))))))
+         (add-after 'unpack 'autogen
+           (lambda _ (zero? (system* "autoreconf" "-vif")))))))
     (inputs
      `(("zlib" ,zlib)
        ("bzip2" ,bzip2)
@@ -8381,14 +8381,14 @@ used by @code{ensembldb}, @code{Organism.dplyr}, and other packages.")
 AC_DEFINE([PLD_png], [1], [Define to 1 if PNG support is available])
 AM_CONDITIONAL(AMPNG, true)"))
              #t))
-         (add-after 'unpack 'disable-update-check
+         (add-after 'fix-checks 'disable-update-check
            (lambda _
              ;; At build time there is no connection to the Internet, so
              ;; looking for updates will not work.
              (substitute* "Makefile.am"
                (("\\$\\(bindir\\)/embossupdate") ""))
              #t))
-         (add-before 'configure 'autogen
+         (add-after 'disable-update-check 'autogen
            (lambda _ (zero? (system* "autoreconf" "-vif")))))))
     (inputs
      `(("perl" ,perl)
@@ -9453,7 +9453,7 @@ problems in genomics, brain imaging, astrophysics, and data mining.")
        (substitute-keyword-arguments (package-arguments htslib)
          ((#:phases phases)
           `(modify-phases  ,phases
-             (add-before 'configure 'bootstrap
+             (add-after 'unpack 'bootstrap
                (lambda _
                  (zero? (system* "autoreconf" "-vif"))))))))
       (native-inputs

gnu/packages/ci.scm
@@ -216,7 +216,7 @@ their dependencies.")
                (substitute* "Makefile.am"
                  (("tests/repo.scm \\\\") "\\"))
                #t))
-           (add-before 'configure 'bootstrap
+           (add-after 'disable-repo-tests 'bootstrap
              (lambda _ (zero? (system* "sh" "bootstrap"))))
            (add-after 'install 'wrap-program
              (lambda* (#:key inputs outputs #:allow-other-keys)

gnu/packages/audio.scm
@@ -328,7 +328,7 @@ engineers, musicians, soundtrack editors and composers.")
        #:phases
        (modify-phases %standard-phases
          ;; FFmpeg is only detected if autoreconf runs.
-         (add-before 'configure 'autoreconf
+         (add-after 'unpack 'autoreconf
            (lambda _
              (zero? (system* "autoreconf" "-vfi")))))
        ;; The test suite is not "well exercised" according to the developers,
@@ -1979,8 +1979,8 @@ tempo and pitch of an audio recording independently of one another.")
     (arguments
      `(#:tests? #f ;no "check" target
        #:phases (modify-phases %standard-phases
-                  (add-before
-                   'configure 'autoconf
+                  (add-after
+                   'unpack 'autoconf
                    (lambda _ (zero? (system* "autoreconf" "-vfi"))))
                   (add-before
                    'build 'fix-makefile

gnu/packages/compression.scm
@@ -114,7 +114,7 @@ in compression.")
        (modify-phases %standard-phases
          (add-after 'unpack 'enter-source
            (lambda _ (chdir "contrib/minizip") #t))
-         (add-before 'configure 'autoreconf
+         (add-after 'enter-source 'autoreconf
            (lambda _
              (zero? (system* "autoreconf" "-vif")))))))
     (native-inputs
@@ -976,7 +976,7 @@ respectively, based on the reference implementation from Google.")
        (modify-phases %standard-phases
          (add-after 'unpack 'enter-build-directory
            (lambda _ (chdir "xdelta3")))
-         (add-before 'configure 'autoconf
+         (add-after 'enter-build-directory 'autoconf
            (lambda _ (zero? (system* "autoreconf" "-vfi")))))))
     (home-page "http://xdelta.com")
     (synopsis "Delta encoder for binary files")

gnu/packages/cpp.scm
@@ -45,12 +45,12 @@
      '(#:phases
        ;; build scripts not in root of archive
        (modify-phases %standard-phases
-         (add-before 'configure 'pre-configure
+         (add-after 'unpack 'pre-configure
            (lambda _
              (chdir "Project/GNU/Library")))
-         (add-before 'configure 'autogen
+         (add-after 'pre-configure 'autogen
            (lambda _
-             (zero? (system* "./autogen.sh")))))))
+             (zero? (system* "sh" "autogen.sh")))))))
     (home-page "https://github.com/MediaArea/ZenLib")
     (synopsis "C++ utility library")
     (description "ZenLib is a C++ utility library.  It includes classes for handling

gnu/packages/crypto.scm
@@ -161,7 +161,7 @@ OpenBSD tool of the same name.")
     (arguments
      `(#:configure-flags '("--disable-tools" "--disable-python")
        #:phases (modify-phases %standard-phases
-                  (add-before 'configure 'autoconf
+                  (add-after 'unpack 'autoconf
                     (lambda _
                       (zero? (system* "autoreconf" "-vfi")))))))
     (home-page "https://github.com/savoirfairelinux/opendht/")

gnu/packages/databases.scm
@@ -115,9 +115,9 @@
     (arguments
      `(#:phases
        (modify-phases %standard-phases
-         (add-before 'configure 'generate-configure
+         (add-after 'unpack 'generate-configure
            (lambda _
-             (zero? (system* "./autogen.sh")))))))
+             (zero? (system* "sh" "autogen.sh")))))))
     ;; http://www.4store.org has been down for a while now.
     (home-page "https://github.com/garlik/4store")
     (synopsis "Clustered RDF storage and query engine")
@@ -1563,7 +1563,7 @@ for ODBC.")
     (arguments
      `(#:phases
        (modify-phases %standard-phases
-         (add-before 'configure 'autoreconf
+         (add-after 'unpack 'autoreconf
            (lambda _
              (zero? (system* "autoreconf" "-vfi")))))))
     (home-page "http://mdbtools.sourceforge.net/")

gnu/packages/debug.scm
@@ -319,7 +319,7 @@ down the road.")
              (add-before 'configure 'repack-make
                (lambda _
                  (zero? (system* "tar" "cJf" "./make.tar.xz" ,make-dir))))
-             (add-before 'configure 'bootstrap
+             (add-after 'unpack 'bootstrap
                (lambda _
                  (zero? (system* "autoreconf" "-vfi"))))))))
       (home-page "https://github.com/losalamos/stress-make")

commit 4561498e1f207e83ecc4a9fe1a08935c2807160f

gnu: Fix build failures caused by commit d10092b849153dc27cfed0a9601fde6c7bdec918.
* gnu/packages/bioinformatics.scm (seek)[arguments]: Restore previous phase
order.
* gnu/packages/ci.scm (cuirass)[arguments]: Add patch-/bin/sh phase.

gnu/packages/bioinformatics.scm
@@ -4486,7 +4486,7 @@ distribution, coverage uniformity, strand specificity, etc.")
                        "Data2DB"
                        "PCL2Bin")))
            (modify-phases %standard-phases
-             (add-after 'unpack 'bootstrap
+             (add-before 'configure 'bootstrap
                (lambda _
                  (zero? (system* "bash" "gen_auto"))))
              (add-after 'build 'build-additional-tools

gnu/packages/ci.scm
@@ -216,7 +216,12 @@ their dependencies.")
                (substitute* "Makefile.am"
                  (("tests/repo.scm \\\\") "\\"))
                #t))
-           (add-after 'disable-repo-tests 'bootstrap
+           (add-after 'disable-repo-tests 'patch-/bin/sh
+             (lambda _
+               (substitute* "build-aux/git-version-gen"
+                 (("#!/bin/sh") (string-append "#!" (which "sh"))))
+               #t))
+           (add-after 'patch-/bin/sh 'bootstrap
              (lambda _ (zero? (system* "sh" "bootstrap"))))
            (add-after 'install 'wrap-program
              (lambda* (#:key inputs outputs #:allow-other-keys)