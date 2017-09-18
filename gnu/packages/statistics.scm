b0c8cba485f1a0b39956363ff51a884174feaa7b
Ricardo Wurmus <rekado@elephly.net>
2017-09-18 14:20:23 +0200

    gnu: r-crayon: Update to 1.3.4.
    * gnu/packages/statistics.scm (r-crayon): Update to 1.3.4.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -1424,13 +1424,13 @@ you call it again with the same arguments it returns the pre-computed value.")
 (define-public r-crayon
   (package
     (name "r-crayon")
-    (version "1.3.2")
+    (version "1.3.4")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "crayon" version))
               (sha256
                (base32
-                "0s2yam18slph7xsw4pyc9f92gdyf609r5w92yax69zh57kb7asws"))))
+                "0s7s6vc3ww8pzanpjisym4jjvwcc5pi2qg8srx7jqlz9j3wrnvpw"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-memoise" ,r-memoise)))
