9f3b27a267ed76ecba33288b86d036e5fd0e8797
Feng Shu <tumashu@163.com>
2017-09-23 11:56:22 +0800

    gnu: Add emacs-pos-tip.
    * gnu/packages/emacs.scm (emacs-pos-tip): New variable.
    
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/emacs.scm
+++ gnu/packages/emacs.scm
@@ -4886,6 +4886,32 @@ constructs.")
 configuration of Chinese fonts.")
     (license license:gpl2+)))
 
+(define-public emacs-pos-tip
+  (package
+    (name "emacs-pos-tip")
+    (version "0.4.6")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/pitkali/pos-tip/archive/"
+                    version ".tar.gz"))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "12jqfy26vjk7lq0aa8yn8zqj8c85fkvx7y9prj0pcn4wqiz2ad2r"))))
+    (build-system emacs-build-system)
+    ;; The following functions and variables needed by emacs-pos-tip are
+    ;; not included in emacs-minimal:
+    ;; x-display-pixel-width, x-display-pixel-height, x-show-tip
+    (arguments `(#:emacs ,emacs))
+    (home-page "https://github.com/pitkali/pos-tip")
+    (synopsis "Show tooltip at point")
+    (description "The standard library tooltip.el provides a function for
+displaying a tooltip at the mouse position.  However, locating a tooltip at an
+arbitrary buffer position in a window is not easy.  Pos-tip provides such a
+function to be used by other frontend programs.")
+    (license license:gpl2+)))
+
 (define-public emacs-pyim-basedict
   (package
     (name "emacs-pyim-basedict")
