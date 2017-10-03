f473b8f1f7db695dce1ed5b145be501424e76b34
Efraim Flashner <efraim@flashner.co.il>
2017-10-03 22:46:40 +0300

    gnu: clisp: Update to 2.49-60.
    * gnu/packages/lisp.scm (clisp): Update to 2.49-60.
    [source]: Download mercurial repository, apply patch.
    [inputs]: Replace readline@6.2 with readline, add ncurses.
    [arguments]: Add multiple configure flags. Remove build phase to build
    in "src" directory. Remove more uses of a timestamp.

--- gnu/local.mk
+++ gnu/local.mk
@@ -560,6 +560,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/chmlib-inttypes.patch			\
   %D%/packages/patches/clang-libc-search-path.patch		\
   %D%/packages/patches/clang-3.8-libc-search-path.patch		\
+  %D%/packages/patches/clisp-remove-failing-test.patch		\
   %D%/packages/patches/clucene-pkgconfig.patch			\
   %D%/packages/patches/clx-remove-demo.patch			\
   %D%/packages/patches/cmake-fix-tests.patch			\
