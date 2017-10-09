bd6d0eba67d00f1c267d8d8592c3395a2a8db82f
Marius Bakke <mbakke@fastmail.com>
2017-10-09 21:23:08 +0200

    gnu: sddm: Update to 0.16.0.
    * gnu/packages/display-managers.scm (sddm): Update to 0.16.0.
    [source]: Switch to '.xz' tarball.

--- gnu/packages/display-managers.scm
+++ gnu/packages/display-managers.scm
@@ -132,16 +132,16 @@ Qt-style API for Wayland clients.")
 (define-public sddm
   (package
     (name "sddm")
-    (version "0.15.0")
+    (version "0.16.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
                     "https://github.com/sddm/sddm"
                     "/releases/download/v" version "/"
-                    "sddm-" version ".tar.gz"))
+                    "sddm-" version ".tar.xz"))
               (sha256
                (base32
-                "0x1igkjm3k8q26xbmg0qah1fc2pn2sfc675w0xg42x7ncrdiw8d4"))))
+                "0fwf1wsdak5yglykfyq4wbx9g9gi079n8ncjrdynz17hwwiql4z9"))))
     (build-system cmake-build-system)
     (native-inputs
      `(("extra-cmake-modules" ,extra-cmake-modules)
