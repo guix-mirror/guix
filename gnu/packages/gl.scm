2a087882b1cb2e40e7b63580c675b58a5cfa1b96
Marius Bakke <mbakke@fastmail.com>
2017-10-12 18:57:30 +0200

    gnu: mesa: Disable imx driver for armhf-linux.
    * gnu/packages/gl.scm (mesa)<#:configure-flags>: Don't build imx driver.

--- gnu/packages/gl.scm
+++ gnu/packages/gl.scm
@@ -271,8 +271,8 @@ also known as DXTn or DXTC) for Mesa.")
      `(#:configure-flags
        '(,@(match (%current-system)
              ("armhf-linux"
-              ;; TODO: Add etnaviv when enabled in libdrm.
-              '("--with-gallium-drivers=freedreno,imx,nouveau,r300,r600,svga,swrast,vc4,virgl"))
+              ;; TODO: Add etnaviv,imx when libdrm supports etnaviv.
+              '("--with-gallium-drivers=freedreno,nouveau,r300,r600,svga,swrast,vc4,virgl"))
              ("aarch64-linux"
               ;; TODO: Fix svga driver for aarch64.
               '("--with-gallium-drivers=freedreno,nouveau,r300,r600,swrast,vc4,virgl"))
