e13a63d9334bec937c6c28df1224268c21fd4325
Tobias Geerinckx-Rice <me@tobias.gr>
2017-10-28 01:54:09 +0200

    gnu: perl-xml-libxml: Update to 2.0131.
    * gnu/packages/xml.scm (perl-xml-libxml): Update to 2.0131.

--- gnu/packages/xml.scm
+++ gnu/packages/xml.scm
@@ -392,7 +392,7 @@ module.")
 (define-public perl-xml-libxml
   (package
     (name "perl-xml-libxml")
-    (version "2.0128")
+    (version "2.0131")
     (source
      (origin
        (method url-fetch)
@@ -400,7 +400,7 @@ module.")
                            "XML-LibXML-" version ".tar.gz"))
        (sha256
         (base32
-         "0awgd2gjzy7kn38bqblsigikzl81xsi561phkz9f9b9v3x2vmrr6"))))
+         "1pqrpq5wixnwja7n8gnsskfdr0cvyzaa3qs02072g7szlpg7fk5h"))))
     (build-system perl-build-system)
     (propagated-inputs
      `(("perl-xml-namespacesupport" ,perl-xml-namespacesupport)
