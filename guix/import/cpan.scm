guix-mirror/guix

commit e4bc1727302b0e1e255ea5cf4e2ccf33cafe7296

import: cpan: Add trailing "/" on home-page.
This appeases 'guix lint', which otherwise complains about permanent
redirects.

* guix/import/cpan.scm (cpan-module->sexp): Add trailing "/" on home-page.
* tests/cpan.scm ("cpan->guix-package"): Adjust accordingly.

guix/import/cpan.scm
@@ -244,7 +244,7 @@ META."
                        (convert-inputs '("configure" "build" "test")))
        ,@(maybe-inputs 'propagated-inputs
                        (convert-inputs '("runtime")))
-       (home-page ,(string-append "http://search.cpan.org/dist/" name))
+       (home-page ,(string-append "http://search.cpan.org/dist/" name "/"))
        (synopsis ,(assoc-ref meta "abstract"))
        (description fill-in-yourself!)
        (license ,(string->license (assoc-ref meta "license"))))))

tests/cpan.scm
@@ -97,7 +97,7 @@
                    ('propagated-inputs
                     ('quasiquote
                      (("perl-test-script" ('unquote 'perl-test-script)))))
-                   ('home-page "http://search.cpan.org/dist/Foo-Bar")
+                   ('home-page "http://search.cpan.org/dist/Foo-Bar/")
                    ('synopsis "Fizzle Fuzz")
                    ('description 'fill-in-yourself!)
                    ('license 'perl-license))

commit 73f33b93797383a356133a2e871af9c07f5b5376

import: cpan: Add trailing "/" to CPAN-HOME.
Prevent regression after commit e4bc1727302b0e1e255ea5cf4e2ccf33cafe7296.

* guix/import/cpan.scm (cpan-home): Add trailing "/".

guix/import/cpan.scm
@@ -115,7 +115,7 @@ or #f on failure.  MODULE should be e.g. \"Test::Script\""
   (json-fetch (string-append "https://fastapi.metacpan.org/v1/release/" name)))
 
 (define (cpan-home name)
-  (string-append "http://search.cpan.org/dist/" name))
+  (string-append "http://search.cpan.org/dist/" name "/"))
 
 (define (cpan-source-url meta)
   "Return the download URL for a module's source tarball."