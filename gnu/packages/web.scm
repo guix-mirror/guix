d284f01826f1b5dcd1584110c5ba7fa6f76ed3e4
Tobias Geerinckx-Rice <me@tobias.gr>
2017-10-30 13:57:30 +0100

    gnu: perl-http-cookies: Update to 6.04.
    * gnu/packages/perl.scm (perl-http-cookies): Update to 6.04.

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -2586,7 +2586,7 @@ jar in conformance with RFC 6265 <http://tools.ietf.org/html/rfc6265>.")
 (define-public perl-http-cookies
   (package
     (name "perl-http-cookies")
-    (version "6.01")
+    (version "6.04")
     (source (origin
              (method url-fetch)
              (uri (string-append
@@ -2594,7 +2594,7 @@ jar in conformance with RFC 6265 <http://tools.ietf.org/html/rfc6265>.")
                    version ".tar.gz"))
              (sha256
               (base32
-               "087bqmg22dg3vj7gssh3pcsh9y1scimkbl5h1kc8jqyfhgisvlzm"))))
+               "1m0kxcirbvbkrm2c59p1bkbvzlcdymg8fdpa7wlxijlx0xwz1iqc"))))
     (build-system perl-build-system)
     (propagated-inputs
      `(("perl-http-message" ,perl-http-message)))
