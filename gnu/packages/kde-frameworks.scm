a7dc8557257a21cf9bd35f33b0721fe50f7cafef
Hartmut Goebel <h.goebel@crazy-compilers.com>
2017-11-01 17:00:12 +0100

    gnu: kdewebkit: Move to be in alphabetical order.
    I was trapped by the definitions not being in alphabetical order.
    
    * gnu/packages/kde-frameworks.scm (kdewebkit): Move in front of
      kemoticons.

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -2240,6 +2240,38 @@ ini-style description files.")
 with su and ssh respectively.")
     (license license:lgpl2.1+)))
 
+(define-public kdewebkit
+  (package
+    (name "kdewebkit")
+    (version "5.39.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://kde/stable/frameworks/"
+                    (version-major+minor version) "/"
+                    name "-" version ".tar.xz"))
+              (sha256
+               (base32
+                "05f0l3zw8cnvyavwwijwy5jx8dairhdbvg6ynpn7r5l10vawcf6x"))))
+    (build-system cmake-build-system)
+    (native-inputs
+     `(("extra-cmake-modules" ,extra-cmake-modules)))
+    (inputs
+     `(("kconfig" ,kconfig)
+       ("kcoreaddons" ,kcoreaddons)
+       ("kio" ,kio)
+       ("kjobwidgets" ,kjobwidgets)
+       ("kparts" ,kparts)
+       ("kservice" ,kservice)
+       ("kwallet" ,kwallet)
+       ("qtbase" ,qtbase)
+       ("qtwebkit" ,qtwebkit)))
+    (home-page "https://community.kde.org/Frameworks")
+    (synopsis "KDE Integration for QtWebKit")
+    (description "This library provides KDE integration of the HTML rendering
+engine WebKit via QtWebKit.")
+    (license license:lgpl2.1+)))
+
 (define-public kemoticons
   (package
     (name "kemoticons")
@@ -2988,38 +3020,6 @@ It supports rich text as well as plain text.")
     (description "This framework contains an interface to KWallet, a safe
 desktop-wide storage for passwords and the kwalletd daemon used to safely store
 the passwords on KDE work spaces.")
-    (license license:lgpl2.1+)))
-
-(define-public kdewebkit
-  (package
-    (name "kdewebkit")
-    (version "5.39.0")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "mirror://kde/stable/frameworks/"
-                    (version-major+minor version) "/"
-                    name "-" version ".tar.xz"))
-              (sha256
-               (base32
-                "05f0l3zw8cnvyavwwijwy5jx8dairhdbvg6ynpn7r5l10vawcf6x"))))
-    (build-system cmake-build-system)
-    (native-inputs
-     `(("extra-cmake-modules" ,extra-cmake-modules)))
-    (inputs
-     `(("kconfig" ,kconfig)
-       ("kcoreaddons" ,kcoreaddons)
-       ("kio" ,kio)
-       ("kjobwidgets" ,kjobwidgets)
-       ("kparts" ,kparts)
-       ("kservice" ,kservice)
-       ("kwallet" ,kwallet)
-       ("qtbase" ,qtbase)
-       ("qtwebkit" ,qtwebkit)))
-    (home-page "https://community.kde.org/Frameworks")
-    (synopsis "KDE Integration for QtWebKit")
-    (description "This library provides KDE integration of the HTML rendering
-engine WebKit via QtWebKit.")
     (license license:lgpl2.1+)))
 
 (define-public kxmlgui
