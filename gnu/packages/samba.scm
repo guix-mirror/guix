ad25310dd517590add51fd4e0511f785cfe08aea
Marius Bakke <mbakke@fastmail.com>
2017-11-03 23:11:54 +0100

    gnu: ldb: Update to 1.3.0.
    * gnu/packages/samba.scm (ldb): Update to 1.3.0.

--- gnu/packages/samba.scm
+++ gnu/packages/samba.scm
@@ -346,14 +346,14 @@ many event types, including timers, signals, and the classic file descriptor eve
 (define-public ldb
   (package
     (name "ldb")
-    (version "1.1.31")
+    (version "1.3.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.samba.org/ftp/ldb/ldb-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "0ipbz9m50dkancq0dbz12q815nkknbsp2i3sxpsqhmmknlm3xm84"))))
+                "03arsnsbkxb2d811pbarb7d12yg8g05f1q576z48sp647dd3xda4"))))
     (build-system gnu-build-system)
     (arguments
      '(#:phases
