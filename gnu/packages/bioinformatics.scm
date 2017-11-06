9f52e53d5db261d86aa19aed128957c94d1f6fca
Ricardo Wurmus <rekado@elephly.net>
2017-11-06 21:33:37 +0100

    gnu: r-gseabase: Update to 1.40.0.
    * gnu/packages/bioinformatics.scm (r-gseabase): Update to 1.40.0.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -5884,14 +5884,14 @@ the graph algorithms contained in the Boost library.")
 (define-public r-gseabase
   (package
     (name "r-gseabase")
-    (version "1.38.0")
+    (version "1.40.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "GSEABase" version))
        (sha256
         (base32
-         "1c6i6g4fj3b8wjyxyygr7i3v8sxrq1ffb2bbicya5ah2gdaclfad"))))
+         "0kpkl6c5lrar6ip7wlhvd5axqlb9lb5l3lgbdb3dlih32c3nz0yq"))))
     (properties `((upstream-name . "GSEABase")))
     (build-system r-build-system)
     (propagated-inputs
