8d688cd27f267f4d143d0f20ed3c352188151302
Tobias Geerinckx-Rice <me@tobias.gr>
2017-11-10 20:13:46 +0100

    gnu: python-sqlparse: Update to 0.2.4.
    * gnu/packages/python.scm (python-sqlparse, python2-sqlparse): Update to
    0.2.4.
    [arguments]: Don't invoke ‘2to3’.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -11761,25 +11761,19 @@ multiple processes (imagine multiprocessing, billiard, futures, celery etc).
 (define-public python-sqlparse
   (package
     (name "python-sqlparse")
-    (version "0.1.19")
+    (version "0.2.4")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "sqlparse" version))
               (sha256
                (base32
-                "1s2fvaxgh9kqzrd6iwy5h7i61ckn05plx9np13zby93z3hdbx5nq"))))
+                "1v3xh0bkfhb262dbndgzhivpnhdwavdzz8jjhx9vx0xbrx2880nf"))))
     (build-system python-build-system)
     (arguments
      `(#:phases
        (modify-phases %standard-phases
          (replace 'check
-           (lambda* _
-             ;; setup.py-integrated 2to3 only affects the build files, but
-             ;; py.test is using the source files. So we need to convert them
-             ;; manually.
-             (when (zero? (system* "python3"))
-               (system* "2to3" "--no-diff" "-wn" "sqlparse" "tests"))
-             (zero? (system* "py.test")))))))
+           (zero? (system* "py.test")))))))
     (native-inputs
      `(("python-pytest" ,python-pytest)))
     (home-page "https://github.com/andialbrecht/sqlparse")
