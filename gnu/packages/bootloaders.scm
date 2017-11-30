guix-mirror/guix

commit 63087721bb54a14dc04ec424474386ae98f7771b

gnu: grub :Disable tests on aarch64.
* gnu/packages/bootloaders.scm (grub)[arguments]: Add aarch64 to the
architectures where the test suite is not run.

gnu/packages/bootloaders.scm
@@ -50,7 +50,9 @@
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26))
 
 (define unifont
   ;; GNU Unifont, <http://gnu.org/s/unifont>.
@@ -103,9 +105,10 @@
                         (("grub_cmd_date grub_cmd_set_date grub_cmd_sleep")
                           "grub_cmd_date grub_cmd_sleep"))
                       #t)))
-       ;; Disable tests on ARM platforms.
-       #:tests? ,(not (string-prefix? "arm" (or (%current-target-system)
-                                                (%current-system))))))
+       ;; Disable tests on ARM and AARCH64 platforms.
+       #:tests? ,(not (any (cute string-prefix? <> (or (%current-target-system)
+                                                       (%current-system)))
+                           '("arm" "aarch64")))))
     (inputs
      `(("gettext" ,gettext-minimal)
 

commit 46ff2dbb4ae6f85554aaf2a16c2e6dc2edf244da

gnu: grub-efi: Disable tests on all platforms.
This is a follow-up to 56f0feb6781e3680541d7adc21d6b3b4746a33ca
and 63087721bb54a14dc04ec424474386ae98f7771b, which accidentally
enabled tests for grub-efi on other platforms.

* gnu/packages/bootloaders.scm (grub-efi)[arguments]: Override inherited
tests field and disable on all platforms.

gnu/packages/bootloaders.scm
@@ -164,8 +164,8 @@ menu to select one of the installed operating systems.")
      `(;; TODO: Tests need a UEFI firmware for qemu. There is one at
        ;; https://github.com/tianocore/edk2/tree/master/OvmfPkg .
        ;; Search for 'OVMF' in "tests/util/grub-shell.in".
-       #:tests? #f
        ,@(substitute-keyword-arguments (package-arguments grub)
+           ((#:tests? _ #f) #f)
            ((#:configure-flags flags ''())
             `(cons "--with-platform=efi" ,flags))
            ((#:phases phases)