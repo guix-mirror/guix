1776d5cd184402895ec420d916a7171a2052c473
Mark H Weaver <mhw@netris.org>
2017-12-01 01:37:08 -0500

    gnu: linux-libre: Update to 4.14.3.
    * gnu/packages/linux.scm (%linux-libre-version): Update to 4.14.3.
    (%linux-libre-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -369,8 +369,8 @@ It has been modified to remove all non-free binary blobs.")
 (define %intel-compatible-systems '("x86_64-linux" "i686-linux"))
 (define %linux-compatible-systems '("x86_64-linux" "i686-linux" "armhf-linux"))
 
-(define %linux-libre-version "4.14.2")
-(define %linux-libre-hash "10bbajas0xk4xvyvyx51dpc7xc5qjxz2nsf361gl1yfsrb6mbkhw")
+(define %linux-libre-version "4.14.3")
+(define %linux-libre-hash "1hs94lj2bryci4m75bfrhhj9aqrjpq6a57nhxic63zj6xjhi53l7")
 
 ;; linux-libre configuration for armhf-linux is derived from Debian armmp.  It
 ;; supports qemu "virt" machine and possibly a large number of ARM boards.
