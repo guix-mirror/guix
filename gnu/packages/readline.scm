a850cbdd43a5447733f902fc26452a528038545d
Marius Bakke <mbakke@fastmail.com>
2017-12-03 17:47:18 +0100

    gnu: readline: Remove obsolete post-install phase.
    The 'strip' phase does this unconditionally now.
    
    * gnu/packages/readline.scm (readline)[arguments]<#:phases>: Remove.

--- gnu/packages/readline.scm
+++ gnu/packages/readline.scm
@@ -50,68 +50,53 @@
    (3 "1027kmymniizcy0zbdlrczxfx3clxcdln5yq05q9yzlc6y9slhwy")))
 
 (define-public readline
-  (let ((post-install-phase
-         '(lambda* (#:key outputs #:allow-other-keys)
-            (let* ((out (assoc-ref outputs "out"))
-                   (lib (string-append out "/lib")))
-              ;; Make libraries writable so that `strip' can work.
-              ;; Failing to do that, it bails out with "Permission
-              ;; denied".
-              (for-each (lambda (f) (chmod f #o755))
-                        (find-files lib "\\.so"))
-              (for-each (lambda (f) (chmod f #o644))
-                        (find-files lib "\\.a"))))))
-    (package
-      (name "readline")
-      (version (string-append "7.0."
-                              (number->string (length %patch-series-7.0))))
-      (source (origin
-               (method url-fetch)
-               (uri (string-append "mirror://gnu/readline/readline-"
-                                   (version-major+minor version) ".tar.gz"))
-               (sha256
-                (base32
-                 "0d13sg9ksf982rrrmv5mb6a2p4ys9rvg9r71d6il0vr8hmql63bm"))
-               (patches (append
-                         %patch-series-7.0
-                         (search-patches "readline-link-ncurses.patch")))
-               (patch-flags '("-p0"))))
-      (build-system gnu-build-system)
-      (propagated-inputs `(("ncurses" ,ncurses)))
-      (arguments `(#:configure-flags
-                   (list (string-append "LDFLAGS=-Wl,-rpath -Wl,"
-                                        (assoc-ref %build-inputs "ncurses")
-                                        "/lib")
+  (package
+    (name "readline")
+    (version (string-append "7.0."
+                            (number->string (length %patch-series-7.0))))
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/readline/readline-"
+                                  (version-major+minor version) ".tar.gz"))
+              (sha256
+               (base32
+                "0d13sg9ksf982rrrmv5mb6a2p4ys9rvg9r71d6il0vr8hmql63bm"))
+              (patches (append
+                        %patch-series-7.0
+                        (search-patches "readline-link-ncurses.patch")))
+              (patch-flags '("-p0"))))
+    (build-system gnu-build-system)
+    (propagated-inputs `(("ncurses" ,ncurses)))
+    (arguments `(#:configure-flags
+                 (list (string-append "LDFLAGS=-Wl,-rpath -Wl,"
+                                      (assoc-ref %build-inputs "ncurses")
+                                      "/lib")
 
-                         ;; This test does an 'AC_TRY_RUN', which aborts when
-                         ;; cross-compiling, so provide the correct answer.
-                         ,@(if (%current-target-system)
-                               '("bash_cv_wcwidth_broken=no")
-                               '())
-                         ;; MinGW: ncurses provides the termcap api.
-                         ,@(if (target-mingw?)
-                               '("bash_cv_termcap_lib=ncurses")
-                               '()))
+                       ;; This test does an 'AC_TRY_RUN', which aborts when
+                       ;; cross-compiling, so provide the correct answer.
+                       ,@(if (%current-target-system)
+                             '("bash_cv_wcwidth_broken=no")
+                             '())
+                       ;; MinGW: ncurses provides the termcap api.
+                       ,@(if (target-mingw?)
+                             '("bash_cv_termcap_lib=ncurses")
+                             '()))
 
-                   ,@(if (target-mingw?)
-                         ;; MinGW: termcap in ncurses
-                         ;; some SIG_* #defined in _POSIX
-                         '(#:make-flags '("TERMCAP_LIB=-lncurses"
-                                          "CPPFLAGS=-D_POSIX -D'chown(f,o,g)=0'"))
-                         '())
-                   #:phases (alist-cons-after
-                             'install 'post-install
-                             ,post-install-phase
-                             %standard-phases)))
-      (synopsis "Edit command lines while typing, with history support")
-      (description
-       "The GNU readline library allows users to edit command lines as they
+                 ,@(if (target-mingw?)
+                       ;; MinGW: termcap in ncurses
+                       ;; some SIG_* #defined in _POSIX
+                       '(#:make-flags '("TERMCAP_LIB=-lncurses"
+                                        "CPPFLAGS=-D_POSIX -D'chown(f,o,g)=0'"))
+                       '())))
+    (synopsis "Edit command lines while typing, with history support")
+    (description
+     "The GNU readline library allows users to edit command lines as they
 are typed in.  It can maintain a searchable history of previously entered
 commands, letting you easily recall, edit and re-enter past commands.  It
 features both Emacs-like and vi-like keybindings, making its usage
 comfortable for anyone.")
-      (license gpl3+)
-      (home-page "https://savannah.gnu.org/projects/readline/"))))
+    (license gpl3+)
+    (home-page "https://savannah.gnu.org/projects/readline/")))
 
 (define-public readline-6.2
   (package (inherit readline)
