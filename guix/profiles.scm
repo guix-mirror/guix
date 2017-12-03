guix-mirror/guix

commit 2f60084f77815f454d1521396c2a383390ea2865

profiles: Avoid _IO* in profile builder.
* guix/profiles.scm (profile-derivation)[builder]: Avoid the deprecated
_IO* constants.

guix/profiles.scm
@@ -1268,8 +1268,8 @@ are cross-built for TARGET."
                          (guix search-paths)
                          (srfi srfi-1))
 
-            (setvbuf (current-output-port) _IOLBF)
-            (setvbuf (current-error-port) _IOLBF)
+            (setvbuf (current-output-port) 'line)
+            (setvbuf (current-error-port) 'line)
 
             #+(if locales? set-utf8-locale #t)
 

commit 2815fca1423cf72e6f3d0e774f1058bcbf8dfdbf

profile: Use _IO* but disable deprecation warning.
This fixes a regression introduced in
2f60084f77815f454d1521396c2a383390ea2865, whereby the profile derivation
would fail to run on Guile 2.0 (as is the case with "guix package
--bootstrap").

Reported by Christopher Baines.

* guix/profiles.scm (profile-derivation)[builder]: Use _IO* but add
'debug-disable' call.

guix/profiles.scm
@@ -1268,8 +1268,11 @@ are cross-built for TARGET."
                          (guix search-paths)
                          (srfi srfi-1))
 
-            (setvbuf (current-output-port) 'line)
-            (setvbuf (current-error-port) 'line)
+            ;; Don't complain about _IO* on Guile 2.2.
+            (debug-disable 'warn-deprecated)
+
+            (setvbuf (current-output-port) _IOLBF)
+            (setvbuf (current-error-port) _IOLBF)
 
             #+(if locales? set-utf8-locale #t)
 