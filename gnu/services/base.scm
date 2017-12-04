be5622e7f1b2f4e4567214b209bfd153dfd59013
Ludovic Courtès <ludo@gnu.org>
2017-12-04 17:57:37 +0100

    maint: Add 'berlin.guixsd.org.pub'.
    * bayfront.guixsd.org.pub: Rename to...
    * berlin.guixsd.org.pub: ... this.
    * Makefile.am (dist_pkgdata_DATA): Adjust accordingly.
    * gnu/services/base.scm (%default-authorized-guix-keys): Likewise.

--- gnu/services/base.scm
+++ gnu/services/base.scm
@@ -1345,7 +1345,7 @@ failed to register hydra.gnu.org public key: ~a~%" status))))))))
 (define %default-authorized-guix-keys
   ;; List of authorized substitute keys.
   (list (file-append guix "/share/guix/hydra.gnu.org.pub")
-        (file-append guix "/share/guix/bayfront.guixsd.org.pub")))
+        (file-append guix "/share/guix/berlin.guixsd.org.pub")))
 
 (define-record-type* <guix-configuration>
   guix-configuration make-guix-configuration
