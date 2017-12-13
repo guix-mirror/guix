ce0be1bcf4a45ef278ee4240fa793b05e09d7b4f
Tobias Geerinckx-Rice <me@tobias.gr>
2017-12-13 20:57:05 +0100

    gnu: perl-lingua-en-inflect: Update to 1.903.
    * gnu/packages/language.scm (perl-lingua-en-inflect): Update to 1.903

--- gnu/packages/language.scm
+++ gnu/packages/language.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2015, 2016 Eric Bavier <bavier@member.fsf.org>
+;;; Copyright © 2017 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -51,7 +52,7 @@ manipulating such numbers.")
 (define-public perl-lingua-en-inflect
   (package
     (name "perl-lingua-en-inflect")
-    (version "1.901")
+    (version "1.903")
     (source
      (origin
        (method url-fetch)
@@ -59,7 +60,7 @@ manipulating such numbers.")
                            "Lingua-EN-Inflect-" version ".tar.gz"))
        (sha256
         (base32
-         "0mcwlgf6hkh4zm3s1x899f25xj4hyzrc2vssiwfxysqja36yf5ys"))))
+         "0j8d1f1wvmgc11d71pc8xp8fv5a1nb2yfw1dgd19xhscn1klpvzw"))))
     (build-system perl-build-system)
     (native-inputs `(("perl-module-build" ,perl-module-build)))
     (home-page "http://search.cpan.org/dist/Lingua-EN-Inflect")
