guix-mirror/guix

commit 614fffe4270cef308573a6d9cef650f3972875af

build-system/gnu: Add the "static" output of libc in cross environments.
Fixes a regression whereby the host libc.a would be missing when
cross-compiling.

This is a followup to commit 6dff905e51202bbdebbad8811b6509584d12a796.

* guix/build-system/gnu.scm (standard-cross-packages): Add the
"cross-libc:static".

guix/build-system/gnu.scm
@@ -414,7 +414,9 @@ is one of `host' or `target'."
                               #:libc (libc target)))
            ("cross-binutils" ,(binutils target))))
         ((target)
-         `(("cross-libc" ,(libc target))))))))
+         (let ((libc (libc target)))
+          `(("cross-libc" ,libc)
+            ("cross-libc:static" ,libc "static"))))))))
 
 (define* (gnu-cross-build store name
                           #:key

commit d2b5ede0539567568cfb917e08f1b80a3eab38e7

build-system/gnu: Check whether the cross-libc has a "static" output.
This is a followup to 614fffe4270cef308573a6d9cef650f3972875af, which
broke cross-compilation to i686-w64-mingw32.

* guix/build-system/gnu.scm (standard-cross-packages): Add
"cross-libc:static" only when LIBC has a "static" output.

guix/build-system/gnu.scm
@@ -415,8 +415,12 @@ is one of `host' or `target'."
            ("cross-binutils" ,(binutils target))))
         ((target)
          (let ((libc (libc target)))
-          `(("cross-libc" ,libc)
-            ("cross-libc:static" ,libc "static"))))))))
+           `(("cross-libc" ,libc)
+
+             ;; MinGW's libc doesn't have a "static" output.
+             ,@(if (member "static" (package-outputs libc))
+                   `(("cross-libc:static" ,libc "static"))
+                   '()))))))))
 
 (define* (gnu-cross-build store name
                           #:key