838fdebb7323d188e67b4fd06bde4c8eb1873a79
Mark H Weaver <mhw@netris.org>
2017-12-30 15:31:59 -0500

    gnu: linux-libre@4.9: Update to 4.9.73.
    * gnu/packages/linux.scm (linux-libre-4.9): Update to 4.9.73.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -384,8 +384,8 @@ It has been modified to remove all non-free binary blobs.")
                     #:configuration-file kernel-config))
 
 (define-public linux-libre-4.9
-  (make-linux-libre "4.9.72"
-                    "1jcjrnbn7q3wa90ibpwxn2hfj2phx8bhj0k7isblgrj674s7zzzv"
+  (make-linux-libre "4.9.73"
+                    "0a5xgy7g7sb3439brv4hvpb2hjzs22nhibz8vxdmi4sqaqchh9r7"
                     %intel-compatible-systems
                     #:configuration-file kernel-config))
 
