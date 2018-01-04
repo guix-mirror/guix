9d8fb0e30fd984c5eddc5e55171deea0530d65f3
Tobias Geerinckx-Rice <me@tobias.gr>
2018-01-04 00:46:17 +0000

    gnu: newsboat: Update to 2.10.2.
    * gnu/packages/syndication.scm (newsboat): Update to 2.10.2.
    [inherit]: Remove inheritance of unmaintained newsbeuter.
    [native-inputs]: Remove perl.
    [arguments]: Remove #:make-flags to build on ARM, which should work now.
    [synopsis, description]: Mash up Newsboat's own description with useful
    information from its predecessor.

--- gnu/packages/syndication.scm
+++ gnu/packages/syndication.scm
@@ -1,4 +1,5 @@
 ;;; Copyright © 2016, 2017 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;;
 ;;; GNU Guix is free software; you can redistribute it and/or modify it
 ;;; under the terms of the GNU General Public License as published by
@@ -28,8 +29,7 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages ruby)
   #:use-module (gnu packages xml)
-  #:use-module (gnu packages web)
-  #:use-module (srfi srfi-1))
+  #:use-module (gnu packages web))
 
 (define-public newsbeuter
   (package
@@ -81,32 +81,50 @@ common to users of @command{mutt} and @command{slrn}.")
 
 (define-public newsboat
   (package
-    (inherit newsbeuter)
     (name "newsboat")
-    (version "2.10.1")
+    (version "2.10.2")
     (source
-      (origin
-        (method url-fetch)
-        (uri (string-append "https://newsboat.org/releases/" version
-                            "/newsboat-" version ".tar.xz"))
-        (sha256
-         (base32
-          "1xgqkhpjbq916g9hkaxs0s2fz8bg103pzjx75ziq5ba688l9imj4"))))
-    (arguments
-     '(#:phases (modify-phases %standard-phases
-                  (delete 'configure) ; no configure
-                  (add-after 'build 'build-documentation
-                    (lambda _ (zero? (system* "make" "doc")))))
-       #:make-flags (list (string-append "prefix=" (assoc-ref %outputs "out"))
-                          ;; see https://github.com/newsboat/newsboat/issues/43
-                          "WARNFLAGS=-Wno-sign-compare")
-       #:test-target "test"))
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://newsboat.org/releases/" version
+                           "/newsboat-" version ".tar.xz"))
+       (sha256
+        (base32
+         "1x4nxx1kvmrcm8jy73dvg56h4z15y3sach2vr13cw8rsbi7v99px"))))
+    (build-system gnu-build-system)
     (native-inputs
-     `(("asciidoc" ,asciidoc)
-       ,@(alist-delete "ruby" (package-native-inputs newsbeuter))))
+     `(("gettext" ,gettext-minimal)
+       ("pkg-config" ,pkg-config)
+       ;; For building documentation.
+       ("asciidoc" ,asciidoc)))
+    (inputs
+     `(("curl" ,curl)
+       ("json-c" ,json-c-0.12)      ; check whether json-c-0.12 can be removed
+       ("libxml2" ,libxml2)
+       ("ncurses" ,ncurses)
+       ("stfl" ,stfl)
+       ("sqlite" ,sqlite)))
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (delete 'configure)            ; no configure script
+         (add-after 'build 'build-documentation
+           (lambda _
+             (zero? (system* "make" "doc")))))
+       #:make-flags
+       (list (string-append "prefix=" (assoc-ref %outputs "out")))
+       #:test-target "test"))
     (home-page "https://newsboat.org/")
-    (description "Newsboat is a fork of Newsbeuter, an RSS/Atom feed reader for
-the text console.  It supports OPML import/exports, HTML rendering, podcast
-(podboat), offline reading, searching and storing articles to your filesystem,
-and many more features.  Its user interface is coherent, easy to use, and might
-look common to users of @command{mutt} and @command{slrn}.")))
+    (synopsis "Text-mode RSS and Atom feed reader with podcast support")
+    (description "Newsboat is a feed reader for @dfn{RSS} and @dfn{Atom}, XML
+formats widely used to transmit, publish, and syndicate news or blog articles.
+It's designed for use on text terminals, and to have a coherent and easy-to-use
+interface that might look familiar to @command{mutt} or @command{slrn} users.
+
+Newsboat supports OPML import/exports, HTML rendering, podcasts (with
+@command{podboat}), off-line reading, searching and storing articles to your
+file system, and many more features.
+
+It started life as a fork of the currently unmaintained Newsbeuter.")
+    (license (list license:gpl2+        ; filter/*
+                   license:expat))))    ; everything else
