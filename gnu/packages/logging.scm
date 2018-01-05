bc6ca5237a6fed32462c1404f215a3589a84eb54
Tobias Geerinckx-Rice <me@tobias.gr>
2018-01-05 13:03:14 +0000

    gnu: log4cpp: Update to 1.1.3.
    * gnu/packages/logging.scm (log4cpp): Update to 1.1.3.

--- gnu/packages/logging.scm
+++ gnu/packages/logging.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2016, 2017 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2017 Stefan Reichör <stefan@xsteve.at>
 ;;; Copyright © 2017 Eric Bavier <bavier@member.fsf.org>
+;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -37,7 +38,7 @@
 (define-public log4cpp
   (package
     (name "log4cpp")
-    (version "1.1.1")
+    (version "1.1.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://sourceforge/log4cpp/log4cpp-"
@@ -46,7 +47,7 @@
                                   "/log4cpp-" version ".tar.gz"))
               (sha256
                (base32
-                "1l5yz5rfzzv6g3ynrj14mxfsk08cp5h1ssr7d74hjs0accrg7arm"))))
+                "07gmr3jyaf2239n9sp6h7hwdz1pv7b7aka8n06gmr2fnlmaymfrc"))))
     (build-system gnu-build-system)
     (synopsis "Log library for C++")
     (description
