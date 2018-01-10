f26fa354f1d76ce9cbc43cc553637af3f0c34cc2
Tobias Geerinckx-Rice <me@tobias.gr>
2018-01-11 00:54:22 +0100

    gnu: newsbeuter: Deprecate in favour of newsboat.
    * gnu/packages/syndication.scm (newsbeuter): Redefine using
    ‘deprecated-package’.
    * gnu/packages/patches/newsbeuter-CVE-2017-12904.patch: Delete file.
    * gnu/packages/patches/newsbeuter-CVE-2017-14500.patch: Likewise.
    * gnu/local.mk (dist_patch_DATA): Remove both.

--- gnu/local.mk
+++ gnu/local.mk
@@ -921,8 +921,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/netsurf-system-utf8proc.patch		\
   %D%/packages/patches/netsurf-y2038-tests.patch		\
   %D%/packages/patches/netsurf-longer-test-timeout.patch	\
-  %D%/packages/patches/newsbeuter-CVE-2017-12904.patch		\
-  %D%/packages/patches/newsbeuter-CVE-2017-14500.patch		\
   %D%/packages/patches/ngircd-handle-zombies.patch		\
   %D%/packages/patches/ninja-zero-mtime.patch			\
   %D%/packages/patches/node-test-http2-server-rst-stream.patch	\
