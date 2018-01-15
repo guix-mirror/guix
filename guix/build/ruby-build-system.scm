b640370d8290ace2507c84e61fe99811effaf7da
Danny Milosavljevic <dannym@scratchpost.org>
2018-01-15 16:56:03 +0100

    ruby-build-system: Fix build error.
    * gnu/build/ruby-build-system.scm: Import (ice-9 rdelim).
    
    Follow-up to d9df4bf055f2bef8c2c428db34c5fa056bdeba73.

--- guix/build/ruby-build-system.scm
+++ guix/build/ruby-build-system.scm
@@ -24,6 +24,7 @@
   #:use-module (ice-9 ftw)
   #:use-module (ice-9 match)
   #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim)
   #:use-module (ice-9 regex)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-26)
