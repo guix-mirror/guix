40267943ae2ac68b64ced37d7b0f9a4a503df545
Tobias Geerinckx-Rice <me@tobias.gr>
2018-01-16 22:19:39 +0100

    gnu: bind: Update to 9.11.2-P1 [fixes CVE-2017-3145].
    * gnu/packages/dns.scm (isc-bind): Update to 9.11.2-P1.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -99,7 +99,7 @@ and BOOTP/TFTP for network booting of diskless machines.")
 (define-public isc-bind
   (package
     (name "bind")
-    (version "9.11.2")
+    (version "9.11.2-P1")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -107,7 +107,7 @@ and BOOTP/TFTP for network booting of diskless machines.")
                     version ".tar.gz"))
               (sha256
                (base32
-                "0yn7wgi2y8mpmvbjbkl4va7p0xsnn48m4yjx6ynb1hzp423asikz"))))
+                "04hjvwvs7ssgj69lqparx0wj0w3xkc0x8y2iv62kzjighd41bhyf"))))
     (build-system gnu-build-system)
     (outputs `("out" "utils"))
     (inputs
