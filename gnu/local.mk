4ff22ec3b16a90b140721aa10c94c595ccb2c390
Tobias Geerinckx-Rice <me@tobias.gr>
2018-01-17 19:18:26 +0100

    gnu: slurm: Update to 17.11.2.
    * gnu/packages/parallel.scm (slurm): Update to 17.11.2.
    [source]: Replace patch with less fragile SUBSTITUTE* in a snippet.
    [arguments]: Rename ‘autogen’ phase to ‘autoconf’.  Use INVOKE.
    * gnu/packages/patches/slurm-configure-remove-nonfree-contribs.patch:
    Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1082,7 +1082,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/slim-sigusr1.patch			\
   %D%/packages/patches/slim-reset.patch				\
   %D%/packages/patches/slim-login.patch				\
-  %D%/packages/patches/slurm-configure-remove-nonfree-contribs.patch \
   %D%/packages/patches/sooperlooper-build-with-wx-30.patch 	\
   %D%/packages/patches/steghide-fixes.patch			\
   %D%/packages/patches/superlu-dist-scotchmetis.patch		\
