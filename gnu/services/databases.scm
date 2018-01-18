1e6b9c6e164df01ecf1b255fa95a246f698a8c34
Christopher Baines <mail@cbaines.net>
2018-01-18 22:45:26 +0000

    services: postgresql: Add a default-value to the postgresql-service-type.
    * gnu/packages/databases.scm (<postgresql-configuration>)
      [config-file,data-directory]: Add default.
      (postgresql-service-type)[default-value]:
      Set to (postgresql-configuration).

--- gnu/services/databases.scm
+++ gnu/services/databases.scm
@@ -77,8 +77,10 @@
                   (default 5432))
   (locale         postgresql-configuration-locale
                   (default "en_US.utf8"))
-  (config-file    postgresql-configuration-file)
-  (data-directory postgresql-configuration-data-directory))
+  (config-file    postgresql-configuration-file
+                  (default %default-postgres-config))
+  (data-directory postgresql-configuration-data-directory
+                  (default "/var/lib/postgresql/data")))
 
 (define %default-postgres-hba
   (plain-file "pg_hba.conf"
@@ -184,7 +186,8 @@ host	all	all	::1/128 	trust"))
                        (service-extension activation-service-type
                                           postgresql-activation)
                        (service-extension account-service-type
-                                          (const %postgresql-accounts))))))
+                                          (const %postgresql-accounts))))
+                (default-value (postgresql-configuration))))
 
 (define* (postgresql-service #:key (postgresql postgresql)
                              (port 5432)
