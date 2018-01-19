5bb12e1462c4ecfbed829b09ed011853dd74209d
Tobias Geerinckx-Rice <me@tobias.gr>
2018-01-19 05:27:06 +0100

    gnu: ninja: Update to 1.8.2.
    * gnu/packages/ninja.scm (ninja): Update to 1.8.2.
    [source]: Remove upstreamed patch.
    [arguments]: Substitute INVOKE for SYSTEM*.  Use standard indentation.
    * gnu/packages/patches/ninja-zero-mtime.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -925,7 +925,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/netsurf-y2038-tests.patch		\
   %D%/packages/patches/netsurf-longer-test-timeout.patch	\
   %D%/packages/patches/ngircd-handle-zombies.patch		\
-  %D%/packages/patches/ninja-zero-mtime.patch			\
   %D%/packages/patches/node-test-http2-server-rst-stream.patch	\
   %D%/packages/patches/nss-increase-test-timeout.patch		\
   %D%/packages/patches/nss-pkgconfig.patch			\
