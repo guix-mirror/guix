a893dc2f484f89d9f0ba21d44bdac3b792872781
Fis Trivial <ybbs.daans@hotmail.com>
2018-01-21 19:31:16 +0000

    gnu: rct: Add missing headers, enable RTTI.
    * gnu/packages/patches/rct-add-missing-headers.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/cpp.scm (rct): Use the patch, enable RTTI.
    [source]: Use the patch to add missing headers from installation.
    [arguments]: Enable RTTI in configure-flags.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1054,6 +1054,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/raptor2-heap-overflow.patch		\
   %D%/packages/patches/ratpoison-shell.patch			\
   %D%/packages/patches/rcs-5.9.4-noreturn.patch			\
+  %D%/packages/patches/rct-add-missing-headers.patch		\
   %D%/packages/patches/readline-link-ncurses.patch		\
   %D%/packages/patches/readline-6.2-CVE-2014-2524.patch		\
   %D%/packages/patches/readline-7.0-mingw.patch			\
