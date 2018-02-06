71e789319cfe81731d61ec3c6a6dd2110baa7190
Ludovic Courtès <ludovic.courtes@inria.fr>
2018-02-06 17:33:14 +0100

    gnu: hwloc: Add 2.0.0.
    * gnu/packages/mpi.scm (hwloc-2.0): New variable.
    * gnu/packages/patches/hwloc-tests-without-sysfs.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -757,6 +757,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/higan-remove-march-native-flag.patch	\
   %D%/packages/patches/hubbub-sort-entities.patch		\
   %D%/packages/patches/hurd-fix-eth-multiplexer-dependency.patch        \
+  %D%/packages/patches/hwloc-tests-without-sysfs.patch		\
   %D%/packages/patches/hydra-disable-darcs-test.patch		\
   %D%/packages/patches/icecat-avoid-bundled-libraries.patch	\
   %D%/packages/patches/icecat-bug-1348660-pt5.patch		\
