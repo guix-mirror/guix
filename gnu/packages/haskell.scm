41faaff11feb04375161ae20350bacf9bdbaed42
Ricardo Wurmus <rekado@elephly.net>
2018-02-12 23:49:15 +0100

    gnu: Add ghc-doctest-0.12.
    * gnu/packages/haskell.scm (ghc-doctest-0.12): New variable.

--- gnu/packages/haskell.scm
+++ gnu/packages/haskell.scm
@@ -4537,6 +4537,24 @@ It is modeled after doctest for Python, see
 @uref{http://docs.python.org/library/doctest.html, the Doctest website}.")
     (license license:expat)))
 
+(define-public ghc-doctest-0.12
+  (package (inherit ghc-doctest)
+    (name "ghc-doctest")
+    (version "0.12.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/doctest/doctest-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32
+         "13h549cpgcvb7c54c7wif28g5wak84dxc3ais0hlqhzk1q6la91a"))))
+    (inputs
+     `(("ghc-code-page" ,ghc-code-page)
+       ,@(package-inputs ghc-doctest)))))
+
 (define-public ghc-lifted-base
   (package
     (name "ghc-lifted-base")
