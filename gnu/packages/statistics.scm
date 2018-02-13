4c9264c1b989b9b6dcdf773db74c18db62685dfb
Ricardo Wurmus <rekado@elephly.net>
2018-02-13 16:02:40 +0100

    gnu: r-mass: Update to 7.3-48.
    * gnu/packages/statistics.scm (r-mass): Update to 7.3-48.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -318,14 +318,14 @@ D.V. Hinkley (1997, CUP), originally written by Angelo Canty for S.")
 (define-public r-mass
   (package
     (name "r-mass")
-    (version "7.3-47")
+    (version "7.3-48")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "MASS" version))
        (sha256
         (base32
-         "1gy6z7ly9wn86rfn9xrmqiqq1ijw3pkasrr2299kbzsgx2mwsi7d"))))
+         "02q95ba909p2gxnzfp295ipxjnv14f7bm55nicxwn2kj8hizpzqd"))))
     (properties `((upstream-name . "MASS")))
     (build-system r-build-system)
     (home-page "http://www.stats.ox.ac.uk/pub/MASS4/")
