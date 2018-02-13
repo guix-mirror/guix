637584253fba0d5b414912b8f7e8593e15f4d932
Ricardo Wurmus <rekado@elephly.net>
2018-02-13 15:21:59 +0100

    gnu: r-pracma: Update to 2.1.4.
    * gnu/packages/maths.scm (r-pracma): Update to 2.1.4.

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -1900,12 +1900,12 @@ programming problems.")
 (define-public r-pracma
   (package
     (name "r-pracma")
-    (version "2.1.1")
+    (version "2.1.4")
     (source (origin
       (method url-fetch)
       (uri (cran-uri "pracma" version))
       (sha256
-        (base32 "1mylrrkyycaw9m01mmg6xkn5wgdlabs5l0qyws60r0n2ycblp897"))))
+        (base32 "1ygm81i7mqvh229dp9935djjyb120p3bqvaf4k572sa4q63fzjhc"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-quadprog" ,r-quadprog)))
