130d290e29fa3af4567fba1132c55cedd2d0e11a
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2018-02-14 14:27:16 +0100

    gnu: Add python-future-fstrings.
    * gnu/packages/python.scm (python-future-fstrings): New variable.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -12684,3 +12684,26 @@ standard library.  It provides two additional tokens @code{ESCAPED_NL} and
 @code{UNIMPORTANT_WS}, and a @code{Token} data type.  Use @code{src_to_tokens}
 and @code{tokens_to_src} to roundtrip.")
     (license license:expat)))
+
+(define-public python-future-fstrings
+  (package
+    (name "python-future-fstrings")
+    (version "0.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "future_fstrings" version))
+       (sha256
+        (base32
+         "0ydxqz2dvns44g55p8ix2x18qkfk3aaz0m0dc70f3g6n8js35h47"))))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-tokenize-rt" ,python-tokenize-rt)))
+    (home-page "https://github.com/asottile/future-fstrings")
+    (synopsis "Backport of fstrings to Python < 3.6")
+    (description
+     "This package provides a UTF-8 compatible encoding
+@code{future_fstrings}, which performs source manipulation.  It decodes the
+source bytes using the UTF-8 encoding and then rewrites Python 3.6 style
+@code{f} strings.")
+    (license license:expat)))
