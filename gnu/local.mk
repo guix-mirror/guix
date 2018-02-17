bb3359ab8e3c20eb91d61509b5ab364d03c9ec3b
Ben Woodcroft <donttrustben@gmail.com>
2018-02-18 00:09:17 +1000

    gnu: ruby-2.4.3: Update rubygems to 2.7.6.
    This fixes the security issues described at
    https://www.ruby-lang.org/en/news/2018/02/17/multiple-vulnerabilities-in-
    rubygems/
    
    * gnu/packages/patches/ruby-rubygems-276-for-ruby24.patch: New file.
    * gnu/packages/ruby.scm (ruby-2.4.3)[source]: Use it.
    * gnu/local.mk (dist_patch_DATA): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1079,6 +1079,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/rpcbind-CVE-2017-8779.patch		\
   %D%/packages/patches/rsem-makefile.patch			\
   %D%/packages/patches/rtags-separate-rct.patch			\
+  %D%/packages/patches/ruby-rubygems-276-for-ruby24.patch	\
   %D%/packages/patches/ruby-concurrent-ignore-broken-test.patch	\
   %D%/packages/patches/ruby-concurrent-test-arm.patch		\
   %D%/packages/patches/ruby-rack-ignore-failing-test.patch      \
