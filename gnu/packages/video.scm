d5a352937aad9e1afbf042889d5134a5bea30423
Tobias Geerinckx-Rice <me@tobias.gr>
2018-02-24 15:00:27 +0100

    gnu: youtube-dl: Update to 2018.02.22.
    * gnu/packages/video.scm (youtube-dl): Update to 2018.02.22.

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1157,7 +1157,7 @@ access to mpv's powerful playback capabilities.")
 (define-public youtube-dl
   (package
     (name "youtube-dl")
-    (version "2018.02.11")
+    (version "2018.02.22")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://yt-dl.org/downloads/"
@@ -1165,7 +1165,7 @@ access to mpv's powerful playback capabilities.")
                                   version ".tar.gz"))
               (sha256
                (base32
-                "0zg34spkfiwjq55jrk417ifdcyvrj80gdbc338rdpfw2s6ibggv8"))))
+                "112qmwrkd0cpyx2h20k6y07lw7iixvj8yya7r97h3k1y1py9vbz8"))))
     (build-system python-build-system)
     (arguments
      ;; The problem here is that the directory for the man page and completion
