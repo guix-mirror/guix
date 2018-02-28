836de07a47aa4a982fe99b774da2db53e235f2ca
Marius Bakke <mbakke@fastmail.com>
2018-02-28 13:54:08 +0100

    gnu: python-wrapt: Update to 1.10.11.
    * gnu/packages/python.scm (python-wrapt): Update to 1.10.11.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -5896,14 +5896,14 @@ and MAC network addresses.")
 (define-public python-wrapt
   (package
     (name "python-wrapt")
-    (version "1.10.8")
+    (version "1.10.11")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "wrapt" version))
         (sha256
           (base32
-            "0wrcm1mydvfivbkzz0h81ygzdchnscshi6xvy5n3r21r9s0px8af"))))
+            "1ip3dwib39xhp79kblskgvz3fjzcwxgx3fs3ahdixhpjg7a61mfl"))))
     (build-system python-build-system)
     (arguments
      ;; Tests are not included in the tarball, they are only available in the
