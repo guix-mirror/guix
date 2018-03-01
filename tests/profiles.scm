435603a1d6106b535cf143d17cb030b2d0795b54
Ludovic Courtès <ludo@gnu.org>
2018-03-01 11:37:36 +0100

    profiles: 'manifest-add' truly deletes duplicate entries.
    Fixes <https://bugs.gnu.org/30569>.
    Reported by Andreas Enge <andreas@enge.fr>.
    
    * guix/profiles.scm (manifest-add): Don't append ENTRIES as is.
    Instead, cons each element of ENTRIES as we fold over it.
    Remove unneeded ellispes in 'match' patterns.

--- tests/profiles.scm
+++ tests/profiles.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2014, 2015, 2016, 2017 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013, 2014, 2015, 2016, 2017, 2018 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2014 Alex Kost <alezost@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -147,6 +147,11 @@
            ((($ <manifest-entry> "guile" "2.0.9" "out")) #t)
            (_ #f))
          (equal? m3 m4))))
+
+(test-equal "manifest-add removes duplicates"    ;<https://bugs.gnu.org/30569>
+  (list guile-2.0.9)
+  (manifest-entries (manifest-add (manifest '())
+                                  (list guile-2.0.9 guile-2.0.9))))
 
 (test-assert "manifest-perform-transaction"
   (let* ((m0 (manifest (list guile-2.0.9 guile-2.0.9:debug)))
