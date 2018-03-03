0645da3fe3112a3568e646babf524bd9439ffb8e
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2018-03-03 14:06:43 +0100

    gnu: grammalecte: Update to 0.6.2.
    * gnu/packages/dictionaries.scm (grammalecte): Update to 0.6.2.
    [arguments]: Remove.

--- gnu/packages/dictionaries.scm
+++ gnu/packages/dictionaries.scm
@@ -211,7 +211,7 @@ It comes with a German-English dictionary with approximately 270,000 entries.")
 (define-public grammalecte
   (package
     (name "grammalecte")
-    (version "0.6.1")
+    (version "0.6.2")
     (source
      (origin
        (method url-fetch/zipbomb)
@@ -219,20 +219,8 @@ It comes with a German-English dictionary with approximately 270,000 entries.")
                            "Grammalecte-fr-v" version ".zip"))
        (sha256
         (base32
-         "0bl342i7nqbg8swk3fxashg9liyp3jdnix59pndhy41cpm1xln4i"))))
+         "0pvblclvbxbfgmq0cvmpmzpf6bi6r41arndwprl7ab9kci9hi8j2"))))
     (build-system python-build-system)
-    (arguments
-     '(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'fix-setup.py
-           ;; FIXME: "setup.py" contains a typo in 0.6.1 release. The
-           ;; issue was reported and fixed upstream
-           ;; (https://dicollecte.org/thread.php?prj=fr&t=674). This
-           ;; phase can be removed in next release.
-           (lambda _
-             (substitute* "setup.py"
-               (("server_options\\.") "grammalecte-server-options."))
-             #t)))))
     (home-page "https://www.dicollecte.org")
     (synopsis  "French spelling and grammar checker")
     (description "Grammalecte is a grammar checker dedicated to the French
