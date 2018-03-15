8e4ce402693d6f6fb5bbb26d7907595948669350
Mark H Weaver <mhw@netris.org>
2018-03-15 19:17:22 -0400

    gnu: linux-libre: Update to 4.15.10.
    * gnu/packages/linux.scm (%linux-libre-version): Update to 4.15.10.
    (%linux-libre-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -383,8 +383,8 @@ It has been modified to remove all non-free binary blobs.")
 ;; supports qemu "virt" machine and possibly a large number of ARM boards.
 ;; See : https://wiki.debian.org/DebianKernel/ARMMP.
 
-(define %linux-libre-version "4.15.9")
-(define %linux-libre-hash "13lcard7i6w2c1cf9rfhvmq79xk4qp2p1c1920mfi69l20yvm572")
+(define %linux-libre-version "4.15.10")
+(define %linux-libre-hash "10fp8jmy0fxq8l01m1nnagpq1hznl9jmhcwknk8izjmdcb5snq6c")
 
 (define-public linux-libre
   (make-linux-libre %linux-libre-version
