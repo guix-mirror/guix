107ebf7b34e619de9f6b50996ede6c7c0e1159cb
Julien Lepiller <julien@lepiller.eu>
2018-03-16 14:38:50 +0100

    gnu: Add groovy-servlet.
    * gnu/packages/groovy.scm (groovy-servlet): New variable.

--- gnu/packages/groovy.scm
+++ gnu/packages/groovy.scm
@@ -879,3 +879,57 @@ module is used for interaction between Groovy and Java code.")))
     (synopsis "Groovy input-output library")
     (description "This package implements an input/output library that extends
 the functionality of the common library of Java.")))
+
+(define groovy-servlet
+  (package
+    (inherit groovy-bootstrap)
+    (name "groovy-servlet")
+    (arguments
+     `(#:jar-name "groovy-servlet.jar"
+       #:test-dir "src/test"
+       #:jdk ,icedtea-8
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'configure 'chdir
+           (lambda _
+             (chdir "subprojects/groovy-servlet")
+             #t))
+         (replace 'build
+           (lambda _
+             (mkdir-p "build/classes")
+             (mkdir-p "build/jar")
+             (apply invoke "java" "-cp" (getenv "CLASSPATH")
+                    "org.codehaus.groovy.tools.FileSystemCompiler"
+                    "-d" "build/classes"
+                    "-j"; joint compilation
+                    (find-files "src/main" ".*\\.(groovy|java)$"))
+             (invoke "jar" "-cf" "build/jar/groovy-servlet.jar"
+                     "-C" "build/classes" ".")
+             #t))
+         (replace 'check
+           (lambda _
+             (mkdir-p "build/test-classes")
+             (substitute* "build.xml"
+               (("depends=\"compile-tests\"") "depends=\"\"")
+               (("}/java") "}/groovy"))
+             (apply invoke "java" "-cp"
+                    (string-append (getenv "CLASSPATH") ":build/classes")
+                    "org.codehaus.groovy.tools.FileSystemCompiler"
+                    "-d" "build/test-classes"
+                    "-j"
+                    (append (find-files "src/test" ".*\\.(groovy|java)$")))
+             (invoke "ant" "check")
+             #t)))))
+    (inputs
+     `(("groovy-templates" ,groovy-templates)
+       ("groovy-xml" ,groovy-xml)
+       ,@(package-inputs groovy-bootstrap)))
+    (native-inputs
+     `(("groovy-bootstrap" ,groovy-bootstrap)
+       ("groovy-json" ,groovy-json)
+       ("groovy-test" ,groovy-test)
+       ("groovy-tests-bootstrap" ,groovy-tests-bootstrap)
+       ,@(package-native-inputs java-groovy-bootstrap)))
+    (synopsis "Groovy's servlet implementation")
+    (description "This package contains a library to create groovlets, Groovy's
+version of Java servlets.")))
