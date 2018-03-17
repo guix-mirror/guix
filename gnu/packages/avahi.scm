guix-mirror/guix

commit 550f4509acf2c0f67882260414b0bb2843e07465

gnu: avahi: Update to 0.7.
* gnu/packages/avahi.scm (avahi): Update to 0.7.
* gnu/packages/patches/avahi-localstatedir.patch: Adjust context.

gnu/packages/avahi.scm
@@ -33,7 +33,7 @@
 (define-public avahi
   (package
     (name "avahi")
-    (version "0.6.31")
+    (version "0.7")
     (home-page "http://avahi.org")
     (source (origin
              (method url-fetch)
@@ -41,7 +41,7 @@
                                  version ".tar.gz"))
              (sha256
               (base32
-               "0j5b5ld6bjyh3qhd2nw0jb84znq0wqai7fsrdzg7bpg24jdp2wl3"))
+               "0128n7jlshw4bpx0vg8lwj8qwdisjxi7mvniwfafgnkzzrfrpaap"))
              (patches (search-patches "avahi-localstatedir.patch"))))
     (build-system gnu-build-system)
     (arguments

gnu/packages/patches/avahi-localstatedir.patch
@@ -2,11 +2,11 @@ Don't "mkdir $(localstatedir)" since we can't do it (/var).
 
 --- avahi-0.6.27/avahi-daemon/Makefile.in	2010-07-13 05:06:35.000000000 +0200
 +++ avahi-0.6.27/avahi-daemon/Makefile.in	2010-07-13 18:03:45.000000000 +0200
-@@ -1554,7 +1554,6 @@ xmllint:
+@@ -1625,7 +1625,6 @@
  	done
  
  install-data-local:
--	test -z "$(localstatedir)/run" || $(mkdir_p) "$(DESTDIR)$(localstatedir)/run"
+-	test -z "$(avahi_runtime_dir)" || $(MKDIR_P) "$(DESTDIR)$(avahi_runtime_dir)"
  
  update-systemd:
  	curl http://cgit.freedesktop.org/systemd/plain/src/sd-daemon.c > sd-daemon.c

commit b34ac8248205d64b4023412e61512dfefe44cb9e

gnu: nss-mdns: Adjust Avahi socket file name.
Fixes <https://bugs.gnu.org/30613>.
Regression introduced in 550f4509acf2c0f67882260414b0bb2843e07465.
Reported by George myglc2 Clemmer <myglc2@gmail.com>.

* gnu/packages/avahi.scm (nss-mdns)[arguments]: Remove
 #:configure-flags.  Add #:phases.

gnu/packages/avahi.scm
@@ -99,10 +99,18 @@ DNS-SD (for \"DNS-Based Service Discovery\") protocols.")
                        (method url-fetch))))))
     (build-system gnu-build-system)
     (arguments
-     ;; The Avahi daemon socket is expected by src/Makefile.am to be at
-     ;; "$(localstatedir)/run/avahi-daemon/socket", so set $(localstatedir)
-     ;; appropriately.
-     '(#:configure-flags '("--localstatedir=/var")))
+     '(#:phases (modify-phases %standard-phases
+                  (add-after 'unpack 'set-avahi-socket-name
+                    (lambda _
+                      ;; The Avahi daemon socket is expected by
+                      ;; src/Makefile.am to be at
+                      ;; "$(localstatedir)/run/avahi-daemon/socket", but
+                      ;; nowadays it lives in /run/avahi-daemon/socket.
+                      ;; Remove the "$(localstatedir)" bit.
+                      (substitute* "src/Makefile.in"
+                        (("\\$\\(localstatedir)/run/avahi-daemon/socket")
+                         "/run/avahi-daemon/socket"))
+                      #t)))))
     (synopsis "Multicast DNS Name Service Switch (@dfn{NSS}) plug-in")
     (description
      "Nss-mdns is a plug-in for the GNU C Library's Name Service Switch