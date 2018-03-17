guix-mirror/guix

commit 05f6e601412a96ca5a4a318e79b11a24b7c764f7

gnu: openssl: Use invoke instead of system*.
* gnu/packages/tls.scm (openssl, openssl-next)[arguments]: Use invoke.
Add missing 'apply'.

gnu/packages/tls.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2012, 2013, 2014, 2015, 2016, 2017 Ludovic Courtès <ludo@gnu.org>
-;;; Copyright © 2014, 2015, 2016, 2017 Mark H Weaver <mhw@netris.org>
+;;; Copyright © 2014, 2015, 2016, 2017, 2018 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2014 Ian Denhardt <ian@zenhack.net>
 ;;; Copyright © 2013, 2015 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2015 David Thompson <davet@gnu.org>
@@ -297,26 +297,25 @@ required structures.")
          'configure
          (lambda* (#:key outputs #:allow-other-keys)
            (let ((out (assoc-ref outputs "out")))
-             (zero?
-              (system* "./config"
-                       "shared"                   ;build shared libraries
-                       "--libdir=lib"
+             (apply invoke "./config"
+                    "shared"                   ;build shared libraries
+                    "--libdir=lib"
 
-                       ;; The default for this catch-all directory is
-                       ;; PREFIX/ssl.  Change that to something more
-                       ;; conventional.
-                       (string-append "--openssldir=" out
-                                      "/share/openssl-" ,version)
+                    ;; The default for this catch-all directory is
+                    ;; PREFIX/ssl.  Change that to something more
+                    ;; conventional.
+                    (string-append "--openssldir=" out
+                                   "/share/openssl-" ,version)
 
-                       (string-append "--prefix=" out)
+                    (string-append "--prefix=" out)
 
-                       ;; XXX FIXME: Work around a code generation bug in GCC
-                       ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
-                       ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
-                       ,@(if (and (not (%current-target-system))
-                                  (string-prefix? "armhf" (%current-system)))
-                             '("-mfpu=vfpv3")
-                             '()))))))
+                    ;; XXX FIXME: Work around a code generation bug in GCC
+                    ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
+                    ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
+                    ,@(if (and (not (%current-target-system))
+                               (string-prefix? "armhf" (%current-system)))
+                          '("-mfpu=vfpv3")
+                          '())))))
         (add-after
          'install 'make-libraries-writable
          (lambda* (#:key outputs #:allow-other-keys)
@@ -422,27 +421,26 @@ required structures.")
              (lambda* (#:key outputs #:allow-other-keys)
                (let* ((out (assoc-ref outputs "out"))
                       (lib (string-append out "/lib")))
-                 (zero?
-                  (system* "./config"
-                           "shared"                   ;build shared libraries
-                           "--libdir=lib"
+                 (apply invoke "./config"
+                        "shared"                   ;build shared libraries
+                        "--libdir=lib"
 
-                           ;; The default for this catch-all directory is
-                           ;; PREFIX/ssl.  Change that to something more
-                           ;; conventional.
-                           (string-append "--openssldir=" out
-                                          "/share/openssl-" ,version)
+                        ;; The default for this catch-all directory is
+                        ;; PREFIX/ssl.  Change that to something more
+                        ;; conventional.
+                        (string-append "--openssldir=" out
+                                       "/share/openssl-" ,version)
 
-                           (string-append "--prefix=" out)
-                           (string-append "-Wl,-rpath," lib)
+                        (string-append "--prefix=" out)
+                        (string-append "-Wl,-rpath," lib)
 
-                           ;; XXX FIXME: Work around a code generation bug in GCC
-                           ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
-                           ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
-                           ,@(if (and (not (%current-target-system))
-                                      (string-prefix? "armhf" (%current-system)))
-                                 '("-mfpu=vfpv3")
-                                 '()))))))
+                        ;; XXX FIXME: Work around a code generation bug in GCC
+                        ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
+                        ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
+                        ,@(if (and (not (%current-target-system))
+                                   (string-prefix? "armhf" (%current-system)))
+                              '("-mfpu=vfpv3")
+                              '())))))
 
            ;; XXX: Duplicate this phase to make sure 'version' evaluates
            ;; in the current scope and not the inherited one.

commit 5011d3f4a009a8f904444235874edcfbebfa8ac6

gnu: openssl: Fix the call of 'apply invoke'.
This fixes the build failure introduced in commit 05f6e6014.

* gnu/packages/tls.scm (openssl, openssl-next)[arguments]: Pass configure
flags to 'apply invoke' as the ARGLIST list.

gnu/packages/tls.scm
@@ -298,24 +298,25 @@ required structures.")
          (lambda* (#:key outputs #:allow-other-keys)
            (let ((out (assoc-ref outputs "out")))
              (apply invoke "./config"
-                    "shared"                   ;build shared libraries
-                    "--libdir=lib"
+                    (list
+                     "shared"                 ;build shared libraries
+                     "--libdir=lib"
 
-                    ;; The default for this catch-all directory is
-                    ;; PREFIX/ssl.  Change that to something more
-                    ;; conventional.
-                    (string-append "--openssldir=" out
-                                   "/share/openssl-" ,version)
+                     ;; The default for this catch-all directory is
+                     ;; PREFIX/ssl.  Change that to something more
+                     ;; conventional.
+                     (string-append "--openssldir=" out
+                                    "/share/openssl-" ,version)
 
-                    (string-append "--prefix=" out)
+                     (string-append "--prefix=" out)
 
-                    ;; XXX FIXME: Work around a code generation bug in GCC
-                    ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
-                    ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
-                    ,@(if (and (not (%current-target-system))
-                               (string-prefix? "armhf" (%current-system)))
-                          '("-mfpu=vfpv3")
-                          '())))))
+                     ;; XXX FIXME: Work around a code generation bug in GCC
+                     ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
+                     ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
+                     ,@(if (and (not (%current-target-system))
+                                (string-prefix? "armhf" (%current-system)))
+                           '("-mfpu=vfpv3")
+                           '()))))))
         (add-after
          'install 'make-libraries-writable
          (lambda* (#:key outputs #:allow-other-keys)
@@ -422,25 +423,26 @@ required structures.")
                (let* ((out (assoc-ref outputs "out"))
                       (lib (string-append out "/lib")))
                  (apply invoke "./config"
-                        "shared"                   ;build shared libraries
-                        "--libdir=lib"
+                        (list
+                         "shared"       ;build shared libraries
+                         "--libdir=lib"
 
-                        ;; The default for this catch-all directory is
-                        ;; PREFIX/ssl.  Change that to something more
-                        ;; conventional.
-                        (string-append "--openssldir=" out
-                                       "/share/openssl-" ,version)
+                         ;; The default for this catch-all directory is
+                         ;; PREFIX/ssl.  Change that to something more
+                         ;; conventional.
+                         (string-append "--openssldir=" out
+                                        "/share/openssl-" ,version)
 
-                        (string-append "--prefix=" out)
-                        (string-append "-Wl,-rpath," lib)
+                         (string-append "--prefix=" out)
+                         (string-append "-Wl,-rpath," lib)
 
-                        ;; XXX FIXME: Work around a code generation bug in GCC
-                        ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
-                        ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
-                        ,@(if (and (not (%current-target-system))
-                                   (string-prefix? "armhf" (%current-system)))
-                              '("-mfpu=vfpv3")
-                              '())))))
+                         ;; XXX FIXME: Work around a code generation bug in GCC
+                         ;; 4.9.3 on ARM when compiled with -mfpu=neon.  See:
+                         ;; <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66917>
+                         ,@(if (and (not (%current-target-system))
+                                    (string-prefix? "armhf" (%current-system)))
+                               '("-mfpu=vfpv3")
+                               '()))))))
 
            ;; XXX: Duplicate this phase to make sure 'version' evaluates
            ;; in the current scope and not the inherited one.