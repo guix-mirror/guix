c657716ede8932da356635802534aa13205a6ecd
Tobias Geerinckx-Rice <me@tobias.gr>
2018-03-19 23:00:01 +0100

    gnu: dnsmasq: Fix description typos.
    * gnu/packages/dns.scm (dnsmasq)[description]: Fix some typos.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -83,11 +83,11 @@
     (home-page "http://www.thekelleys.org.uk/dnsmasq/doc.html")
     (synopsis "Small caching DNS proxy and DHCP/TFTP server")
     (description
-     "Dnsmasq is a lightweight DNS forwarder and DHCP server.  It is designed
-to provide DNS and optionally, DHCP, to a small network.  It can serve the
+     "Dnsmasq is a light-weight DNS forwarder and DHCP server.  It is designed
+to provide DNS and, optionally, DHCP to a small network.  It can serve the
 names of local machines which are not in the global DNS.  The DHCP server
 integrates with the DNS server and allows machines with DHCP-allocated
-addresses to appear in the DNS with names configured either in each host or in
+addresses to appear in the DNS with names configured either on each host or in
 a central configuration file.  Dnsmasq supports static and dynamic DHCP leases
 and BOOTP/TFTP for network booting of diskless machines.")
     ;; Source files only say GPL2 and GPL3 are allowed.
