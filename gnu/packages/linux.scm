dd07a549d09af86db326b724e00da0c1a71b39fd
Mark H Weaver <mhw@netris.org>
2018-03-19 18:01:49 -0400

    gnu: linux-libre@4.14: Update to 4.14.28.
    * gnu/packages/linux.scm (%linux-libre-4.14-version): Update to 4.14.28.
    (%linux-libre-4.14-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -392,8 +392,8 @@ It has been modified to remove all non-free binary blobs.")
                     %linux-compatible-systems
                     #:configuration-file kernel-config))
 
-(define %linux-libre-4.14-version "4.14.27")
-(define %linux-libre-4.14-hash "0mgkka9niyd0lj4qliy4v7jjh9lg1a5jwlv60yw7z0s4k7ajyyrp")
+(define %linux-libre-4.14-version "4.14.28")
+(define %linux-libre-4.14-hash "0xg3zsm1yjsvxir8sz7zliz8gcc8d45xh23qyiszl75cfqjl36l3")
 
 (define-public linux-libre-4.14
   (make-linux-libre %linux-libre-4.14-version
