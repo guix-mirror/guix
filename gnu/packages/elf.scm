c24f1f1614a2ba7ff7c5828a8753f82c9792349a
Efraim Flashner <efraim@flashner.co.il>
2018-03-19 21:33:29 +0200

    gnu: patchelf: Use 'target-arm32?' for armhf-linux case.
    * gnu/packages/elf.scm (patchelf)[inputs]: Replace armhf-linux detection
    logic with 'target-arm32?' macro.
    [arguments]: Same.

--- gnu/packages/elf.scm
+++ gnu/packages/elf.scm
@@ -22,6 +22,7 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages elf)
+  #:use-module (guix utils)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
@@ -121,12 +122,12 @@ Executable and Linkable Format (@dfn{ELF}).  This includes @command{ld},
     ;;      patch makes significant changes to the algorithm, possibly
     ;;      introducing bugs.  So, we apply the patch only on ARM systems.
     (inputs
-     (if (string-prefix? "arm" (or (%current-target-system) (%current-system)))
+     (if (target-arm32?)
          `(("patch/rework-for-arm" ,(search-patch
                                      "patchelf-rework-for-arm.patch")))
          '()))
     (arguments
-     (if (string-prefix? "arm" (or (%current-target-system) (%current-system)))
+     (if (target-arm32?)
          `(#:phases (alist-cons-after
                      'unpack 'patch/rework-for-arm
                      (lambda* (#:key inputs #:allow-other-keys)
