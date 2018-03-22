e7a5d40e243c0e8e93851cf99a4e4582bfbed46e
Marius Bakke <mbakke@fastmail.com>
2018-03-22 15:04:10 +0100

    gnu: gtk+: Update to 3.22.29.
    * gnu/packages/gtk.scm (gtk+): Update to 3.22.29.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -659,7 +659,7 @@ application suites.")
    (name "gtk+")
    ;; NOTE: When updating the version of 'gtk+', the hash of 'mate-themes' in
    ;;       mate.scm will also need to be updated.
-   (version "3.22.28")
+   (version "3.22.29")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnome/sources/" name "/"
@@ -667,7 +667,7 @@ application suites.")
                                 name "-" version ".tar.xz"))
             (sha256
              (base32
-              "07syy63d2q12b7mkbhqpirq270365fsql5l9qsrdgzlc04mn36fj"))
+              "1y5vzdbgww9l7xcrg13azff2rs94kggkywmpcsh39h7w76wn8zd0"))
             (patches (search-patches "gtk3-respect-GUIX_GTK3_PATH.patch"
                                      "gtk3-respect-GUIX_GTK3_IM_MODULE_FILE.patch"))))
    (outputs '("out" "bin" "doc"))
