c77fb2d65810265574d05b697e2592bcf8e71148
Tobias Geerinckx-Rice <me@tobias.gr>
2018-03-23 17:53:49 +0100

    gnu: libdvdcss: Update to 1.4.2.
    * gnu/packages/video.scm (libdvdcss): Update to 1.4.2.

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1511,7 +1511,7 @@ encapsulated.")
 (define-public libdvdcss
   (package
     (name "libdvdcss")
-    (version "1.4.1")
+    (version "1.4.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://download.videolan.org/pub/"
@@ -1519,7 +1519,7 @@ encapsulated.")
                                   name "-" version ".tar.bz2"))
               (sha256
                (base32
-                "1b7awvyahivglp7qmgx2g5005kc5npv257gw7wxdprjsnx93f1zb"))))
+                "0x957zzpf4w2cp8zlk29prj8i2q6hay3lzdzsyz8y3cwxivyvhkq"))))
     (build-system gnu-build-system)
     (home-page "https://www.videolan.org/developers/libdvdcss.html")
     (synopsis "Library for accessing DVDs as block devices")
