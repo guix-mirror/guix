4d99a3215c1cb3eda1e9f1159e6fb87915d7390a
Leo Famulari <leo@famulari.name>
2018-03-27 18:59:20 -0400

    gnu: mupdf: Fix CVE-2018-{6544,1000051}.
    * gnu/packages/patches/mupdf-CVE-2018-1000051.patch,
    gnu/packages/patches/mupdf-CVE-2018-6544.patch: New files.
    * gnu/local.mk (dist_patch_DATA): Add them.
    * gnu/packages/pdf.scm (mupdf)[source]: Use them.

--- gnu/local.mk
+++ gnu/local.mk
@@ -929,6 +929,8 @@ dist_patch_DATA =						\
   %D%/packages/patches/mumps-build-parallelism.patch		\
   %D%/packages/patches/mupdf-build-with-latest-openjpeg.patch	\
   %D%/packages/patches/mupdf-CVE-2017-17858.patch		\
+  %D%/packages/patches/mupdf-CVE-2018-6544.patch		\
+  %D%/packages/patches/mupdf-CVE-2018-1000051.patch		\
   %D%/packages/patches/mupen64plus-ui-console-notice.patch	\
   %D%/packages/patches/mutt-store-references.patch		\
   %D%/packages/patches/net-tools-bitrot.patch			\
