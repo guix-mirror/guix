4503b18701449f7ea79fd414f9ab7e05259ec695
Ricardo Wurmus <rekado@elephly.net>
2018-03-29 22:21:15 +0200

    gnu: padthv1: Update to 0.9.0.
    * gnu/packages/music.scm (padthv1): Update to 0.9.0.

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -1358,7 +1358,7 @@ effects.")
 (define-public padthv1
   (package
     (name "padthv1")
-    (version "0.8.6")
+    (version "0.9.0")
     (source (origin
               (method url-fetch)
               (uri
@@ -1366,7 +1366,7 @@ effects.")
                               "/padthv1-" version ".tar.gz"))
               (sha256
                (base32
-                "1mikab2f9n5q1sfgnp3sbm1rf3v57k4085lsgh0a5gzga2h4hwxq"))))
+                "0c519qk2g0dk8gqf9ywqfp7dnr4b25lsnxxbf2l1spnnvf8nysvh"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f ; There are no tests.
