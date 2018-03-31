5125b3d5b0dc507942332d97d63fbaa4c6741323
Rutger Helling <rhelling@mykolab.com>
2018-03-31 09:10:10 +0200

    gnu: wine-staging-patchset-data: Update to 3.5.
    * gnu/packages/wine.scm (wine-staging-patchset-data): Update to 3.5.

--- gnu/packages/wine.scm
+++ gnu/packages/wine.scm
@@ -221,7 +221,7 @@ integrate Windows applications into your desktop.")
 (define-public wine-staging-patchset-data
   (package
    (name "wine-staging-patchset-data")
-   (version "3.4")
+   (version "3.5")
    (source
     (origin
      (method url-fetch)
@@ -230,7 +230,7 @@ integrate Windows applications into your desktop.")
      (file-name (string-append name "-" version ".zip"))
      (sha256
       (base32
-       "00yzh9bqs2rjgvk78xv3gfkbv4f2bkch9vb1ii4xh883f7wvkz93"))))
+       "1d95gzzfx87vvj85mrzv2lgg6w0m917dccja02g6vids28kf9g30"))))
    (build-system trivial-build-system)
    (native-inputs
     `(("bash" ,bash)
