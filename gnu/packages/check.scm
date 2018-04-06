bebe3a4f95fa1a5f7a99b844a3bfe50d8c6e861d
Fis Trivial <ybbs.daans@hotmail.com>
2018-04-06 19:31:47 +0000

    gnu: Add unittest-cpp.
    * gnu/packages/check.scm (unittest-cpp): New public variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -2003,3 +2003,28 @@ retried.")
 
 (define-public python2-pyhamcrest
   (package-with-python2 python-pyhamcrest))
+
+(define-public unittest-cpp
+  (package
+    (name "unittest-cpp")
+    (version "2.0.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/unittest-cpp/unittest-cpp/archive/v"
+                    version ".tar.gz"))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32 "1fgmna2la7z4pwwy2gd10gpgi2q1fk89npjfvkmzvhkxhyc231bl"))))
+    (arguments
+     `(#:tests? #f))                     ; It's run after build automatically.
+    (build-system cmake-build-system)
+    (home-page "https://github.com/unittest-cpp/unittest-cpp")
+    (synopsis "Lightweight unit testing framework for C++")
+    (description "UnitTest++ is a lightweight unit testing framework for C++.
+It was designed to do test-driven development on a wide variety of platforms.
+Simplicity, portability, speed, and small footprint are all very important
+aspects of UnitTest++.  UnitTest++ is mostly standard C++ and makes minimal use
+of advanced library and language features, which means it should be easily
+portable to just about any platform.")
+    (license license:expat)))
