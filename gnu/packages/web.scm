e90c5f35d14b4c956d4b1a39eac656ac6884c992
Mark H Weaver <mhw@netris.org>
2018-04-12 15:22:11 -0400

    Revert "Revert "gnu: nghttp2: Update to 1.31.1 [fixes CVE-2018-1000168].""
    This reverts commit 4ccbdcbcadd6d2e432ee0afa220bc003e72a5b8a.

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -6356,7 +6356,7 @@ derivation by David Revoy from the original MonsterID by Andreas Gohr.")
 (define-public nghttp2
   (package
     (name "nghttp2")
-    (version "1.31.0")
+    (version "1.31.1")
     (source
      (origin
        (method url-fetch)
@@ -6365,7 +6365,7 @@ derivation by David Revoy from the original MonsterID by Andreas Gohr.")
                            name "-" version ".tar.xz"))
        (sha256
         (base32
-         "1ivs74v9sa2sds3dq0s7vn9bkmhr2hgwyg1an1rah3agqwnkqmrn"))))
+         "00z1687m4wi2gbgkijbv099l9hs1sjlyzbhh8jhn0xssx4xcifb5"))))
     (build-system gnu-build-system)
     (outputs (list "out"
                    "lib"))              ; only libnghttp2
