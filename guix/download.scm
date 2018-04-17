guix-mirror/guix

commit 20927c9331b493eaf94211ad9f8a5055e11b4588

download: Use invoke instead of system*.
* guix/download.scm (url-fetch/tarbomb, url-fetch/zipbomb): Use invoke.

guix/download.scm
@@ -517,8 +517,8 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
                           (mkdir #$output)
                           (setenv "PATH" (string-append #$gzip "/bin"))
                           (chdir #$output)
-                          (zero? (system* (string-append #$tar "/bin/tar")
-                                          "xf" #$drv)))
+                          (invoke (string-append #$tar "/bin/tar")
+                                  "xf" #$drv))
                       #:local-build? #t)))
 
 (define* (url-fetch/zipbomb url hash-algo hash
@@ -546,8 +546,8 @@ own.  This helper makes it easier to deal with \"zip bombs\"."
                       #~(begin
                           (mkdir #$output)
                           (chdir #$output)
-                          (zero? (system* (string-append #$unzip "/bin/unzip")
-                                          #$drv)))
+                          (invoke (string-append #$unzip "/bin/unzip")
+                                  #$drv))
                       #:local-build? #t)))
 
 (define* (download-to-store store url #:optional (name (basename url))

commit 6c293a809bba57d4363517fa0bd8ebc34247c577

download: Fix 'url-fetch/tarbomb' and 'url-fetch/zipbomb'.
Fixes a regression introduced in 20927c9331b493eaf94211ad9f8a5055e11b4588.
Reported by Eric Bavier <ericbavier@centurylink.net>.

* guix/download.scm (url-fetch/tarbomb, url-fetch/zipbomb): Within the gexp,
import (guix build utils) for 'invoke'.

guix/download.scm
@@ -510,12 +510,14 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
                                       #:guile guile)))
     ;; Take the tar bomb, and simply unpack it as a directory.
     (gexp->derivation (or name file-name)
-                      #~(begin
-                          (mkdir #$output)
-                          (setenv "PATH" (string-append #$gzip "/bin"))
-                          (chdir #$output)
-                          (invoke (string-append #$tar "/bin/tar")
-                                  "xf" #$drv))
+                      (with-imported-modules '((guix build utils))
+                        #~(begin
+                            (use-modules (guix build utils))
+                            (mkdir #$output)
+                            (setenv "PATH" (string-append #$gzip "/bin"))
+                            (chdir #$output)
+                            (invoke (string-append #$tar "/bin/tar")
+                                    "xf" #$drv)))
                       #:local-build? #t)))
 
 (define* (url-fetch/zipbomb url hash-algo hash
@@ -540,11 +542,13 @@ own.  This helper makes it easier to deal with \"zip bombs\"."
                                       #:guile guile)))
     ;; Take the zip bomb, and simply unpack it as a directory.
     (gexp->derivation (or name file-name)
-                      #~(begin
-                          (mkdir #$output)
-                          (chdir #$output)
-                          (invoke (string-append #$unzip "/bin/unzip")
-                                  #$drv))
+                      (with-imported-modules '((guix build utils))
+                        #~(begin
+                            (use-modules (guix build utils))
+                            (mkdir #$output)
+                            (chdir #$output)
+                            (invoke (string-append #$unzip "/bin/unzip")
+                                    #$drv)))
                       #:local-build? #t)))
 
 (define* (download-to-store store url #:optional (name (basename url))