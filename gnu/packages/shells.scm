91653e074fb92981b9b1a89f1391aedac806e65a
Tobias Geerinckx-Rice <me@tobias.gr>
2018-04-17 03:45:02 +0200

    gnu: loksh: Update to 6.3.
    * gnu/packages/shells.scm (loksh): Update to 6.3.

--- gnu/packages/shells.scm
+++ gnu/packages/shells.scm
@@ -575,7 +575,7 @@ The OpenBSD Korn Shell is a cleaned up and enhanced ksh.")
 (define-public loksh
   (package
     (name "loksh")
-    (version "6.2")
+    (version "6.3")
     (source
      (origin
        (method url-fetch)
@@ -584,7 +584,7 @@ The OpenBSD Korn Shell is a cleaned up and enhanced ksh.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32
-         "16mgxby77wgjg1ybp4fb9cvi7a4a387xmfivdxnhphg0vhyr8qd3"))))
+         "0i1b60g1p19s5cnzz0nmjzjnxywm9szzyp1rcwfcx3gmzvrwr2sc"))))
     (build-system gnu-build-system)
     (inputs
      `(("libbsd" ,libbsd)))
