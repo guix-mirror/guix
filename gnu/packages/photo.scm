a0a553dcf12b3d6ad99237b75c3907bcb6ccaeeb
Marius Bakke <mbakke@fastmail.com>
2018-04-24 20:12:07 +0200

    gnu: libraw: Update to 0.18.9.
    * gnu/packages/photo.scm (libraw): Update to 0.18.9.

--- gnu/packages/photo.scm
+++ gnu/packages/photo.scm
@@ -69,14 +69,14 @@
 (define-public libraw
   (package
     (name "libraw")
-    (version "0.18.8")
+    (version "0.18.9")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.libraw.org/data/LibRaw-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "1qi0fkw2zmd0yplrf79z7lgpz0hxl45dj5rdgpaj7283jzys9b2n"))))
+                "0kmjfdr409k9q9n17k9ywims5z4kqc16s81ba7y09n7669q1gvyj"))))
     (build-system gnu-build-system)
     (home-page "https://www.libraw.org")
     (synopsis "Raw image decoder")
