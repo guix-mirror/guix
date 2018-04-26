c10176649f694e6c5a6cfad6c496abd572d0421b
Tobias Geerinckx-Rice <me@tobias.gr>
2018-04-26 20:51:28 +0200

    gnu: python2-pathlib2: Update to 2.3.2.
    * gnu/packages/python.scm (python2-pathlib2): Update to 2.3.2.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -8175,13 +8175,13 @@ anymore.")
 (define-public python2-pathlib2
   (package
     (name "python2-pathlib2")
-    (version "2.3.0")
+    (version "2.3.2")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "pathlib2" version))
               (sha256
                (base32
-                "1cx5gs2v9j2vnzmcrbq5l8fq2mwrr1h6pyf1sjdji2w1bavm09fk"))))
+                "10yb0iv5x2hs631rcppkhbddx799d3h8pcwmkbh2a66ns3w71ccf"))))
     (build-system python-build-system)
     ;; We only need the the Python 2 variant, since for Python 3 our minimum
     ;; version is 3.4 which already includes this package as part of the
