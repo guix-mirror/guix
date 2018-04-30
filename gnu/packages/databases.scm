1672e7e71482b68192f3a4625cff27fb7c25bd52
Tobias Geerinckx-Rice <me@tobias.gr>
2018-04-30 20:36:47 +0200

    gnu: perl-dbix-class-schema-loader: Update to 0.07049.
    * gnu/packages/databases.scm (perl-dbix-class-schema-loader): Update to 0.07049.

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -1232,7 +1232,7 @@ introspected and examined.")
 (define-public perl-dbix-class-schema-loader
   (package
     (name "perl-dbix-class-schema-loader")
-    (version "0.07047")
+    (version "0.07049")
     (source
      (origin
        (method url-fetch)
@@ -1240,7 +1240,7 @@ introspected and examined.")
                            "DBIx-Class-Schema-Loader-" version ".tar.gz"))
        (sha256
         (base32
-         "06s2q6xj95600sdlfph57spjk2z1gjs4zwq5b7mz7d5izcxgnwb6"))))
+         "0r57fv71ypxafb85cpxph1hdqii7ipjwvc19yb6fpkvq2ggcssg8"))))
     (build-system perl-build-system)
     (native-inputs
      `(("perl-config-any" ,perl-config-any)
