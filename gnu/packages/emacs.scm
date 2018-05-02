b8f910436c9f0593a39b4ec0eeb36ac35b18c02f
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-02 18:57:15 +0300

    gnu: Add emacs-pulseaudio-control.
    * gnu/packages/emacs.scm (emacs-pulseaudio-control): New public variable.

--- gnu/packages/emacs.scm
+++ gnu/packages/emacs.scm
@@ -8479,3 +8479,27 @@ navigate and display hierarchy structures.")
       (description
        "This package allows to read Reddit from within Emacs interactively.")
       (license license:gpl3+))))
+
+(define-public emacs-pulseaudio-control
+  (let ((commit "08c59e1dc45ec96edb62f34036e82cf5f14c0e8b")
+        (revision "1"))
+    (package
+      (name "emacs-pulseaudio-control")
+      (version (string-append "0.0.1" "-" revision "."
+                              (string-take commit 7)))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/flexibeast/pulseaudio-control.git")
+               (commit commit)))
+         (file-name (string-append name "-" version "-checkout"))
+         (sha256
+          (base32
+           "10cgg1r00kz2fsnnryvzay5pf8s1pwb1dzlds1fbjdnyfvdgammv"))))
+      (build-system emacs-build-system)
+      (home-page "https://github.com/flexibeast/pulseaudio-control")
+      (synopsis "Control @code{pulseaudio} from Emacs")
+      (description
+       "This package allows to control @code{pulseaudio} from Emacs.")
+      (license license:gpl3+))))
