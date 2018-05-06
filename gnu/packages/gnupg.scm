7ebc6dc432d0ba051ccae91242f7bb90cb703a5a
Efraim Flashner <efraim@flashner.co.il>
2018-05-06 10:01:59 +0300

    Revert "gnu: gnupg: Update to 2.2.7."
    This reverts commit 0c183f6fb1efe9762cf33f691607a1b2ed2fa57e.

--- gnu/packages/gnupg.scm
+++ gnu/packages/gnupg.scm
@@ -215,14 +215,14 @@ compatible to GNU Pth.")
 (define-public gnupg
   (package
     (name "gnupg")
-    (version "2.2.7")
+    (version "2.2.6")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnupg/gnupg/gnupg-" version
                                   ".tar.bz2"))
               (sha256
                (base32
-                "0vlpis0q7gvq9mhdc43hkyn3cdriz4mwgj20my3gyzpgwqg3cnyr"))))
+                "110rf476l3cgn52gh9ia5y0y06y2ialq9dqc12jkhnfhl9gqqkg6"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
