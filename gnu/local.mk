d2cebbf694a820997b8e0a3f77dd1ec110add3a5
Mark H Weaver <mhw@netris.org>
2018-05-11 03:53:51 -0400

    gnu: icecat: Relabel patches to reflect CVE assignments.
    Document the fact that we include fixes for CVE-2018-5154, CVE-2018-5155,
    CVE-2018-5157, CVE-2018-5158, CVE-2018-5159, CVE-2018-5168, CVE-2018-5178,
    and 9/10 changesets for CVE-2018-5150.
    
    * gnu/packages/gnuzilla.scm (icecat)[source]: Relabel patches to reflect CVE
    assignments.
    * gnu/packages/patches/icecat-bug-1452075.patch: Rename to...
    * gnu/packages/patches/icecat-CVE-2018-5157-and-CVE-2018-5158.patch: ...this.
    * gnu/local.mk (dist_patch_DATA): Rename it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -784,7 +784,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/hurd-fix-eth-multiplexer-dependency.patch        \
   %D%/packages/patches/hydra-disable-darcs-test.patch		\
   %D%/packages/patches/icecat-avoid-bundled-libraries.patch	\
-  %D%/packages/patches/icecat-bug-1452075.patch			\
+  %D%/packages/patches/icecat-CVE-2018-5157-and-CVE-2018-5158.patch	\
   %D%/packages/patches/icecat-use-system-graphite2.patch	\
   %D%/packages/patches/icecat-use-system-harfbuzz.patch		\
   %D%/packages/patches/id3lib-CVE-2007-4460.patch			\
