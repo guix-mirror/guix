e8f4d53e037df2b88d8cadd9f933c03f27e8d0ab
Fis Trivial <ybbs.daans@hotmail.com>
2018-05-12 14:53:56 +0000

    gnu: Add pybind11.
    * gnu/packages/python.scm (pybind11): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -13440,3 +13440,32 @@ MacFUSE.  The binding is created using the standard @code{ctypes} library.")
     (description "@code{gdrivefs} provides a FUSE wrapper for Google Drive
 under Python 2.7.")
     (license license:gpl2)))
+
+(define-public pybind11
+  (package
+    (name "pybind11")
+    (version "2.2.3")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/pybind/pybind11/archive/v"
+                    version ".tar.gz"))
+              (sha256
+               (base32
+                "1sj0x4fwsbnwdai5sxpw1l1vh8m5hpbkfk3zanxcbcgs39jpnfrs"))
+              (file-name (string-append name "-" version ".tar.gz"))))
+    (build-system cmake-build-system)
+    (native-inputs
+     `(("python" ,python)
+       ("python-pytest" ,python-pytest)))
+    (arguments
+     `(#:test-target "check"))
+    (home-page "https://github.com/pybind/pybind11/")
+    (synopsis "Seamless operability between C++11 and Python")
+    (description "pybind11 is a lightweight header-only library that exposes
+C++ types in Python and vice versa, mainly to create Python bindings of
+existing C++ code.  Its goals and syntax are similar to the excellent
+Boost.Python library by David Abrahams: to minimize boilerplate code in
+traditional extension modules by inferring type information using compile-time
+introspection.")
+    (license license:expat)))
