27f44a46df15d8ac073ca463ab403f24ba704cd1
Tobias Geerinckx-Rice <me@tobias.gr>
2018-05-23 01:48:22 +0200

    gnu: tor: Update to 0.3.3.6 [security fixes].
    * gnu/packages/tor.scm (tor): Update to 0.3.3.6.

--- gnu/packages/tor.scm
+++ gnu/packages/tor.scm
@@ -47,14 +47,14 @@
 (define-public tor
   (package
     (name "tor")
-    (version "0.3.2.10")
+    (version "0.3.3.6")
     (source (origin
              (method url-fetch)
              (uri (string-append "https://dist.torproject.org/tor-"
                                  version ".tar.gz"))
              (sha256
               (base32
-               "1vnb2wkcmm8rnz0fqi3k7arl60mpycs8rjn8hvbgv56g3p1pgpv0"))))
+               "1drk2h8zd05xrfpx7xn77pcxz0hs4nrq6figw56qk5gkvgv5kg4r"))))
     (build-system gnu-build-system)
     (arguments
      `(#:configure-flags (list "--enable-gcc-hardening"
