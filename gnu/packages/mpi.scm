guix-mirror/guix

commit 9129804230a3a4272731609f6418b876ba450575

gnu: psm: Only use as an input on supported-systems.
Syntax help supplied by bavier@member.fsf.org

* gnu/packages/linux.scm (libfabric)[inputs]: Only use psm as an input
on systems where psm is supported.
* gnu/packages/mpi.scm (openmpi)[inputs]: Same.

gnu/packages/mpi.scm
@@ -4,6 +4,7 @@
 ;;; Copyright © 2014 Ian Denhardt <ian@zenhack.net>
 ;;; Copyright © 2016 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2017 Dave Love <fx@gnu.org>
+;;; Copyright © 2017 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -37,7 +38,8 @@
   #:use-module (gnu packages perl)
   #:use-module (gnu packages ncurses)
   #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages valgrind))
+  #:use-module (gnu packages valgrind)
+  #:use-module (ice-9 match))
 
 (define-public hwloc
   (package
@@ -126,7 +128,10 @@ bind processes, and much more.")
      `(("hwloc" ,hwloc "lib")
        ("gfortran" ,gfortran)
        ("libfabric" ,libfabric)
-       ("psm" ,psm)
+       ,@(match (%current-system)
+                ((member (package-supported-systems psm))
+                 `(("psm" ,psm)))
+                (_ `()))
        ("rdma-core" ,rdma-core)
        ("valgrind" ,valgrind)))
     (native-inputs

gnu/packages/linux.scm
@@ -4394,7 +4394,10 @@ comparing system environments.")
         (base32 "19l2m1frna1l765z4j7wl8hp4rb9wrh0hy5496685hd183hmy5pv"))))
     (build-system gnu-build-system)
     (inputs `(("rdma-core" ,rdma-core)
-              ("psm" ,psm)
+              ,@(match (%current-system)
+                       ((member (package-supported-systems psm))
+                        `(("psm" ,psm)))
+                       (_ `()))
               ("libnl" ,libnl)))
     (home-page "https://ofiwg.github.io/libfabric/")
     (synopsis "Open Fabric Interfaces")

commit a560641a604c4766acfa2ff015a10c9465a721eb

gnu: openmpi: Reintroduce PSM (Intel TrueScale) support.
Fixes a regression introduced in
9129804230a3a4272731609f6418b876ba450575 whereby PSM would never be
added as an input.

* gnu/packages/mpi.scm (openmpi)[inputs]: Re-add PSM on supported
systems.

gnu/packages/mpi.scm
@@ -168,10 +168,10 @@ bind processes, and much more.")
      `(("hwloc" ,hwloc "lib")
        ("gfortran" ,gfortran)
        ("libfabric" ,libfabric)
-       ,@(match (%current-system)
-                ((member (package-supported-systems psm))
-                 `(("psm" ,psm)))
-                (_ `()))
+       ,@(if (and (not (%current-target-system))
+                  (member (%current-system) (package-supported-systems psm)))
+             `(("psm" ,psm))
+             '())
        ("rdma-core" ,rdma-core)
        ("valgrind" ,valgrind)))
     (native-inputs