26633cb8b305ea25a2cdeb14fd47842d5a9940f1
Marius Bakke <mbakke@fastmail.com>
2018-05-26 17:00:51 +0200

    gnu: samba: Update to 4.8.2.
    * gnu/packages/samba.scm (samba): Update to 4.8.2.

--- gnu/packages/samba.scm
+++ gnu/packages/samba.scm
@@ -148,14 +148,14 @@ anywhere.")
 (define-public samba
   (package
     (name "samba")
-    (version "4.8.1")
+    (version "4.8.2")
     (source (origin
              (method url-fetch)
              (uri (string-append "https://download.samba.org/pub/samba/stable/"
                                  "samba-" version ".tar.gz"))
              (sha256
               (base32
-               "17bqgxyv34hjnb4hfamag75gwhqipp6mpvba5xg7lszi0xskdxwf"))))
+               "08mz29jmjxqvyyhm6pa388paagw1i2i21lc7pd2aprj9dllm5rb2"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
