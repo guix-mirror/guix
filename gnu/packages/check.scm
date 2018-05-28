56e72c0084f14c854502df4cbe69406a3a214416
Fis Trivial <ybbs.daans@hotmail.com>
2018-05-28 08:08:33 +0000

    gnu: Add catch-framework2.
    * gnu/packages/check.scm (catch-framework2): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -161,6 +161,8 @@ supervised tests.")
                (base32
                 "1027cyfx5gsjkdkaf6c2wnjh68882grw8n672018cj3vs9lrhmix"))))))
 
+;; When dependent packages upgraded to use newer version of catch, this one should
+;; be removed.
 (define-public catch-framework
   (package
     (name "catch")
@@ -197,6 +199,24 @@ supervised tests.")
     (description
      "Catch stands for C++ Automated Test Cases in Headers and is a
 multi-paradigm automated test framework for C++ and Objective-C.")
+    (license license:boost1.0)))
+
+(define-public catch-framework2
+  (package
+    (name "catch2")
+    (version "1.12.2")
+    (home-page "https://github.com/catchorg/Catch2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append home-page "/archive/v" version ".tar.gz"))
+              (sha256
+               (base32
+                "0g2ysxc6adqca5wh7nsicnxb9wkxg75cd5izjsl39rcj0v903gr7"))
+              (file-name (string-append name "-" version ".tar.gz"))))
+    (build-system cmake-build-system)
+    (synopsis "Automated test framework for C++ and Objective-C")
+    (description "Catch2 stands for C++ Automated Test Cases in Headers and is
+a multi-paradigm automated test framework for C++ and Objective-C.")
     (license license:boost1.0)))
 
 (define-public cmdtest
