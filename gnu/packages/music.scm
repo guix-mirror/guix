42639cc98091518c6bbb09db24925c2c599a0a2d
Ricardo Wurmus <rekado@elephly.net>
2018-06-05 23:20:42 +0200

    gnu: qtractor: Update to 0.9.1.
    * gnu/packages/music.scm (qtractor): Update to 0.9.1.
    [arguments]: Add phase "fix-build-with-qt-5.11".

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -2174,16 +2174,28 @@ from the command line.")
 (define-public qtractor
   (package
     (name "qtractor")
-    (version "0.9.0")
+    (version "0.9.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "http://downloads.sourceforge.net/qtractor/"
                                   "qtractor-" version ".tar.gz"))
               (sha256
                (base32
-                "03892177k3jn2bsi366dhq28rcdsc1p9v5qqc0k6hg3cnrkh23na"))))
+                "07csbqr7q4m1j0pqg89kn7jdw0snd5lwna5rha0986s4plq4z1qb"))))
     (build-system gnu-build-system)
-    (arguments `(#:tests? #f)) ; no "check" target
+    (arguments
+     `(#:tests? #f  ; no "check" target
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'fix-build-with-qt-5.11
+           (lambda _
+             (substitute* "src/qtractorMeter.h"
+               (("#include <QFrame>" m)
+                (string-append "#include <QAction>\n" m)))
+             (substitute* "src/qtractorTrackButton.h"
+               (("#include <QPushButton>" m)
+                (string-append "#include <QAction>\n" m)))
+             #t)))))
     (inputs
      `(("qt" ,qtbase)
        ("qtx11extras" ,qtx11extras)
