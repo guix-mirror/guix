af09dabf70659a891bc1cb392aeff1ff5b8aeded
宋文武 <iyzsong@member.fsf.org>
2018-06-07 22:07:30 +0800

    gnu: Add keepalived.
    * gnu/packages/cluster.scm: New file.
    * gnu/local.mk (GNU_SYSTEM_MODULES): Add it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -96,6 +96,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/packages/chez.scm				\
   %D%/packages/ci.scm				\
   %D%/packages/cinnamon.scm			\
+  %D%/packages/cluster.scm			\
   %D%/packages/cmake.scm			\
   %D%/packages/cobol.scm			\
   %D%/packages/code.scm				\
