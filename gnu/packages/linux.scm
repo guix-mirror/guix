6e5a6e641a12319af35b51d44537cec1c158e099
Marius Bakke <mbakke@fastmail.com>
2018-06-11 17:52:39 +0200

    gnu: efivar: Update to 36.
    * gnu/packages/linux.scm (efivar): Update to 36.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -3921,7 +3921,7 @@ under OpenGL graphics workloads.")
 (define-public efivar
   (package
     (name "efivar")
-    (version "35")
+    (version "36")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/rhinstaller/" name
@@ -3929,7 +3929,7 @@ under OpenGL graphics workloads.")
                                   "-" version ".tar.bz2"))
               (sha256
                (base32
-                "153k2ifyl4giz5fkryxhz8z621diqjy7v25hfga4z94rs32ks0qy"))))
+                "0cqdikspdmj7imc646q0k7hhw10r8spxa22zm1w0ji49131crgwl"))))
     (build-system gnu-build-system)
     (arguments
      `(;; Tests require a UEFI system and is not detected in the chroot.
