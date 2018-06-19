guix-mirror/guix

commit 3f311279d35a00c40536ee37c7808f42ed5fae4b

gnu: guix: Update to 7af5c2a.
* gnu/packages/package-management.scm (guix): Update to 7af5c2a.

gnu/packages/package-management.scm
@@ -100,8 +100,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "0.14.0")
-        (commit "77a1aac6cccc79d7c8085762f610e22e6ebfb43b")
-        (revision 12))
+        (commit "7af5c2a248b6c229187fc850517c84b0917c452b")
+        (revision 13))
     (package
       (name "guix")
 
@@ -117,7 +117,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "0f0agvw3fizy0aaf51vc2d1rbsvxhg6vnzgjb1n1hbyym79f17j6"))
+                  "06kjimcln4ydirgl05qy98kxjyx3l6brxnq1ly7wb85f73s97gix"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments

commit 1fb947f6d7b7d4859d2f2a495d78b4745557b5ca

gnu: guix-daemon: Build in the absence of guile-sqlite3.
Fixes a regression introduced in
3f311279d35a00c40536ee37c7808f42ed5fae4b whereby guix-daemon's configure
would error out due to the lack of guile-sqlite3.

* gnu/packages/package-management.scm (guix-daemon)[inputs]: Remove
"boot-guile", "boot-guile/i686", and "util-linux".
[arguments]: Add #:configure-flags.

gnu/packages/package-management.scm
@@ -310,11 +310,18 @@ the Nix package manager.")
     (inputs
      `(("gnutls" ,gnutls)
        ("guile-git" ,guile-git)
-       ,@(package-inputs guix)))
+       ,@(fold alist-delete (package-inputs guix)
+               '("boot-guile" "boot-guile/i686" "util-linux"))))
+
     (propagated-inputs '())
 
     (arguments
      (substitute-keyword-arguments (package-arguments guix)
+       ((#:configure-flags flags '())
+        ;; Pretend we have those libraries; we don't actually need them.
+        `(append ,flags
+                 '("guix_cv_have_recent_guile_sqlite3=yes"
+                   "guix_cv_have_recent_guile_ssh=yes")))
        ((#:tests? #f #f)
         #f)
        ((#:phases phases '%standard-phases)