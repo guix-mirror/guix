9b63458b09326c66f5c8cda7df977ab54cb9a5b2
Tobias Geerinckx-Rice <me@tobias.gr>
2018-06-21 13:47:00 +0200

    gnu: babl: Update to 0.1.50.
    * gnu/packages/gimp.scm (babl): Update to 0.1.50.

--- gnu/packages/gimp.scm
+++ gnu/packages/gimp.scm
@@ -48,7 +48,7 @@
 (define-public babl
   (package
     (name "babl")
-    (version "0.1.46")
+    (version "0.1.50")
     (source (origin
               (method url-fetch)
               (uri (list (string-append "https://download.gimp.org/pub/babl/"
@@ -62,7 +62,7 @@
                                         "/babl-" version ".tar.bz2")))
               (sha256
                (base32
-                "0nwyhvfca6m35wjcccvwca7fcihzgdfyc012qi703y5d3cxl1hmv"))))
+                "0bavr2y4v88pip7vlca4kwmnksk2qxcvkkdp9jyfi6pzh701sb5m"))))
     (build-system gnu-build-system)
     (home-page "http://gegl.org/babl/")
     (synopsis "Image pixel format conversion library")
