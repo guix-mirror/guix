aaaede49c1aacb43ffec8ad2cf34bcc764b3ff2a
Tobias Geerinckx-Rice <me@tobias.gr>
2018-06-26 17:49:18 +0200

    gnu: r-with-tests: Return #t from all phases.
    * gnu/packages/statistics.scm (r-with-tests)[arguments]: Substitute
    INVOKE for SYSTEM*.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -213,9 +213,9 @@ as.POSIXct(if (\"\" != Sys.getenv(\"SOURCE_DATE_EPOCH\")) {\
                                     "/share/zoneinfo"))
              #t))
          (add-after 'build 'make-info
-          (lambda _ (zero? (system* "make" "info"))))
+          (lambda _ (invoke "make" "info")))
          (add-after 'build 'install-info
-          (lambda _ (zero? (system* "make" "install-info")))))
+          (lambda _ (invoke "make" "install-info"))))
        #:configure-flags
        `(;; We build the recommended packages here, because they are needed in
          ;; order to run the test suite.  We disable them in the r-minimal
