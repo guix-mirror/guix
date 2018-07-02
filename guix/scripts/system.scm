71bf6cb700965cfe5b8f3661315017b022d0aca1
Ludovic Courtès <ludo@gnu.org>
2018-07-02 23:59:52 +0200

    guix system: init: Check the available space before copying.
    * guix/scripts/system.scm (copy-closure): Call 'query-path-info*' on
    TO-COPY and REFS.  Compute the total size.  Call 'check-available-space'.

--- guix/scripts/system.scm
+++ guix/scripts/system.scm
@@ -148,11 +148,17 @@ REFERENCES as its set of references."
   "Copy ITEM and all its dependencies to the store under root directory
 TARGET, and register them."
   (mlet* %store-monad ((to-copy (topologically-sorted* (list item)))
-                       (refs    (mapm %store-monad references* to-copy)))
+                       (refs    (mapm %store-monad references* to-copy))
+                       (info    (mapm %store-monad query-path-info*
+                                      (delete-duplicates
+                                       (append to-copy (concatenate refs)))))
+                       (size -> (reduce + 0 (map path-info-nar-size info))))
     (define progress-bar
       (progress-reporter/bar (length to-copy)
                              (format #f (G_ "copying to '~a'...")
                                      target)))
+
+    (check-available-space size target)
 
     (call-with-progress-reporter progress-bar
       (lambda (report)
