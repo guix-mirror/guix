469cc535f4b07e861a0873ac8f4c63242466a41f
Danny Milosavljevic <dannym@scratchpost.org>
2018-07-10 20:41:26 +0200

    gnu: fc-host-tools: Update description.
    * gnu/packages/embedded.scm (fc-host-tools)[description]: Modify.

--- gnu/packages/embedded.scm
+++ gnu/packages/embedded.scm
@@ -1092,6 +1092,10 @@ feeding melodies to be played to it.
 that can be issued through the RVTMUX (debug trace) serial channel.
 This program is our test mode shell for sending Test Mode commands to targets
 and displaying decoded target responses.
+@item fcup-smsend Send a short message via SMS
+@item fcup-smsendmult Send multiple short messages via SMS in one go
+@item fcup-smsendpdu Send multiple short messages given in PDU format via SMS
+@item sms-pdu-decode Decode PDU format messages
 @end enumerate")
     (home-page "https://www.freecalypso.org/")
     (license license:public-domain)))
