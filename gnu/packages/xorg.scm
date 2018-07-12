2c3434150baf75ca71410c1968852d63b13c527a
Marius Bakke <mbakke@fastmail.com>
2018-07-12 16:33:00 +0200

    gnu: xf86-input-libinput: Update to 0.28.0.
    * gnu/packages/xorg.scm (xf86-input-libinput): Update to 0.28.0.

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -2447,7 +2447,7 @@ including most mice, keyboards, tablets and touchscreens.")
 (define-public xf86-input-libinput
   (package
     (name "xf86-input-libinput")
-    (version "0.27.1")
+    (version "0.28.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -2455,7 +2455,7 @@ including most mice, keyboards, tablets and touchscreens.")
                     name "-" version ".tar.bz2"))
               (sha256
                (base32
-                "1riflw6dc8fp5d74i7zfgsss0zz3z3lsj6zn2lzjm5kgmp2qvbfl"))))
+                "189h8vl0005yizwrs4d0sng6j8lwkd3xi1zwqg8qavn2bw34v691"))))
     (build-system gnu-build-system)
     (arguments
      '(#:configure-flags
