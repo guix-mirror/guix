0728f60074e407a014d028d36d7cb891a35c597f
Efraim Flashner <efraim@flashner.co.il>
2018-07-15 11:04:10 +0300

    gnu: fltk: Update to 1.3.4-2.
    * gnu/packages/fltk.scm (fltk): Update to 1.3.4-2.
    [source]: Remove patches.
    [inputs]: Switch from libjpeg-8 to libjpeg.
    * gnu/packages/patches/fltk-shared-lib-defines.patch,
    gnu/packages/patches/fltk-xfont-on-demand.patch: Remove files.
    * gnu/local.mk (dist_patch_DATA): Remove them.

--- gnu/local.mk
+++ gnu/local.mk
@@ -676,8 +676,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/findutils-test-xargs.patch		\
   %D%/packages/patches/flann-cmake-3.11.patch			\
   %D%/packages/patches/flint-ldconfig.patch			\
-  %D%/packages/patches/fltk-shared-lib-defines.patch		\
-  %D%/packages/patches/fltk-xfont-on-demand.patch		\
   %D%/packages/patches/foomatic-filters-CVE-2015-8327.patch	\
   %D%/packages/patches/foomatic-filters-CVE-2015-8560.patch	\
   %D%/packages/patches/freeimage-CVE-2015-0852.patch		\
