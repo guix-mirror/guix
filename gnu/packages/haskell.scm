89b48821767d78fede67065028ee0a0f3f172acb
Christopher Baines <mail@cbaines.net>
2018-07-17 20:46:19 +0100

    gnu: Remove duplicated ghc-th-lift-instances.
    It was first added in a6d1c4e75a76399c8d82a9570db5cd14351f661b, then again in
    ff7dd11f0a4fd6ca947cfa40cec42628fde44667.
    
    * gnu/packages/haskell.scm (ghc-th-lift-instances): Remove duplicated package
    definition.

--- gnu/packages/haskell.scm
+++ gnu/packages/haskell.scm
@@ -9221,31 +9221,6 @@ connections.")
 from aeson.")
     (license license:bsd-3)))
 
-(define-public ghc-th-lift-instances
-  (package
-    (name "ghc-th-lift-instances")
-    (version "0.1.11")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "https://hackage.haskell.org/package/"
-                           "th-lift-instances-" version "/"
-                           "th-lift-instances-" version ".tar.gz"))
-       (sha256
-        (base32
-         "1f56cp6ckcalld5jchv0kxpjkwcsixd7smd0g7r8cg67ppx6m90x"))))
-    (build-system haskell-build-system)
-    (inputs `(("ghc-th-lift" ,ghc-th-lift)
-              ("ghc-vector" ,ghc-vector)
-              ("ghc-text" ,ghc-text)))
-    (native-inputs `(("ghc-quickcheck" ,ghc-quickcheck)))
-    (home-page "https://github.com/bennofs/th-lift-instances")
-    (synopsis "Lift instances for template-haskell for common data types")
-    (description "Most data types in the Haskell platform do not have Lift
-instances.  This package provides orphan instances for containers, text,
-bytestring and vector.")
-    (license license:bsd-3)))
-
 (define-public ghc-generics-sop
   (package
     (name "ghc-generics-sop")
