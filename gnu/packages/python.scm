6465497fb9a8980bc010ec8f68b320dc1a73b5cb
Christopher Baines <mail@cbaines.net>
2018-07-17 21:13:38 +0100

    gnu: Remove duplicated python-pafy.
    It was first added in 02f8f804f0242df885311715412a1e3ee5436f3b, then again in
    2dd12924cf4a30a96262b6d392fcde58c9f10d4b.
    
    * gnu/packages/python.scm (python-pafy): Remove duplicated package definition.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -1747,30 +1747,6 @@ matching them against a list of media-ranges.")
      `(#:tests? #f
        ,@(package-arguments python2-funcsigs)))))
 
-(define-public python-pafy
-  (package
-    (name "python-pafy")
-    (version "0.5.3.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "pafy" version))
-       (sha256
-        (base32
-         "1a7dxi95m1043rxx1r5x3ngb66nwlq6aqcasyqqjzmmmjps4zrim"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:tests? #f)) ; Currently pafy can not find itself in the tests
-    (propagated-inputs
-     ;; Youtube-dl is a python package which is imported in the file
-     ;; "backend_youtube_dl.py", therefore it needs to be propagated.
-     `(("youtube-dl" ,youtube-dl)))
-    (home-page "https://np1.github.io/pafy/")
-    (synopsis "Retrieve YouTube content and metadata")
-    (description
-     "@code{pafy} is a python library to retrieve YouTube content and metadata.")
-    (license license:lgpl3+)))
-
 (define-public python-py
   (package
     (name "python-py")
