1d3de9796778f5a11dc91c666ec2f19e8dd56969
Marius Bakke <mbakke@fastmail.com>
2018-07-18 14:37:08 +0200

    gnu: libgc: Update to 7.6.6.
    * gnu/packages/bdw-gc.scm (libgc): Update to 7.6.6.
    [source](uri): Change to the new download location, as noted on the home page.

--- gnu/packages/bdw-gc.scm
+++ gnu/packages/bdw-gc.scm
@@ -30,14 +30,14 @@
 (define-public libgc
   (package
    (name "libgc")
-   (version "7.6.4")
+   (version "7.6.6")
    (source (origin
             (method url-fetch)
-            (uri (string-append "http://www.hboehm.info/gc/gc_source/gc-"
-                                version ".tar.gz"))
+            (uri (string-append "https://github.com/ivmai/bdwgc/releases"
+                                "/download/v" version "/gc-" version ".tar.gz"))
             (sha256
              (base32
-              "076dzsqqyxd3nlzs0z277vvhqjp8nv5dqi763s0m90zr6ljiyk5r"))))
+              "1p1r015a7jbpvkkbgzv1y8nxrbbp6dg0mq3ksi6ji0qdz3wfss79"))))
    (build-system gnu-build-system)
    (arguments
     `(#:configure-flags
