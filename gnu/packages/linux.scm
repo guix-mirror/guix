7da1db5b5799a8e18eeaf1a391d567e48028d87e
Mark H Weaver <mhw@netris.org>
2018-07-18 22:12:57 -0400

    gnu: linux-libre: Update to 4.17.8.
    * gnu/packages/linux.scm (%linux-libre-version): Update to 4.17.8.
    (%linux-libre-hash): Update hash.
    (linux-libre): Remove patch.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -404,25 +404,14 @@ It has been modified to remove all non-free binary blobs.")
 ;; supports qemu "virt" machine and possibly a large number of ARM boards.
 ;; See : https://wiki.debian.org/DebianKernel/ARMMP.
 
-(define %linux-libre-version "4.17.7")
-(define %linux-libre-hash "1815h0kf6c07zp9782v9n9a21x8wzdr4ws190nqp7z2xyip6xfjj")
+(define %linux-libre-version "4.17.8")
+(define %linux-libre-hash "0anq35i9x0kslz42g73vq8wbg56rmrcb38j8mgnhc5c99037gkwr")
 
 (define-public linux-libre
   (make-linux-libre %linux-libre-version
                     %linux-libre-hash
                     %linux-compatible-systems
-                    #:configuration-file kernel-config
-                    #:patches
-                    (list
-                     (origin
-                       (method url-fetch)
-                       (uri "https://git.kernel.org/pub/scm/linux/kernel/git/\
-stable/linux.git/patch/?id=27d8b7daf73827e8263ab0b740da6ebdda9f9d56")
-                       (file-name "linux-libre-4.17.7-fix-i686.patch")
-                       (sha256
-                        (base32
-                         "0xbv04vwhyk3wvia13ca6kbysjqwsywcj6501i2wpddawm4q23sc")))
-                     %boot-logo-patch)))
+                    #:configuration-file kernel-config))
 
 (define %linux-libre-4.14-version "4.14.56")
 (define %linux-libre-4.14-hash "15a4gsi1nsjab16qwfizc0bdn0h27w54qy2a50847i0ln4akdrzl")
