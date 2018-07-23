fdf5344a23359ead55df86e7f1da9f0545bc2d97
Marius Bakke <mbakke@fastmail.com>
2018-07-23 21:21:55 +0200

    gnu: python-pycodestyle: Update to 2.4.0.
    * gnu/packages/python.scm (python-pycodestyle): Update to 2.4.0.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -3806,14 +3806,14 @@ as the original project seems to have been abandoned circa 2007.")
 (define-public python-pycodestyle
   (package
     (name "python-pycodestyle")
-    (version "2.3.1")
+    (version "2.4.0")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "pycodestyle" version))
         (sha256
           (base32
-            "0rk78b66p57ala26mdldl9lafr48blv5s659sah9q50qnfjmc8k8"))))
+            "0fhy4vnlgpjq4qd1wdnl6pvdw7rah0ypmn8c9mkhz8clsndskz6b"))))
     (build-system python-build-system)
     (home-page "https://pycodestyle.readthedocs.io/")
     (synopsis "Python style guide checker")
