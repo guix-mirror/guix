42531fd3a7cf117e20806a9f7ed4df1c16b9ddd9
Efraim Flashner <efraim@flashner.co.il>
2018-07-24 17:14:55 +0300

    gnu: Add python-docker-pycreds.
    * gnu/packages/docker.scm (python-docker-pycreds): New variable.

--- gnu/packages/docker.scm
+++ gnu/packages/docker.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2016 David Thompson <davet@gnu.org>
+;;; Copyright © 2018 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -24,6 +25,7 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system python)
   #:use-module (guix utils)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-web))
 
@@ -102,4 +104,41 @@ client.")
 multi-container Docker applications.  A Compose file is used to configure an
 application’s services.  Then, using a single command, the containers are
 created and all the services are started as specified in the configuration.")
+    (license license:asl2.0)))
+
+(define-public python-docker-pycreds
+  (package
+    (name "python-docker-pycreds")
+    (version "0.3.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (pypi-uri "docker-pycreds" version))
+        (sha256
+         (base32
+          "1zxvam1q22qb0jf48553nnncnfrcp88ag4xa0qmq6vr0imn9a3lb"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'fix-versioning
+           (lambda _
+             (substitute* "test-requirements.txt"
+               (("3.0.2") ,(package-version python-pytest))
+               (("2.3.1") ,(package-version python-pytest-cov))
+               (("2.4.1") ,(package-version python-flake8)))
+             #t)))))
+    (native-inputs
+     `(("python-flake8" ,python-flake8)
+       ("python-pytest" ,python-pytest)
+       ("python-pytest-cov" ,python-pytest-cov)))
+    (propagated-inputs
+     `(("python-six" ,python-six)))
+    (home-page "https://github.com/shin-/dockerpy-creds")
+    (synopsis
+     "Python bindings for the Docker credentials store API")
+    (description
+     "Docker-Pycreds contains the Python bindings for the docker credentials
+store API.  It allows programmers to interact with a Docker registry using
+Python without keeping their credentials in a Docker configuration file.")
     (license license:asl2.0)))
