02062f42e66234fbb414c35abd75123f0ad2146d
Efraim Flashner <efraim@flashner.co.il>
2018-07-24 22:38:03 +0300

    Revert "gnu: sddm: Fix CMake build."
    This reverts commit 8674abb1dda0278f81b5965985e6b78497aab386.
    
    This is no longer necessary after the upgrade to Qt-5.11.1.

--- gnu/packages/display-managers.scm
+++ gnu/packages/display-managers.scm
@@ -6,7 +6,6 @@
 ;;; Copyright © 2017 Sou Bunnbu <iyzsong@gmail.com>
 ;;; Copyright © 2017 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2017 Tobias Geerinckx-Rice <me@tobias.gr>
-;;; Copyright © 2018 Stefan Stefanović <stefanx2ovic@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -144,8 +143,7 @@ Qt-style API for Wayland clients.")
                     "sddm-" version ".tar.xz"))
               (sha256
                (base32
-                "0ch6rdppgy2vbzw0c2x9a4c6ry46vx7p6b76d8xbh2nvxh23xv0k"))
-              (patches (search-patches "sddm-fix-build-with-qt-5.11-1024.patch"))))
+                "0ch6rdppgy2vbzw0c2x9a4c6ry46vx7p6b76d8xbh2nvxh23xv0k"))))
     (build-system cmake-build-system)
     (native-inputs
      `(("extra-cmake-modules" ,extra-cmake-modules)
