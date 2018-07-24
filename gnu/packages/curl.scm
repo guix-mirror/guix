dcf105da63993abd25e76ae8b152deb09baf5cf9
Marius Bakke <mbakke@fastmail.com>
2018-07-24 20:46:41 +0200

    gnu: curl: Remove graft for 7.61.0.
    * gnu/packages/curl.scm (curl): Update to 7.61.0.
    [replacement]: Remove field.
    (curl-7.61.0): Remove variable.

--- gnu/packages/curl.scm
+++ gnu/packages/curl.scm
@@ -50,15 +50,14 @@
 (define-public curl
   (package
    (name "curl")
-   (version "7.59.0")
-   (replacement curl-7.61.0)
+   (version "7.61.0")
    (source (origin
             (method url-fetch)
             (uri (string-append "https://curl.haxx.se/download/curl-"
                                 version ".tar.xz"))
             (sha256
              (base32
-              "1z310hrjm2vmbcpkyp81dcmj9rk127zkjyawpy2pah0nz6yslkp4"))))
+              "080p9r2kln8cbfj0rqfn6wqp5kdn9k5wp720nirkcw845lcmavpg"))))
    (build-system gnu-build-system)
    (outputs '("out"
               "doc"))                             ;1.2 MiB of man3 pages
@@ -140,19 +139,6 @@ tunneling, and so on.")
    (license (license:non-copyleft "file://COPYING"
                                   "See COPYING in the distribution."))
    (home-page "https://curl.haxx.se/")))
-
-(define-public curl-7.61.0
-  (package
-    (inherit curl)
-    (version "7.61.0")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append "https://curl.haxx.se/download/curl-"
-                            version ".tar.xz"))
-        (sha256
-         (base32
-          "080p9r2kln8cbfj0rqfn6wqp5kdn9k5wp720nirkcw845lcmavpg"))))))
 
 (define-public kurly
   (package
