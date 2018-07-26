guix-mirror/guix

commit 3e95125e9bd0676d4a9add9105217ad3eaef3ff0

Revert "packages: Enable threaded compression of source tarballs."
Threaded compression makes tarballs non-deterministic: the result depends on
the number of threads used for compressing.  See <https://bugs.gnu.org/31015>.

This reverts commit c8a3dea847bb9f87fa1876d0c6c3356d6226f121.

guix/packages.scm
@@ -629,11 +629,6 @@ specifies modules in scope when evaluating SNIPPET."
               (apply invoke
                      (string-append #+tar "/bin/tar")
                      "cvf" #$output
-                     ;; The bootstrap xz does not support
-                     ;; threaded compression (introduced in
-                     ;; 5.2.0), but it ignores the extra flag.
-                     (string-append "--use-compress-program="
-                                    #+xz "/bin/xz --threads=0")
                      ;; avoid non-determinism in the archive
                      "--mtime=@0"
                      "--owner=root:0"

commit 28c4905b123e7de85e5d2b654f4d15289625f2c0

packages: 'patch-and-repack' compresses tarballs again.
* guix/packages.scm (patch-and-repack): Add missing "a" in "tar cvfa".
Fixes a regression introduced in
3e95125e9bd0676d4a9add9105217ad3eaef3ff0 whereby we'd always create
uncompressed tarballs.

guix/packages.scm
@@ -628,7 +628,7 @@ specifies modules in scope when evaluating SNIPPET."
                                           #:fail-on-error? #t)))))
               (apply invoke
                      (string-append #+tar "/bin/tar")
-                     "cvf" #$output
+                     "cvfa" #$output
                      ;; avoid non-determinism in the archive
                      "--mtime=@0"
                      "--owner=root:0"