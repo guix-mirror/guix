2fcbc572f9c0f922150ed6fbca6482b632a745ee
Marius Bakke <mbakke@fastmail.com>
2018-07-27 13:48:19 +0200

    gnu: mbedtls-apache: Update to 2.7.5.
    * gnu/packages/tls.scm (mbedtls-apache): Update to 2.7.5.

--- gnu/packages/tls.scm
+++ gnu/packages/tls.scm
@@ -824,7 +824,7 @@ then ported to the GNU / Linux environment.")
 (define-public mbedtls-apache
   (package
     (name "mbedtls-apache")
-    (version "2.7.4")
+    (version "2.7.5")
     (source
      (origin
        (method url-fetch)
@@ -834,7 +834,7 @@ then ported to the GNU / Linux environment.")
                            version "-apache.tgz"))
        (sha256
         (base32
-         "1x9qia3rd77brz6qiv46w3ham2q78shn2rsz1jbpgqq0jpa69q9l"))))
+         "0h4vks2z68bkwzg093mn0a7aqsva8rxr4m971n4bkasa17cjlc51"))))
     (build-system cmake-build-system)
     (arguments
      `(#:configure-flags
