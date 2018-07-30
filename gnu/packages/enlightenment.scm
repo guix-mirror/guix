967de7c5b0100e49acf2ea1fd85bf8503b9d2057
Efraim Flashner <efraim@flashner.co.il>
2018-07-30 12:36:45 +0300

    gnu: Add ephoto.
    * gnu/packages/enlightenment.scm (ephoto): New variable.

--- gnu/packages/enlightenment.scm
+++ gnu/packages/enlightenment.scm
@@ -421,3 +421,50 @@ and in creating applications based on the Enlightenment Foundation Library suite
      "Simple PDF viewer based on the Enlightenment Foundation Libraries.")
     (home-page "https://github.com/kaihu/lekha")
     (license license:gpl3+)))
+
+(define-public ephoto
+  (package
+    (name "ephoto")
+    (version "1.5")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (list (string-append "http://www.smhouston.us/stuff/ephoto-"
+                                  version ".tar.xz")
+                   (string-append "https://download.enlightenment.org/rel/"
+                                  "apps/ephoto/ephoto-" version ".tar.xz")))
+        (sha256
+         (base32
+          "04kli43sfsy6s660g13pjc0kjmgdcmq8m4qh02vvpcwv60mf9mgz"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'set-home-directory
+           ;; FATAL: Cannot create run dir '/homeless-shelter/.run' - errno=2
+           (lambda _ (setenv "HOME" "/tmp") #t)))))
+    (native-inputs
+     `(("check" ,check)
+       ("pkg-config" ,pkg-config)))
+    (inputs
+     `(("efl" ,efl)))
+    (home-page "http://smhouston.us/ephoto/")
+    (synopsis "EFL image viewer/editor/manipulator/slideshow creator")
+    (description "Ephoto is an image viewer and editor written using the
+@dfn{Enlightenment Foundation Libraries} (EFL).  It focuses on simplicity and ease
+of use, while taking advantage of the speed and small footprint the EFL provide.
+
+Ephoto’s features include:
+@enumerate
+@item Browsing the filesystem and displaying images in an easy to use grid view.
+@item Browsing images in a single image view format.
+@item Viewing images in a slideshow.
+@item Editing your image with features such as cropping, auto enhance,
+blurring, sharpening, brightness/contrast/gamma adjustments, hue/saturation/value
+adjustments, and color level adjustment.
+@item Applying artistic filters to your image such as black and white and old photo.
+@item Drag And Drop along with file operations to easy maintain your photo directories.
+@end enumerate\n")
+    (license (list
+               license:bsd-2 ; Ephoto's thumbnailing code
+               license:bsd-3))))
