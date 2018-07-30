e4a744da9f772b0073759b4c09f622fd50159944
Kei Kebreau <kkebreau@posteo.net>
2018-07-30 13:16:04 -0400

    gnu: minetest: Update to 0.4.17.1.
    * gnu/packages/games.scm (minetest): Update to 0.4.17.1.

--- gnu/packages/games.scm
+++ gnu/packages/games.scm
@@ -1618,7 +1618,7 @@ match, cannon keep, and grave-itation pit.")
 (define-public minetest
   (package
     (name "minetest")
-    (version "0.4.17")
+    (version "0.4.17.1")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1627,7 +1627,7 @@ match, cannon keep, and grave-itation pit.")
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0wpbad5bssbbgspgdcq3hhq4bhckrj53nhymsz34d8g01j0csr46"))))
+                "0ngb3h6hw0zbsr6isjpyp4fach0g4nbn6bxxv9g354plac6d89fd"))))
     (build-system cmake-build-system)
     (arguments
      '(#:configure-flags
