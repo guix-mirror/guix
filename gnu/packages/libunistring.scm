c0b3867c21d60c35dca6ecd44f00dc81afa87476
Marius Bakke <mbakke@fastmail.com>
2018-08-20 15:07:34 +0200

    gnu: libunistring: Do install static library.
    ...but do so to a separate output.  This reverts commit
    1f2d64e4a86fbd77cfd7c773a9621a001516e1ce.
    
    * gnu/packages/libunistring.scm (libunistring)[outputs]: New field.
    [arguments]: Add phase for moving libunistring.a.  Remove #:configure-flags.

--- gnu/packages/libunistring.scm
+++ gnu/packages/libunistring.scm
@@ -6,6 +6,7 @@
 ;;; Copyright © 2017 Mathieu Othacehe <m.othacehe@gmail.com>
 ;;; Copyright © 2017 Eric Bavier <bavier@member.fsf.org>
 ;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2018 Marius Bakke <mbakke@fastmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -43,13 +44,23 @@
              (base32
               "1mq57h06622m6qc5cv347fc3qk5mj840axw3c0vd7qmnwk1v53zb"))))
    (propagated-inputs (libiconv-if-needed))
+   (outputs '("out" "static"))
    (build-system gnu-build-system)
    (arguments
     ;; Work around parallel build issue whereby C files may be compiled before
     ;; config.h is built: see <http://hydra.gnu.org/build/59381/nixlog/2/raw> and
     ;; <http://lists.openembedded.org/pipermail/openembedded-core/2012-April/059850.html>.
     '(#:parallel-build? #f
-      #:configure-flags '("--disable-static")))
+      #:phases (modify-phases %standard-phases
+                 (add-after 'install 'move-static-library
+                   (lambda* (#:key outputs #:allow-other-keys)
+                     (let ((out (assoc-ref outputs "out"))
+                           (static (assoc-ref outputs "static")))
+                       (with-directory-excursion (string-append out "/lib")
+                         (install-file "libunistring.a"
+                                       (string-append static "/lib"))
+                         (delete-file "libunistring.a")
+                         #t)))))))
    (synopsis "C library for manipulating Unicode strings")
    (description
     "GNU libunistring is a library providing functions to manipulate
