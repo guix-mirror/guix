guix-mirror/guix

commit 7cbf06d8c2935abfc6c688cf3f9b99e0e5393960

gnu: mysql: Fix build.
MySQL demands boost@1.59.0, and lying about it no longer works:

sql/item_geofunc_internal.cc: In function ‘void handle_gis_exception(const char*)’:
sql/item_geofunc_internal.cc:37:81: error: expected unqualified-id before ‘&’ token
   catch (const boost::geometry::detail::self_get_turn_points::self_ip_exception &)
                                                                                 ^
[...]

* gnu/packages/databases.scm (boost-for-mysql): New variable.
(mysql)[inputs]: Use that instead of the regular boost.
[arguments]: Remove now-unnecessary ‘patch-boost-version’ phase.

gnu/packages/databases.scm
@@ -548,6 +548,22 @@ RDBMS systems (which are deep in functionality).")
                    ;; Some parts are licensed under the Apache License
                    license:asl2.0))))
 
+(define boost-for-mysql
+  (package
+    (inherit boost)
+    (version "1.59.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://sourceforge/boost/boost/" version "/boost_"
+                    (string-map (lambda (x) (if (eq? x #\.) #\_ x)) version)
+                    ".tar.bz2"))
+              (sha256
+               (base32
+                "1jj1aai5rdmd72g90a3pd8sw9vi32zad46xv5av8fhnr48ir6ykj"))))))
+
+;; XXX When updating, check whether boost-for-mysql is still needed.
+;; It might suffice to patch ‘cmake/boost.cmake’ as done in the past.
 (define-public mysql
   (package
     (name "mysql")
@@ -589,15 +605,6 @@ RDBMS systems (which are deep in functionality).")
          "-DINSTALL_SQLBENCHDIR=")
        #:phases (modify-phases %standard-phases
                   (add-after
-                   'unpack 'patch-boost-version
-                   (lambda _
-                     ;; Mysql wants boost-1.59.0 specifically
-                     (substitute* "cmake/boost.cmake"
-                       (("59")
-                        ,(match (string-split (package-version boost) #\.)
-                           ((_ minor . _) minor))))
-                     #t))
-                  (add-after
                    'install 'remove-extra-binaries
                    (lambda* (#:key outputs #:allow-other-keys)
                      (let ((out (assoc-ref outputs "out")))
@@ -611,7 +618,7 @@ RDBMS systems (which are deep in functionality).")
      `(("bison" ,bison)
        ("perl" ,perl)))
     (inputs
-     `(("boost" ,boost)
+     `(("boost" ,boost-for-mysql)
        ("libaio" ,libaio)
        ("ncurses" ,ncurses)
        ("openssl" ,openssl)

commit 444488fc1b0d12190bf751b58191daea90ab243c

gnu: boost: Move 1.59.0 definition to boost.scm.
Fixes a regression introduced in commit
7cbf06d8c2935abfc6c688cf3f9b99e0e5393960 where the top-level code of
these two modules would depend on each other, leading to "boost: unbound
variable" kind of errors in some circumstances.

* gnu/packages/databases.scm (boost-for-mysql): Move to...
* gnu/packages/boost.scm (boost-for-mysql): ... here.  Make public and
add 'properties' field.

gnu/packages/boost.scm
@@ -110,6 +110,22 @@ across a broad spectrum of applications.")
     (license (license:x11-style "https://www.boost.org/LICENSE_1_0.txt"
                                 "Some components have other similar licences."))))
 
+(define-public boost-for-mysql
+  ;; Older version for MySQL 5.7.23.
+  (package
+    (inherit boost)
+    (version "1.59.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://sourceforge/boost/boost/" version "/boost_"
+                    (string-map (lambda (x) (if (eq? x #\.) #\_ x)) version)
+                    ".tar.bz2"))
+              (sha256
+               (base32
+                "1jj1aai5rdmd72g90a3pd8sw9vi32zad46xv5av8fhnr48ir6ykj"))))
+    (properties '((hidden? . #t)))))
+
 (define-public boost-sync
   (let ((commit "c72891d9b90e2ceb466ec859f640cd012b2d8709")
         (version "1.55")

gnu/packages/databases.scm
@@ -549,20 +549,6 @@ RDBMS systems (which are deep in functionality).")
                    ;; Some parts are licensed under the Apache License
                    license:asl2.0))))
 
-(define boost-for-mysql
-  (package
-    (inherit boost)
-    (version "1.59.0")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "mirror://sourceforge/boost/boost/" version "/boost_"
-                    (string-map (lambda (x) (if (eq? x #\.) #\_ x)) version)
-                    ".tar.bz2"))
-              (sha256
-               (base32
-                "1jj1aai5rdmd72g90a3pd8sw9vi32zad46xv5av8fhnr48ir6ykj"))))))
-
 ;; XXX When updating, check whether boost-for-mysql is still needed.
 ;; It might suffice to patch ‘cmake/boost.cmake’ as done in the past.
 (define-public mysql