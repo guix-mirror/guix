7eee87e65d9e2da183887ade425a29de1a1f024d
Timothy Sample <samplet@ngyro.com>
2018-08-29 01:27:50 -0400

    gnu: ghc-persistent-sqlite: Update to 2.8.2.
    * gnu/packages/haskell.scm (ghc-persistent-sqlite): Update to 2.8.2.

--- gnu/packages/haskell.scm
+++ gnu/packages/haskell.scm
@@ -10217,7 +10217,7 @@ working with it.")
 (define-public ghc-persistent-sqlite
   (package
     (name "ghc-persistent-sqlite")
-    (version "2.6.4")
+    (version "2.8.2")
     (source
      (origin
        (method url-fetch)
@@ -10226,7 +10226,7 @@ working with it.")
                            "persistent-sqlite-" version ".tar.gz"))
        (sha256
         (base32
-         "16mc2ra0hbyyc8ckjlxxc11bpskdymbr8c3g6ih6wzik639xprbm"))))
+         "1chbmvjz46smhgnzhha3bbkhys3fys6dip1jr4v7xp1jf78zbyp6"))))
     (build-system haskell-build-system)
     (inputs `(("ghc-persistent" ,ghc-persistent)
               ("ghc-unliftio-core" ,ghc-unliftio-core)
