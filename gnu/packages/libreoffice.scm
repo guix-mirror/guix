002d73a01fa5c39930471b81bb8938ab22bf7d93
Tobias Geerinckx-Rice <me@tobias.gr>
2018-09-15 02:38:15 +0200

    gnu: libwps: Update to 0.4.10.
    * gnu/packages/libreoffice.scm (libwps): Update to 0.4.10.

--- gnu/packages/libreoffice.scm
+++ gnu/packages/libreoffice.scm
@@ -695,19 +695,19 @@ from the old StarOffice (.sdc, .sdw, ...).")
 (define-public libwps
   (package
     (name "libwps")
-    (version "0.4.9")
+    (version "0.4.10")
     (source
      (origin
       (method url-fetch)
       (uri (string-append "mirror://sourceforge/" name "/" name "/"
                           name "-" version "/" name "-" version ".tar.xz"))
       (sha256 (base32
-               "08j9nxnrzxsnq35d9l824ad8w8az42fivaxn8ajl85dv6g3v1ghk"))))
+               "1ji9zd4wxmas03g8jyx0ih0amrqfazm5874a2v9rd7va50sf088l"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("doxygen" ,doxygen)
        ("pkg-config" ,pkg-config)))
-    (propagated-inputs ; in Requires field of .pkg
+    (propagated-inputs                  ; in Requires field of .pkg
      `(("librevenge" ,librevenge)))
     (inputs
      `(("boost" ,boost)
@@ -716,7 +716,7 @@ from the old StarOffice (.sdc, .sdw, ...).")
     (synopsis "Import library for Microsoft Works text documents")
     (description "Libwps is a library for importing files in the Microsoft
 Works word processor file format.")
-    (license (list mpl2.0 lgpl2.1+)))) ; dual license
+    (license (list mpl2.0 lgpl2.1+))))  ; dual license
 
 (define-public libzmf
   (package
