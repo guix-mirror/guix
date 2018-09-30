guix-mirror/guix

commit c32863e094d3489d2e08ccb3188988d7c6410976

gnu: sshuttle: Patch FHS assumptions.
Reported-By: Nam Nguyen <namn@berkeley.edu>

* gnu/packages/vpn.scm (sshuttle)[arguments]: Set correct paths to 'env'
and 'sh'.

gnu/packages/vpn.scm
@@ -347,6 +347,17 @@ private network between hosts on the internet.")
         (base32
          "0pqk43kd7crqhg6qgnl8kapncwgw1xgaf02zarzypcw64kvdih9h"))))
     (build-system python-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'patch-FHS-file-names
+           (lambda _
+             (substitute* "sshuttle/client.py"
+               (("/usr/bin/env") (which "env")))
+             (substitute* "sshuttle/ssh.py"
+               ;; Perhaps this is unreachable, but don't let's take risks.
+               (("/bin/sh") (which "sh")))
+             #t)))))
     (native-inputs
      `(("python-setuptools-scm" ,python-setuptools-scm)
        ;; For tests only.

commit 0e2d3ce20e7bd71aaad5baa6b977b9684216b66e

gnu: sshuttle: Rely on $PATH to find a POSIX sh.
This fixes a regression introduced in commit
c32863e094d3489d2e08ccb3188988d7c6410976, where the sshuttle client
will try to invoke its own /gnu/store/.../bin/sh on the server.

Rely on $PATH instead of regressing to upstream's hard-coded /bin/sh.

* gnu/packages/vpn.scm (sshuttle)[arguments]: SUBSTITUTE* "sh" for
"/bin/sh".

gnu/packages/vpn.scm
@@ -355,8 +355,7 @@ private network between hosts on the internet.")
              (substitute* "sshuttle/client.py"
                (("/usr/bin/env") (which "env")))
              (substitute* "sshuttle/ssh.py"
-               ;; Perhaps this is unreachable, but don't let's take risks.
-               (("/bin/sh") (which "sh")))
+               (("/bin/sh") "sh"))
              #t)))))
     (native-inputs
      `(("python-setuptools-scm" ,python-setuptools-scm)