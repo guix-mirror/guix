8d18edde755f4bf5b4723fc9ce89e1cd831d2b7b
Efraim Flashner <efraim@flashner.co.il>
2018-10-09 10:32:16 +0300

    * gnu: agg: Replace bootstrap phase.
    * gnu/packages/graphics.scm (agg)[arguments]: Remove custom 'autoreconf
    phase and replace default 'bootstrap phase. Use INVOKE.
    [native-inputs]: Remove bash.

--- gnu/packages/graphics.scm
+++ gnu/packages/graphics.scm
@@ -709,17 +709,16 @@ and understanding different BRDFs (and other component functions).")
              "--disable-examples")
        #:phases
        (modify-phases %standard-phases
-         (add-after 'unpack 'autoreconf
+         (replace 'bootstrap
            (lambda _
              ;; let's call configure from configure phase and not now
              (substitute* "autogen.sh" (("./configure") "# ./configure"))
-             (zero? (system* "sh" "autogen.sh")))))))
+             (invoke "sh" "autogen.sh"))))))
     (native-inputs
      `(("pkg-config" ,pkg-config)
        ("libtool" ,libtool)
        ("autoconf" ,autoconf)
-       ("automake" ,automake)
-       ("bash" ,bash)))
+       ("automake" ,automake)))
     (inputs
      `(("libx11" ,libx11)
        ("freetype" ,freetype)
