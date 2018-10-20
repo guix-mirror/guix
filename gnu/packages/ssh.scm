df71447e992e82d029ac57d0d721af02fc6a5061
Leo Famulari <leo@famulari.name>
2018-10-20 12:26:51 -0400

    gnu: OpenSSH: Update to 7.9p1.
    * gnu/packages/ssh.scm (openssh): Update to 7.9p1.

--- gnu/packages/ssh.scm
+++ gnu/packages/ssh.scm
@@ -147,14 +147,14 @@ a server that supports the SSH-2 protocol.")
 (define-public openssh
   (package
    (name "openssh")
-   (version "7.8p1")
+   (version "7.9p1")
    (source (origin
              (method url-fetch)
              (uri (string-append "mirror://openbsd/OpenSSH/portable/"
                                  name "-" version ".tar.gz"))
              (sha256
               (base32
-               "1jj4f586r9lhakp2w0zv7j616d6x62m15q8l4nxq7haja6qlnj0s"))))
+               "1b8sy6v0b8v4ggmknwcqx3y1rjcpsll0f1f8f4vyv11x4ni3njvb"))))
    (build-system gnu-build-system)
    (native-inputs `(("groff" ,groff)))
    (inputs `(("openssl" ,openssl)
