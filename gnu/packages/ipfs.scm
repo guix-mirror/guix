96b4977a1924ca648a3cceb15443539104d294f1
Pierre Neidhardt <mail@ambrevar.xyz>
2018-10-24 14:41:09 +0200

    gnu: Add gx-go.
    * gnu/packages/ipfs.scm (gx-go): New variable.

--- gnu/packages/ipfs.scm
+++ gnu/packages/ipfs.scm
@@ -160,3 +160,53 @@ powerful and simple.")
     (arguments
      '(#:unpack-path "github.com/whyrusleeping/gx"
        #:import-path "github.com/whyrusleeping/gx/gxutil"))))
+
+(define-public gx-go
+  (package
+    (name "gx-go")
+    (version "1.9.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/whyrusleeping/gx-go")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "0fdy4b3ymqw6hzvvjwq37mfrdmizc8lxm53axw93n3x6118na9jc"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/whyrusleeping/gx-go"))
+    (native-inputs
+     `(("go-github-com-whyrusleeping-gx-util" ,go-github-com-whyrusleeping-gx-util)
+       ("go-github-com-kr-fs" ,go-github-com-kr-fs)
+       ("go-github-com-gxed-hashland-keccakpg" ,go-github-com-gxed-hashland-keccakpg)
+       ("go-github-com-ipfs-go-ipfs-api" ,go-github-com-ipfs-go-ipfs-api)
+       ("go-github-com-ipfs-go-ipfs-cmdkit-files" ,go-github-com-ipfs-go-ipfs-cmdkit-files)
+       ("go-github-com-libp2p-go-flow-metrics" ,go-github-com-libp2p-go-flow-metrics)
+       ("go-github-com-libp2p-go-libp2p-crypto" ,go-github-com-libp2p-go-libp2p-crypto)
+       ("go-github-com-libp2p-go-libp2p-metrics" ,go-github-com-libp2p-go-libp2p-metrics)
+       ("go-github-com-libp2p-go-libp2p-peer" ,go-github-com-libp2p-go-libp2p-peer)
+       ("go-github-com-libp2p-go-libp2p-protocol" ,go-github-com-libp2p-go-libp2p-protocol)
+       ("go-github-com-minio-blake2b-simd" ,go-github-com-minio-blake2b-simd)
+       ("go-github-com-minio-sha256-simd" ,go-github-com-minio-sha256-simd)
+       ("go-github-com-mitchellh-go-homedir" ,go-github-com-mitchellh-go-homedir)
+       ("go-github-com-mr-tron-base58" ,go-github-com-mr-tron-base58)
+       ("go-github-com-multiformats-go-multiaddr" ,go-github-com-multiformats-go-multiaddr)
+       ("go-github-com-multiformats-go-multiaddr-net" ,go-github-com-multiformats-go-multiaddr-net)
+       ("go-github-com-multiformats-go-multihash" ,go-github-com-multiformats-go-multihash)
+       ("go-github-com-spaolacci-murmur3" ,go-github-com-spaolacci-murmur3)
+       ("go-github-com-whyrusleeping-tar-utils" ,go-github-com-whyrusleeping-tar-utils)
+       ("go-golang-org-x-crypto-union" ,(go-golang-org-x-crypto-union))
+       ("go-github-com-btcsuite-btcd-btcec" ,go-github-com-btcsuite-btcd-btcec)
+       ("go-github-com-gogo-protobuf-proto" ,go-github-com-gogo-protobuf-proto)
+       ("go-github-com-sabhiram-go-gitignore" ,go-github-com-sabhiram-go-gitignore)
+       ("go-github-com-urfave-cli" ,go-github-com-urfave-cli)
+       ("go-github-com-whyrusleeping-progmeter" ,go-github-com-whyrusleeping-progmeter)
+       ("go-github-com-whyrusleeping-stump" ,go-github-com-whyrusleeping-stump)))
+    (home-page "https://github.com/whyrusleeping/gx-go")
+    (synopsis "Golang subtool for the @command{gx} package manager")
+    (description "A subtool for the @command{gx} package manager for packages
+written in Go.")
+    (license license:expat)))
