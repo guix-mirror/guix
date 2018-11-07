7d7c702c03b1f1d1e86b48fe44147227c6447a82
Ricardo Wurmus <rekado@elephly.net>
2018-11-07 12:07:32 +0100

    gnu: r-zlibbioc: Update to 1.28.0.
    * gnu/packages/bioinformatics.scm (r-zlibbioc): Update to 1.28.0.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -8542,13 +8542,13 @@ two-dimensional genome scans.")
 (define-public r-zlibbioc
   (package
     (name "r-zlibbioc")
-    (version "1.26.0")
+    (version "1.28.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "zlibbioc" version))
               (sha256
                (base32
-                "1rwr0mci8a712q0isavi4jmhm94gwivc4nr8j7r4kw05flp4g7gz"))))
+                "0bjvzy24kab7ank02cc1qk2ikcz4dllgf66wpsdl0d3zp4gn3l2h"))))
     (properties
      `((upstream-name . "zlibbioc")))
     (build-system r-build-system)
