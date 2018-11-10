9829e0699b6b0373c1742393f18db261a6ae67a8
Efraim Flashner <efraim@flashner.co.il>
2018-11-10 20:13:20 +0200

    gnu: lbzip2: Remove false comment.
    This is a follow-up to f268b744b84734b374a15feae1f02b9f2c4085c7.
    
    * gnu/packages/compression.scm (lbzip2)[source]: This snippet is, in
    fact, a snippet.

--- gnu/packages/compression.scm
+++ gnu/packages/compression.scm
@@ -367,9 +367,6 @@ decompression.")
              (modules '((guix build utils)))
              (snippet
               '(begin
-                 ;; Adjust the bundled gnulib to work with glibc 2.28.  See e.g.
-                 ;; "m4-gnulib-libio.patch".  This is a phase rather than patch
-                 ;; or snippet to work around <https://bugs.gnu.org/32347>.
                  (substitute* (find-files "lib" "\\.c$")
                    (("#if defined _IO_ftrylockfile")
                     "#if defined _IO_EOF_SEEN"))
