3b144946164641f806fbbf27ac592c7c717ea6c6
Mark H Weaver <mhw@netris.org>
2018-11-10 13:42:50 -0500

    gnu: icecat: Update to 60.3.0-gnu1.
    * gnu/packages/gnuzilla.scm (icecat): Update to 60.3.0-gnu1.
    [source]: Switch back to the normal source URI.  Remove patches that
    are no longer applicable.
    * gnu/packages/patches/icecat-CVE-2018-12383.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -811,7 +811,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/hplip-remove-imageprocessor.patch	\
   %D%/packages/patches/hydra-disable-darcs-test.patch		\
   %D%/packages/patches/icecat-avoid-bundled-libraries.patch	\
-  %D%/packages/patches/icecat-CVE-2018-12383.patch		\
   %D%/packages/patches/icecat-use-system-graphite2+harfbuzz.patch	\
   %D%/packages/patches/icecat-use-system-media-libs.patch	\
   %D%/packages/patches/icedtea-6-hotspot-gcc-segfault-workaround.patch  \
