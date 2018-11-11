23dd886a95379a8aaed5d58025e603e7f9ea659e
Ricardo Wurmus <rekado@elephly.net>
2018-11-11 23:11:33 +0100

    gnu: r-rtsne: Update to 0.15.
    * gnu/packages/statistics.scm (r-rtsne): Update to 0.15.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -3268,14 +3268,14 @@ the way current RNG settings can be changed.")
 (define-public r-rtsne
   (package
     (name "r-rtsne")
-    (version "0.13")
+    (version "0.15")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "Rtsne" version))
        (sha256
         (base32
-         "17crbdi80q4g2pwp9v7j3bdaznk96qlrqx01zvj3wwqippizyfqw"))))
+         "0v17vxizrs1msay24xl2bckfajr2c82wpqj07lyssbrq197nwdsn"))))
     (properties `((upstream-name . "Rtsne")))
     (build-system r-build-system)
     (propagated-inputs
