d22ba64276c0bbacb77d0670635f67f053a16c6b
Pierre Neidhardt <mail@ambrevar.xyz>
2018-11-18 10:38:54 +0100

    gnu: ccl: Include x86-headers and remove missing "contrib" folder.
    * gnu/packages/lisp.scm (ccl): Include x86-headers and remove missing "contrib" folder.

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -530,8 +530,12 @@ statistical profiler, a code coverage tool, and many other extensions.")
                (install-file kernel libdir)
                (install-file heap libdir)
 
-               (let ((dirs '("lib" "library" "examples" "contrib"
-                             "tools" "objc-bridge")))
+               (let ((dirs '("lib" "library" "examples" "tools" "objc-bridge"
+                             ,(match (%current-system)
+                                ("x86_64-linux"
+                                 "x86-headers64")
+                                ("i686-linux"
+                                 "x86-headers")))))
                  (for-each copy-recursively
                            dirs
                            (map (cut string-append libdir <>) dirs)))
