bc6062aef563726fdb1bb7199f2c5628ad967534
Marius Bakke <mbakke@fastmail.com>
2018-11-25 17:56:51 +0100

    gnu: dovecot: Update to 2.3.4.
    * gnu/packages/mail.scm (dovecot): Update to 2.3.4.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -1202,7 +1202,7 @@ facilities for checking incoming mail.")
 (define-public dovecot
   (package
     (name "dovecot")
-    (version "2.3.2.1")
+    (version "2.3.4")
     (source
      (origin
        (method url-fetch)
@@ -1210,7 +1210,7 @@ facilities for checking incoming mail.")
                            (version-major+minor version) "/"
                            name "-" version ".tar.gz"))
        (sha256 (base32
-                "0d2ffbicgl3wswbnyjbw6qigz7r1aqzprpchbwp5cynw122i2raa"))))
+                "01ggzf7b3jpl89mjiqr7xbpbs181g2gjf6wzg70qaqfzz3ppc6yr"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
