b96bd82133420383e58180d725af6c7e178cdc24
Pierre Neidhardt <mail@ambrevar.xyz>
2018-12-05 00:38:44 +0100

    gnu: next-gtk-webkit: Update to 1.1.0.
    * gnu/packages/web-browsers.scm (next-gtk-webkit): Update to 1.1.0.

--- gnu/packages/web-browsers.scm
+++ gnu/packages/web-browsers.scm
@@ -364,45 +364,44 @@ driven and does not detract you from your daily work.")
     (license license:gpl3+)))
 
 (define-public next-gtk-webkit
-  (let ((commit "b8899341bbdefd0a33412608fbb0b1f92f818c65"))
-    (package
-      (name "next-gtk-webkit")
-      (version (git-version "1.0.0" "1" commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://source.atlas.engineer/public/next")
-               (commit commit)))
-         (sha256
-          (base32
-           "12jmf1b9qr85il9h15mb9vpsfh1wzcln9x9xpn4lps0kkccnpkz9"))
-         (file-name (git-file-name "next" version))))
-      (build-system glib-or-gtk-build-system)
-      (arguments
-       `(#:tests? #f                    ; no tests
-         #:make-flags (list "gtk-webkit"
-                            "CC=gcc"
-                            (string-append "PREFIX=" %output))
-         #:phases
-         (modify-phases %standard-phases
-           (delete 'configure)
-           (replace 'install
-             (lambda* (#:key (make-flags '()) #:allow-other-keys)
-               (apply invoke "make" "install-gtk-webkit" make-flags))))))
-      (inputs
-       `(("glib-networking" ,glib-networking)
-         ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
-         ("webkitgtk" ,webkitgtk)))
-      (native-inputs
-       `(("pkg-config" ,pkg-config)))
-      (home-page "https://next.atlas.engineer")
-      (synopsis "Infinitely extensible web-browser (user interface only)")
-      (description "Next is a keyboard-oriented, extensible web-browser
+  (package
+    (name "next-gtk-webkit")
+    (version "1.1.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://source.atlas.engineer/public/next")
+             (commit version)))
+       (sha256
+        (base32
+         "00xi01r6gxlrv7xc2dhf4da30y0vng1snbdmc8d829qyn0chl55q"))
+       (file-name (git-file-name "next" version))))
+    (build-system glib-or-gtk-build-system)
+    (arguments
+     `(#:tests? #f                      ; no tests
+       #:make-flags (list "gtk-webkit"
+                          "CC=gcc"
+                          (string-append "PREFIX=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (replace 'install
+           (lambda* (#:key (make-flags '()) #:allow-other-keys)
+             (apply invoke "make" "install-gtk-webkit" make-flags))))))
+    (inputs
+     `(("glib-networking" ,glib-networking)
+       ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
+       ("webkitgtk" ,webkitgtk)))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)))
+    (home-page "https://next.atlas.engineer")
+    (synopsis "Infinitely extensible web-browser (user interface only)")
+    (description "Next is a keyboard-oriented, extensible web-browser
 inspired by Emacs and designed for power users.  The application has familiar
 key-bindings, is fully configurable and extensible in Lisp, and has powerful
 features for productive professionals.")
-      (license license:bsd-3))))
+    (license license:bsd-3)))
 
 (define-public sbcl-next
   (package
@@ -428,8 +427,6 @@ features for productive professionals.")
                       (define expected-fasl (string-append
                                              lib
                                              "/lib/sbcl/next--system.fasl"))
-                      (pk actual-fasl)
-                      (pk expected-fasl)
                       (copy-file actual-fasl expected-fasl)
                       #t))
                   (add-after 'create-symlinks 'build-program
