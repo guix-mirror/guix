a18f8eaf61dbc359a1ca724c147e4600553247fb
Björn Höfling <bjoern.hoefling@bjoernhoefling.de>
2018-12-05 17:44:28 -0500

    gnu: arc-theme: Update to 20181022 and fix build.
    * gnu/packages/gnome.scm (arc-theme)[version]: Update to 20181022.
    [source]: Use new fork, change to git-fetch.
    [inputs]: Remove and move gtk+ to native-inputs.
    [native-inputs]: Add gtk+, glib:bin, gnome-shell, inkscape, optipng and sassc.
    [home-page]: Adapt to new fork.
    [license]: Fix to GPL 3 or later (gpl3+).
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -31,6 +31,7 @@
 ;;; Copyright © 2017, 2018 Rutger Helling <rhelling@mykolab.com>
 ;;; Copyright © 2018 Jovany Leandro G.C <bit4bit@riseup.net>
 ;;; Copyright © 2018 Vasile Dumitrascu <va511e@yahoo.com>
+;;; Copyright © 2018 Björn Höfling <bjoern.hoefling@bjoernhoefling.de>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -95,6 +96,7 @@
   #:use-module (gnu packages polkit)
   #:use-module (gnu packages popt)
   #:use-module (gnu packages ghostscript)
+  #:use-module (gnu packages inkscape)
   #:use-module (gnu packages ibus)
   #:use-module (gnu packages iso-codes)
   #:use-module (gnu packages libcanberra)
@@ -151,6 +153,7 @@
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages virtualization)
   #:use-module (gnu packages vpn)
+  #:use-module (gnu packages web)
   #:use-module (gnu packages xorg)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1))
@@ -6435,15 +6438,16 @@ functionality and behavior.")
 (define-public arc-theme
   (package
     (name "arc-theme")
-    (version "20170302")
+    (version "20181022")
     (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/horst3180/arc-theme"
-                                  "/archive/" version ".tar.gz"))
-              (file-name (string-append name "-" version ".tar.gz"))
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/NicoHood/arc-theme.git")
+                    (commit version)))
+              (file-name (git-file-name name version))
               (sha256
                (base32
-                "0igxpngnkf1wpsg872a9jg3c9f5z8afm312yfbillz16mk8w39cw"))))
+                "08951dk1irfadwpr3p323a4fprmxg53rk2r2niwq3v62ryhi3663"))))
     (build-system gnu-build-system)
     (arguments
      '(#:phases
@@ -6454,16 +6458,20 @@ functionality and behavior.")
     (native-inputs
      `(("autoconf" ,autoconf)
        ("automake" ,automake)
-       ("pkg-config" ,pkg-config)))
-    (inputs
-     `(("gtk+" ,gtk+)))
+       ("glib" ,glib "bin")             ; for glib-compile-resources
+       ("gnome-shell" ,gnome-shell)
+       ("gtk+" ,gtk+)
+       ("inkscape" ,inkscape)
+       ("optipng" ,optipng)
+       ("pkg-config" ,pkg-config)
+       ("sassc" ,sassc)))
     (synopsis "A flat GTK+ theme with transparent elements")
     (description "Arc is a flat theme with transparent elements for GTK 3, GTK
 2, and GNOME Shell which supports GTK 3 and GTK 2 based desktop environments
 like GNOME, Unity, Budgie, Pantheon, XFCE, Mate, etc.")
     (home-page "https://github.com/horst3180/arc-theme")
     ;; No "or later" language found.
-    (license license:gpl3)))
+    (license license:gpl3+)))
 
 (define-public faba-icon-theme
   (package
