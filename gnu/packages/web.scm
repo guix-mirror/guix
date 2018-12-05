4a21d1765d8c158b3f1e9851f106cd8120f6caea
Marius Bakke <mbakke@fastmail.com>
2018-12-05 16:49:22 +0100

    gnu: nginx: Update to 1.14.2.
    * gnu/packages/web.scm (nginx): Update to 1.14.2.

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -196,14 +196,14 @@ Interface} specification.")
     (name "nginx")
     ;; Consider updating the nginx-documentation package if the nginx package is
     ;; updated.
-    (version "1.14.1")
+    (version "1.14.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://nginx.org/download/nginx-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "19542jxcjf4dvrqvgb5vr36mhbzcjrxc3v0xh451rm60610rf2dz"))))
+                "15wppq12qmq8acjs35xfj61czhf9cdc0drnl5mm8hcg3aihryb80"))))
     (build-system gnu-build-system)
     (inputs `(("openssl" ,openssl)
               ("pcre" ,pcre)
