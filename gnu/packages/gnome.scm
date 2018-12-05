8c6d7b61bf05ee3c4154d8fc4279a20f509d2740
Ricardo Wurmus <rekado@elephly.net>
2018-12-05 17:24:44 +0100

    gnu: network-manager-applet: Update to 1.8.18.
    * gnu/packages/gnome.scm (network-manager-applet): Update to 1.8.18.
    [inputs]: Add mobile-broadband-provider-info.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -5487,7 +5487,7 @@ to virtual private networks (VPNs) via OpenVPN.")
 (define-public network-manager-applet
   (package
     (name "network-manager-applet")
-    (version "1.8.14")
+    (version "1.8.18")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -5495,7 +5495,7 @@ to virtual private networks (VPNs) via OpenVPN.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1js0i2kwfklahsn77qgxzdscy33drrlym3mrj1qhlw0zf8ri56ya"))))
+                "0y31g0lxr93370xi74hbpvcy9m81n5wdkdhq8xy2nqp0y4219p13"))))
     (build-system glib-or-gtk-build-system)
     (arguments '(#:configure-flags '("--disable-migration")
                  #:phases
