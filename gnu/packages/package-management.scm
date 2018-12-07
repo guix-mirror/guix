guix-mirror/guix

commit 6eac835f178c0c78637b0db8a4585a617b2f7622

maint: update-guix-package: Really register GC roots.
Previously we'd pass a relative file name to 'add-indirect-root', which
the daemon would interpret as relative to "/".  Consequently, checkouts
were not protected from GC.

* build-aux/update-guix-package.scm (main): Pass an absolute file name
to 'add-indirect-root'.

gnu/packages/package-management.scm
@@ -102,8 +102,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "0.16.0")
-        (commit "c845323d4b0a31ce5241a9d98187e1eeed43f71c")
-        (revision 1))
+        (commit "5227d938e2b7bda22e2a7aa733e1e09a6c726f18")
+        (revision 2))
     (package
       (name "guix")
 
@@ -119,7 +119,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "0i431d5p9ckr7kxfiwpp94wgjgqn6mgyypf0smw64bk635fn6ycr"))
+                  "0v1a99k0qpxa5ksgqqwydd1nb00hkd71fj7374rqhklvk0a2cdz9"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments

build-aux/update-guix-package.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2017 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2017, 2018 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -133,7 +133,8 @@ COMMIT."
            ;; Add an indirect GC root for SOURCE in the current directory.
            (false-if-exception (delete-file root))
            (symlink source root)
-           (add-indirect-root store root)
+           (add-indirect-root store
+                              (string-append (getcwd) "/" root))
 
            (format #t "source code for commit ~a: ~a (GC root: ~a)~%"
                    commit source root)))))

commit 23a1f738b1ef83c3dcb329e1c68617b0452e6a07

gnu: guix: Update to 6ddc63e599a26c.
This fixes a problem introduced with commit
6eac835f178c0c78637b0db8a4585a617b2f7622.  Reported on the #guix IRC channel
by tune.

* gnu/packages/package-management.scm (guix): Update to 6ddc63e599a26c.

gnu/packages/package-management.scm
@@ -102,8 +102,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "0.16.0")
-        (commit "5227d938e2b7bda22e2a7aa733e1e09a6c726f18")
-        (revision 2))
+        (commit "6ddc63e599a26c302f74d0622f67cfd987f0dc5f")
+        (revision 3))
     (package
       (name "guix")
 
@@ -119,7 +119,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "0v1a99k0qpxa5ksgqqwydd1nb00hkd71fj7374rqhklvk0a2cdz9"))
+                  "0vzxrsfbr4phhy60m7pc6klb61whqc404c3x76ydj70xvi1xa0wz"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments