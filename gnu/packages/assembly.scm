cb95e79f14716b5db94d18e16a8f84243132dac3
Danny Milosavljevic <dannym@scratchpost.org>
2019-01-09 19:30:21 +0100

    gnu: fasm: Fix grammatical errors in description.
    * gnu/packages/assembly.scm (fasm)[description]: Modify.

--- gnu/packages/assembly.scm
+++ gnu/packages/assembly.scm
@@ -163,9 +163,9 @@ to the clients.")
     (supported-systems '("x86_64-linux" "i686-linux"))
     (synopsis "Assembler for x86 processors")
     (description
-     "FASM is a assembler that supports x86, and IA-64 Intel architectures.
-It does multiple passes to optimize machine code.It have macro abilities and
-focus on operating system portability.")
+     "FASM is an assembler that supports x86 and IA-64 Intel architectures.
+It does multiple passes to optimize machine code.  It has macro abilities and
+focuses on operating system portability.")
     (home-page "https://flatassembler.net/")
     (license license:bsd-2)))
 
