5327e912a8a477e472da9ec03c99cdedcc04af75
Ricardo Wurmus <rekado@elephly.net>
2019-01-15 14:58:13 +0100

    gnu: Add missing module reference.
    This is a follow-up to commit 44d10b1f722856ab8e9b942804aa7ef33e2ef739.
    
    * gnu/packages/tex.scm: Add missing reference to python-xyz.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -61,6 +61,7 @@
   #:use-module (gnu packages perl-check)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages ruby)
   #:use-module (gnu packages shells)
