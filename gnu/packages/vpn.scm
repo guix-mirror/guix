8b1d0ff43c034d3ecabbc58aab255565ff83185a
Tobias Geerinckx-Rice <me@tobias.gr>
2019-01-17 21:41:19 +0100

    gnu: openconnect: Use HTTPS home page.
    * gnu/packages/vpn.scm (openconnect)[home-page]: Use HTTPS.

--- gnu/packages/vpn.scm
+++ gnu/packages/vpn.scm
@@ -269,7 +269,7 @@ supported by the ASA5500 Series, by IOS 12.4(9)T or later on Cisco SR500,
 870, 880, 1800, 2800, 3800, 7200 Series and Cisco 7301 Routers,
 and probably others.")
    (license license:lgpl2.1)
-   (home-page "http://www.infradead.org/openconnect/")))
+   (home-page "https://www.infradead.org/openconnect/")))
 
 (define-public openvpn
   (package
