5cb9754cf44598e6715ce6e984a97e6a589cf5f2
Ricardo Wurmus <rekado@elephly.net>
2019-01-25 14:25:48 +0100

    gnu: proplib: Use INVOKE.
    * gnu/packages/embedded.scm (proplib)[arguments]: Use INVOKE.

--- gnu/packages/embedded.scm
+++ gnu/packages/embedded.scm
@@ -575,7 +575,7 @@ with a layered architecture of JTAG interface and TAP support.")
            ;; have to create the target directories at build time.
            (add-before 'build 'create-target-directories
              (lambda* (#:key make-flags #:allow-other-keys)
-               (zero? (apply system* "make" "install-dirs" make-flags))))
+               (apply invoke "make" "install-dirs" make-flags)))
            (add-before 'build 'set-cross-environment-variables
              (lambda* (#:key outputs #:allow-other-keys)
                (setenv "CROSS_LIBRARY_PATH"
@@ -589,7 +589,7 @@ with a layered architecture of JTAG interface and TAP support.")
                #t))
            (add-before 'install 'install-includes
              (lambda* (#:key make-flags #:allow-other-keys)
-               (zero? (apply system* "make" "install-includes" make-flags)))))))
+               (apply invoke "make" "install-includes" make-flags))))))
       (native-inputs
        `(("propeller-gcc" ,propeller-gcc)
          ("propeller-binutils" ,propeller-binutils)
