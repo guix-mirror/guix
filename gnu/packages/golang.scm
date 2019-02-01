bc58bb9bdfbe5b2128d96416611994f14b5b1757
Leo Famulari <leo@famulari.name>
2019-02-01 12:21:38 +0100

    gnu: Add go-github-com-direnv-go-dotenv.
    * gnu/packages/golang.scm (go-github-com-direnv-go-dotenv): New variable.

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -3365,3 +3365,28 @@ have super fancy logs.")
       (synopsis "Filesystem-related functions for Go")
       (description "Package fs provides filesystem-related functions.")
       (license license:bsd-3))))
+
+(define-public go-github-com-direnv-go-dotenv
+  (let ((commit "4cce6d1a66f7bc8dc730eab85cab6af1b801abed")
+        (revision "0"))
+    (package
+      (name "go-github-com-direnv-go-dotenv")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/direnv/go-dotenv")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "00wn4fc2lma0csf6ryvlc6k9jbpbifm4n7i3kkd2xrfw5qlm29b6"))))
+      (build-system go-build-system)
+      (arguments
+       '(#:import-path "github.com/direnv/go-dotenv"))
+      (home-page "https://github.com/direnv/go-dotenv")
+      (synopsis "Go dotenv parsing library")
+      (description "This package provides a library for parsing the dotenv
+format in Go.")
+      (license license:expat))))
