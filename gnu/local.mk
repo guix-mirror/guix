99ee274f0de8868abea66c4cab0cf1ddc839ded9
Leo Famulari <leo@famulari.name>
2019-02-05 00:02:57 +0100

    gnu: Remove unused patch 'glibc-memchr-overflow-i686.patch'.
    This is a followup to commit 67d527e35e367c9e9e89ec01cda2ce32cabd2d89.
    
    * gnu/packages/patches/glibc-memchr-overflow-i686.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -823,7 +823,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/glibc-hurd-magic-pid.patch		\
   %D%/packages/patches/glibc-ldd-x86_64.patch			\
   %D%/packages/patches/glibc-locales.patch			\
-  %D%/packages/patches/glibc-memchr-overflow-i686.patch		\
   %D%/packages/patches/glibc-o-largefile.patch			\
   %D%/packages/patches/glibc-reinstate-prlimit64-fallback.patch	\
   %D%/packages/patches/glibc-vectorized-strcspn-guards.patch	\
