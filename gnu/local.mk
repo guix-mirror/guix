800d74dc9affc0f28e1a337ec89f93af438af9f6
Leo Famulari <leo@famulari.name>
2019-02-05 00:07:10 +0100

    gnu: Remove unused patch 'openldap-CVE-2017-9287.patch'.
    This is a followup to commit 47415332d70cd9166b0914ce43018ae57083c120.
    
    * gnu/packages/patches/openldap-CVE-2017-9287.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1090,7 +1090,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/opencv-rgbd-aarch64-test-fix.patch	\
   %D%/packages/patches/openfoam-4.1-cleanup.patch			\
   %D%/packages/patches/openjdk-10-idlj-reproducibility.patch	\
-  %D%/packages/patches/openldap-CVE-2017-9287.patch		\
   %D%/packages/patches/openocd-nrf52.patch			\
   %D%/packages/patches/opensmtpd-fix-crash.patch		\
   %D%/packages/patches/openssh-CVE-2018-20685.patch		\
