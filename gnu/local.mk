6bf2201350e0ce64a52c1aee9d878783eeb098a7
Leo Famulari <leo@famulari.name>
2019-02-04 23:50:06 +0100

    gnu: Remove unused patch 'ao-cad-aarch64-support.patch'.
    This is a followup to commit 28f4d56dd311a3b4c7102394120de05be6937494.
    
    * gnu/packages/patches/ao-cad-aarch64-support.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -639,7 +639,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/antiword-CVE-2014-8123.patch			\
   %D%/packages/patches/antlr3-3_1-fix-java8-compilation.patch	\
   %D%/packages/patches/antlr3-3_3-fix-java8-compilation.patch	\
-  %D%/packages/patches/ao-cad-aarch64-support.patch		\
   %D%/packages/patches/apr-skip-getservbyname-test.patch	\
   %D%/packages/patches/aria2-CVE-2019-3500.patch		\
   %D%/packages/patches/aspell-default-dict-dir.patch		\
