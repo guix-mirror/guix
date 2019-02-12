0a87bc9c2bd3d5eaef5b5d026d4e75755f33d3c2
Tobias Geerinckx-Rice <me@tobias.gr>
2019-02-12 16:09:16 +0100

    gnu: nsd: Update to 4.1.26.
    * gnu/packages/dns.scm (nsd): Update to 4.1.26.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -294,15 +294,14 @@ asynchronous fashion.")
 (define-public nsd
   (package
     (name "nsd")
-    (version "4.1.25")
+    (version "4.1.26")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://www.nlnetlabs.nl/downloads/nsd/nsd-"
                            version ".tar.gz"))
        (sha256
-        (base32
-         "0zyzjd3wmq258jiry62ci1z23qfd0rc5ggnpmybc60xvpddgynwg"))))
+        (base32 "1x0mvj4872dzj1rr9adnchdm4dhn41xmc459p5j4s0r13m1l32lz"))))
     (build-system gnu-build-system)
     (arguments
      `(#:configure-flags
