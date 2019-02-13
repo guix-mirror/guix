5bd0813a0f8243f6e7f07ad590c9f620943ed417
Marius Bakke <mbakke@fastmail.com>
2019-02-13 18:13:49 +0100

    gnu: GnuPG: Update to 2.2.13.
    * gnu/packages/gnupg.scm (gnupg): Update to 2.2.13.

--- gnu/packages/gnupg.scm
+++ gnu/packages/gnupg.scm
@@ -224,14 +224,14 @@ compatible to GNU Pth.")
 (define-public gnupg
   (package
     (name "gnupg")
-    (version "2.2.12")
+    (version "2.2.13")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnupg/gnupg/gnupg-" version
                                   ".tar.bz2"))
               (sha256
                (base32
-                "1jw282iy27j1qygym52aa44zxy7ly4bdadhd628hwr4q9j5hy0yv"))))
+                "1n0igfn9nwdajzkcgdx7apwm9m5rpw0fg6vwskmf1rpranlqgivn"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
