d100b9bd97f27cf3257b1b14bb0f9b27a966c27e
Tobias Geerinckx-Rice <me@tobias.gr>
2019-02-14 03:45:11 +0100

    gnu: rrdtool: Update to 1.7.1.
    * gnu/packages/rrdtool.scm (rrdtool): Update to 1.7.1.

--- gnu/packages/rrdtool.scm
+++ gnu/packages/rrdtool.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2014 Mark H Weaver <mhw@netris.org>
-;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2018, 2019 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -37,14 +37,14 @@
 (define-public rrdtool
   (package
     (name "rrdtool")
-    (version "1.7.0")
+    (version "1.7.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "http://oss.oetiker.ch/rrdtool/pub/rrdtool-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "0ssjqpa0dwwzbylc0drmlbq922qcw8crffc0rpr805xr6n4k8zgr"))))
+                "1bhsg119j94xwykp2sbp01hhxcg78gzblfn7j98slrv9va77g6wq"))))
     (build-system gnu-build-system)
     (inputs
      `(("cairo" ,cairo)
