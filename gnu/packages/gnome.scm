5fed63dda963851647ce707c6a3cba2e4f694530
Tobias Geerinckx-Rice <me@tobias.gr>
2019-02-26 13:38:35 +0100

    gnu: eog-plugins: Update to 3.26.3.
    * gnu/packages/gnome.scm (eog-plugins): Update to 3.26.3.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -3983,7 +3983,7 @@ supports image conversion, rotation, and slideshows.")
   ;; 'XDG_DATA_DIRS' appropriately set.
   (package
     (name "eog-plugins")
-    (version "3.26.2")
+    (version "3.26.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/eog-plugins/"
@@ -3991,7 +3991,7 @@ supports image conversion, rotation, and slideshows.")
                                   "eog-plugins-" version ".tar.xz"))
               (sha256
                (base32
-                "1w8zw7kwfvlwlyb1k1inqdvbwnzq959sqawlmnwfb8ykn98hbk8y"))))
+                "06fnjs2p18ad5vk07z685cx26sc7d3azywss00w9xvz794b2i1g3"))))
     (build-system gnu-build-system)
     (home-page "https://wiki.gnome.org/Apps/EyeOfGnome/Plugins")
     (synopsis "Extensions for the Eye of GNOME image viewer")
