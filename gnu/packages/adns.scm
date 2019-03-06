e8393ebe46d9ad09aceb152618db490378ba5ded
Marius Bakke <mbakke@fastmail.com>
2019-03-06 14:39:17 +0100

    gnu: c-ares: Update to 1.15.0.
    * gnu/packages/adns.scm (c-ares): Update to 1.15.0.
    (c-ares-next): Remove variable.

--- gnu/packages/adns.scm
+++ gnu/packages/adns.scm
@@ -61,32 +61,6 @@ scripts.")
 (define-public c-ares
   (package
     (name "c-ares")
-    (version "1.14.0")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "https://c-ares.haxx.se/download/" name "-" version
-                    ".tar.gz"))
-              (sha256
-               (base32
-                "0vnwmbvymw677k780kpb6sb8i3szdp89rzy8mz1fwg1657yw3ls5"))))
-    (build-system gnu-build-system)
-    (native-inputs
-     `(("pkg-config" ,pkg-config)))
-    (home-page "https://c-ares.haxx.se/")
-    (synopsis "C library for asynchronous DNS requests")
-    (description
-      "C-ares is a C library that performs DNS requests and name resolution
-asynchronously.  It is intended for applications which need to perform DNS
-queries without blocking, or need to perform multiple DNS queries in parallel.
-The primary examples of such applications are servers which communicate with
-multiple clients and programs with graphical user interfaces.")
-    (license (x11-style "https://c-ares.haxx.se/license.html"))))
-
-;; XXX: temporary package for tensorflow / grpc
-(define-public c-ares-next
-  (package
-    (name "c-ares")
     (version "1.15.0")
     (source (origin
               (method url-fetch)
@@ -96,11 +70,7 @@ multiple clients and programs with graphical user interfaces.")
               (sha256
                (base32
                 "0lk8knip4xk6qzksdkn7085mmgm4ixfczdyyjw656c193y3rgnvc"))))
-    (build-system cmake-build-system)
-    (arguments
-     `(#:tests? #f ; some tests seem to require Internet connection
-       #:configure-flags
-       (list "-DCARES_BUILD_TESTS=ON")))
+    (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
     (home-page "https://c-ares.haxx.se/")
