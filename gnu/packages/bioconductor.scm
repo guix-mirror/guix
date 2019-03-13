704de8f5eeee67402648cc6624defa79600d1d75
Ricardo Wurmus <rekado@elephly.net>
2019-03-13 11:18:27 +0100

    gnu: Add r-ihw.
    * gnu/packages/bioconductor.scm (r-ihw): New variable.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -1868,3 +1868,33 @@ to install interface to SYMPHONY.")
     ;; Symphony 5.4 or later is distributed under the terms of the EPL 1.0.
     ;; lpsimphony is released under the same terms.
     (license license:epl1.0)))
+
+(define-public r-ihw
+  (package
+    (name "r-ihw")
+    (version "1.10.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (bioconductor-uri "IHW" version))
+       (sha256
+        (base32
+         "10wqasl8k2j3y5qvak3xr2xj6symk656xww1y5n2l22nz832j19n"))))
+    (properties `((upstream-name . "IHW")))
+    (build-system r-build-system)
+    (propagated-inputs
+     `(("r-biocgenerics" ,r-biocgenerics)
+       ("r-fdrtool" ,r-fdrtool)
+       ("r-lpsymphony" ,r-lpsymphony)
+       ("r-slam" ,r-slam)))
+    (home-page "https://bioconductor.org/packages/IHW")
+    (synopsis "Independent hypothesis weighting")
+    (description
+     "@dfn{Independent hypothesis weighting} (IHW) is a multiple testing
+procedure that increases power compared to the method of Benjamini and
+Hochberg by assigning data-driven weights to each hypothesis.  The input to
+IHW is a two-column table of p-values and covariates.  The covariate can be
+any continuous-valued or categorical variable that is thought to be
+informative on the statistical properties of each hypothesis test, while it is
+independent of the p-value under the null hypothesis.")
+    (license license:artistic2.0)))
