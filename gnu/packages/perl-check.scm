cb5c64c6f59b4415c190d237e0bbe927bc92ccda
Tobias Geerinckx-Rice <me@tobias.gr>
2019-03-15 11:52:48 +0100

    gnu: perl-test-mockobject: Update to 1.20180705.
    * gnu/packages/perl-check.scm (perl-test-mockobject): Update to 1.20180705.

--- gnu/packages/perl-check.scm
+++ gnu/packages/perl-check.scm
@@ -735,15 +735,14 @@ you @code{unmock()} the subroutine.")
 (define-public perl-test-mockobject
   (package
     (name "perl-test-mockobject")
-    (version "1.20150527")
+    (version "1.20180705")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://cpan/authors/id/C/CH/CHROMATIC/"
                            "Test-MockObject-" version ".tar.gz"))
        (sha256
-        (base32
-         "160xvhbpwqjaff4fgckvldknldzcbn1z3jvyzybs7cqlj1x3bwdd"))))
+        (base32 "06250ayzzd90vbvkpxwr9d3nlbbngl1b9nk2qk0ma4aibn6ha5j5"))))
     (build-system perl-build-system)
     (native-inputs
      `(("perl-test-exception" ,perl-test-exception)
