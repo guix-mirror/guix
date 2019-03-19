8b672b2c48164c78731ae142ae4761d91666bdec
Mark H Weaver <mhw@netris.org>
2019-03-18 21:39:12 -0400

    gnu: blender: Restore version 2.79b for systems without OpenGL 3.
    Fixes <https://bugs.gnu.org/33882>.
    
    * gnu/packages/graphics.scm (blender-2.79): New variable.
    (blender)[description]: Remove obsolete claim that the stable
    release no longer works in Guix.
    * gnu/packages/patches/blender-2.79-newer-ffmpeg.patch,
    gnu/packages/patches/blender-2.79-python-3.7-fix.patch: New files.
    * gnu/local.mk (dist_patch_DATA): Add them.

--- gnu/local.mk
+++ gnu/local.mk
@@ -672,6 +672,8 @@ dist_patch_DATA =						\
   %D%/packages/patches/beignet-correct-file-names.patch		\
   %D%/packages/patches/biber-fix-encoding-write.patch		\
   %D%/packages/patches/binutils-loongson-workaround.patch	\
+  %D%/packages/patches/blender-2.79-newer-ffmpeg.patch		\
+  %D%/packages/patches/blender-2.79-python-3.7-fix.patch	\
   %D%/packages/patches/boost-fix-icu-build.patch		\
   %D%/packages/patches/byobu-writable-status.patch		\
   %D%/packages/patches/calibre-no-updates-dialog.patch		\
