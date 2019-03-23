e54f0cf68067cbce0ad27baf52d48f968af100ec
Marius Bakke <mbakke@fastmail.com>
2019-03-23 19:27:30 +0100

    gnu: libgpg-error: Update to 1.36.
    * gnu/packages/gnupg.scm (libgpg-error): Update to 1.36.

--- gnu/packages/gnupg.scm
+++ gnu/packages/gnupg.scm
@@ -77,7 +77,7 @@
 (define-public libgpg-error
   (package
     (name "libgpg-error")
-    (version "1.35")
+    (version "1.36")
     (source
      (origin
       (method url-fetch)
@@ -85,7 +85,7 @@
                           version ".tar.bz2"))
       (sha256
        (base32
-        "0rz00xyrk7qzg7mmcjhvrnm34hddkvyg9zsqq548vj58m1ifxmfb"))))
+        "0z696dmhfxm2n6pmr8b857wwljq9h633yi99bhbn7h88f91rigds"))))
     (build-system gnu-build-system)
     (home-page "https://gnupg.org")
     (synopsis "Library of error values for GnuPG components")
