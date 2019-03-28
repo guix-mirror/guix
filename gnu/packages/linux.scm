fa3c2aad6581d219ec557c1ee87309ff19085785
Mark H Weaver <mhw@netris.org>
2019-03-28 19:35:11 -0400

    gnu: linux-libre: Update to 5.0.5.
    * gnu/packages/linux.scm (%linux-libre-version): Update to 5.0.5.
    (%linux-libre-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -415,8 +415,8 @@ for ARCH and optionally VARIANT, or #f if there is no such configuration."
 It has been modified to remove all non-free binary blobs.")
     (license license:gpl2)))
 
-(define %linux-libre-version "5.0.4")
-(define %linux-libre-hash "1j369x19kqryhf7w5p14rah0p3lglrhr5kac9ysqigx92as7kq50")
+(define %linux-libre-version "5.0.5")
+(define %linux-libre-hash "1yivxqprxfzhzid4qv9hpnb5i38kijrj2g2pyzz7niliya1c58li")
 
 (define %linux-libre-5.0-patches
   (list %boot-logo-patch
