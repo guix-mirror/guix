guix-mirror/guix

commit e8cfce439afed945e352ad28f73f0a5f7840f503

gnu: php: Disable failing tests on armhf.
* gnu/packages/php.scm (php)[arguments]: Disable failing tests on armhf.

gnu/packages/php.scm
@@ -86,7 +86,7 @@
                   #t))))
     (build-system gnu-build-system)
     (arguments
-     '(#:configure-flags
+     `(#:configure-flags
        (let-syntax ((with (syntax-rules ()
                             ((_ option input)
                              (string-append option "="
@@ -181,6 +181,17 @@
 
              (substitute* "ext/standard/tests/streams/bug60602.phpt"
                (("'ls'") (string-append "'" (which "ls") "'")))
+
+             ,@(if (string-prefix? "armhf" (or (%current-system)
+                                               (%current-target-system)))
+                   ;; Drop tests known to fail on armhf.
+                   '((for-each delete-file
+                              (list
+                                "ext/calendar/tests/unixtojd_error1.phpt"
+                                ;; arm can be a lot slower, so a time-related test fails
+                                "ext/fileinfo/tests/cve-2014-3538-nojit.phpt"
+                                "ext/pcre/tests/bug76514.phpt"
+                                "ext/pcre/tests/preg_match_error3.phpt"))))
 
              ;; Drop tests that are known to fail.
              (for-each delete-file

commit 9398152ad1f4e130ad895c8bb756de8a33e91f7f

gnu: php: Fix typo.
Regression introduced in e8cfce439afed945e352ad28f73f0a5f7840f503.

* gnu/packages/php.scm (php)[arguments]: Add missing 'else' branch of
the 'if'.  Change first argument to 'string-prefix?' to match GNU
triplets as well.

gnu/packages/php.scm
@@ -182,8 +182,8 @@
              (substitute* "ext/standard/tests/streams/bug60602.phpt"
                (("'ls'") (string-append "'" (which "ls") "'")))
 
-             ,@(if (string-prefix? "armhf" (or (%current-system)
-                                               (%current-target-system)))
+             ,@(if (string-prefix? "arm" (or (%current-system)
+                                             (%current-target-system)))
                    ;; Drop tests known to fail on armhf.
                    '((for-each delete-file
                               (list
@@ -191,7 +191,8 @@
                                 ;; arm can be a lot slower, so a time-related test fails
                                 "ext/fileinfo/tests/cve-2014-3538-nojit.phpt"
                                 "ext/pcre/tests/bug76514.phpt"
-                                "ext/pcre/tests/preg_match_error3.phpt"))))
+                                "ext/pcre/tests/preg_match_error3.phpt")))
+                   '())
 
              ;; Drop tests that are known to fail.
              (for-each delete-file