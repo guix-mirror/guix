guix-mirror/guix

commit c11fcc6aa27db5f68aed807a6190143f4a156564

gnu: http-parser: Update to 2.9.1.
* gnu/packages/web.scm (http-parser): Update to 2.9.1.

gnu/packages/web.scm
@@ -5154,7 +5154,7 @@ into your tests.  It automatically starts up a HTTP server in a separate thread 
 (define-public http-parser
   (package
     (name "http-parser")
-    (version "2.9.0")
+    (version "2.9.1")
     (home-page "https://github.com/nodejs/http-parser")
     (source (origin
               (method git-fetch)
@@ -5163,7 +5163,7 @@ into your tests.  It automatically starts up a HTTP server in a separate thread 
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1qa3rh9x4as2fc2p0y2hah83iqs7jr5106a8anv317359dgf3ssj"))))
+                "12ilmjma6vmlm2kl23fx5pbi226x098gljgxpx3h2kmq8pg2lm28"))))
     (build-system gnu-build-system)
     (arguments
      `(#:test-target "test"

commit 90b94761b673665e212e2677e5ed9924f9a6af42

gnu: http-parser: Fix build on i686-linux.
Fixes a regression introduced in
c11fcc6aa27db5f68aed807a6190143f4a156564, whereby 'http-parser' would
fail to build on i686.

* gnu/packages/web.scm (http-parser)[source](modules, snippet): New
fields.

gnu/packages/web.scm
@@ -5163,7 +5163,16 @@ into your tests.  It automatically starts up a HTTP server in a separate thread 
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "12ilmjma6vmlm2kl23fx5pbi226x098gljgxpx3h2kmq8pg2lm28"))))
+                "12ilmjma6vmlm2kl23fx5pbi226x098gljgxpx3h2kmq8pg2lm28"))
+              (modules '((guix build utils)))
+              (snippet
+               '(begin
+                  ;; There are signedness errors that trigger on 32-bit
+                  ;; platforms that trigger, and '-Werror' leads to build
+                  ;; failures.
+                  (substitute* "Makefile"
+                    (("-Werror") ""))
+                  #t))))
     (build-system gnu-build-system)
     (arguments
      `(#:test-target "test"