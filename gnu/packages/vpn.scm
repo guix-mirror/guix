28341601b323906d91821ed698537d261f2b0854
Tobias Geerinckx-Rice <me@tobias.gr>
2019-04-23 22:00:41 +0200

    gnu: xl2tpd: Don't use NAME in source URI.
    * gnu/packages/vpn.scm (xl2tpd)[source]: Hard-code NAME.

--- gnu/packages/vpn.scm
+++ gnu/packages/vpn.scm
@@ -528,10 +528,10 @@ WireGuard support.")
               (uri (git-reference
                     (url "https://github.com/xelerance/xl2tpd")
                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
               (sha256
                (base32
-                "1c2ahxz2zmmxwmk951d2qhijgz67zhwa1hn0r59fgz0y14w22myi"))
-              (file-name (string-append "xl2tpd-" version "-checkout"))))
+                "1c2ahxz2zmmxwmk951d2qhijgz67zhwa1hn0r59fgz0y14w22myi"))))
     (build-system gnu-build-system)
     (arguments
      `(#:make-flags (list (string-append "DESTDIR=" %output)
