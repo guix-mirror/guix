guix-mirror/guix

commit 7ecd1761a236a2256e420032d561aa6c372d95a8

gnu: bubblewrap: Edit description.
* gnu/packages/virtualization.scm (bubblewrap)[description]: Mark up & edit.

gnu/packages/virtualization.scm
@@ -1089,11 +1089,11 @@ virtual machines.")
     (home-page "https://github.com/projectatomic/bubblewrap")
     (synopsis "Unprivileged sandboxing tool")
     (description "Bubblewrap is aimed at running applications in a sandbox,
-where it has restricted access to parts of the operating system or user data
-such as the home directory.  Bubblewrap always creates a new mount namespace,
-and the user can specify exactly what parts of the filesystem should be visible
-in the sandbox.  Any such directories specified is mounted nodev by default,
-and can be made readonly.")
+restricting their access to parts of the operating system or user data such as
+the home directory.  Bubblewrap always creates a new mount namespace, and the
+user can specify exactly what parts of the file system should be made visible
+in the sandbox.  These directories are mounted with the @code{nodev) option
+by default and can be made read-only.")
     (license license:lgpl2.0+)))
 
 (define-public bochs

commit ef1f1ca742082806afe0a430ea593ad93b573f8c

gnu: bubblewrap: Fix Texinfo markup in description.
Fixes a regression introduced in
7ecd1761a236a2256e420032d561aa6c372d95a8.

* gnu/packages/virtualization.scm (bubblewrap): Fix typo.

gnu/packages/virtualization.scm
@@ -1092,7 +1092,7 @@ virtual machines.")
 restricting their access to parts of the operating system or user data such as
 the home directory.  Bubblewrap always creates a new mount namespace, and the
 user can specify exactly what parts of the file system should be made visible
-in the sandbox.  These directories are mounted with the @code{nodev) option
+in the sandbox.  These directories are mounted with the @code{nodev} option
 by default and can be made read-only.")
     (license license:lgpl2.0+)))
 