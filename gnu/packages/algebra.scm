guix-mirror/guix

commit 6d3896b9fc115b03046860a9aa12966926bccf53

gnu: giac-xcas: Add inputs.
* gnu/packages/algebra.scm (giac-xcas)[inputs]: Add glpk, libsamplerate, bison
  and flex.

gnu/packages/algebra.scm
@@ -42,6 +42,7 @@
   #:use-module (gnu packages mpi)
   #:use-module (gnu packages multiprecision)
   #:use-module (gnu packages perl)
+  #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages readline)
@@ -301,13 +302,16 @@ precision.")
                (delete-file (string-append out "/bin/xcasnew"))
                #t))))))
     (inputs
+     ;;; TODO: Add libnauty.
      `(("fltk" ,fltk)
+       ("glpk" ,glpk)
        ("gmp" ,gmp)
        ("gsl" ,gsl)
        ("lapack" ,lapack)
        ("libao" ,ao)
        ("libjpeg" ,libjpeg)
        ("libpng" ,libpng)
+       ("libsamplerate" ,libsamplerate)
        ("libx11" ,libx11)
        ("libxinerama" ,libxinerama)
        ("libxft" ,libxft)
@@ -320,7 +324,9 @@ precision.")
        ("pari-gp" ,pari-gp)
        ("tcsh" ,tcsh)))
     (native-inputs
-     `(("readline" ,readline)
+     `(("bison" ,bison)
+       ("flex" ,flex)
+       ("readline" ,readline)
        ("texlive" ,texlive-tiny)))
     (home-page "https://www-fourier.ujf-grenoble.fr/~parisse/giac.html")
     (synopsis "Computer algebra system")

commit fdb513a7045f9731ddffff23bc8fb78b012d58ce

gnu: Fix missing bison package in (gnu packages algebra).
This fixes a regression introduced in
6d3896b9fc115b03046860a9aa12966926bccf53.

* gnu/packages/algebra.scm: Use the (gnu packages bison) module.

gnu/packages/algebra.scm
@@ -27,6 +27,7 @@
 (define-module (gnu packages algebra)
   #:use-module (gnu packages)
   #:use-module (gnu packages autotools)
+  #:use-module (gnu packages bison)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages cpp)