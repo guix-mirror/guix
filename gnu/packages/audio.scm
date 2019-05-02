c2974d1e46832b45bfb0c6391d5da3dcbd6b6c24
Rutger Helling <rhelling@mykolab.com>
2019-05-02 08:56:09 +0200

    gnu: faudio: Update to 19.05.
    * gnu/packages/audio.scm (faudio): Update to 19.05.

--- gnu/packages/audio.scm
+++ gnu/packages/audio.scm
@@ -3701,7 +3701,7 @@ library.")
 (define-public faudio
   (package
     (name "faudio")
-    (version "19.04")
+    (version "19.05")
     (source
      (origin
        (method git-fetch)
@@ -3710,7 +3710,7 @@ library.")
              (commit version)))
        (file-name (string-append name "-" version "-checkout"))
        (sha256
-        (base32 "00lqf8bjcwm4k8yky9jmqghkxijcm2lxspb9zyl1270yqmj05kiw"))))
+        (base32 "1dja2ykixk1ycqda116cg9fy4qg364dqj88amfln0r9pnsj2kbxk"))))
     (arguments
      '(#:tests? #f                      ; No tests.
        #:configure-flags '("-DFFMPEG=ON")))
