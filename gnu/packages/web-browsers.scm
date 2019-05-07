cbbdc5c795ab3f411ea138a9857a695f7185ce63
Pierre Neidhardt <mail@ambrevar.xyz>
2019-05-07 19:18:28 +0200

    gnu: next-gtk-webkit: Update to 1.2.1.
    * gnu/packages/web-browsers.scm (next-gtk-webkit): Update to 1.2.1.

--- gnu/packages/web-browsers.scm
+++ gnu/packages/web-browsers.scm
@@ -368,7 +368,7 @@ driven and does not detract you from your daily work.")
 (define-public next-gtk-webkit
   (package
     (name "next-gtk-webkit")
-    (version "1.2.0")
+    (version "1.2.1")
     (source
      (origin
        (method git-fetch)
@@ -377,7 +377,7 @@ driven and does not detract you from your daily work.")
              (commit version)))
        (sha256
         (base32
-         "0a066f56hnb9znbwnv1blm31j0ysv05n4wzlkli0zgw087c9047x"))
+         "19s6dbn47xy30dwfdd2p8fxz6z63rp5h7sm0barb69r7mvgnqvc1"))
        (file-name (git-file-name "next" version))))
     (build-system glib-or-gtk-build-system)
     (arguments
@@ -399,7 +399,7 @@ driven and does not detract you from your daily work.")
        ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
        ("webkitgtk" ,webkitgtk-2.24)))
     (native-inputs
-     `(("gcc-7" ,gcc-7) ; needed because webkitgtk-2.22 is compiled with gcc-7
+     `(("gcc-7" ,gcc-7) ; needed because webkitgtk-2.22 and above are compiled with gcc-7
        ("pkg-config" ,pkg-config)))
     (home-page "https://next.atlas.engineer")
     (synopsis "Infinitely extensible web-browser (user interface only)")
