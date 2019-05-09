013130cf0ba79d9a3ee84263b0d246d3de9bf0b8
Mark H Weaver <mhw@netris.org>
2019-05-08 22:19:29 -0400

    gnu: linux-libre@4.19: Update to 4.19.41.
    * gnu/packages/linux.scm (%linux-libre-4.19-version): Update to 4.19.41.
    (%linux-libre-4.19-hash): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -462,8 +462,8 @@ It has been modified to remove all non-free binary blobs.")
 between the CDemu userspace daemon and linux kernel.")
     (license license:gpl2+)))
 
-(define %linux-libre-4.19-version "4.19.40")
-(define %linux-libre-4.19-hash "1sqx6jlir5lmkjgi5nabr2qwdvk87s9ax0ygi5qpdlraaw5b0kyk")
+(define %linux-libre-4.19-version "4.19.41")
+(define %linux-libre-4.19-hash "0lf1w72y4qkrg57qd78zs16r658qaqzwly0y9195nf7bdwqzb03v")
 
 (define %linux-libre-4.19-patches
   (list %boot-logo-patch
