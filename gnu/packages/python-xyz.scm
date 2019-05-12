9dc6ff5a85ebd6f4968f33888646ac9ce707d8ce
Marius Bakke <mbakke@fastmail.com>
2019-05-12 19:51:45 +0200

    gnu: python-packaging: Update to 19.0.
    * gnu/packages/python-xyz.scm (python-packaging): Update to 19.0.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -12436,14 +12436,14 @@ several utilities, as well as an API for building localization tools.")
 (define-public python-packaging
   (package
     (name "python-packaging")
-    (version "18.0")
+    (version "19.0")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "packaging" version))
         (sha256
          (base32
-          "01wq9c53ix5rz6qg2c98gy8n4ff768rmanifm8m5jpjiaizj51h8"))))
+          "1brjhygq9dz6x1kdljivkjfldi3qf5rbkqgck1bpgv9qpv8ab60c"))))
     (build-system python-build-system)
     (arguments
      `(#:phases (modify-phases %standard-phases
