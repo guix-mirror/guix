guix-mirror/guix

commit 3ed497d42a5af8756bd95c64f9f9bed4de5f6d3c

gnu: Allow building gcc with non-default libc.
* gnu/packages/base.scm (make-gcc-libc): New procedure, returns a gcc
  that targets a specified libc.
  (gcc-glibc-2.26, gcc-glibc-2.27): New public variables.

Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/base.scm
@@ -1009,6 +1009,35 @@ with the Linux kernel.")
                   (("/bin/pwd") "pwd"))
                 #t))))))))
 
+(define (make-gcc-libc base-gcc libc)
+  "Return a GCC that targets LIBC."
+  (package (inherit base-gcc)
+           (name (string-append (package-name base-gcc) "-"
+                                (package-name libc) "-"
+                                (package-version libc)))
+           (arguments
+            (substitute-keyword-arguments
+             (ensure-keyword-arguments (package-arguments base-gcc)
+                                       '(#:implicit-inputs? #f))
+             ((#:make-flags flags)
+              `(let ((libc (assoc-ref %build-inputs "libc")))
+                 ;; FLAGS_FOR_TARGET are needed for the target libraries to receive
+                 ;; the -Bxxx for the startfiles.
+                 (cons (string-append "FLAGS_FOR_TARGET=-B" libc "/lib")
+                       ,flags)))))
+           (native-inputs
+            `(("libc" ,libc)
+              ("libc:static" ,libc "static")
+              ,@(append (package-inputs base-gcc)
+                        (fold alist-delete (%final-inputs) '("libc" "libc:static")))))
+           (inputs '())))
+
+(define-public gcc-glibc-2.27
+  (make-gcc-libc gcc glibc-2.27))
+
+(define-public gcc-glibc-2.26
+  (make-gcc-libc gcc glibc-2.26))
+
 (define-public (make-glibc-locales glibc)
   (package
     (inherit glibc)

commit 04a3ecc79ec01242acd0928f89bf982f50c866df

gnu: Remove 'gcc-glibc-2.27' and 'gcc-glibc-2.26'.
These top-level references to GCC could cause problems, such as:

  $ guix build -e '(@ (gnu packages gcc) gcc)' -n
  guix build: error: failed to evaluate expression '(@ (gnu packages gcc) gcc)':
  In procedure module-lookup: Unbound variable: gcc

Regression introduced in 3ed497d42a5af8756bd95c64f9f9bed4de5f6d3c.

* gnu/packages/base.scm (gcc-glibc-2.27, gcc-glibc-2.26): Remove.

gnu/packages/base.scm
@@ -1032,12 +1032,6 @@ with the Linux kernel.")
                         (fold alist-delete (%final-inputs) '("libc" "libc:static")))))
            (inputs '())))
 
-(define-public gcc-glibc-2.27
-  (make-gcc-libc gcc glibc-2.27))
-
-(define-public gcc-glibc-2.26
-  (make-gcc-libc gcc glibc-2.26))
-
 (define-public (make-glibc-locales glibc)
   (package
     (inherit glibc)