5da54e8004555dbb631a3d80106a321340cd4f16
Tobias Geerinckx-Rice <me@tobias.gr>
2019-05-20 03:35:19 +0200

    gnu: thunar: Update to 1.8.6.
    * gnu/packages/xfce.scm (thunar): Update to 1.8.6.

--- gnu/packages/xfce.scm
+++ gnu/packages/xfce.scm
@@ -596,7 +596,7 @@ like appearance, display, keyboard and mouse settings.")
 (define-public thunar
   (package
     (name "thunar")
-    (version "1.8.4")
+    (version "1.8.6")
     (source (origin
               (method url-fetch)
               (uri (string-append "http://archive.xfce.org/src/xfce/"
@@ -604,7 +604,7 @@ like appearance, display, keyboard and mouse settings.")
                                   "Thunar-" version ".tar.bz2"))
               (sha256
                (base32
-                "1c4kg8w77nwi75b4ashy2m1y39icv951h63qsr1gdj6yipd53gy2"))))
+                "0apg70dwkh8q89ybzhj4q2gp4yxiybwxfzpbvx0nj0rxis1gprf0"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)
