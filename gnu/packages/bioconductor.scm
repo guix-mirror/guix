371c73b2e6c139cdfa742c3ebf3250e7e62cceda
Ricardo Wurmus <rekado@elephly.net>
2019-05-20 15:05:48 +0200

    gnu: r-rhisat2: Update to 1.0.1.
    * gnu/packages/bioconductor.scm (r-rhisat2): Update to 1.0.1.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -4438,14 +4438,14 @@ interpretation.")
 (define-public r-rhisat2
   (package
     (name "r-rhisat2")
-    (version "1.0.0")
+    (version "1.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "Rhisat2" version))
        (sha256
         (base32
-         "1d1gwl8l2z8kzfvkp1s8vfrh6jbq9ha25dplpf9g62qqa8a46hga"))))
+         "01jhj5vvfl4n2d0nl3nd1iw9nii85mgw2adnrmxb8wwlxgy240vr"))))
     (properties `((upstream-name . "Rhisat2")))
     (build-system r-build-system)
     (native-inputs
