164cb4da63b929e1b89928fc721a20f912cdd1f0
Ricardo Wurmus <rekado@elephly.net>
2019-05-26 10:39:23 +0200

    gnu: pigx-chipseq: Update to 0.0.41.
    * gnu/packages/bioinformatics.scm (pigx-chipseq): Update to 0.0.41.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -12603,7 +12603,7 @@ expression report comparing samples in an easily configurable manner.")
 (define-public pigx-chipseq
   (package
     (name "pigx-chipseq")
-    (version "0.0.40")
+    (version "0.0.41")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/BIMSBbioinfo/pigx_chipseq/"
@@ -12611,7 +12611,7 @@ expression report comparing samples in an easily configurable manner.")
                                   "/pigx_chipseq-" version ".tar.gz"))
               (sha256
                (base32
-                "0y9x62cfwzhsp82imnawyamxp58bcb00yjxdy44spylqnjdlsaj8"))))
+                "0akbxdmsjsq5fzbwaap04hqjpsfgv1l6yrc2pwgbya1xgqvcq6vy"))))
     (build-system gnu-build-system)
     ;; parts of the tests rely on access to the network
     (arguments '(#:tests? #f))
