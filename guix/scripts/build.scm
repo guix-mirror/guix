guix-mirror/guix

commit 9353b199c18caca4a429f20423e1a5e7bc26a8da

guix build: '--log-file' looks for build logs of equivalent derivations.
Previously, '--log-file' would look for log files only for the current
derivation, and thus wouldn't find log files of equivalent derivations
that produce the same output.

* guix/scripts/build.scm (guix-build) <log-file?>: Replace
'derivation-file-name' by 'derivation->output-path'.

guix/scripts/build.scm
@@ -935,9 +935,12 @@ needed."
                                     #:mode mode))
 
               (cond ((assoc-ref opts 'log-file?)
+                     ;; Pass 'show-build-log' the output file names, not the
+                     ;; derivation file names, because there can be several
+                     ;; derivations leading to the same output.
                      (for-each (cut show-build-log store <> urls)
                                (delete-duplicates
-                                (append (map derivation-file-name drv)
+                                (append (map derivation->output-path drv)
                                         items))))
                     ((assoc-ref opts 'derivations-only?)
                      (format #t "~{~a~%~}" (map derivation-file-name drv))

commit 92359aed40fe76dcfb13ddf521fa8ac4c2735611

guix build: '--log-file' no longer returns several log files.
Fixes a regression introduced in
9353b199c18caca4a429f20423e1a5e7bc26a8da whereby something like:

  guix build --log-file $(guix build -d guile)

would return two log files.  This led to a failure of 'tests/guix-build.sh'.

* guix/scripts/build.scm (guix-build): Filter out derivation file names
from ITEMS.

guix/scripts/build.scm
@@ -916,7 +916,8 @@ needed."
                                    '())))
                    (items (filter-map (match-lambda
                                         (('argument . (? store-path? file))
-                                         file)
+                                         (and (not (derivation-path? file))
+                                              file))
                                         (_ #f))
                                       opts))
                    (roots (filter-map (match-lambda