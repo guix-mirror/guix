guix-mirror/guix

commit dd0ee954c4fa965023fd887452927c02edb8b52f

store: 'run-with-store' preserves the functional object cache.
This increases the hit rate of the object cache from 56% to 85% when
running something like 'guix build libreoffice -nd'.

* guix/store.scm (set-store-connection-object-cache!): New procedure.
(run-with-store): Copy 'object-cache' field of the new store into the
original one.

guix/store.scm
@@ -1783,6 +1783,9 @@ the store."
   ;; when using 'gexp->derivation' and co.
   (make-parameter #f))
 
+(define set-store-connection-object-cache!
+  (record-modifier <store-connection> 'object-cache))
+
 (define* (run-with-store store mval
                          #:key
                          (guile-for-build (%guile-for-build))
@@ -1798,9 +1801,12 @@ connection, and return the result."
                  (%current-target-system target))
     (call-with-values (lambda ()
                         (run-with-state mval store))
-      (lambda (result store)
-        ;; Discard the state.
-        result))))
+      (lambda (result new-store)
+        ;; Copy the object cache from NEW-STORE so we don't fully discard the
+        ;; state.
+        (let ((cache (store-connection-object-cache new-store)))
+          (set-store-connection-object-cache! store cache)
+          result)))))
 
 
 ;;;

commit 728a4ab1019fda2f14a5c69d3f1b277fb5e09cda

store: 'run-with-store' gracefully deals with #f store.
This is a followup to dd0ee954c4fa965023fd887452927c02edb8b52f, which
introduced a failure in tests/graph.scm.

* guix/store.scm (run-with-store): Check whether STORE and NEW-STORE are
true before calling 'store-connection-object-cache' etc.  Fixes a
failure in tests/graph.scm related to %REVERSE-PACKAGE-NODE-TYPE, which
uses #f as the store.

guix/store.scm
@@ -1802,11 +1802,12 @@ connection, and return the result."
     (call-with-values (lambda ()
                         (run-with-state mval store))
       (lambda (result new-store)
-        ;; Copy the object cache from NEW-STORE so we don't fully discard the
-        ;; state.
-        (let ((cache (store-connection-object-cache new-store)))
-          (set-store-connection-object-cache! store cache)
-          result)))))
+        (when (and store new-store)
+          ;; Copy the object cache from NEW-STORE so we don't fully discard
+          ;; the state.
+          (let ((cache (store-connection-object-cache new-store)))
+            (set-store-connection-object-cache! store cache)))
+        result))))
 
 
 ;;;