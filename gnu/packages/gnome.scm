794ce8eaf72537d05a3ca460b9c189e96e683001
Kei Kebreau <kkebreau@posteo.net>
2019-07-05 01:09:33 -0400

    gnu: libgweather: Update to 3.28.3.
    * gnu/packages/gnome.scm (libgweather): Update to 3.28.3.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -3197,7 +3197,7 @@ service via the system message bus.")
 (define-public libgweather
   (package
     (name "libgweather")
-    (version "3.28.2")
+    (version "3.28.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -3205,7 +3205,7 @@ service via the system message bus.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0xfy5ghwvnz2g9074dy6512m4z2pv66pmja14vhi9imgacbfh708"))))
+                "1xz60h59zgyk61y2wwzm0xlqav72i668g7v0x7rh9idz240hxls5"))))
     (build-system meson-build-system)
     (arguments
      `(#:tests? #f ; one of two tests requires network access
