1475554147d8e287afb243912d3cb6e5a3b20baf
Mathieu Othacehe <m.othacehe@gmail.com>
2019-07-05 17:21:37 +0200

    gnu: xmlto: Fix cross-compilation.
    * gnu/packages/xml.scm (xmlto)[native-inputs]: Add util-linux.

--- gnu/packages/xml.scm
+++ gnu/packages/xml.scm
@@ -940,6 +940,8 @@ code for classes that correspond to data structures defined by XMLSchema.")
                                               (assoc-ref %build-inputs
                                                          "util-linux")
                                               "/bin/getopt"))))
+    (native-inputs
+     `(("util-linux" ,util-linux)))
     (inputs
      `(("util-linux" ,util-linux)                 ; for 'getopt'
        ("libxml2" ,libxml2)                       ; for 'xmllint'
