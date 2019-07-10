ff452beac5dd7ffecc39556083c94435570d9c1d
Tobias Geerinckx-Rice <me@tobias.gr>
2019-07-10 10:39:29 +0200

    gnu: mupen64plus-ui-console: Correct synopsis.
    * gnu/packages/emulators.scm (mupen64plus-ui-console)[synopisis]: Correct.

--- gnu/packages/emulators.scm
+++ gnu/packages/emulators.scm
@@ -987,7 +987,7 @@ Z64 video plugin.")
        ;; There are no tests.
        #:tests? #f))
     (home-page "https://www.mupen64plus.org/")
-    (synopsis "Mupen64Plus SDL input plugin")
+    (synopsis "Mupen64Plus command line user interface")
     (description
      "Mupen64Plus is a cross-platform plugin-based Nintendo 64 (N64) emulator
 which is capable of accurately playing many games.  This package contains the
