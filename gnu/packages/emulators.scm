84532c2ba4fdf2a496d9ecac61f30c98c019b7c2
Tobias Geerinckx-Rice <me@tobias.gr>
2019-07-10 10:28:11 +0200

    gnu: mupen64plus-video-glide64mk2: Use HTTPS home page.
    * gnu/packages/emulators.scm (mupen64plus-video-glide64mk2)[home-page]: Use HTTPS.

--- gnu/packages/emulators.scm
+++ gnu/packages/emulators.scm
@@ -819,7 +819,7 @@ Glide64 video plugin.")
                (string-append "APIDIR=" m64p "/include/mupen64plus")))
        ;; There are no tests.
        #:tests? #f))
-    (home-page "http://www.mupen64plus.org/")
+    (home-page "https://www.mupen64plus.org/")
     (synopsis "Mupen64Plus Rice Video plugin")
     (description
      "Mupen64Plus is a cross-platform plugin-based Nintendo 64 (N64) emulator
