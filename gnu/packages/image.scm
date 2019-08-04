febbe431554e7f010b43f80fa0273b4122cec343
Marius Bakke <mbakke@fastmail.com>
2019-08-04 16:21:43 +0200

    gnu: exiv2: Update to 0.27.2.
    * gnu/packages/image.scm (exiv2): Update to 0.27.2.

--- gnu/packages/image.scm
+++ gnu/packages/image.scm
@@ -1108,14 +1108,14 @@ channels.")
 (define-public exiv2
   (package
     (name "exiv2")
-    (version "0.27.1")
+    (version "0.27.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://www.exiv2.org/builds/exiv2-" version
                            "-Source.tar.gz"))
        (sha256
-        (base32 "109hbfk63dh14fz20ivq20gcclb9jj9jmh48w4lcn6zxh1ljh9gi"))))
+        (base32 "0gqminvj14xm3rgbnydbywf22608js80rp7nmxxk4497j5mzali6"))))
     (build-system cmake-build-system)
     (arguments '(#:tests? #f))          ; no test suite
     (propagated-inputs
