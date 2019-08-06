c02bb7e9011f7843948e60893054bdf81892e20e
Ricardo Wurmus <rekado@elephly.net>
2019-08-06 13:47:41 +0200

    gnu: r-checkmate: Update to 1.9.4.
    * gnu/packages/statistics.scm (r-checkmate): Update to 1.9.4.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -2137,14 +2137,14 @@ R version.")
 (define-public r-checkmate
   (package
     (name "r-checkmate")
-    (version "1.9.3")
+    (version "1.9.4")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "checkmate" version))
        (sha256
         (base32
-         "15ccwvmw73c2zz1k10k5zdn8px0rrbnvs1b4nzvlb0iwj7cimhp4"))))
+         "08ddpgs4mv5d5y4j054pm8drmxkn7yvhfpbghwxlizjpnxa5g8ps"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-backports" ,r-backports)))
