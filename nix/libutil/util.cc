546a709f202024c8a5173ad372a87ddc1c284c63
Eelco Dolstra <eelco.dolstra@logicblox.com>
2019-08-29 15:04:49 +0200

    daemon: Don't reply on 'st_blocks'.
    Ported by Ludovic Courtès <ludo@gnu.org>
    from <https://github.com/NixOS/nix/commit/a2c4fcd5e9782dc8d2998773380c7171ee53b813>.
    
    * nix/libstore/gc.cc (LocalStore::removeUnusedLinks): Use 'st.st_size'
    instead of 'st.st_blocks * 512'.
    * nix/libutil/util.cc (_deletePath): Likewise.

--- nix/libutil/util.cc
+++ nix/libutil/util.cc
@@ -309,7 +309,7 @@ static void _deletePath(const Path & path, unsigned long long & bytesFreed)
     struct stat st = lstat(path);
 
     if (!S_ISDIR(st.st_mode) && st.st_nlink == 1)
-        bytesFreed += st.st_blocks * 512;
+	bytesFreed += st.st_size;
 
     if (S_ISDIR(st.st_mode)) {
         /* Make the directory writable. */
