466e68b4b2c306d4ae1941eef4a7313343dd1e7b
Ricardo Wurmus <rekado@elephly.net>
2019-09-02 14:34:24 +0200

    gnu: r-ggforce: Update to 0.3.1.
    * gnu/packages/cran.scm (r-ggforce): Update to 0.3.1.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -13765,14 +13765,14 @@ sets of URLs.")
 (define-public r-ggforce
   (package
     (name "r-ggforce")
-    (version "0.3.0")
+    (version "0.3.1")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "ggforce" version))
        (sha256
         (base32
-         "118qyzy8h9kkkdpjd09667gxgw7xy1kbc8r87pswh54ixn8hymwk"))))
+         "04926cqrda6psvy2nzkkw4czwyxdp7fnxg76byp14v12kgd72lm0"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-ggplot2" ,r-ggplot2)
