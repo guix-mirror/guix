07c0110eac24f2e1dc292e143d93f681244838a3
Tobias Geerinckx-Rice <me@tobias.gr>
2019-09-10 15:26:38 +0200

    gnu: weechat: Update to 2.6.
    * gnu/packages/irc.scm (weechat): Update to 2.6.
    [source]: Remove obsolete patch.
    [inputs]: Replace python-2 with python(@3).
    [arguments]: Remove ‘wrap’ phase.
    * gnu/packages/patches/weechat-python.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1398,7 +1398,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/wavpack-CVE-2018-6767.patch		\
   %D%/packages/patches/wavpack-CVE-2018-7253.patch		\
   %D%/packages/patches/wavpack-CVE-2018-7254.patch		\
-  %D%/packages/patches/weechat-python.patch			\
   %D%/packages/patches/wicd-bitrate-none-fix.patch		\
   %D%/packages/patches/wicd-get-selected-profile-fix.patch	\
   %D%/packages/patches/wicd-urwid-1.3.patch			\
