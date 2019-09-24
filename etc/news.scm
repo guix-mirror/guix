8f5e835b0e51a1af173bb969f19c5f1385818b79
Ludovic Courtès <ludo@gnu.org>
2019-09-24 13:52:24 +0200

    news: Add entry for the reduced binary seed bootstrap.
    * etc/news.scm: Add entry for cdd3bcf03883d129581a79e6d6611b2afd3b277b.

--- etc/news.scm
+++ etc/news.scm
@@ -8,6 +8,16 @@
 
 (channel-news
  (version 0)
+
+ (entry (commit "cdd3bcf03883d129581a79e6d6611b2afd3b277b")
+        (title (en "New reduced binary seed bootstrap"))
+        (body
+         (en "The package graph on x86_64 and i686 is now rooted in a
+@dfn{reduced set of binary seeds}.  The initial set of binaries from which
+packages are built now weighs in at approximately 130 MiB, half of what it
+used to be.  Run @code{info \"(guix) Bootstrapping\"} to learn more, or watch
+the talk at @uref{https://archive.fosdem.org/2019/schedule/event/gnumes/}.")))
+
  (entry (commit "dcc90d15581189dbc30e201db2b807273d6484f0")
         (title (en "New channel news mechanism")
                (de "Neuer Mechanismus, um Neuigkeiten über Kanäle anzuzeigen.")
