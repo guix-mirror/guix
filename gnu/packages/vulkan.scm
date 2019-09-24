aa1c7b4914fc7aeb31b2953388d3041dbf0b0ac6
Tobias Geerinckx-Rice <me@tobias.gr>
2019-09-25 01:13:08 +0200

    gnu: spirv-tools: Update to 2019.2.
    * gnu/packages/vulkan.scm (spirv-tools): Update to 2019.2.

--- gnu/packages/vulkan.scm
+++ gnu/packages/vulkan.scm
@@ -82,7 +82,7 @@ and for the GLSL.std.450 extended instruction set.
 (define-public spirv-tools
   (package
     (name "spirv-tools")
-    (version "2019.1")
+    (version "2019.2")
     (source
      (origin
       (method git-fetch)
@@ -90,8 +90,7 @@ and for the GLSL.std.450 extended instruction set.
             (url "https://github.com/KhronosGroup/SPIRV-Tools")
             (commit (string-append "v" version))))
       (sha256
-       (base32
-        "0vddjzhkrhrm3l3i57nxmq2smv3r1s0ka5ff2kziaahr4hqb479r"))
+       (base32 "0zwz6qg8g8165h7cw52agryjrdb29gbmsbziw3pwiddfkyma8vvg"))
       (file-name (string-append name "-" version "-checkout"))))
     (build-system cmake-build-system)
     (arguments
