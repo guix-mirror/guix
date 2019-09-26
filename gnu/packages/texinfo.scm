99fcefb94c19c25dfa77f28fc8569da6d3aca64f
Ludovic Courtès <ludo@gnu.org>
2019-09-26 22:34:35 +0200

    gnu: texinfo: Add version 6.7.
    * gnu/packages/texinfo.scm (texinfo-6.7): New variable.

--- gnu/packages/texinfo.scm
+++ gnu/packages/texinfo.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012, 2013, 2015, 2016, 2017 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012, 2013, 2015, 2016, 2017, 2019 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2014, 2016 Eric Bavier <bavier@member.fsf.org>
 ;;; Copyright © 2015 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2017 Efraim Flashner <efraim@flashner.co.il>
@@ -61,6 +61,18 @@ package includes both the tools necessary to produce Info documents from
 their source and the command-line Info reader.  The emphasis of the language
 is on expressing the content semantically, avoiding physical markup commands.")
     (license gpl3+)))
+
+(define-public texinfo-6.7
+  (package
+    (inherit texinfo)
+    (version "6.7")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/texinfo/texinfo-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "1aicn1v3czqii08wc91jw089n1x3gfchkf808q2as59dak0h714q"))))))
 
 (define-public texinfo-5
   (package (inherit texinfo)
