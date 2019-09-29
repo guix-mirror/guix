512c1696665fba8e6ba189dd9dc2a192cc08a1df
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2019-09-29 15:25:13 +0200

    gnu: mame: Update to 0.214.
    * gnu/packages/emulators.scm (mame): Update to 0.214.
    [source]: Remove unnecessary patch.
    * gnu/packages/patches/mame-rapidjson-fix.patch: Delete file.
    * gnu/local.mk: Remove reference to deleted patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1091,7 +1091,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/lxsession-use-gapplication.patch         \
   %D%/packages/patches/make-glibc-compat.patch			\
   %D%/packages/patches/make-impure-dirs.patch			\
-  %D%/packages/patches/mame-rapidjson-fix.patch			\
   %D%/packages/patches/mariadb-client-test-32bit.patch		\
   %D%/packages/patches/mars-install.patch			\
   %D%/packages/patches/mars-sfml-2.3.patch			\
