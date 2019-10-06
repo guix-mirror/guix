7d82e920717f08bceb42bb570d786dff233171e1
Vagrant Cascadian <vagrant@debian.org>
2019-10-06 06:07:15 +0000

    gnu: linux-libre: Update to 5.3.4.
    * gnu/packages/linux (linux-libre-5.3-version): Update to 5.3.4.
      (deblob-scripts-5.3): Update to 5.3.4.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -184,9 +184,9 @@ defconfig.  Return the appropriate make target if applicable, otherwise return
 
 (define deblob-scripts-5.3
   (linux-libre-deblob-scripts
-   "5.3.1"
+   "5.3.4"
    (base32 "15n09zq38d69y1wl28s3nasf3377qp2yil5b887zpqrm00dif7i4")
-   (base32 "1av9ykv714cnl0clls8rhwa8rwflz6ivg17gharj1x650qp6vnw3")))
+   (base32 "0nrimraf46nf6y1hwkg29fyl0a83wnj0mwq54ggxvffn9gk5h9pa")))
 
 (define deblob-scripts-5.2
   (linux-libre-deblob-scripts
@@ -358,10 +358,10 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                         "linux-" version ".tar.xz"))
     (sha256 hash)))
 
-(define-public linux-libre-5.3-version "5.3.2")
+(define-public linux-libre-5.3-version "5.3.4")
 (define-public linux-libre-5.3-pristine-source
   (let ((version linux-libre-5.3-version)
-        (hash (base32 "0szw21mpp94gp3zn2fgllbv6fdjjf20njgrcjay7vjmm7farq7rn")))
+        (hash (base32 "0vi4bgcr921z5l6fbcrcgmhaji5gl2avpmp7njna6v0f7sxism0r")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.3)))
