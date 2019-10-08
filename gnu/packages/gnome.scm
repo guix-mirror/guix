4ed041d90159f0efc0068745ce9ef03bdb6ef234
Marius Bakke <mbakke@fastmail.com>
2019-10-08 22:04:12 +0200

    gnu: mate-applets: Build with a fixed version of yelp-tools.
    Fixes <https://bugs.gnu.org/37668>.
    Reported by Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>.
    
    * gnu/packages/gnome.scm (yelp-tools/fixed): New public variable.
    * gnu/packages/mate.scm (mate-applets)[native-inputs]: Replace YELP-TOOLS with
    YELP-TOOLS/FIXED.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -4623,6 +4623,17 @@ lifting is done by packages like yelp-xsl and itstool.  This package just
 wraps things up in a developer-friendly way.")
     (license license:gpl2+)))
 
+(define-public yelp-tools/fixed
+  ;; This variant fixes a python-libxml2 crash when processing UTF-8
+  ;; sequences: <https://bugs.gnu.org/37468>.  TODO: Remove this in
+  ;; the next rebuild cycle.
+  (hidden-package
+   (package/inherit
+    yelp-tools
+    (propagated-inputs
+     `(("itstool" ,itstool/fixed)
+       ,@(alist-delete "itstool" (package-propagated-inputs yelp-tools)))))))
+
 (define-public libgee
   (package
     (name "libgee")
