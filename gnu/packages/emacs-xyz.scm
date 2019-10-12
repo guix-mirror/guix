b51c402f23e77a17c564a035887e55b61ef7aecf
Pierre Neidhardt <mail@ambrevar.xyz>
2019-10-12 21:01:14 +0200

    gnu: Add emacs-sly-asdf.
    * gnu/packages/emacs-xyz.scm (emacs-sly-asdf): New variable.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -7008,6 +7008,43 @@ xref, etc...) are still available, but with better integration.")
 sly-quickload command that prompts the user for a package to install. ")
       (license license:gpl3+))))
 
+(define-public emacs-sly-asdf
+  (let ((commit "355739e42c91b9b2339f84453292b938b6d17b0d")
+        (revision "1"))
+    (package
+      (name "emacs-sly-asdf")
+      (version (git-version "0.1.0" revision commit))
+      (home-page "https://github.com/mmgeorge/sly-asdf")
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url home-page)
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "1plkqh4dj35c3cf8ykan8fcvqmxcdqragh4j6xg0sls27mjjz1bq"))))
+      (build-system emacs-build-system)
+      (propagated-inputs
+       `(("emacs-sly" ,emacs-sly)))
+      (arguments
+       `(#:include (cons* "\\.lisp$" "\\.asd$" %default-include)
+         #:phases
+         (modify-phases %standard-phases
+           (add-after 'unpack 'fix-autoload
+             ;; TODO: Reported upstream: https://github.com/mmgeorge/sly-asdf/pull/11
+             (lambda _
+               (substitute* "sly-asdf.el"
+                 (("\\(add-to-list 'sly-contribs 'sly-asdf 'append\\)")
+                  "(with-eval-after-load 'sly
+ (add-to-list 'sly-contribs 'sly-asdf 'append))")))))))
+      (synopsis "ASDF contrib for SLY")
+      (description
+       "@command{sly-asdf} is an external contrib for SLY that provides
+additional support for working with ASDF projects.")
+      (license license:gpl3+))))
+
 (define-public emacs-lua-mode
   (let ((commit "95c64bb5634035630e8c59d10d4a1d1003265743")
         (revision "2"))
