e08ba87e6c56cc14729cc31b109a27ae3d53a71f
Efraim Flashner <efraim@flashner.co.il>
2019-10-23 10:10:55 +0300

    gnu: Add rust-winapi-x86-64-pc-windows-gnu-0.4.
    * gnu/packages/rust-cbindgen.scm (rust-winapi-x86-64-pc-windows-gnu-0.4):
    New hidden variable.

--- gnu/packages/rust-cbindgen.scm
+++ gnu/packages/rust-cbindgen.scm
@@ -902,3 +902,25 @@ i686-pc-windows-gnu target.  Please don't use this crate directly, depend on
     (properties '((hidden? . #t)))
     (license (list license:asl2.0
                    license:expat))))
+
+(define rust-winapi-x86-64-pc-windows-gnu-0.4
+  (package
+    (name "rust-winapi-x86-64-pc-windows-gnu")
+    (version "0.4.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "winapi-x86_64-pc-windows-gnu" version))
+        (file-name (string-append name "-" version ".crate"))
+        (sha256
+         (base32
+          "0gqq64czqb64kskjryj8isp62m2sgvx25yyj3kpc2myh85w24bki"))))
+    (build-system cargo-build-system)
+    (home-page "https://github.com/retep998/winapi-rs")
+    (synopsis "Import libraries for the x86_64-pc-windows-gnu target")
+    (description "This package provides import libraries for the
+x86_64-pc-windows-gnu target.  Please don't use this crate directly, depend on
+@code{winapi} instead.")
+    (properties '((hidden? . #t)))
+    (license (list license:asl2.0
+                   license:expat))))
