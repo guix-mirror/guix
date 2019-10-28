guix-mirror/guix

commit d727a9343d861cf775645df8be5bfefd43d6c6f0

derivations: Don't memoize 'derivation->bytevector'.
Its hit rate was only 8%.  Removing it reduces heap size of "guix build
libreoffice -nd" from 69MiB to 61MiB and the wall-clock time is unchanged.

* guix/derivations.scm (derivation->bytevector): Change from 'mlambda'
to 'lambda'.

guix/derivations.scm
@@ -622,7 +622,7 @@ that form."
      (display ")" port))))
 
 (define derivation->bytevector
-  (mlambda (drv)
+  (lambda (drv)
     "Return the external representation of DRV as a UTF-8-encoded string."
     (with-fluids ((%default-port-encoding "UTF-8"))
       (call-with-values open-bytevector-output-port

commit 8c8d60752e1ad73d5bd87d8497b357f8a8a389ab

derivation: Remove memoization invalidation for 'derivation->bytevector'.
This is a followup to d727a9343d861cf775645df8be5bfefd43d6c6f0, which
broke 'hydra-jobs' from (gnu ci).

* guix/derivations.scm (invalidate-derivation-caches!): Remove call to
'invalidate-memoization!' for 'derivation->bytevector'.

guix/derivations.scm
@@ -919,7 +919,6 @@ derivation.  It is kept as-is, uninterpreted, in the derivation."
 long-running processes that know what they're doing.  Use with care!"
   ;; Typically this is meant to be used by Cuirass and Hydra, which can clear
   ;; caches when they start evaluating packages for another architecture.
-  (invalidate-memoization! derivation->bytevector)
   (invalidate-memoization! derivation-base16-hash)
 
   ;; FIXME: Comment out to work around <https://bugs.gnu.org/36487>.