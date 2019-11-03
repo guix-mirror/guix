a349835edb8e7e702089f6295e043c94caec6b11
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-03 13:25:39 +0300

    gnu: docker: Adjust tests for changes in go.
    * gnu/packages/patches/docker-adjust-tests-for-changes-in-go.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Use this.
    * gnu/packages/docker.scm (docker): Use this.

--- gnu/local.mk
+++ gnu/local.mk
@@ -781,6 +781,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/docbook-xsl-nonrecursive-string-subst.patch	\
   %D%/packages/patches/doc++-include-directives.patch		\
   %D%/packages/patches/doc++-segfault-fix.patch			\
+  %D%/packages/patches/docker-adjust-tests-for-changes-in-go.patch	\
   %D%/packages/patches/docker-engine-test-noinstall.patch	\
   %D%/packages/patches/docker-fix-tests.patch			\
   %D%/packages/patches/docker-use-fewer-modprobes.patch		\
