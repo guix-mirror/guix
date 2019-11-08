eaa52b5baa8fbb26a79890561d0c40344d1e1bf3
Ludovic Courtès <ludovic.courtes@inria.fr>
2019-11-08 17:51:31 +0100

    gnu: openmpi: Pass explicit "--with-*" configure flags.
    * gnu/packages/mpi.scm (openmpi)[arguments]: Pass "--with-ucx",
    "--with-psm", and "--with-psm2".

--- gnu/packages/mpi.scm
+++ gnu/packages/mpi.scm
@@ -204,6 +204,12 @@ bind processes, and much more.")
                            "--with-hwloc=external"
                            "--with-libevent"
 
+                           ;; Make sure ./configure fails if one of these is
+                           ;; missing.
+                           "--with-ucx"
+                           "--with-psm"
+                           "--with-psm2"
+
                            ;; InfiniBand support
                            "--enable-openib-control-hdr-padding"
                            "--enable-openib-dynamic-sl"
