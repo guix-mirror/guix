d4f4b30bbc6040eca187f080cda785e21c3a5028
Ricardo Wurmus <rekado@elephly.net>
2019-11-14 08:20:58 +0100

    gnu: r-future: Update to 1.15.0.
    * gnu/packages/cran.scm (r-future): Update to 1.15.0.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -11405,14 +11405,14 @@ them in distributed compute environments.")
 (define-public r-future
   (package
     (name "r-future")
-    (version "1.14.0")
+    (version "1.15.0")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "future" version))
        (sha256
         (base32
-         "1jyv2wlmpfqbk3hw269h4xg36na3wh1kd1lxmwdb40bsv4850lqa"))))
+         "1cbp7agb9lipjxsh7xm1yphh8a4hrjy7wrbkvhsxn1swh0c4s3b7"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-digest" ,r-digest)
