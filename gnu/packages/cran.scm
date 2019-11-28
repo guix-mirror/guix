ad6ab1a88160e46bb91cd73545b589c261f407f9
Ricardo Wurmus <rekado@elephly.net>
2019-11-28 09:59:23 +0100

    gnu: r-emmeans: Update to 1.4.3.
    * gnu/packages/cran.scm (r-emmeans): Update to 1.4.3.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -8069,14 +8069,14 @@ Hothorn, Westfall, 2010, CRC Press).")
 (define-public r-emmeans
   (package
     (name "r-emmeans")
-    (version "1.4.2")
+    (version "1.4.3")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "emmeans" version))
        (sha256
         (base32
-         "1sxwbh6sym2shrj7gva8q96ca2csqz3081q4d84avpxz15dfz1z1"))))
+         "15d78jhpil9p98cy3aac6svbkjsml1dh0z8hm8cka5l6nik8x0k9"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-estimability" ,r-estimability)
