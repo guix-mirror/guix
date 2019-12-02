857d443494740a04a4c1a90f9be23d955f54a7a6
Marius Bakke <mbakke@fastmail.com>
2019-12-03 00:10:55 +0100

    gnu: openldap: Update to 2.4.48.
    * gnu/packages/openldap.scm (openldap): Update to 2.4.48.

--- gnu/packages/openldap.scm
+++ gnu/packages/openldap.scm
@@ -59,7 +59,7 @@
 (define-public openldap
   (package
    (name "openldap")
-   (version "2.4.47")
+   (version "2.4.48")
    (source (origin
             (method url-fetch)
 
@@ -76,7 +76,7 @@
                         "openldap-release/openldap-" version ".tgz")))
             (sha256
              (base32
-              "02sj0p1pq12hqq29b22m3f5zs2rykgvc0q3wlynxjcsjhrvmhk7m"))))
+              "0k87qra4kirb6xgja4q1jyw6piwb9v8b8g6gkxq4plawmgy3ylnr"))))
    (build-system gnu-build-system)
    (inputs `(("bdb" ,bdb-5.3)
              ("cyrus-sasl" ,cyrus-sasl)
