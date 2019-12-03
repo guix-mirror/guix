4427417bd74c369162aff3ca51b37bc6313a6438
Guillaume Le Vaillant <glv@posteo.net>
2019-12-03 18:58:19 +0100

    gnu: websocketpp: Add patch for Boost >= 1.70.
    * gnu/packages/patches/websocket-fix-for-boost-1.70.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/web.scm (websocketpp): Use it.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1442,6 +1442,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/wavpack-CVE-2018-7253.patch		\
   %D%/packages/patches/wavpack-CVE-2018-7254.patch		\
   %D%/packages/patches/weasyprint-library-paths.patch		\
+  %D%/packages/patches/websocketpp-fix-for-boost-1.70.patch	\
   %D%/packages/patches/wicd-bitrate-none-fix.patch		\
   %D%/packages/patches/wicd-get-selected-profile-fix.patch	\
   %D%/packages/patches/wicd-urwid-1.3.patch			\
