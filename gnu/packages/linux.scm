2eec86e1457362af7cc410fa368d30ac9e40ab14
Mark H Weaver <mhw@netris.org>
2019-12-05 14:26:46 -0500

    gnu: linux-libre@4.4: Update to 4.4.206.
    * gnu/packages/linux.scm (linux-libre-4.4-version): Update to 4.4.206.
    (linux-libre-4.4-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -384,10 +384,10 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.9)))
 
-(define-public linux-libre-4.4-version "4.4.205")
+(define-public linux-libre-4.4-version "4.4.206")
 (define-public linux-libre-4.4-pristine-source
   (let ((version linux-libre-4.4-version)
-        (hash (base32 "19pasidvfmf94rs86v80x7hpirz9gavmkxwcl76ya61fq7lqy7zs")))
+        (hash (base32 "14ylg9cm7z12mvkzg8z92gsw0libw9xz392ayzw0d9cgw1py39ax")))
     (make-linux-libre-source version
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.4)))
