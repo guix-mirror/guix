c6af97b70a46efc59e4bf22f4566ffa502306f3e
Marius Bakke <mbakke@fastmail.com>
2019-12-06 19:13:27 +0100

    gnu: libfreehand: Fix build with ICU 65.
    * gnu/packages/libreoffice.scm (libfreehand)[arguments]: New field.

--- gnu/packages/libreoffice.scm
+++ gnu/packages/libreoffice.scm
@@ -482,6 +482,17 @@ library primarily intended for language guessing.")
       (sha256 (base32
                "1b1lvqh68rwij1yvmxy02hsmh7i74ma5767mk8mg5nx6chajshhf"))))
     (build-system gnu-build-system)
+    (arguments
+     '(#:phases (modify-phases %standard-phases
+                  (add-before 'build 'adjust-for-ICU-65
+                    (lambda _
+                      ;; Fix build with ICU 65 and later.  Taken from this
+                      ;; upstream commit, remove for libfreehand > 0.1.2:
+                      ;; https://gerrit.libreoffice.org/#/c/80224/
+                      (substitute* "src/lib/libfreehand_utils.cpp"
+                        (("U16_NEXT.*" all)
+                         (string-append all ";\n")))
+                      #t)))))
     (native-inputs
      `(("cppunit" ,cppunit)
        ("doxygen" ,doxygen)
