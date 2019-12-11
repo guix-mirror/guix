328dd9b7c2030dfb66013e12f055b5b449c94007
Jethro Cao <jethrocao@gmail.com>
2019-12-11 21:33:44 +0700

    gnu: Add love-nuklear.
    * gnu/packages/game-development.scm (love-nuklear): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/game-development.scm
+++ gnu/packages/game-development.scm
@@ -16,6 +16,7 @@
 ;;; Copyright © 2018 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2019 Pierre Neidhardt <mail@ambrevar.xyz>
 ;;; Copyright © 2019 Leo Prikler <leo.prikler@student.tugraz.at>
+;;; Copyright © 2019 Jethro Cao <jethrocao@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -652,6 +653,37 @@ archive on a per-file basis.")
 programming language.")
     (home-page "https://love2d.org/")
     (license license:zlib)))
+
+(define-public love-nuklear
+  (let ((version "v2.6")
+        (commit "fef4e00a602efb16c57ae962850b6e7a01f7a29a"))
+    (package
+      (name "love-nuklear")
+      (version (git-version version "1" commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/keharriso/love-nuklear/")
+                      (commit commit)
+                      (recursive? #t)))
+                ;; NOTE: the HEAD of the Nuklear git-submodule is at commit 
+                ;; "adc52d710fe3c87194b99f540c53e82eb75c2521" of Oct 1 2019
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "15qmy8mfwkxy2x9rmxs6f9cyvjvwwj6yf78bs863xmc56dmjzzbn"))))
+      (build-system cmake-build-system)
+      (arguments
+       `(#:build-type "Release"
+         #:tests? #f))
+      (inputs
+       `(("luajit" ,luajit)))
+      (synopsis "Lightweight immediate mode GUI for LÖVE games")
+      (description "LÖVE is a Lua framework for making 2D games.  Nuklear
+is a minimal state immediate mode graphical user interface toolkit.  This
+package is the Nuklear bindings for LÖVE created by Kevin Harrison.")
+      (home-page "https://github.com/keharriso/love-nuklear/")
+      (license license:expat))))
 
 (define-public allegro-4
   (package
