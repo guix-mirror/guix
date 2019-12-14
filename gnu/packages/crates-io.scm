878ff13ee17cd8f275bd41613a5cd4195038f815
John Soo <jsoo1@asu.edu>
2019-12-14 13:37:42 -0800

    gnu: Add rust-term-0.5.1.
    * gnu/packages/crates-io.scm (rust-term-0.5.1): New variable.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -5030,6 +5030,23 @@ directories.")
     (license (list license:asl2.0
                    license:expat))))
 
+(define-public rust-term-0.5.1
+  (package
+    (inherit rust-term-0.4)
+    (name "rust-term")
+    (version "0.5.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "term" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0qbmqd8jbjlqr4608qdmvp6yin5ypifzi5s2xyhlw8g8s5ynfssy"))))
+    (arguments `(#:skip-build? #t))
+    (properties '())))
+
 (define-public rust-termcolor-1.0
   (package
     (name "rust-termcolor")
