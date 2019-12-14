e45eb8086e1be367db5cf172663d38b9da8426e8
John Soo <jsoo1@asu.edu>
2019-12-14 13:52:11 -0800

    gnu: Add rust-lock-api-0.2.
    * gnu/packages/crates-io.scm (rust-lock-api-0.2): New variable.
    (rust-lock-api-0.1): Inherit from rust-lock-api-0.2.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -2402,9 +2402,37 @@ functions and static variables these libraries contain.")
     (license (list license:asl2.0
                    license:expat))))
 
+(define-public rust-lock-api-0.2
+  (package
+    (name "rust-lock-api")
+    (version "0.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lock_api" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "1zx7pksmgyggpczgw4qrr4vj2nkdk5lipgiysvr20slm552nv57d"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-owning-ref" ,rust-owning-ref-0.4)
+        ("rust-scopeguard" ,rust-scopeguard-1.0)
+        ("rust-serde" ,rust-serde-1.0))))
+    (home-page "https://github.com/Amanieu/parking_lot")
+    (synopsis
+     "Wrappers to create fully-featured Mutex and RwLock types")
+    (description
+     "This package provides wrappers to create fully-featured @code{Mutex} and
+@code{RwLock} types.  It is compatible with @code{no_std}.")
+    (license (list license:expat license:asl2.0))))
 
 (define-public rust-lock-api-0.1
   (package
+    (inherit rust-lock-api-0.2)
     (name "rust-lock-api")
     (version "0.1.5")
     (source
@@ -2415,15 +2443,7 @@ functions and static variables these libraries contain.")
         (sha256
          (base32
           "0b24q9mh258xa52ap636q1sxz0j5vrnp0hwbbh7ddjka3wwz3sv2"))))
-    (build-system cargo-build-system)
-    (home-page "https://github.com/Amanieu/parking_lot")
-    (synopsis "Wrappers to create fully-featured Mutex and RwLock types")
-    (description
-     "This package provides wrappers to create fully-featured @code{Mutex} and
-@code{RwLock} types.  It is compatible with @code{no_std}.")
-    (properties '((hidden? . #t)))
-    (license (list license:asl2.0
-                   license:expat))))
+    (properties '((hidden? . #t)))))
 
 (define-public rust-log-0.4
   (package
