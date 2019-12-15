c88448d4b81978111e2ded8e5af97196fafbf9da
Ricardo Wurmus <rekado@elephly.net>
2019-12-15 14:38:42 +0100

    gnu: r-biocsingular: Update to 1.2.0.
    * gnu/packages/bioconductor.scm (r-biocsingular): Update to 1.2.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -2070,14 +2070,14 @@ achieved for all methods using the BiocParallel framework.")
 (define-public r-biocsingular
   (package
     (name "r-biocsingular")
-    (version "1.0.0")
+    (version "1.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BiocSingular" version))
        (sha256
         (base32
-         "129z6bkdhm5wlvrjiwrr8yl2jj9chh4i6dm6firlj4c4ql3jp4f5"))))
+         "0qd7r2k56ym7ivjgapxbk7fyj2d7396f1ad1hkgnicgyw1an5q1r"))))
     (properties `((upstream-name . "BiocSingular")))
     (build-system r-build-system)
     (propagated-inputs
