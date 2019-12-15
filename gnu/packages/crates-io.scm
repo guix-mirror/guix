ef6e6faa96dcd4760a7149aa364ee96f684eb0bd
John Soo <jsoo1@asu.edu>
2019-12-15 07:40:18 -0800

    gnu: Add rust-encoding-index-japanese-1.20141219.
    * gnu/packages/crates-io.scm (rust-encoding-index-japanese-1.20141219): New variable.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -1973,6 +1973,30 @@ floating-point primitives to an @code{io::Write}.")
 char, u8 and u16.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-encoding-index-japanese-1.20141219
+  (package
+    (name "rust-encoding-index-japanese")
+    (version "1.20141219.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "encoding-index-japanese" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "148c1lmd640p1d7fzk0nv7892mbyavvwddgqvcsm78798bzv5s04"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-encoding-index-tests" ,rust-encoding-index-tests-0.1))))
+    (home-page "https://github.com/lifthrasiir/rust-encoding")
+    (synopsis "Index tables for Japanese character encodings")
+    (description
+     "Index tables for Japanese character encodings.")
+    (license license:cc0)))
+
 (define-public rust-encoding-index-korean-1.20141219
   (package
     (name "rust-encoding-index-korean")
