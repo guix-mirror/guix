3381dc4b5c11ca60ef9ae00164a8563fc3ba3bd4
Ricardo Wurmus <rekado@elephly.net>
2019-12-15 14:57:50 +0100

    gnu: r-mzr: Update to 2.20.0.
    * gnu/packages/bioinformatics.scm (r-mzr): Update to 2.20.0.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -9202,14 +9202,14 @@ proteomics packages.")
 (define-public r-mzr
   (package
     (name "r-mzr")
-    (version "2.18.1")
+    (version "2.20.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "mzR" version))
        (sha256
         (base32
-         "1pr1pcrg3r3pccm5ag6l8ic6rpqbk9jnlb9mm7g4ak5jwrajbzjq"))
+         "1cwd7phlc5jbx6r6cznyfbdpvcin5fvsaasbbi65zn0s92a80r13"))
        (modules '((guix build utils)))
        (snippet
         '(begin
@@ -9231,7 +9231,7 @@ proteomics packages.")
              #t)))))
     (inputs
      `(;; Our default boost package won't work here, unfortunately, even with
-       ;; mzR version 2.18.1.
+       ;; mzR version 2.20.0.
        ("boost" ,boost-for-mysql) ; use this instead of the bundled boost sources
        ("zlib" ,zlib)))
     (propagated-inputs
