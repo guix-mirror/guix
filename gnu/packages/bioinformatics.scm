da03fa3c908ffe6c1082e03d5cdfdf071269304f
Ricardo Wurmus <rekado@elephly.net>
2019-12-15 15:14:31 +0100

    gnu: r-edger: Update to 3.28.0.
    * gnu/packages/bioinformatics.scm (r-edger): Update to 3.28.0.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -7448,13 +7448,13 @@ names in their natural, rather than lexicographic, order.")
 (define-public r-edger
   (package
     (name "r-edger")
-    (version "3.26.8")
+    (version "3.28.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "edgeR" version))
               (sha256
                (base32
-                "1wwimzviy2vklp80faz7sbbp74qcw2csbmlfgvzj7b785vwarpwg"))))
+                "1m7l6fbyik1b79v0h9qkmpa39svij1yiw24gxahbdqjg7fzsavh3"))))
     (properties `((upstream-name . "edgeR")))
     (build-system r-build-system)
     (propagated-inputs
