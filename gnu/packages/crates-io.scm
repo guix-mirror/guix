3c313f185f28e7d8e21f67057c752f5f02f24f93
John Soo <jsoo1@asu.edu>
2019-12-14 18:57:25 -0800

    gnu: Add rust-slog-2.4.
    * gnu/packages/crates-io.scm (rust-slog-2.4): New variable.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -5929,6 +5929,32 @@ data type.")
     (properties '((hidden? . #t)))
     (license license:expat)))
 
+(define-public rust-slog-2.4
+  (package
+    (name "rust-slog")
+    (version "2.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "slog" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "13jh74jlckzh5cygkhs0k4r82wnmw8ha2km829xwslhr83n2w6hy"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-erased-serde" ,rust-erased-serde-0.3))))
+    (home-page "https://github.com/slog-rs/slog")
+    (synopsis "Structured, extensible, composable logging for Rust")
+    (description
+     "Structured, extensible, composable logging for Rust.")
+    (license (list license:mpl2.0
+                   license:expat
+                   license:asl2.0))))
+
 (define-public rust-smallvec-0.6
   (package
     (name "rust-smallvec")
