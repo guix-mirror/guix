fe9a113bb49532f8637fa05304bd0ffcf9bc0a5d
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-01-03 12:41:31 +0100

    gnu: python-zstandard: Fix indentation.
    * gnu/packages/python-compression.scm (python-zstandard): Fix indentation.

--- gnu/packages/python-compression.scm
+++ gnu/packages/python-compression.scm
@@ -238,20 +238,20 @@ wrapper.  It provides a backport of the @code{Path} object.")
 
 (define-public python-zstandard
   (package
-  (name "python-zstandard")
-  (version "0.13.0")
-  (source
-    (origin
-      (method url-fetch)
-      (uri (pypi-uri "zstandard" version))
-      (sha256
+    (name "python-zstandard")
+    (version "0.13.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "zstandard" version))
+       (sha256
         (base32 "0q9msi00s93iqm8vzd839r7yc51gz54z90h5bckqyjdxa6vxijz5"))))
-  (build-system python-build-system)
-  (native-inputs
-   `(("python-hypothesis" ,python-hypothesis)))
-  (home-page "https://github.com/indygreg/python-zstandard")
-  (synopsis "Zstandard bindings for Python")
-  (description "This project provides Python bindings for interfacing
-with the Zstandard compression library. A C extension and CFFI
-interface are provided.")
-  (license license:bsd-3)))
+    (build-system python-build-system)
+    (native-inputs
+     `(("python-hypothesis" ,python-hypothesis)))
+    (home-page "https://github.com/indygreg/python-zstandard")
+    (synopsis "Zstandard bindings for Python")
+    (description "This project provides Python bindings for interfacing with
+the Zstandard compression library.  A C extension and CFFI interface are
+provided.")
+    (license license:bsd-3)))
