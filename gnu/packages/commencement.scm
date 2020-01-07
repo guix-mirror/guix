34e8f0afef7cb6dbe7891707326a625a8f02657d
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2020-01-07 17:48:13 +0100

    gnu: make-gcc-toolchain: Fix typo in description.
    * gnu/packages/commencement.scm (make-gcc-toolchain)[description]: Fix typo
    and triple spacing.
    
    Signed-off-by: Marius Bakke <mbakke@fastmail.com>

--- gnu/packages/commencement.scm
+++ gnu/packages/commencement.scm
@@ -2562,8 +2562,8 @@ COREUTILS-FINAL vs. COREUTILS, etc."
       (synopsis "Complete GCC tool chain for C/C++ development")
       (description
        "This package provides a complete GCC tool chain for C/C++ development to
-be   installed in user profiles.  This includes GCC, as well as libc (headers
-an  d binaries, plus debugging symbols in the @code{debug} output), and Binutils.")
+be installed in user profiles.  This includes GCC, as well as libc (headers and
+binaries, plus debugging symbols in the @code{debug} output), and Binutils.")
       (home-page "https://gcc.gnu.org/")
       (outputs '("out" "debug" "static"))
 
