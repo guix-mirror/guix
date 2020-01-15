guix-mirror/guix

commit 8f4cff0e21e4142d5f51022ef1efad569c3ca0bc

gnu: popt: Update source URI.
* gnu/packages/popt.scm (popt)[source](uri): Add Debian mirror.

gnu/packages/popt.scm
@@ -62,8 +62,13 @@ line syntax.")
     (version "1.16")
     (source (origin
              (method url-fetch)
-             (uri (string-append "http://rpm5.org/files/popt/popt-"
-                                 version ".tar.gz"))
+             ;; The rpm5.org domain does not resolve since 2019-06-13, so we
+             ;; borrow Debians infrastructure for the source download.
+             (uri (list (string-append "https://deb.debian.org/debian/pool/main"
+                                       "/p/popt/popt_" version ".orig.tar.gz")
+                        ;; Keep the old URL around in case it reappears.
+                        (string-append "http://rpm5.org/files/popt/popt-"
+                                       version ".tar.gz")))
              (sha256
               (base32
                "1j2c61nn2n351nhj4d25mnf3vpiddcykq005w2h6kw79dwlysa77"))))

commit 98ca002aaf14b38ae3445e801d497177888f8438

gnu: popt: Reverse source mirror entries.
Commit 8f4cff0e21e4142d5f51022ef1efad569c3ca0bc accidentally caused rebuilds
because the expected file name changed.

* gnu/packages/popt.scm (popt)[source](uri): Add the Debian mirror last.

gnu/packages/popt.scm
@@ -62,13 +62,12 @@ line syntax.")
     (version "1.16")
     (source (origin
              (method url-fetch)
-             ;; The rpm5.org domain does not resolve since 2019-06-13, so we
-             ;; borrow Debians infrastructure for the source download.
-             (uri (list (string-append "https://deb.debian.org/debian/pool/main"
-                                       "/p/popt/popt_" version ".orig.tar.gz")
-                        ;; Keep the old URL around in case it reappears.
-                        (string-append "http://rpm5.org/files/popt/popt-"
-                                       version ".tar.gz")))
+             (uri (list (string-append "http://rpm5.org/files/popt/popt-"
+                                       version ".tar.gz")
+                        ;; The rpm5.org domain does not resolve since 2019-06-13,
+                        ;; so fallback to Debians copy.
+                        (string-append "https://deb.debian.org/debian/pool/main"
+                                       "/p/popt/popt_" version ".orig.tar.gz")))
              (sha256
               (base32
                "1j2c61nn2n351nhj4d25mnf3vpiddcykq005w2h6kw79dwlysa77"))))