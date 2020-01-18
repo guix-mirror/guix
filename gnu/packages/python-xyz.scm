e24c672e740664c1340a4ff8bf43a21b4bd3e5d1
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-01-18 17:27:25 +0800

    gnu: Add python-asynctest.
    * gnu/packages/python-xyz.scm (python-asynctest): New variable.
    
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -17117,3 +17117,36 @@ memoization.")
     (description "This package is a parser for Coffeescript Object
 Notation (CSON).")
     (license license:expat)))
+
+(define-public python-asynctest
+  (package
+    (name "python-asynctest")
+    (version "0.13.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "asynctest" version))
+       (sha256
+        (base32
+         "1b3zsy7p84gag6q8ai2ylyrhx213qdk2h2zb6im3xn0m5n264y62"))))
+    (build-system python-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (replace 'check
+           (lambda _
+             (invoke "python" "-X" "dev" "-m" "unittest" "-v" "test")))
+         (add-after 'unpack 'disable-tests
+           (lambda* _
+             (substitute* "test/test_selector.py"
+               ;; XXX: This test fails for unknown reason inside the build
+               ;; environment.
+               (("def test_events_watched_outside_test_are_ignored")
+                "@unittest.skip('disabled by guix')
+    def test_events_watched_outside_test_are_ignored")))))))
+    (home-page "https://github.com/Martiusweb/asynctest")
+    (synopsis "Extension of unittest for testing asyncio libraries")
+    (description
+     "The package asynctest is built on top of the standard unittest module
+and cuts down boilerplate code when testing libraries for asyncio.")
+    (license license:asl2.0)))
