2daded5a9152003c3e64cb47250084c8a3192035
Marius Bakke <mbakke@fastmail.com>
2020-01-19 20:25:12 +0100

    gnu: python-pygments: Update to 2.5.2.
    * gnu/packages/python-xyz.scm (python-pygments): Update to 2.5.2.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -2616,14 +2616,14 @@ reStructuredText.")
 (define-public python-pygments
   (package
     (name "python-pygments")
-    (version "2.4.2")
+    (version "2.5.2")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "Pygments" version))
        (sha256
         (base32
-         "15v2sqm5g12bqa0c7wikfh9ck2nl97ayizy1hpqhmws5gqalq748"))))
+         "1zmhnswy0wxfn0xprs9aqsvx2c3kmzfn2wx14q8cv3vpkxdamj4q"))))
     (build-system python-build-system)
     (arguments
      ;; FIXME: Tests require sphinx, which depends on this.
