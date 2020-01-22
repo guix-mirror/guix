1b0290255399cdd695a4a669d886036654bf3a57
Jakub Kądziołka <kuba@kadziolka.net>
2020-01-22 18:47:32 +0100

    gnu: Add megacmd.
    * gnu/packages/sync.scm (megacmd): New variable.
    
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/sync.scm
+++ gnu/packages/sync.scm
@@ -5,6 +5,7 @@
 ;;; Copyright © 2018 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2018, 2019 Nicolas Goaziou <mail@nicolasgoaziou.fr>
 ;;; Copyright © 2019 Clément Lassieur <clement@lassieur.org>
+;;; Copyright © 2020 Jakub Kądziołka <kuba@kadziolka.net>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -24,6 +25,7 @@
 (define-module (gnu packages sync)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system cmake)
+  #:use-module (guix build-system gnu)
   #:use-module (guix build-system go)
   #:use-module (guix build-system meson)
   #:use-module (guix download)
@@ -31,20 +33,72 @@
   #:use-module (guix packages)
   #:use-module (gnu packages)
   #:use-module (gnu packages acl)
+  #:use-module (gnu packages adns)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
+  #:use-module (gnu packages crypto)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages golang)
+  #:use-module (gnu packages image)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages lua)
+  #:use-module (gnu packages pcre)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages qt)
+  #:use-module (gnu packages readline)
   #:use-module (gnu packages rsync)
   #:use-module (gnu packages selinux)
   #:use-module (gnu packages sphinx)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages tls))
+
+(define-public megacmd
+  (package
+    (name "megacmd")
+    (version "1.1.0")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://github.com/meganz/MEGAcmd.git")
+              (commit (string-append version "_Linux"))
+              (recursive? #t)))
+        (sha256
+         (base32
+          "004j8m3xs6slx03g2g6wzr97myl2v3zc09wxnfar5c62a625pd53"))
+        (file-name (git-file-name name version))))
+    (build-system gnu-build-system)
+    ;; XXX: Disabling tests because they depend on libgtest.la from googletest,
+    ;; which is not installed for unclear reasons.
+    (arguments
+     `(#:tests? #f
+       #:configure-flags '("--with-pcre")))
+    (native-inputs
+     `(("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("libtool" ,libtool)))
+    (inputs
+     `(("c-ares" ,c-ares)
+       ("crypto++" ,crypto++)
+       ("curl" ,curl)
+       ("freeimage" ,freeimage)
+       ("gtest" ,googletest)
+       ("openssl" ,openssl)
+       ("pcre" ,pcre)
+       ("readline" ,readline)
+       ("sodium" ,libsodium)
+       ("sqlite3" ,sqlite)
+       ("zlib" ,zlib)))
+    (home-page "https://mega.nz/cmd")
+    (synopsis
+     "Command Line Interactive and Scriptable Application to access mega.nz")
+    (description "MEGAcmd provides non UI access to MEGA services.  It intends
+to offer all the functionality of a MEGA account via commands.  It features
+synchronization, backup of local folders into a MEGA account and a
+webdav/streaming server.")
+    (license (list license:bsd-2 license:gpl3+))))
 
 (define-public owncloud-client
   (package
