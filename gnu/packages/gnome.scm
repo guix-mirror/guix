632a9b32c560606dc7061027683c10c9b16be29f
Marius Bakke <mbakke@fastmail.com>
2020-01-23 23:20:21 +0100

    gnu: yelp-xsl: Update to 3.34.2.
    * gnu/packages/gnome.scm (yelp-xsl): Update to 3.34.2.
    [native-inputs]: Remove INTLTOOL.  Add GETTEXT-MINIMAL.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -5574,7 +5574,7 @@ of running programs and invoke methods on those interfaces.")
 (define-public yelp-xsl
   (package
     (name "yelp-xsl")
-    (version "3.32.1")
+    (version "3.34.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -5582,10 +5582,10 @@ of running programs and invoke methods on those interfaces.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "013z2ixx9kfrs6hq79qpil093xfbc12y1p0mvsh6lpala30iphya"))))
+                "1bdpgkzawhqmw52l6zx8czzg1ndfgcf1p44m2bxjdpqkc4afcgqc"))))
     (build-system gnu-build-system)
     (native-inputs
-     `(("intltool" ,intltool)
+     `(("gettext-minimal" ,gettext-minimal)
        ("itstool" ,itstool)
        ("xmllint" ,libxml2)))
     (home-page "https://wiki.gnome.org/Apps/Yelp")
