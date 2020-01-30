6610e847c0f978cf3cf706aa91b4e62ac53d0669
Marius Bakke <mbakke@fastmail.com>
2020-01-30 22:12:02 +0100

    gnu: gdb: Add separate variant with source highlighting support.
    This partially reverts commit 480b3279fd8f937fac986a88592ee5cb968ab3ff in
    order to prevent a huge rebuild.
    
    * gnu/packages/gdb.scm (gdb-8.3)[properties]: New field.
    [inputs]: Remove SOURCE-HIGHLIGHT.
    * gnu/packages/gdb.scm (gdb-8.2)[inputs]: Remove.
    (gdb/next): New public variable.

--- gnu/packages/gdb.scm
+++ gnu/packages/gdb.scm
@@ -2,6 +2,7 @@
 ;;; Copyright © 2013, 2014, 2015, 2019, 2020 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2015 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015, 2016, 2019 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2020 Marius Bakke <mbakke@fastmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -47,6 +48,10 @@
              (sha256
               (base32
                "1i2pjwaafrlz7wqm40b4znr77ai32rjsxkpl2az38yyarpbv8m8y"))))
+
+    ;; Hide this package so that end users get 'gdb/next' below.
+    (properties '((hidden? . #t)))
+
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f ; FIXME "make check" fails on single-processor systems.
@@ -91,7 +96,6 @@
        ("python" ,python)
        ("python-wrapper" ,python-wrapper)
        ("dejagnu" ,dejagnu)
-       ("source-highlight" ,source-highlight)
 
        ;; Allow use of XML-formatted syscall information.  This enables 'catch
        ;; syscall' and similar commands.
@@ -109,6 +113,8 @@ the program is running to try to fix bugs.  It can be used to debug programs
 written in C, C++, Ada, Objective-C, Pascal and more.")
     (license gpl3+)))
 
+;; This version of GDB is required by some of the Rust compilers, see
+;; <https://bugs.gnu.org/37810>.
 (define-public gdb-8.2
   (package/inherit
    gdb-8.3
@@ -119,8 +125,16 @@ written in C, C++, Ada, Objective-C, Pascal and more.")
                                  version ".tar.xz"))
              (sha256
               (base32
-               "00i27xqawjv282a07i73lp1l02n0a3ywzhykma75qg500wll6sha"))))
-   (inputs (alist-delete "source-highlight" (package-inputs gdb-8.3)))))
+               "00i27xqawjv282a07i73lp1l02n0a3ywzhykma75qg500wll6sha"))))))
+
+;; The "next" version of GDB, to be merged with 'gdb' in the next rebuild cycle.
+(define-public gdb/next
+  (package/inherit
+   gdb-8.3
+   (inputs
+    `(("source-highlight" ,source-highlight)
+      ,@(package-inputs gdb-8.3)))
+   (properties (alist-delete 'hidden? (package-properties gdb-8.3)))))
 
 (define-public gdb
   ;; This is the fixed version that packages depend on.  Update it rarely
