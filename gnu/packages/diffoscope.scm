3d8fe3e2768607bd55270d8d09b4f1bea63809ce
Vagrant Cascadian <vagrant@reproducible-builds.org>
2020-02-01 22:21:38 +0000

    gnu: diffoscope: Update to 136.
    * gnu/packages/diffoscope (diffoscope): Update to 136.

--- gnu/packages/diffoscope.scm
+++ gnu/packages/diffoscope.scm
@@ -67,7 +67,7 @@
   #:use-module (ice-9 match))
 
 (define-public diffoscope
-  (let ((version "135"))
+  (let ((version "136"))
     (package
       (name "diffoscope")
       (version version)
@@ -79,7 +79,7 @@
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "0rkpvajkp3qryi6dxkrh8aq5xg79aybnw8iy73wsblcnfq6yhba7"))))
+                  "1wp4fnmwcsgv17dmvk9xr3h63gp4nmmpysr248qvxs8s5qy5xlyk"))))
       (build-system python-build-system)
       (arguments
        `(#:phases (modify-phases %standard-phases
