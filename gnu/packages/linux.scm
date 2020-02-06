d94e5dffcbd83cda9e44e294514ea088fa5a91b6
Mark H Weaver <mhw@netris.org>
2020-02-05 20:20:55 -0500

    gnu: linux-libre: Update to 5.4.18.
    * gnu/packages/linux.scm (linux-libre-5.4-version): Update to 5.4.18.
    (linux-libre-5.4-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -359,10 +359,10 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                         "linux-" version ".tar.xz"))
     (sha256 hash)))
 
-(define-public linux-libre-5.4-version "5.4.17")
+(define-public linux-libre-5.4-version "5.4.18")
 (define-public linux-libre-5.4-pristine-source
   (let ((version linux-libre-5.4-version)
-        (hash (base32 "1fbl5knf6pini9lsx8mqkdmf3qbsydqvaxggh6nd1vk9mzv2npwl")))
+        (hash (base32 "12ad4fnxag16ar2afiljv4nnv15i4f493sz6m7i9qgjld7yz3scj")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.4)))
