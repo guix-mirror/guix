fd1fde6041b7f221bed114593944365d21cb925c
Ludovic Courtès <ludo@gnu.org>
2020-02-07 23:26:38 +0100

    gnu: rust: Switch to 'C_INCLUDE_PATH'.
    * gnu/packages/rust.scm (rust-1.19)[native-search-paths]: Remove "CPATH"
    and add "C_INCLUDE_PATH" and "CPLUS_INCLUDE_PATH" instead.

--- gnu/packages/rust.scm
+++ gnu/packages/rust.scm
@@ -413,8 +413,11 @@ test = { path = \"../libtest\" }
     ;; modules (see <https://bugs.gnu.org/31392>).
     (native-search-paths
      (list (search-path-specification
-            (variable "CPATH")
+            (variable "C_INCLUDE_PATH")
             (files '("include")))
+           (search-path-specification
+            (variable "CPLUS_INCLUDE_PATH")
+            (files '("include/c++" "include")))
            (search-path-specification
             (variable "LIBRARY_PATH")
             (files '("lib" "lib64")))))
