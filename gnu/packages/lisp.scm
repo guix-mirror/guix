2035134bff2f03948b2d17ce454cc7c7787b3c79
Guillaume Le Vaillant <glv@posteo.net>
2020-02-10 11:19:03 +0100

    gnu: txr: Update to 232.
    * gnu/packages/lisp.scm (txr): Update to 232.

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -797,7 +797,7 @@ enough to play the original mainframe Zork all the way through.")
 (define-public txr
   (package
     (name "txr")
-    (version "230")
+    (version "232")
     (source
      (origin
        (method git-fetch)
@@ -808,7 +808,7 @@ enough to play the original mainframe Zork all the way through.")
        (patches (search-patches "txr-shell.patch"))
        (sha256
         (base32
-         "1ma6nbqsnl4f8ndh47zzc8n5vzcny66v0z3ndddgm3g0bqaxzjzm"))))
+         "1vcnask4a1rxrljp4rcbkfihrndkv25l256l125vv39gjhf315xp"))))
     (build-system gnu-build-system)
     (arguments
      '(#:configure-flags '("cc=gcc")
