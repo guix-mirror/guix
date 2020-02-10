65eada58a07562809ba2daf6310244bf43133871
Ludovic Courtès <ludo@gnu.org>
2020-02-10 17:47:07 +0100

    gnu: pybind11: Change 'license' field to BSD-3.
    * gnu/packages/python-xyz.scm (pybind11)[license]: Change to BSD-3.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -15419,7 +15419,7 @@ existing C++ code.  Its goals and syntax are similar to the excellent
 Boost.Python library by David Abrahams: to minimize boilerplate code in
 traditional extension modules by inferring type information using compile-time
 introspection.")
-    (license license:expat)))
+    (license license:bsd-3)))
 
 (define-public python-fasteners
   (package
