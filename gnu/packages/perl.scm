871a2c2f676efa950f32f68a205b12503c829f4e
Marius Bakke <mbakke@fastmail.com>
2020-02-12 17:02:54 +0100

    gnu: perl-term-readkey: Update to 2.38.
    * gnu/packages/perl.scm (perl-term-readkey): Update to 2.38.

--- gnu/packages/perl.scm
+++ gnu/packages/perl.scm
@@ -8495,7 +8495,7 @@ done, how much is left to do, and estimate how long it will take.")
 (define-public perl-term-readkey
   (package
     (name "perl-term-readkey")
-    (version "2.37")
+    (version "2.38")
     (source
      (origin
        (method url-fetch)
@@ -8503,7 +8503,7 @@ done, how much is left to do, and estimate how long it will take.")
                            "TermReadKey-" version ".tar.gz"))
        (sha256
         (base32
-         "0hdj5mldpj3pyprd4hbbalfx9yjgi5p59gg2ixk9808f5v7q74sa"))))
+         "143jlibah1g14bym7sj3gphvqkpj1w4vn7sqc4vc62jpviw5hr2s"))))
     (build-system perl-build-system)
     (home-page "https://metacpan.org/release/TermReadKey")
     (synopsis "Simple terminal control")
