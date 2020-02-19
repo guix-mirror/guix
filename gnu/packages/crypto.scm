bfa3caa2fb1436c4d0e24036a9ea60ccc19aabae
Tobias Geerinckx-Rice <me@tobias.gr>
2020-02-19 18:46:04 +0100

    gnu: crypto++: Build in parallel.
    * gnu/packages/crypto.scm (crypto++)[arguments]: Make ‘build-shared’
    the main ‘build’ phase.  Respect MAKE-FLAGS and PARALLEL-JOB-COUNT.

--- gnu/packages/crypto.scm
+++ gnu/packages/crypto.scm
@@ -627,10 +627,12 @@ data on your platform, so the seed itself will be as random as possible.
                ((" -march=native") ""))
              #t))
          (delete 'configure)
-         (add-after 'build 'build-shared
-           (lambda _
-             ;; By default, only the static library is built.
-             (invoke "make" "shared")))
+         (replace 'build
+           ;; By default, only the static library is built.
+           (lambda* (#:key (make-flags '()) #:allow-other-keys)
+             (apply invoke "make" "shared"
+                    "-j" (number->string (parallel-job-count))
+                    make-flags)))
          (add-after 'install 'install-shared-library-links
            ;; By default, only .so and .so.x.y.z are installed.
            ;; Create all the ‘intermediates’ expected by dependent packages.
