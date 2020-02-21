3d314a5642a57aa70248c7ee61789d4d53aedae6
Vincent Legoll <vincent.legoll@gmail.com>
2020-02-21 23:39:48 +0100

    gnu: debootstrap: Update to 1.0.118
    The attached patch has been tested by creating
    arm64, mipsel & amd64 chroots and the amd64
    one has been actually run.
    
    --
    Vincent Legoll
    
    From a23b962c21e20c72f425675e6ea6e11cd0f944d0 Mon Sep 17 00:00:00 2001
    From: Vincent Legoll <vincent.legoll@gmail.com>
    Date: Fri, 21 Feb 2020 23:34:40 +0100
    Subject: [PATCH] gnu: debootstrap: Update to 1.0.118. *
     gnu/packages/debian.scm (debootstrap): Update to 1.0.118.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/debian.scm
+++ gnu/packages/debian.scm
@@ -117,7 +117,7 @@ contains the archive keys used for that.")
 (define-public debootstrap
   (package
     (name "debootstrap")
-    (version "1.0.114")
+    (version "1.0.118")
     (source
       (origin
         (method git-fetch)
@@ -127,7 +127,7 @@ contains the archive keys used for that.")
         (file-name (git-file-name name version))
         (sha256
          (base32
-          "147308flz9y8g6f972izi3szmsywf5f8xm64z2smy1cayd340i63"))))
+          "07x1x67dli6qwrlaribi07wgjp7fs1gzwkqgq1xy6lq1xz1xfdl2"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
