1f8838d1f6295e10851b2915dc3b0a97b3bbc236
Marius Bakke <mbakke@fastmail.com>
2020-03-02 23:08:13 +0100

    gnu: libgc: Fix replacement inheritance.
    * gnu/packages/bdw-gc.scm (libgc-8.0): Do not use PACKAGE/INHERIT, because it
    does not make sense to inherit any replacements of LIBGC.
    (libgc/back-pointers): Use PACKAGE/INHERIT.

--- gnu/packages/bdw-gc.scm
+++ gnu/packages/bdw-gc.scm
@@ -92,8 +92,8 @@ C or C++ programs, though that is not its primary goal.")
    (license (x11-style (string-append home-page "license.txt")))))
 
 (define-public libgc-8.0
-  (package/inherit
-   libgc
+  (package
+   (inherit libgc)
    (version "8.0.4")
    (source (origin
              (method url-fetch)
@@ -104,8 +104,8 @@ C or C++ programs, though that is not its primary goal.")
                "1798rp3mcfkgs38ynkbg2p47bq59pisrc6mn0l20pb5iczf0ssj3"))))))
 
 (define-public libgc/back-pointers
-  (package
-    (inherit libgc)
+  (package/inherit
+    libgc
     (name "libgc-back-pointers")
     (arguments
      `(#:make-flags
