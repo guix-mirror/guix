5d01cc3673c4073f1e4c6b7d7b8bb943321144ac
Mark H Weaver <mhw@netris.org>
2020-03-06 14:19:34 -0500

    gnu: linux-libre@4.19: Update to 4.19.108.
    * gnu/packages/linux.scm (linux-libre-4.19-version): Update to 4.19.108.
    (linux-libre-4.19-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -370,10 +370,10 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.4)))
 
-(define-public linux-libre-4.19-version "4.19.107")
+(define-public linux-libre-4.19-version "4.19.108")
 (define-public linux-libre-4.19-pristine-source
   (let ((version linux-libre-4.19-version)
-        (hash (base32 "0h02pxzzwc5w2kfqw686bpxc13a93yq449lyzxxkxq1qilcsqjv5")))
+        (hash (base32 "18shyy1z2s8r26qb4rcz7gwl43dnmycjjywp9gss5zlfn2jyrbh9")))
     (make-linux-libre-source version
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.19)))
