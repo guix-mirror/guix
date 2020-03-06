a009a3eead5bb627c143dfb97280bec5131a28ce
Marius Bakke <mbakke@fastmail.com>
2020-03-06 14:37:39 +0100

    gnu: ungoogled-chromium: Increase max-silent-time.
    * gnu/packages/chromium.scm (ungoogled-chromium)[properties]: Add
    'max-silent-time'.

--- gnu/packages/chromium.scm
+++ gnu/packages/chromium.scm
@@ -839,7 +839,9 @@ from forcing GEXP-PROMISE."
 
     ;; Building Chromium takes ... a very long time.  On a single core, a busy
     ;; mid-end x86 system may need more than 24 hours to complete the build.
-    (properties '((timeout . 144000)))  ;40 hours
+    (properties '((timeout . 144000)    ;40 hours
+                  ;; The linking step may take more than an hour on some hardware.
+                  (max-silent-time . 7200)))
 
     (home-page "https://github.com/Eloston/ungoogled-chromium")
     (description
