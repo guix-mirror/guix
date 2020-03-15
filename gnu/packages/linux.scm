f24aaa81de8c709adfda2e89271c562a5ca8d959
Vincent Legoll <vincent.legoll@gmail.com>
2020-03-15 19:07:57 +0100

    gnu: BlueZ: Update to 5.54.
    * gnu/packages/linux.scm (bluez): Update to 5.54.
    [replacement]: Remove field.
    (bluez/fixed): Remove variable.
    * gnu/packages/patches/bluez-CVE-2020-0556.patch: Remove file.
    * gnu/local.mk (dist_patch_DATA): Remove it.
    
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -4039,8 +4039,7 @@ Bluetooth audio output devices like headphones or loudspeakers.")
 (define-public bluez
   (package
     (name "bluez")
-    (replacement bluez/fixed)
-    (version "5.53")
+    (version "5.54")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -4048,7 +4047,7 @@ Bluetooth audio output devices like headphones or loudspeakers.")
                     version ".tar.xz"))
               (sha256
                (base32
-                "1g1qg6dz6hl3csrmz75ixr12lwv836hq3ckb259svvrg62l2vaiq"))))
+                "1p2ncvjz6alr9n3l5wvq2arqgc7xjs6dqyar1l9jp0z8cfgapkb8"))))
     (build-system gnu-build-system)
     (arguments
      `(#:configure-flags
@@ -4104,14 +4103,6 @@ Bluetooth audio output devices like headphones or loudspeakers.")
      "BlueZ provides support for the core Bluetooth layers and protocols.  It
 is flexible, efficient and uses a modular implementation.")
     (license license:gpl2+)))
-
-(define bluez/fixed
-  (package
-    (inherit bluez)
-    (source (origin
-              (inherit (package-source bluez))
-              (patches (append (origin-patches (package-source bluez))
-                               (search-patches "bluez-CVE-2020-0556.patch")))))))
 
 (define-public fuse-exfat
   (package
