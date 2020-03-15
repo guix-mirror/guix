ae863ccd63fedb734138d4bbc28fb51de26ac290
Efraim Flashner <efraim@flashner.co.il>
2020-03-15 23:12:19 +0200

    gnu: Add go-github-com-golang-freetype.
    * gnu/packages/golang.scm (go-github-com-golang-freetype): New variable.

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -3497,3 +3497,30 @@ based on murmurhash.")
       (description "Unicode transliterator in Golang - Replaces non-ASCII
 characters with their ASCII approximations.")
       (license license:asl2.0))))
+
+(define-public go-github-com-golang-freetype
+  (let ((commit "e2365dfdc4a05e4b8299a783240d4a7d5a65d4e4")
+        (revision "1"))
+    (package
+      (name "go-github-com-golang-freetype")
+      (version (git-version "0.0.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://github.com/golang/freetype")
+                       (commit commit)))
+                (file-name (string-append "go-github-com-golang-freetype-"
+                                          version "-checkout"))
+                (sha256
+                 (base32
+                  "194w3djc6fv1rgcjqds085b9fq074panc5vw582bcb8dbfzsrqxc"))))
+      (build-system go-build-system)
+      (arguments
+       `(#:import-path "github.com/golang/freetype"))
+      (propagated-inputs
+       `(("go-golang-org-x-image" ,go-golang-org-x-image)))
+      (home-page "https://github.com/golang/freetype")
+      (synopsis "Freetype font rasterizer in the Go programming language")
+      (description "The Freetype font rasterizer in the Go programming language.")
+      (license (list license:freetype
+                     license:gpl2+)))))
