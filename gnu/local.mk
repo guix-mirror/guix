e76cbdcd90e603d98f9d074aa2ee0788c65d97df
Marius Bakke <mbakke@fastmail.com>
2020-03-16 04:39:17 +0100

    gnu: beets: Fix build with newer Werkzeug.
    * gnu/packages/patches/beets-werkzeug-compat.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Adjust accordingly.
    * gnu/packages/music.scm (beets)[source](patches): New field.

--- gnu/local.mk
+++ gnu/local.mk
@@ -756,6 +756,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/bash-completion-directories.patch	\
   %D%/packages/patches/bastet-change-source-of-unordered_set.patch	\
   %D%/packages/patches/bazaar-CVE-2017-14176.patch		\
+  %D%/packages/patches/beets-werkzeug-compat.patch		\
   %D%/packages/patches/beignet-correct-file-names.patch		\
   %D%/packages/patches/benchmark-unbundle-googletest.patch	\
   %D%/packages/patches/biber-fix-encoding-write.patch		\
