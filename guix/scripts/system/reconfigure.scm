5517750344be05c91bc2979c1a0e2348a9ae902d
Ludovic Courtès <ludo@gnu.org>
2020-04-02 22:46:18 +0200

    reconfigure: Run the effect scripts as separate processes.
    Fixes <https://bugs.gnu.org/39301>.
    Reported by strypsteen@posteo.net.
    
    * guix/scripts/system/reconfigure.scm (switch-to-system)
    (upgrade-shepherd-services, install-bootloader): Use 'system*' instead
    of 'primitive-load'.

--- guix/scripts/system/reconfigure.scm
+++ guix/scripts/system/reconfigure.scm
@@ -100,7 +100,7 @@ atomically, and run OS's activation script."
   "Using EVAL, a monadic procedure taking a single G-Expression as an argument,
 create a new generation of PROFILE pointing to the directory of OS, switch to
 it atomically, and run OS's activation script."
-  (eval #~(primitive-load #$(switch-system-program os profile))))
+  (eval #~(system* #$(switch-system-program os profile))))
 
 
 ;;;
@@ -176,10 +176,10 @@ services as defined by OS."
                                         (map live-service-canonical-name
                                              live-services)))
              (service-files (map shepherd-service-file target-services)))
-        (eval #~(primitive-load #$(upgrade-services-program service-files
-                                                            to-start
-                                                            to-unload
-                                                            to-restart)))))))
+        (eval #~(system* #$(upgrade-services-program service-files
+                                                     to-start
+                                                     to-unload
+                                                     to-restart)))))))
 
 
 ;;;
@@ -252,9 +252,9 @@ additional configurations specified by MENU-ENTRIES can be selected."
          (package (bootloader-package bootloader))
          (device (bootloader-configuration-target configuration))
          (bootcfg-file (bootloader-configuration-file bootloader)))
-    (eval #~(primitive-load #$(install-bootloader-program installer
-                                                          package
-                                                          bootcfg
-                                                          bootcfg-file
-                                                          device
-                                                          target)))))
+    (eval #~(system* #$(install-bootloader-program installer
+                                                   package
+                                                   bootcfg
+                                                   bootcfg-file
+                                                   device
+                                                   target)))))
