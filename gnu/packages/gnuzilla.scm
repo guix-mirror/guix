d24e598a7840a6ab70424e27fd858d9bc0ae12b1
Mark H Weaver <mhw@netris.org>
2020-04-03 16:56:20 -0400

    gnu: icecat: Update to 68.6.1-guix0-preview1 [security fixes].
    Includes fixes for CVE-2020-6819 and CVE-2020-6820.
    
    * gnu/packages/gnuzilla.scm (%icecat-version, %icecat-build-id): Update.
    (icecat-source): Update gnuzilla commit, base version, and hashes.
    * gnu/packages/patches/icecat-makeicecat.patch: Adapt to new version.

--- gnu/packages/gnuzilla.scm
+++ gnu/packages/gnuzilla.scm
@@ -554,8 +554,8 @@ from forcing GEXP-PROMISE."
                       #:system system
                       #:guile-for-build guile)))
 
-(define %icecat-version "68.6.0-guix0-preview1")
-(define %icecat-build-id "20200309000000") ;must be of the form YYYYMMDDhhmmss
+(define %icecat-version "68.6.1-guix0-preview1")
+(define %icecat-build-id "20200403000000") ;must be of the form YYYYMMDDhhmmss
 
 ;; 'icecat-source' is a "computed" origin that generates an IceCat tarball
 ;; from the corresponding upstream Firefox ESR tarball, using the 'makeicecat'
@@ -577,11 +577,11 @@ from forcing GEXP-PROMISE."
                   "firefox-" upstream-firefox-version ".source.tar.xz"))
             (sha256
              (base32
-              "17qwfq9hwra8jarawy8k2sqfa6hdhwa9qk84ndr6gjvmxcy22a14"))))
+              "1y69rrm73nb77p2yydny7hs7zwsbfdhyz8xg5y6xihvsakwsxn59"))))
 
-         (upstream-icecat-base-version "68.6.0") ; maybe older than base-version
+         (upstream-icecat-base-version "68.6.1") ; maybe older than base-version
          ;;(gnuzilla-commit (string-append "v" upstream-icecat-base-version))
-         (gnuzilla-commit "9dcb24d885eae5973eb2245b532b158c685d707a")
+         (gnuzilla-commit "f27cf24f9b5f85c1effcbac46d75e8fb83728df9")
          (gnuzilla-source
           (origin
             (method git-fetch)
@@ -593,7 +593,7 @@ from forcing GEXP-PROMISE."
                                       (string-take gnuzilla-commit 8)))
             (sha256
              (base32
-              "1y3jmh055vmx44gsjgwxvwv3zcyvz8pc5mhgrwkzm0ybbwpp2pqi"))))
+              "1xrqk3iik9if6qcdfvschyya1vhiank6gnpkwix8m2shsdimaaq2"))))
 
          (makeicecat-patch
           (local-file (search-patch "icecat-makeicecat.patch"))))
