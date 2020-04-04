95b800e67ca9b6ca6be4f7afd74ddd70286ac852
R Veera Kumar <vkor@vkten.in>
2020-04-04 23:03:25 +0530

    gnu: Add mtpaint.
    * gnu/packages/image.scm (mtpaint): New variable.
    
    Signed-off-by: Danny Milosavljevic <dannym@scratchpost.org>

--- gnu/packages/image.scm
+++ gnu/packages/image.scm
@@ -24,6 +24,7 @@
 ;;; Copyright © 2018 Alex Vong <alexvong1995@gmail.com>
 ;;; Copyright © 2018 Rutger Helling <rhelling@mykolab.com>
 ;;; Copyright © 2020 Giacomo Leidi <goodoldpaul@autistici.org>
+;;; Copyright © 2020 R Veera Kumar <vkor@vkten.in>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -45,6 +46,7 @@
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages assembly)
   #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages boost)
   #:use-module (gnu packages check)
   #:use-module (gnu packages curl)
@@ -1979,3 +1981,56 @@ AOM, including with alpha.")
     (home-page "https://github.com/AOMediaCodec/libavif")
     (license (list license:bsd-2    ; libavif itself
                    license:expat)))) ; cJSON in the test suite
+
+(define-public mtpaint
+  (let ((commit "03b1b0938067b88d86d9f1b1088730f1934d411e")
+        (revision "1"))
+    (package
+      (name "mtpaint")
+      ;; The author neither releases tarballs nor uses git version tags.
+      ;; Instead, author puts version in git commit title.
+      (version (git-version "3.49.25" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/wjaguar/mtPaint/")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0izm2wvj26566fd8mqvypr7bmv7jnq8qhp4760m7z2wrc4y8pjn1"))))
+      (build-system gnu-build-system)
+      (native-inputs
+       `(("gettext" ,gettext-minimal)
+         ("pkg-config" ,pkg-config)
+         ("which" ,which)))
+      (inputs
+       `(("imlib2" ,imlib2)
+         ("libtiff" ,libtiff)
+         ("libpng" ,libpng)
+         ("libungif", libungif)
+         ("libjpeg", libjpeg)
+         ("libwebp" ,libwebp)
+         ("openjpeg" ,openjpeg)
+         ("lcms" ,lcms)
+         ("zlib", zlib)
+         ("glib" ,glib)
+         ;; support for gtk3 is in testing stage
+         ("gtk+" ,gtk+-2)))
+      (arguments
+       `(#:configure-flags
+         (list
+          ;; internationalized version
+          "intl"
+          ;; install man page
+          "man")
+         ;; no check target
+         #:tests? #f))
+      (home-page "http://mtpaint.sourceforge.net/")
+      (synopsis "Create pixel art and manipulate digital images")
+      (description
+       "Mtpaint is a graphic editing program which uses the GTK+ toolkit.
+It can create and edit indexed palette or 24bit RGB images, offers basic
+painting and palette manipulation tools.  It also handles JPEG, JPEG2000,
+GIF, TIFF, WEBP, BMP, PNG, XPM formats.")
+      (license license:gpl3+))))
