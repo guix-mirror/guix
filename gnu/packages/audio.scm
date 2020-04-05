guix-mirror/guix

commit 74c7f367daa554cedeb5f02a00d1cd02acf1584b

gsm: Add the -fPIC compiler option.
This is necessary for building mediastream2 (a dependency of Linphone).

* gnu/packages/audio.scm (gsm): Add the default CCFLAGS as a make flag, and
prepend -fPIC to these.  Pass INSTALL_ROOT as a make flag as well, which
removes the need to use substitute* in a phase.

gnu/packages/audio.scm
@@ -11,7 +11,7 @@
 ;;; Copyright © 2016, 2017, 2018, 2019, 2020 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2018, 2020 Oleg Pykhalov <go.wigust@gmail.com>
 ;;; Copyright © 2018 okapi <okapi@firemail.cc>
-;;; Copyright © 2018 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2018, 2020 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;; Copyright © 2018 Clément Lassieur <clement@lassieur.org>
 ;;; Copyright © 2018 Brett Gilio <brettg@gnu.org>
 ;;; Copyright © 2018, 2019 Marius Bakke <mbakke@fastmail.com>
@@ -3356,14 +3356,14 @@ code, used in @code{libtoxcore}.")
     (build-system gnu-build-system)
     (arguments
      `(#:test-target "tst"
+       #:make-flags '("INSTALL_ROOT=%output"
+                      "CCFLAGS=-fPIC \
+-c -O2 -DNeedFunctionPrototypes=1 -Wall -Wno-comment") ;default options
        #:phases
        (modify-phases %standard-phases
          (add-before 'install 'pre-install
            (lambda _
              (let ((out (assoc-ref %outputs "out")))
-               (substitute* "Makefile"
-                 (("INSTALL_ROOT\t=")
-                  (string-append "INSTALL_ROOT\t=\t" out)))
                (mkdir-p (string-append out "/inc"))
                (mkdir-p (string-append out "/man"))
                (mkdir-p (string-append out "/man/man1"))

commit 5b05f8e9654ea722270c45c0fd0eead369bc0daf

gnu: gsm: Fix installation directory.
This is a follow-up commit to 74c7f367daa, which broke the installation of the
bin, lib and share artifacts.

* gnu/packages/audio.scm (gsm)[make-flags]: Programmatically build the
INSTALL_ROOT make flag so that the %output variable gets properly expanded.
Instead of repeating the default CCFLAGS values as a make flag...
[phases]: ...add "-fPIC" using substitutes* in a new add-fpic-ccflag phase.

gnu/packages/audio.scm
@@ -3356,11 +3356,17 @@ code, used in @code{libtoxcore}.")
     (build-system gnu-build-system)
     (arguments
      `(#:test-target "tst"
-       #:make-flags '("INSTALL_ROOT=%output"
-                      "CCFLAGS=-fPIC \
--c -O2 -DNeedFunctionPrototypes=1 -Wall -Wno-comment") ;default options
+       #:make-flags (list (string-append "INSTALL_ROOT=" %output))
        #:phases
        (modify-phases %standard-phases
+         (add-after 'unpack 'add-fpic-ccflag
+           (lambda _
+             ;; The -fPIC compiler option is needed when building
+             ;; mediastreamer.
+             (substitute* "Makefile"
+               (("^CCFLAGS.*" all)
+                (string-append all "CCFLAGS += -fPIC")))
+             #t))
          (add-before 'install 'pre-install
            (lambda _
              (let ((out (assoc-ref %outputs "out")))