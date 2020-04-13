13d25c546010ded6a275f167497386e6e253b54e
Marius Bakke <mbakke@fastmail.com>
2020-04-13 19:45:02 +0200

    gnu: python2-backports-shutil-get-terminal-size: Fix test failure.
    * gnu/packages/python-xyz.scm (python2-backports-shutil-get-terminal-size)[arguments]:
    In the check phase, set PYTHONPATH before running the tests.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -7196,6 +7196,8 @@ Python 2 and Python 3.")
        (modify-phases %standard-phases
          (replace 'check
            (lambda _
+             (setenv "PYTHONPATH" (string-append "./build/lib:"
+                                                 (getenv "PYTHONPATH")))
              (invoke "py.test" "-v"))))))
     (native-inputs
      `(("python2-pytest" ,python2-pytest)))
