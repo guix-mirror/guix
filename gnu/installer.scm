guix-mirror/guix

commit b5c2d93d7a223155898dd0ed6932f6acf78ac454

installer: Hide shepherd messages.
* gnu/installer.scm (apply-locale): Set "shepherd-message-port" instead of
redirecting stderr to make sure that nothing is printed on console.

gnu/installer.scm
@@ -113,8 +113,10 @@ version of this file."
          (setlocale LC_ALL locale))
 
         ;; Restart the documentation viewer so it displays the manual in
-        ;; language that corresponds to LOCALE.
-        (with-error-to-port (%make-void-port "w")
+        ;; language that corresponds to LOCALE.  Make sure that nothing is
+        ;; printed on the console.
+        (parameterize ((shepherd-message-port
+                        (%make-void-port "w")))
           (lambda ()
             (stop-service 'term-tty2)
             (start-service 'term-tty2 (list locale)))))))

commit 893651af3ec8ea003404cfefbbaa4fcd56dad3d5

installer: Open manual in the selected language on tty2.
Fixes <https://bugs.gnu.org/40624>.
Reported by Florian Pelz <pelzflorian@pelzflorian.de>.

Regression introduced in b5c2d93d7a223155898dd0ed6932f6acf78ac454.

* gnu/installer.scm (apply-locale): Remove 'lambda' around
'stop-service' and 'start-service' calls.

gnu/installer.scm
@@ -118,9 +118,8 @@ version of this file."
         ;; printed on the console.
         (parameterize ((shepherd-message-port
                         (%make-void-port "w")))
-          (lambda ()
-            (stop-service 'term-tty2)
-            (start-service 'term-tty2 (list locale)))))))
+          (stop-service 'term-tty2)
+          (start-service 'term-tty2 (list locale))))))
 
 (define* (compute-locale-step #:key
                               locales-name