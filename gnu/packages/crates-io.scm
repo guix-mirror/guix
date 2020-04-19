6b4db2a21e8613bca171b9be0e9b0971c446a569
Hartmut Goebel <h.goebel@crazy-compilers.com>
2020-04-19 21:39:33 +0200

    gnu: Add rust-bytes-0.5.
    * gnu/packages/crates-io.scm (rust-bytes-0.5): New variable.
      (rust-bytes-0.4): Inherit from it.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -2115,8 +2115,32 @@ little-endian.")
        (("rust-quickcheck" ,rust-quickcheck-0.2)
         ("rust-rand" ,rust-rand-0.3))))))
 
-(define-public rust-bytes-0.4
+(define-public rust-bytes-0.5
   (package
+    (name "rust-bytes")
+    (version "0.5.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "bytes" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1q9r7si1l8vndg4n2ny2nv833ghp5vyqzk5indb9rmhd5ibaq2hk"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t ;; FIXME requires Raus >= 1.39
+       #:cargo-inputs
+       (("rust-serde" ,rust-serde-1.0))
+       #:cargo-development-inputs
+       (("rust-loom" ,rust-loom-0.2)
+        ("rust-serde-test" ,rust-serde-test-1.0))))
+    (home-page "https://github.com/tokio-rs/bytes")
+    (synopsis "Types and traits for working with bytes")
+    (description "Types and traits for working with bytes.")
+    (license license:expat)))
+
+(define-public rust-bytes-0.4
+  (package/inherit rust-bytes-0.5
     (name "rust-bytes")
     (version "0.4.12")
     (source
@@ -2137,13 +2161,7 @@ little-endian.")
         ("rust-iovec" ,rust-iovec-0.1)
         ("rust-serde" ,rust-serde-1.0))
        #:cargo-development-inputs
-       (("rust-serde-test" ,rust-serde-test-1.0))))
-    (home-page "https://github.com/tokio-rs/bytes")
-    (synopsis
-     "Types and traits for working with bytes")
-    (description
-     "Types and traits for working with bytes.")
-    (license license:expat)))
+       (("rust-serde-test" ,rust-serde-test-1.0))))))
 
 (define-public rust-bytes-0.3
   (package
