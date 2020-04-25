0e72d49ef7cb7463012704b3d107fe38b085a562
Marius Bakke <mbakke@fastmail.com>
2020-04-25 22:17:07 +0200

    tests: Add 'guile-final' to the installation test GC roots.
    * gnu/tests/install.scm (run-install): Add GUILE-FINAL to
    OPERATING-SYSTEM-WITH-GC-ROOTS.

--- gnu/tests/install.scm
+++ gnu/tests/install.scm
@@ -29,6 +29,7 @@
   #:use-module ((gnu build vm) #:select (qemu-command))
   #:use-module (gnu packages admin)
   #:use-module (gnu packages bootloaders)
+  #:use-module (gnu packages commencement)       ;for 'guile-final'
   #:use-module (gnu packages cryptsetup)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages ocr)
@@ -226,10 +227,11 @@ packages defined in installation-os."
                        ;; Since the installation system has no network access,
                        ;; we cheat a little bit by adding TARGET to its GC
                        ;; roots.  This way, we know 'guix system init' will
-                       ;; succeed.
+                       ;; succeed.  Also add guile-final, which is pulled in
+                       ;; through provenance.drv and may not always be present.
                        (image  (system-disk-image
                                 (operating-system-with-gc-roots
-                                 os (list target))
+                                 os (list target guile-final))
                                 #:disk-image-size install-size
                                 #:file-system-type
                                 installation-disk-image-file-system-type
