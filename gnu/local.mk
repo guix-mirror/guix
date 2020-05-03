06ed1dba359aeb70f6da908ca5672c541c714ab1
Vincent Legoll <vincent.legoll@gmail.com>
2020-05-04 00:39:36 +0200

    gnu: Add gromacs.
    * gnu/packages/chemistry.scm (gromacs): New variable.
    * gnu/packages/patches/gromacs-tinyxml2.patch: New file...
    * gnu/local.mk (dist_patch_DATA): ...add it here.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1006,6 +1006,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/gpsbabel-qstring.patch     		\
   %D%/packages/patches/grantlee-merge-theme-dirs.patch		\
   %D%/packages/patches/grep-timing-sensitive-test.patch		\
+  %D%/packages/patches/gromacs-tinyxml2.patch			\
   %D%/packages/patches/groovy-add-exceptionutilsgenerator.patch	\
   %D%/packages/patches/grub-efi-fat-serial-number.patch		\
   %D%/packages/patches/gsl-test-i686.patch			\
