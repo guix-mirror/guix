0eed77127592323d89f56c215a15374a1aaae110
Ludovic Courtès <ludo@gnu.org>
2020-05-03 17:25:28 +0200

    locale: Add glibc 2.29 to '%default-locale-libcs'.
    This eases transition for anyone reconfiguring and still having profiles
    with packages using the former libc.
    
    * gnu/system/locale.scm (%default-locale-libcs): Add GLIBC-2.29.

--- gnu/system/locale.scm
+++ gnu/system/locale.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014, 2015, 2016, 2017, 2019 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014, 2015, 2016, 2017, 2019, 2020 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2018 Mark H Weaver <mhw@netris.org>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -147,7 +147,8 @@ data format changes between libc versions."
 
 (define %default-locale-libcs
   ;; The libcs for which we build locales by default.
-  (list glibc))
+  ;; List the previous and current libc to ease transition.
+  (list glibc-2.29 glibc))
 
 (define %default-locale-definitions
   ;; Arbitrary set of locales that are built by default.  They are here mostly
