969811ad0e51adcbf503e38d9c9265814e3c2e27
Ryan Prior <rprior@protonmail.com>
2020-05-15 14:57:34 -0500

    gnu: Add pantheon-calculator.
    * gnu/packages/pantheon.scm (pantheon-calculator): New variable.
    
    Signed-off-by: Danny Milosavljevic <dannym@scratchpost.org>

--- gnu/packages/pantheon.scm
+++ gnu/packages/pantheon.scm
@@ -17,6 +17,7 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages pantheon)
+  #:use-module (gnu packages cmake)
   #:use-module (gnu packages gettext)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnome)
@@ -65,3 +66,44 @@
 things, it provides complex widgets and convenience functions designed for use
 in apps built for the Pantheon desktop.")
     (license license:lgpl3+)))
+
+(define-public pantheon-calculator
+  (package
+    (name "pantheon-calculator")
+    (version "1.5.5")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/elementary/calculator.git")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1csxsr2c8qvl97xz9ahwn91z095nzgr0i1mbcb1spljll2sr9lkj"))))
+    (build-system meson-build-system)
+    (arguments
+     `(#:glib-or-gtk? #t
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'disable-schema-cache-generation
+           (lambda _
+             (setenv "DESTDIR" "/")
+             #t)))))
+    (inputs
+     `(("granite" ,granite)
+       ("glib" ,glib)
+       ("gtk" ,gtk+)
+       ("libgee" ,libgee)))
+    (native-inputs
+     `(("cmake" ,cmake)
+       ("glib:bin" ,glib "bin") ; for glib-compile-schemas
+       ("gettext" ,gettext-minimal)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "https://github.com/elementary/calculator")
+    (synopsis "Desktop calculator")
+    (description "Calculator is an application for performing simple
+arithmetic.  It is the default calculator application in the Pantheon
+desktop.")
+    (license license:gpl3)))
