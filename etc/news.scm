489699c45676978fed745b9f1de723e73f7535ed
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2020-05-20 08:25:31 -0400

    news: Add entry for Btrfs subvolume boot support.
    * etc/news.scm: Add entry.

--- etc/news.scm
+++ etc/news.scm
@@ -11,6 +11,21 @@
 (channel-news
  (version 0)
 
+ (entry (commit "b460ba7992a0b4af2ddb5927dcf062784539ef7b")
+        (title (en "Add support to boot from a Btrfs subvolume")
+               (fr "Ajout du support pour démarrer depuis un sous-volume Btrfs"))
+        (body
+         (en "The generation of the GRUB configuration file produced from an
+operating system declaration now takes into account the use of a Btrfs
+subvolume for the partition holding @file{/gnu/store}.  Run the command
+@command{info \"(guix) Btrfs file system\"} for more information and
+examples."
+         (fr "La génération du fichier de configuration de GRUB produite à
+partir de la déclaration d'un @code{operating-system} tient maintenant compte
+de l'utilisation d'un sous-volume Btrfs pour la partition contenant
+@file{/gnu/store}.  Exécutez la commande @command{info\"(guix) Btrfs file
+system\"} pour des exemples et plus d'information.")))
+
  (entry (commit "6456232164890dbf5aa20394ee24637feb4b7b9e")
         (title (en "@command{guix pack -RR} introduces a new execution
 engine")
