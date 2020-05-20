fa876b2cd08ba46e494c6df5abd3caa2ce96dc3e
Tobias Geerinckx-Rice <me@tobias.gr>
2020-05-20 19:25:29 +0200

    gnu: gnu-efi: Update to 3.0.12.
    * gnu/packages/efi.scm (gnu-efi): Update to 3.0.12.

--- gnu/packages/efi.scm
+++ gnu/packages/efi.scm
@@ -37,14 +37,14 @@
 (define-public gnu-efi
   (package
     (name "gnu-efi")
-    (version "3.0.11")
+    (version "3.0.12")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://sourceforge/gnu-efi/"
                            "gnu-efi-" version ".tar.bz2"))
        (sha256
-        (base32 "1ffnc4xbzfggs37ymrgfx76j56kk2644c081ivhr2bjkla9ag3gj"))))
+        (base32 "0sbn6am3k5lqafycggh1g964fcwjwnh0i9hhwrk4ncrwzphz55h1"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f                      ; none exist
