21d3b765d9f22cdc2d027531411af27216592ef4
Christopher Baines <mail@cbaines.net>
2020-05-23 22:54:46 +0100

    gnu: local.mk: Remove absent patch.
    This follows on from 899ffa1381afbf5f955aeba4839e920b3d910953.
    
    * gnu/local.mk (dist_patch_DATA): Remove
    packages/patches/deja-dup-use-ref-keyword-for-iter.patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -860,7 +860,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/dconf-meson-0.52.patch			\
   %D%/packages/patches/debops-constants-for-external-program-names.patch \
   %D%/packages/patches/debops-debops-defaults-fall-back-to-less.patch \
-  %D%/packages/patches/deja-dup-use-ref-keyword-for-iter.patch	\
   %D%/packages/patches/desmume-gcc6-fixes.patch			\
   %D%/packages/patches/desmume-gcc7-fixes.patch			\
   %D%/packages/patches/dfu-programmer-fix-libusb.patch		\
