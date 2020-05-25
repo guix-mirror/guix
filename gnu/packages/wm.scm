8b2a637dcd7d5e8e4e5e765e89196f542fbddac4
Marcin Karpezo <sirmacik@wioo.waw.pl>
2020-05-25 19:44:57 +0200

    gnu: stumpish: Update to 0.0.1-2.920f8fc.
    Signed-off-by: Oleg Pykhalov <go.wigust@gmail.com>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -1661,8 +1661,8 @@ productive, customizable lisp based systems.")
            (delete 'create-symlinks)))))))
 
 (define-public stumpish
-  (let ((commit "dd5b037923ec7d3cc27c55806bcec5a1b8cf4e91")
-        (revision "1"))
+  (let ((commit "920f8fc1488f7953f205e1dda4c2ecbbbda56d6")
+        (revision "2"))
     (package
       (name "stumpish")
       (version (git-version "0.0.1" revision commit)) ;no upstream release
@@ -1674,7 +1674,7 @@ productive, customizable lisp based systems.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0ahxdj9f884afpzxczx6mx7l4nwg4kw6afqaq7lwhf7lxcwylldn"))))
+          (base32 "0giac390bq95ag41xkxqp8jjrhfx1wpgglz7jg5rkm0wjhcwmyml"))))
       (inputs
        `(("bash" ,bash)
          ("rlwrap" ,rlwrap)))
