29334165ff01b1957dae5f2bac15ac7dfa021b6e
Marius Bakke <marius@gnu.org>
2020-05-30 23:09:29 +0200

    Revert "gnu: python-pylint: Udpate to 2.5.2."
    This update broke 'Totem', see <https://bugs.gnu.org/41620>.
    
    This reverts commit c2e19b6291cc4981e39d01aae85af0899614061d.

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -1887,10 +1887,12 @@ unit tests and failing them if the unit test module does not exercise all
 statements in the module it tests.")
     (license license:gpl3+)))
 
+;; Further releases, up to 2.4.3, have failing unit tests. See:
+;; https://github.com/PyCQA/pylint/issues/3198.
 (define-public python-pylint
   (package
     (name "python-pylint")
-    (version "2.5.2")
+    (version "2.3.1")
     (source
      (origin
        (method git-fetch)
@@ -1900,11 +1902,8 @@ statements in the module it tests.")
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "150x679mrlgm1s4ym7irf9mnsjilqyaakss4spc4pbrzkl11agnh"))))
+         "17vvzbcqmkhr4icq5p3737nbiiyj1y3g1pa08n9mb1bsnvxmqq0z"))))
     (build-system python-build-system)
-    ;; FIXME: Tests are failing since version 2.4.3, see:
-    ;; https://github.com/PyCQA/pylint/issues/3198.
-    (arguments '(#:tests? #f))
     (native-inputs
      `(("python-pytest" ,python-pytest)
        ("python-pytest-runner" ,python-pytest-runner)
@@ -1946,7 +1945,7 @@ possible to write plugins to add your own checks.")
                  (base32
                   "02a89d8a47s7nfiv1ady3j0sg2sbyja3np145brarfp5x9qxz9x2"))))
              (arguments
-              `(,@(strip-keyword-arguments '(#:tests?) (package-arguments pylint))
+              `(,@(package-arguments pylint)
                 #:phases
                 (modify-phases %standard-phases
                   (replace 'check
