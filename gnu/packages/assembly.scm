22da777040876b9faf4af93689d093315f2e0b04
Tobias Geerinckx-Rice <me@tobias.gr>
2020-06-02 00:27:16 +0200

    gnu: xa: Update to 2.3.11.
    * gnu/packages/assembly.scm (xa): Update to 2.3.11.

--- gnu/packages/assembly.scm
+++ gnu/packages/assembly.scm
@@ -347,14 +347,14 @@ Supported architectures are:
 (define-public xa
   (package
     (name "xa")
-    (version "2.3.10")
+    (version "2.3.11")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.floodgap.com/retrotech/xa"
                                   "/dists/xa-" version ".tar.gz"))
               (sha256
                (base32
-                "0y5sd247g11jfk5msxy91hz2nhpy7smj125dzfyfhjsjnqk5nyw6"))))
+                "0b81r7mvzqxgnbbmhixcnrf9nc72v1nqaw19k67221g3k561dwij"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f   ; TODO: custom test harness, not sure how it works
