a4620c019b8d5e9b24f3ea8470cd20b9a0bebb70
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-06-10 22:17:11 +0200

    gnu: fet: Update to 5.45.0.
    * gnu/packages/education.scm (fet): Update to 5.45.0.

--- gnu/packages/education.scm
+++ gnu/packages/education.scm
@@ -609,14 +609,14 @@ Portuguese, Spanish and Italian.")
 (define-public fet
   (package
     (name "fet")
-    (version "5.44.8")
+    (version "5.45.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://www.lalescu.ro/liviu/fet/download/"
                            "fet-" version ".tar.bz2"))
        (sha256
-        (base32 "1i59xpkdnrd3qzgqs11vsws57g33zvkad5q3an56vd94aw7z3kpw"))))
+        (base32 "04jns6wc20rz6cp410znsllknhp4zlf4rn8wgv712855nffs42c6"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
