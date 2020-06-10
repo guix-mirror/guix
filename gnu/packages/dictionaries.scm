b26ad6dbe2ac53f1d42f22a4e62c99d90e937078
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-06-10 17:04:25 +0200

    gnu: grammalecte: Update to 1.10.0.
    * gnu/packages/dictionaries.scm (grammalecte): Update to 1.10.0.

--- gnu/packages/dictionaries.scm
+++ gnu/packages/dictionaries.scm
@@ -205,14 +205,14 @@ It comes with a German-English dictionary with approximately 270,000 entries.")
 (define-public grammalecte
   (package
     (name "grammalecte")
-    (version "1.9.0")
+    (version "1.10.0")
     (source
      (origin
        (method url-fetch/zipbomb)
        (uri (string-append "https://grammalecte.net/grammalecte/zip/"
                            "Grammalecte-fr-v" version ".zip"))
        (sha256
-        (base32 "1dmbmvf27dy0cnq2x7ldp9xmni8fl1p7vflgpy0yrgifgrjvnxzr"))))
+        (base32 "0lyngnvg995qdm9bhqj8vd8aywpcdqmzv3j4j77wxhsfa9k0pz08"))))
     (build-system python-build-system)
     (home-page "https://grammalecte.net")
     (synopsis "French spelling and grammar checker")
