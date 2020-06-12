16dd6bacaafc4f9708d5896918f75abfaee23584
Ricardo Wurmus <rekado@elephly.net>
2020-06-12 16:36:07 +0200

    gnu: r-beachmat: Update to 2.4.0.
    * gnu/packages/bioinformatics.scm (r-beachmat): Update to 2.4.0.
    [native-inputs]: Add r-knitr.

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -10957,19 +10957,21 @@ packages.")
 (define-public r-beachmat
   (package
     (name "r-beachmat")
-    (version "2.2.1")
+    (version "2.4.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "beachmat" version))
        (sha256
         (base32
-         "1bpnlw2kdy9yc2vq948k980r0j25ipb80llhvn0j3kxjiwyfgs3i"))))
+         "1vl6jbf9ia78cm4ikdb8vz04jv4b46zhvg5i006c63a9pzw7zhxi"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-biocgenerics" ,r-biocgenerics)
        ("r-delayedarray" ,r-delayedarray)
        ("r-matrix" ,r-matrix)))
+    (native-inputs
+     `(("r-knitr" ,r-knitr)))
     (home-page "https://bioconductor.org/packages/beachmat")
     (synopsis "Compiling Bioconductor to handle each matrix type")
     (description "This package provides a consistent C++ class interface for a
