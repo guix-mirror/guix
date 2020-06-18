620796e66bb828ca22b5d4932275bc36a7b4706a
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2020-06-18 01:49:47 -0400

    gnu: lib2geom: Fix tests on aarch64, i686-linux architectures.
    Fixes <http://issues.guix.gnu.org/41827>.
    
    This change also removes obsolete patches.
    
    * gnu/packages/patches/lib2geom-fix-tests.patch: New file.
    * gnu/packages/patches/lib2geom-link-tests-against-glib.patch: Delete file.
    * gnu/packages/patches/lib2geom-use-system-googletest.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Adjust accordingly.
    * gnu/packages/graphics.scm (lib2geom)[origin]: Use new patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1161,8 +1161,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/leela-zero-gtest.patch			\
   %D%/packages/patches/less-hurd-path-max.patch			\
   %D%/packages/patches/lib2geom-enable-assertions.patch		\
-  %D%/packages/patches/lib2geom-link-tests-against-glib.patch	\
-  %D%/packages/patches/lib2geom-use-system-googletest.patch	\
+  %D%/packages/patches/lib2geom-fix-tests.patch			\
   %D%/packages/patches/liba52-enable-pic.patch			\
   %D%/packages/patches/liba52-link-with-libm.patch		\
   %D%/packages/patches/liba52-set-soname.patch			\
