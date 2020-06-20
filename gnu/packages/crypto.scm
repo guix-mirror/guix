12d69fc5745759b11d6e19cdcdad64d34ecd9dd4
Brice Waegeneire <brice@waegenei.re>
2020-06-20 17:11:10 +0200

    gnu: tomb: Add 'getent' to PATH.
    * gnu/packages/crypto.scm (tomb)[arguments]: In 'wrap' phase add
    'getent' to the PATH.
    
    Signed-off-by: Julien Lepiller <julien@lepiller.eu>

--- gnu/packages/crypto.scm
+++ gnu/packages/crypto.scm
@@ -16,6 +16,7 @@
 ;;; Copyright © 2019 Tanguy Le Carrour <tanguy@bioneland.org>
 ;;; Copyright © 2020 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2020 Jakub Kądziołka <kuba@kadziolka.net>
+;;; Copyright © 2020 Brice Waegeneire <brice@waegenei.re>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -445,7 +446,7 @@ no man page, refer to the home page for usage details.")
                                  (error "program not found:" program)))
                            '("seq" "mkfs.ext4" "pinentry"
                              "gpg" "cryptsetup" "gettext" "lsof"
-                             "qrencode" "steghide" "findmnt")))))
+                             "qrencode" "steghide" "findmnt" "getent")))))
                #t)))
          (delete 'check)
          (add-after 'wrap 'check
