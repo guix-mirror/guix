6a3ae2eec7c7ea06499e70343884ce576e2c29f8
Marius Bakke <marius@gnu.org>
2020-06-27 16:26:02 +0200

    gnu: libcyaml: Update to 1.1.0.
    * gnu/packages/web.scm (libcyaml): Update to 1.1.0.

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -5018,7 +5018,7 @@ written in C.  It is developed as part of the NetSurf project.")
 (define-public libcyaml
   (package
     (name "libcyaml")
-    (version "1.0.1")
+    (version "1.1.0")
     (source
      (origin
        (method git-fetch)
@@ -5027,7 +5027,7 @@ written in C.  It is developed as part of the NetSurf project.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0h5ydyqdl8kzh526np3jsi0pm7ks16nh1hjkdsjcd6pacw7y6i6z"))))
+        (base32 "0428p0rwq71nhh5nzcbapsbrjxa0x5l6h6ns32nxv7j624f0zd93"))))
     (build-system gnu-build-system)
     (arguments
      `(#:make-flags
