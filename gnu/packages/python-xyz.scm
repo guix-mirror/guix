6be8ac2d81be9defab76290f48f466b55e9ffccb
Marius Bakke <marius@gnu.org>
2020-06-27 15:50:46 +0200

    gnu: python-greenlet: Update to 0.4.16.
    * gnu/packages/python-xyz.scm (python-greenlet): Update to 0.4.16.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -11412,13 +11412,13 @@ multiple processes (imagine multiprocessing, billiard, futures, celery etc).
 (define-public python-greenlet
   (package
     (name "python-greenlet")
-    (version "0.4.15")
+    (version "0.4.16")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "greenlet" version))
               (sha256
                (base32
-                "1g4g1wwc472ds89zmqlpyan3fbnzpa8qm48z3z1y6mlk44z485ll"))))
+                "0v2571d0av1hz3yx63gvmnrinyn57nnvibg4zkl9frv74b3yl1kf"))))
     (build-system python-build-system)
     (home-page "https://greenlet.readthedocs.io/")
     (synopsis "Lightweight in-process concurrent programming")
