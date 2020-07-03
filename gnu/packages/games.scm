e7e69e56f59546efce6ffd8e7d3cbee70970930c
Jesse Gibbons <jgibbons2357+guix@gmail.com>
2020-07-03 16:54:21 -0600

    gnu: freedink-engine: Call autoreconf.
    * gnu/packages/games.scm: freedink-engine (phases): add 'autoreconf
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/games.scm
+++ gnu/packages/games.scm
@@ -2745,6 +2745,14 @@ interface or via an external visual interface such as GNU XBoard.")
              ;; These tests require a graphical interface.
              (substitute* "src/Makefile.am"
                (("test_gfx_fonts TestIOGfxDisplay") ""))
+             #t))
+         (add-before 'bootstrap 'autoreconf
+           (lambda _
+	     ;; automake is out of date in the source
+	     ;; autoreconf updates the automake scripts
+	     (invoke "autoreconf")
+	     ;; Build fails when autom4te.cache exists.
+	     (delete-file-recursively "autom4te.cache")
              #t)))))
     (native-inputs `(("autoconf" ,autoconf)
                      ("automake" ,automake)
