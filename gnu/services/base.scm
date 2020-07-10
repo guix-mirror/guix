ec4c404c0575b340a04b3922fb828ac5417832dd
Ludovic Courtès <ludo@gnu.org>
2020-07-10 11:42:35 +0200

    services: guix: Refer to 'berlin.guixsd.org.pub'.
    This partly reverts d283bb960f927dd5f7bb8b96bc697221e4e8ad39.
    
    * gnu/services/base.scm (%default-authorized-guix-keys): Refer to berlin.guixsd.org.pub.

--- gnu/services/base.scm
+++ gnu/services/base.scm
@@ -1481,7 +1481,9 @@ archive' public keys, with GUIX."
 
 (define %default-authorized-guix-keys
   ;; List of authorized substitute keys.
-  (list (file-append guix "/share/guix/berlin.guix.gnu.org.pub")))
+  ;; TODO: Change to "berlin.guix.gnu.org.pub" when the 'guix' package is
+  ;; updated.
+  (list (file-append guix "/share/guix/berlin.guixsd.org.pub")))
 
 (define-record-type* <guix-configuration>
   guix-configuration make-guix-configuration
