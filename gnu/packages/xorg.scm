7f9d8a62ca69ea587cbf07e85510b1fa767ee0a0
Marius Bakke <marius@gnu.org>
2020-07-24 23:55:30 +0200

    gnu: xorg-server-for-tests: Update to 1.20.8.
    * gnu/packages/xorg.scm (xorg-server-for-tests)[source, version]: Inherit from
    XORG-SERVER.

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -5442,19 +5442,10 @@ draggable titlebars and borders.")
 ;; This package is intended to be used when building GTK+.
 ;; Note: It's currently marked as "hidden" to avoid having two non-eq?
 ;; packages with the same name and version.
-;; TODO: Update this in the next rebuild cycle.
 (define-public xorg-server-for-tests
   (hidden-package
    (package
-     (inherit xorg-server)
-     (version "1.20.7")
-     (source (origin
-               (inherit (package-source xorg-server))
-               (uri (string-append "mirror://xorg/individual/xserver/"
-                                   "xorg-server-" version ".tar.bz2"))
-               (sha256
-                (base32
-                 "18bfl04ihw1jr3h0fs522nnxxq5ixjay77y9dcymnkzk23q8cndx")))))))
+     (inherit xorg-server))))
 
 (define-public xorg-server-xwayland
   (package
