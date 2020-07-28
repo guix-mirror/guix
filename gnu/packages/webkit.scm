748ab8b3642a339919ce0bba6cdbc2423a95e029
Mark H Weaver <mhw@netris.org>
2020-07-28 18:28:26 -0400

    gnu: webkitgtk: Update to 2.28.4.
    * gnu/packages/webkit.scm (webkitgtk): Update to 2.28.4.

--- gnu/packages/webkit.scm
+++ gnu/packages/webkit.scm
@@ -121,14 +121,14 @@ engine that uses Wayland for graphics output.")
 (define-public webkitgtk
   (package
     (name "webkitgtk")
-    (version "2.28.3")
+    (version "2.28.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.webkitgtk.org/releases/"
                                   "webkitgtk-" version ".tar.xz"))
               (sha256
                (base32
-                "08imj2q6h975mz7714dqdrmbav0s884990gf98xf2862fb08m2gh"))
+                "0r4lkk21pny2g4mmsw0ds14m5hhjys1l47gvy59dfgihr7l546c2"))
               (patches (search-patches "webkitgtk-share-store.patch"))))
     (build-system cmake-build-system)
     (outputs '("out" "doc"))
