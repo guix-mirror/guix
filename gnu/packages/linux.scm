b00a1f2d09d71013f872ced78aa12aac11a9eb7c
Mark H Weaver <mhw@netris.org>
2020-08-01 00:07:08 -0400

    gnu: linux-libre: Update to 5.4.55.
    * gnu/packages/linux.scm (linux-libre-5.4-version): Update to 5.4.55.
    (linux-libre-5.4-pristine-source): Update hash.
    (deblob-scripts-5.4): Update to 5.4.55.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -210,7 +210,7 @@ defconfig.  Return the appropriate make target if applicable, otherwise return
 
 (define deblob-scripts-5.4
   (linux-libre-deblob-scripts
-   "5.4.54"
+   "5.4.55"
    (base32 "0ckxn7k5zgcqk30dq943bnamr6a6zjbw2aqjl3x30f4kvh5f6k25")
    (base32 "10qb890is4z58vr8czh3xx69q62l3b3j38y410kgiw8nii3zx5lr")))
 
@@ -387,10 +387,10 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.7)))
 
-(define-public linux-libre-5.4-version "5.4.54")
+(define-public linux-libre-5.4-version "5.4.55")
 (define-public linux-libre-5.4-pristine-source
   (let ((version linux-libre-5.4-version)
-        (hash (base32 "0xzlvd9h9vf9m9x088kacwd25whn1anq4bxpd3bg9s5c1r7a3qh3")))
+        (hash (base32 "0h7r9ggc6412hb20f3sy6k7mlbwif137w6shv31xmvw0iv9ky2yc")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.4)))
