guix-mirror/guix

commit efe037fc5cc3134bbc3ef4e36b49a3f788921b68

ui: Factorize '&message' handling.
* guix/ui.scm (call-with-error-handling): Factorize the three
'message-condition?' clauses into one.

guix/ui.scm
@@ -782,16 +782,13 @@ directories:~{ ~a~}~%")
                      (invoke-error-stop-signal c)
                      (cons (invoke-error-program c)
                            (invoke-error-arguments c))))
-             ((and (error-location? c) (message-condition? c))
-              (report-error (error-location c) (G_ "~a~%")
+             ((message-condition? c)
+              ;; Normally '&message' error conditions have an i18n'd message.
+              (report-error (and (error-location? c) (error-location c))
+                            (G_ "~a~%")
                             (gettext (condition-message c) %gettext-domain))
               (when (fix-hint? c)
                 (display-hint (condition-fix-hint c)))
-              (exit 1))
-             ((and (message-condition? c) (fix-hint? c))
-              (report-error (G_ "~a~%")
-                            (gettext (condition-message c) %gettext-domain))
-              (display-hint (condition-fix-hint c))
               (exit 1))
 
              ;; On Guile 3.0.0, exceptions such as 'unbound-variable' are
@@ -806,12 +803,7 @@ directories:~{ ~a~}~%")
                 (guile-3
                  ((exception-predicate &exception-with-kind-and-args) c))
                 (else #f))
-              (raise c))
-
-             ((message-condition? c)
-              ;; Normally '&message' error conditions have an i18n'd message.
-              (leave (G_ "~a~%")
-                     (gettext (condition-message c) %gettext-domain))))
+              (raise c)))
       ;; Catch EPIPE and the likes.
       (catch 'system-error
         thunk

commit 05f3d34094b23dc9612ff6641a0257bc4f7dcd12

ui: Report key-and-arg exceptions correctly.
Fixes <https://bugs.gnu.org/42601>.
Reported by Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>.

Regression introduced in efe037fc5cc3134bbc3ef4e36b49a3f788921b68
whereby errors like 'wrong-type-arg' would be improperly reported:

  guix environment: error: Wrong type argument in position ~A (expecting ~A): ~S

See also commit a07d5e558b5403dad0a59776b950b6b02169c249.

* guix/ui.scm (call-with-error-handling): Move 'message-condition?'
clause after '&exception-with-kind-and-args' clause.

guix/ui.scm
@@ -795,14 +795,6 @@ directories:~{ ~a~}~%")
                      (invoke-error-stop-signal c)
                      (cons (invoke-error-program c)
                            (invoke-error-arguments c))))
-             ((message-condition? c)
-              ;; Normally '&message' error conditions have an i18n'd message.
-              (report-error (and (error-location? c) (error-location c))
-                            (G_ "~a~%")
-                            (gettext (condition-message c) %gettext-domain))
-              (when (fix-hint? c)
-                (display-hint (condition-fix-hint c)))
-              (exit 1))
 
              ((formatted-message? c)
               (apply report-error
@@ -825,7 +817,16 @@ directories:~{ ~a~}~%")
                 (guile-3
                  ((exception-predicate &exception-with-kind-and-args) c))
                 (else #f))
-              (raise c)))
+              (raise c))
+
+             ((message-condition? c)
+              ;; Normally '&message' error conditions have an i18n'd message.
+              (report-error (and (error-location? c) (error-location c))
+                            (G_ "~a~%")
+                            (gettext (condition-message c) %gettext-domain))
+              (when (fix-hint? c)
+                (display-hint (condition-fix-hint c)))
+              (exit 1)))
       ;; Catch EPIPE and the likes.
       (catch 'system-error
         thunk