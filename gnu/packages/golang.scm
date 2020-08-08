1871d26444e83fedfdedba943fdcfcf4745a4d04
Oleg Pykhalov <go.wigust@gmail.com>
2020-08-08 23:44:00 +0300

    gnu: Add go-github-com-sergi-go-diff.
    * gnu/packages/golang.scm (go-github-com-sergi-go-diff): New variable.

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -4571,3 +4571,33 @@ stripped.")
       (description "This package attempts to represent Go values in a form that
 can be used almost directly in Go source code.")
       (license license:expat))))
+
+(define-public go-github-com-sergi-go-diff
+  (package
+    (name "go-github-com-sergi-go-diff")
+    (version "1.1.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/sergi/go-diff")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0ir8ali2vx0j7pipmlfd6k8c973akyy2nmbjrf008fm800zcp7z2"))))
+    (build-system go-build-system)
+    (arguments
+     `(#:import-path "github.com/sergi/go-diff/diffmatchpatch"
+       #:unpack-path "github.com/sergi/go-diff"))
+    (native-inputs
+     `(("go-github-com-stretchr-testify" ,go-github-com-stretchr-testify)))
+    (home-page "https://github.com/sergi/go-diff/")
+    (synopsis "Algorithms to perform operations for synchronizing plain text")
+    (description "@code{go-diff} offers algorithms to perform operations required for
+synchronizing plain text:
+@itemize
+@item compare two texts and return their differences
+@item perform fuzzy matching of text
+@item apply patches onto text
+@end itemize\n")
+    (license license:expat)))
