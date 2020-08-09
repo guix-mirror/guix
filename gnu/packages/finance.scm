8809696efaf373e2cdbeaeff4dd436aa7f56df8b
Guillaume Le Vaillant <glv@posteo.net>
2020-08-09 09:48:00 +0200

    gnu: monero: Update to 0.16.0.3.
    * gnu/packages/finance.scm (monero): Update to 0.16.0.3.

--- gnu/packages/finance.scm
+++ gnu/packages/finance.scm
@@ -547,7 +547,7 @@ other machines/servers.  Electroncash does not download the Bitcoin Cash blockch
   ;; the system's dynamically linked library.
   (package
     (name "monero")
-    (version "0.16.0.1")
+    (version "0.16.0.3")
     (source
      (origin
        (method git-fetch)
@@ -567,8 +567,7 @@ other machines/servers.  Electroncash does not download the Bitcoin Cash blockch
               "external/unbound"))
            #t))
        (sha256
-        (base32
-         "0n2cviqm8radpynx70fc0819k1xknjc58cvb4whlc49ilyvh8ky6"))))
+        (base32 "1r9x3712vhb24dxxirfiwj5f9x0h4m7x0ngiiavf5983dfdlgz33"))))
     (build-system cmake-build-system)
     (native-inputs
      `(("doxygen" ,doxygen)
