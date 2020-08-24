7bcc8004b192f2316beee837521ec251b3d13aca
Efraim Flashner <efraim@flashner.co.il>
2020-08-24 12:03:12 +0300

    gnu: scroll: Fix build.
    * gnu/packages/haskell-apps.scm (scroll)[arguments]: Add custom phase to
    add a place-holder file to the static output.

--- gnu/packages/haskell-apps.scm
+++ gnu/packages/haskell-apps.scm
@@ -738,6 +738,20 @@ is programmed in Haskell.")
          (base32
           "0apzrvf99rskj4dbmn57jjxrsf19j436s8a09m950df5aws3a0wj"))))
     (build-system haskell-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'install 'touch-static-output
+           (lambda* (#:key outputs #:allow-other-keys)
+             ;; The Haskell build system adds a "static" output by
+             ;; default, and there is no way to override this until
+             ;; <https://issues.guix.gnu.org/41569> is fixed.  Without
+             ;; this phase, the daemon complains because we do not
+             ;; create the "static" output.
+             (with-output-to-file (assoc-ref outputs "static")
+               (lambda ()
+                 (display "static output not used\n")))
+             #t)))))
     (inputs
      `(("ghc-case-insensitive" ,ghc-case-insensitive)
        ("ghc-data-default" ,ghc-data-default)
