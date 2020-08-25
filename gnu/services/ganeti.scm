2e832d4b8a1c68a9f28d5953819954ee37d9800f
Tobias Geerinckx-Rice <me@tobias.gr>
2020-08-25 16:05:05 +0200

    services: ganeti-kvmd-service-type: Fix typo in description.
    * gnu/services/ganeti.scm (ganeti-kvmd-service-type)[description]:
    Fix typo.

--- gnu/services/ganeti.scm
+++ gnu/services/ganeti.scm
@@ -550,7 +550,7 @@ The KVM daemon monitors, using @code{inotify}, KVM instances through their QMP
 sockets, which are provided by KVM.  Using the QMP sockets, the KVM daemon
 listens for particular shutdown, powerdown, and stop events which will determine
 if a given instance was shutdown by the user or Ganeti, and this result is
-communicated to Ganeti via a special file in the filesystem.")))
+communicated to Ganeti via a special file in the file system.")))
 
 (define-record-type* <ganeti-mond-configuration>
   ganeti-mond-configuration make-ganeti-mond-configuration
