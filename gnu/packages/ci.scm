7f765a9d9c21ea3c0e5426795ae909f1e3c7e0a7
Mathieu Othacehe <othacehe@gnu.org>
2020-08-25 16:38:19 +0200

    gnu: cuirass: Update to 0.0.1-43.f2984c7.
    Cuirass now depends of "guile-zlib" instead of Guix zlib module.
    
    * gnu/packages/ci.scm (cuirass): Update to 0.0.1-43.f2984c7,
    [inputs]: add "guile-zlib" ...
    [arguments]: ... and add it to the dependencies.

--- gnu/packages/ci.scm
+++ gnu/packages/ci.scm
@@ -47,8 +47,8 @@
   #:use-module (guix build-system gnu))
 
 (define-public cuirass
-  (let ((commit "d3329551fe2b90a454617da0f241e51fb4a72c58")
-        (revision "42"))
+  (let ((commit "f2984c7230f69a6e50810edc5e9d36bd671801f9")
+        (revision "43"))
     (package
       (name "cuirass")
       (version (git-version "0.0.1" revision commit))
@@ -60,7 +60,7 @@
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "03ylkfwjk3qjl8pbml9gjz0skfq42klv3xbh9jpnf7rxillmcf20"))))
+                  "1p9mlmhv4kz8wixgywh1ffm3140p4mkgz92n7ry3n5s9w5n7fpjl"))))
       (build-system gnu-build-system)
       (arguments
        '(#:modules ((guix build utils)
@@ -93,8 +93,10 @@
                       (git    (assoc-ref inputs "guile-git"))
                       (bytes  (assoc-ref inputs "guile-bytestructures"))
                       (fibers (assoc-ref inputs "guile-fibers"))
+                      (zlib   (assoc-ref inputs "guile-zlib"))
                       (guix   (assoc-ref inputs "guix"))
-                      (deps   (list gcrypt json sqlite git bytes fibers guix))
+                      (deps   (list gcrypt json sqlite git bytes fibers
+                                    zlib guix))
                       (guile  (assoc-ref %build-inputs "guile"))
                       (effective (read-line
                                   (open-pipe* OPEN_READ
@@ -126,6 +128,7 @@
          ("guile-json" ,guile-json-4)
          ("guile-sqlite3" ,guile-sqlite3)
          ("guile-git" ,guile-git)
+         ("guile-zlib" ,guile-zlib)
          ;; FIXME: this is propagated by "guile-git", but it needs to be among
          ;; the inputs to add it to GUILE_LOAD_PATH.
          ("guile-bytestructures" ,guile-bytestructures)
