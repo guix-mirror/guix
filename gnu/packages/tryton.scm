63c5e70e9560685d0a08a1337443654016ea4388
Vinicius Monego <monego@posteo.net>
2020-08-26 10:51:50 -0300

    gnu: Add python-trytond-account-invoice.
    * gnu/packages/tryton.scm (python-trytond-account-invoice): New variable.
    
    Signed-off-by: Mathieu Othacehe <othacehe@gnu.org>

--- gnu/packages/tryton.scm
+++ gnu/packages/tryton.scm
@@ -520,6 +520,63 @@ required to analyse accounting using multiple different axes.")
 and category.")
     (license license:gpl3+)))
 
+(define-public python-trytond-account-invoice
+  (package
+    (name "python-trytond-account-invoice")
+    (version "5.6.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "trytond_account_invoice" version))
+       (sha256
+        (base32 "0ic5zghg50m6jwdr88y4vgzy58g1h17b63sbhh61brys1cp2dxhc"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (replace 'check
+           (let ((runtest
+                  (string-append
+                   (assoc-ref %build-inputs "python-trytond")
+                   "/lib/python"
+                   ,(version-major+minor (package-version python))
+                   "/site-packages/trytond/tests/run-tests.py")))
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (add-installed-pythonpath inputs outputs)
+               (invoke "python" runtest "-m" "account_invoice")))))))
+    (native-inputs
+     `(("python" ,python-minimal-wrapper)
+       ("python-genshi" ,python-genshi)
+       ("python-lxml" ,python-lxml)
+       ("python-magic" ,python-magic)
+       ("python-passlib" ,python-passlib)
+       ("python-polib" ,python-polib)
+       ("python-proteus" ,python-proteus)
+       ("python-relatorio" ,python-relatorio)
+       ("python-werkzeug" ,python-werkzeug)
+       ("python-wrapt" ,python-wrapt)))
+    (propagated-inputs
+     `(("python-dateutil" ,python-dateutil)
+       ("python-sql" ,python-sql)
+       ("python-trytond" ,python-trytond)
+       ("python-trytond-account"
+        ,python-trytond-account)
+       ("python-trytond-account-product"
+        ,python-trytond-account-product)
+       ("python-trytond-company"
+        ,python-trytond-company)
+       ("python-trytond-currency"
+        ,python-trytond-currency)
+       ("python-trytond-party" ,python-trytond-party)
+       ("python-trytond-product"
+        ,python-trytond-product)))
+    (home-page "https://www.tryton.org/")
+    (synopsis "Tryton module for invoicing")
+    (description
+     "This package provides a Tryton module that adds the invoice, payment
+term.")
+    (license license:gpl3+)))
+
 (define-public python-trytond-stock
   (package
     (name "python-trytond-stock")
