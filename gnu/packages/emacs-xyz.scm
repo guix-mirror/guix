ad476912b6987507a0dc5b6d4b53bbf6a3708113
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-08-27 15:34:35 +0200

    gnu: emacs-modus-vivendi-theme: Update to 0.12.0.
    * gnu/packages/emacs-xyz.scm (emacs-modus-vivendi-theme): Update to 0.12.0.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -22313,14 +22313,14 @@ standard.  This is the highest standard of its kind.")
 (define-public emacs-modus-vivendi-theme
   (package
     (name "emacs-modus-vivendi-theme")
-    (version "0.11.0")
+    (version "0.12.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "modus-vivendi-theme-" version ".el"))
        (sha256
-        (base32 "14ky9cxg9cpvhgg24ra0xla2dapqjlf948470q7v0m402x1r2iif"))))
+        (base32 "01f6z5xjnmki1k9m83jwva42lxidb31pdpwm4wpxjzxqmb96picn"))))
     (build-system emacs-build-system)
     (home-page "https://gitlab.com/protesilaos/modus-themes")
     (synopsis "Accessible dark theme (WCAG AAA)")
