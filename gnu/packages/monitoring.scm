54c995cc7ef3b89ae1cfe4bfb67066539884c177
Tobias Geerinckx-Rice <me@tobias.gr>
2020-08-31 17:43:16 +0200

    gnu: zabbix-agentd: Update to 5.0.3.
    * gnu/packages/monitoring.scm (zabbix-agentd): Update to 5.0.3.

--- gnu/packages/monitoring.scm
+++ gnu/packages/monitoring.scm
@@ -158,7 +158,7 @@ etc. via a Web interface.  Features include:
 (define-public zabbix-agentd
   (package
     (name "zabbix-agentd")
-    (version "5.0.2")
+    (version "5.0.3")
     (source
      (origin
        (method url-fetch)
@@ -166,7 +166,7 @@ etc. via a Web interface.  Features include:
              "https://cdn.zabbix.com/zabbix/sources/stable/"
              (version-major+minor version) "/zabbix-" version ".tar.gz"))
        (sha256
-        (base32 "1cnns7ixqi7ank3cbvcs7d8rb5zh9qiqbmgivazr83jnz81qg46w"))))
+        (base32 "1dc3fb9pa2cr6mfwgfyy1hpx26pbrws7989wj2kiiilmvimvrz1l"))))
     (build-system gnu-build-system)
     (arguments
      `(#:configure-flags
