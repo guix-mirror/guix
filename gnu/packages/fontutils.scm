9f89779e1fcb5412b9274189dfe89f810d4f6fe5
Marius Bakke <marius@gnu.org>
2020-09-27 20:55:54 +0200

    gnu: libspiro: Replace with 20200505 [fixes CVE-2019-19847].
    * gnu/packages/fontutils.scm (libspiro)[replacement]: New field.
    (libspiro-20200505): New variable.

--- gnu/packages/fontutils.scm
+++ gnu/packages/fontutils.scm
@@ -556,6 +556,7 @@ using the above tables.")
   (package
     (name "libspiro")
     (version "20190731")
+    (replacement libspiro-20200505)
     (source
      (origin
       (method url-fetch)
@@ -573,6 +574,19 @@ using the above tables.")
 smooth contours with constant curvature at the spline joins.")
     (license license:gpl2+)
     (home-page "http://libspiro.sourceforge.net/")))
+
+(define libspiro-20200505
+  (package
+    (inherit libspiro)
+    (version "20200505")
+    (source
+     (origin
+      (method url-fetch)
+      (uri (string-append "https://github.com/fontforge/libspiro/releases"
+                          "/download/" version "/libspiro-dist-" version ".tar.gz"))
+      (sha256
+       (base32
+        "0j8fmyj4wz6mqk17dqs6f8jx0i52n68gv5px17qbrjnbilg9mih6"))))))
 
 (define-public libuninameslist
   (package
