04a459a069155a6b676b6b93d7dbddb426dd0679
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-09-30 07:53:35 +0200

    services: hurd-vm: Add childhurd user to kvm group.
    This is a follow-up to commit d692ebf98077d6b651d426aba92bf2a38599c4dc.
    
    * gnu/services/virtualization.scm (%hurd-vm-accounts)[supplementary-groups]:
    Add ’kvm’.
    * gnu/services/virtualization.scm (hurd-vm-shepherd-service): Use #:group
    "kvm"

--- gnu/services/virtualization.scm
+++ gnu/services/virtualization.scm
@@ -974,7 +974,9 @@ is added to the OS specified in CONFIG."
          #~(lambda ()
              (let ((pid  (fork+exec-command #$vm-command
                                             #:user "childhurd"
-                                            #:group "childhurd"
+                                            ;; XXX TODO: use "childhurd" after
+                                            ;; updating Shepherd
+                                            #:group "kvm"
                                             #:environment-variables
                                             ;; QEMU tries to write to /var/tmp
                                             ;; by default.
@@ -1005,6 +1007,7 @@ is added to the OS specified in CONFIG."
         (user-account
          (name "childhurd")
          (group "childhurd")
+         (supplementary-groups '("kvm"))
          (comment "Privilege separation user for the childhurd")
          (home-directory "/var/empty")
          (shell (file-append shadow "/sbin/nologin"))
