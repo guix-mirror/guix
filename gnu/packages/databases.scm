25fb1e308d5c95d98f86707732a2cc58b165caa7
Tobias Geerinckx-Rice <me@tobias.gr>
2020-09-30 12:19:11 +0200

    gnu: perl-dbd-sqlite: Update to 1.66.
    * gnu/packages/databases.scm (perl-dbd-sqlite): Update to 1.66.

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -1741,7 +1741,7 @@ columns, primary keys, unique constraints and relationships.")
 (define-public perl-dbd-sqlite
   (package
     (name "perl-dbd-sqlite")
-    (version "1.64")
+    (version "1.66")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1749,7 +1749,7 @@ columns, primary keys, unique constraints and relationships.")
                     version ".tar.gz"))
               (sha256
                (base32
-                "00gz5aw3xrr92lf9nfk0dhmy7a8jzmxhznddd9b0a8w4a1xqzbpl"))))
+                "1zljln5nh61gj3k22a1fv2vhx5l83waizmarwkh77hk6kzzmvrw9"))))
     (build-system perl-build-system)
     (inputs `(("sqlite" ,sqlite)))
     (propagated-inputs `(("perl-dbi" ,perl-dbi)))
