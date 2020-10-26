17aeabdd5238c16f6f88fa1dc45a38ed84fd9ef0
Ricardo Wurmus <rekado@elephly.net>
2020-10-26 19:35:56 +0100

    gnu: texlive-base: Add texlive-cm-super to default set.
    This adds T1 variants of the CM fonts, avoiding the need to generate bitmap
    fonts (.pk files) at runtime.
    
    * gnu/packages/tex.scm (texlive-base)[propagated-inputs]: Add
    texlive-cm-super.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -3459,6 +3459,7 @@ packages.")
                 texlive-dvips
                 texlive-fontname
                 texlive-cm
+                texlive-cm-super ; to avoid bitmap fonts
                 texlive-fonts-latex
                 texlive-metafont-base
                 texlive-latex-base
