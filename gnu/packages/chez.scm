5b6600e6893e9e428cecfbc0537e602f80fff319
Ricardo Wurmus <rekado@elephly.net>
2020-10-27 09:26:44 +0100

    gnu: chez-web: Remove obsolete build phase.
    * gnu/packages/chez.scm (chez-web)[arguments]: Remove build phase "set-HOME".

--- gnu/packages/chez.scm
+++ gnu/packages/chez.scm
@@ -289,12 +289,6 @@ and 32-bit PowerPC architectures.")
                       #:tests? #f        ; no tests
                       #:phases
                       (modify-phases %standard-phases
-                        (add-before 'build 'set-HOME
-                          (lambda _
-                            ;; FIXME: texlive-union does not find the built
-                            ;; metafonts, so it tries to generate them in HOME.
-                            (setenv "HOME" "/tmp")
-                            #t))
                         ;; This package has a custom "bootstrap" script that
                         ;; is meant to be run from the Makefile.
                         (delete 'bootstrap)
