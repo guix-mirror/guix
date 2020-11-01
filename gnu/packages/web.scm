ba1d3451c68bc2666ea7c5e806fe7b7432334390
Tobias Geerinckx-Rice <me@tobias.gr>
2020-11-01 13:49:36 +0100

    gnu: nginx: Update to 1.19.4.
    * gnu/packages/web.scm (nginx): Update to 1.19.4.

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -234,14 +234,14 @@ Interface} specification.")
     ;; ’stable’ and recommends that “in general you deploy the NGINX mainline
     ;; branch at all times” (https://www.nginx.com/blog/nginx-1-6-1-7-released/)
     ;; Consider updating the nginx-documentation package together with this one.
-    (version "1.19.3")
+    (version "1.19.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://nginx.org/download/nginx-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "1w4dkq7bl5gyix3x0ap3d9lndh7zyvc3mscl693d4ybql57vgrci"))))
+                "03h0hhrbfy3asla9gki2cp97zjn7idxbp5lk9xi0snlh4xlm9pv1"))))
     (build-system gnu-build-system)
     (inputs `(("openssl" ,openssl)
               ("pcre" ,pcre)
