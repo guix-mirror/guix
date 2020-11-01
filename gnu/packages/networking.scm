0886ff913bdfd54228fd7abe2c9d32e758c0070f
Tobias Geerinckx-Rice <me@tobias.gr>
2020-11-01 14:22:05 +0100

    gnu: batctl: Update to 2020.4.
    * gnu/packages/networking.scm (batctl): Update to 2020.4.

--- gnu/packages/networking.scm
+++ gnu/packages/networking.scm
@@ -3448,14 +3448,14 @@ maximum extent possible.")
 (define-public batctl
   (package
    (name "batctl")
-   (version "2020.3")
+   (version "2020.4")
    (source
     (origin
      (method url-fetch)
      (uri (string-append "https://downloads.open-mesh.org/batman/releases/batman-adv-"
                          version "/batctl-" version ".tar.gz"))
      (sha256
-      (base32 "0r2w2v4sy6wgr7mp9lc7yj9k4ldsbsm3425rjil7p0b17zmzf4rm"))))
+      (base32 "05rrpfbpdhxn5zgdps849qls2ifis6a94cjryb60d4y1nc2n0d7w"))))
    (inputs
     `(("libnl" ,libnl)))
    (native-inputs
