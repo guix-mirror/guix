28e2f27d5c36c3eed15e2d486dbd75edecbc6146
Tobias Geerinckx-Rice <me@tobias.gr>
2020-11-09 22:26:19 +0100

    gnu: amsynth: Find external commands.
    * gnu/packages/music.scm (amsynth)[arguments]: Add a ‘patch-file-names’
    phase.
    [inputs]: Add unzip and which.

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -1804,6 +1804,16 @@ special variant of additive synthesis.")
         (base32
          "1882pfcmf3rqg3vd4qflzkppcv158d748i603spqjbxqi8z7x7w0"))))
     (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'patch-file-names
+           (lambda _
+             (substitute* "src/GUI/editor_pane.c"
+               (("/usr/bin/unzip") (which "unzip")))
+             (substitute* "src/GUI/GUI.cc"
+               (("/usr/bin/which") (which "which")))
+             #t)))))
     (inputs
      `(("alsa-lib" ,alsa-lib)
        ("gtk+" ,gtk+-2)
@@ -1811,7 +1821,10 @@ special variant of additive synthesis.")
        ("jack" ,jack-1)
        ("lash" ,lash)
        ("libsndfile" ,libsndfile)
-       ("lv2" ,lv2)))
+       ("lv2" ,lv2)
+       ;; External commands invoked at run time.
+       ("unzip" ,unzip)
+       ("which" ,which)))
     (native-inputs
      `(("intltool" ,intltool)
        ("pkg-config" ,pkg-config)))
