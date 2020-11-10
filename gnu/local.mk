74361d3ee8c1524ee3f3496f113b9a8bf644e023
Ludovic Courtès <ludo@gnu.org>
2020-11-10 16:43:59 +0100

    gnu: python2-pygobject@2: Shorten patch file name.
    The file would not be included in the tarball produced by "make dist"
    because its name was too long.
    
    * gnu/packages/patches/python2-pygobject-2-gi-info-type-error-domain.patch:
    Rename to...
    * gnu/packages/patches/python2-pygobject-2-deprecation.patch: ... this.
    * gnu/packages/glib.scm (python2-pygobject-2)[source]: Adjust accordingly.
    * gnu/local.mk (dist_patch_DATA): Likewise.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1524,7 +1524,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-paste-remove-timing-test.patch	\
   %D%/packages/patches/python-pycrypto-CVE-2013-7459.patch	\
   %D%/packages/patches/python-pycrypto-time-clock.patch		\
-  %D%/packages/patches/python2-pygobject-2-gi-info-type-error-domain.patch \
+  %D%/packages/patches/python2-pygobject-2-deprecation.patch	\
   %D%/packages/patches/python-pygpgme-fix-pinentry-tests.patch	\
   %D%/packages/patches/python-robotframework-source-date-epoch.patch \
   %D%/packages/patches/python-shouldbe-0.1.2-cpy3.8.patch	\
