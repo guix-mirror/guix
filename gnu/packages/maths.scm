7ea957e2c5886c7b93c3ae31a1a5c41c2bf679dd
Tobias Geerinckx-Rice <me@tobias.gr>
2020-11-15 00:25:49 +0100

    gnu: superlu: Update to 5.2.2.
    * gnu/packages/maths.scm (superlu): Update to 5.2.2.

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -2908,14 +2908,14 @@ easy-to-write markup language for mathematics.")
 (define-public superlu
   (package
     (name "superlu")
-    (version "5.2.1")
+    (version "5.2.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://portal.nersc.gov/project/sparse/superlu/"
                            "superlu_" version ".tar.gz"))
        (sha256
-        (base32 "0qzlb7cd608q62kyppd0a8c65l03vrwqql6gsm465rky23b6dyr8"))
+        (base32 "13520vk6fqspyl22cq4ak2jh3rlmhja4czq56j75fdx65fkk80s7"))
        (modules '((guix build utils)))
        (snippet
         ;; Replace the non-free implementation of MC64 with a stub adapted
