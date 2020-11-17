guix-mirror/guix

commit 28a13226fd5cd08b3eb090151c0753424c3f6eb4

gnu: gnutls: Enable p11-kit.
Everywhere except mips64el, where a comment says it doesn't work. I can't test
as I can't build this on mips64el anyway.

This change will enable running the tests for libcacard.

* gnu/packages/tls.scm (gnutls)[arguments]: Only specify --without-p11-kit on
mips64el.
[inputs]: Include the p11-kit for all systems except mips64el.

gnu/packages/tls.scm
@@ -188,7 +188,7 @@ living in the same process.")
                                     '()
                                     (list net-tools iproute socat))
        #:configure-flags
-       (list
+       (cons*
              ;; GnuTLS doesn't consult any environment variables to specify
              ;; the location of the system-wide trust store.  Instead it has a
              ;; configure-time option.  Unless specified, its configure script
@@ -209,9 +209,14 @@ living in the same process.")
              (string-append "--with-guile-extension-dir="
                             "$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/extensions")
 
-             ;; FIXME: Temporarily disable p11-kit support since it is not
-             ;; working on mips64el.
-             "--without-p11-kit")
+             (let ((system ,(or (%current-target-system)
+                                (%current-system))))
+               (if (string-prefix? "mips64el" system)
+                   (list
+                    ;; FIXME: Temporarily disable p11-kit support since it is
+                    ;; not working on mips64el.
+                    "--without-p11-kit")
+                   '())))
 
        #:phases (modify-phases %standard-phases
                   ;; fastopen.sh fails to connect to the server in the builder
@@ -250,7 +255,12 @@ living in the same process.")
              `(("datefudge" ,datefudge)))         ;tests rely on 'datefudge'
        ("util-linux" ,util-linux)))               ;one test needs 'setsid'
     (inputs
-     `(("guile" ,guile-3.0)))
+     `(("guile" ,guile-3.0)
+       ,@(let ((system (or (%current-target-system)
+                           (%current-system))))
+           (if (string-prefix? "mips64el" system)
+               '()
+               `(("p11-kit" ,p11-kit))))))
     (propagated-inputs
      ;; These are all in the 'Requires.private' field of gnutls.pc.
      `(("libtasn1" ,libtasn1)

commit 8a594487700ddd2d6c293cc5e302ce74f3f56b70

gnu: gnutls: Make p11-kit a propagated input.
Because p11-kit appears in the gnutls.pc file, it needs to be a propagated
input.

This was a regression introduced in 28a13226fd5cd08b3eb090151c0753424c3f6eb4
which broke things like HTTPS support in curl, which then broke Git clones
from repositories using HTTPS.

* gnu/packages/tls.scm (gnutls)[inputs]: Move p11-kit from here.
[propagated-inputs]: To here.

gnu/packages/tls.scm
@@ -255,18 +255,18 @@ living in the same process.")
              `(("datefudge" ,datefudge)))         ;tests rely on 'datefudge'
        ("util-linux" ,util-linux)))               ;one test needs 'setsid'
     (inputs
-     `(("guile" ,guile-3.0)
-       ,@(let ((system (or (%current-target-system)
-                           (%current-system))))
-           (if (string-prefix? "mips64el" system)
-               '()
-               `(("p11-kit" ,p11-kit))))))
+     `(("guile" ,guile-3.0)))
     (propagated-inputs
      ;; These are all in the 'Requires.private' field of gnutls.pc.
      `(("libtasn1" ,libtasn1)
        ("libidn2" ,libidn2)
        ("nettle" ,nettle)
-       ("zlib" ,zlib)))
+       ("zlib" ,zlib)
+       ,@(let ((system (or (%current-target-system)
+                           (%current-system))))
+           (if (string-prefix? "mips64el" system)
+               '()
+               `(("p11-kit" ,p11-kit))))))
     (home-page "https://www.gnu.org/software/gnutls/")
     (synopsis "Transport layer security library")
     (description