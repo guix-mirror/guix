9ca38df74281bbfe5cd8ccea116c7cba0f62fc84
Marius Bakke <marius@gnu.org>
2020-11-19 23:56:46 +0100

    gnu: mako: Update to 1.4.1.
    * gnu/packages/wm.scm (mako): Update to 1.4.1.

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -1560,7 +1560,7 @@ Wlroots based compositors.")
 (define-public mako
   (package
     (name "mako")
-    (version "1.4")
+    (version "1.4.1")
     (source
      (origin
        (method git-fetch)
@@ -1569,7 +1569,7 @@ Wlroots based compositors.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "11ymiq6cr2ma0iva1mqybn3j6k73bsc6lv6pcbdq7hkhd4f9b7j9"))))
+        (base32 "0hwvibpnrximb628w9dsfjpi30b5jy7nfkm4d94z5vhp78p43vxh"))))
     (build-system meson-build-system)
     (inputs `(("cairo" ,cairo)
               ("elogind" ,elogind)
