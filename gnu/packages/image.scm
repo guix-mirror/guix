8dc429165c9a354ffb18703598ea923d7f5e9e9a
Marius Bakke <marius@gnu.org>
2020-11-20 00:00:30 +0100

    gnu: slurp: Update to 1.3.1.
    * gnu/packages/image.scm (slurp): Update to 1.3.1.

--- gnu/packages/image.scm
+++ gnu/packages/image.scm
@@ -1930,7 +1930,7 @@ identical visual appearance.")
 (define-public slurp
   (package
    (name "slurp")
-   (version "1.2.0")
+   (version "1.3.1")
    (source
     (origin
      (method git-fetch)
@@ -1939,7 +1939,7 @@ identical visual appearance.")
            (commit (string-append "v" version))))
      (file-name (git-file-name name version))
      (sha256
-      (base32 "0580m6kaiilgsrcj608r837r37sl6a25y7w21p7d6ij20fs3gvg1"))))
+      (base32 "1fby2v2ylcadgclds05wpkl9xi2r9dfz49dqyqpn20rjv1wnz3jv"))))
    (build-system meson-build-system)
    (native-inputs `(("pkg-config" ,pkg-config)))
    (inputs `(("cairo" ,cairo)
