d407dd84e498240279b3cb15ed3889d7a31ff919
Tobias Geerinckx-Rice <me@tobias.gr>
2020-11-24 23:45:24 +0100

    gnu: postgis: Update to 3.0.3.
    * gnu/packages/geo.scm (postgis): Update to 3.0.3.

--- gnu/packages/geo.scm
+++ gnu/packages/geo.scm
@@ -788,14 +788,14 @@ utilities for data translation and processing.")
 (define-public postgis
   (package
     (name "postgis")
-    (version "3.0.2")
+    (version "3.0.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://download.osgeo.org/postgis/source/postgis-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "1jmji8i2wjabkrzqil683lypnmimigdmn64a10j3kj3kzlfn98d3"))))
+                "05s8cx1rlysiq7dd44kf7cid0la61a4p895j9g95bvfb8v8dpzh7"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f
