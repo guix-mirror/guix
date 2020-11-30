c3e459d266992939a190b9c36d7446ffeed8535a
Guillaume Le Vaillant <glv@posteo.net>
2020-11-30 19:42:04 +0100

    gnu: sbcl: Increase default maximum heap size.
    * gnu/packages/lisp.scm (sbcl)[arguments]: Build with a max heap size
      of 3 GiB.

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -507,7 +507,7 @@ an interpreter, a compiler, a debugger, and much more.")
                                          `("clisp")))
                      (string-append "--prefix="
                                     (assoc-ref outputs "out"))
-                     "--dynamic-space-size=2Gb"
+                     "--dynamic-space-size=3072"
                      "--with-sb-core-compression"
                      "--with-sb-xref-for-internals")))
          (replace 'install
