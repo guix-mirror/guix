04b83678653fda3c66e600e88f54f5108290ec1c
Marius Bakke <marius@gnu.org>
2020-12-01 23:23:10 +0100

    Revert "gnu: python-packaging: Update to 20.4."
    This reverts commit 71b15b4874b7f9ec7001d2916a8ab27dcce6cdc0.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -16003,14 +16003,17 @@ several utilities, as well as an API for building localization tools.")
 (define-public python-packaging
   (package
     (name "python-packaging")
-    (version "20.4")
+    (version "20.0")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "packaging" version))
+        ;; XXX: The URL in the patch file is wrong, it should be
+        ;; <https://github.com/pypa/packaging/pull/256>.
+        (patches (search-patches "python-packaging-test-arch.patch"))
         (sha256
          (base32
-          "1y3rc1ams1i25calk6b9jf1gl85ix5a23a146swjvhdr8x7zfms3"))))
+          "1y2ip3a4ykkpgnwgn85j6hkspcl0cg3mzms97f40mk57vwqq67gy"))))
     (build-system python-build-system)
     (arguments
      `(#:phases (modify-phases %standard-phases
