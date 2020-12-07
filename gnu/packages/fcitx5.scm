aa5cfb39a5e4d2c5d394d013d222e0d56bb6b0a1
Zhu Zihao <all_but_last@163.com>
2020-12-07 22:38:35 +0800

    gnu: Add fcitx5.
    * gnu/packages/fcitx5.scm (fcitx5): New variable.
    
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/fcitx5.scm
+++ gnu/packages/fcitx5.scm
@@ -22,8 +22,21 @@
   #:use-module (guix build-system cmake)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages datastructures)
+  #:use-module (gnu packages enchant)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages iso-codes)
   #:use-module (gnu packages kde-frameworks)
+  #:use-module (gnu packages libevent)
+  #:use-module (gnu packages linux)
   #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages pretty-print)
+  #:use-module (gnu packages unicode)
+  #:use-module (gnu packages web)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xml)
   #:use-module (gnu packages xorg))
 
 (define-public xcb-imdkit
@@ -60,3 +73,63 @@ comparing with the implementation of IMDkit with Xlib, and xim inside Xlib, it
 has less memory foot print, better performance, and safer on malformed
 client.")
     (license license:lgpl2.1)))
+
+(define-public fcitx5
+  (package
+    (name "fcitx5")
+    (version "5.0.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://download.fcitx-im.org/fcitx5/fcitx5/fcitx5-"
+             version "_dict.tar.xz"))
+       (sha256
+        (base32 "06zkb33m2rnhg385iy79n3r4svz5jbav74di61xqa3lhbv7534s3"))))
+    (build-system cmake-build-system)
+    (arguments
+     `(#:configure-flags
+       (list (string-append "-DCLDR_DIR="
+                            (assoc-ref %build-inputs "unicode-cldr-common")
+                            "/share/unicode/cldr"))))
+    (inputs
+     `(("cairo" ,cairo)
+       ("cairo-xcb" ,cairo-xcb)
+       ("dbus" ,dbus)
+       ("enchant" ,enchant)
+       ("expat" ,expat)
+       ("fmt" ,fmt)
+       ("gdk-pixbuf" ,gdk-pixbuf)
+       ("gettext" ,gettext-minimal)
+       ("glib" ,glib)
+       ("iso-codes" ,iso-codes)
+       ("json-c" ,json-c)
+       ("libevent" ,libevent)
+       ("libpthread-stubs" ,libpthread-stubs)
+       ("libuuid" ,util-linux "lib")
+       ("libx11" ,libx11)
+       ("libxcb" ,libxcb)
+       ("libxfixes" ,libxfixes)
+       ("libxinerama" ,libxinerama)
+       ("libxkbcommon" ,libxkbcommon)
+       ("libxkbfile" ,libxkbfile)
+       ("pango" ,pango)
+       ("unicode-cldr-common" ,unicode-cldr-common)
+       ("wayland" ,wayland)
+       ("wayland-protocols" ,wayland-protocols)
+       ("xcb-imdkit" ,xcb-imdkit)
+       ("xcb-util" ,xcb-util)
+       ("xcb-util-keysyms" ,xcb-util-keysyms)
+       ("xcb-util-wm" ,xcb-util-wm)
+       ("xkeyboard-config" ,xkeyboard-config)))
+    (native-inputs
+     `(("extra-cmake-modules" ,extra-cmake-modules)
+       ("pkg-config" ,pkg-config)))
+    (native-search-paths
+     (list (search-path-specification
+            (variable "FCITX_ADDON_DIRS")
+            (files '("lib/fcitx5")))))
+    (home-page "https://github.com/fcitx/fcitx5")
+    (synopsis "Input method framework")
+    (description "Fcitx 5 is a generic input method framework.")
+    (license license:lgpl2.1+)))
