257ff75b2d56a1144acc7dc63bc1a812bc27de1c
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-12-09 15:02:09 +0100

    gnu: Add rust-serde-urlencoded-0.5.
    * gnu/packages/crates-io.scm (rust-serde-urlencoded-0.5): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -26881,6 +26881,27 @@ by dynamically growing the stack.")
     (description "x-www-form-urlencoded meets serde.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-serde-urlencoded-0.5
+  (package
+    (inherit rust-serde-urlencoded-0.6)
+    (name "rust-serde-urlencoded")
+    (version "0.5.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "serde_urlencoded" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0nhnzllx5xrij4x17g351n14md691r95mxr7sbpz4sl80n8xcbb4"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-dtoa" ,rust-dtoa-0.4)
+        ("rust-itoa" ,rust-itoa-0.4)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-url" ,rust-url-1))
+       #:cargo-development-inputs
+       (("rust-serde-derive" ,rust-serde-derive-1))))))
+
 (define-public rust-serde-yaml-0.8
   (package
     (name "rust-serde-yaml")
