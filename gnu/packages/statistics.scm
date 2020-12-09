d0c7b9f718252d4208434437e5e36666d2d881f8
Ricardo Wurmus <rekado@elephly.net>
2020-12-09 22:01:36 +0100

    gnu: r-lme4: Update to 1.1-26.
    * gnu/packages/statistics.scm (r-lme4): Update to 1.1-26.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -5161,14 +5161,14 @@ algorithms.")
 (define-public r-lme4
   (package
     (name "r-lme4")
-    (version "1.1-25")
+    (version "1.1-26")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "lme4" version))
        (sha256
         (base32
-         "0vvjac7hnn5lfbyl92zi8qxyc8kkjcwsg94z0p2s2q311in4gnp4"))))
+         "0ls2krph4pfjn45vf9fn3ln3x98s85wxrkvm5pnxyx55n1pnsjrn"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-boot" ,r-boot)
