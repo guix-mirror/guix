92d99bf2c4bb65bae72a93f70c9e32eda9c85075
Leo Famulari <leo@famulari.name>
2020-12-12 00:36:23 -0500

    gnu: linux-libre: Update to 5.9.14.
    * gnu/packages/linux.scm (linux-libre-5.9-version): Update to 5.9.14.
    (linux-libre-5.9-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -352,7 +352,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 
 ;; The current "stable" kernel. That is, the most recently released major
 ;; version.
-(define-public linux-libre-5.9-version "5.9.13")
+(define-public linux-libre-5.9-version "5.9.14")
 (define deblob-scripts-5.9
   (linux-libre-deblob-scripts
    linux-libre-5.9-version
@@ -360,7 +360,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0yb04a4j2wq3mwvks3cj7kcm2pscmfs29lrz3falkxpbvjxbbgq2")))
 (define-public linux-libre-5.9-pristine-source
   (let ((version linux-libre-5.9-version)
-        (hash (base32 "0ancdbfmrhq7wnvzrrnayiw8j4k1pjjnmvjxkl12lal7bjdahdml")))
+        (hash (base32 "0jbb3rzbkh0l75zq9bnc60w55ryvrvcg7vw85fsbcwfzvi0zpz1r")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.9)))
