19c782b3cab617f6d628c342308632e1dde44a22
Andreas Enge <andreas@enge.fr>
2020-12-13 15:07:40 +0100

    gnu: gp2c: Update to 0.0.12.
    * gnu/packages/algebra.scm (gp2c): Update to 0.0.12.

--- gnu/packages/algebra.scm
+++ gnu/packages/algebra.scm
@@ -276,7 +276,7 @@ PARI is also available as a C library to allow for faster computations.")
 (define-public gp2c
   (package
    (name "gp2c")
-   (version "0.0.11pl4")
+   (version "0.0.12")
    (source (origin
             (method url-fetch)
             (uri (string-append
@@ -284,7 +284,7 @@ PARI is also available as a C library to allow for faster computations.")
                   version ".tar.gz"))
             (sha256
               (base32
-                "1cnnh7diqc97q76q5pyhpbljbhc0sz8mlrbqgiwi0sjkgh8iqsj0"))))
+                "039ip7qkwwv46wrcdrz7y12m30kazzkjr44kqbc0h137g4wzd7zf"))))
    (build-system gnu-build-system)
    (native-inputs `(("perl" ,perl)))
    (inputs `(("pari-gp" ,pari-gp)))
