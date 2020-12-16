aa02c6f6d789621cb945e1b5f0c35c1e99951c16
Tobias Geerinckx-Rice <me@tobias.gr>
2020-12-16 23:32:45 +0100

    gnu: bind: Update to 9.16.10.
    * gnu/packages/dns.scm (isc-bind): Update to 9.16.10.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -317,7 +317,7 @@ and BOOTP/TFTP for network booting of diskless machines.")
   (package
     (name "bind")
     ;; When updating, check whether isc-dhcp's bundled copy should be as well.
-    (version "9.16.9")
+    (version "9.16.10")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -325,7 +325,7 @@ and BOOTP/TFTP for network booting of diskless machines.")
                     "/bind-" version ".tar.xz"))
               (sha256
                (base32
-                "0nja60jxb8d2m5fi6xd55xjpfkzmxhjsmbxqphy6x91qsz295cmw"))))
+                "1cv26gzbyk3ahidr1fip0pgj28s7l52cafdqpykfc1b2kh0zqixw"))))
     (build-system gnu-build-system)
     (outputs `("out" "utils"))
     (inputs
