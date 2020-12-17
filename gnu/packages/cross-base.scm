4c6824a8182f8c46a15bb0d0bf5068ab9f2f5639
Ludovic Courtès <ludo@gnu.org>
2020-12-17 18:47:41 +0100

    gnu: cross-base: Remove unused Hurd patches from 'inputs'.
    * gnu/packages/cross-base.scm (cross-libc)[inputs]: Remove.  They have
    been unused since 6690b6d1e7333137e4731ec5e4bb30d6637c3237.

--- gnu/packages/cross-base.scm
+++ gnu/packages/cross-base.scm
@@ -534,16 +534,6 @@ and the cross tool chain."
         ;; "kernel-headers" input to point to the right thing.
         (propagated-inputs `(("kernel-headers" ,xheaders)))
 
-        ;; FIXME: 'static-bash' should really be an input, not a native input, but
-        ;; to do that will require building an intermediate cross libc.
-        (inputs (if (hurd-triplet? target)
-                    `(;; TODO: move to glibc in the next rebuild cycle
-                      ("hurd-mach-print.patch"
-                       ,(search-patch "glibc-hurd-mach-print.patch"))
-                      ("hurd-gettyent.patch"
-                       ,(search-patch "glibc-hurd-gettyent.patch")))
-                    '()))
-
         (native-inputs `(("cross-gcc" ,xgcc)
                          ("cross-binutils" ,xbinutils)
                          ,@(package-inputs libc)  ;FIXME: static-bash
