7b9967176cb3a78e0192f0d6b08950ed19cf5cf9
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-12-21 00:35:10 +0100

    gnu: Add rust-blocking-1.
    * gnu/packages/crates-io.scm (rust-blocking-1): New variable.
    (rust-blocking-0.6): Inherit from above.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -3086,8 +3086,36 @@ algorithms.  This package is deprecated.  Please use block-cipher instead")
      `(#:cargo-inputs
        (("rust-byte-tools" ,rust-byte-tools-0.3))))))
 
+(define-public rust-blocking-1
+  (package
+    (name "rust-blocking")
+    (version "1.0.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "blocking" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1s9myg9gqmwzrbc38p23bh4vkc8w4pbpddqrcrrl1xz1xpdp1qf5"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-async-channel" ,rust-async-channel-1)
+        ("rust-async-task" ,rust-async-task-4)
+        ("rust-atomic-waker" ,rust-atomic-waker-1)
+        ("rust-fastrand" ,rust-fastrand-1)
+        ("rust-futures-lite" ,rust-futures-lite-1)
+        ("rust-once-cell" ,rust-once-cell-1))))
+    (home-page "https://github.com/stjepang/blocking")
+    (synopsis "Thread pool for isolating blocking I/O in async programs")
+    (description
+     "This package provides a thread pool for isolating blocking I/O in async
+programs.")
+    (license (list license:asl2.0 license:expat))))
+
 (define-public rust-blocking-0.6
   (package
+    (inherit rust-blocking-1)
     (name "rust-blocking")
     (version "0.6.1")
     (source
@@ -3097,7 +3125,6 @@ algorithms.  This package is deprecated.  Please use block-cipher instead")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "13g1xlsd1ardf9wxkj1d03baxfy8f8kvgjz55l6zi1q4jn5f0c1z"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
        (("rust-async-channel" ,rust-async-channel-1)
@@ -3105,13 +3132,7 @@ algorithms.  This package is deprecated.  Please use block-cipher instead")
         ("rust-fastrand" ,rust-fastrand-1)
         ("rust-futures-lite" ,rust-futures-lite-1)
         ("rust-once-cell" ,rust-once-cell-1)
-        ("rust-waker-fn" ,rust-waker-fn-1))))
-    (home-page "https://github.com/stjepang/blocking")
-    (synopsis "Thread pool for isolating blocking I/O in async programs")
-    (description
-     "This package provides a thread pool for isolating blocking I/O in async
-programs.")
-    (license (list license:asl2.0 license:expat))))
+        ("rust-waker-fn" ,rust-waker-fn-1))))))
 
 (define-public rust-bresenham-0.1
   (package
