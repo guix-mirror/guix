guix-mirror/guix

commit ce29031a23104928155b498d552fc4140cc03030

gnu: MariaDB: Update to 10.5.8.
* gnu/packages/databases.scm (mariadb): Update to 10.5.8.
[arguments]: Adjust #:configure-flags to not install tests; remove code for
the same.  Adjust phases for upstream test changes.  Add workaround.

gnu/packages/databases.scm
@@ -898,7 +898,7 @@ Language.")
 (define-public mariadb
   (package
     (name "mariadb")
-    (version "10.5.6")
+    (version "10.5.8")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://downloads.mariadb.com/MariaDB"
@@ -906,7 +906,7 @@ Language.")
                                   version ".tar.gz"))
               (sha256
                (base32
-                "1i257h0zdypdfj5wkg6ck9pxlkph0jvjs92k22pjr6gnx5lxs1gz"))
+                "1s3vfm73911cddjhgpcbkya6nz7ag2zygg56qqzwscn5ybv28j7b"))
               (modules '((guix build utils)))
               (snippet
                '(begin
@@ -956,6 +956,11 @@ Language.")
          "-DDEFAULT_COLLATION=utf8_general_ci"
          "-DMYSQL_DATADIR=/var/lib/mysql"
          "-DMYSQL_UNIX_ADDR=/run/mysqld/mysqld.sock"
+
+         ;; Do not install the tests or benchmark suite.
+         "-DINSTALL_MYSQLTESTDIR=false"
+         "-DINSTALL_SQLBENCHDIR=false"
+
          (string-append "-DCMAKE_INSTALL_PREFIX=" (assoc-ref %outputs "lib"))
          (string-append "-DCMAKE_INSTALL_RPATH=" (assoc-ref %outputs "lib")
                         "/lib")
@@ -1005,7 +1010,14 @@ Language.")
                       "main.explain_non_select"
                       "main.stat_tables"
                       "main.stat_tables_innodb"
+                      "main.upgrade_MDEV-19650"
                       "roles.acl_statistics"
+
+                      ;; FIXME: This test checks various table encodings and
+                      ;; fails because Guix defaults to UTF8 instead of the
+                      ;; upstream default latin1_swedish_ci.  It's not easily
+                      ;; substitutable because several encodings are tested.
+                      "main.sp2"
 
                       ;; This file contains a time bomb which makes it fail after
                       ;; 2030-12-31.  See <https://bugs.gnu.org/34351> for details.
@@ -1077,12 +1089,15 @@ Language.")
                 (("\\$basedir/share/mysql")
                  (string-append lib "/share/mysql")))
 
-              ;; Remove unneeded files for testing.
               (with-directory-excursion lib
-                (for-each delete-file-recursively
-                          '("mysql-test" "sql-bench"))
-                ;; And static libraries.
+                ;; FIXME: Something creates an empty gnu/store/xxx-mariadb/bin
+                ;; directory at the root of the lib output.  It's not present
+                ;; in the installation log.  This started occuring between
+                ;; 10.5.6 and 10.5.8.  How to prevent it?
+                (delete-file-recursively "gnu")
+                ;; Remove static libraries.
                 (for-each delete-file (find-files "lib" "\\.a$")))
+
               (with-directory-excursion out
                 (delete-file "share/man/man1/mysql-test-run.pl.1")
                 ;; Delete huge and unnecessary executables.

commit 704b7861df926240fa730c05a59f7050fa45fec9

gnu: MariaDB: Fix 'mariadb_config' output to refer to correct directories.
This fixes a regression introduced in ce29031a23104928155b498d552fc4140cc03030
where 'mariadb_config' would return libdir and plugindir relative to the
location of the 'mariadb_config' program (which lives in a separate output).

While at it, fix a couple other wrong references, and a another regression
related to test installation.

* gnu/packages/databases.scm (mariadb)[arguments]: Patch "mariadb_config.c.in"
so that it falls back to old behavior.  Patch 'mariadb.pc.in' and
'mariadb_connector_c.cmake' to cope with absolute directory names. In
 #:configure-flags, don't set INSTALL_MYSQLTESTDIR, but delete the installed
tests with a phase.

gnu/packages/databases.scm
@@ -957,8 +957,7 @@ Language.")
          "-DMYSQL_DATADIR=/var/lib/mysql"
          "-DMYSQL_UNIX_ADDR=/run/mysqld/mysqld.sock"
 
-         ;; Do not install the tests or benchmark suite.
-         "-DINSTALL_MYSQLTESTDIR=false"
+         ;; Do not install the benchmark suite.
          "-DINSTALL_SQLBENCHDIR=false"
 
          (string-append "-DCMAKE_INSTALL_PREFIX=" (assoc-ref %outputs "lib"))
@@ -990,14 +989,26 @@ Language.")
              ;; to other variables such as $INSTALL_INCLUDEDIR, which does
              ;; not work when the latter uses an absolute file name.
              (substitute* "libmariadb/mariadb_config/mariadb_config.c.in"
-               (("@CMAKE_INSTALL_PREFIX@/@INSTALL_INCLUDEDIR@")
-                "@INSTALL_INCLUDEDIR@"))
+               (("%s/@INSTALL_INCLUDEDIR@")
+                (string-append "@INSTALL_INCLUDEDIR@"))
+               ;; As of 10.5.8, the mariadb_config program tries to be
+               ;; clever and computes the installation directory relative
+               ;; to /proc/self/exe when running on Linux.  Make it fall
+               ;; back to the old behaviour.
+               (("defined\\(__linux__\\)")
+                "0"))
              (substitute* "libmariadb/mariadb_config/libmariadb.pc.in"
                (("\\$\\{prefix\\}/@INSTALL_INCLUDEDIR@")
                 "@INSTALL_INCLUDEDIR@"))
+             (substitute* "support-files/mariadb.pc.in"
+               (("^(include|bin|script|doc|man)dir=\\$\\{prefix\\}/" _ dir)
+                (string-append dir "dir=")))
              (substitute* "include/CMakeLists.txt"
                (("\\\\\\$\\{CMAKE_INSTALL_PREFIX\\}/\\$\\{INSTALL_INCLUDEDIR\\}")
                 "${INSTALL_INCLUDEDIR}"))
+             (substitute* "cmake/mariadb_connector_c.cmake"
+               (("\\\\\\$\\{CMAKE_INSTALL_PREFIX\\}/\\$\\{INSTALL_BINDIR\\}")
+                "${INSTALL_BINDIR}"))
              #t))
          (add-after 'unpack 'adjust-tests
            (lambda _
@@ -1090,11 +1101,8 @@ Language.")
                  (string-append lib "/share/mysql")))
 
               (with-directory-excursion lib
-                ;; FIXME: Something creates an empty gnu/store/xxx-mariadb/bin
-                ;; directory at the root of the lib output.  It's not present
-                ;; in the installation log.  This started occuring between
-                ;; 10.5.6 and 10.5.8.  How to prevent it?
-                (delete-file-recursively "gnu")
+                ;; Remove tests.
+                (delete-file-recursively "mysql-test")
                 ;; Remove static libraries.
                 (for-each delete-file (find-files "lib" "\\.a$")))
 