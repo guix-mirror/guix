fa23813a5c9b5d3c2cb9659104d62a52f7c950d2
Michael Rohleder <mike@rohleder.de>
2020-12-23 09:03:19 +0100

    gnu: xfce4-terminal: Update to 0.8.10.
    * gnu/packages/xfce.scm (xfce4-terminal): Update to 0.8.10.
    
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/xfce.scm
+++ gnu/packages/xfce.scm
@@ -925,7 +925,7 @@ devices and folders.")
 (define-public xfce4-terminal
   (package
     (name "xfce4-terminal")
-    (version "0.8.9.2")
+    (version "0.8.10")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://archive.xfce.org/src/apps/" name "/"
@@ -933,7 +933,7 @@ devices and folders.")
                                   name "-" version ".tar.bz2"))
               (sha256
                (base32
-                "1szfmvx4gbwcqag7fnlqh96i9cmvs6xm1yrdbnlzh3imdpw3p8lv"))))
+                "1irxyg5vp6vyd9vxdqav6jhchfkmhlqq511386h644p0k30kfcvs"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)
