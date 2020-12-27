f9e1baa8933ea2ed385d0272dbfbe4e22f192bde
Ludovic Courtès <ludo@gnu.org>
2020-12-27 22:13:27 +0100

    gnu: guile-json: Update to 4.4.1.
    * gnu/packages/guile.scm (guile-json-4): Update to 4.4.1.

--- gnu/packages/guile.scm
+++ gnu/packages/guile.scm
@@ -578,14 +578,14 @@ specification.  These are the main features:
   (package
     (inherit guile-json-3)
     (name "guile-json")
-    (version "4.3.2")
+    (version "4.4.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://savannah/guile-json/guile-json-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "0255c7f053z4p9mqzhpxwbfx3y47j9nfvlgnm8xasdclyzmjl9y2"))))))
+                "1xq4f59rdk28xy4sdn6amy07aa19ikrk48iily3kfhwpkbg6v9jj"))))))
 
 (define-public guile2.2-json
   (package-for-guile-2.2 guile-json-4))
