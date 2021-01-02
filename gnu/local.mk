c389899d83ee8e43c66e6e468483389a865a716b
Efraim Flashner <efraim@flashner.co.il>
2021-01-02 20:48:50 +0200

    gnu: Register missing patch.
    This is a follow-up to 402e697a1144788102b16ca8c9ea74607d2c2c3c.
    
    * gnu/local.mk (dist_patch_DATA): Register rust-1.48 patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1595,6 +1595,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/rust-1.19-mrustc.patch			\
   %D%/packages/patches/rust-1.25-accept-more-detailed-gdb-lines.patch \
   %D%/packages/patches/rust-1.45-linker-locale.patch		\
+  %D%/packages/patches/rust-1.48-linker-locale.patch		\
   %D%/packages/patches/rust-bootstrap-stage0-test.patch		\
   %D%/packages/patches/rust-coresimd-doctest.patch		\
   %D%/packages/patches/rust-ndarray-remove-blas-src-dep.patch	\
