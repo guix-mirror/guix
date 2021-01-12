4de576e9b26ea3e634c1cd819600851ee3a5c6d6
Tobias Geerinckx-Rice <me@tobias.gr>
2021-01-12 22:25:04 +0100

    gnu: Fix comment typo.
    * gnu/packages/efi.scm: Purge spurious space.

--- gnu/packages/efi.scm
+++ gnu/packages/efi.scm
@@ -2,7 +2,7 @@
 ;;; Copyright © 2019 Danny Milosavljevic <dannym@scratchpost.org>
 ;;; Copyright © 2019 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2019–2021 Tobias Geerinckx-Rice <me@tobias.gr>
-;;; Copyright ©  2021 Vincent Legoll <vincent.legoll@gmail.com>
+;;; Copyright © 2021 Vincent Legoll <vincent.legoll@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
