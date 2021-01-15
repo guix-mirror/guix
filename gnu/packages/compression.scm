guix-mirror/guix

commit 959a397d220ad49b5193feb84b036220313fb9c7

gnu: zstd: Update to 1.4.8.
* gnu/packages/compression.scm (zstd): Update to 1.4.8.

Signed-off-by: Leo Famulari <leo@famulari.name>

gnu/packages/compression.scm
@@ -1390,14 +1390,14 @@ or junctions, and always follows hard links.")
 (define-public zstd
   (package
     (name "zstd")
-    (version "1.4.5")
+    (version "1.4.8")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://github.com/facebook/zstd/releases/download/"
                            "v" version "/zstd-" version ".tar.gz"))
        (sha256
-        (base32 "17nf0r20360i80689bg5ipxbk2413b2dn3z7wj8byqpiddy1rscq"))))
+        (base32 "094zqsj92iwz8glpgxjw96c43ib7adpjg5nm1082200mrabq4irj"))))
     (build-system gnu-build-system)
     (outputs '("out"                    ;1.2MiB executables and documentation
                "lib"                    ;1.2MiB shared library and headers

commit ff985fad6dc6fe5d5a0785be372bb1aa3630a7d6

gnu: zstd: Add missing slash in 'libzstd.pc'.
Fixes a regression introduced in
959a397d220ad49b5193feb84b036220313fb9c7, whereby directory names in
'libzstd.pc' would lack a slash after $prefix.

* gnu/packages/compression.scm (zstd)[arguments]: In
'adjust-library-locations' phase, add trailing slash for $prefix in .pc
file.

gnu/packages/compression.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012, 2013, 2014, 2015, 2017, 2019, 2020 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012, 2013, 2014, 2015, 2017, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2014, 2015, 2018 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015 Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
@@ -1431,9 +1431,8 @@ or junctions, and always follows hard links.")
                ;; Make sure the pkg-config file refers to the right output.
                (substitute* (string-append shared-libs "/pkgconfig/libzstd.pc")
                  (("^prefix=.*")
-                  (string-append "prefix=" lib "\n")))
-
-               #t))))
+                  ;; Note: The .pc file expects a trailing slash for 'prefix'.
+                  (string-append "prefix=" lib "/\n")))))))
        #:make-flags
        (list ,(string-append "CC=" (cc-for-target))
              (string-append "prefix=" (assoc-ref %outputs "out"))