1470d2dde17833dca55160a29496ac1251576f54
Leo Famulari <leo@famulari.name>
2021-01-19 15:39:27 -0500

    gnu: linux-libre: Update to 5.10.9.
    * gnu/packages/linux.scm (linux-libre-5.10-version): Update to 5.10.9.
    (linux-libre-5.10-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -353,7 +353,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 
 ;; The current "stable" kernels. That is, the most recently released major
 ;; versions that are still supported upstream.
-(define-public linux-libre-5.10-version "5.10.8")
+(define-public linux-libre-5.10-version "5.10.9")
 (define deblob-scripts-5.10
   (linux-libre-deblob-scripts
    linux-libre-5.10-version
@@ -361,7 +361,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0hh27ccqimagr3aij7ygwikxw66y63sqwd0xlf49bhpjd090r9a7")))
 (define-public linux-libre-5.10-pristine-source
   (let ((version linux-libre-5.10-version)
-        (hash (base32 "1v83wm8xbhq1sgn7c84zi7l40vmd9k1gb653b686jp8n4na85z2w")))
+        (hash (base32 "0la7dklpy6xd79fkzavpmlfyrc60kmmwz491msd95dmvv06kwwvz")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.10)))
