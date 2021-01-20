guix-mirror/guix

commit 1d77141f6d5004cf10d1f9ead53855f25a973847

gnu: node: Fix npx shebang.
* gnu/packages/node.scm (node)[arguments]: Add build phase
"patch-node-shebang".

gnu/packages/node.scm
@@ -7,6 +7,7 @@
 ;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2018, 2019, 2020 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2020 Pierre Langlois <pierre.langlois@gmx.com>
+;;; Copyright © 2020 Ricardo Wurmus <rekado@elephly.net>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -170,6 +171,14 @@
                     (target (readlink npm)))
                (with-directory-excursion bindir
                  (patch-shebang target (list bindir))
+                 #t))))
+         (add-after 'install 'patch-node-shebang
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((bindir (string-append (assoc-ref outputs "out")
+                                           "/bin"))
+                    (npx    (readlink (string-append bindir "/npx"))))
+               (with-directory-excursion bindir
+                 (patch-shebang npx (list bindir))
                  #t)))))))
     (native-inputs
      `(("python" ,python-2)

commit 41134f915f523601fa183f180b801a872b428fc3

gnu: libnode: Fix regression in unused phases.
Fixes regression introduced with commit
1d77141f6d5004cf10d1f9ead53855f25a973847.

Reported by Pierre-Antoine Bouttier
<pierre-antoine.bouttier@univ-grenoble-alpes.fr>.

* gnu/packages/node.scm (libnode)[arguments]: Delete phase
"patch-node-shebang".

Signed-off-by: Ricardo Wurmus <rekado@elephly.net>

gnu/packages/node.scm
@@ -8,6 +8,7 @@
 ;;; Copyright © 2018, 2019, 2020 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2020 Pierre Langlois <pierre.langlois@gmx.com>
 ;;; Copyright © 2020 Ricardo Wurmus <rekado@elephly.net>
+;;; Copyright © 2021 Simon Tournier <zimon.toutoune@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -238,4 +239,5 @@ devices.")
         `(cons* "--shared" "--without-npm" ,flags))
        ((#:phases phases '%standard-phases)
         `(modify-phases ,phases
-           (delete 'patch-npm-shebang)))))))
+           (delete 'patch-npm-shebang)
+           (delete 'patch-node-shebang)))))))