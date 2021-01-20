d5ce26c35ee6a2fa3104be2735f27216f4405704
Ricardo Wurmus <rekado@elephly.net>
2021-01-20 17:08:21 +0100

    gnu: r-rmysql: Update to 0.10.21.
    * gnu/packages/cran.scm (r-rmysql): Update to 0.10.21.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -3553,14 +3553,14 @@ dimensioned arrays.")
 (define-public r-rmysql
   (package
     (name "r-rmysql")
-    (version "0.10.20")
+    (version "0.10.21")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "RMySQL" version))
        (sha256
         (base32
-         "0lv9m6zpm8dgv7yixr6xhw379vbq45d7n7gkrmjrppdj8vcih77i"))))
+         "0cbb5ln9iigl816nzvsls9zil5qlfr8yv2aginapqv6n69nz0srs"))))
     (properties `((upstream-name . "RMySQL")))
     (build-system r-build-system)
     (inputs
