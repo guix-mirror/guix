51418c32d95d8188d8877616829f26479f1135c6
Sharlatan Hellseher <sharlatanus@gmail.com>
2021-01-25 17:43:03 +0000

    gnu: Add eye.
    * gnu/packages/astronomy.scm (eye): New variable.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/astronomy.scm
+++ gnu/packages/astronomy.scm
@@ -88,6 +88,29 @@ in FITS files.")
     (license (license:non-copyleft "file://License.txt"
                           "See License.txt in the distribution."))))
 
+(define-public eye
+  (package
+    (name "eye")
+    (version "1.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://www.astromatic.net/download/eye/"
+                           "eye-" version ".tar.gz"))
+       (sha256
+        (base32 "092qhzcbrkcfidbx4bv9wz42w297n80jk7a6kwyi9a3fjfz81d7k"))))
+    (build-system gnu-build-system)
+    (home-page "https://www.astromatic.net/software/eye")
+    (synopsis "Small image feature detector using machine learning")
+    (description
+     "In EyE (Enhance Your Extraction) an artificial neural network connected to
+pixels of a moving window (retina) is trained to associate these input stimuli
+to the corresponding response in one or several output image(s).  The resulting
+filter can be loaded in SExtractor to operate complex, wildly non-linear filters
+on astronomical images.  Typical applications of EyE include adaptive filtering,
+feature detection and cosmetic corrections.")
+    (license license:cecill)))
+
 (define-public wcslib
   (package
     (name "wcslib")
