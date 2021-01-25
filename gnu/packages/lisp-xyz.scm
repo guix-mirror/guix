582bc6a8778b14e8fd2d57aed9c6c96684b1b4db
Sharlatan Hellseher <sharlatanus@gmail.com>
2021-01-25 18:12:44 +0000

    gnu: Add cl-ubiquitous.
    * gnu/packages/lisp-xyz.scm (cl-ubiquitous, ecl-ubiquitous, sbcl-ubiquitous):
      New variables.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/lisp-xyz.scm
+++ gnu/packages/lisp-xyz.scm
@@ -459,6 +459,39 @@ compatible with ANSI-compliant Common Lisp implementations.")
 (define-public ecl-cl-ppcre
   (sbcl-package->ecl-package sbcl-cl-ppcre))
 
+(define-public sbcl-ubiquitous
+  (let ((commit "35eb7bd9e1b3daee1705f6b41260775180cce8af")
+        (revision "1"))
+    (package
+      (name "sbcl-ubiquitous")
+      (version (git-version "2.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/Shinmera/ubiquitous")
+               (commit commit)))
+         (file-name (git-file-name "ubiquitous" version))
+         (sha256
+          (base32 "1xlkaqmjcpkiv2xl2s2pvvrv976dlc846wm16s1lj62iy1315i49"))))
+      (build-system asdf-build-system/sbcl)
+      (inputs
+       `(("bordeaux-threads" ,sbcl-bordeaux-threads)))
+      (home-page "https://shinmera.github.io/ubiquitous/")
+      (synopsis "Application configuration mechanism for Common Lisp")
+      (description
+       "@code{UBIQUITOUS} is a very easy-to-use library for persistent
+configuration storage.  It automatically takes care of finding a suitable place
+to save your data, and provides simple functions to access and modify the data
+within.")
+      (license license:zlib))))
+
+(define-public ecl-ubiquitous
+  (sbcl-package->ecl-package sbcl-ubiquitous))
+
+(define-public cl-ubiquitous
+  (sbcl-package->cl-source-package sbcl-ubiquitous))
+
 (define-public sbcl-uax-15
   (package
     (name "sbcl-uax-15")
