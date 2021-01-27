1aee32a26e1a96dd457fcf62f97f514c7a562475
Sharlatan Hellseher <sharlatanus@gmail.com>
2021-01-27 00:11:51 +0000

    gnu: Add missfits.
    * gnu/packages/astronomy.scm (missfits): New variable.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/astronomy.scm
+++ gnu/packages/astronomy.scm
@@ -421,6 +421,33 @@ standard astronomy libraries:
 @end itemize\n")
       (license license:gpl2+))))
 
+(define-public missfits
+  (package
+    (name "missfits")
+    (version "2.8.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://www.astromatic.net/download/missfits/"
+                           "missfits-" version ".tar.gz"))
+       (sha256
+        (base32 "04jrd7fsvzr14vdmwgj2f6v97gdcfyjyz6jppml3ghr9xh12jxv5"))))
+    (build-system gnu-build-system)
+    (home-page "https://www.astromatic.net/software/missfits")
+    (synopsis "FITS files Maintenance program")
+    (description
+     "MissFITS is a program that performs basic maintenance and packaging tasks
+on FITS files:
+
+@itemize
+@item add/edit FITS header keywords
+@item split/join Multi-Extension-FITS (MEF) files
+@item unpack/pack FITS data-cubes
+@item create/check/update FITS checksums, using R. Seaman's protocol
+      (see http://www.adass.org/adass/proceedings/adass94/seamanr.html)
+@end itemize\n")
+    (license license:gpl3+)))
+
 (define-public xplanet
   (package
     (name "xplanet")
