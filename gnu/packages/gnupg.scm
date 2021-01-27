guix-mirror/guix

commit 5e163ba00969e7ba05897840a1199b967252b4ae

gnu: guile-gcrypt: Add libgcrypt to 'native-inputs'.
* gnu/packages/gnupg.scm (guile-gcrypt)[arguments]: Remove.
[native-inputs]: Add LIBGCRYPT.

gnu/packages/gnupg.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013, 2015, 2018 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2014, 2018 Eric Bavier <bavier@member.fsf.org>
 ;;; Copyright © 2014, 2015, 2016, 2020 Mark H Weaver <mhw@netris.org>
@@ -433,26 +433,13 @@ gpgpme starting with version 1.7.")
                 "0m29fg4pdfifnqqsa437zc5c1bhbfh62mc69ba25ak4x2cla41ll"))
               (file-name (git-file-name name version))))
     (build-system gnu-build-system)
-    (arguments
-     ;; When cross-compiling, the bash script libgcrypt-config provided by
-     ;; libgcrypt must be accessible during configure phase.
-     `(,@(if (%current-target-system)
-             '(#:phases
-               (modify-phases %standard-phases
-                 (add-before 'configure 'add-libgrypt-config
-                   (lambda _
-                     (setenv "PATH" (string-append
-                                     (assoc-ref %build-inputs "libgcrypt")
-                                     "/bin:"
-                                     (getenv "PATH")))
-                     #t))))
-             '())))
     (native-inputs
      `(("pkg-config" ,pkg-config)
        ("autoconf" ,autoconf)
        ("automake" ,automake)
        ("texinfo" ,texinfo)
-       ("guile" ,guile-3.0)))
+       ("guile" ,guile-3.0)
+       ("libgcrypt" ,libgcrypt)))                 ;for 'libgcrypt-config'
     (inputs
      `(("guile" ,guile-3.0)
        ("libgcrypt" ,libgcrypt)))

commit c45a821a63b73e1655314c028315114f34b26417

gnu: guile-gcrypt: Refer to the right libgcrypt when cross-compiling.
Fixes a regression introduced in
5e163ba00969e7ba05897840a1199b967252b4ae where (gcrypt package-config)
would end up referring to the native libgcrypt.so instead of the target
one.

* gnu/packages/gnupg.scm (guile-gcrypt)[arguments]: Reintroduce
'add-libgrypt-config' phase.
[native-inputs]: Remove LIBGCRYPT.

gnu/packages/gnupg.scm
@@ -436,14 +436,24 @@ gpgpme starting with version 1.7.")
     (arguments
      ;; Work around <https://bugs.gnu.org/20272> to achieve reproducible
      ;; builds.
-     '(#:parallel-build? #f))
+     '(#:parallel-build? #f
+
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'configure 'add-libgrypt-config
+           (lambda* (#:key inputs target #:allow-other-keys)
+             (when target
+               ;; When cross-compiling, the bash script 'libgcrypt-config'
+               ;; must be accessible during the configure phase.
+               (setenv "PATH"
+                       (string-append (assoc-ref inputs "libgcrypt")
+                                      "/bin:" (getenv "PATH")))))))))
     (native-inputs
      `(("pkg-config" ,pkg-config)
        ("autoconf" ,autoconf)
        ("automake" ,automake)
        ("texinfo" ,texinfo)
-       ("guile" ,guile-3.0)
-       ("libgcrypt" ,libgcrypt)))                 ;for 'libgcrypt-config'
+       ("guile" ,guile-3.0)))
     (inputs
      `(("guile" ,guile-3.0)
        ("libgcrypt" ,libgcrypt)))