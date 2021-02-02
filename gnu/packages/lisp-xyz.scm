df484455f4a6e2a349705a188b356afefd78bcd5
Sharlatan Hellseher <sharlatanus@gmail.com>
2021-02-02 23:58:57 +0000

    gnu: Add cl-slug.
    * gnu/packages/lisp-xyz.scm (cl-slug, ecl-cl-slug, sbcl-cl-slug): New
      variables.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/lisp-xyz.scm
+++ gnu/packages/lisp-xyz.scm
@@ -6650,6 +6650,42 @@ of C+GObject libraries without the need of writing dedicated bindings.")
 (define-public ecl-cl-gobject-introspection
   (sbcl-package->ecl-package sbcl-cl-gobject-introspection))
 
+(define-public sbcl-cl-slug
+  (let ((commit "ffb229d10f0d3f7f54e706791725225e200bf749")
+        (revision "1"))
+    (package
+      (name "sbcl-cl-slug")
+      (version (git-version "0.4.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/EuAndreh/cl-slug")
+               (commit commit)))
+         (file-name (git-file-name "cl-slug" version))
+         (sha256
+          (base32 "1asdq6xllmsvfw5fky9wblqcx9isac9jrrlkfl7vyxcq1wxrnflx"))))
+      (build-system asdf-build-system/sbcl)
+      (arguments
+       `(#:asd-files '("cl-slug-test.asd" "cl-slug.asd")
+         #:asd-systems '("cl-slug-test" "cl-slug")))
+      (native-inputs
+       `(("prove" ,sbcl-prove)))
+      (inputs
+       `(("ppcre" ,sbcl-cl-ppcre)))
+      (home-page "https://github.com/EuAndreh/cl-slug")
+      (synopsis "Multi-language slug formater")
+      (description
+       "This is a small Common Lisp library to make slugs, mainly for URIs,
+from english and beyond.")
+      (license license:llgpl))))
+
+(define-public ecl-cl-slug
+  (sbcl-package->ecl-package sbcl-cl-slug))
+
+(define-public cl-slug
+  (sbcl-package->cl-source-package sbcl-cl-slug))
+
 (define-public sbcl-string-case
   (let ((commit "718c761e33749e297cd2809c7ba3ade1985c49f7")
         (revision "0"))
