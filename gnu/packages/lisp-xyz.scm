8ff9cc8b9a122623660a0fbe51838f6145a60388
Sharlatan Hellseher <sharlatanus@gmail.com>
2021-02-03 00:10:14 +0000

    gnu: Add cl-printv.
    * gnu/packages/lisp-xyz.scm (cl-printv, ecl-printv, sbcl-printv): New
      variables.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/lisp-xyz.scm
+++ gnu/packages/lisp-xyz.scm
@@ -4731,6 +4731,35 @@ various levels and mix text with expressions.")
 (define-public ecl-log4cl
   (sbcl-package->ecl-package sbcl-log4cl))
 
+(define-public sbcl-printv
+  (let ((commit "646d31978dbbb460fffb160fd65bb2be9a5a434e")
+        (revision "1"))
+    (package
+      (name "sbcl-printv")
+      (version (git-version "0.1.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/danlentz/printv")
+               (commit commit)))
+         (file-name (git-file-name "printv" version))
+         (sha256
+          (base32 "08jvy82abm7qi3wrxh6gvmwg9gy0zzhg4cfqajdwrggbah8mj5a6"))))
+      (build-system asdf-build-system/sbcl)
+      (home-page "https://github.com/danlentz/printv")
+      (synopsis "Common Lisp tracing and debug-logging macro")
+      (description
+       "@code{PRINTV} is a \"batteries-included\" tracing and debug-logging
+macro for Common Lisp.")
+      (license license:asl2.0))))
+
+(define-public ecl-printv
+  (sbcl-package->ecl-package sbcl-printv))
+
+(define-public cl-printv
+  (sbcl-package->cl-source-package sbcl-printv))
+
 (define-public sbcl-verbose
   (let ((commit "c5b7ecd465be61b35af17ef57564697b88397174")
         (revision "1"))
