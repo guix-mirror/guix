102f668acd3487a6b68e65a0d972e1df81cec766
Sharlatan Hellseher <sharlatanus@gmail.com>
2021-02-03 00:15:10 +0000

    gnu: Add cl-trivial-timeout.
    * gnu/packages/lisp-xyz.scm (cl-trivial-timeout, ecl-trivial-timeout,
      sbcl-trivial-timeout): New variables.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/lisp-xyz.scm
+++ gnu/packages/lisp-xyz.scm
@@ -278,6 +278,41 @@ interactive development model in mind.")
 (define-public ecl-fiveam
   (sbcl-package->ecl-package sbcl-fiveam))
 
+(define-public sbcl-trivial-timeout
+  (let ((commit "feb869357f40f5e109570fb40abad215fb370c6c")
+        (revision "1"))
+    (package
+      (name "sbcl-trivial-timeout")
+      (version (git-version "0.1.5" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/gwkkwg/trivial-timeout/")
+               (commit commit)))
+         (file-name (git-file-name "trivial-timeout" version))
+         (sha256
+          (base32 "1kninxwvvih9nhh7a9y8lfgi7pdr76675y1clw4ss17vz8fbim5p"))))
+      (build-system asdf-build-system/sbcl)
+      (native-inputs
+       `(("lift" ,sbcl-lift)))
+      (arguments
+       ;; NOTE: (Sharlatan-20210202T231437+0000): Due to the age of this library
+       ;; tests use some deprecated functionality and keep failing.
+       `(#:tests? #f))
+      (home-page "https://github.com/gwkkwg/trivial-timeout/")
+      (synopsis "Timeout library for Common Lisp")
+      (description
+       "This library provides an OS and implementation independent access to
+timeouts.")
+      (license license:expat))))
+
+(define-public ecl-trivial-timeout
+  (sbcl-package->ecl-package sbcl-trivial-timeout))
+
+(define-public cl-trivial-timeout
+  (sbcl-package->cl-source-package sbcl-trivial-timeout))
+
 (define-public sbcl-bordeaux-threads
   (package
     (name "sbcl-bordeaux-threads")
