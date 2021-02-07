6d4d3d89486f1be1fffd2a1c01b04f631ac40186
Tobias Geerinckx-Rice <me@tobias.gr>
2021-02-07 23:00:37 +0100

    gnu: s6-dns: Update to 2.3.5.0.
    * gnu/packages/skarnet.scm (s6-dns): Update to 2.3.5.0.

--- gnu/packages/skarnet.scm
+++ gnu/packages/skarnet.scm
@@ -150,14 +150,14 @@ functionality with a very small amount of code.")))
 (define-public s6-dns
   (package
    (name "s6-dns")
-   (version "2.3.4.0")
+   (version "2.3.5.0")
    (source
     (origin
      (method url-fetch)
      (uri (string-append "https://skarnet.org/software/s6-dns/s6-dns-"
                          version ".tar.gz"))
      (sha256
-      (base32 "03s94bc1y2yf0i3v3gh27xz3l2ycf246nb29mrf1zm4v9kdff5fn"))))
+      (base32 "0h5p5dbkkdadahrp4pqhc3x9ds758i6djy49k5zrn7mm5k4722wz"))))
     (build-system gnu-build-system)
     (inputs `(("skalibs" ,skalibs)))
     (arguments
