51c0724f59ae9dbe53a2f4f95e28b3b575a0bf95
Tobias Geerinckx-Rice <me@tobias.gr>
2021-02-07 22:38:53 +0100

    gnu: oil: Update to 0.8.7.
    * gnu/packages/shells.scm (oil): Update to 0.8.7.

--- gnu/packages/shells.scm
+++ gnu/packages/shells.scm
@@ -814,14 +814,14 @@ Shell (pdksh).")
 (define-public oil
   (package
     (name "oil")
-    (version "0.8.6")
+    (version "0.8.7")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://www.oilshell.org/download/oil-"
                            version ".tar.gz"))
        (sha256
-        (base32 "1g7ij3va9rlapfyl3zd08g2iffcr6b0n8b0zrp0bnxwvvnysl95h"))))
+        (base32 "0yxd020jkwhhvq2l8hd9npp2vn6fvm11f0izfzwdl2npbnc976jr"))))
     (build-system gnu-build-system)
     (arguments
      `(#:strip-binaries? #f             ; strip breaks the binary
