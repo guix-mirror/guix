a65c935e29766940148d52b8116634b1e1cbcba6
Vagrant Cascadian <vagrant@debian.org>
2021-02-08 15:57:42 -0800

    gnu: bootloader: Update install-allwinner64-u-boot.
    * gnu/bootloader/u-boot.scm (install-allwinner64-u-boot): Update to
      use a single file to install.

--- gnu/bootloader/u-boot.scm
+++ gnu/bootloader/u-boot.scm
@@ -73,12 +73,9 @@
 
 (define install-allwinner64-u-boot
   #~(lambda (bootloader root-index image)
-      (let ((spl (string-append bootloader "/libexec/spl/sunxi-spl.bin"))
-            (u-boot (string-append bootloader "/libexec/u-boot.itb")))
+      (let ((spl (string-append bootloader "/libexec/u-boot-sunxi-with-spl.fit.itb")))
         (write-file-on-device spl (stat:size (stat spl))
-                              image (* 8 1024))
-        (write-file-on-device u-boot (stat:size (stat u-boot))
-                              image (* 40 1024)))))
+                              image (* 8 1024)))))
 
 (define install-imx-u-boot
   #~(lambda (bootloader root-index image)
