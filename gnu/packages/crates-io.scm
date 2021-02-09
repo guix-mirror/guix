17d1ddd245ac7827b5ad93f491bfefa21210581f
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-09 22:56:22 +0100

    gnu: Add rust-winreg-0.5.
    * gnu/packages/crates-io.scm (rust-winreg-0.5): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -43593,6 +43593,26 @@ API.")
      "This package provides Rust bindings to MS Windows Registry API.")
     (license license:expat)))
 
+(define-public rust-winreg-0.5
+  (package
+    (inherit rust-winreg-0.7)
+    (name "rust-winreg")
+    (version "0.5.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "winreg" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0jkh4jj2g8g0bl7r1xvq9vv9hr4gdzphg9ndqm65q6f1jn9paym2"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-clippy" ,rust-clippy-0.0)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-winapi" ,rust-winapi-0.3))))))
+
 (define-public rust-winutil-0.1
   (package
     (name "rust-winutil")
