2699e634dcdeb3950735716d01e2a6380ff627eb
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-10 00:29:36 +0100

    gnu: Add rust-slice-deque-0.2.
    * gnu/packages/crates-io.scm (rust-slice-deque-0.2): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -35004,6 +35004,30 @@ data type.")
      "Rust FFI bindings to the SLEEF Vectorized Math Library.")
     (license (list license:asl2.0 license:expat))))
 
+(define-public rust-slice-deque-0.2
+  (package
+    (name "rust-slice-deque")
+    (version "0.2.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "slice-deque" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1mq78l0vfwabnyanb85amgzakfhdaxx455yq6cszd5zmynagbpgz"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-libc" ,rust-libc-0.2)
+        ("rust-mach" ,rust-mach-0.2)
+        ("rust-winapi" ,rust-winapi-0.3))))
+    (home-page "https://github.com/gnzlbg/slice_deque")
+    (synopsis "Double-ended queue that Deref's into a slice")
+    (description
+     "This package provides a double-ended queue that Deref's into a slice.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-slog-2
   (package
     (name "rust-slog")
