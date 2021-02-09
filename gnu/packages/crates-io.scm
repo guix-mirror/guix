5570831260ca7466c004b8f45c0565fa114ec748
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-10 00:36:12 +0100

    gnu: Add rust-headers-0.3.
    * gnu/packages/crates-io.scm (rust-headers-0.3): New variable.
    (rust-headers-0.2): Inherit from above.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -15719,8 +15719,37 @@ hash map.")
 their key-value pairs in a user controllable order.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-headers-0.3
+  (package
+    (name "rust-headers")
+    (version "0.3.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "headers" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1hyafm9q5havnbljfdzpkb7wrpx11ayzqzv02abycmklgk2rss32"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-base64" ,rust-base64-0.13)
+        ("rust-bitflags" ,rust-bitflags-1)
+        ("rust-bytes" ,rust-bytes-1)
+        ("rust-headers-core" ,rust-headers-core-0.2)
+        ("rust-http" ,rust-http-0.2)
+        ("rust-mime" ,rust-mime-0.3)
+        ("rust-sha-1" ,rust-sha-1-0.8)
+        ("rust-time" ,rust-time-0.1))))
+    (home-page "https://hyper.rs")
+    (synopsis "Typed HTTP headers")
+    (description "This package provides typed HTTP headers.")
+    (license license:expat)))
+
 (define-public rust-headers-0.2
   (package
+    (inherit rust-headers-0.3)
     (name "rust-headers")
     (version "0.2.3")
     (source
@@ -15730,7 +15759,6 @@ their key-value pairs in a user controllable order.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "0hmnrra00cjqpsn05klnr9cysrv2bm19akxl5lncwcrgfbcafb48"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
        (("rust-base64" ,rust-base64-0.10)
@@ -15740,11 +15768,7 @@ their key-value pairs in a user controllable order.")
         ("rust-http" ,rust-http-0.1)
         ("rust-mime" ,rust-mime-0.3)
         ("rust-sha-1" ,rust-sha-1-0.8)
-        ("rust-time" ,rust-time-0.1))))
-    (home-page "https://hyper.rs")
-    (synopsis "typed HTTP headers")
-    (description "This package provides typed HTTP headers.")
-    (license license:expat)))
+        ("rust-time" ,rust-time-0.1))))))
 
 (define-public rust-headers-core-0.2
   (package
