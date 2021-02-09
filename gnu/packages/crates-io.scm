28e8df3a0373004bcdb066e349fd4beca7cbbea6
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-09 11:31:21 +0100

    gnu: Add rust-nipper-0.1.
    * gnu/packages/crates-io.scm (rust-nipper-0.1): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -21579,6 +21579,34 @@ cryptographic library.")
 release (fork of debug_unreachable)")
     (license license:expat)))
 
+(define-public rust-nipper-0.1
+  (package
+    (name "rust-nipper")
+    (version "0.1.8")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "nipper" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0l003qvzivrqq2lj2cdn4q5x0fg6xczld13m41x492x0g91hwvap"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-cssparser" ,rust-cssparser-0.27)
+        ("rust-html5ever" ,rust-html5ever-0.25)
+        ("rust-markup5ever" ,rust-markup5ever-0.10)
+        ("rust-selectors" ,rust-selectors-0.22)
+        ("rust-tendril" ,rust-tendril-0.4))))
+    (home-page "https://github.com/importcjj/nipper")
+    (synopsis "HTML manipulation with CSS selectors")
+    (description
+     "Nipper is based on HTML crate html5ever and the CSS selector crate
+selectors.  You can use the jQuery-like syntax to query and manipulate an HTML
+document quickly.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-nix-0.19
   (package
     (name "rust-nix")
