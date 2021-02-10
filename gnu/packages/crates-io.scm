50a135cfb1c41e56c451e9b675e530bab4625ebf
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-10 11:02:20 +0100

    gnu: Add rust-aes-gcm-0.5.
    * gnu/packages/crates-io.scm (rust-aes-gcm-0.5): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -1003,6 +1003,28 @@ Data (AEAD) Cipher with optional architecture-specific hardware
 acceleration.")
     (license (list license:asl2.0 license:expat))))
 
+(define-public rust-aes-gcm-0.5
+  (package
+    (inherit rust-aes-gcm-0.6)
+    (name "rust-aes-gcm")
+    (version "0.5.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "aes-gcm" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0f66b5bmyj38r1hj55wzamlzw3y1aql34lgwr2vxn93073d6njl3"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-aead" ,rust-aead-0.2)
+        ("rust-aes" ,rust-aes-0.3)
+        ("rust-block-cipher-trait" ,rust-block-cipher-trait-0.6)
+        ("rust-ghash" ,rust-ghash-0.2)
+        ("rust-subtle" ,rust-subtle-2)
+        ("rust-zeroize" ,rust-zeroize-1))))))
+
 (define-public rust-aes-soft-0.4
   (package
     (name "rust-aes-soft")
