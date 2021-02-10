4cb8a06981099a3da194de649da187dcf109d28f
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-10 17:55:37 +0100

    gnu: Add rust-calamine-0.16.
    * gnu/packages/crates-io.scm (rust-calamine-0.16): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -5883,6 +5883,36 @@ cached data.")
 directories and @code{CACHEDIR.TAG} files.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-calamine-0.16
+  (package
+    (name "rust-calamine")
+    (version "0.16.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "calamine" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "03nl0n79rbq5x0crgg59brxr86hc9k5kjbf3h7d5c8g5mzlkxf9n"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-byteorder" ,rust-byteorder-1)
+        ("rust-chrono" ,rust-chrono-0.4)
+        ("rust-codepage" ,rust-codepage-0.1)
+        ("rust-encoding-rs" ,rust-encoding-rs-0.8)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-quick-xml" ,rust-quick-xml-0.19)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-zip" ,rust-zip-0.5))))
+    (home-page "https://github.com/tafia/calamine")
+    (synopsis "Reader and deserializer for spreadsheet files")
+    (description
+     "@code{calamine} is a pure Rust library to read and deserialize any
+spreadsheet file.")
+    (license license:expat)))
+
 (define-public rust-calloop-0.6
   (package
     (name "rust-calloop")
