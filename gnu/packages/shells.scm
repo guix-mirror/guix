be46f51e3c48661e792e2908314be67fdce66cd0
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-11 00:15:37 +0100

    gnu: Add rust-nu-errors-0.26.
    * gnu/packages/shells.scm (rust-nu-errors-0.26): New variable.

--- gnu/packages/shells.scm
+++ gnu/packages/shells.scm
@@ -930,6 +930,39 @@ Underpinning these utilities are many Scheme interfaces for manipulating
 files and text.")
     (license gpl3+)))
 
+(define-public rust-nu-errors-0.26
+  (package
+    (name "rust-nu-errors")
+    (version "0.26.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "nu-errors" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0q6cbb02m9cfvjc7fcfzj814iz9ab2j2m3c3qwqskkrig4cwz3sl"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-ansi-term" ,rust-ansi-term-0.12)
+        ("rust-bigdecimal" ,rust-bigdecimal-0.2)
+        ("rust-codespan-reporting" ,rust-codespan-reporting-0.11)
+        ("rust-derive-new" ,rust-derive-new-0.5)
+        ("rust-getset" ,rust-getset-0.1)
+        ("rust-glob" ,rust-glob-0.3)
+        ("rust-nu-source" ,rust-nu-source-0.26)
+        ("rust-num-bigint" ,rust-num-bigint-0.3)
+        ("rust-num-traits" ,rust-num-traits-0.2)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-serde-yaml" ,rust-serde-yaml-0.8)
+        ("rust-toml" ,rust-toml-0.5))))
+    (home-page "https://www.nushell.sh")
+    (synopsis "Core error subsystem for Nushell")
+    (description "Core error subsystem for Nushell")
+    (license expat)))
+
 (define-public rust-nu-source-0.26
   (package
     (name "rust-nu-source")
