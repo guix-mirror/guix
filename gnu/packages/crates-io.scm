7b5ba8e50bded0a2af98cb675c31d2679a810f40
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-10 10:54:21 +0100

    gnu: Add rust-hkdf-0.8.
    * gnu/packages/crates-io.scm (rust-hkdf-0.8): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -16118,6 +16118,24 @@ compile time.")
 Derivation Function (HKDF).")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-hkdf-0.8
+  (package
+    (inherit rust-hkdf-0.9)
+    (name "rust-hkdf")
+    (version "0.8.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "hkdf" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1qzsmqrvcmgnrb109qr2mvsmr5c4psm1702vrpcqnj02c408m81z"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-digest" ,rust-digest-0.8)
+        ("rust-hmac" ,rust-hmac-0.7))))))
+
 (define-public rust-hmac-0.8
   (package
     (name "rust-hmac")
