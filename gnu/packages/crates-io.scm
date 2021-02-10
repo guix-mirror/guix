144c7e508ad1750654828024daa4d9e3f68b95d1
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-10 17:47:44 +0100

    gnu: Add rust-rust-decimal-0.10.
    * gnu/packages/crates-io.scm (rust-rust-decimal-0.10): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -32351,6 +32351,33 @@ password hashing function.")
  Currently the conversion uses the Bitcoin base58 alphabet.")
     (license (list license:asl2.0 license:expat))))
 
+(define-public rust-rust-decimal-0.10
+  (package
+    (name "rust-rust-decimal")
+    (version "0.10.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "rust_decimal" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1h80xz88f07ifb4i0mfh9j9p67afiy4xm6xswkk9gnf1spirag59"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-byteorder" ,rust-byteorder-1)
+        ("rust-lazy-static" ,rust-lazy-static-1)
+        ("rust-num" ,rust-num-0.2)
+        ("rust-postgres" ,rust-postgres-0.15)
+        ("rust-serde" ,rust-serde-1))))
+    (home-page "https://github.com/paupino/rust-decimal")
+    (synopsis "Decimal implementation suitable for financial calculations")
+    (description
+     "This package provides a decimal implementation written in pure Rust
+suitable for financial calculations.")
+    (license license:expat)))
+
 (define-public rust-rust-embed-5
   (package
     (name "rust-rust-embed")
