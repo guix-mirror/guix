da4c7ba9263d11e3ee1b4e9032e976fccb97b517
Leo Famulari <leo@famulari.name>
2021-02-12 17:09:04 -0500

    gnu: ncmpc: Update to 0.45.
    * gnu/packages/mpd.scm (ncmpc): Update to 0.45.

--- gnu/packages/mpd.scm
+++ gnu/packages/mpd.scm
@@ -196,7 +196,7 @@ player daemon.")
 (define-public ncmpc
   (package
     (name "ncmpc")
-    (version "0.44")
+    (version "0.45")
     (source (origin
               (method url-fetch)
               (uri
@@ -205,7 +205,7 @@ player daemon.")
                               "/ncmpc-" version ".tar.xz"))
               (sha256
                (base32
-                "135m1rlhc2c2xmz03h712lg1mzhczlj4gw33x31mxmajw3whxkz9"))))
+                "11gpy6kd7xr8x7f7gwdwfryxyc58cd135ds28gnz40p08xj49zqp"))))
     (build-system meson-build-system)
     (arguments
      `(#:configure-flags
