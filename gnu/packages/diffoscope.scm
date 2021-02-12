f813750a4aa07797e0120babdd5efbe17f1d3911
Vagrant Cascadian <vagrant@reproducible-builds.org>
2021-02-11 18:57:38 -0800

    gnu: diffoscope: Update to 166.
    * gnu/packages/diffoscope.scm (diffoscope): Update to 166.
      [source]: Add patch to use magic.open compatibility interface.
    * gnu/packages/patches/diffoscope-revert-to-magic-open.patch: New file.
    * gnu/local.mk [dist_patch_DATA]: New patch.

--- gnu/packages/diffoscope.scm
+++ gnu/packages/diffoscope.scm
@@ -72,7 +72,7 @@
 (define-public diffoscope
   (package
     (name "diffoscope")
-    (version "165")
+    (version "166")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -81,7 +81,9 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0chi8drkxdwk7mlfgljij0nihnxp9pi5ybhqaq8rc4l1zl6srirb"))))
+                "0vc4a38ii6b10af4c7cxfkvj4lk4ihx1xs4q5lshnkyg74gmm21b"))
+              (patches (search-patches
+                        "diffoscope-revert-to-magic-open.patch"))))
     (build-system python-build-system)
     (arguments
      `(#:phases (modify-phases %standard-phases
