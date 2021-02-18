guix-mirror/guix

commit 41abf3b3a0fabe06817f3940cba778974ce79c46

gnu: emacs-find-file-in-project: Update to 6.0.0.
* gnu/packages/emacs-xyz.scm (emacs-find-file-in-project): Update to 6.0.0.

gnu/packages/emacs-xyz.scm
@@ -8974,7 +8974,7 @@ maximizes flexibility (at the expense of conciseness).")
 (define-public emacs-find-file-in-project
   (package
     (name "emacs-find-file-in-project")
-    (version "5.7.10")
+    (version "6.0.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -8983,7 +8983,7 @@ maximizes flexibility (at the expense of conciseness).")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "156sfd5xs831sdlkf7p5m20hzznv1zlf9nm3pfm8n8ixp13fnzj0"))))
+                "1iagywiihwv96y9p811xlx4cmbsj8h76niymprv1vm4fj6cmihr6"))))
     (build-system emacs-build-system)
     (arguments
      `(#:tests? #t

commit 5fccdbc079402eee345684bcbd1b1d0306cab1e8

gnu: emacs-elpy: Fix build.
Fixes <https://issues.guix.gnu.org/46516>, which was caused by commit
41abf3b3a0fabe06817f3940cba778974ce79c46.

* gnu/packages/emacs-xyz.scm (emacs-elpy)[phases]
{patch-ffip-project-search-call}: New phase.

Reported-by: Simon Streit <simon@netpanic.org> and others.

gnu/packages/emacs-xyz.scm
@@ -9166,6 +9166,15 @@ indentation guides in Emacs:
      `(#:include (cons* "^elpy/[^/]+\\.py$" "^snippets\\/" %default-include)
        #:phases
        (modify-phases %standard-phases
+         (add-after 'unpack 'patch-ffip-project-search-call
+           (lambda _
+             ;; Since version 6.0.0 of find-file-in-project,
+             ;; ffip-project-search doesn't accept a third argument anymore
+             ;; (see: https://github.com/jorgenschaefer/elpy/issues/1889).
+             (substitute* "elpy.el"
+               (("\\((ffip-project-search nil nil) project-root\\)" _ signature)
+                (format #f "(let ((ffip-project-root project-root)) (~a))"
+                        signature)))))
          ;; The default environment of the RPC uses Virtualenv to install
          ;; Python dependencies from PyPI.  We don't want/need this in Guix.
          (add-before 'check 'do-not-use-virtualenv