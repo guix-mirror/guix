f601cdc6981888f55882c942dab413cb783caecc
Gerd Heber <gerd.heber@gmail.com>
2021-02-21 12:57:51 -0600

    gnu: Add HDF5 1.12.0.
    * gnu/packages/maths.scm (hdf5-1.12): New variable.
    
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -42,6 +42,7 @@
 ;;; Copyright © 2020 Vinicius Monego <monego@posteo.net>
 ;;; Copyright © 2020 Simon Tournier <zimon.toutoune@gmail.com>
 ;;; Copyright © 2020 Martin Becze <mjbecze@riseup.net>
+;;; Copyright © 2021 Gerd Heber <gerd.heber@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -1217,6 +1218,25 @@ extremely large and complex data collections.")
                                  "/src/hdf5-" version ".tar.bz2")))
        (sha256
         (base32 "0pm5xxry55i0h7wmvc7svzdaa90rnk7h78rrjmnlkz2ygsn8y082"))
+       (patches (search-patches "hdf5-config-date.patch"))))))
+
+(define-public hdf5-1.12
+  (package/inherit hdf5-1.8
+    (version "1.12.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (list (string-append "https://support.hdfgroup.org/ftp/HDF5/releases/"
+                                 "hdf5-" (version-major+minor version)
+                                 "/hdf5-" version "/src/hdf5-"
+                                 version ".tar.bz2")
+                  (string-append "https://support.hdfgroup.org/ftp/HDF5/"
+                                 "current"
+                                 (apply string-append
+                                        (take (string-split version #\.) 2))
+                                 "/src/hdf5-" version ".tar.bz2")))
+       (sha256
+        (base32 "0qazfslkqbmzg495jafpvqp0khws3jkxa0z7rph9qvhacil6544p"))
        (patches (search-patches "hdf5-config-date.patch"))))))
 
 (define-public hdf5
