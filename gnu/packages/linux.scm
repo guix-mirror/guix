7c15b9cfd23031a53ca60b5710254ad1ad762b8d
Leo Famulari <leo@famulari.name>
2021-02-26 14:42:46 -0500

    gnu: linux-libre-with-bpf: Update to 5.11.2.
    * gnu/packages/linux.scm (linux-libre-with-bpf): Update to 5.11.2.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -1066,8 +1066,8 @@ It has been modified to remove all non-free binary blobs.")
 (define-public linux-libre-with-bpf
   (let ((base-linux-libre
          (make-linux-libre*
-          linux-libre-5.10-version
-          linux-libre-5.10-source
+          linux-libre-5.11-version
+          linux-libre-5.11-source
           '("x86_64-linux" "i686-linux" "armhf-linux"
             "aarch64-linux" "riscv64-linux")
           #:extra-version "bpf"
