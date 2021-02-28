bebfe06776c7848838dd8c286cf0e30c9f0a0f63
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-28 11:02:20 +0100

    gnu: elpa: Use HTTPS home page URI.
    * gnu/packages/maths.scm (elpa)[home-page]: Use HTTPS URI.

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -30,7 +30,7 @@
 ;;; Copyright © 2018 Eric Brown <brown@fastmail.com>
 ;;; Copyright © 2018 Julien Lepiller <julien@lepiller.eu>
 ;;; Copyright © 2018 Amin Bandali <bandali@gnu.org>
-;;; Copyright © 2019 Nicolas Goaziou <mail@nicolasgoaziou.fr>
+;;; Copyright © 2019, 2021 Nicolas Goaziou <mail@nicolasgoaziou.fr>
 ;;; Copyright © 2019 Steve Sprang <scs@stevesprang.com>
 ;;; Copyright © 2019 Robert Smith <robertsmith@posteo.net>
 ;;; Copyright © 2020 Jakub Kądziołka <kuba@kadziolka.net>
@@ -4870,7 +4870,7 @@ theories} (SMT) solver.  It provides a C/C++ API, as well as Python bindings.")
                ;; Test scripts are generated, patch the shebang
                (("#!/bin/bash") (string-append "#!" (which "sh"))))
              #t)))))
-    (home-page "http://elpa.mpcdf.mpg.de")
+    (home-page "https://elpa.mpcdf.mpg.de")
     (synopsis "Eigenvalue solvers for symmetric matrices")
     (description
      "The ELPA library provides efficient and scalable direct eigensolvers for
