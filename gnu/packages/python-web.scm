b0a869ff80afbc03669dfac663caccea540ffcec
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-28 11:25:12 +0100

    gnu: python-jose: Use HTTPS home page URI.
    * gnu/packages/python-web.scm (python-jose)[home-page]: Use HTTPS URI.

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -24,7 +24,7 @@
 ;;; Copyright © 2015, 2016 David Thompson <davet@gnu.org>
 ;;; Copyright © 2017 Mark Meyer <mark@ofosos.org>
 ;;; Copyright © 2018 Tomáš Čech <sleep_walker@gnu.org>
-;;; Copyright © 2018, 2019 Nicolas Goaziou <mail@nicolasgoaziou.fr>
+;;; Copyright © 2018, 2019, 2021 Nicolas Goaziou <mail@nicolasgoaziou.fr>
 ;;; Copyright © 2018 Mathieu Othacehe <m.othacehe@gmail.com>
 ;;; Copyright © 2018, 2020, 2021 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;; Copyright © 2019 Vagrant Cascadian <vagrant@debian.org>
@@ -655,7 +655,7 @@ Swartz.")
   (package
     (name "python-jose")
     (version "3.2.0")
-    (home-page "http://github.com/mpdavis/python-jose")
+    (home-page "https://github.com/mpdavis/python-jose")
     (source (origin
               (method git-fetch)
               (uri (git-reference (url home-page) (commit version)))
