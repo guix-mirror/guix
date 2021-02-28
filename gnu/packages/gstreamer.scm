ba8a246baa388bdb61a2b94fc51dc5f97c055334
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-28 11:08:02 +0100

    gnu: gst123: Use HTTPS home page URI.
    * gnu/packages/gstreamer.scm (gst123)[home-page]: Use HTTPS URI.

--- gnu/packages/gstreamer.scm
+++ gnu/packages/gstreamer.scm
@@ -989,7 +989,7 @@ be used by Python applications using GStreamer.")
        ("gst-plugins-base" ,gst-plugins-base)))
     (native-inputs
      `(("pkg-config" ,pkg-config)))
-    (home-page "http://space.twc.de/~stefan/gst123.php")
+    (home-page "https://space.twc.de/~stefan/gst123.php")
     (synopsis "Flexible command line media player based on gstreamer")
     (description "The program gst123 is designed to be a more flexible command
 line player in the spirit of ogg123 and mpg123, based on the gstreamer media
