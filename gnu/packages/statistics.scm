0cdb622c9c0fda129f8e8eb0b06f42ad2fde1e53
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-02-28 11:40:22 +0100

    gnu: r-biased-urn: Use HTTPS home page URI.
    * gnu/packages/statistics.scm (r-biased-urn)[home-page]: Use HTTPS URI.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -3883,7 +3883,7 @@ data behind them) can be viewed and modified in a web browser.")
        "13i2lgfnjhlbbm2yxfc2l5hswqw6x03pwba5csjmirv8kpjw4xr3"))))
    (properties `((upstream-name . "BiasedUrn")))
    (build-system r-build-system)
-   (home-page "http://www.agner.org/random/")
+   (home-page "https://www.agner.org/random/")
    (synopsis "Biased urn model distributions")
    (description
     "This package provides statistical models of biased sampling in the form
