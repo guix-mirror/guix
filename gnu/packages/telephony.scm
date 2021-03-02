4bb9bb78c2482627b1afa2cffda68a4427a3b21d
Léo Le Bouter <lle-bout@zaclys.net>
2021-03-03 00:06:33 +0100

    gnu: osip: Update to 5.2.0.
    * gnu/packages/telephony.scm (osip): Update to 5.2.0.

--- gnu/packages/telephony.scm
+++ gnu/packages/telephony.scm
@@ -323,14 +323,14 @@ GNU ccRTP stack and serves as library for other RTP stacks
 (define-public osip
   (package
    (name "osip")
-   (version "5.1.2")
+   (version "5.2.0")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/osip/libosip2-" version ".tar.gz"))
             (patches (search-patches "osip-CVE-2017-7853.patch"))
             (sha256
              (base32
-              "148j1i0zkwf09qdpk3nc5sssj1dvppw7p0n9rgrg8k56447l1h1b"))))
+              "0xdk3cszkzb8nb757gl47slrr13mf6xz43ab4k343fv8llp8pd2g"))))
    (build-system gnu-build-system)
 
    (synopsis "Library implementing SIP (RFC-3261)")
