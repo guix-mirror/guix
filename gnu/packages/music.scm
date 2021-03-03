5c709614028c53a94f60b8d24bd465f1bbbd228a
Tobias Geerinckx-Rice <me@tobias.gr>
2021-03-03 23:17:20 +0100

    gnu: synthv1: Update to 0.9.20.
    * gnu/packages/music.scm (synthv1): Update to 0.9.20.

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -1927,7 +1927,7 @@ users to select LV2 plugins and run them with jalv.")
 (define-public synthv1
   (package
     (name "synthv1")
-    (version "0.9.19")
+    (version "0.9.20")
     (source (origin
               (method url-fetch)
               (uri
@@ -1935,7 +1935,7 @@ users to select LV2 plugins and run them with jalv.")
                               "/synthv1-" version ".tar.gz"))
               (sha256
                (base32
-                "17sizhav01mn07gi812n8wqdcr85290zqg609s18cww2b95dy6mn"))))
+                "1g2i79gkl1j7i49djz6igkbvdmfwxhcg6kx97n63bcqcvvy03rqz"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f))                    ; there are no tests
