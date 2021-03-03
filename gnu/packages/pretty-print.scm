47093ca4cd403d95962c5ba8ce744a1848bbb4e3
Greg Hogan <code@greghogan.com>
2021-03-03 14:57:19 +0000

    gnu: fmt: Update to 7.1.3.
    * gnu/packages/pretty-print.scm (fmt): Update to 7.1.3.
    
    Signed-off-by: Tobias Geerinckx-Rice <me@tobias.gr>

--- gnu/packages/pretty-print.scm
+++ gnu/packages/pretty-print.scm
@@ -7,6 +7,7 @@
 ;;; Copyright © 2019 Meiyo Peng <meiyo@riseup.net>
 ;;; Copyright © 2020 Paul Garlick <pgarlick@tourbillion-technology.com>
 ;;; Copyright © 2020 Vincent Legoll <vincent.legoll@gmail.com>
+;;; Copyright © 2021 Greg Hogan <code@greghogan.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -166,14 +167,14 @@ different programming languages.")
 (define-public fmt
   (package
     (name "fmt")
-    (version "7.0.3")
+    (version "7.1.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://github.com/fmtlib/fmt/releases/download/"
                            version "/fmt-" version ".zip"))
        (sha256
-        (base32 "0kgajl3qsrdyj0csqsz8b8h84kasdy0pnh16byl0y1vlsaddzkyy"))))
+        (base32 "17sc10hfg087z0s774lnn05wwy3bfzmcv7j448p92pr0s02cb62x"))))
     (build-system cmake-build-system)
     (arguments
      '(#:configure-flags '("-DBUILD_SHARED_LIBS=ON")))
