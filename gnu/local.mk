759a526f5fdd49b9136a639269afd77e2599dcb6
Léo Le Bouter <lle-bout@zaclys.net>
2021-03-03 06:39:37 +0100

    gnu: openssh: Update to 8.5p1.
    * gnu/packages/patches/openssh-fix-ssh-copy-id.patch: Remove patch.
    * gnu/local.mk (dist_patch_DATA): Also unregister it.
    * gnu/packages/ssh.scm (openssh): Update to 8.5p1.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1426,7 +1426,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/openfoam-4.1-cleanup.patch			\
   %D%/packages/patches/openjdk-10-idlj-reproducibility.patch	\
   %D%/packages/patches/openmpi-mtl-priorities.patch		\
-  %D%/packages/patches/openssh-fix-ssh-copy-id.patch		\
   %D%/packages/patches/openssh-hurd.patch			\
   %D%/packages/patches/openresolv-restartcmd-guix.patch	\
   %D%/packages/patches/opensles-add-license-file.patch			\
