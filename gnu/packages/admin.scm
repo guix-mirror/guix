48dd1ac030061c25c1d3e7710ebdf297509ed412
Léo Le Bouter <lle-bout@zaclys.net>
2021-03-04 00:21:27 +0100

    gnu: libpcap: Update to 1.10.0.
    * gnu/packages/admin.scm (libpcap): Update to 1.10.0.

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -1198,14 +1198,14 @@ tools: server, client, and relay agent.")
 (define-public libpcap
   (package
     (name "libpcap")
-    (version "1.9.1")
+    (version "1.10.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.tcpdump.org/release/libpcap-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "153h1378diqyc27jjgz6gg5nxmb4ddk006d9xg69nqavgiikflk3"))))
+                "07ibr6zzfh1wk5gqcbnlyh6v0dfmhpfd0fqj5y3yxvzf4ckb84ld"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("bison" ,bison)
