76b6c61609fd8352ce01059307cf1aec85b8d0df
Leo Famulari <leo@famulari.name>
2021-03-05 15:13:20 -0500

    gnu: Python: Absorb grafted replacement.
    * gnu/packages/python.scm (python-3.8)[source]: Add
    'python-3.8-CVE-2021-3177.patch'.
    [replacement]: Remove field.
    (python-3.8/fixed): Remove variable.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -365,7 +365,6 @@ data types.")
 (define-public python-3.8
   (package (inherit python-2)
     (name "python")
-    (replacement python-3.8/fixed)
     (version "3.8.2")
     (source (origin
               (method url-fetch)
@@ -373,6 +372,7 @@ data types.")
                                   version "/Python-" version ".tar.xz"))
               (patches (search-patches
                         "python-CVE-2020-26116.patch"
+                        "python-3.8-CVE-2021-3177.patch"
                         "python-3-fix-tests.patch"
                         "python-3.8-fix-tests.patch"
                         "python-3-deterministic-build-info.patch"
@@ -522,14 +522,6 @@ data types.")
             (files (list (string-append "lib/python"
                                         (version-major+minor version)
                                         "/site-packages"))))))))
-
-(define python-3.8/fixed
-  (package
-    (inherit python-3.8)
-    (source (origin
-              (inherit (package-source python-3.8))
-              (patches (append (search-patches "python-3.8-CVE-2021-3177.patch")
-                               (origin-patches (package-source python-3.8))))))))
 
 (define-public python-3.9
   (package (inherit python-3.8)
