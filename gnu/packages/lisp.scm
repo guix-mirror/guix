ed128a6bbc507de2c4440e66698c638a0cac316b
Pierre Neidhardt <mail@ambrevar.xyz>
2021-03-08 09:31:04 +0100

    gnu: lisp-repl-core-dumper: Update to 0.6.0.
    * gnu/packages/lisp.scm (lisp-repl-core-dumper): Update to 0.6.0.

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -1090,7 +1090,7 @@ assembler, PEG) is less than 1MB.")
 (define-public lisp-repl-core-dumper
   (package
     (name "lisp-repl-core-dumper")
-    (version "0.5.0")
+    (version "0.6.0")
     (source
      (origin
        (method git-fetch)
@@ -1099,7 +1099,7 @@ assembler, PEG) is less than 1MB.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1hrilm9lxy7zdidn4wac4yfqryg3hfw0371nanhd7g9bcfjx7n1q"))))
+        (base32 "0f9rfi8lszm0v35f9vfxxdvwm8fc5909qdsrwj7c2jvl6iaa9n6p"))))
     (build-system copy-build-system)
     (arguments
      '(#:install-plan
