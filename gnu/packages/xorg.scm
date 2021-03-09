guix-mirror/guix

commit 07f77812c44aa3b7a6b963613b783da56110d2f0

gnu: xvfb-run: Add man page licence.
* gnu/packages/xorg.scm (xvfb-run)[license]: Add gpl2+.

gnu/packages/xorg.scm
@@ -6839,4 +6839,5 @@ within a virtual X server environment.  It sets up an X authority file or uses
 an existing user-specified one, writes a cookie to it, and then starts the
 @command{Xvfb} X server as a background process.  It also takes care of killing
 the server and cleaning up before returning the exit status of the command.")
-    (license license:x11)))
+    (license license:x11                ; the script
+             license:gpl2+)))           ; the man page

commit b1cabedd28b92324259875fc52ca5d52d411a026

gnu: xvfb-run: Fix syntax error.
Error introduced in 07f77812c44aa3b7a6b963613b783da56110d2f0.

* gnu/packages/xorg.scm (xvfb-run)[license]: Fix syntax.

gnu/packages/xorg.scm
@@ -6839,5 +6839,5 @@ within a virtual X server environment.  It sets up an X authority file or uses
 an existing user-specified one, writes a cookie to it, and then starts the
 @command{Xvfb} X server as a background process.  It also takes care of killing
 the server and cleaning up before returning the exit status of the command.")
-    (license license:x11                ; the script
-             license:gpl2+)))           ; the man page
+    (license (list license:x11                    ; the script
+                   license:gpl2+))))              ; the man page