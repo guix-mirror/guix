8cc3a16cbb2521a737d9276d4de4fff3b4f2bd17
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-03-10 16:14:53 +0100

    gnu: Add rust-object-0.22.
    * gnu/packages/crates-io.scm (rust-object-0.22): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -25785,6 +25785,29 @@ Foundation framework.")
 file formats.")
     (license (list license:asl2.0 license:expat))))
 
+(define-public rust-object-0.22
+  (package
+    (inherit rust-object-0.23)
+    (name "rust-object")
+    (version "0.22.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "object" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "15r383qxxwq08q3a5rfqhp971wd0nixd9ny22xw37jy31qv66fwd"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-compiler-builtins" ,rust-compiler-builtins-0.1)
+        ("rust-crc32fast" ,rust-crc32fast-1)
+        ("rust-flate2" ,rust-flate2-1)
+        ("rust-indexmap" ,rust-indexmap-1)
+        ("rust-rustc-std-workspace-alloc" ,rust-rustc-std-workspace-alloc-1)
+        ("rust-rustc-std-workspace-core" ,rust-rustc-std-workspace-core-1)
+        ("rust-wasmparser" ,rust-wasmparser-0.57))))))
+
 (define-public rust-object-0.17
   (package
     (inherit rust-object-0.23)
