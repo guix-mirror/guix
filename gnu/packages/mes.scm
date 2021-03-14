7327295462fccae4ee3e9bf74962c82d3445fec8
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-03-14 17:32:01 +0100

    gnu: mes: Remove aarch64-linux from supported-systems.
    * gnu/packages/mes.scm (mes)[supported-systems]: Remove aarch64-linux.
    Thinko: mes only builds on aarch64-linux using --system=armhf-linux.

--- gnu/packages/mes.scm
+++ gnu/packages/mes.scm
@@ -202,8 +202,7 @@ Guile.")
               (sha256
                (base32
                 "0mnryfkl0dwbr5gxp16j5s95gw7z1vm1fqa1pxabp0aiar1hw53s"))))
-    (supported-systems '("armhf-linux" "aarch64-linux"
-                         "i686-linux" "x86_64-linux"))
+    (supported-systems '("armhf-linux" "i686-linux" "x86_64-linux"))
     (propagated-inputs
      `(("mescc-tools" ,mescc-tools)
        ("nyacc" ,nyacc-1.00.2)))
