b3c29d90a637bbfd50ae4de6bd34d8c440a41aa9
Tobias Geerinckx-Rice <me@tobias.gr>
2021-03-21 17:24:23 +0100

    gnu: openresolv: Update to 3.12.0.
    * gnu/packages/dns.scm (openresolv): Update to 3.12.0.

--- gnu/packages/dns.scm
+++ gnu/packages/dns.scm
@@ -1287,14 +1287,14 @@ and TCP-capable recursive DNS server for finding domains on the internet.")
 (define-public openresolv
   (package
     (name "openresolv")
-    (version "3.10.0")
+    (version "3.12.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://roy.marples.name/downloads/openresolv/"
                                   "openresolv-" version ".tar.xz"))
               (sha256
                (base32
-                "01ms6c087la4hk0f0w6n2vpsb7dg4kklah2rqyhz88p0vr9bqy20"))
+                "15qvp5va2yrqpz0ba54clvn8cbc66v4sl7k3bi9ji8jpx040bcs2"))
               (patches
                (search-patches "openresolv-restartcmd-guix.patch"))))
     (build-system gnu-build-system)
