4d2553e7b56eaeae83010f60ea87587860e431c0
Tobias Geerinckx-Rice <me@tobias.gr>
2021-03-22 23:39:10 +0100

    gnu: man-pages: Update to 5.11.
    * gnu/packages/man.scm (man-pages): Update to 5.11.

--- gnu/packages/man.scm
+++ gnu/packages/man.scm
@@ -224,7 +224,7 @@ the traditional flat-text whatis databases.")
 (define-public man-pages
   (package
     (name "man-pages")
-    (version "5.10")
+    (version "5.11")
     (source
      (origin
        (method url-fetch)
@@ -234,7 +234,7 @@ the traditional flat-text whatis databases.")
               (string-append "mirror://kernel.org/linux/docs/man-pages/Archive/"
                              "man-pages-" version ".tar.xz")))
        (sha256
-        (base32 "0ql7fqs0w2nbwv1b6ffnzyjz1sysvkhq8kb77wi2z7qip8sja43m"))))
+        (base32 "1aiwn6yi19idg4jbf7x4x5i06macjv7r8d5fgp1rwnc4a775vniy"))))
     (build-system gnu-build-system)
     (arguments
      '(#:phases (modify-phases %standard-phases (delete 'configure))
