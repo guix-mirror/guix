guix-mirror/guix

commit 0681f05486d901c92637c098bb7afddef35068b0

gnu: gfortran: Remove unusable versions.
The gfortran packages cannot be used without being wrapped with
gfortran-toolchain. However, as the comment in (gnu packages
commencement) explains, there cannot be more than one gfortran-toolchain
package because of ABI issues. Thus, versions of gfortran other than the
main one are useless.

* gnu/packages/gcc.scm (gfortran-4.8, gfortran-4.9, gfortran-5,
  gfortran-6, gfortran-7, gfortran-8, gfortran-9): Remove variables.
  (gfortran): Define the gfortran package based on the gcc variable.

gnu/packages/gcc.scm
@@ -687,47 +687,10 @@ as the 'native-search-paths' field."
          (variable "LIBRARY_PATH")
          (files '("lib" "lib64")))))
 
-(define-public gfortran-4.8
-  (hidden-package
-   (custom-gcc gcc-4.8 "gfortran" '("fortran")
-               %generic-search-paths)))
-
-(define-public gfortran-4.9
-  (hidden-package
-   (custom-gcc gcc-4.9 "gfortran" '("fortran")
-               %generic-search-paths)))
-
-(define-public gfortran-5
-  (hidden-package
-   (custom-gcc gcc-5 "gfortran" '("fortran")
-               %generic-search-paths)))
-
-(define-public gfortran-6
-  (hidden-package
-   (custom-gcc gcc-6 "gfortran" '("fortran")
-               %generic-search-paths)))
-
-(define-public gfortran-7
-  (hidden-package
-   (custom-gcc gcc-7 "gfortran" '("fortran")
-               %generic-search-paths)))
-
-(define-public gfortran-8
-  (hidden-package
-   (custom-gcc gcc-8 "gfortran" '("fortran")
-               %generic-search-paths)))
-
-(define-public gfortran-9
-  (hidden-package
-   (custom-gcc gcc-9 "gfortran" '("fortran")
-               %generic-search-paths)))
-
 (define-public gfortran
-  ;; Note: Update this when GCC changes!  We cannot use
-  ;; (custom-gcc gcc "fortran" …) because that would lead to a package object
-  ;; that is not 'eq?' with GFORTRAN-5, and thus 'fold-packages' would
-  ;; report two gfortran@5 that are in fact identical.
-  gfortran-7)
+  (hidden-package
+   (custom-gcc gcc "gfortran" '("fortran")
+               %generic-search-paths)))
 
 (define-public libgccjit
   (package

commit a172def55ad762b2a9e796531b88da33a74ccce7

gnu: gcc: Fix comment about gfortran.
Added by: cb4805e34c9997edca14743516b9a0fde6303d1c
Fixes: 0681f05486d901c92637c098bb7afddef35068b0

* gnu/packages/gcc.scm (gcc): Fix comment about gfortran.

Signed-off-by: Christopher Baines <mail@cbaines.net>

gnu/packages/gcc.scm
@@ -595,8 +595,7 @@ It also includes runtime support libraries for these languages.")))
                                      "gcc-5.0-libvtv-runpath.patch"))))))
 
 ;; Note: When changing the default gcc version, update
-;;       the gcc-toolchain-* definitions and the gfortran definition
-;;       accordingly.
+;;       the gcc-toolchain-* definitions.
 (define-public gcc gcc-7)
 
 (define-public (make-libstdc++ gcc)