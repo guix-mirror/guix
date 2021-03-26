2e9cdb6a65aa0260f01110d26e83080e898ea0a5
qblade <qblade@protonmail.com>
2021-03-26 01:33:42 +0000

    gnu: Add hikari.
    * gnu/packages/wm.scm (hikari): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -2400,3 +2400,55 @@ for wayland conceptually based on the X11 window manager
      (description "libucl implements a configuration language that is easy to
 read and write, and compatible with JSON.")
     (license license:bsd-2)))
+
+(define-public hikari
+  (package
+    (name "hikari")
+    (version "2.2.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hikari.acmelabs.space/releases/hikari-"
+             version ".tar.gz"))
+       (sha256
+        (base32
+         "1qsd1qb4bn24jh5658gxmfg6hk9p7g235gsbvnjrbfdjqsv8r6yz"))))
+    (build-system gnu-build-system)
+    (native-inputs
+     `(("bmake" ,bmake)
+       ("pkg-config" ,pkg-config)
+       ("wayland-protocols" ,wayland-protocols)))
+    (inputs
+     `(("wayland" ,wayland)
+       ("wlroots" ,wlroots)
+       ("libinput" ,libinput)
+       ("cairo" ,cairo)
+       ("pango" ,pango)
+       ("libxkbcommon" ,libxkbcommon)
+       ("libucl" ,libucl)
+       ("pam" ,linux-pam)))
+    (arguments
+     `(#:tests? #f ; no tests
+       #:make-flags
+       (list
+        (string-append "PREFIX=" (assoc-ref %outputs "out"))
+        (string-append "CC=" ,(cc-for-target))
+        "WITH_XWAYLAND=YES"
+        "WITH_SCREENCOPY=YES"
+        "WITH_LAYERSHELL=YES"
+        "WITH_VIRTUAL_INPUT=YES")
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (replace 'build
+           (lambda* (#:key inputs outputs make-flags #:allow-other-keys)
+             (apply invoke "bmake" make-flags)))
+         (replace 'install
+           (lambda* (#:key inputs outputs make-flags #:allow-other-keys)
+             (apply invoke "bmake" "install" make-flags))))))
+    (home-page "https://hikari.acmelabs.space/")
+    (synopsis "Stacking Wayland compositor with additional tiling capabilities")
+    (description "Stacking Wayland compositor with additional tiling
+capabilities, it is heavily inspired by the Calm Window manager(cwm)")
+    (license license:bsd-2)))
