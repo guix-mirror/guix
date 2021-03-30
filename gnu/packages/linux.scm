38894abe6a53aa235261159e321bac091b79f5fe
Leo Famulari <leo@famulari.name>
2021-03-30 13:11:46 -0400

    gnu: linux-libre 4.9: Update to 4.9.264.
    * gnu/packages/linux.scm (linux-libre-4.9-version): Update to 4.9.264.
    (linux-libre-4.9-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -422,7 +422,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.14)))
 
-(define-public linux-libre-4.9-version "4.9.263")
+(define-public linux-libre-4.9-version "4.9.264")
 (define deblob-scripts-4.9
   (linux-libre-deblob-scripts
    linux-libre-4.9-version
@@ -430,7 +430,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0fxajshb75siq39lj5h8xvhdj8lcmddkslwlyj65rhlwk6g2r4b2")))
 (define-public linux-libre-4.9-pristine-source
   (let ((version linux-libre-4.9-version)
-        (hash (base32 "1dhmgyg6asqg1pmhnzqymwz4bm6gy8gi0n2gr794as38dhn2szwz")))
+        (hash (base32 "1df2dv26c9z6zsdlqzbcc60f2pszh0hx1n94v65jswlb72a2mipc")))
     (make-linux-libre-source version
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.9)))
