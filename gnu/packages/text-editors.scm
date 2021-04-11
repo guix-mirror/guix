9d92819dbaa81fe9c93ab2b8ced7f0ba7f3b59fe
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-04-11 14:42:25 +0200

    gnu: scintilla: Update to 5.0.1.
    * gnu/packages/text-editors.scm (scintilla): Update to 5.0.1.

--- gnu/packages/text-editors.scm
+++ gnu/packages/text-editors.scm
@@ -861,14 +861,14 @@ Octave.  TeXmacs is completely extensible via Guile.")
 (define-public scintilla
   (package
     (name "scintilla")
-    (version "4.4.6")
+    (version "5.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (let ((v (apply string-append (string-split version #\.))))
               (string-append "https://www.scintilla.org/scintilla" v ".tgz")))
        (sha256
-        (base32 "1p62dq2fgdkvdn2clz1xjdj09acv87rbifl67zhlz7skqip31y9d"))))
+        (base32 "0w5550fijkhmzvdydd8770qq9dgnbq1sd0a8rn4g6mwyfpcyhbfy"))))
     (build-system gnu-build-system)
     (arguments
      `(#:make-flags (list "GTK3=1" "CC=gcc" "-Cgtk")
@@ -888,7 +888,8 @@ Octave.  TeXmacs is completely extensible via Guile.")
                          (find-files "include/" "."))
                #t))))))
     (native-inputs
-     `(("pkg-config" ,pkg-config)
+     `(("gcc" ,gcc-9)                   ;Code has C++17 requirements
+       ("pkg-config" ,pkg-config)
        ("python" ,python-wrapper)))
     (inputs
      `(("gtk+" ,gtk+)))
