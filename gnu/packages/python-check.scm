bceca1ff58cbf8d26b701b70c3e129f727d81b8e
Guillaume Le Vaillant <glv@posteo.net>
2021-04-12 09:50:54 +0200

    gnu: Add python-allpairspy.
    * gnu/packages/python-check.scm (python-allpairspy): New variable.

--- gnu/packages/python-check.scm
+++ gnu/packages/python-check.scm
@@ -11,6 +11,7 @@
 ;;; Copyright © 2020 Tanguy Le Carrour <tanguy@bioneland.org>
 ;;; Copyright © 2021 Sharlatan Hellseher <sharlatanus@gmail.com>
 ;;; Copyright © 2021 Brendan Tildesley <mail@brendan.scot>
+;;; Copyright © 2021 Guillaume Le Vaillant <glv@posteo.net>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -1309,4 +1310,27 @@ help in debugging failures and optimizing the scheduler to improve speed.")
     (synopsis "Pytest plugin for Sanic")
     (description "A pytest plugin for Sanic.  It helps you to test your
 code asynchronously.")
+    (license license:expat)))
+
+(define-public python-allpairspy
+  (package
+    (name "python-allpairspy")
+    (version "2.5.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "allpairspy" version))
+       (sha256
+        (base32 "1c987h13dly9919d15w3h747rgn50ilnv7dginhlprxbj564hn4k"))))
+    (build-system python-build-system)
+    (native-inputs
+     `(("python-pytest" ,python-pytest)
+       ("python-pytest-runner" ,python-pytest-runner)))
+    (home-page "https://github.com/thombashi/allpairspy")
+    (synopsis "Pairwise test combinations generator")
+    (description
+     "This is a Python library for test combinations generator.  The generator
+allows one to create a set of tests using @emph{pairwise combinations} method,
+reducing a number of combinations of variables into a lesser set that covers
+most situations.")
     (license license:expat)))
