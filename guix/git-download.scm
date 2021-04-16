guix-mirror/guix

commit c1940fde43c7aca37d67589cc5cb248086d17d56

git-download: Autoload Guile-Git.
* guix/git-download.scm: Autoload (git ...) modules.

guix/git-download.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2017 Mathieu Lirzin <mthl@gnu.org>
 ;;; Copyright © 2017 Christopher Baines <mail@cbaines.net>
 ;;; Copyright © 2020 Jakub Kądziołka <kuba@kadziolka.net>
@@ -27,7 +27,14 @@
   #:use-module (guix packages)
   #:use-module (guix modules)
   #:autoload   (guix build-system gnu) (standard-packages)
-  #:use-module (git)
+  #:autoload   (git repository) (repository-open
+                                 repository-close!
+                                 repository-discover
+                                 repository-head
+                                 repository-working-directory)
+  #:autoload   (git commit)     (commit-lookup commit-tree)
+  #:autoload   (git reference)  (reference-target)
+  #:autoload   (git tree)       (tree-list)
   #:use-module (ice-9 match)
   #:use-module (ice-9 vlist)
   #:use-module (srfi srfi-1)

commit 5b35c9adc899749a0bd96a0e6d2c3bbf88e38963

git-download: Call 'libgit2-init!'.
Fixes <https://bugs.gnu.org/47797>.
Reported by Ingo Ruhnke <grumbel@gmail.com>
and Nicolò Balzarotti <anothersms@gmail.com>.

Regression introduced in c1940fde43c7aca37d67589cc5cb248086d17d56.

* guix/git-download.scm (git-predicate): Add call to 'libgit2-init!'.

guix/git-download.scm
@@ -27,6 +27,7 @@
   #:use-module (guix packages)
   #:use-module (guix modules)
   #:autoload   (guix build-system gnu) (standard-packages)
+  #:autoload   (git bindings)   (libgit2-init!)
   #:autoload   (git repository) (repository-open
                                  repository-close!
                                  repository-discover
@@ -225,6 +226,7 @@ upon Git errors, return #f instead of a predicate.
 
 The returned predicate takes two arguments FILE and STAT where FILE is an
 absolute file name and STAT is the result of 'lstat'."
+  (libgit2-init!)
   (catch 'git-error
     (lambda ()
       (let* ((files  (git-file-list directory))