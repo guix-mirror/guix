164800b2a36c979081295c8f3a0341cf92565049
Tobias Geerinckx-Rice <me@tobias.gr>
2021-04-19 12:37:59 +0200

    gnu: youtube-dl: Update to 2021.04.17.
    * gnu/packages/video.scm (youtube-dl): Update to 2021.04.17.

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -2179,14 +2179,14 @@ To load this plugin, specify the following option when starting mpv:
 (define-public youtube-dl
   (package
     (name "youtube-dl")
-    (version "2021.04.07")
+    (version "2021.04.17")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://youtube-dl.org/downloads/latest/"
                                   "youtube-dl-" version ".tar.gz"))
               (sha256
                (base32
-                "02d51l6gdjr3zhhi7ydf5kzv8dv4jzq0ygja7zb2h9k7hnl0l27m"))
+                "0hhfv9hdfvbxxqsll5bg5166jn20x9na9wbmy0dqww41dadgbvvm"))
               (snippet
                '(begin
                   ;; Delete the pre-generated files, except for the man page
