d5fdd88739c609987c914a01d2a71d6e3935fec3
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-04-30 16:01:55 +0200

    gnu: emacs-xref: Update to 1.1.0.
    * gnu/packages/emacs-xyz.scm (emacs-xref): Update to 1.1.0.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -27594,14 +27594,14 @@ work on alists, hash-table and arrays.  All functions are prefixed with
 (define-public emacs-xref
   (package
     (name "emacs-xref")
-    (version "1.0.4")
+    (version "1.1.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "xref-" version ".el"))
        (sha256
-        (base32 "0hkm59qqlsfw3w9ws9xhpmmz30ylifmh05a00ba58zvv1kz04x1g"))))
+        (base32 "1s7pwk09bry4nqr4bc78a3mbwyrxagai2gpsd49x47czy2x7m3ax"))))
     (build-system emacs-build-system)
     (home-page "http://elpa.gnu.org/packages/xref.html")
     (synopsis "Cross-referencing commands")
