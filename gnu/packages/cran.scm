aca781b9a4e24f8578b83266ebdc662b97071a89
Ricardo Wurmus <rekado@elephly.net>
2021-05-04 07:13:13 +0200

    gnu: r-clue: Update to 0.3-59.
    * gnu/packages/cran.scm (r-clue): Update to 0.3-59.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -17486,14 +17486,14 @@ experiments in a well-organized and reproducible way.")
 (define-public r-clue
   (package
     (name "r-clue")
-    (version "0.3-58")
+    (version "0.3-59")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "clue" version))
        (sha256
         (base32
-         "1rwwxlnpl1k0n4k96vxc9zajw9kb4syqwzj76fvaf0qim8p6ddia"))))
+         "0ychvjbqkcgqdp5gy4hsi4297phywd7fj89y3yi5ckmnmjb39kjm"))))
     (build-system r-build-system)
     (propagated-inputs `(("r-cluster" ,r-cluster)))
     (home-page "https://cran.r-project.org/web/packages/clue/")
