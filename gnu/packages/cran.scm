da20f1f0b271cd672c7d2d93086f166758e69f82
Ricardo Wurmus <rekado@elephly.net>
2021-05-04 07:13:09 +0200

    gnu: r-tmb: Update to 1.7.20.
    * gnu/packages/cran.scm (r-tmb): Update to 1.7.20.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -10394,14 +10394,14 @@ Bayesian modeling.")
 (define-public r-tmb
   (package
     (name "r-tmb")
-    (version "1.7.19")
+    (version "1.7.20")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "TMB" version))
        (sha256
         (base32
-         "0vnjh8d5gnjswil8ld3gbbspy4m5l2pzar1mpj62qxn2mwx0cc4s"))))
+         "14wapc8bicr4j7mmrv1hrsw407lsbr7l4cfvy3j9sfjdbrq31a37"))))
     (properties `((upstream-name . "TMB")))
     (build-system r-build-system)
     (propagated-inputs
