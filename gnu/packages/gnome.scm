8ff0d32df6fb012db0a32bc253f7bcd63a2113d2
Tobias Geerinckx-Rice <me@tobias.gr>
2021-05-04 16:09:03 +0200

    gnu: gthumb: Update to 3.10.3.
    * gnu/packages/gnome.scm (gthumb): Update to 3.10.3.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -10838,7 +10838,7 @@ functionality.")
 (define-public gthumb
   (package
     (name "gthumb")
-    (version "3.10.2")
+    (version "3.10.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/gthumb/"
@@ -10846,7 +10846,7 @@ functionality.")
                                   "gthumb-" version ".tar.xz"))
               (sha256
                (base32
-                "14sw8d37g1lkp44dwsgyxjjsgh5pnpp4wq00mcy9p3rp30lf9spx"))))
+                "04n2sgasc03kiczyzkq362pjilj12hq2r5qj07lynqr9rivkzdys"))))
     (build-system meson-build-system)
     (arguments
      `(#:glib-or-gtk? #t
