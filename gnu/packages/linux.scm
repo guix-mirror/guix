guix-mirror/guix

commit 58805da44dac6b180245edd8a6419343f64d221c

doc: Fix typo and use @acronym for RAS.
* doc/guix.texi (Linux Services): Fix typo and use @acronym for RAS.

doc/guix.texi
@@ -31457,9 +31457,9 @@ parameters, can be done as follow:
 @cindex Platform Reliability, Availability and Serviceability daemon
 @subsubheading Rasdaemon Service
 
-The Rasdaemon service provides a daemon which monitors the platform Reliablity,
-Availability and Serviceability (RAS) reports from the Linux kernel trace
-events, logging them to syslogd.
+The Rasdaemon service provides a daemon which monitors platform
+@acronym{RAS, Reliability, Availability, and Serviceability} reports from
+Linux kernel trace events, logging them to syslogd.
 
 Reliability, Availability and Serviceability is a concept used on servers meant
 to measure their robustness.

commit 1ca0a9a30775b32ced8463f0ae7f200c715047e5

gnu: rasdaemon: Fix Texinfo syntax of description.
Regression introduced in 58805da44dac6b180245edd8a6419343f64d221c.

* gnu/packages/linux.scm (rasdaemon)[description]: Avoid @acronym since
Guile's Texinfo module doesn't currently support @comma{}.
* doc/guix.texi (Linux Services): Use @comma{} within @acronym.

doc/guix.texi
@@ -31452,7 +31452,7 @@ parameters, can be done as follow:
 @subsubheading Rasdaemon Service
 
 The Rasdaemon service provides a daemon which monitors platform
-@acronym{RAS, Reliability, Availability, and Serviceability} reports from
+@acronym{RAS, Reliability@comma{} Availability@comma{} and Serviceability} reports from
 Linux kernel trace events, logging them to syslogd.
 
 Reliability, Availability and Serviceability is a concept used on servers meant

gnu/packages/linux.scm
@@ -8098,8 +8098,8 @@ provides user-space tools for creating EROFS file systems.")
     (home-page "https://github.com/mchehab/rasdaemon")
     (synopsis "Platform Reliability, Availability, and Serviceability tools")
     (description
-     "The @code{rasdaemon} daemon monitors platform @acronym{RAS, Reliability,
-Availability, and Serviceability} reports from Linux kernel trace events.
+     "The @code{rasdaemon} daemon monitors platform Reliability,
+Availability, and Serviceability (RAS) reports from Linux kernel trace events.
 These trace events are logged in @file{/sys/kernel/debug/tracing} and reported
 through standard log mechanisms like syslog.")
     (license license:gpl2)))