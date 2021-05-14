e4beaa7c8a71f4fdfe1948a3d96e5214407b7e49
Jelle Licht <jlicht@fsfe.org>
2021-05-14 22:54:47 +0200

    gnu: sdcc: Build sdcdb debugger.
    * gnu/packages/embedded.scm (sdcc)[arguments]<phases>: Add sdcc-misc target.

--- gnu/packages/embedded.scm
+++ gnu/packages/embedded.scm
@@ -1547,6 +1547,12 @@ and Zilog Z80 families, plus many of their variants.")
            (lambda _
              (substitute* (find-files "." "(\\.mk$|\\.in$)")
                (("/bin/sh") (which "sh")))
+             ;; --disable-ucsim disables sdcc-misc, patch it back in.
+             (substitute* "Makefile.in"
+               (("debugger/mcs51" line)
+                (string-append line  "\n"
+                               "TARGETS += sdcc-misc\n"
+                               "PKGS += $(SDCC_MISC)")))
              #t)))))
     (home-page "http://sdcc.sourceforge.net")
     (synopsis "C compiler suite for 8-bit microcontrollers")
