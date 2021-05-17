d7beff1f7fa33a38f5831892314883dbe4ee4592
Tobias Geerinckx-Rice <me@tobias.gr>
2021-05-18 00:25:11 +0200

    gnu: samplv1: Update to 0.9.22.
    * gnu/packages/music.scm (samplv1): Update to 0.9.22.

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -2020,7 +2020,7 @@ users to select LV2 plugins and run them with jalv.")
 (define-public synthv1
   (package
     (name "synthv1")
-    (version "0.9.21")
+    (version "0.9.22")
     (source (origin
               (method url-fetch)
               (uri
@@ -2028,7 +2028,7 @@ users to select LV2 plugins and run them with jalv.")
                               "/synthv1-" version ".tar.gz"))
               (sha256
                (base32
-                "0wg4ywkqf307vln0y923p083xacb5ahr2ghzvb9gmqyszd7k2v15"))))
+                "0cmxbsfhkkyqn97rc47cb7d3bv6bd9r71xp4z85mi2kl3q4k569i"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f))                    ; there are no tests
