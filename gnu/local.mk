3c52c96ca2828c580986a72944c60ae10b6024c3
Alexandr Vityazev <avityazev@posteo.org>
2021-05-20 14:58:14 +0000

    gnu: Remove unnecessary patches for emacs-geiser.
    * gnu/packages/patches/emacs-geiser-autoload-activate-implementation.patch:
    * gnu/packages/patches/emacs-geiser-guile-auto-activate.patch: Delete files.
    * gnu/local.mk (dist_patch_DATA): Apply file removal.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/local.mk
+++ gnu/local.mk
@@ -972,8 +972,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/emacs-exec-path.patch			\
   %D%/packages/patches/emacs-exwm-fix-fullscreen-states.patch	\
   %D%/packages/patches/emacs-fix-scheme-indent-function.patch	\
-  %D%/packages/patches/emacs-geiser-guile-auto-activate.patch	\
-  %D%/packages/patches/emacs-geiser-autoload-activate-implementation.patch	\
   %D%/packages/patches/emacs-ignore-empty-xim-styles.patch	\
   %D%/packages/patches/emacs-json-reformat-fix-tests.patch	\
   %D%/packages/patches/emacs-highlight-stages-add-gexp.patch	\
