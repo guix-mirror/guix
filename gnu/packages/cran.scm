cd488787af9e9e1a3cfcde7dec5ce22f2b90530d
Ricardo Wurmus <rekado@elephly.net>
2021-05-20 10:54:42 +0200

    gnu: r-rcppparallel: Update to 5.1.4.
    * gnu/packages/cran.scm (r-rcppparallel): Update to 5.1.4.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -11376,14 +11376,14 @@ package provides a minimal R interface by relying on the Rcpp package.")
 (define-public r-rcppparallel
   (package
     (name "r-rcppparallel")
-    (version "5.1.3")
+    (version "5.1.4")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "RcppParallel" version))
        (sha256
         (base32
-         "1c3y80m695xqhs4ynwig2bsl045nryi9ww0a1s2rb3gsjbs5vx7v"))))
+         "10pa9vaz7p4m6hjca2cpy6vj7rvgly4zv0q1g2xfsmcchyk4bdbn"))))
     (properties `((upstream-name . "RcppParallel")))
     (build-system r-build-system)
     (home-page "https://rcppcore.github.io/RcppParallel/")
