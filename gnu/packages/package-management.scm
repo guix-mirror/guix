fbabf0bec61c88755d6dd238717a356ca3a20b80
Christopher Baines <mail@cbaines.net>
2021-05-22 09:18:54 +0100

    gnu: guix: Update to 9f2b2c4.
    * gnu/packages/package-management.scm (guix): Update to 9f2b2c4.

--- gnu/packages/package-management.scm
+++ gnu/packages/package-management.scm
@@ -132,8 +132,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "1.3.0")
-        (commit "771b866c55e5f992df60f6c32f5fdd78b64e17f6")
-        (revision 1))
+        (commit "9f2b2c432b834c8057ce2f4119599d36197bc063")
+        (revision 2))
     (package
       (name "guix")
 
@@ -149,7 +149,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "0w8w05w6v3lc6478vfw67jzn0hbd9nsncpx9i9ch1lck53r8vs44"))
+                  "09w6b4hqcxaj6x1q6m1q8vlp4dyk6anfv8p6mfwmpqis81cspphn"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments
