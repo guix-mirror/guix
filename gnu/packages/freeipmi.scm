45dd728510646a6f3017a6e5780ecc217f05152b
Tobias Geerinckx-Rice <me@tobias.gr>
2021-05-23 12:49:17 +0200

    gnu: freeipmi: Update to 1.6.8.
    * gnu/packages/freeipmi.scm (freeipmi): Update to 1.6.8.

--- gnu/packages/freeipmi.scm
+++ gnu/packages/freeipmi.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2015, 2016, 2017, 2020, 2021 Efraim Flashner <efraim@flashner.co.il>
-;;; Copyright © 2019, 2020 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2019–2021 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -28,14 +28,14 @@
 (define-public freeipmi
   (package
     (name "freeipmi")
-    (version "1.6.7")
+    (version "1.6.8")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnu/freeipmi/freeipmi-"
                                  version ".tar.gz"))
              (sha256
               (base32
-               "1gyyx99q02p3v2nqm3h53mkjd33l0hrapwg4alg6qr9k74qik1dv"))))
+               "0w8af1i57szmxl9vfifwwyal7xh8aixz2l9487wvy6yckqk6m92a"))))
     (build-system gnu-build-system)
     (arguments
      '(#:configure-flags '("--disable-static")))
