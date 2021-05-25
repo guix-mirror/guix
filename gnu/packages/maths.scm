f5871b19cc32593149b91ca3f33075200844b4c0
Eric Brown <ecbrown@ericcbrown.com>
2021-05-25 00:58:50 -0500

    gnu: Add cbc.
    * gnu/packages/maths.scm (cbc): New variable.
    
    Co-authored-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -1798,6 +1798,35 @@ large-scale nonlinear optimization.  It provides C++, C, and Fortran
 interfaces.")
     (license license:epl1.0)))
 
+(define-public cbc
+  (package
+    (name "cbc")
+    (version "2.10.5")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://www.coin-or.org/download/source/"
+                                  "Cbc/Cbc-" version ".tgz"))
+              (sha256
+               (base32
+                "0wk9vr6zc62gw71v7gnra5wxqlcljcgbhm5lasx236v791b986ns"))
+              (modules '((guix build utils)))
+              (snippet
+               ;; Make sure we don't use the bundled software.
+               '(delete-file-recursively "ThirdParty"))))
+    (build-system gnu-build-system)
+    (native-inputs
+     `(("gfortran" ,gfortran)
+       ("pkg-config" ,pkg-config)))
+    (inputs
+     `(("openblas" ,openblas)))
+    (home-page "https://www.coin-or.org")
+    (synopsis "Branch-and-cut solver")
+    (description
+     "Cbc (Coin-or branch and cut) is a mixed integer linear programming
+solver written in C++.  It can be used as a library or as a standalone
+executable.")
+    (license license:epl1.0)))
+
 (define-public clp
   (package
     (name "clp")
