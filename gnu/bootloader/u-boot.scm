guix-mirror/guix

commit a65c935e29766940148d52b8116634b1e1cbcba6

gnu: bootloader: Update install-allwinner64-u-boot.
* gnu/bootloader/u-boot.scm (install-allwinner64-u-boot): Update to
  use a single file to install.

gnu/bootloader/u-boot.scm
@@ -73,12 +73,9 @@
 
 (define install-allwinner64-u-boot
   #~(lambda (bootloader root-index image)
-      (let ((spl (string-append bootloader "/libexec/spl/sunxi-spl.bin"))
-            (u-boot (string-append bootloader "/libexec/u-boot.itb")))
+      (let ((spl (string-append bootloader "/libexec/u-boot-sunxi-with-spl.fit.itb")))
         (write-file-on-device spl (stat:size (stat spl))
-                              image (* 8 1024))
-        (write-file-on-device u-boot (stat:size (stat u-boot))
-                              image (* 40 1024)))))
+                              image (* 8 1024)))))
 
 (define install-imx-u-boot
   #~(lambda (bootloader root-index image)

commit d7d6c9e4e697c33634c89628d10995d0f070db96

gnu: bootloader: Fix install-allwinner64-u-boot.
This fixes boot breakage introduced in commit:

  a65c935e29766940148d52b8116634b1e1cbcba6

Multiple files were still needed, but the filenames changed.

* gnu/bootloader/u-boot.scm (install-allwinner64-u-boot): Update to
  use both SPL and u-boot files with new filenames.

gnu/bootloader/u-boot.scm
@@ -73,9 +73,12 @@
 
 (define install-allwinner64-u-boot
   #~(lambda (bootloader root-index image)
-      (let ((spl (string-append bootloader "/libexec/u-boot-sunxi-with-spl.fit.itb")))
+      (let ((spl (string-append bootloader "/libexec/u-boot-sunxi-with-spl.bin"))
+            (u-boot (string-append bootloader "/libexec/u-boot-sunxi-with-spl.fit.itb")))
         (write-file-on-device spl (stat:size (stat spl))
-                              image (* 8 1024)))))
+                              image (* 8 1024))
+        (write-file-on-device u-boot (stat:size (stat u-boot))
+                              image (* 40 1024)))))
 
 (define install-imx-u-boot
   #~(lambda (bootloader root-index image)