eb8928eebab3c00dc2835564da383639ca3539b7
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-05-31 21:37:53 +0200

    gnu: emacs-tramp: Update to 2.5.0.5.
    * gnu/packages/emacs-xyz.scm (emacs-tramp): Update to 2.5.0.5.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -24137,14 +24137,14 @@ well as an option for visually flashing evaluated s-expressions.")
 (define-public emacs-tramp
   (package
     (name "emacs-tramp")
-    (version "2.5.0.4")
+    (version "2.5.0.5")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "tramp-" version ".tar"))
        (sha256
-        (base32 "0yk4ckk45gkjp24nfywz49j8pazq33m6pga3lirb5h6zc8an5z24"))))
+        (base32 "1dclxffynfacvwi2scpda35sxjb42603yyf2p0477qa9b0i4xha0"))))
     (build-system emacs-build-system)
     (arguments
      `(#:emacs ,emacs                   ;need D-Bus
