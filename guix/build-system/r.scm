c343c3bc1f1310ca1075491ed48fe2db16b978a7
Ricardo Wurmus <rekado@elephly.net>
2021-06-05 01:37:51 +0200

    guix: Update to Bioconductor 3.13.
    * guix/build-system/r.scm (bioconductor-uri): Update version.
    * guix/import/cran.scm (%bioconductor-version): Update.

--- guix/build-system/r.scm
+++ guix/build-system/r.scm
@@ -59,7 +59,7 @@ release corresponding to NAME and VERSION."
                          "/src/contrib/"
                          name "_" version ".tar.gz")
           ;; TODO: use %bioconductor-version from (guix import cran)
-          (string-append "https://bioconductor.org/packages/3.12"
+          (string-append "https://bioconductor.org/packages/3.13"
                          type-url-part
                          "/src/contrib/"
                          name "_" version ".tar.gz"))))
