bd17befeb1b11e963eea2854ad79a1c55464ee54
Ricardo Wurmus <rekado@elephly.net>
2021-06-05 08:49:40 +0200

    gnu: r-affycompatible: Update to 1.52.0.
    * gnu/packages/bioconductor.scm (r-affycompatible): Update to 1.52.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -1772,14 +1772,14 @@ measures for Affymetrix Oligonucleotide Arrays.")
 (define-public r-affycompatible
   (package
     (name "r-affycompatible")
-    (version "1.50.0")
+    (version "1.52.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "AffyCompatible" version))
        (sha256
         (base32
-         "0pcs51miy45bky26i1d4iarbjh569gssb5g4fr26bzgjmq19yl7x"))))
+         "1xwz22wf2smsvmd0d5lfadbsgscnl3fl9msypy2mml38k048p6vj"))))
     (properties
      `((upstream-name . "AffyCompatible")))
     (build-system r-build-system)
