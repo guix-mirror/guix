352f23f71cdfd2f72a77906551b74f2d13f63688
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2021-06-09 22:43:55 -0400

    gnu: tini: Update to 0.19.0 and provide tini-static.
    * gnu/packages/docker.scm (tini): Update to 0.19.0.  Remove trailing #t.
    [phases]: Remove argument.
    [configure-flags]: New argument, to clear the CMAKE_INSTALL_RPATH option.

--- gnu/packages/docker.scm
+++ gnu/packages/docker.scm
@@ -716,7 +716,7 @@ defined in a per-project configuration file.")
 (define-public tini
   (package
     (name "tini")
-    (version "0.18.0")
+    (version "0.19.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -725,19 +725,14 @@ defined in a per-project configuration file.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1h20i3wwlbd8x4jr2gz68hgklh0lb0jj7y5xk1wvr8y58fip1rdn"))))
+                "1hnnvjydg7gi5gx6nibjjdnfipblh84qcpajc08nvr44rkzswck4"))))
     (build-system cmake-build-system)
     (arguments
      `(#:tests? #f                    ;tests require a Docker daemon
-       #:phases (modify-phases %standard-phases
-                  (add-after 'unpack 'disable-static-build
-                    ;; Disable the static build as it fails to install, with
-                    ;; the error: "No valid ELF RPATH or RUNPATH entry exists
-                    ;; in the file".
-                    (lambda _
-                      (substitute* "CMakeLists.txt"
-                        ((".*tini-static.*") ""))
-                      #t)))))
+       ;; 'tini-static' is a static binary, which leads CMake to fail with
+       ;; ‘file RPATH_CHANGE could not write new RPATH: ...’.  Clear
+       ;; CMAKE_INSTALL_RPATH to avoid that problem.
+       #:configure-flags '("-DCMAKE_INSTALL_RPATH=")))
     (home-page "https://github.com/krallin/tini")
     (synopsis "Tiny but valid init for containers")
     (description "Tini is an init program specifically designed for use with
