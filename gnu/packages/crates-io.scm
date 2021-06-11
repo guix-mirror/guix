952de8bdc291534b426e405c52314709fea3cff3
Zheng junjie <873216071@qq.com>
2021-06-11 20:53:08 +0800

    gnu: Add rust-lsp-types-0.83.
    * gnu/packages/crates-io.scm (rust-lsp-types-0.83): New variable.
    (rust-lsp-types-0.80): inherit from above.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -24167,17 +24167,19 @@ key-value pairs.")
         (base32
          "0jxsgkn378kxkiqdshdjdclw5wwp2xaz45cqd3yw85fhn8a38fza"))))))
 
-(define-public rust-lsp-types-0.80
+(define-public rust-lsp-types-0.83
   (package
     (name "rust-lsp-types")
-    (version "0.80.0")
+    (version "0.83.1")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "lsp-types" version))
-       (file-name (string-append name "-" version ".tar.gz"))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1vwjmi4apa4np6fgy95gq1nxc322br9cx7q2mm6vpb5x2lkmw9pl"))))
+        (base32
+         "0vdbdb74w1aim0q0nqnjvddrqz9cwirwbkjykwgjg5sbhcwrzry4"))))
     (build-system cargo-build-system)
     (arguments
      `(#:skip-build? #t
@@ -24194,6 +24196,28 @@ key-value pairs.")
      "This package provides types useful for interacting with a language
 server (LSP).")
     (license license:expat)))
+
+(define-public rust-lsp-types-0.80
+  (package
+    (inherit rust-lsp-types-0.83)
+    (name "rust-lsp-types")
+    (version "0.80.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lsp-types" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1vwjmi4apa4np6fgy95gq1nxc322br9cx7q2mm6vpb5x2lkmw9pl"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-base64" ,rust-base64-0.12)
+        ("rust-bitflags" ,rust-bitflags-1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-serde-repr" ,rust-serde-repr-0.1)
+        ("rust-url" ,rust-url-2))))))
 
 (define-public rust-lzma-sys-0.1
   (package
