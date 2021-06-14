624a6b60b5f4051ec38829e9c9d2adbc54a093f9
Boris A. Dekshteyn <boris.dekshteyn@gmail.com>
2021-06-15 01:27:44 +1200

    gnu: inkscape: Update to version 1.1.
    * gnu/packages/patches/inkscape-1.1-fix-build-witch-gcc7.5.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Adjust accordingly.
    * gnu/packages/inkscape.scm (inkscape-1.0) Rename to inkscape-1.1.
    * gnu/packages/inkscape.scm (inkscape-1.1) Update to version 1.1.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1245,6 +1245,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/imagemagick-WriteTHUMBNAILImage-fix.patch	\
   %D%/packages/patches/inetutils-hurd.patch			\
   %D%/packages/patches/inkscape-poppler-0.76.patch		\
+  %D%/packages/patches/inkscape-1.1-fix-build-witch-gcc7.5.patch	\
   %D%/packages/patches/intel-xed-fix-nondeterminism.patch	\
   %D%/packages/patches/intltool-perl-compatibility.patch	\
   %D%/packages/patches/iputils-libcap-compat.patch		\
