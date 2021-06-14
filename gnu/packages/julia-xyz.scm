272ca183b43a89218629b40bd850d07a063e529c
Efraim Flashner <efraim@flashner.co.il>
2021-06-14 19:40:38 +0300

    gnu: julia-compat: Update to 3.30.0.
    * gnu/packages/julia-xyz.scm (julia-compat): Update to 3.30.0.

--- gnu/packages/julia-xyz.scm
+++ gnu/packages/julia-xyz.scm
@@ -632,7 +632,7 @@ common subexpression elimination.")
 (define-public julia-compat
   (package
     (name "julia-compat")
-    (version "3.29.0")
+    (version "3.30.0")
     (source
      (origin
        (method git-fetch)
@@ -641,7 +641,7 @@ common subexpression elimination.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "00wn28kmzn61fpj3i8f6p987927h9315j9pzzvjpfk5c0ppd1p6q"))))
+        (base32 "1qs6fm58dicdjy69qk5p0ndj2b7qsvg7rmydq7igvrvirad55v56"))))
     (build-system julia-build-system)
     (home-page "https://github.com/JuliaLang/Compat.jl")
     (synopsis "Compatibility across Julia versions")
