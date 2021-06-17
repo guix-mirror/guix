375d5d03c91e722c1fcf17d456c303c53b0b5a80
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2021-06-16 21:34:48 -0400

    pack: Fix the system value passed to build-docker-image.
    Before this change, the system value would be passed as x86_64 when using a
    i686-linux emulated system, e.g.:
    
      $ guix environment --system=i686-linux --ad-hoc  guile -- \
          guile -c '(display (utsname:machine (uname))) (newline)'
      x86_64
    
    This change uses the Guile builtin %host-type variable, which doesn't have
    this problem:
    
      $ guix environment --system=i686-linux --ad-hoc guile -- \
          guile -c '(display %host-type) (newline)'
      i686-unknown-linux-gnu
    
    * guix/scripts/pack.scm (docker-image)[#:system] Use %host-type as a fall-back
    when target is not defined.
    
    Co-authored-by: Ludovic Courtès <ludo@gnu.org>

--- guix/scripts/pack.scm
+++ guix/scripts/pack.scm
@@ -610,7 +610,7 @@ the image."
                                 #$profile
                                 #:repository tag
                                 #:database #+database
-                                #:system (or #$target (utsname:machine (uname)))
+                                #:system (or #$target %host-type)
                                 #:environment environment
                                 #:entry-point
                                 #$(and entry-point
