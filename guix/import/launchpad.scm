3aabe51e8c09b9a2a87c03c40e3cc0f90d531bfd
Brice Waegeneire <brice@waegenei.re>
2021-06-19 22:21:47 +0200

    import: launchpad: Fix typo.
    * guix/import/launchpad.scm (latest-release): Rename 'origin-github-uri'
    to 'origin-launchpad-uri'.

--- guix/import/launchpad.scm
+++ guix/import/launchpad.scm
@@ -1,6 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2019, 2020 Arun Isaac <arunisaac@systemreboot.net>
 ;;; Copyright © 2021 Matthew James Kraai <kraai@ftbfs.org>
+;;; Copyright © 2020 Brice Waegeneire <brice@waegenei.re>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -122,13 +123,13 @@ for example, 'linuxdcpp'. Return #f if there is no releases."
 
 (define (latest-release pkg)
   "Return an <upstream-source> for the latest release of PKG."
-  (define (origin-github-uri origin)
+  (define (origin-launchpad-uri origin)
     (match (origin-uri origin)
       ((? string? url) url) ; surely a Launchpad URL
       ((urls ...)
        (find (cut string-contains <> "launchpad.net") urls))))
 
-  (let* ((source-uri (origin-github-uri (package-source pkg)))
+  (let* ((source-uri (origin-launchpad-uri (package-source pkg)))
          (name (package-name pkg))
          (repository (launchpad-repository source-uri))
          (newest-version (latest-released-version repository)))
