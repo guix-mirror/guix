81ab5377c58ca6f7a24d23929397f4936398c0bd
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-06-24 15:46:50 +0200

    gnu: emacs-leaf: Update to 4.5.2.
    * gnu/packages/emacs-xyz.scm (emacs-leaf): Update to 4.5.2.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -13022,14 +13022,14 @@ performance-oriented and tidy.")
 (define-public emacs-leaf
   (package
     (name "emacs-leaf")
-    (version "4.4.8")
+    (version "4.5.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "leaf-" version ".tar"))
        (sha256
-        (base32 "0h0ksmgrhn29ci6z8y54dbbzcqlvfs1ra0kmf226gz0dqzk45vb3"))))
+        (base32 "0i90shhhkpdcwmfi8zv0008qgmg4g3cqd2yvpycfv9n2axvhag54"))))
     (build-system emacs-build-system)
     (home-page "https://github.com/conao3/leaf.el")
     (synopsis "Simplify your init.el configuration, extended use-package")
