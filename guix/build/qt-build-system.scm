15fd870c3f46314e02a36818f7f16da0de32d421
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2021-07-02 00:58:57 -0400

    Revert "build-system/qt: Wrappers set 'QTWEBENGINEPROCESS_PATH' if needed."
    This reverts commit 06eb21856f9535ab62d0becc92b4146e0620654e.  It will need to
    be refactored to fit on top of 2214b7b78d34a0e4d574b743dbeb8457356f6cff.

--- guix/build/qt-build-system.scm
+++ guix/build/qt-build-system.scm
@@ -87,8 +87,7 @@
            "/cursors" "/wallpapers" "/icons" "/mime")
          '("XDG_CONFIG_DIRS" "/etc/xdg")
          '("QT_PLUGIN_PATH" "/lib/qt5/plugins")
-         '("QML2_IMPORT_PATH" "/lib/qt5/qml")
-         '("QTWEBENGINEPROCESS_PATH" "/lib/qt5/libexec/QtWebEngineProcess"))))
+         '("QML2_IMPORT_PATH" "/lib/qt5/qml"))))
 
 (define* (wrap-all-programs #:key inputs outputs
                             (qt-wrap-excluded-outputs '())
