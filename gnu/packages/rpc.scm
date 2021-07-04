73a5891d744c69ab01433b568ba5c55dd927f139
Efraim Flashner <efraim@flashner.co.il>
2021-07-04 12:52:32 +0300

    gnu: grpc-1.16.1: Build with older protobuf.
    * gnu/packages/rpc.scm (grpc-1.16.1)[native-inputs]: Replace protobuf
    with protobuf-3.6.

--- gnu/packages/rpc.scm
+++ gnu/packages/rpc.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2019 Ricardo Wurmus <rekado@elephly.net>
-;;; Copyright © 2020 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2020, 2021 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2020 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2020 Brett Gilio <brettg@gnu.org>
 ;;; Copyright © 2021 Greg Hogan <code@greghogan.com>
@@ -42,7 +42,8 @@
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages regex)
-  #:use-module (gnu packages tls))
+  #:use-module (gnu packages tls)
+  #:use-module (srfi srfi-1))
 
 (define-public grpc
   (package
@@ -152,7 +153,10 @@ browsers to backend services.")
                               "src/core/lib/iomgr/ev_epollex_linux.cc")
                  (("gettid\\(")
                   "sys_gettid("))
-               #t))))))))
+               #t))))))
+    (native-inputs
+     `(("protobuf" ,protobuf-3.6)
+       ,@(alist-delete "protobuf" (package-native-inputs grpc))))))
 
 (define-public python-grpcio
   (package
