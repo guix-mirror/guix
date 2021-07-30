7041abe38aed6b438bbd975fe5d68f23bd6d16e5
Efraim Flashner <efraim@flashner.co.il>
2021-07-30 19:17:52 +0300

    gnu: parallel: Update to 20210722.
    * gnu/packages/parallel.scm (parallel): Update to 20210722.

--- gnu/packages/parallel.scm
+++ gnu/packages/parallel.scm
@@ -61,14 +61,14 @@
 (define-public parallel
   (package
     (name "parallel")
-    (version "20210622")
+    (version "20210722")
     (source
      (origin
       (method url-fetch)
       (uri (string-append "mirror://gnu/parallel/parallel-"
                           version ".tar.bz2"))
       (sha256
-       (base32 "11pcc5kim104wz25k5q1pqhd6z85d7dx509h74ncaxhyyydjfcvv"))))
+       (base32 "0jaa5137sjw2szvmnnslkqv1n3gg2rkkgr71j7hpp5a3q15hjf9j"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
