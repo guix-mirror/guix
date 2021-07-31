b07581d6e24bf5fa4676bcebaba75315bbbb01e6
Pierre Neidhardt <mail@ambrevar.xyz>
2021-07-31 13:23:13 +0200

    gnu: sbcl: Update to 2.1.7.
    * gnu/packages/lisp.scm (sbcl): Update to 2.1.7.

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -404,14 +404,14 @@ an interpreter, a compiler, a debugger, and much more.")
 (define-public sbcl
   (package
     (name "sbcl")
-    (version "2.1.6")
+    (version "2.1.7")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://sourceforge/sbcl/sbcl/" version "/sbcl-"
                            version "-source.tar.bz2"))
        (sha256
-        (base32 "1vydq6mmssla2vn0plpsrm50n3b42a464zxw72j6qihas9fhq8cb"))))
+        (base32 "1ihxx8bjvcqg5f6m7xxvrilqjphshlx6nahns81j1bij70anyq0j"))))
     (build-system gnu-build-system)
     (outputs '("out" "doc"))
     (native-inputs
