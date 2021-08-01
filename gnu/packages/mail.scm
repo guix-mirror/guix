5ab679f4dacbc8c6da3a551e284cb41d6a0f6792
Ricardo Wurmus <rekado@elephly.net>
2021-08-01 13:21:24 +0200

    gnu: Add libpst.
    * gnu/packages/mail.scm (libpst): New variable.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -4407,6 +4407,34 @@ score.")
 undelete email messages from Outlook Express .dbx files.")
     (license license:gpl3+)))
 
+(define-public libpst
+  (package
+    (name "libpst")
+    (version "0.6.76")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://www.five-ten-sg.com/libpst/packages/"
+                           "libpst-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0hhbbb8ddsgjhv9y1xd8s9ixlhdnjmhw12v06jwx4j6vpgp1na9x"))))
+    (build-system gnu-build-system)
+    (inputs
+     `(("boost" ,boost)
+       ("libgsf" ,libgsf)
+       ("python" ,python)
+       ("zlib" ,zlib)))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)))
+    (home-page "https://www.five-ten-sg.com/libpst/")
+    (synopsis "")
+    (description "The Libpst utilities include @code{readpst} which can
+convert email messages to both mbox and MH mailbox formats, @code{pst2ldif}
+which can convert the contacts to @code{.ldif} format for import into LDAP
+databases, and other tools to process Outlook email archives.")
+    (license license:gpl2+)))
+
 (define-public crm114
   (package
     (name "crm114")
