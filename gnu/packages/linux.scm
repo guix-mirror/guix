3f09c48452f501ccde4b6169776ebfaafc5cb0d5
Leo Famulari <leo@famulari.name>
2021-08-08 15:21:46 -0400

    gnu: linux-libre: Update to 5.13.9.
    * gnu/packages/linux.scm (linux-libre-5.13-version): Update to 5.13.9.
    (linux-libre-5.13-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -356,7 +356,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 
 ;; The current "stable" kernels. That is, the most recently released major
 ;; versions that are still supported upstream.
-(define-public linux-libre-5.13-version "5.13.8")
+(define-public linux-libre-5.13-version "5.13.9")
 (define deblob-scripts-5.13
   (linux-libre-deblob-scripts
    linux-libre-5.13-version
@@ -364,7 +364,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "153jf5l5x4438zgxwggaky2ahjlfl48j438vhpzks6h77lzc51a5")))
 (define-public linux-libre-5.13-pristine-source
   (let ((version linux-libre-5.13-version)
-        (hash (base32 "0a54r71mcyqvq7c8kff817vh6zfj0bdzmp6ql4az84wq9nwc526h")))
+        (hash (base32 "16hm6sb64f1hlr0qmf2w81zv55s6flj1x8jr2q326d9ny30przkj")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.13)))
