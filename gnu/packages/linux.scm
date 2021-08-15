66680c59d93cd9494fd3868203e28d393c798c96
Leo Famulari <leo@famulari.name>
2021-08-15 11:49:39 -0400

    gnu: linux-libre 5.4: Update to 5.4.141.
    * gnu/packages/linux.scm (linux-libre-5.4-version): Update to 5.4.141.
    (linux-libre-5.4-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -385,7 +385,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.10)))
 
-(define-public linux-libre-5.4-version "5.4.140")
+(define-public linux-libre-5.4-version "5.4.141")
 (define deblob-scripts-5.4
   (linux-libre-deblob-scripts
    linux-libre-5.4-version
@@ -393,7 +393,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1xghbbnaisjd0k1klbyn1p7r6r4x5a1bpmkm56a3gh2zvw4s7mj8")))
 (define-public linux-libre-5.4-pristine-source
   (let ((version linux-libre-5.4-version)
-        (hash (base32 "0hvygxc6nj0lzpm1bkvz3r8b2k0ybp7rxcxfa2n0bs7jyhnplr9c")))
+        (hash (base32 "0a4c2fbv0j1p8fliz62ycw319nzw9q5gn7g9z1bkifn51hv6sqqh")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.4)))
