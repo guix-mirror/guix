d2c1bf6b9a45bd2d2ea80f1c67f47e0f3e9ed92b
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-08-16 10:30:23 +0200

    gnu: emacs-olivetti: Update to 2.0.1.
    * gnu/packages/emacs-xyz.scm (emacs-olivetti): Update to 2.0.1.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -3774,7 +3774,7 @@ a command.")
 (define-public emacs-olivetti
   (package
     (name "emacs-olivetti")
-    (version "2.0.0")
+    (version "2.0.1")
     (source
      (origin
        (method git-fetch)
@@ -3783,7 +3783,7 @@ a command.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0c0nkxik74p6s1pbf1l8pix5yy4xgnakqkwrrmf2ic1x1m4xv3hj"))))
+        (base32 "0njkzd8x3wryyzd3czzk6f5w9jr7jly5l03ar706h77kpakd8z2y"))))
     (build-system emacs-build-system)
     (home-page "https://github.com/rnkn/olivetti")
     (synopsis "Emacs minor mode for a nice writing environment")
