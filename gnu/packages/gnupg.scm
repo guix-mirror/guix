903b68cac5812b0f007f372d236f3e82ac66995b
Leo Famulari <leo@famulari.name>
2021-08-20 14:41:40 -0400

    gnu: GnuPG: Update to 2.2.29.
    * gnu/packages/gnupg.scm (gnupg): Update to 2.22.9.

--- gnu/packages/gnupg.scm
+++ gnu/packages/gnupg.scm
@@ -262,7 +262,7 @@ compatible to GNU Pth.")
 (define-public gnupg
   (package
     (name "gnupg")
-    (version "2.2.27")
+    (version "2.2.29")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnupg/gnupg/gnupg-" version
@@ -270,7 +270,7 @@ compatible to GNU Pth.")
               (patches (search-patches "gnupg-default-pinentry.patch"))
               (sha256
                (base32
-                "1693s2rp9sjwvdslj94n03wnb6rxysjy0dli0q1698af044h1ril"))))
+                "1j8cpp08zlz9a9n4g9z6352m1bvl369r94p4pjg8z0948pdprl1r"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)))
