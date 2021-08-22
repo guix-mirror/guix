6b4e6deea60eca661cd242c697efca03618f75fe
Ricardo Wurmus <rekado@elephly.net>
2021-08-22 17:53:29 +0200

    gnu: r-terra: Update to 1.3-22.
    * gnu/packages/cran.scm (r-terra): Update to 1.3-22.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -19240,14 +19240,14 @@ emphasize hidden group structures in networks or focus on specific nodes.")
 (define-public r-terra
   (package
     (name "r-terra")
-    (version "1.3-4")
+    (version "1.3-22")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "terra" version))
        (sha256
         (base32
-         "04fcraicr79vrvzqfxqsgzqp6awf2g5qrvpi76dcm3c04i5z3rlc"))))
+         "1z5zql68q4n81ccpl02hndhbavhhqs65bli1hjxlqdwdq192whkh"))))
     (properties `((upstream-name . "terra")))
     (build-system r-build-system)
     (inputs
