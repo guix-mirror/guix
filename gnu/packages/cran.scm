10b152ae15e1fa5b140a9bffcb7114a2da7ac1ba
Ricardo Wurmus <rekado@elephly.net>
2021-08-22 17:53:23 +0200

    gnu: r-cachem: Update to 1.0.6.
    * gnu/packages/cran.scm (r-cachem): Update to 1.0.6.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -134,14 +134,14 @@ files and provide more explicit control over line endings.")
 (define-public r-cachem
   (package
     (name "r-cachem")
-    (version "1.0.5")
+    (version "1.0.6")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "cachem" version))
        (sha256
         (base32
-         "1xigqfhkl4n768bjgn2bqvb9k70h1ibj4lv5vvpwsh1p6vw4mkw2"))))
+         "0r0q5w8lsxak46crnkbi739qzys90hlw7cqqqhv99xzkpkvm554s"))))
     (properties `((upstream-name . "cachem")))
     (build-system r-build-system)
     (propagated-inputs
