8e8d85f722c7fa44ee6320b219274d716046b51f
Ludovic Courtès <ludo@gnu.org>
2021-08-30 12:26:03 +0200

    build: Build (gnu system setuid).
    This is a followup to 45235e67e4aeac96ad8d60265bef054990aeb425.
    
    * gnu/local.mk (GNU_SYSTEM_MODULES): Add %D%/system/setuid.scm.

--- gnu/local.mk
+++ gnu/local.mk
@@ -668,6 +668,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/system/mapped-devices.scm			\
   %D%/system/nss.scm				\
   %D%/system/pam.scm				\
+  %D%/system/setuid.scm				\
   %D%/system/shadow.scm				\
   %D%/system/uuid.scm				\
   %D%/system/vm.scm				\
