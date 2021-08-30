0f903aa6521420095ddc556633eb81d880481cdc
Leo Famulari <leo@famulari.name>
2021-08-30 15:59:49 -0400

    gnu: linux-libre 4.14: Update to 4.14.245.
    * gnu/packages/linux.scm (linux-libre-4.14-version): Update to 4.14.245.
    (linux-libre-4.14-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -412,7 +412,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.19)))
 
-(define-public linux-libre-4.14-version "4.14.244")
+(define-public linux-libre-4.14-version "4.14.245")
 (define deblob-scripts-4.14
   (linux-libre-deblob-scripts
    linux-libre-4.14-version
@@ -420,7 +420,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1qij18inijj6c3ma8hv98yjagnzxdxyn134da9fd23ky8q6hbvky")))
 (define-public linux-libre-4.14-pristine-source
   (let ((version linux-libre-4.14-version)
-        (hash (base32 "0x554dck5f78ljknwahjvf49952s1w0zja3yh4vfz6lmf6hvzq5n")))
+        (hash (base32 "085jls7b2rzxlmvp0zsp4l3wi5xdrqlv2qczzwvbhzna1f4n2x0d")))
     (make-linux-libre-source version
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.14)))
