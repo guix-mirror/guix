4c91b5d6acb487cd63abab7cd00e927ccd8e6e7f
Tobias Geerinckx-Rice <me@tobias.gr>
2021-09-04 19:33:09 +0200

    gnu: btrfs-progs: Update to 5.13.1.
    * gnu/packages/linux.scm (btrfs-progs): Update to 5.13.1.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -5274,7 +5274,7 @@ and copy/paste text in the console and in xterm.")
 (define-public btrfs-progs
   (package
     (name "btrfs-progs")
-    (version "5.13")
+    (version "5.13.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kernel.org/linux/kernel/"
@@ -5282,7 +5282,7 @@ and copy/paste text in the console and in xterm.")
                                   "btrfs-progs-v" version ".tar.xz"))
               (sha256
                (base32
-                "0hsqbv7kvw5j2669xmp226cf7mgdfyq6xg8vjhm5w9ix43922aaf"))))
+                "16gkyhvvpqpq0skpavkvnmglbld7p331bnpiqn2z9flgwq0mlzix"))))
     (build-system gnu-build-system)
     (outputs '("out"
                "static"))      ; static versions of the binaries in "out"
