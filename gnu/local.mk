bac597cc968a4fd6c1bf41cffebf921e8581ca72
Andrew Tropin <andrew@trop.in>
2021-09-10 09:26:33 +0300

    home-services: Add Mcron.
    * gnu/home-services/mcron.scm: New file.
    * gnu/local.mk: Add this.
    * doc/guix.texi: Add documentation about Mcron Home Service.
    
    Signed-off-by: Oleg Pykhalov <go.wigust@gmail.com>

--- gnu/local.mk
+++ gnu/local.mk
@@ -80,6 +80,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/home-services/configuration.scm		\
   %D%/home-services/shells.scm			\
   %D%/home-services/shepherd.scm		\
+  %D%/home-services/mcron.scm			\
   %D%/home-services/utils.scm			\
   %D%/home-services/xdg.scm			\
   %D%/image.scm					\
