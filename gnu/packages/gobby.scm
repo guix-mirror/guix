3cea6f400953fa1a749f50cbb08735a51fcf2467
Felix Gruber <felgru@posteo.net>
2021-09-12 12:30:47 +0000

    gnu: gobby: Update to 0.6.0.
    This fixes a build failure due to our libinfinite being too recent for
    gobby 0.5.0.
    
    * gnu/packages/gobby.scm (gobby): Update to 0.6.0.
    [build-system]: Change to glib-or-gtk-build-system which helps to find
    glib-compile-schemas.
    [native-inputs]: Add itstool.
    [inputs]: Replace gtkmm-2 with gtkmm and gtksourceview-2 with
    gtksourceview-3.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/gobby.scm
+++ gnu/packages/gobby.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2017, 2019 Arun Isaac <arunisaac@systemreboot.net>
 ;;; Copyright © 2021 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2021 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2021 Felix Gruber <felgru@posteo.net>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -24,6 +25,7 @@
   #:use-module (guix download)
   #:use-module (guix utils)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system glib-or-gtk)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages)
   #:use-module (gnu packages glib)
@@ -143,7 +145,7 @@ connect to a server running the old 0.4 protocol.")
 (define-public gobby
   (package
     (name "gobby")
-    (version "0.5.0")
+    (version "0.6.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "http://releases.0x539.de/gobby/gobby-"
@@ -151,16 +153,17 @@ connect to a server running the old 0.4 protocol.")
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "165x0r668ma5blziisvbr8qig3jw9hf7i6w8r7wwvz3wsac3bswc"))))
-    (build-system gnu-build-system)
+                "1p2wbnchxy2wdzk19p7bxfpbq5zawa0l500na57jp8jgk3qz7czx"))))
+    (build-system glib-or-gtk-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)
-       ("intltool" ,intltool)))
+       ("intltool" ,intltool)
+       ("itstool" ,itstool)))
     (inputs
      `(("gnutls" ,gnutls)
        ("gsasl" ,gsasl)
-       ("gtkmm-2" ,gtkmm-2)
-       ("gtksourceview-2" ,gtksourceview-2)
+       ("gtkmm" ,gtkmm)
+       ("gtksourceview" ,gtksourceview-3)
        ("libinfinity" ,libinfinity)
        ("libxml++-2" ,libxml++-2)))
     (arguments
