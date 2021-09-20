650e85d85514c5fae06adf97ae615643a41bbbd8
Mathieu Othacehe <othacehe@gnu.org>
2021-09-20 09:12:01 +0000

    gnu: grub: Strip debug symbols.
    This is a follow-up of 71aa29911cf3f4e6db5f9bff9237308b5f93283d. Grub does not
    support modules stripped with --strip-unneeded but supports the --strip-debug
    option.
    
    * gnu/packages/bootloaders.scm (grub)[arguments]<#:strip-binaries>: Set to
    true and use ...
    <#:strip-flags>: ... to pass --strip-debug instead of --strip-unneeded.

--- gnu/packages/bootloaders.scm
+++ gnu/packages/bootloaders.scm
@@ -114,8 +114,8 @@
        ;; calling the ‘true’ binary instead.  Python is only needed during
        ;; bootstrapping (for genptl.py), not when building from a release.
        (list "PYTHON=true")
-       ;; Grub fails to load stripped modules.
-       #:strip-binaries? #f
+       ;; Grub fails to load modules stripped with --strip-unneeded.
+       #:strip-flags '("--strip-debug" "--enable-deterministic-archives")
        #:phases (modify-phases %standard-phases
                   (add-after 'unpack 'patch-stuff
                    (lambda* (#:key native-inputs inputs #:allow-other-keys)
