9bc8b1d950d2d43b3cc3ecb27c751e184425cc9f
Ricardo Wurmus <rekado@elephly.net>
2021-09-29 15:33:26 +0200

    gnu: r-ggfun: Update to 0.0.4.
    * gnu/packages/cran.scm (r-ggfun): Update to 0.0.4.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -18143,14 +18143,14 @@ function for computing a matrix of correlation p-values.")
 (define-public r-ggfun
   (package
     (name "r-ggfun")
-    (version "0.0.3")
+    (version "0.0.4")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "ggfun" version))
        (sha256
         (base32
-         "0bf8pl3scy5fx15h1fx0799cb9sg9spjapqif9gcz27pmmgdc0n9"))))
+         "07njcjcj212zxanq8x3nkh7mgvqmym08rx5s41rz9flhk9gkc9jr"))))
     (properties `((upstream-name . "ggfun")))
     (build-system r-build-system)
     (propagated-inputs
