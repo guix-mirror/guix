7d59858886992bcba3f468229f457c7339e98cf4
Matthew James Kraai <kraai@ftbfs.org>
2021-10-01 08:04:27 -0700

    gnu: xpra: Update to 4.2.2.
    * gnu/packages/xorg.scm (xpra): Update to 4.2.2.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -6325,14 +6325,14 @@ basic eye-candy effects.")
 (define-public xpra
   (package
     (name "xpra")
-    (version "4.2.1")
+    (version "4.2.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://www.xpra.org/src/xpra-"
                            version ".tar.gz"))
        (sha256
-        (base32 "0gqdcw5cfk919jk8g0g4xjxbsvr5j9gskn8q3cmrz388pvfvm8x7"))
+        (base32 "0nky9j07zq8d5ifx568dima7a0jgs8kwhcqbg8p6gsxlgq5zdw96"))
        (patches (search-patches "xpra-4.2-systemd-run.patch"))))
     (build-system python-build-system)
     ;; see also http://xpra.org/trac/wiki/Dependencies
