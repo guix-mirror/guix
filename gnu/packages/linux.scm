bf2575ef90c8bf2a816dc50f5dbde59e8690fa97
Leo Famulari <leo@famulari.name>
2021-10-10 12:58:23 -0400

    gnu: linux-libre 4.19: Update to 4.19.210.
    * gnu/packages/linux.scm (linux-libre-4.19-version): Update to 4.19.210.
    (linux-libre-4.19-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -394,7 +394,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.4)))
 
-(define-public linux-libre-4.19-version "4.19.209")
+(define-public linux-libre-4.19-version "4.19.210")
 (define-public linux-libre-4.19-gnu-revision "gnu1")
 (define deblob-scripts-4.19
   (linux-libre-deblob-scripts
@@ -404,7 +404,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1a0k9i8gnzkyvfr80f8xw2fnxfwddhz1pzicz9fh0y3jzzkzk45p")))
 (define-public linux-libre-4.19-pristine-source
   (let ((version linux-libre-4.19-version)
-        (hash (base32 "04z9hxgqw2p86xxi2667kaj2w99xxxiw9gn9i1217c305swxa9vk")))
+        (hash (base32 "0cv126j4sghwgzjm9p5l1brcnxkbgggja2ai37gl8m2s99cj59wv")))
     (make-linux-libre-source version
                              (%upstream-linux-source version hash)
                              deblob-scripts-4.19)))
