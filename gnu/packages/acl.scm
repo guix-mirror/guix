guix-mirror/guix

commit 133f780039e63798535e4f52a4d9f1810ea5b2ab

gnu: python-pylibacl: Improve description.
* gnu/packages/acl.scm (python-pylibacl)[synopsis]: Use @acronym{}.
[description]: Turn into a full sentence, mark up, and edit for GNU.

gnu/packages/acl.scm
@@ -128,8 +128,9 @@
     (inputs `(("acl" ,acl)))
     (native-inputs `(("python-pytest" ,python-pytest)))
     (home-page "https://pylibacl.k1024.org/")
-    (synopsis "POSIX.1e ACLs for python")
-    (description "Python 3.4+ extension module that allows you to manipulate
-the POSIX.1e Access Control Lists present in some OS/file-systems
-combinations.")
+    (synopsis "POSIX.1e @acronym{ACLs, access control lists} for Python")
+    (description
+     "This Python extension module manipulates the POSIX.1e @acronym{ACLs,
+Access Control Lists available on many file systems.  These allow more
+fine-grained access control than traditional user/group permissions.")
     (license lgpl2.1+)))

commit 9cda21cf20a5c9bdf97e3a6d6c8f901fc3e4307d

gnu: python-pylibacl: Add missing brace in description.
Regression introduced in 133f780039e63798535e4f52a4d9f1810ea5b2ab.

* gnu/packages/acl.scm (python-pylibacl)[description]: Add missing brace.

gnu/packages/acl.scm
@@ -131,6 +131,6 @@
     (synopsis "POSIX.1e @acronym{ACLs, access control lists} for Python")
     (description
      "This Python extension module manipulates the POSIX.1e @acronym{ACLs,
-Access Control Lists available on many file systems.  These allow more
+Access Control Lists} available on many file systems.  These allow more
 fine-grained access control than traditional user/group permissions.")
     (license lgpl2.1+)))