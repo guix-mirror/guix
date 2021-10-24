ab8c08c51913671861ebadf11cc3c2bf3d82658c
Timothy Sample <samplet@ngyro.com>
2021-10-24 13:45:59 -0400

    gnu: disarchive: Update to 0.3.0.
    * gnu/packages/backup.scm (disarchive): Update to 0.3.0.
    [source]: Remove cross-compilation patch.
    [arguments]: Remove field.
    * gnu/packages/patches/disarchive-cross-compilation.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -998,7 +998,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/dfu-programmer-fix-libusb.patch		\
   %D%/packages/patches/diffoscope-fix-test_item3_deflate_llvm_bitcode.patch	\
   %D%/packages/patches/diffutils-gets-undeclared.patch		\
-  %D%/packages/patches/disarchive-cross-compilation.patch	\
   %D%/packages/patches/dkimproxy-add-ipv6-support.patch		\
   %D%/packages/patches/docbook-xsl-nonrecursive-string-subst.patch	\
   %D%/packages/patches/doc++-include-directives.patch		\
