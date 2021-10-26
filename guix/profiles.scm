3c1158ac4e5ef825a9b9a229a233fabd7cef334e
Ludovic Courtès <ludo@gnu.org>
2021-10-26 16:01:40 +0200

    profiles: Build the man database only if 'man-db' is in the profile.
    This allows us to skip the expensive man-db profile hook in most cases.
    
    Suggested by Liliana Marie Prikler <liliana.prikler@gmail.com>.
    
    * guix/profiles.scm (manual-database/optional): New procedure.
    (%default-profile-hooks): Use it instead of 'manual-database'.
    * doc/guix.texi (Documentation): Add footnote about 'man -k' database
    creation.

--- guix/profiles.scm
+++ guix/profiles.scm
@@ -1682,6 +1682,16 @@ the entries in MANIFEST."
                     `((type . profile-hook)
                       (hook . manual-database))))
 
+(define (manual-database/optional manifest)
+  "Return a derivation to build the manual database of MANIFEST, but only if
+MANIFEST contains the \"man-db\" package.  Otherwise, return #f."
+  ;; Building the man database (for "man -k") is expensive and rarely used.
+  ;; Build it only if the profile also contains "man-db".
+  (mlet %store-monad ((man-db (manifest-lookup-package manifest "man-db")))
+    (if man-db
+        (manual-database manifest)
+        (return #f))))
+
 (define (texlive-configuration manifest)
   "Return a derivation that builds a TeXlive configuration for the entries in
 MANIFEST."
@@ -1784,7 +1794,7 @@ MANIFEST."
   ;; This is the list of derivation-returning procedures that are called by
   ;; default when making a non-empty profile.
   (list info-dir-file
-        manual-database
+        manual-database/optional
         fonts-dir-file
         ghc-package-cache-file
         ca-certificate-bundle
