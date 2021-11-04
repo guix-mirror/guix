0b341967e3b950c21552b95213bdf0257d9632fc
Ricardo Wurmus <rekado@elephly.net>
2021-11-04 22:04:10 +0000

    gnu: r-vsn: Update to 3.62.0.
    * gnu/packages/bioconductor.scm (r-vsn): Update to 3.62.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -4580,14 +4580,14 @@ coding changes and predict coding outcomes.")
 (define-public r-vsn
   (package
     (name "r-vsn")
-    (version "3.60.0")
+    (version "3.62.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "vsn" version))
        (sha256
         (base32
-         "0nppph3kv8z83368snb8s3n4vcqj829yyi1kh4q09qvq18rhvssv"))))
+         "03p2wdjbjnrn1ddyz0fbn04mdxpsmv83qhh3apj6azshl0bs1j2x"))))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-affy" ,r-affy)
