b9c74b05d766b29be8d9bbccb8074f76d7cc95cf
Ricardo Wurmus <rekado@elephly.net>
2021-11-04 22:04:40 +0000

    gnu: r-biocworkflowtools: Update to 1.20.0.
    * gnu/packages/bioconductor.scm (r-biocworkflowtools): Update to 1.20.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -12064,14 +12064,14 @@ accessing web references for elements/sets are also available in BiocSet.")
 (define-public r-biocworkflowtools
   (package
     (name "r-biocworkflowtools")
-    (version "1.18.0")
+    (version "1.20.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BiocWorkflowTools" version))
        (sha256
         (base32
-         "1a32bwgnxaw0gv2gij2p4rm0a6l06jjhidvfz2v4k900pz1w79av"))))
+         "1j9s8w5y8savcmh70npkanxacq1kipxnwk1wsiw5hwnp1p13ldaa"))))
     (properties
      `((upstream-name . "BiocWorkflowTools")))
     (build-system r-build-system)
