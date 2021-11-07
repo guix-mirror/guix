071f4e3db39cebf1623d9b7437b0d39dde3d219a
Tobias Geerinckx-Rice <me@tobias.gr>
2021-11-07 20:09:46 +0100

    guix-install.sh: Undo some bugs introduced by shellcheck.
    * etc/guix-install.sh: Unquote all file names containing ~root.

--- etc/guix-install.sh
+++ etc/guix-install.sh
@@ -345,11 +345,11 @@ sys_create_store()
     mv "${tmp_path}/gnu" /
 
     _msg "${INF}Linking the root user's profile"
-    mkdir -p "~root/.config/guix"
+    mkdir -p ~root/.config/guix
     ln -sf /var/guix/profiles/per-user/root/current-guix \
-       "~root/.config/guix/current"
+       ~root/.config/guix/current
 
-    GUIX_PROFILE="~root/.config/guix/current"
+    GUIX_PROFILE=~root/.config/guix/current
     # shellcheck disable=SC1090
     source "${GUIX_PROFILE}/etc/profile"
     _msg "${PAS}activated root profile at ${GUIX_PROFILE}"
@@ -405,7 +405,7 @@ sys_enable_guix_daemon()
     case "$INIT_SYS" in
         upstart)
             { initctl reload-configuration;
-              cp "~root/.config/guix/current/lib/upstart/system/guix-daemon.conf" \
+              cp ~root/.config/guix/current/lib/upstart/system/guix-daemon.conf \
                  /etc/init/ &&
                   configure_substitute_discovery /etc/init/guix-daemon.conf &&
                   start guix-daemon; } &&
@@ -415,15 +415,15 @@ sys_enable_guix_daemon()
             { # systemd .mount units must be named after the target directory.
               # Here we assume a hard-coded name of /gnu/store.
               # XXX Work around <https://issues.guix.gnu.org/41356> until next release.
-              if [ -f "~root/.config/guix/current/lib/systemd/system/gnu-store.mount" ]; then
-                  cp "~root/.config/guix/current/lib/systemd/system/gnu-store.mount" \
+              if [ -f ~root/.config/guix/current/lib/systemd/system/gnu-store.mount ]; then
+                  cp ~root/.config/guix/current/lib/systemd/system/gnu-store.mount \
                      /etc/systemd/system/;
                   chmod 664 /etc/systemd/system/gnu-store.mount;
                   systemctl daemon-reload &&
                       systemctl enable gnu-store.mount;
               fi
 
-              cp "~root/.config/guix/current/lib/systemd/system/guix-daemon.service" \
+              cp ~root/.config/guix/current/lib/systemd/system/guix-daemon.service \
                  /etc/systemd/system/;
               chmod 664 /etc/systemd/system/guix-daemon.service;
 
@@ -447,7 +447,7 @@ sys_enable_guix_daemon()
             ;;
         sysv-init)
             { mkdir -p /etc/init.d;
-              cp "~root/.config/guix/current/etc/init.d/guix-daemon" \
+              cp ~root/.config/guix/current/etc/init.d/guix-daemon \
                  /etc/init.d/guix-daemon;
               chmod 775 /etc/init.d/guix-daemon;
 
@@ -460,7 +460,7 @@ sys_enable_guix_daemon()
             ;;
         openrc)
             { mkdir -p /etc/init.d;
-              cp "~root/.config/guix/current/etc/openrc/guix-daemon" \
+              cp ~root/.config/guix/current/etc/openrc/guix-daemon \
                  /etc/init.d/guix-daemon;
               chmod 775 /etc/init.d/guix-daemon;
 
@@ -492,7 +492,7 @@ sys_authorize_build_farms()
     if prompt_yes_no "Permit downloading pre-built package binaries from the \
 project's build farm? (yes/no)"; then
         guix archive --authorize \
-             < "~root/.config/guix/current/share/guix/ci.guix.gnu.org.pub" \
+             < ~root/.config/guix/current/share/guix/ci.guix.gnu.org.pub \
             && _msg "${PAS}Authorized public key for ci.guix.gnu.org"
         else
             _msg "${INF}Skipped authorizing build farm public keys"
