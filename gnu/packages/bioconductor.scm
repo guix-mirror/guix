8f392c48174f429b35af2afa42d2c8631e0d2f41
Vagrant Cascadian <vagrant@debian.org>
2021-11-14 17:40:40 -0800

    gnu: r-msfeatures: Fix typo.
    * gnu/packages/bioconductor.scm (r-msfeatures)[description]: Fix spelling of
      "package".

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -13123,7 +13123,7 @@ within the R for Mass Spectrometry packages.")
      "The MsFeature package defines functionality for Mass Spectrometry
 features.  This includes functions to group (LC-MS) features based on some of
 their properties, such as retention time (coeluting features), or correlation
-of signals across samples.  This packge hence allows to group features, and
+of signals across samples.  This package hence allows to group features, and
 its results can be used as an input for the @code{QFeatures} package which
 allows to aggregate abundance levels of features within each group.  This
 package defines concepts and functions for base and common data types,
