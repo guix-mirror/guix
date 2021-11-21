7cc2f02281d93a1f9a032de4f4d3604575cce877
Songlin Jiang <hollowman@hollowman.ml>
2021-11-21 14:10:32 +0800

    gnu: Add gnome-shell-extension-customize-ibus.
    * gnu/packages/gnome-xyz.scm (gnome-shell-extension-customize-ibus): New
    variable.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/gnome-xyz.scm
+++ gnu/packages/gnome-xyz.scm
@@ -11,6 +11,7 @@
 ;;; Copyright © 2020 Ellis Kenyo <me@elken.dev>
 ;;; Copyright © 2020 Stefan Reichör <stefan@xsteve.at>
 ;;; Copyright © 2021 Vinicius Monego <monego@posteo.net>
+;;; Copyright © 2021 Songlin Jiang <hollowman@hollowman.ml>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -280,6 +281,39 @@ GNOME Shell.")
     (description "Clipboard Indicator is a clipboard manager for GNOME Shell
 that caches clipboard history.")
     (license license:expat)))
+
+(define-public gnome-shell-extension-customize-ibus
+  (package
+    (name "gnome-shell-extension-customize-ibus")
+    (version "78")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/openSUSE/Customize-IBus.git")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1hnnsjriq7xaakk8biwz55mn077lnm9nsmi4wz5zk7clgxmasvq9"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "VERSION=" ,version)
+             (string-append "INSTALLBASE=" (assoc-ref %outputs "out")
+                            "/share/gnome-shell/extensions"))
+       #:tests? #f ; No test target
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'bootstrap)
+         (delete 'configure))))
+    (native-inputs
+     `(("gettext" ,gettext-minimal)
+       ("glib:bin" ,glib "bin")))
+    (home-page "https://github.com/openSUSE/Customize-IBus")
+    (synopsis "GNOME Shell Extension for IBus Customization")
+    (description "Customize IBus provides full customization of appearance,
+behavior, system tray and input source indicator for IBus.")
+    (license license:gpl3+)))
 
 (define-public gnome-shell-extension-topicons-redux
   (package
