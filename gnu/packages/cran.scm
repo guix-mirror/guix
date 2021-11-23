1d3dce75396b987e4f7cfdce5cda3969cc2604ff
Ricardo Wurmus <rekado@elephly.net>
2021-11-23 15:23:31 +0100

    gnu: r-mstate: Update to 0.3.2.
    * gnu/packages/cran.scm (r-mstate): Update to 0.3.2.
    [propagated-inputs]: Remove r-magrittr and r-viridis; add r-viridislite.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -26045,24 +26045,23 @@ with posterior predictive checks and leave-one-out cross-validation.")
 (define-public r-mstate
   (package
     (name "r-mstate")
-    (version "0.3.1")
+    (version "0.3.2")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "mstate" version))
        (sha256
         (base32
-         "11i3p7fph8nbnfis1m7rdrq32qryaajv2wrkxk1x6k17zkh4rq6i"))))
+         "054dzrd5b0xjjjl7862q3aq1jwgrxbkqz7zpvbdirqsld3zksirw"))))
     (properties `((upstream-name . "mstate")))
     (build-system r-build-system)
     (propagated-inputs
      `(("r-data-table" ,r-data-table)
        ("r-lattice" ,r-lattice)
-       ("r-magrittr" ,r-magrittr)
        ("r-rcolorbrewer" ,r-rcolorbrewer)
        ("r-rlang" ,r-rlang)
        ("r-survival" ,r-survival)
-       ("r-viridis" ,r-viridis)))
+       ("r-viridislite" ,r-viridislite)))
     (native-inputs
      `(("r-knitr" ,r-knitr)))
     (home-page
