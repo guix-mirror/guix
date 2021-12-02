10554e0a57feeea470127a1d0441957d1776b0bd
Marius Bakke <marius@gnu.org>
2021-12-02 21:50:27 +0100

    gnu: python-django-netfields: Update to 1.2.4.
    * gnu/packages/django.scm (python-django-netfields): Update to 1.2.4.

--- gnu/packages/django.scm
+++ gnu/packages/django.scm
@@ -1289,13 +1289,13 @@ to ElasticSearch.")
 (define-public python-django-netfields
   (package
     (name "python-django-netfields")
-    (version "1.2.2")
+    (version "1.2.4")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "django-netfields" version))
               (sha256
                (base32
-                "1c47azr5am0q8g45x0fbn0cay7vyrack6n7k6siliw1j2p0gzi7s"))))
+                "0jwlbyaxk91fq69g2y0zpfjgmjgh6l0lqm5mhys7m5968lkihvgp"))))
     (build-system python-build-system)
     (arguments '(#:tests? #f))      ;XXX: Requires a running PostgreSQL server
     (native-inputs
