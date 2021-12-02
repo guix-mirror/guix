bef86fed3e34ac62fba178c46cd9732526c2b9bf
Leo Famulari <leo@famulari.name>
2021-12-02 01:25:00 -0500

    Revert "gnu: godot: Update to 3.4."
    This reverts commit dd4365efa1c6fb609de9bd7cd10f9c82e064aed4.
    
    We reverted this commit because it broke the operation of the only
    package in Guix that depends on Godot, SuperStarfighter:
    
    https://debbugs.gnu.org/cgi/bugreport.cgi?bug=52233#20

--- gnu/packages/game-development.scm
+++ gnu/packages/game-development.scm
@@ -1846,7 +1846,7 @@ games.")
 (define-public godot
   (package
     (name "godot")
-    (version "3.4")
+    (version "3.2.3")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -1855,7 +1855,7 @@ games.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0y542zla6msgxf31rd0349d9j3ya7f3njnwmmrh8lmzfgxx86qbx"))
+                "19vrp5lhyvxbm6wjxzn28sn3i0s8j08ca7nani8l1nrhvlc8wi0v"))
               (modules '((guix build utils)
                          (ice-9 ftw)
                          (srfi srfi-1)))
@@ -1870,23 +1870,19 @@ games.")
                               "assimp"
                               "certs"
                               "cvtt"
-                              "embree"
                               "enet"
                               "etc2comp"
                               "fonts"
                               "glad"
                               "jpeg-compressor"
                               "libsimplewebm"
-                              "minimp3"
                               "miniupnpc"
                               "minizip"
                               "misc"
                               "nanosvg"
-                              "oidn"
                               "pvrtccompressor"
                               "recastnavigation"
                               "squish"
-                              "stb_rect_pack"
                               "tinyexr"
                               "vhacd"
                               "xatlas")))
@@ -1973,7 +1969,6 @@ games.")
     (native-inputs `(("pkg-config" ,pkg-config)))
     (inputs `(("alsa-lib" ,alsa-lib)
               ("bullet" ,bullet)
-              ("eudev" ,eudev)
               ("freetype" ,freetype)
               ("glew" ,glew)
               ("glu" ,glu)
