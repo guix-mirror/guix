1da7f9089f727bb3dce73e4d2acfc2803b82e090
Efraim Flashner <efraim@flashner.co.il>
2021-12-09 23:14:09 +0200

    gnu: Add gsl-static.
    * gnu/packages/maths.scm (gsl-static): New variable.

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -581,6 +581,15 @@ differential equations, linear algebra, Fast Fourier Transforms and random
 numbers.")
     (license license:gpl3+)))
 
+;; TODO: Merge back into the gsl package as a separate output.
+(define-public gsl-static
+  (package/inherit gsl
+    (name "gsl-static")
+    (arguments
+     `(,@(package-arguments gsl)
+        #:configure-flags (list "--disable-shared")
+        #:make-flags (list "CFLAGS=-fPIC")))))
+
 (define-public sleef
   (package
     (name "sleef")
