e5d93c87d564312b3ac9dff369ad54161d7371ad
Efraim Flashner <efraim@flashner.co.il>
2021-12-15 12:20:15 +0200

    gnu: mercurial: Update native-search-paths.
    * gnu/packages/version-control.scm (mercurial)[naive-search-paths]:
    Search for extensions in python3.9 subfolder.

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -1790,7 +1790,7 @@ execution of any hook written in any language before every commit.")
     (native-search-paths
      (list (search-path-specification
             (variable "HGEXTENSIONPATH")
-            (files '("lib/python3.8/site-packages/hgext3rd")))))
+            (files '("lib/python3.9/site-packages/hgext3rd")))))
     (home-page "https://www.mercurial-scm.org/")
     (synopsis "Decentralized version control system")
     (description
