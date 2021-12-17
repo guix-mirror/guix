96ccb89bef04405cc615df492e1219c75ef4f11a
Ricardo Wurmus <rekado@elephly.net>
2021-12-17 23:38:44 +0100

    gnu: Remove icedtea-6.
    * gnu/packages/java.scm (icedtea-6): Remove variable.
    * gnu/packages/patches/icedtea-6-extend-hotspot-aarch64-support.patch: Remove file.
    * gnu/local.mk (dist_patch_DATA): Remove patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1264,7 +1264,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/icecat-use-older-reveal-hidden-html.patch	\
   %D%/packages/patches/icecat-use-system-graphite2+harfbuzz.patch	\
   %D%/packages/patches/icecat-use-system-media-libs.patch	\
-  %D%/packages/patches/icedtea-6-extend-hotspot-aarch64-support.patch	\
   %D%/packages/patches/icedtea-7-hotspot-aarch64-use-c++98.patch\
   %D%/packages/patches/id3lib-CVE-2007-4460.patch			\
   %D%/packages/patches/id3lib-UTF16-writing-bug.patch			\
