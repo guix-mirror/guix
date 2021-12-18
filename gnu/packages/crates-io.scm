a3765072d29ffe5a693cd05068bf448c646a79cb
Hartmut Goebel <h.goebel@crazy-compilers.com>
2021-12-18 21:02:27 +0100

    gnu: rust-sequoia-openpgp-0.9: Move to (gnu packages sequoia).
    * gnu/packages/crates-io.scm (rust-sequoia-openpgp-0.9): Move package to ...
    * gnu/packages/sequoia.scm: ... here. Inherit from
      rust-sequoia-openpgp-1. [arguments]<skip-build?> add,
      <cargo-development-inputs>: Remove.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -73,6 +73,7 @@
   #:use-module (gnu packages python)
   #:use-module (gnu packages rust)
   #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages sequoia)
   #:use-module (gnu packages serialization)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages ssh)
@@ -48213,57 +48214,6 @@ formats:
      "This package provides a macro to repeat sequentially indexed copies of a
 fragment of code.")
     (license (list license:expat license:asl2.0))))
-
-(define-public rust-sequoia-openpgp-0.9
-  (package
-    (name "rust-sequoia-openpgp")
-    (version "0.9.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "sequoia-openpgp" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32
-         "007h2pi7lcph5jf5bxjydm7hjwjai33yk6dic3cxknki22lxlkfw"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-inputs
-       (("rust-base64" ,rust-base64-0.9)
-        ("rust-buffered-reader" ,rust-buffered-reader-0.9)
-        ("rust-bzip2" ,rust-bzip2-0.3)
-        ("rust-failure" ,rust-failure-0.1)
-        ("rust-flate2" ,rust-flate2-1)
-        ("rust-idna" ,rust-idna-0.1)
-        ("rust-lalrpop" ,rust-lalrpop-0.17)
-        ("rust-lalrpop-util" ,rust-lalrpop-util-0.17)
-        ("rust-lazy-static" ,rust-lazy-static-1)
-        ("rust-memsec" ,rust-memsec-0.5)
-        ("rust-nettle" ,rust-nettle-5)
-        ("rust-quickcheck" ,rust-quickcheck-0.8)
-        ("rust-rand" ,rust-rand-0.6)
-        ("rust-sequoia-rfc2822" ,rust-sequoia-rfc2822-0.9)
-        ("rust-time" ,rust-time-0.1))
-       #:cargo-development-inputs
-       (("rust-rpassword" ,rust-rpassword-3))))
-    (native-inputs
-     (list pkg-config))
-    (inputs
-     (list clang nettle))
-    (home-page "https://sequoia-pgp.org/")
-    (synopsis "OpenPGP data types and associated machinery")
-    (description
-     "This crate aims to provide a complete implementation of OpenPGP as
-defined by RFC 4880 as well as some extensions (e.g., RFC 6637, which
-describes ECC cryptography) for OpenPGP.  This includes support for unbuffered
-message processing.
-
-A few features that the OpenPGP community considers to be deprecated (e.g.,
-version 3 compatibility) have been left out.  We have also updated some
-OpenPGP defaults to avoid foot guns (e.g., we selected modern algorithm
-defaults).  If some functionality is missing, please file a bug report.")
-    (license license:gpl3)))
 
 (define-public rust-sequoia-rfc2822-0.9
   (package
--- gnu/packages/sequoia.scm
+++ gnu/packages/sequoia.scm
@@ -249,6 +249,38 @@ defaults).
 This Guix package is built to use the nettle cryptographic library.")
     (license license:lgpl2.0+)))
 
+(define-public rust-sequoia-openpgp-0.9
+  (package
+    (inherit rust-sequoia-openpgp-1)
+    (name "rust-sequoia-openpgp")
+    (version "0.9.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "sequoia-openpgp" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "007h2pi7lcph5jf5bxjydm7hjwjai33yk6dic3cxknki22lxlkfw"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-base64" ,rust-base64-0.9)
+        ("rust-buffered-reader" ,rust-buffered-reader-0.9)
+        ("rust-bzip2" ,rust-bzip2-0.3)
+        ("rust-failure" ,rust-failure-0.1)
+        ("rust-flate2" ,rust-flate2-1)
+        ("rust-idna" ,rust-idna-0.1)
+        ("rust-lalrpop" ,rust-lalrpop-0.17)
+        ("rust-lalrpop-util" ,rust-lalrpop-util-0.17)
+        ("rust-lazy-static" ,rust-lazy-static-1)
+        ("rust-memsec" ,rust-memsec-0.5)
+        ("rust-nettle" ,rust-nettle-5)
+        ("rust-quickcheck" ,rust-quickcheck-0.8)
+        ("rust-rand" ,rust-rand-0.6)
+        ("rust-sequoia-rfc2822" ,rust-sequoia-rfc2822-0.9)
+        ("rust-time" ,rust-time-0.1))))))
+
 (define-public sequoia-sq
   (package
     (name "sequoia-sq")
