guix-mirror/guix

commit a2b7abb99bd5396c59b404160c3bafa810be470a

gnu: rust-rayon-1: Update to 1.5.1.
* gnu/packages/crates-io.scm (rust-rayon-1): Update to 1.5.1.

gnu/packages/crates-io.scm
@@ -39054,14 +39054,14 @@ accessors.")
 (define-public rust-rayon-1
   (package
     (name "rust-rayon")
-    (version "1.5.0")
+    (version "1.5.1")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "rayon" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "0x2n4zkrm6z3avdfh7zgcwx0wq6hx8332dx89v3j1p7s3448w3cb"))))
+        (base32 "143dl2453bazgk7rwjrickmyqd0ks3q69nfz8axcins19n0clsn0"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs

commit d077579681534c6597762ec296b98cfbc9ddc301

gnu: rust-rayon@1.5: Update dependencies.
Previous versions of rust-rand and rust-rand-xorshift were too old,
leading cargo to error out.

Regression most likely introduced in
a2b7abb99bd5396c59b404160c3bafa810be470a.

* gnu/packages/crates-io.scm (rust-rayon-1)[arguments]: Depend on
RUST-RAND-0.8 and RUST-RAND-XORSHIFT-0.3.

gnu/packages/crates-io.scm
@@ -41280,8 +41280,8 @@ accessors.")
        #:cargo-development-inputs
        (("rust-docopt" ,rust-docopt-1)
         ("rust-lazy-static" ,rust-lazy-static-1)
-        ("rust-rand" ,rust-rand-0.7)
-        ("rust-rand-xorshift" ,rust-rand-xorshift-0.2)
+        ("rust-rand" ,rust-rand-0.8)
+        ("rust-rand-xorshift" ,rust-rand-xorshift-0.3)
         ("rust-serde" ,rust-serde-1))))
     (home-page "https://github.com/rayon-rs/rayon")
     (synopsis "Simple work-stealing parallelism for Rust")