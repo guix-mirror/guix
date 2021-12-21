0b2d0d1148b7b462c34de3310dbbb4300ac6ebd3
Julien Lepiller <julien@lepiller.eu>
2021-12-21 23:59:56 +0100

    gnu: java-log4j-api: Update to 2.17.0.
    * gnu/packages/java.scm (java-log4j-api): Update to 2.17.0.

--- gnu/packages/java.scm
+++ gnu/packages/java.scm
@@ -7812,14 +7812,14 @@ JavaMail API.")
 (define-public java-log4j-api
   (package
     (name "java-log4j-api")
-    (version "2.16.0")
+    (version "2.17.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://apache/logging/log4j/" version
                                   "/apache-log4j-" version "-src.tar.gz"))
               (sha256
                (base32
-                "1943lsqf3rywh11h6g5apyqm25am42cvrxpfc8sb9k8aqhlfvl0a"))))
+                "1bn9hrxyvw2d29z7mcd0frcqa8mbxmq59zb6b930zibkq68n1g01"))))
     (build-system ant-build-system)
     (arguments
      `(#:tests? #f ; tests require unpackaged software
