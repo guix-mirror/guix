8760007e521852027beec7a8ecfc2c716be4f5d6
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-12-27 18:54:04 +0100

    gnu: Add rust-atomic-polyfill-0.1.
    * gnu/packages/crates-io.scm (rust-atomic-polyfill-0.1): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -4199,6 +4199,29 @@ methods.")
     (description "This package provides a safe abstraction around AtomicPtr.")
     (license license:asl2.0)))
 
+(define-public rust-atomic-polyfill-0.1
+  (package
+    (name "rust-atomic-polyfill")
+    (version "0.1.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "atomic-polyfill" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0hk08pdp6dgxqhy05l75518svrrrm7c1shfn51dk4claad4dg1p6"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-critical-section" ,rust-critical-section-0.2)
+        ("rust-riscv-target" ,rust-riscv-target-0.1))))
+    (home-page "https://github.com/embassy-rs/atomic-polyfill")
+    (synopsis "Atomic polyfills, for targets where they're not available")
+    (description "This package provides atomic polyfills, for targets where
+they're not available.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-atomic-waker-1
   (package
     (name "rust-atomic-waker")
