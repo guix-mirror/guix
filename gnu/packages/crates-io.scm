a2e0731375ddd532bf39871f001cf1e92b7f48a5
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-12-27 18:50:07 +0100

    gnu: Add rust-embedded-hal-0.2.
    * gnu/packages/crates-io.scm (rust-embedded-hal-0.2): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -16836,6 +16836,29 @@ and authentication, easy email builders, and async support.")
 inclusion of Windows resources in the most resilient fashion imaginable.")
     (license license:expat)))
 
+(define-public rust-embedded-hal-0.2
+  (package
+    (name "rust-embedded-hal")
+    (version "0.2.6")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "embedded-hal" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1faa71mvs9zklyiiy9l5br9f2bwmxwak0br7jb49cr8mzxignv73"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-nb" ,rust-nb-0.1)
+        ("rust-void" ,rust-void-1))))
+    (home-page "https://github.com/rust-embedded/embedded-hal")
+    (synopsis "Hardware Abstraction Layer (HAL) for embedded systems")
+    (description "This package provides a Hardware Abstraction Layer (HAL) for
+embedded systems.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-ena-0.14
   (package
     (name "rust-ena")
