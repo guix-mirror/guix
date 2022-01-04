de099ca5b922fdbf069d9adc7e509512b87b98f6
Tobias Geerinckx-Rice <me@tobias.gr>
2022-01-04 19:56:51 +0100

    gnu: moreutils: Use G-expressions.
    * gnu/packages/moreutils.scm (moreutils)[arguments]:
    Rewrite as G-expressions.

--- gnu/packages/moreutils.scm
+++ gnu/packages/moreutils.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2015 Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
 ;;; Copyright © 2016, 2017, 2019 Efraim Flashner <efraim@flashner.co.il>
-;;; Copyright © 2016–2018, 2020, 2021 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2016–2018, 2020–2022 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -19,6 +19,7 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages moreutils)
+  #:use-module (guix gexp)
   #:use-module ((guix licenses) #:prefix l:)
   #:use-module (guix packages)
   #:use-module (guix download)
@@ -47,22 +48,22 @@
     (inputs
      (list perl perl-timedate perl-time-duration))
     (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'install 'wrap-program
-                    (lambda* (#:key outputs #:allow-other-keys)
-                      (let* ((out (assoc-ref outputs "out")))
-                        (wrap-program
-                            (string-append out "/bin/ts")
-                          `("PERL5LIB" ":" prefix (,(getenv "PERL5LIB")))))))
-         (delete 'configure))           ; no configure script
-       #:make-flags
-       (list (string-append "PREFIX=" (assoc-ref %outputs "out"))
-             (string-append "DOCBOOKXSL="
-                            (assoc-ref %build-inputs "docbook-xsl") "/xml/xsl/"
-                            ,(package-name docbook-xsl) "-"
-                            ,(package-version docbook-xsl))
-             (string-append "CC=" ,(cc-for-target)))))
+     (list #:phases
+           #~(modify-phases %standard-phases
+               (add-after 'install 'wrap-program
+                 (lambda _
+                   (wrap-program
+                       (string-append #$output "/bin/ts")
+                     `("PERL5LIB" ":" prefix (,(getenv "PERL5LIB"))))))
+               (delete 'configure))     ; no configure script
+           #:make-flags
+           #~(list (string-append "PREFIX=" #$output)
+                   (string-append "DOCBOOKXSL="
+                                  #$(this-package-native-input "docbook-xsl")
+                                  "/xml/xsl/docbook-xsl-"
+                                  #$(package-version (this-package-native-input
+                                                      "docbook-xsl")))
+                   (string-append "CC=" #$(cc-for-target)))))
     (home-page "https://joeyh.name/code/moreutils/")
     (synopsis "Miscellaneous general-purpose command-line tools")
     (description
