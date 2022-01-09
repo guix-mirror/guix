b168c9f441d13718446fecee02205a7167cb43cc
Efraim Flashner <efraim@flashner.co.il>
2022-01-09 10:58:54 +0200

    gnu: vim: Remove unneeded make-flag.
    Upstream reports this fixed with 8.2.3406.
    
    * gnu/packages/vim.scm (vim)[arguments]: Remove make-flags.
    (vim-full)[arguments]: Adjust accordingly.

--- gnu/packages/vim.scm
+++ gnu/packages/vim.scm
@@ -89,8 +89,6 @@
     (arguments
      `(#:test-target "test"
        #:parallel-tests? #f
-       ;; Fix test_signals.vim. https://github.com/vim/vim/issues/7402
-       #:make-flags (list "CFLAGS=-D_REENTRANT")
        #:phases
        (modify-phases %standard-phases
          (add-after 'configure 'patch-absolute-paths
@@ -225,7 +223,7 @@ with the editor vim.")))
            ((#:make-flags flags)
             `(append
               (list "LDFLAGS=-lexpat")
-              (delete "CFLAGS=-D_REENTRANT" ,flags)))
+              ,flags))
            ((#:phases phases)
             `(modify-phases ,phases
                (add-before 'check 'start-xserver
