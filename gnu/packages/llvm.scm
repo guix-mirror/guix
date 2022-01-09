guix-mirror/guix

commit a05e573d60bf64129abf4d4e5f27355c84ebb2ee

gnu: lld: Update to 13.0.0.
* gnu/packages/llvm.scm (lld): Update to 13.0.0.
[inputs]: Change from LLVM-12 to LLVM-13.

gnu/packages/llvm.scm
@@ -1122,20 +1122,20 @@ of programming tools as well as libraries with equivalent functionality.")
 (define-public lld
   (package
     (name "lld")
-    (version "12.0.1")
+    (version "13.0.0")
     (source (origin
               (method url-fetch)
               (uri (llvm-uri "lld" version))
               (sha256
                (base32
-                "0qg3fgc7wj34hdkqn21y03zcmsdd01szhhm1hfki63iifrm3y2v9"))))
+                "11lkwv4jy35z2f3zcpv7hbbk9v9gpavfvxlif8265zv4rl5r1l90"))))
     (build-system cmake-build-system)
     (native-inputs
      ;; Note: check <https://bugs.llvm.org/show_bug.cgi?id=49228> to see
      ;; whether this is still necessary.
      (list libunwind-headers))
     (inputs
-     (list llvm-12))
+     (list llvm-13))
     (arguments
      `(#:build-type "Release"
        ;; TODO: Tests require the lit tool, which isn't installed by the LLVM

commit 8993d9663606eeb075725daa669482d428d2356d

gnu: zig: Build with LLD 12.
This fixes a regression introduced in a05e573d60bf64129abf4d4e5f27355c84ebb2ee
as the LLD version must match the LLVM version.

* gnu/packages/llvm.scm (lld-12): New variable.
* gnu/packages/zig.scm (zig)[inputs]: Change from LLD to LLD-12.

gnu/packages/llvm.scm
@@ -1139,6 +1139,19 @@ of programming tools as well as libraries with equivalent functionality.")
 components which highly leverage existing libraries in the larger LLVM Project.")
     (license license:asl2.0))) ; With LLVM exception
 
+(define-public lld-12
+  (package
+    (inherit lld)
+    (version "12.0.1")
+    (source (origin
+              (method url-fetch)
+              (uri (llvm-uri "lld" version))
+              (sha256
+               (base32
+                "0qg3fgc7wj34hdkqn21y03zcmsdd01szhhm1hfki63iifrm3y2v9"))))
+    (inputs (modify-inputs (package-inputs lld)
+              (replace "llvm" llvm-12)))))
+
 (define* (make-lld-wrapper lld #:key lld-as-ld?)
   "Return a LLD wrapper.  When LLD-AS-LD? is true, create a 'ld' symlink that
 points to 'lld'."

gnu/packages/zig.scm
@@ -45,7 +45,7 @@
     (build-system cmake-build-system)
     (inputs
      (list clang-12 ; Clang propagates llvm.
-           lld))
+           lld-12))
     ;; Zig compiles fine with GCC, but also needs native LLVM libraries.
     (native-inputs
      (list llvm-12))