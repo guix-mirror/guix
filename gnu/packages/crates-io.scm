6ffa11b4eea9548d2077768302e483c594c53f94
Z572 <873216071@qq.com>
2022-01-12 23:27:26 +0800

    gnu: Add rust-windows-i686-msvc-0.28.
    * gnu/packages/crates-io.scm (rust-windows-i686-msvc-0.28): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -66615,6 +66615,25 @@ crate.")
 crate.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-windows-i686-msvc-0.28
+  (package
+    (name "rust-windows-i686-msvc")
+    (version "0.28.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "windows_i686_msvc" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0r0z8s1wcdwd20azsdfilf2a6bz68xkavl990wy64hyc8f51bmai"))))
+    (build-system cargo-build-system)
+    (arguments `(#:skip-build? #t))
+    (home-page "https://github.com/microsoft/windows-rs")
+    (synopsis "Code gen support for the windows crate")
+    (description "This package provides code gen support for the windows
+crate.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-windows-gen-0.9
   (package
     (name "rust-windows-gen")
