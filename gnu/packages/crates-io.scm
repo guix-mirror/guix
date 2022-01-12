6992626793f22aaa6359892ee6ae3c4f4315d05c
Z572 <873216071@qq.com>
2022-01-12 23:27:52 +0800

    gnu: Add rust-lsp-types-0.91.
    * gnu/packages/crates-io.scm (rust-lsp-types-0.91): New variable.
    (rust-lsp-types-0.89): Inherit from above.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -29852,34 +29852,58 @@ key-value pairs.")
         (base32
          "0jxsgkn378kxkiqdshdjdclw5wwp2xaz45cqd3yw85fhn8a38fza"))))))
 
-(define-public rust-lsp-types-0.89
+(define-public rust-lsp-types-0.91
   (package
     (name "rust-lsp-types")
-    (version "0.89.2")
+    (version "0.91.1")
     (source
-      (origin
-        (method url-fetch)
-        (uri (crate-uri "lsp-types" version))
-        (file-name
-          (string-append name "-" version ".tar.gz"))
-        (sha256
-          (base32
-            "18h9dckjy11pz2lpd5lm12vvfcds1qg67clqalr35k2jzpnhsbl5"))))
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lsp-types" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "1bk8iy7s80i2iqj1siyfm0rsdq2vwvz7lclsrcrx2pa2b4n32s13"))))
     (build-system cargo-build-system)
     (arguments
-      `(#:skip-build? #t
-        #:cargo-inputs
-        (("rust-bitflags" ,rust-bitflags-1)
-         ("rust-serde" ,rust-serde-1)
-         ("rust-serde-json" ,rust-serde-json-1)
-         ("rust-serde-repr" ,rust-serde-repr-0.1)
-         ("rust-url" ,rust-url-2))))
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-bitflags" ,rust-bitflags-1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-serde-repr" ,rust-serde-repr-0.1)
+        ("rust-url" ,rust-url-2))))
     (home-page "https://github.com/gluon-lang/lsp-types")
     (synopsis "Types for interaction with a language server")
     (description
      "This package provides types useful for interacting with a language
 server (LSP).")
     (license license:expat)))
+
+(define-public rust-lsp-types-0.89
+  (package
+    (inherit rust-lsp-types-0.91)
+    (name "rust-lsp-types")
+    (version "0.89.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lsp-types" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "18h9dckjy11pz2lpd5lm12vvfcds1qg67clqalr35k2jzpnhsbl5"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-bitflags" ,rust-bitflags-1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-serde-repr" ,rust-serde-repr-0.1)
+        ("rust-url" ,rust-url-2))))))
 
 (define-public rust-lsp-types-0.83
   (package
