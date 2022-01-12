df35058ecf0ee77e3fc7c0bd287631f6a1802764
Z572 <873216071@qq.com>
2022-01-12 23:27:24 +0800

    gnu: Add rust-windows-x86-64-msvc-0.28.
    * gnu/packages/crates-io.scm (rust-windows-x86-64-msvc-0.28): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -66577,6 +66577,25 @@ configuration file and/or environment variables.")
     (description "Rust for Windows")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-windows-x86-64-msvc-0.28
+  (package
+    (name "rust-windows-x86-64-msvc")
+    (version "0.28.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "windows_x86_64_msvc" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "17z8q25pd3dp6b84qm9nlayd3ym78sbryxlqmgcxvz9vpmy8qarz"))))
+    (build-system cargo-build-system)
+    (arguments `(#:skip-build? #t))
+    (home-page "https://github.com/microsoft/windows-rs")
+    (synopsis "Code gen support for the windows crate")
+    (description "This package provides code gen support for the windows
+crate.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-windows-gen-0.9
   (package
     (name "rust-windows-gen")
