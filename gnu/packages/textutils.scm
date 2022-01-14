aeeaf4eef40811d2f934cff914ba31dbbf04d449
jgart <jgart@dismail.de>
2022-01-14 03:33:10 -0500

    gnu: Add utf-8-lineseparator.
    * gnu/packages/textutils.scm (utf-8-lineseparator): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/textutils.scm
+++ gnu/packages/textutils.scm
@@ -1374,6 +1374,39 @@ around it also has a list of style guides implemented with Vale in
 @url{https://github.com/errata-ai/styles, their styles repo}.")
     (license license:expat)))
 
+(define-public utf-8-lineseparator
+  (package
+    (name "utf-8-lineseparator")
+    (version "cj3")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/pflanze/utf-8-lineseparator")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1xnbcanqn5jr965gw9195ij6hz04clfm77m5776dysn9nykn20w1"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list
+         (string-append "CC=" ,(cc-for-target)))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (replace 'install
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (bin (string-append out "/bin")))
+               (install-file "utf-8-lineseparator" bin)))))))
+    (home-page "https://github.com/pflanze/utf-8-lineseparator")
+    (synopsis "Line ending detection library")
+    (description
+"@code{utf-8-lineseparator} provides a tool to efficiently check text
+files for valid UTF-8 use and to report which line endings they use.")
+    (license license:expat)))
+
 (define-public csvdiff
   (package
     (name "csvdiff")
