100fc7507b771542419df22327f6c588b00f2c86
Aleksandr Vityazev <avityazev@posteo.org>
2022-01-17 13:07:47 +0000

    gnu: Add rust-fiat-crypto-0.1.
    * gnu/packages/crates-io.scm (rust-fiat-crypto-0.1): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -19586,6 +19586,24 @@ Atom, RSS 2.0, RSS 1.0, RSS 0.x and JSON Feed")
     ;; No copyright headers in the source code.  LICENSE indicates gpl3.
     (license license:gpl3)))
 
+(define-public rust-fiat-crypto-0.1
+  (package
+    (name "rust-fiat-crypto")
+    (version "0.1.11")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "fiat-crypto" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "12gyr01z3bkahbxcrj2nsdq42qbqgq12w32dvpr6a7930i0qfxi1"))))
+    (build-system cargo-build-system)
+    (home-page "https://github.com/mit-plv/fiat-crypto")
+    (synopsis "Fiat-crypto generated Rust")
+    (description "This crate provides the extracted Rust code from the Coq
+@code{fiat-crypto} libraries.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-filesize-0.2
   (package
     (name "rust-filesize")
