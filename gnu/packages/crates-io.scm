de473a7a5fdd7b34b3609ed631a3fb899ed7478e
Aleksandr Vityazev <avityazev@posteo.org>
2022-01-17 13:26:07 +0000

    gnu: Add rust-cookie-store-0.15.
    * gnu/packages/crates-io.scm (rust-cookie-store-0.15): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -12024,6 +12024,36 @@ management.  It supports signed and private (encrypted, authenticated) jars.")
        #:cargo-development-inputs
        (("rust-version-check" ,rust-version-check-0.9))))))
 
+(define-public rust-cookie-store-0.15
+  (package
+    (name "rust-cookie-store-15")
+    (version "0.15.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "cookie-store" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0z0navy9k0ivrdvz492q8c4nhd3iv5l77hwfppskdp1j15607xxk"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-cookie" ,rust-cookie-0.15)
+        ("rust-idna" ,rust-idna-0.2)
+        ("rust-indexmap" ,rust-indexmap-1)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-publicsuffix" ,rust-publicsuffix-2)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-time" ,rust-time-0.2)
+        ("rust-url" ,rust-url-2))))
+    (home-page "https://github.com/pfernie/cookie_store")
+    (synopsis "Cookie storage and retrieval")
+    (description "This package implements cookie storage and retrieval.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-cookie-store-0.12
   (package
     (name "rust-cookie-store")
