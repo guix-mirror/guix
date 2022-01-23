c486d20dc777642d4a5127f693858241df629640
Ricardo Wurmus <rekado@elephly.net>
2022-01-23 13:33:29 +0100

    gnu: r-activepathways: Update to 1.0.4.
    * gnu/packages/bioconductor.scm (r-activepathways): Update to 1.0.4.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -11457,14 +11457,14 @@ using whole genome sequencing data.")
 (define-public r-activepathways
   (package
     (name "r-activepathways")
-    (version "1.0.3")
+    (version "1.0.4")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "ActivePathways" version))
        (sha256
         (base32
-         "0xcn2ymxb4npk1q8hap1ckj27mrkici44wjckc9v42mija29n8wk"))))
+         "1prhwx0nnwy2q62l2r0z31mhk4mq6xdr6mjihdlwpwgwq4rfi60y"))))
     (properties
      `((upstream-name . "ActivePathways")))
     (build-system r-build-system)
