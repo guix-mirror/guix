d7085a3c5001635ec2f63dabdd970ecf7d64a3f8
Efraim Flashner <efraim@flashner.co.il>
2022-01-23 20:14:43 +0200

    gnu: python-pillow: Add cpe-name.
    * gnu/packages/python-xyz.scm (python-pillow)[properties]: Add cpe-name.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -7136,7 +7136,8 @@ efficient internal representation, and fairly powerful image processing
 capabilities.  The core image library is designed for fast access to data
 stored in a few basic pixel formats.  It should provide a solid foundation for
 a general image processing tool.")
-    (properties `((python2-variant . ,(delay python2-pillow))))
+    (properties `((python2-variant . ,(delay python2-pillow))
+                  (cpe-name . "pillow")))
     (license (license:x11-style
               "http://www.pythonware.com/products/pil/license.htm"
               "The PIL Software License"))))
