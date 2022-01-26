1121fa432f0e6422d5f9ebb96fb0014c4d5231f5
Leo Famulari <leo@famulari.name>
2022-01-26 14:23:03 -0500

    gnu: Borg: Improve the description.
    Attic... Now that's a name I've not heard in a long time.
    
    * gnu/packages/backup.scm (borg)[description]: Improve it.

--- gnu/packages/backup.scm
+++ gnu/packages/backup.scm
@@ -751,8 +751,8 @@ detection, and lossless compression.")
 supports compression and authenticated encryption.  The main goal of Borg is to
 provide an efficient and secure way to backup data.  The data deduplication
 technique used makes Borg suitable for daily backups since only changes are
-stored.  The authenticated encryption technique makes it suitable for backups
-to not fully trusted targets.  Borg is a fork of Attic.")
+stored.  The authenticated encryption technique makes it suitable for
+storing backups on untrusted computers.")
     (home-page "https://www.borgbackup.org/")
     (license license:bsd-3)))
 
