c47dc7ffd757d7964d120fd00bce54a007f743e8
Andrew Tropin <andrew@trop.in>
2022-02-06 07:58:46 +0300

    gnu: Add emacs-consult-dir.
    * gnu/packages/emacs-xyz.scm (emacs-consult-dir): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -8381,6 +8381,31 @@ Emacs completion function completing-read, which allows quickly selecting from a
 list of candidates.")
     (license license:gpl3+)))
 
+(define-public emacs-consult-dir
+  (package
+    (name "emacs-consult-dir")
+    (version "0.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/karthink/consult-dir")
+                    (commit (string-append "v" version))))
+              (sha256
+               (base32 "1cff4ssrn1mw2s5n090pdmwdirnfih8idg5f0ll2bi2djc4hq5kn"))
+              (file-name (git-file-name name version))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-consult))
+    (home-page "https://github.com/karthink/consult-dir")
+    (synopsis "Insert paths into minibuffer prompts in Emacs")
+    (description "Consult-dir allows you to easily insert directory
+paths into the minibuffer prompt in Emacs.
+
+When using the minibuffer, you can switch---with completion and filtering
+provided by your completion setup---to any directory you’ve visited recently,
+or to a project or bookmarked directory.  The minibuffer prompt will be
+replaced with the directory you choose.")
+    (license license:gpl3+)))
+
 (define-public emacs-consult-notmuch
   (package
     (name "emacs-consult-notmuch")
