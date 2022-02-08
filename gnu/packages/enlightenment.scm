c8af59e845ab1372082a4d00d5da46fd13344d20
Efraim Flashner <efraim@flashner.co.il>
2022-02-08 12:06:56 +0200

    gnu: efl: Update to 1.26.2.
    * gnu/packages/enlightenment.scm (efl): Update to 1.26.2.

--- gnu/packages/enlightenment.scm
+++ gnu/packages/enlightenment.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2015 Tomáš Čech <sleep_walker@suse.cz>
 ;;; Copyright © 2015 Daniel Pimentel <d4n1@member.fsf.org>
-;;; Copyright © 2015, 2016, 2017, 2018, 2019, 2020, 2021 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2017 Nikita <nikita@n0.is>
 ;;; Copyright © 2018, 2019, 2020 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2018 Timo Eisenmann <eisenmann@fn.de>
@@ -70,7 +70,7 @@
 (define-public efl
   (package
     (name "efl")
-    (version "1.26.1")
+    (version "1.26.2")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -78,7 +78,7 @@
                     version ".tar.xz"))
               (sha256
                (base32
-                "0hm6i1f2g4mwj726rc6na38xhys1plbv9swrlc9hrpa87mz6gac6"))))
+                "071h0pscbd8g341yy5rz9mk1xn8yhryldhl6mmr1y6lafaycyy99"))))
     (build-system meson-build-system)
     (native-inputs
      `(("check" ,check)
